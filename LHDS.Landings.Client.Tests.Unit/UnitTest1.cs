12bef44dde0a3ce53fcbe6e3708f65b83c2cbfe6
Christo du Toit <christo@dutoit.co.uk>
2023-01-23 10:22:31 +0000

    CODE RUB: Code Cleanup

--- LHDS.Landings.Client.Tests.Unit/UnitTest1.cs
+++ LHDS.Landings.Client.Tests.Unit/UnitTest1.cs
@@ -1,3 +1,7 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
 namespace LHDS.Landings.Client.Tests.Unit
 {
     public class UnitTest1

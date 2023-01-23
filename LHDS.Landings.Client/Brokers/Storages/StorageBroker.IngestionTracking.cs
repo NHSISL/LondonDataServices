2e9cdcfdfe19516e7a1c94b14729be4568310b11
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-01-23 14:12:30 +0000

    BROKER: Adding Empty Storage Broker

---
+++ LHDS.Landings.Client/Brokers/Storages/StorageBroker.IngestionTracking.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Security.Policy;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace LHDS.Landings.Client.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Site> Sites { get; set; }
+    }
+}

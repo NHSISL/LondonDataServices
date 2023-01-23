1fd8d55c12e6bea954a1e0e2a06d4e6128344a51
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-01-23 14:20:41 +0000

    Revert "Revert "BROKER: Adding Empty Storage Broker""
    This reverts commit ab5d3d932b9e91be52c184179564680f5209e9c7.

---
+++ LHDS.Landings.Client/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,50 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using EFxceptions;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace LHDS.Landings.Client.Brokers.Storages
+{
+    public partial class StorageBroker : EFxceptionsContext, IStorageBroker
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private static void AddConfigurations(ModelBuilder modelBuilder)
+        {
+            AddIngestionTrackingConfigurations(modelBuilder);
+        }
+
+        private void AddSeedData(ModelBuilder modelBuilder)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            string connectionString = this.configuration.GetConnectionString(name: "DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public override void Dispose() { }
+    }
+}

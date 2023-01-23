a5c5bd142a556dd3da3353095f76f4512f5f535d
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-01-23 16:17:45 +0000

    CODE RUB: Correcting Storage Broker

--- LHDS.Landings.Client/Brokers/Storages/StorageBroker.IngestionTracking.cs
+++ LHDS.Landings.Client/Brokers/Storages/StorageBroker.IngestionTracking.cs
@@ -3,13 +3,13 @@
 // ---------------------------------------------------------------
 
 using System.Security.Policy;
-
+using LHDS.Landings.Client.Models.IngestionTracking;
 using Microsoft.EntityFrameworkCore;
 
 namespace LHDS.Landings.Client.Brokers.Storages
 {
     public partial class StorageBroker
     {
-        public DbSet<Site> Sites { get; set; }
+        public DbSet<IngestionTracking> IngestionTrackings { get; set; }
     }
 }

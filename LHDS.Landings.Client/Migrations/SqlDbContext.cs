ccc9c675dceea6dccd444cb7c1e47a6769e99abd
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-01-23 15:06:51 +0000

    CODE RUB: Adding Migration

---
+++ LHDS.Landings.Client/Migrations/SqlDbContext.cs
@@ -0,0 +1,12 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Data.Entity;
+
+
+namespace LHDS.Landings.Client.Migrations
+{
+    public partial class SqlDbContext : DbContext
+    { }
+}

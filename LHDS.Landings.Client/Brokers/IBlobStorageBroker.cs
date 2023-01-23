12521744c389b79d7d34b75dcdf46aeba50f5a5e
davidhayes1981 <107634691+davidhayes1981@users.noreply.github.com>
2023-01-23 14:36:14 +0000

    CODE RUB: Fix Spacing

--- LHDS.Landings.Client/Brokers/IBlobStorageBroker.cs
+++ LHDS.Landings.Client/Brokers/IBlobStorageBroker.cs
@@ -2,7 +2,6 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
-using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -11,7 +10,6 @@ namespace LHDS.Landings.Client.Brokers
     public interface IBlobStorageBroker
     {
         ValueTask InsertFileAsync(string fileName, Stream stream, string container);
-
-        ValueTask<string> SelectDownloadFileLinkAsync(string fileName, string container, DateTimeOffset expiresOn);
+        ValueTask<string> SelectDownloadFileLinkAsync(string fileName, string container);
     }
 }

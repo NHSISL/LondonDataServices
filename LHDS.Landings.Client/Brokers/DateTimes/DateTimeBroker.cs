7ff225b812aa94f73f4a8c220f5fefbe35e1c611
Christo du Toit <christo@dutoit.co.uk>
2023-01-23 10:06:05 +0000

    BROKERS: DateTime - Get Current DateTime Offset

---
+++ LHDS.Landings.Client/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace LHDS.Landings.Client.Brokers.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTimeOffset() =>
+            DateTimeOffset.UtcNow;
+    }
+}

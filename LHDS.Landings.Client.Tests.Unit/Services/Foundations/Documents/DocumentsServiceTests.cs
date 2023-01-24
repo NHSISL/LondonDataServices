9c464081ae91b3b1f8e9c0dc16e4a7f70135abda
davidhayes1981 <107634691+davidhayes1981@users.noreply.github.com>
2023-01-24 14:44:59 +0000

    ShouldAddFileAsync -> FAIL

---
+++ LHDS.Landings.Client.Tests.Unit/Services/Foundations/Documents/DocumentsServiceTests.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using LHDS.Landings.Client.Brokers;
+using LHDS.Landings.Client.Brokers.DateTimes;
+using LHDS.Landings.Client.Brokers.Loggings;
+using LHDS.Landings.Client.Services.Foundations.Downloads;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace LHDS.Landings.Client.Tests.Unit.Services.Foundations.Downloads
+{
+    public partial class DocumentsServiceTests
+    {
+        private readonly Mock<IBlobStorageBroker> blobStorageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly IConfiguration inMemoryConfiguration;
+        private readonly IDocumentService documentService;
+
+        public DocumentsServiceTests()
+        {
+            this.blobStorageBrokerMock = new Mock<IBlobStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+
+            var appSettingsStub = new Dictionary<string, string> {
+                {"blobContainerName", GetRandomString()}
+            };
+
+            this.inMemoryConfiguration = new ConfigurationBuilder()
+                .AddInMemoryCollection(appSettingsStub)
+                .Build();
+
+            this.documentService = new DocumentService(
+                blobStorageBroker: this.blobStorageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object,
+                configuration: this.inMemoryConfiguration);
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+    }
+}
---
+++ LHDS.Landings.Client/Services/Foundations/Documents/DocumentService.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using LHDS.Landings.Client.Brokers;
+using LHDS.Landings.Client.Brokers.Loggings;
+using LHDS.Landings.Client.Models.Foundations.Documents;
+using Microsoft.Extensions.Configuration;
+
+namespace LHDS.Landings.Client.Services.Foundations.Downloads
+{
+    public partial class DocumentService : IDocumentService
+    {
+        private readonly IBlobStorageBroker blobStorageBroker;
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IConfiguration configuration;
+
+        public DocumentService(IBlobStorageBroker blobStorageBroker, ILoggingBroker loggingBroker, IConfiguration configuration)
+        {
+            this.blobStorageBroker = blobStorageBroker;
+            this.loggingBroker = loggingBroker;
+            this.configuration = configuration;
+        }
+
+        public async ValueTask AddDocumentAsync(Document documentName) =>
+            new NotImplementedException();
+    }
+}

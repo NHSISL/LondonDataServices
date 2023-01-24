1c6444d2e61406b1c1941ab909c9df7984e3cc8f
davidhayes1981 <107634691+davidhayes1981@users.noreply.github.com>
2023-01-24 15:27:59 +0000

    ShouldThrowValidationExceptionsOnAddIfDocumentIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Landings.Client/Models/Foundations/Documents/Exceptions/NullDocumentException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Landings.Client.Models.Foundations.Documents.Exceptions
+{
+    public class NullDocumentException : Xeption
+    {
+        public NullDocumentException(Document document)
+            : base(message: $"Couldn't find document with fileName: {document}")
+        { }
+    }
+}

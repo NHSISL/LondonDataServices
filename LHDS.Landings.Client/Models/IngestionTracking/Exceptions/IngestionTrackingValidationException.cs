627cd2a41e25c5aec729a57580728ace38bdd6d6
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-01-24 21:01:49 +0000

    ShouldThrowValidationExceptionOnAddIfIngestionTrackingIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Landings.Client/Models/IngestionTracking/Exceptions/IngestionTrackingValidationException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Landings.Client.Models.IngestionTracking.Exceptions
+{
+    public class IngestionTrackingValidationException : Xeption
+    {
+        public IngestionTrackingValidationException(Xeption innerException)
+            : base(message: "Document validation errors occurred, please try again.",
+                  innerException)
+        { }
+    }
+}
---
+++ LHDS.Landings.Client/Models/IngestionTracking/Exceptions/NullIngestionTrackingException.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xeptions;
+
+namespace LHDS.Landings.Client.Models.IngestionTracking.Exceptions
+{
+    public class NullIngestionTrackingException : Xeption
+    {
+        public NullIngestionTrackingException() 
+            : base(message: "Ingestion tracking is null.")
+        { }
+    }
+}

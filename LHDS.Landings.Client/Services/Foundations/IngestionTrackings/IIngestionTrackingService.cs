15401076020994cdd9c23ed5bcbe61b2b9b9b75c
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-01-24 15:54:34 +0000

    ShouldAddIngestionTrackingAsync -> FAIL

---
+++ LHDS.Landings.Client/Services/Foundations/IngestionTrackings/IIngestionTrackingService.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using LHDS.Landings.Client.Models.IngestionTracking;
+
+namespace LHDS.Landings.Client.Services.Foundations.IngestionTrackings
+{
+    public interface IIngestionTrackingService
+    {
+        ValueTask<IngestionTracking> AddIngestionTrackingAsync(IngestionTracking ingestionTracking);
+    }
+}

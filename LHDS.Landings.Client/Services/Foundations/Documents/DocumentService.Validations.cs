c2e95e9f8e56e2e444f5edefbcdd7dca9b075162
davidhayes1981 <107634691+davidhayes1981@users.noreply.github.com>
2023-01-25 09:46:38 +0000

    ShouldThrowValidationExceptionOnAddIfDocumentStreamIsInvalidAndLogItAsync -> PASS

--- LHDS.Landings.Client/Services/Foundations/Documents/DocumentService.Validations.cs
+++ LHDS.Landings.Client/Services/Foundations/Documents/DocumentService.Validations.cs
@@ -2,19 +2,53 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.IO;
 using LHDS.Landings.Client.Models.Foundations.Documents;
 using LHDS.Landings.Client.Models.Foundations.Documents.Exceptions;
+using NEL.Premises.Api.Models.Documents.Exceptions;
 
 namespace LHDS.Landings.Client.Services.Foundations.Downloads
 {
     public partial class DocumentService
     {
+        private static void ValidateDocument(Document document)
+        {
+            ValidateDocumentIsNotNull(document);
+
+            Validate(
+                    (Rule: IsInvalid(document.DocumentStream), Parameter: nameof(Document.DocumentStream))
+                );
+        }
+
         private static void ValidateDocumentIsNotNull(Document document)
         {
             if (document is null)
             {
                 throw new NullDocumentException();
             }
+        }
+
+        private static dynamic IsInvalid(Stream stream) => new
+        {
+            Condition = (stream == null || stream.Length == 0),
+            Message = "Stream is required"
+        };
+
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidDocumentException = new InvalidDocumentException();
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidDocumentException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidDocumentException.ThrowIfContainsErrors();
         }
     }
 }

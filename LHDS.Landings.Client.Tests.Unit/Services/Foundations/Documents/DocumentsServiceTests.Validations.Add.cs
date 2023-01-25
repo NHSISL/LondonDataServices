009a21df54db10492df5f7763f0a6bb35a739499
davidhayes1981 <107634691+davidhayes1981@users.noreply.github.com>
2023-01-25 09:45:55 +0000

    ShouldThrowValidationExceptionOnAddIfDocumentStreamIsInvalidAndLogItAsync -> FAIL

--- LHDS.Landings.Client.Tests.Unit/Services/Foundations/Documents/DocumentsServiceTests.Validations.Add.cs
+++ LHDS.Landings.Client.Tests.Unit/Services/Foundations/Documents/DocumentsServiceTests.Validations.Add.cs
@@ -2,11 +2,14 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.IO;
 using System.Threading.Tasks;
 using FluentAssertions;
 using LHDS.Landings.Client.Models.Foundations.Documents;
 using LHDS.Landings.Client.Models.Foundations.Documents.Exceptions;
+using Microsoft.Extensions.Configuration;
 using Moq;
+using NEL.Premises.Api.Models.Documents.Exceptions;
 
 namespace LHDS.Landings.Client.Tests.Unit.Services.Foundations.Downloads
 {
@@ -43,6 +46,53 @@ namespace LHDS.Landings.Client.Tests.Unit.Services.Foundations.Downloads
             this.blobStorageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        public async Task ShouldThrowValidationExceptionOnAddIfDocumentStreamIsInvalidAndLogItAsync(Stream invalidInput)
+        {
+            // Given
+            var blobContainerName = this.inMemoryConfiguration.GetValue<string>("blobContainerName");
+
+            string validFileName = GetRandomString();
+            Stream invalidStream = invalidInput;
+
+            Document document = new Document
+            {
+                FileName = validFileName,
+                DocumentStream = invalidStream
+            };
+
+            var invalidDocumentException = new InvalidDocumentException();
+
+            invalidDocumentException.AddData(
+                 key: "DocumentStream",
+                 values: "Stream is required");
+
+            var expectedDocumentValidationException
+                = new DocumentValidationException(invalidDocumentException);
+
+            // When
+            ValueTask uploadFileTask = this.documentService.AddDocumentAsync(document);
+
+            DocumentValidationException actualDocumentValidationException =
+                await Assert.ThrowsAsync<DocumentValidationException>(uploadFileTask.AsTask);
+
+            // Then
+            actualDocumentValidationException.Should().BeEquivalentTo(expectedDocumentValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedDocumentValidationException))),
+                        Times.Once);
+
+            this.blobStorageBrokerMock.Verify(broker =>
+                broker.InsertFileAsync(validFileName, invalidStream, blobContainerName),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.blobStorageBrokerMock.VerifyNoOtherCalls();
         }
     }
 }

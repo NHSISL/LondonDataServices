f4c94e8a0efafadebfbd445bd991ddf16895d178
davidhayes1981 <107634691+davidhayes1981@users.noreply.github.com>
2023-01-25 10:36:46 +0000

    ShouldThrowServiceExceptionOnUploadFileIfServiceErrorOccursAndLogItAsync -> PASS

--- LHDS.Landings.Client.Tests.Unit/Services/Foundations/Documents/DocumentsServiceTests.Exceptions.Add.cs
+++ LHDS.Landings.Client.Tests.Unit/Services/Foundations/Documents/DocumentsServiceTests.Exceptions.Add.cs
@@ -2,6 +2,7 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
+using System;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -57,6 +58,52 @@ namespace LHDS.Landings.Client.Tests.Unit.Services.Foundations.Downloads
             this.loggingBrokerMock.Verify(broker =>
                  broker.LogError(It.Is(SameExceptionAs(
                      expectedDependencyException))),
+                         Times.Once);
+
+            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnUploadFileIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            var blobContainerName = this.inMemoryConfiguration.GetValue<string>("blobContainerName");
+            var randomString = GetRandomString();
+            var randomStream = new MemoryStream(Encoding.ASCII.GetBytes(GetRandomString()));
+            var randomMessage = GetRandomString();
+
+            Document document = new Document
+            {
+                FileName = randomString,
+                DocumentStream = randomStream
+            };
+
+            var serviceException = new Exception(randomMessage);
+            var failedDocumentServiceException = new FailedDocumentServiceException(serviceException);
+
+            var expectedDocumentServiceException =
+                new DocumentServiceException(failedDocumentServiceException);
+
+            this.blobStorageBrokerMock.Setup(broker =>
+                 broker.InsertFileAsync(document.FileName, document.DocumentStream, blobContainerName))
+                     .Throws(failedDocumentServiceException);
+
+            // when
+            ValueTask uploadFileTask = this.documentService.AddDocumentAsync(document);
+
+            var actualServiceException =
+                 await Assert.ThrowsAsync<DocumentServiceException>(uploadFileTask.AsTask);
+
+            // then
+            actualServiceException.Should().BeEquivalentTo(expectedDocumentServiceException);
+
+            this.blobStorageBrokerMock.Verify(broker =>
+                broker.InsertFileAsync(document.FileName, document.DocumentStream, blobContainerName),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedDocumentServiceException))),
                          Times.Once);
 
             this.blobStorageBrokerMock.VerifyNoOtherCalls();

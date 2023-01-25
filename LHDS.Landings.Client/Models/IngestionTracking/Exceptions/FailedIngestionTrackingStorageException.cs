0d4f37993469a556a0b07e56fdaca01eaec88d18
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-01-25 10:43:53 +0000

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Landings.Client/Models/IngestionTracking/Exceptions/FailedIngestionTrackingStorageException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace LHDS.Landings.Client.Models.IngestionTracking.Exceptions
+{
+    public class FailedIngestionTrackingStorageException : Xeption
+    {
+        public FailedIngestionTrackingStorageException(Exception innerException)
+            : base(message: "Failed ingestion tracking storage error occurred, contact support.")
+        { }
+    }
+}
---
+++ LHDS.Landings.Client/Models/IngestionTracking/Exceptions/IngestionTrackingDependencyException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Landings.Client.Models.IngestionTracking.Exceptions
+{
+    public class IngestionTrackingDependencyException : Xeption
+    {
+        public IngestionTrackingDependencyException(Xeption innerException) 
+            : base(message: "Ingestion tracking dependency error occurred, contact support.")
+        { }
+    }
+}

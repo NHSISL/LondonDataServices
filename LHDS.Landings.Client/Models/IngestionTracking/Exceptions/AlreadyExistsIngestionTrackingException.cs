cf6a6a1bd340da3a39302f4a42db1c2462fee3d4
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-01-25 12:34:11 +0000

    ShouldThrowDependencyValidationExceptionOnAddIfIngestionTrackingAlreadyExsitsAndLogItAsync -> FAIL

---
+++ LHDS.Landings.Client/Models/IngestionTracking/Exceptions/AlreadyExistsIngestionTrackingException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace LHDS.Landings.Client.Models.IngestionTracking.Exceptions
+{
+    public class AlreadyExistsIngestionTrackingException : Xeption
+    {
+        public AlreadyExistsIngestionTrackingException(Exception innerException)
+            : base(message: "Ingestion tracking with the same Id already exists.")
+        { }
+    }
+}
---
+++ LHDS.Landings.Client/Models/IngestionTracking/Exceptions/IngestionTrackingDependencyValidationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Landings.Client.Models.IngestionTracking.Exceptions
+{
+    public class IngestionTrackingDependencyValidationException : Xeption
+    {
+        public IngestionTrackingDependencyValidationException(Xeption innerException)
+            : base(message: "Ingestion tracking dependency validation occurred, please try again.")
+        { }
+    }
+}

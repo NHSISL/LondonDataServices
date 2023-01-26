b1a7474236e35adc1248ced20a2a7ef6fdf26fae
Christo du Toit <christo@dutoit.co.uk>
2023-01-26 14:29:44 +0000

    ShouldThrowDependencyValidationExceptionOnAddIfDownloadAlreadyExsitsAndLogItAsync -> FAIL

---
+++ LHDS.Landings.Client/Models/Downloads/Exceptions/AlreadyExistsDownloadException.cs
@@ -0,0 +1,12 @@
+using System;
+using Xeptions;
+
+namespace LHDS.Landings.Client.Models.Downloads.Exceptions
+{
+    public class AlreadyExistsDownloadException : Xeption
+    {
+        public AlreadyExistsDownloadException(Exception innerException)
+            : base(message: "Download with the same Id already exists.", innerException)
+        { }
+    }
+}

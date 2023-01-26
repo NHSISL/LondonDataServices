a3e3d34aa6a91a83028b76b17e09951823fe9c27
Christo du Toit <christo@dutoit.co.uk>
2023-01-26 11:34:28 +0000

    PROVIDERS:  Download Provider Interface

---
+++ LHDS.Landings.Client/Providers/Downloads/IDownloadProvider.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LHDS.Landings.Client.Models.Foundations.Documents;
+
+namespace LHDS.Landings.Client.Providers.Downloads
+{
+    public interface IDownloadProvider
+    {
+        ValueTask<List<Document>> GetListOfDocumentsToProcessAsync();
+        ValueTask<Document> GetDocumentAsync();
+    }
+}

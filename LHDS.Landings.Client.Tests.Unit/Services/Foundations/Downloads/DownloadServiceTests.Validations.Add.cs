76777a8ae80c601ce3fbaf60c2ac5a567952ed84
Christo du Toit <christo@dutoit.co.uk>
2023-01-26 14:29:36 +0000

    ShouldThrowValidationExceptionOnAddIfDownloadIsInvalidAndLogItAsync -> FAIL

--- LHDS.Landings.Client.Tests.Unit/Services/Foundations/Downloads/DownloadServiceTests.Validations.Add.cs
+++ LHDS.Landings.Client.Tests.Unit/Services/Foundations/Downloads/DownloadServiceTests.Validations.Add.cs
@@ -30,7 +30,8 @@ namespace LHDS.Landings.Client.Tests.Unit.Services.Foundations.Downloads
                     addDownloadTask.AsTask);
 
             // then
-            actualDownloadValidationException.Should().BeEquivalentTo(expectedDownloadValidationException);
+            actualDownloadValidationException.Should()
+                .BeEquivalentTo(expectedDownloadValidationException);
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
@@ -40,6 +41,76 @@ namespace LHDS.Landings.Client.Tests.Unit.Services.Foundations.Downloads
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfDownloadIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given
+            var invalidDownload = new Download
+            {
+                // TODO:  Add default values for your properties i.e. Name = invalidText
+            };
+
+            var invalidDownloadException =
+                new InvalidDownloadException();
+
+            invalidDownloadException.AddData(
+                key: nameof(Download.Id),
+                values: "Id is required");
+
+            //invalidDownloadException.AddData(
+            //    key: nameof(Download.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the Download model
+
+            invalidDownloadException.AddData(
+                key: nameof(Download.CreatedDate),
+                values: "Date is required");
+
+            invalidDownloadException.AddData(
+                key: nameof(Download.CreatedByUserId),
+                values: "Id is required");
+
+            invalidDownloadException.AddData(
+                key: nameof(Download.UpdatedDate),
+                values: "Date is required");
+
+            invalidDownloadException.AddData(
+                key: nameof(Download.UpdatedByUserId),
+                values: "Id is required");
+
+            var expectedDownloadValidationException =
+                new DownloadValidationException(invalidDownloadException);
+
+            // when
+            ValueTask<Download> addDownloadTask =
+                this.downloadService.AddDownloadAsync(invalidDownload);
+
+            DownloadValidationException actualDownloadValidationException =
+                await Assert.ThrowsAsync<DownloadValidationException>(
+                    addDownloadTask.AsTask);
+
+            // then
+            actualDownloadValidationException.Should()
+                .BeEquivalentTo(expectedDownloadValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedDownloadValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertDownloadAsync(It.IsAny<Download>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }
 }

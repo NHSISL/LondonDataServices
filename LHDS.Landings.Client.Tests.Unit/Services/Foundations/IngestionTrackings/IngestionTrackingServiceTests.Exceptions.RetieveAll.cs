8c51e93aa0c436544e376827f5463a2fe1f840ba
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-01-26 16:12:44 +0000

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ LHDS.Landings.Client.Tests.Unit/Services/Foundations/IngestionTrackings/IngestionTrackingServiceTests.Exceptions.RetieveAll.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using LHDS.Landings.Client.Models.Foundations.IngestionTracking.Exceptions;
+
+namespace LHDS.Landings.Client.Tests.Unit.Services.Foundations.IngestionTrackings
+{
+    public partial class IngestionTrackingServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedStorageException =
+                new FailedIngestionTrackingStorageException(sqlException);
+
+            var expectedIngestionTrackingDependencyException =
+                new IngestionTrackingDependencyException(failedStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.ReadAllIngestionTracking())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllIngestionTrackingsAction = () =>
+                this.ingestionTrackingService.RetrieveAllIngestionTracking();
+
+            IngestionTrackingDependencyException actualIngestionTrackingDependencyException =
+                Assert.Throws<IngestionTrackingDependencyException>(retrieveAllIngestionTrackingsAction);
+
+            // then
+            actualIngestionTrackingDependencyException.Should()
+                .BeEquivalentTo(expectedIngestionTrackingDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.ReadAllIngestionTracking(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedIngestionTrackingDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

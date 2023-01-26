69dbdf38d10ed8474a3c72651cd772a6793897cd
Christo du Toit <christo@dutoit.co.uk>
2023-01-26 14:29:35 +0000

    ShouldThrowValidationExceptionOnAddIfDownloadIsNullAndLogItAsync -> PASS

---
+++ LHDS.Landings.Client/Services/Foundations/Downloads/DownloadService.Exceptions.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using LHDS.Landings.Client.Models.Downloads;
+using LHDS.Landings.Client.Models.Downloads.Exceptions;
+using Xeptions;
+
+namespace LHDS.Landings.Client.Services.Foundations.Downloads
+{
+    public partial class DownloadService
+    {
+        private delegate ValueTask<Download> ReturningDownloadFunction();
+
+        private async ValueTask<Download> TryCatch(ReturningDownloadFunction returningDownloadFunction)
+        {
+            try
+            {
+                return await returningDownloadFunction();
+            }
+            catch (NullDownloadException nullDownloadException)
+            {
+                throw CreateAndLogValidationException(nullDownloadException);
+            }
+        }
+
+        private DownloadValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var downloadValidationException =
+                new DownloadValidationException(exception);
+
+            this.loggingBroker.LogError(downloadValidationException);
+
+            return downloadValidationException;
+        }
+    }
+}

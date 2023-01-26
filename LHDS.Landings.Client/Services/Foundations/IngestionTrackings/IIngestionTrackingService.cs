3aeac36eb88b22a858fddd5723c50cef56786d9b
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-01-26 11:36:12 +0000

    FOUNDATIONS: Ingestion Tracking Service Stubs

---
+++ LHDS.Landings.Client/Services/Foundations/IngestionTrackings/IIngestionTrackingService.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LHDS.Landings.Client.Models.IngestionTracking;
+
+namespace LHDS.Landings.Client.Services.Foundations.IngestionTrackings
+{
+    public interface IIngestionTrackingService
+    {
+        ValueTask<IngestionTracking> AddIngestionTrackingAsync(IngestionTracking ingestionTracking);
+        IQueryable<IngestionTracking> RetrieveAllIngestionTracking();
+        ValueTask<IngestionTracking> RetrieveIngestionTrackingByIdAsync(Guid ingestionTrackingId);
+        ValueTask<IngestionTracking> ModifyIngestionTrackingAsync(IngestionTracking ingestionTracking);
+        ValueTask<IngestionTracking> RemoveIngestionTrackingByIdAsync(Guid ingestionTrackingId);
+
+    }
+}

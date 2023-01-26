f6dfa0379802c0b0b4638095d1069826663bfddc
David Hayes <david.hayes17@nhs.net>
2023-01-26 11:09:07 +0000

    CODE RUB: Refactor namespaces on retrieve

--- LHDS.Landings.Client.Tests.Unit/Services/Foundations/Documents/DocumentServiceTests.Exceptions.RetrieveByfileName.cs
+++ LHDS.Landings.Client.Tests.Unit/Services/Foundations/Documents/DocumentServiceTests.Exceptions.RetrieveByfileName.cs
@@ -12,11 +12,10 @@ using LHDS.Landings.Client.Models.Foundations.Documents.Exceptions;
 using Microsoft.Extensions.Configuration;
 using Moq;
 
-namespace LHDS.Landings.Client.Tests.Unit.Services.Foundations.Downloads
+namespace LHDS.Landings.Client.Tests.Unit.Services.Foundations.Documents
 {
-    public partial class DocumentsServiceTests
+    public partial class DocumentServiceTests
     {
-
         [Fact]
         public async Task ShouldThrowDependencyExceptionOnSelectFileAndLogItAsync()
         {

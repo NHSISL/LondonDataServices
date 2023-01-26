67c75f649d094dbe609555a807cdec50bf26e87c
Christo du Toit <christo@dutoit.co.uk>
2023-01-26 11:35:19 +0000

    PROVIDERS:  Download Provider Abstraction

---
+++ LHDS.Landings.Client/Providers/Downloads/DownloadAbstractProvider.cs
@@ -0,0 +1,29 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LHDS.Landings.Client.Models.Foundations.Documents;
+
+namespace LHDS.Landings.Client.Providers.Downloads
+{
+    public class DownloadAbstractProvider
+    {
+        private readonly RestProvider provider;
+
+        public DownloadAbstractProvider(RestProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public async ValueTask<List<Document>> GetListOfDocumentsToProcessAsync() =>
+            await this.provider.GetListOfDocumentsToProcessAsync();
+
+        public async ValueTask<Document> GetDocumentAsync() =>
+            await this.provider.GetDocumentAsync();
+    }
+}

72aefe6fa827b09744d78503176bf86dc3a5dc3d
Christo du Toit <christo@dutoit.co.uk>
2023-01-27 20:32:25 +0000

    BROKERS: Storage - Insert Audit

---
+++ LHDS.Landings.Client/Brokers/Storages/IStorageBroker.Audits.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using LHDS.Landings.Client.Models.Audits;
+
+namespace LHDS.Landings.Client.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Audit> InsertAuditAsync(Audit audit);
+    }
+}

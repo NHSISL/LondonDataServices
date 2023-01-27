7c37135c0f154f93304ee5e19a567f281ec2247c
Christo du Toit <christo@dutoit.co.uk>
2023-01-27 20:34:01 +0000

    ShouldThrowValidationExceptionOnAddIfAuditIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Landings.Client.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.Validations.Add.cs
@@ -0,0 +1,45 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using LHDS.Landings.Client.Models.Audits;
+using LHDS.Landings.Client.Models.Audits.Exceptions;
+using Xunit;
+
+namespace LHDS.Landings.Client.Tests.Unit.Services.Foundations.Audits
+{
+    public partial class AuditServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfAuditIsNullAndLogItAsync()
+        {
+            // given
+            Audit nullAudit = null;
+
+            var nullAuditException =
+                new NullAuditException();
+
+            var expectedAuditValidationException =
+                new AuditValidationException(nullAuditException);
+
+            // when
+            ValueTask<Audit> addAuditTask =
+                this.auditService.AddAuditAsync(nullAudit);
+
+            AuditValidationException actualAuditValidationException =
+                await Assert.ThrowsAsync<AuditValidationException>(
+                    addAuditTask.AsTask);
+
+            // then
+            actualAuditValidationException.Should().BeEquivalentTo(expectedAuditValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAuditValidationException))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

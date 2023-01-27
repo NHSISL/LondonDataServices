1ad2cd20de42c2081548ebffadcea7031fd8f155
Christo du Toit <christo@dutoit.co.uk>
2023-01-27 20:34:01 +0000

    ShouldThrowValidationExceptionOnAddIfAuditIsNullAndLogItAsync -> PASS

---
+++ LHDS.Landings.Client/Services/Foundations/Audits/AuditService.Exceptions.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using LHDS.Landings.Client.Models.Audits;
+using LHDS.Landings.Client.Models.Audits.Exceptions;
+using Xeptions;
+
+namespace LHDS.Landings.Client.Services.Foundations.Audits
+{
+    public partial class AuditService
+    {
+        private delegate ValueTask<Audit> ReturningAuditFunction();
+
+        private async ValueTask<Audit> TryCatch(ReturningAuditFunction returningAuditFunction)
+        {
+            try
+            {
+                return await returningAuditFunction();
+            }
+            catch (NullAuditException nullAuditException)
+            {
+                throw CreateAndLogValidationException(nullAuditException);
+            }
+        }
+
+        private AuditValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var auditValidationException =
+                new AuditValidationException(exception);
+
+            this.loggingBroker.LogError(auditValidationException);
+
+            return auditValidationException;
+        }
+    }
+}

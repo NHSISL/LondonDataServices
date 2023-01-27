888da669d2b94aeb38cd914f4a2645f6fd9f7f3b
Christo du Toit <christo@dutoit.co.uk>
2023-01-27 20:32:00 +0000

    DATA: Audit Model

---
+++ LHDS.Landings.Client/Models/Audits/Audit.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace LHDS.Landings.Client.Models.Audits
+{
+    public class Audit
+    {
+        public Guid Id { get; set; }
+
+        // TODO:  Add your properties here
+
+        public Guid CreatedByUserId { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public Guid UpdatedByUserId { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}

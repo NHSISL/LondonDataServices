471a39aec041b517e68083222d7bd614147fe48e
Christo du Toit <christo@dutoit.co.uk>
2023-01-27 20:32:32 +0000

    BROKERS: Storage - Select All Audits

--- LHDS.Landings.Client/Brokers/Storages/IStorageBroker.Audits.cs
+++ LHDS.Landings.Client/Brokers/Storages/IStorageBroker.Audits.cs
@@ -8,5 +8,6 @@ namespace LHDS.Landings.Client.Brokers.Storages
     public partial interface IStorageBroker
     {
         ValueTask<Audit> InsertAuditAsync(Audit audit);
+        IQueryable<Audit> SelectAllAudits();
     }
 }

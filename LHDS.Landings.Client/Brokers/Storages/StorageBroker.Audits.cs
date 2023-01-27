7eebcd829e53108d7271362e071b13ef1b37fb98
Christo du Toit <christo@dutoit.co.uk>
2023-01-27 20:32:25 +0000

    BROKERS: Storage - Insert Audit

---
+++ LHDS.Landings.Client/Brokers/Storages/StorageBroker.Audits.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using LHDS.Landings.Client.Models.Audits;
+
+namespace LHDS.Landings.Client.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Audit> Audits { get; set; }
+
+        public async ValueTask<Audit> InsertAuditAsync(Audit audit)
+        {
+            using var broker =
+                new StorageBroker(this.configuration);
+
+            EntityEntry<Audit> auditEntityEntry =
+                await broker.Audits.AddAsync(audit);
+
+            await broker.SaveChangesAsync();
+
+            return auditEntityEntry.Entity;
+        }
+    }
+}

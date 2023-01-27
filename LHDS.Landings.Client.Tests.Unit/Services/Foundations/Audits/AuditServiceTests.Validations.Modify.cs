62da1d103c97e78afe9baf269669425f5ea75d06
Christo du Toit <christo@dutoit.co.uk>
2023-01-27 20:34:49 +0000

    ShouldThrowValidationExceptionOnModifyIfUpdatedDateIsNotRecentAndLogItAsync -> FAIL

--- LHDS.Landings.Client.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.Validations.Modify.cs
+++ LHDS.Landings.Client.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.Validations.Modify.cs
@@ -106,7 +106,8 @@ namespace LHDS.Landings.Client.Tests.Unit.Services.Foundations.Audits
                     modifyAuditTask.AsTask);
 
             //then
-            actualAuditValidationException.Should().BeEquivalentTo(expectedAuditValidationException);
+            actualAuditValidationException.Should()
+                .BeEquivalentTo(expectedAuditValidationException);
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
@@ -147,7 +148,8 @@ namespace LHDS.Landings.Client.Tests.Unit.Services.Foundations.Audits
                     modifyAuditTask.AsTask);
 
             // then
-            actualAuditValidationException.Should().BeEquivalentTo(expectedAuditValidationException);
+            actualAuditValidationException.Should()
+                .BeEquivalentTo(expectedAuditValidationException);
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
@@ -161,6 +163,58 @@ namespace LHDS.Landings.Client.Tests.Unit.Services.Foundations.Audits
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(MinutesBeforeOrAfter))]
+        public async Task ShouldThrowValidationExceptionOnModifyIfUpdatedDateIsNotRecentAndLogItAsync(int minutes)
+        {
+            // given
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+            Audit randomAudit = CreateRandomAudit(randomDateTimeOffset);
+            randomAudit.UpdatedDate = randomDateTimeOffset.AddMinutes(minutes);
+
+            var invalidAuditException =
+                new InvalidAuditException();
+
+            invalidAuditException.AddData(
+                key: nameof(Audit.UpdatedDate),
+                values: "Date is not recent");
+
+            var expectedAuditValidatonException =
+                new AuditValidationException(invalidAuditException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                .Returns(randomDateTimeOffset);
+
+            // when
+            ValueTask<Audit> modifyAuditTask =
+                this.auditService.ModifyAuditAsync(randomAudit);
+
+            AuditValidationException actualAuditValidationException =
+                await Assert.ThrowsAsync<AuditValidationException>(
+                    modifyAuditTask.AsTask);
+
+            // then
+            actualAuditValidationException.Should().BeEquivalentTo(expectedAuditValidatonException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAuditValidatonException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAuditByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
         }
     }
 }

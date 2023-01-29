d2fcb0afb4c2a9e8f122ea62e1158efbb02754b2
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-01-29 21:58:08 +0000

    ShouldThrowCriticalDependencyExceptionOnRetrieveByFileNameIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ LHDS.Landings.Client.Tests.Unit/Services/Foundations/IngestionTrackings/IngestionTrackingServiceTests.Exceptions.RetieveByFileName.cs
@@ -0,0 +1,60 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using System.Threading.Tasks;
+using LHDS.Landings.Client.Models.Foundations.IngestionTrackings.Exceptions;
+using LHDS.Landings.Client.Models.Foundations.IngestionTrackings;
+
+namespace LHDS.Landings.Client.Tests.Unit.Services.Foundations.IngestionTrackings
+{
+    public partial class IngestionTrackingServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByFileNameIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            string someFileName = GetRandomMessage();
+            SqlException sqlException = GetSqlException();
+
+            var failedIngestionTrackingStorageException =
+                new FailedIngestionTrackingStorageException(sqlException);
+
+            var expectedIngestionTrackingDependencyException =
+                new IngestionTrackingDependencyException(failedIngestionTrackingStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.ReadIngestionTrackingByFileNameAsync(It.IsAny<string>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<IngestionTracking> retrieveIngestionTrackingByFileNameTask =
+                this.ingestionTrackingService.RetrieveIngestionTrackingByFileNameAsync(someFileName);
+
+            IngestionTrackingDependencyException actualIngestionTrackingDependencyException =
+                await Assert.ThrowsAsync<IngestionTrackingDependencyException>(
+                    retrieveIngestionTrackingByFileNameTask.AsTask);
+
+            // then
+            actualIngestionTrackingDependencyException.Should()
+                .BeEquivalentTo(expectedIngestionTrackingDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.ReadIngestionTrackingByFileNameAsync(It.IsAny<string>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedIngestionTrackingDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

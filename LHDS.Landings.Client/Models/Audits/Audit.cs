b15b91df1bbc11e2d30f82a26165c9fb3aa24adf
Christo du Toit <christo@dutoit.co.uk>
2023-01-30 11:22:31 +0000

    CODE RUB: Amended Model

--- LHDS.Landings.Client/Models/Audits/Audit.cs
+++ LHDS.Landings.Client/Models/Audits/Audit.cs
@@ -5,12 +5,8 @@ namespace LHDS.Landings.Client.Models.Audits
     public class Audit
     {
         public Guid Id { get; set; }
-
-        // TODO:  Add your properties here
-
-        public Guid CreatedByUserId { get; set; }
+        public string IngestionTrackingId { get; set; }
+        public string Message { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
-        public Guid UpdatedByUserId { get; set; }
-        public DateTimeOffset UpdatedDate { get; set; }
     }
 }

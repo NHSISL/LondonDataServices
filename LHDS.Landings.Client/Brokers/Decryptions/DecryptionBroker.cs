a8d9cf483d05c6ad36f41b88c08726d337f18b38
Christo du Toit <christo@dutoit.co.uk>
2023-01-30 14:21:49 +0000

    BROKERS: Decryption Broker

---
+++ LHDS.Landings.Client/Brokers/Decryptions/DecryptionBroker.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+
+namespace LHDS.Landings.Client.Brokers.Decryptions
+{
+    public class DecryptionBroker : IDecryptionBroker
+    {
+        private readonly IDecryptionAbstractProvider decryptionAbstractProvider;
+
+
+        public ValueTask<byte[]> DecryptAsync(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

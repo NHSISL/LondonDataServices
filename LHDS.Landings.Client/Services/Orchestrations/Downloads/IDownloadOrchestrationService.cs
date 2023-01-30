0889b21f0200be64b8a8f57ffda8bef7952c2582
Christo du Toit <christo@dutoit.co.uk>
2023-01-30 14:29:55 +0000

    ORCHESTRATIONS: Download Orchestration

---
+++ LHDS.Landings.Client/Services/Orchestrations/Downloads/IDownloadOrchestrationService.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace LHDS.Landings.Client.Services.Orchestrations.Download
+{
+    public interface IDownloadOrchestrationService
+    {
+        ValueTask ProcessAsync();
+    }
+}

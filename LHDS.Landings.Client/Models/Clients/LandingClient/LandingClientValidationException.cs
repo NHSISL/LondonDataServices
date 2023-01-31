0ed5f294b97869277cf39fe7261cdf4aa9ac1956
Christo du Toit <christo@dutoit.co.uk>
2023-01-31 15:25:46 +0000

    WIP

---
+++ LHDS.Landings.Client/Models/Clients/LandingClient/LandingClientValidationException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Landings.Client.Models.Clients.LandingClient
+{
+    public class LandingClientValidationException : Xeption
+    {
+        public LandingClientValidationException(Xeption innerException)
+            : base(message: "Landing client validation error occurred, fix errors and try again.",
+          innerException)
+        { }
+    }
+}

3100c1fd442b55ea66e18973a1215c8c7836a386
davidhayes1981 <107634691+davidhayes1981@users.noreply.github.com>
2023-01-31 11:42:55 +0000

    ShouldThrowDependancyExceptionOnProcessIfDependancyExceptionOccursAndLogItAsync -> PASS

--- LHDS.Landings.Client/Services/Orchestrations/Downloads/DownloadOrchestrationService.Exceptions.cs
+++ LHDS.Landings.Client/Services/Orchestrations/Downloads/DownloadOrchestrationService.Exceptions.cs
@@ -54,6 +54,38 @@ namespace LHDS.Landings.Client.Services.Orchestrations.Downloads
             {
                 throw CreateAndLogAuditDependencyValidationException(auditDependencyValidationException);
             }
+            catch (DocumentDependencyException documentDependencyException)
+            {
+                throw CreateAndLogDocumentDependencyException(documentDependencyException);
+            }
+            catch (DocumentServiceException documentServiceException)
+            {
+                throw CreateAndLogDocumentServiceException(documentServiceException);
+            }
+            catch (DownloadDependencyException downloadDependencyException)
+            {
+                throw CreateAndLogDownloadDependencyException(downloadDependencyException);
+            }
+            catch (DownloadServiceException downloadServiceException)
+            {
+                throw CreateAndLogDownloadServiceException(downloadServiceException);
+            }
+            catch (IngestionTrackingDependencyException ingestionTrackingDependencyException)
+            {
+                throw CreateAndLogIngestionTrackingDependencyException(ingestionTrackingDependencyException);
+            }
+            catch (IngestionTrackingServiceException ingestionTrackingServiceException)
+            {
+                throw CreateAndLogIngestionTrackingServiceException(ingestionTrackingServiceException);
+            }
+            catch (AuditDependencyException auditDependencyException)
+            {
+                throw CreateAndLogAuditDependencyException(auditDependencyException);
+            }
+            catch (AuditServiceException auditServiceException)
+            {
+                throw CreateAndLogAuditServiceException(auditServiceException);
+            }
         }
 
         private DownloadOrchestrationDependancyValidationException CreateAndLogDocumentValidationException(Xeption exception)
@@ -142,5 +174,92 @@ namespace LHDS.Landings.Client.Services.Orchestrations.Downloads
             return downloadOrchestrationDependancyValidationException;
         }
 
+        private DownloadOrchestrationDependancyException
+            CreateAndLogDocumentDependencyException(Xeption exception)
+        {
+            var documentOrchestrationDependencyException =
+                new DownloadOrchestrationDependancyException(exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(documentOrchestrationDependencyException);
+
+            throw documentOrchestrationDependencyException;
+        }
+
+        private DownloadOrchestrationDependancyException
+            CreateAndLogDocumentServiceException(Xeption exception)
+        {
+            var documentOrchestrationDependencyException =
+                new DownloadOrchestrationDependancyException(exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(documentOrchestrationDependencyException);
+
+            throw documentOrchestrationDependencyException;
+        }
+
+        private DownloadOrchestrationDependancyException
+            CreateAndLogDownloadDependencyException(Xeption exception)
+        {
+            var documentOrchestrationDependencyException =
+                new DownloadOrchestrationDependancyException(exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(documentOrchestrationDependencyException);
+
+            throw documentOrchestrationDependencyException;
+        }
+
+        private DownloadOrchestrationDependancyException
+            CreateAndLogDownloadServiceException(Xeption exception)
+        {
+            var documentOrchestrationDependencyException =
+                new DownloadOrchestrationDependancyException(exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(documentOrchestrationDependencyException);
+
+            throw documentOrchestrationDependencyException;
+        }
+
+        private DownloadOrchestrationDependancyException
+           CreateAndLogIngestionTrackingDependencyException(Xeption exception)
+        {
+            var documentOrchestrationDependencyException =
+                new DownloadOrchestrationDependancyException(exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(documentOrchestrationDependencyException);
+
+            throw documentOrchestrationDependencyException;
+        }
+
+        private DownloadOrchestrationDependancyException
+            CreateAndLogIngestionTrackingServiceException(Xeption exception)
+        {
+            var documentOrchestrationDependencyException =
+                new DownloadOrchestrationDependancyException(exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(documentOrchestrationDependencyException);
+
+            throw documentOrchestrationDependencyException;
+        }
+
+        private DownloadOrchestrationDependancyException
+          CreateAndLogAuditDependencyException(Xeption exception)
+        {
+            var documentOrchestrationDependencyException =
+                new DownloadOrchestrationDependancyException(exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(documentOrchestrationDependencyException);
+
+            throw documentOrchestrationDependencyException;
+        }
+
+        private DownloadOrchestrationDependancyException
+            CreateAndLogAuditServiceException(Xeption exception)
+        {
+            var documentOrchestrationDependencyException =
+                new DownloadOrchestrationDependancyException(exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(documentOrchestrationDependencyException);
+
+            throw documentOrchestrationDependencyException;
+        }
     }
 }

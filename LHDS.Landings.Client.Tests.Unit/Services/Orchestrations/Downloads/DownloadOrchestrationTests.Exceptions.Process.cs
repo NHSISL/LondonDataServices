8126d7c65c5e25d992b0ab5c895f253788fafe7c
davidhayes1981 <107634691+davidhayes1981@users.noreply.github.com>
2023-01-31 12:22:35 +0000

    ShouldThrowServiceExceptionOnProcessIfServiceErrorOccursAndLogItAsync -> FAIL

--- LHDS.Landings.Client.Tests.Unit/Services/Orchestrations/Downloads/DownloadOrchestrationTests.Exceptions.Process.cs
+++ LHDS.Landings.Client.Tests.Unit/Services/Orchestrations/Downloads/DownloadOrchestrationTests.Exceptions.Process.cs
@@ -2,8 +2,10 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
+using LHDS.Landings.Client.Models.Audits.Exceptions;
 using LHDS.Landings.Client.Models.Orchestrations.Downloads.Exceptions;
 using Moq;
 using Xeptions;
@@ -80,6 +82,46 @@ namespace LHDS.Landings.Client.Tests.Unit.Services.Orchestrations.Downloads
             this.loggingBrokerMock.Verify(broker =>
                broker.LogError(It.Is(SameExceptionAs(
                    expectedDependancyException))),
+                       Times.Once);
+
+            this.documentServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.ingestionTrackingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnProcessIfServiceErrorOccursAndLogItAsync()
+        {
+            //Given
+            var serviceException = new Exception();
+
+            var failedDownloadOrchestrationServiceException =
+               new FailedDownloadOrchestrationServiceException(serviceException);
+
+            var expectedDownloadOrchestrationServiceException =
+                new DownloadOrchestrationServiceException(failedDownloadOrchestrationServiceException);
+
+            this.downloadServiceMock.Setup(service =>
+             service.RetrieveListOfDocumentsToProcessAsync())
+                 .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask processTask = this.downloadOrchestrationService.ProcessAsync();
+
+            DownloadOrchestrationServiceException actualException =
+                await Assert.ThrowsAsync<DownloadOrchestrationServiceException>(processTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedDownloadOrchestrationServiceException);
+
+            this.downloadServiceMock.Verify(service =>
+              service.RetrieveListOfDocumentsToProcessAsync(),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(
+                   expectedDownloadOrchestrationServiceException))),
                        Times.Once);
 
             this.documentServiceMock.VerifyNoOtherCalls();

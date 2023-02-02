efd864f273dee808fb2993b5725fba711cad1c17
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-02-02 14:01:57 +0000

    ShouldProcessDecryptedDocumentAsync -> FAIL

--- LHDS.Landings.Client.Tests.Unit/Services/Orchestrations/Decryptions/DecryptionOrchestrationTests.Logic.Decrypt.cs
+++ LHDS.Landings.Client.Tests.Unit/Services/Orchestrations/Decryptions/DecryptionOrchestrationTests.Logic.Decrypt.cs
@@ -3,14 +3,82 @@
 // ---------------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LHDS.Landings.Client.Models.Audits;
+using LHDS.Landings.Client.Models.Foundations.Documents;
+using LHDS.Landings.Client.Models.Foundations.IngestionTrackings;
+using Moq;
 
 namespace LHDS.Landings.Client.Tests.Unit.Services.Orchestrations.Decryptions
 {
-    internal class DecryptionOrchestrationTests
+
+    public partial class DecryptionOrchestrationTests
     {
+        [Fact]
+        public async Task ShouldProcessDecryptedDocumentAsync()
+        {
+            // given
+            string randomFileName = GetRandomMessage();
+            byte[] randomEncryptedString = Encoding.ASCII.GetBytes(GetRandomMessage());
+            byte[] randomDecryptedString = Encoding.ASCII.GetBytes(GetRandomMessage());
+            DateTimeOffset randomDateTime = GetRandomDateTimeOffset();
+            var isDecrypted = false;
+            Document randomDocument = new Document { FileName=randomFileName, DocumentData=randomEncryptedString};
+
+            //setup documentservice mock
+            //
+
+            IngestionTracking externalIngestionTrackingFound = null;
+
+            this.documentServiceMock.Setup(service =>
+                service.RetrieveDocumentByFileNameAsync(randomFileName, false))
+                    .ReturnsAsync(randomDocument);
+
+            this.decryptionServiceMock.Setup(service =>
+                service.DecryptAsync(randomDocument.DocumentData))
+                    .ReturnsAsync(randomDecryptedString);
+
+            IngestionTracking ingestionTracking = new IngestionTracking
+            {
+                Id = randomDocument.FileName,
+                FileName = randomDocument.FileName,
+                Decrypted = true,
+                CreatedDate = randomDateTime,
+            };
+
+            this.ingestionTrackingServiceMock.Setup(service =>
+                service.RetrieveIngestionTrackingByFileNameAsync(randomDocument.FileName))
+                    .ReturnsAsync(ingestionTracking);
+
+            // when
+            await this.decryptionOrchestrationService.DecryptAsync(randomFileName);
+
+            // then
+            this.documentServiceMock.Verify(service =>
+                service.RetrieveDocumentByFileNameAsync(randomFileName, false),
+                    Times.Once);
+
+            this.decryptionServiceMock.Verify(service =>
+                service.DecryptAsync(randomDocument.DocumentData),
+                    Times.Once);
+
+            this.documentServiceMock.Verify(service =>
+                service.AddDocumentAsync(randomDocument, true),
+                    Times.Once);
+
+            //this.ingestionTrackingServiceMock.Verify(service =>
+            //    service.RetrieveIngestionTrackingByFileNameAsync(randomDocument.FileName),
+            //        Times.Once);
+            
+            this.auditServiceMock.Verify(service =>
+                service.AddAuditAsync(It.IsAny<Audit>()),
+                    Times.Once);
+
+            this.documentServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.ingestionTrackingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
--- LHDS.Landings.Client/Services/Orchestrations/Decryptions/DecryptionOrchestrationService.cs
+++ LHDS.Landings.Client/Services/Orchestrations/Decryptions/DecryptionOrchestrationService.cs
@@ -10,7 +10,91 @@ using System.Threading.Tasks;
 
 namespace LHDS.Landings.Client.Services.Orchestrations.Decryptions
 {
-    internal class DecryptionOrchestrationService
+    using System;
+    using System.ComponentModel;
+    using System.Threading.Tasks;
+    using global::LHDS.Landings.Client.Brokers.DateTimes;
+    using global::LHDS.Landings.Client.Brokers.Loggings;
+    using global::LHDS.Landings.Client.Models.Foundations.Documents;
+    using global::LHDS.Landings.Client.Models.Foundations.IngestionTrackings;
+    using global::LHDS.Landings.Client.Services.Foundations.Audits;
+    using global::LHDS.Landings.Client.Services.Foundations.Decryptions;
+    using global::LHDS.Landings.Client.Services.Foundations.Documents;
+    using global::LHDS.Landings.Client.Services.Foundations.IngestionTrackings;
+    using Microsoft.IdentityModel.Protocols;
+
+    namespace LHDS.Landings.Client.Services.Foundations.Decryptions
     {
+        public class DecryptionOrchestrationService : IDecryptionOrchestrationService
+        {
+            private readonly IDocumentService documentService;
+            private readonly IDecryptionService decryptionService;
+            private readonly IIngestionTrackingService ingestionTrackingService;
+            private readonly IAuditService auditService;
+            private readonly ILoggingBroker loggingBroker;
+            private readonly IDateTimeBroker dateTimeBroker;
+
+            public DecryptionOrchestrationService(
+                IDocumentService documentService,
+                IDecryptionService decryptionService,
+                IIngestionTrackingService ingestionTrackingService,
+                IAuditService auditService,
+                ILoggingBroker loggingBroker,
+                IDateTimeBroker dateTimeBroker)
+            {
+                this.documentService = documentService;
+                this.decryptionService = decryptionService;
+                this.ingestionTrackingService = ingestionTrackingService;
+                this.auditService = auditService;
+                this.loggingBroker = loggingBroker;
+                this.dateTimeBroker = dateTimeBroker;
+            }
+
+            public async ValueTask DecryptAsync(string fileName)
+            {
+                Document retrievedDocument =
+                await this.documentService.RetrieveDocumentByFileNameAsync(fileName, false);
+
+                byte[] 
+
+                byte[] randomDecryption =
+                    await this.decryptionService.DecryptAsync(randomDecryption);
+
+                var 
+
+            
+                IngestionTracking maybeIngestionTracking =
+                    await this.ingestionTrackingService
+                        .RetrieveIngestionTrackingByFileNameAsync(document.FileName);
+
+                if (maybeIngestionTracking == null)
+                {
+                    Document retrievedDocument =
+                        await this.downloadService.RetrieveDownloadByFileNameAsync(document.FileName);
+
+                    var currentDateTime = this.dateTimeBroker.GetCurrentDateTimeOffset();
+
+                    IngestionTracking newIngestionTracking =
+                        new IngestionTracking
+                        {
+                            Id = document.FileName,
+                            FileName = document.FileName,
+                            Decrypted = false,
+                            CreatedDate = currentDateTime,
+                        };
+
+                    await this.ingestionTrackingService.AddIngestionTrackingAsync(newIngestionTracking);
+                    await this.documentService.AddDocumentAsync(retrievedDocument, false);
+                    LogAudit(document, currentDateTime);
+                }
+                
+                byte[] encryptedData = await documentService.RetrieveDocumentByFileNameAsync(fileName, false);
+                byte[] decryptedData = await auditService.DecryptAsync(encryptedData);
+                await documentService.AddDocumentAsync(fileName, decryptedData);
+                await ingestionTrackingService.TrackAsync(fileName, decryptedData);
+                await auditService.LogDecryptionAsync(fileName, decryptedData);
+            }
+        }
     }
+
 }

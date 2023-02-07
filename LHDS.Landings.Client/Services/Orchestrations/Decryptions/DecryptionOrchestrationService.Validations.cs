0963ebab2455669b0bad07c11ea176ad6e4e291f
davidhayes1981 <107634691+davidhayes1981@users.noreply.github.com>
2023-02-07 17:02:11 +0000

    ShouldThrowValidationExceptionOnDecryptIfFileNameIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Landings.Client/Services/Orchestrations/Decryptions/DecryptionOrchestrationService.Validations.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using LHDS.Landings.Client.Models.Orchestrations.Decryptions.Exceptions;
+
+namespace LHDS.Landings.Client.Services.Orchestrations.Decryptions
+{
+    public partial class DecryptionOrchestrationService
+    {
+        private static void ValidateFileNameIsNotNull(string fileName)
+        {
+            if (fileName is null)
+            {
+                throw new NullDecryptionOrchestrationFileNameException();
+            }
+        }
+    }
+}

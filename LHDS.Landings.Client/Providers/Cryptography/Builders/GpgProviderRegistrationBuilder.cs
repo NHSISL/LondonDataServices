af729b39af036ad305244bf8e0bbcf877de30827
davidhayes1981 <107634691+davidhayes1981@users.noreply.github.com>
2023-02-08 16:13:03 +0000

    EXPOSERS: Wire up Dycryption Orchestration

---
+++ LHDS.Landings.Client/Providers/Cryptography/Builders/GpgProviderRegistrationBuilder.cs
@@ -0,0 +1,40 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using LHDS.Landings.Client.Providers.Cryptography;
+using LHDS.Landings.Client.Providers.Cryptography.Gpg;
+
+namespace LHDS.Landings.Client.Providers.Downloads.Builders
+{
+    public class GpgProviderRegistrationBuilder
+    {
+        private readonly List<ICryptographyProvider> cryptographyProviderRegistrations;
+        private readonly IGpgCryptographyProviderSettings gpgCryptographyProviderSettings;
+
+        public GpgProviderRegistrationBuilder(IGpgCryptographyProviderSettings gpgCryptographyProviderSettings)
+        {
+            cryptographyProviderRegistrations = new List<ICryptographyProvider>();
+            this.gpgCryptographyProviderSettings = gpgCryptographyProviderSettings;
+        }
+
+        public void AddGpgCryptographyProvider()
+        {
+            var gpgCryptographyProvider = new GpgCryptographyProvider(gpgCryptographyProviderSettings);
+
+            if (cryptographyProviderRegistrations.Count > 0)
+            {
+                throw new Exception("Only one cryptography provider can be registered at a time");
+            }
+
+            cryptographyProviderRegistrations.Add(gpgCryptographyProvider);
+        }
+
+        public List<ICryptographyProvider> Build()
+        {
+            return cryptographyProviderRegistrations;
+        }
+    }
+}

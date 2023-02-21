caf2b974b80add9e74a25cfb93207aba0d7a873c
davidhayes1981 <107634691+davidhayes1981@users.noreply.github.com>
2023-02-21 15:24:41 +0000

    INFRASTRUCTURE: Web project template

---
+++ LHDS.AdminPortal.Web/ClientApp/src/App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './custom.css'
+import { MsalProvider } from '@azure/msal-react';
+import { ReactQueryDevtools } from 'react-query/devtools'
+import "./styles/App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { PageLayout } from './components/PageLayout';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/home';
+import { QueryClientProvider } from 'react-query';
+import { queryClientGlobalOptions } from './brokers/apiBroker.globals';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { AppInsightsContext, ReactPlugin } from '@microsoft/applicationinsights-react-js';
+
+var reactPlugin = new ReactPlugin();
+var appInsights = new ApplicationInsights({
+    config: {
+        connectionString: process.env.REACT_APP_AI_CONNECTIONSTRING,
+        enableAutoRouteTracking: true,
+        extensions: [reactPlugin],
+    }
+});
+appInsights.loadAppInsights();
+
+const App = ({ msalInstance }: any) => {
+    return (
+        <AppInsightsContext.Provider value={reactPlugin} >
+            <MsalProvider instance={msalInstance}>
+                <QueryClientProvider client={queryClientGlobalOptions}>
+                    <PageLayout>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                        </Routes>
+                    </PageLayout>
+                    <ReactQueryDevtools initialIsOpen={false} />
+                </QueryClientProvider>
+            </MsalProvider>
+        </AppInsightsContext.Provider>
+    );
+}
+export default App;
--- LHDS.AdminPortal.Web/Program.cs
+++ LHDS.AdminPortal.Web/Program.cs
@@ -2,39 +2,21 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
-namespace LHDS.AdminPortal.Web
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller}/{action=Index}/{id?}");
-
-            app.MapFallbackToFile("index.html");
-
-            app.Run();
-        }
-    }
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.MapFallbackToFile("index.html");
+
+app.Run();

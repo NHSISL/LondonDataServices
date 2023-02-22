dcf09086959904f8c2f6a79181f15e5c3c7d92ae
Christo du Toit <christo@dutoit.co.uk>
2023-02-22 16:25:19 +0000

    ShouldThrowValidationExceptionOnModifyIfReferenceErrorOccursAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Foundations/Suppliers/SupplierServiceTests.Exceptions.Modify.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Suppliers/SupplierServiceTests.Exceptions.Modify.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
 using Moq;
@@ -54,6 +55,60 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Suppliers
 
             this.storageBrokerMock.Verify(broker =>
                 broker.UpdateSupplierAsync(randomSupplier),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnModifyIfReferenceErrorOccursAndLogItAsync()
+        {
+            // given
+            Supplier someSupplier = CreateRandomSupplier();
+            string randomMessage = GetRandomMessage();
+            string exceptionMessage = randomMessage;
+
+            var foreignKeyConstraintConflictException =
+                new ForeignKeyConstraintConflictException(exceptionMessage);
+
+            var invalidSupplierReferenceException =
+                new InvalidSupplierReferenceException(foreignKeyConstraintConflictException);
+
+            SupplierDependencyValidationException expectedSupplierDependencyValidationException =
+                new SupplierDependencyValidationException(invalidSupplierReferenceException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(foreignKeyConstraintConflictException);
+
+            // when
+            ValueTask<Supplier> modifySupplierTask =
+                this.supplierService.ModifySupplierAsync(someSupplier);
+
+            SupplierDependencyValidationException actualSupplierDependencyValidationException =
+                await Assert.ThrowsAsync<SupplierDependencyValidationException>(
+                    modifySupplierTask.AsTask);
+
+            // then
+            actualSupplierDependencyValidationException.Should()
+                .BeEquivalentTo(expectedSupplierDependencyValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSupplierByIdAsync(someSupplier.Id),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedSupplierDependencyValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateSupplierAsync(someSupplier),
                     Times.Never);
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();

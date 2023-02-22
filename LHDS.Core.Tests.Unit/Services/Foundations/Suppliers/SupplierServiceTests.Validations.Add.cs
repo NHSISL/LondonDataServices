7c903cee9234c0391eaec2cffc91351f260c9319
Christo du Toit <christo@dutoit.co.uk>
2023-02-22 16:24:29 +0000

    ShouldThrowValidationExceptionOnAddIfSupplierIsInvalidAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/Suppliers/SupplierServiceTests.Validations.Add.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Suppliers/SupplierServiceTests.Validations.Add.cs
@@ -30,7 +30,8 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Suppliers
                     addSupplierTask.AsTask);
 
             // then
-            actualSupplierValidationException.Should().BeEquivalentTo(expectedSupplierValidationException);
+            actualSupplierValidationException.Should()
+                .BeEquivalentTo(expectedSupplierValidationException);
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
@@ -40,6 +41,76 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Suppliers
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfSupplierIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given
+            var invalidSupplier = new Supplier
+            {
+                // TODO:  Add default values for your properties i.e. Name = invalidText
+            };
+
+            var invalidSupplierException =
+                new InvalidSupplierException();
+
+            invalidSupplierException.AddData(
+                key: nameof(Supplier.Id),
+                values: "Id is required");
+
+            //invalidSupplierException.AddData(
+            //    key: nameof(Supplier.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the Supplier model
+
+            invalidSupplierException.AddData(
+                key: nameof(Supplier.CreatedDate),
+                values: "Date is required");
+
+            invalidSupplierException.AddData(
+                key: nameof(Supplier.CreatedByUserId),
+                values: "Id is required");
+
+            invalidSupplierException.AddData(
+                key: nameof(Supplier.UpdatedDate),
+                values: "Date is required");
+
+            invalidSupplierException.AddData(
+                key: nameof(Supplier.UpdatedByUserId),
+                values: "Id is required");
+
+            var expectedSupplierValidationException =
+                new SupplierValidationException(invalidSupplierException);
+
+            // when
+            ValueTask<Supplier> addSupplierTask =
+                this.supplierService.AddSupplierAsync(invalidSupplier);
+
+            SupplierValidationException actualSupplierValidationException =
+                await Assert.ThrowsAsync<SupplierValidationException>(
+                    addSupplierTask.AsTask);
+
+            // then
+            actualSupplierValidationException.Should()
+                .BeEquivalentTo(expectedSupplierValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSupplierValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertSupplierAsync(It.IsAny<Supplier>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }
 }

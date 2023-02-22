74503cfc252a142ad5bf5af48a538914ea6b32f7
Christo du Toit <christo@dutoit.co.uk>
2023-02-22 14:48:09 +0000

    BROKERS: Storage Broker - Add Storage Broker And Initial Migration

---
+++ LHDS.Core/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,36 @@
+using System;
+using EFxceptions.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using LHDS.Core.Models.Users;
+
+namespace LHDS.Core.Brokers.Storages
+{
+    public partial class StorageBroker : EFxceptionsIdentityContext<ApplicationUser, ApplicationRole, Guid>, IStorageBroker
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            // TODO: Add your fluent configurations here i.e. AddSupplierConfigurations(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = this.configuration
+                .GetConnectionString(name: "DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public override void Dispose() { }
+    }
+}

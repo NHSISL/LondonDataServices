934785597ae9dc4e0d626eca319cd8412558d0d4
Christo du Toit <christo@dutoit.co.uk>
2023-02-22 16:24:47 +0000

    ShouldReturnSupplier -> FAIL

--- LHDS.Core/Services/Foundations/Suppliers/SupplierService.cs
+++ LHDS.Core/Services/Foundations/Suppliers/SupplierService.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.DateTimes;
 using LHDS.Core.Brokers.Loggings;
@@ -29,5 +30,8 @@ namespace LHDS.Core.Services.Foundations.Suppliers
 
                 return await this.storageBroker.InsertSupplierAsync(supplier);
             });
+
+        public IQueryable<Supplier> RetrieveAllSuppliers() =>
+            throw new System.NotImplementedException();
     }
 }

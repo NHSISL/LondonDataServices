45e5bedeabd7a6d5433425095d1d594da00a91ab
Christo du Toit <christo@dutoit.co.uk>
2023-02-22 16:25:01 +0000

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/Suppliers/SupplierServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using LHDS.Core.Models.Suppliers;
+using LHDS.Core.Models.Suppliers.Exceptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Suppliers
+{
+    public partial class SupplierServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedSupplierStorageException =
+                new FailedSupplierStorageException(sqlException);
+
+            var expectedSupplierDependencyException =
+                new SupplierDependencyException(failedSupplierStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectSupplierByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Supplier> retrieveSupplierByIdTask =
+                this.supplierService.RetrieveSupplierByIdAsync(someId);
+
+            SupplierDependencyException actualSupplierDependencyException =
+                await Assert.ThrowsAsync<SupplierDependencyException>(
+                    retrieveSupplierByIdTask.AsTask);
+
+            // then
+            actualSupplierDependencyException.Should()
+                .BeEquivalentTo(expectedSupplierDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSupplierByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedSupplierDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

bc3f7a66a5f4d4898090a78b8b93d6057fddf707
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 14:24:40 +0000

    ShouldThrowValidationExceptionOnAddIfIngestionTrackingIsNullAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/IngestionTrackings/IngestionTrackingServiceTests.Validations.Add.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/IngestionTrackings/IngestionTrackingServiceTests.Validations.Add.cs
@@ -1,13 +1,8 @@
-// ---------------------------------------------------------------
-// Copyright (c) North East London ICB. All rights reserved.
-// ---------------------------------------------------------------
-
-using System;
 using System.Threading.Tasks;
 using FluentAssertions;
-using LHDS.Core.Models.Foundations.IngestionTrackings;
-using LHDS.Core.Models.Foundations.IngestionTrackings.Exceptions;
 using Moq;
+using LHDS.Core.Models.IngestionTrackings;
+using LHDS.Core.Models.IngestionTrackings.Exceptions;
 using Xunit;
 
 namespace LHDS.Core.Tests.Unit.Services.Foundations.IngestionTrackings
@@ -31,12 +26,11 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.IngestionTrackings
                 this.ingestionTrackingService.AddIngestionTrackingAsync(nullIngestionTracking);
 
             IngestionTrackingValidationException actualIngestionTrackingValidationException =
-                await Assert.ThrowsAsync<IngestionTrackingValidationException>(() =>
-                    addIngestionTrackingTask.AsTask());
+                await Assert.ThrowsAsync<IngestionTrackingValidationException>(
+                    addIngestionTrackingTask.AsTask);
 
             // then
-            actualIngestionTrackingValidationException.Should()
-                .BeEquivalentTo(expectedIngestionTrackingValidationException);
+            actualIngestionTrackingValidationException.Should().BeEquivalentTo(expectedIngestionTrackingValidationException);
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
@@ -45,139 +39,6 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.IngestionTrackings
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
-            this.storageBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData(" ")]
-        public async Task ShouldThrowValidationExceptionOnAddIfIngestionTrackingIsInvalidAndLogItAsync(string invalidText)
-        {
-            // given
-            var invalidIngestionTracking = new IngestionTracking
-            {
-                Id = invalidText,
-                Source = invalidText,
-                EncryptedFileName = invalidText,
-                DecryptedFileName = invalidText,
-            };
-
-            var invalidIngestionTrackingException =
-                new InvalidIngestionTrackingException();
-
-            invalidIngestionTrackingException.AddData(
-                key: nameof(IngestionTracking.Id),
-                values: "Text is required");
-
-            invalidIngestionTrackingException.AddData(
-                key: nameof(IngestionTracking.Source),
-                values: "Text is required");
-
-            invalidIngestionTrackingException.AddData(
-                key: nameof(IngestionTracking.EncryptedFileName),
-                values: "Text is required");
-
-            invalidIngestionTrackingException.AddData(
-                key: nameof(IngestionTracking.DecryptedFileName),
-                values: "Text is required");
-
-            invalidIngestionTrackingException.AddData(
-                key: nameof(IngestionTracking.CreatedDate),
-                values: "Date is required");
-
-            invalidIngestionTrackingException.AddData(
-                key: nameof(IngestionTracking.LastSeen),
-                values: "Date is required");
-
-            var expectedIngestionTrackingValidationException =
-                new IngestionTrackingValidationException(invalidIngestionTrackingException);
-
-            // when
-            ValueTask<IngestionTracking> addIngestionTrackingTask =
-                this.ingestionTrackingService.AddIngestionTrackingAsync(invalidIngestionTracking);
-
-            IngestionTrackingValidationException actualIngestionTrackingValidationException =
-                await Assert.ThrowsAsync<IngestionTrackingValidationException>(() =>
-                    addIngestionTrackingTask.AsTask());
-
-            // then
-            actualIngestionTrackingValidationException.Should()
-                .BeEquivalentTo(expectedIngestionTrackingValidationException);
-
-            this.dateTimeBrokerMock.Verify(broker =>
-                broker.GetCurrentDateTimeOffset(),
-                    Times.Once());
-
-            this.loggingBrokerMock.Verify(broker =>
-                broker.LogError(It.Is(SameExceptionAs(
-                    expectedIngestionTrackingValidationException))),
-                        Times.Once);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.InsertIngestionTrackingAsync(It.IsAny<IngestionTracking>()),
-                    Times.Never);
-
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Theory]
-        [MemberData(nameof(MinutesBeforeOrAfter))]
-        public async Task ShouldThrowValidationExceptionOnAddIfCreatedDateIsNotRecentAndLogItAsync(
-            int minutesBeforeOrAfter)
-        {
-            // given
-            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
-
-            DateTimeOffset invalidDateTime =
-                randomDateTimeOffset.AddMinutes(minutesBeforeOrAfter);
-
-            IngestionTracking randomIngestionTracking = CreateRandomIngestionTracking(invalidDateTime);
-            IngestionTracking invalidIngestionTracking = randomIngestionTracking;
-
-            var invalidIngestionTrackingException =
-                new InvalidIngestionTrackingException();
-
-            invalidIngestionTrackingException.AddData(
-                key: nameof(IngestionTracking.CreatedDate),
-                values: "Date is not recent");
-
-            var expectedIngestionTrackingValidationException =
-                new IngestionTrackingValidationException(invalidIngestionTrackingException);
-
-            this.dateTimeBrokerMock.Setup(broker =>
-                broker.GetCurrentDateTimeOffset())
-                    .Returns(randomDateTimeOffset);
-
-            // when
-            ValueTask<IngestionTracking> addIngestionTrackingTask =
-                this.ingestionTrackingService.AddIngestionTrackingAsync(invalidIngestionTracking);
-
-            IngestionTrackingValidationException actualIngestionTrackingValidationException =
-                await Assert.ThrowsAsync<IngestionTrackingValidationException>(() =>
-                    addIngestionTrackingTask.AsTask());
-
-            // then
-            actualIngestionTrackingValidationException.Should()
-                .BeEquivalentTo(expectedIngestionTrackingValidationException);
-
-            this.dateTimeBrokerMock.Verify(broker =>
-                broker.GetCurrentDateTimeOffset(),
-                    Times.Once());
-
-            this.loggingBrokerMock.Verify(broker =>
-                broker.LogError(It.Is(SameExceptionAs(
-                    expectedIngestionTrackingValidationException))),
-                        Times.Once);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.InsertIngestionTrackingAsync(It.IsAny<IngestionTracking>()),
-                    Times.Never);
-
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
     }

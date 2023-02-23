007914ce4d4a2e8a9b055c202558e276df281b94
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 13:30:17 +0000

    ShouldThrowValidationExceptionOnAddIfAuditIsInvalidAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/Audits/AuditService.Validations.cs
+++ LHDS.Core/Services/Foundations/Audits/AuditService.Validations.cs
@@ -1,3 +1,4 @@
+using System;
 using LHDS.Core.Models.Audits;
 using LHDS.Core.Models.Audits.Exceptions;
 
@@ -8,6 +9,16 @@ namespace LHDS.Core.Services.Foundations.Audits
         private void ValidateAuditOnAdd(Audit audit)
         {
             ValidateAuditIsNotNull(audit);
+
+            Validate(
+                (Rule: IsInvalid(audit.Id), Parameter: nameof(Audit.Id)),
+
+                // TODO: Add any other required validation rules
+
+                (Rule: IsInvalid(audit.CreatedDate), Parameter: nameof(Audit.CreatedDate)),
+                (Rule: IsInvalid(audit.CreatedByUserId), Parameter: nameof(Audit.CreatedByUserId)),
+                (Rule: IsInvalid(audit.UpdatedDate), Parameter: nameof(Audit.UpdatedDate)),
+                (Rule: IsInvalid(audit.UpdatedByUserId), Parameter: nameof(Audit.UpdatedByUserId)));
         }
 
         private static void ValidateAuditIsNotNull(Audit audit)
@@ -16,6 +27,35 @@ namespace LHDS.Core.Services.Foundations.Audits
             {
                 throw new NullAuditException();
             }
+        }
+
+        private static dynamic IsInvalid(Guid id) => new
+        {
+            Condition = id == Guid.Empty,
+            Message = "Id is required"
+        };
+
+        private static dynamic IsInvalid(DateTimeOffset date) => new
+        {
+            Condition = date == default,
+            Message = "Date is required"
+        };
+
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidAuditException = new InvalidAuditException();
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidAuditException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidAuditException.ThrowIfContainsErrors();
         }
     }
 }

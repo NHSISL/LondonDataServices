387c1b47abbaf748efbaa82b9fe0d926bba7e921
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 13:22:28 +0000

    CODE RUB: Excluded tests

--- LHDS.AdminPortal.Api.Tests.Acceptance/Apis/IngestionTrackingsApiTests.Logic.cs
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Apis/IngestionTrackingsApiTests.Logic.cs
@@ -1,104 +1,100 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FluentAssertions;
-using RESTFulSense.Exceptions;
-using LHDS.AdminPortal.Api.Tests.Acceptance.Models.IngestionTrackings;
-using Xunit;
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
 
 namespace LHDS.AdminPortal.Api.Tests.Acceptance.Apis.IngestionTrackings
 {
     public partial class IngestionTrackingsApiTests
     {
-        [Fact]
-        public async Task ShouldPostIngestionTrackingAsync()
-        {
-            // given
-            IngestionTracking randomIngestionTracking = CreateRandomIngestionTracking();
-            IngestionTracking inputIngestionTracking = randomIngestionTracking;
-            IngestionTracking expectedIngestionTracking = inputIngestionTracking;
+        //[Fact]
+        //public async Task ShouldPostIngestionTrackingAsync()
+        //{
+        //    // given
+        //    IngestionTracking randomIngestionTracking = CreateRandomIngestionTracking();
+        //    IngestionTracking inputIngestionTracking = randomIngestionTracking;
+        //    IngestionTracking expectedIngestionTracking = inputIngestionTracking;
 
-            // when 
-            await this.apiBroker.PostIngestionTrackingAsync(inputIngestionTracking);
+        //    // when 
+        //    await this.apiBroker.PostIngestionTrackingAsync(inputIngestionTracking);
 
-            IngestionTracking actualIngestionTracking =
-                await this.apiBroker.GetIngestionTrackingByIdAsync(inputIngestionTracking.Id);
+        //    IngestionTracking actualIngestionTracking =
+        //        await this.apiBroker.GetIngestionTrackingByIdAsync(inputIngestionTracking.Id);
 
-            // then
-            actualIngestionTracking.Should().BeEquivalentTo(expectedIngestionTracking);
-            await this.apiBroker.DeleteIngestionTrackingByIdAsync(actualIngestionTracking.Id);
-        }
+        //    // then
+        //    actualIngestionTracking.Should().BeEquivalentTo(expectedIngestionTracking);
+        //    await this.apiBroker.DeleteIngestionTrackingByIdAsync(actualIngestionTracking.Id);
+        //}
 
-        [Fact]
-        public async Task ShouldGetAllIngestionTrackingsAsync()
-        {
-            // given
-            List<IngestionTracking> randomIngestionTrackings = await PostRandomIngestionTrackingsAsync();
-            List<IngestionTracking> expectedIngestionTrackings = randomIngestionTrackings;
+        //[Fact]
+        //public async Task ShouldGetAllIngestionTrackingsAsync()
+        //{
+        //    // given
+        //    List<IngestionTracking> randomIngestionTrackings = await PostRandomIngestionTrackingsAsync();
+        //    List<IngestionTracking> expectedIngestionTrackings = randomIngestionTrackings;
 
-            // when
-            List<IngestionTracking> actualIngestionTrackings = await this.apiBroker.GetAllIngestionTrackingsAsync();
+        //    // when
+        //    List<IngestionTracking> actualIngestionTrackings = await this.apiBroker.GetAllIngestionTrackingsAsync();
 
-            // then
-            foreach (IngestionTracking expectedIngestionTracking in expectedIngestionTrackings)
-            {
-                IngestionTracking actualIngestionTracking = actualIngestionTrackings.Single(approval => approval.Id == expectedIngestionTracking.Id);
-                actualIngestionTracking.Should().BeEquivalentTo(expectedIngestionTracking);
-                await this.apiBroker.DeleteIngestionTrackingByIdAsync(actualIngestionTracking.Id);
-            }
-        }
+        //    // then
+        //    foreach (IngestionTracking expectedIngestionTracking in expectedIngestionTrackings)
+        //    {
+        //        IngestionTracking actualIngestionTracking = actualIngestionTrackings.Single(approval => approval.Id == expectedIngestionTracking.Id);
+        //        actualIngestionTracking.Should().BeEquivalentTo(expectedIngestionTracking);
+        //        await this.apiBroker.DeleteIngestionTrackingByIdAsync(actualIngestionTracking.Id);
+        //    }
+        //}
 
-        [Fact]
-        public async Task ShouldGetIngestionTrackingAsync()
-        {
-            // given
-            IngestionTracking randomIngestionTracking = await PostRandomIngestionTrackingAsync();
-            IngestionTracking expectedIngestionTracking = randomIngestionTracking;
+        //[Fact]
+        //public async Task ShouldGetIngestionTrackingAsync()
+        //{
+        //    // given
+        //    IngestionTracking randomIngestionTracking = await PostRandomIngestionTrackingAsync();
+        //    IngestionTracking expectedIngestionTracking = randomIngestionTracking;
 
-            // when
-            IngestionTracking actualIngestionTracking = await this.apiBroker.GetIngestionTrackingByIdAsync(randomIngestionTracking.Id);
+        //    // when
+        //    IngestionTracking actualIngestionTracking = await this.apiBroker.GetIngestionTrackingByIdAsync(randomIngestionTracking.Id);
 
-            // then
-            actualIngestionTracking.Should().BeEquivalentTo(expectedIngestionTracking);
-            await this.apiBroker.DeleteIngestionTrackingByIdAsync(actualIngestionTracking.Id);
-        }
+        //    // then
+        //    actualIngestionTracking.Should().BeEquivalentTo(expectedIngestionTracking);
+        //    await this.apiBroker.DeleteIngestionTrackingByIdAsync(actualIngestionTracking.Id);
+        //}
 
-        [Fact]
-        public async Task ShouldPutIngestionTrackingAsync()
-        {
-            // given
-            IngestionTracking randomIngestionTracking = await PostRandomIngestionTrackingAsync();
-            IngestionTracking modifiedIngestionTracking = UpdateIngestionTrackingWithRandomValues(randomIngestionTracking);
+        //[Fact]
+        //public async Task ShouldPutIngestionTrackingAsync()
+        //{
+        //    // given
+        //    IngestionTracking randomIngestionTracking = await PostRandomIngestionTrackingAsync();
+        //    IngestionTracking modifiedIngestionTracking = UpdateIngestionTrackingWithRandomValues(randomIngestionTracking);
 
-            // when
-            await this.apiBroker.PutIngestionTrackingAsync(modifiedIngestionTracking);
-            IngestionTracking actualIngestionTracking = await this.apiBroker.GetIngestionTrackingByIdAsync(randomIngestionTracking.Id);
+        //    // when
+        //    await this.apiBroker.PutIngestionTrackingAsync(modifiedIngestionTracking);
+        //    IngestionTracking actualIngestionTracking = await this.apiBroker.GetIngestionTrackingByIdAsync(randomIngestionTracking.Id);
 
-            // then
-            actualIngestionTracking.Should().BeEquivalentTo(modifiedIngestionTracking);
-            await this.apiBroker.DeleteIngestionTrackingByIdAsync(actualIngestionTracking.Id);
-        }
+        //    // then
+        //    actualIngestionTracking.Should().BeEquivalentTo(modifiedIngestionTracking);
+        //    await this.apiBroker.DeleteIngestionTrackingByIdAsync(actualIngestionTracking.Id);
+        //}
 
-        [Fact]
-        public async Task ShouldDeleteIngestionTrackingAsync()
-        {
-            // given
-            IngestionTracking randomIngestionTracking = await PostRandomIngestionTrackingAsync();
-            IngestionTracking inputIngestionTracking = randomIngestionTracking;
-            IngestionTracking expectedIngestionTracking = inputIngestionTracking;
+        //[Fact]
+        //public async Task ShouldDeleteIngestionTrackingAsync()
+        //{
+        //    // given
+        //    IngestionTracking randomIngestionTracking = await PostRandomIngestionTrackingAsync();
+        //    IngestionTracking inputIngestionTracking = randomIngestionTracking;
+        //    IngestionTracking expectedIngestionTracking = inputIngestionTracking;
 
-            // when
-            IngestionTracking deletedIngestionTracking =
-                await this.apiBroker.DeleteIngestionTrackingByIdAsync(inputIngestionTracking.Id);
+        //    // when
+        //    IngestionTracking deletedIngestionTracking =
+        //        await this.apiBroker.DeleteIngestionTrackingByIdAsync(inputIngestionTracking.Id);
 
-            ValueTask<IngestionTracking> getIngestionTrackingbyIdTask =
-                this.apiBroker.GetIngestionTrackingByIdAsync(inputIngestionTracking.Id);
+        //    ValueTask<IngestionTracking> getIngestionTrackingbyIdTask =
+        //        this.apiBroker.GetIngestionTrackingByIdAsync(inputIngestionTracking.Id);
 
-            // then
-            deletedIngestionTracking.Should().BeEquivalentTo(expectedIngestionTracking);
+        //    // then
+        //    deletedIngestionTracking.Should().BeEquivalentTo(expectedIngestionTracking);
 
-            await Assert.ThrowsAsync<HttpResponseNotFoundException>(() =>
-                getIngestionTrackingbyIdTask.AsTask());
-        }
+        //    await Assert.ThrowsAsync<HttpResponseNotFoundException>(() =>
+        //        getIngestionTrackingbyIdTask.AsTask());
+        //}
     }
 }

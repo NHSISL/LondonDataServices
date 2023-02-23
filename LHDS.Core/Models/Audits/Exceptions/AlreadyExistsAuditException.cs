f66e175b248293912e5a24b18c7e3c9f561da3a5
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 13:30:26 +0000

    ShouldThrowDependencyValidationExceptionOnAddIfAuditAlreadyExsitsAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Audits/Exceptions/AlreadyExistsAuditException.cs
@@ -0,0 +1,12 @@
+using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Audits.Exceptions
+{
+    public class AlreadyExistsAuditException : Xeption
+    {
+        public AlreadyExistsAuditException(Exception innerException)
+            : base(message: "Audit with the same Id already exists.", innerException)
+        { }
+    }
+}

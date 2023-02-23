c353e86c20fe84b4275d985326b540fa9c6ec44d
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 12:00:27 +0000

    CODE RUB: Fixed model

--- LHDS.AdminPortal.Api.Tests.Acceptance/Models/Suppliers/Supplier.cs
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Models/Suppliers/Supplier.cs
@@ -5,12 +5,13 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Models.Suppliers
     public class Supplier
     {
         public Guid Id { get; set; }
-
-        // TODO:  Add your properties here
-
-        public Guid CreatedByUserId { get; set; }
+        public string Name { get; set; }
+        public string FriendlyName { get; set; }
+        public string Description { get; set; }
+        public string LandingManualTriggerUrl { get; set; }
+        public string CreatedBy { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
-        public Guid UpdatedByUserId { get; set; }
+        public string UpdatedBy { get; set; }
         public DateTimeOffset UpdatedDate { get; set; }
     }
 }

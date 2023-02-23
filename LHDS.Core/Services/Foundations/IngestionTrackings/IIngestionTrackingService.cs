c1944489b1e3add7178dcbb401197c0346b96e80
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 14:25:27 +0000

    ShouldRetrieveIngestionTrackingByIdAsync -> FAIL

--- LHDS.Core/Services/Foundations/IngestionTrackings/IIngestionTrackingService.cs
+++ LHDS.Core/Services/Foundations/IngestionTrackings/IIngestionTrackingService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Models.IngestionTrackings;
@@ -8,5 +9,6 @@ namespace LHDS.Core.Services.Foundations.IngestionTrackings
     {
         ValueTask<IngestionTracking> AddIngestionTrackingAsync(IngestionTracking ingestionTracking);
         IQueryable<IngestionTracking> RetrieveAllIngestionTrackings();
+        ValueTask<IngestionTracking> RetrieveIngestionTrackingByIdAsync(Guid ingestionTrackingId);
     }
 }

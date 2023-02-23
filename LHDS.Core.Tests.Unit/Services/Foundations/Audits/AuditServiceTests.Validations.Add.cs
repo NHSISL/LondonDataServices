24eaa248249f4aa1660ac0a47f7b7d266e847896
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 13:30:15 +0000

    ShouldThrowValidationExceptionOnAddIfAuditIsInvalidAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.Validations.Add.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.Validations.Add.cs
@@ -30,7 +30,8 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Audits
                     addAuditTask.AsTask);
 
             // then
-            actualAuditValidationException.Should().BeEquivalentTo(expectedAuditValidationException);
+            actualAuditValidationException.Should()
+                .BeEquivalentTo(expectedAuditValidationException);
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
@@ -40,6 +41,76 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Audits
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfAuditIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given
+            var invalidAudit = new Audit
+            {
+                // TODO:  Add default values for your properties i.e. Name = invalidText
+            };
+
+            var invalidAuditException =
+                new InvalidAuditException();
+
+            invalidAuditException.AddData(
+                key: nameof(Audit.Id),
+                values: "Id is required");
+
+            //invalidAuditException.AddData(
+            //    key: nameof(Audit.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the Audit model
+
+            invalidAuditException.AddData(
+                key: nameof(Audit.CreatedDate),
+                values: "Date is required");
+
+            invalidAuditException.AddData(
+                key: nameof(Audit.CreatedByUserId),
+                values: "Id is required");
+
+            invalidAuditException.AddData(
+                key: nameof(Audit.UpdatedDate),
+                values: "Date is required");
+
+            invalidAuditException.AddData(
+                key: nameof(Audit.UpdatedByUserId),
+                values: "Id is required");
+
+            var expectedAuditValidationException =
+                new AuditValidationException(invalidAuditException);
+
+            // when
+            ValueTask<Audit> addAuditTask =
+                this.auditService.AddAuditAsync(invalidAudit);
+
+            AuditValidationException actualAuditValidationException =
+                await Assert.ThrowsAsync<AuditValidationException>(
+                    addAuditTask.AsTask);
+
+            // then
+            actualAuditValidationException.Should()
+                .BeEquivalentTo(expectedAuditValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAuditValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertAuditAsync(It.IsAny<Audit>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }
 }

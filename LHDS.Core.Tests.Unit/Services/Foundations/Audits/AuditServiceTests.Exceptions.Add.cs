3d5de1d993bb1ba27e5622f7d0dfe8f46b5eb4c4
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 13:30:26 +0000

    ShouldThrowDependencyValidationExceptionOnAddIfAuditAlreadyExsitsAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.Exceptions.Add.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.Exceptions.Add.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
 using Moq;
@@ -50,6 +51,57 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Audits
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(
                     expectedAuditDependencyException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnAddIfAuditAlreadyExsitsAndLogItAsync()
+        {
+            // given
+            Audit randomAudit = CreateRandomAudit();
+            Audit alreadyExistsAudit = randomAudit;
+            string randomMessage = GetRandomMessage();
+
+            var duplicateKeyException =
+                new DuplicateKeyException(randomMessage);
+
+            var alreadyExistsAuditException =
+                new AlreadyExistsAuditException(duplicateKeyException);
+
+            var expectedAuditDependencyValidationException =
+                new AuditDependencyValidationException(alreadyExistsAuditException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(duplicateKeyException);
+
+            // when
+            ValueTask<Audit> addAuditTask =
+                this.auditService.AddAuditAsync(alreadyExistsAudit);
+
+            // then
+            AuditDependencyValidationException actualAuditDependencyValidationException =
+                await Assert.ThrowsAsync<AuditDependencyValidationException>(
+                    addAuditTask.AsTask);
+
+            actualAuditDependencyValidationException.Should()
+                .BeEquivalentTo(expectedAuditDependencyValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertAuditAsync(It.IsAny<Audit>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAuditDependencyValidationException))),
                         Times.Once);
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();

451cd2cc44923aaca9d0e2bd7628d665222b9ea1
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 13:30:44 +0000

    ShouldReturnAudit -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.Serialization;
 using Microsoft.Data.SqlClient;
@@ -62,6 +63,13 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Audits
 
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static IQueryable<Audit> CreateRandomAudits()
+        {
+            return CreateAuditFiller(dateTimeOffset: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber())
+                    .AsQueryable();
+        }
 
         private static Audit CreateRandomAudit() =>
             CreateAuditFiller(dateTimeOffset: GetRandomDateTimeOffset()).Create();

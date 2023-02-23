94c7cfedda718e302ec563436e291af1b20de455
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 13:30:59 +0000

    ShouldRetrieveAuditByIdAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.Logic.RetrieveById.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.Logic.RetrieveById.cs
@@ -1,12 +1,8 @@
-// ---------------------------------------------------------------
-// Copyright (c) North East London ICB. All rights reserved.
-// ---------------------------------------------------------------
-
 using System.Threading.Tasks;
 using FluentAssertions;
 using Force.DeepCloner;
-using LHDS.Core.Models.Foundations.Audits;
 using Moq;
+using LHDS.Core.Models.Audits;
 using Xunit;
 
 namespace LHDS.Core.Tests.Unit.Services.Foundations.Audits

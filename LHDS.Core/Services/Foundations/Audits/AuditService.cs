3feefa70ee4f5f4b60db5c2fe34c74c65004a7d4
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 13:31:19 +0000

    ShouldModifyAuditAsync -> PASS

--- LHDS.Core/Services/Foundations/Audits/AuditService.cs
+++ LHDS.Core/Services/Foundations/Audits/AuditService.cs
@@ -48,7 +48,7 @@ namespace LHDS.Core.Services.Foundations.Audits
                 return maybeAudit;
             });
 
-        public ValueTask<Audit> ModifyAuditAsync(Audit audit) =>
-            throw new NotImplementedException();
+        public async ValueTask<Audit> ModifyAuditAsync(Audit audit) =>
+            await this.storageBroker.UpdateAuditAsync(audit);
     }
 }

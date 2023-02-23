5a6030786a4c5ab09dffbd71d9cf3e1e2d762701
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 13:31:03 +0000

    ShouldThrowNotFoundExceptionOnRetrieveByIdIfAuditIsNotFoundAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Audits/Exceptions/NotFoundAuditException.cs
@@ -0,0 +1,12 @@
+using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Audits.Exceptions
+{
+    public class NotFoundAuditException : Xeption
+    {
+        public NotFoundAuditException(Guid auditId)
+            : base(message: $"Couldn't find audit with auditId: {auditId}.")
+        { }
+    }
+}

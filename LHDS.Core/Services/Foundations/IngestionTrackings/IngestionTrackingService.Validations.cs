73b6a93fcda0e35037b399029f8f7b7e07579cfa
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 14:24:41 +0000

    ShouldThrowValidationExceptionOnAddIfIngestionTrackingIsNullAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/IngestionTrackings/IngestionTrackingService.Validations.cs
+++ LHDS.Core/Services/Foundations/IngestionTrackings/IngestionTrackingService.Validations.cs
@@ -1,10 +1,5 @@
-﻿// ---------------------------------------------------------------
-// Copyright (c) North East London ICB. All rights reserved.
-// ---------------------------------------------------------------
-
-using System;
-using LHDS.Core.Models.Foundations.IngestionTrackings;
-using LHDS.Core.Models.Foundations.IngestionTrackings.Exceptions;
+using LHDS.Core.Models.IngestionTrackings;
+using LHDS.Core.Models.IngestionTrackings.Exceptions;
 
 namespace LHDS.Core.Services.Foundations.IngestionTrackings
 {
@@ -13,66 +8,6 @@ namespace LHDS.Core.Services.Foundations.IngestionTrackings
         private void ValidateIngestionTrackingOnAdd(IngestionTracking ingestionTracking)
         {
             ValidateIngestionTrackingIsNotNull(ingestionTracking);
-
-            Validate(
-                (Rule: IsInvalid(ingestionTracking.Id), Parameter: nameof(IngestionTracking.Id)),
-                (Rule: IsInvalid(ingestionTracking.Source), Parameter: nameof(IngestionTracking.Source)),
-                (Rule: IsInvalid(ingestionTracking.EncryptedFileName),
-                    Parameter: nameof(IngestionTracking.EncryptedFileName)),
-                (Rule: IsInvalid(ingestionTracking.DecryptedFileName),
-                    Parameter: nameof(IngestionTracking.DecryptedFileName)),
-                (Rule: IsInvalid(ingestionTracking.CreatedDate), Parameter: nameof(IngestionTracking.CreatedDate)),
-                (Rule: IsNotRecent(ingestionTracking.CreatedDate), Parameter: nameof(IngestionTracking.CreatedDate)),
-                (Rule: IsInvalid(ingestionTracking.LastSeen), Parameter: nameof(IngestionTracking.LastSeen)));
-        }
-
-        private void ValidateIngestionTrackingOnModify(IngestionTracking ingestionTracking)
-        {
-            ValidateIngestionTrackingIsNotNull(ingestionTracking);
-
-            Validate(
-                (Rule: IsInvalid(ingestionTracking.Id), Parameter: nameof(IngestionTracking.Id)),
-                (Rule: IsInvalid(ingestionTracking.Source), Parameter: nameof(IngestionTracking.Source)),
-                (Rule: IsInvalid(ingestionTracking.EncryptedFileName),
-                    Parameter: nameof(IngestionTracking.EncryptedFileName)),
-                (Rule: IsInvalid(ingestionTracking.DecryptedFileName),
-                    Parameter: nameof(IngestionTracking.DecryptedFileName)),
-                (Rule: IsInvalid(ingestionTracking.CreatedDate), Parameter: nameof(IngestionTracking.CreatedDate)),
-                (Rule: IsInvalid(ingestionTracking.LastSeen), Parameter: nameof(IngestionTracking.LastSeen)));
-        }
-
-        private static void ValidateAgainstStorageIngestionTrackingOnModify(
-            IngestionTracking inputIngestionTracking,
-            IngestionTracking storageIngestionTracking)
-        {
-            Validate(
-                (Rule: IsNotSame(
-                    firstDate: inputIngestionTracking.CreatedDate,
-                    secondDate: storageIngestionTracking.CreatedDate,
-                    secondDateName: nameof(IngestionTracking.CreatedDate)),
-                Parameter: nameof(IngestionTracking.CreatedDate)));
-        }
-
-        public void ValidateIngestionTrackingId(string ingestionTrackingId) =>
-            Validate((Rule: IsInvalid(ingestionTrackingId), Parameter: nameof(IngestionTracking.Id)));
-
-        public void ValidateFileName(string FileName) =>
-            Validate((Rule: IsInvalid(FileName), Parameter: nameof(IngestionTracking.Id)));
-
-        private static void ValidateStorageIngestionTracking(IngestionTracking maybeIngestionTracking, string ingestionTrackingId)
-        {
-            if (maybeIngestionTracking is null)
-            {
-                throw new NotFoundIngestionTrackingException(ingestionTrackingId);
-            }
-        }
-
-        private static void ValidateStorageIngestionTrackingForFileName(IngestionTracking maybeIngestionTracking, string fileName)
-        {
-            if (maybeIngestionTracking is null)
-            {
-                throw new NotFoundIngestionTrackingForFileNameException(fileName);
-            }
         }
 
         private static void ValidateIngestionTrackingIsNotNull(IngestionTracking ingestionTracking)
@@ -81,94 +16,6 @@ namespace LHDS.Core.Services.Foundations.IngestionTrackings
             {
                 throw new NullIngestionTrackingException();
             }
-        }
-
-        private static dynamic IsInvalid(Guid id) => new
-        {
-            Condition = id == Guid.Empty,
-            Message = "Id is required"
-        };
-
-        private static dynamic IsInvalid(Guid? id) => new
-        {
-            Condition = id.HasValue && id == Guid.Empty,
-            Message = "Id is required"
-        };
-
-        private static dynamic IsInvalid(string text) => new
-        {
-            Condition = string.IsNullOrWhiteSpace(text),
-            Message = "Text is required"
-        };
-
-        private static dynamic IsInvalid(int number) => new
-        {
-            Condition = number < 0,
-            Message = "Non-zero value is required"
-        };
-
-        private static dynamic IsEqualOrSmallerThan(string text, int maxLength) => new
-        {
-            Condition = (text ?? string.Empty).Length > maxLength,
-            Message = "Text is exceeding max length"
-        };
-
-        private static dynamic IsInvalid(DateTimeOffset date) => new
-        {
-            Condition = date == default,
-            Message = "Date is required"
-        };
-
-        private static dynamic IsNotSame(
-            DateTimeOffset firstDate,
-            DateTimeOffset secondDate,
-            string secondDateName) => new
-            {
-                Condition = firstDate != secondDate,
-                Message = $"Date is not the same as {secondDateName}"
-            };
-
-        private static dynamic IsNotSame(
-                 string firstUser,
-                 string secondUser,
-                 string secondUserName) => new
-                 {
-                     Condition = firstUser != secondUser,
-                     Message = $"User is not the same as {secondUserName}"
-                 };
-
-        private dynamic IsNotRecent(DateTimeOffset date) => new
-        {
-            Condition = IsDateNotRecent(date),
-            Message = "Date is not recent"
-        };
-
-        private bool IsDateNotRecent(DateTimeOffset date)
-        {
-            DateTimeOffset currentDateTime =
-                this.dateTimeBroker.GetCurrentDateTimeOffset();
-
-            TimeSpan timeDifference = currentDateTime.Subtract(date);
-            TimeSpan oneMinute = TimeSpan.FromMinutes(1);
-
-            return timeDifference.Duration() > oneMinute;
-        }
-
-        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
-        {
-            var invalidIngestionTrackingException = new InvalidIngestionTrackingException();
-
-            foreach ((dynamic rule, string parameter) in validations)
-            {
-                if (rule.Condition)
-                {
-                    invalidIngestionTrackingException.UpsertDataList(
-                        key: parameter,
-                        value: rule.Message);
-                }
-            }
-
-            invalidIngestionTrackingException.ThrowIfContainsErrors();
         }
     }
 }

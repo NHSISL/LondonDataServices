35e844820657673c7f50f15e39f0e616d4e133cc
David Hayes <david.hayes17@nhs.net>
2023-02-23 10:45:20 +0000

    MODELS: Ingestion and audit

---
+++ LHDS.AdminPortal.Web/ClientApp/src/models/ingestionTracking/ingestionTracking.ts
@@ -0,0 +1,32 @@
+import { Guid } from 'guid-typescript';
+import { Audit } from '../audit/audit';
+
+export class IngestionTracking {
+    public id: Guid;
+    public source: string;
+    public encryptedFileName: string;
+    public decryptedFileName: string;
+    public decrypted: boolean;
+    public createdDate: Date;
+    public lastSeen: Date;
+    public fileDeleted: boolean;
+    public recordCount: number;
+    public encryptedFileSize: number;
+    public decryptedFileSize: number;
+    public audit: Audit;
+
+    constructor(ingestionTracking: any) {
+        this.id = Guid.parse(ingestionTracking.id);
+        this.source = ingestionTracking.source || "";
+        this.encryptedFileName = ingestionTracking.encryptedFileName || "";
+        this.decryptedFileName = ingestionTracking.decryptedFileName || "";
+        this.decrypted = ingestionTracking.decrypted;
+        this.createdDate = ingestionTracking.createdDate ? new Date(ingestionTracking.createdDate) : new Date();
+        this.lastSeen = ingestionTracking.lastSeen ? new Date(ingestionTracking.lastSeen) : new Date();
+        this.fileDeleted = ingestionTracking.fileDeleted;
+        this.recordCount = ingestionTracking.recordCount;
+        this.encryptedFileSize = ingestionTracking.encryptedFileSize;
+        this.decryptedFileSize = ingestionTracking.decryptedFileSize;
+        this.audit = ingestionTracking.audit;
+    }
+}

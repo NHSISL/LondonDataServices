ccd5d16ee237c30202ab2bca266cb340c89a490a
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 14:25:49 +0000

    ShouldThrowValidationExceptionOnModifyIfIngestionTrackingIsInvalidAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/IngestionTrackings/IngestionTrackingServiceTests.Validations.Modify.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/IngestionTrackings/IngestionTrackingServiceTests.Validations.Modify.cs
@@ -48,5 +48,73 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.IngestionTrackings
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfIngestionTrackingIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given 
+            var invalidIngestionTracking = new IngestionTracking
+            {
+                // TODO:  Add default values for your properties i.e. Name = invalidText
+            };
+
+            var invalidIngestionTrackingException = new InvalidIngestionTrackingException();
+
+            invalidIngestionTrackingException.AddData(
+                key: nameof(IngestionTracking.Id),
+                values: "Id is required");
+
+            //invalidIngestionTrackingException.AddData(
+            //    key: nameof(IngestionTracking.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the IngestionTracking model
+
+            invalidIngestionTrackingException.AddData(
+                key: nameof(IngestionTracking.CreatedDate),
+                values: "Date is required");
+
+            invalidIngestionTrackingException.AddData(
+                key: nameof(IngestionTracking.CreatedByUserId),
+                values: "Id is required");
+
+            invalidIngestionTrackingException.AddData(
+                key: nameof(IngestionTracking.UpdatedDate),
+                values: "Date is required");
+
+            invalidIngestionTrackingException.AddData(
+                key: nameof(IngestionTracking.UpdatedByUserId),
+                values: "Id is required");
+
+            var expectedIngestionTrackingValidationException =
+                new IngestionTrackingValidationException(invalidIngestionTrackingException);
+
+            // when
+            ValueTask<IngestionTracking> modifyIngestionTrackingTask =
+                this.ingestionTrackingService.ModifyIngestionTrackingAsync(invalidIngestionTracking);
+
+            IngestionTrackingValidationException actualIngestionTrackingValidationException =
+                await Assert.ThrowsAsync<IngestionTrackingValidationException>(
+                    modifyIngestionTrackingTask.AsTask);
+
+            //then
+            actualIngestionTrackingValidationException.Should().BeEquivalentTo(expectedIngestionTrackingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedIngestionTrackingValidationException))),
+                        Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateIngestionTrackingAsync(It.IsAny<IngestionTracking>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

b4ee6a3082fddc195f9c32e218bdadd15fbd2f59
Christo du Toit <christo@dutoit.co.uk>
2023-02-23 13:31:20 +0000

    ShouldThrowValidationExceptionOnModifyIfAuditIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.Validations.Modify.cs
@@ -0,0 +1,52 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using LHDS.Core.Models.Audits;
+using LHDS.Core.Models.Audits.Exceptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Audits
+{
+    public partial class AuditServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfAuditIsNullAndLogItAsync()
+        {
+            // given
+            Audit nullAudit = null;
+            var nullAuditException = new NullAuditException();
+
+            var expectedAuditValidationException =
+                new AuditValidationException(nullAuditException);
+
+            // when
+            ValueTask<Audit> modifyAuditTask =
+                this.auditService.ModifyAuditAsync(nullAudit);
+
+            AuditValidationException actualAuditValidationException =
+                await Assert.ThrowsAsync<AuditValidationException>(
+                    modifyAuditTask.AsTask);
+
+            // then
+            actualAuditValidationException.Should()
+                .BeEquivalentTo(expectedAuditValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAuditValidationException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateAuditAsync(It.IsAny<Audit>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

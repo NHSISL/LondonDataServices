689ff185aca5f572b29c0fcaa01642beb7d0091e
Christo du Toit <christo@dutoit.co.uk>
2023-02-27 09:56:59 +0000

    CODE RUB: Code Cleanup

--- LHDS.Core/Brokers/Storages/Blobs/BlobStorageBroker.cs
+++ LHDS.Core/Brokers/Storages/Blobs/BlobStorageBroker.cs
@@ -14,19 +14,24 @@ namespace LHDS.Core.Brokers.Storages.Blobs
         private readonly IAzureBlobClient azureBlobClient;
         private readonly IBlobStorageBrokerSettings blobStorageBrokerSettings;
 
-        public BlobStorageBroker(IAzureBlobClient azureBlobClient, IBlobStorageBrokerSettings blobStorageBrokerSettings)
+        public BlobStorageBroker(
+            IAzureBlobClient azureBlobClient,
+            IBlobStorageBrokerSettings blobStorageBrokerSettings)
         {
             this.azureBlobClient = azureBlobClient;
             this.blobStorageBrokerSettings = blobStorageBrokerSettings;
         }
 
         public async ValueTask InsertFileAsync(string fileName, Stream stream) =>
-            await azureBlobClient.UploadFileAsync(fileName, stream, blobStorageBrokerSettings.BlobContainerName);
+            await azureBlobClient.UploadFileAsync(
+                fileName,
+                stream,
+                container: blobStorageBrokerSettings.BlobContainerName);
 
         public async ValueTask<byte[]> SelectByFileNameAsync(string fileName)
         {
             MemoryStream ms = await azureBlobClient
-                .DownloadFileAsync(fileName, blobStorageBrokerSettings.BlobContainerName);
+                .DownloadFileAsync(fileName, container: blobStorageBrokerSettings.BlobContainerName);
 
             return ms.ToArray();
         }
@@ -36,7 +41,10 @@ namespace LHDS.Core.Brokers.Storages.Blobs
 
         public async ValueTask<string> GetDownloadLinkAsync(string fileName, DateTimeOffset expiresOn)
         {
-            Uri uri = await azureBlobClient.GetDownloadUriAsync(fileName, blobStorageBrokerSettings.BlobContainerName, expiresOn);
+            Uri uri = await azureBlobClient.GetDownloadUriAsync(
+                fileName,
+                container: blobStorageBrokerSettings.BlobContainerName,
+                expiresOn);
 
             return uri.ToString();
         }

f1ee30001bd6c3ce1ee29d0aa39209af88b9db9f
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-02-27 22:29:59 +0000

    WIP: Ingestion Tracking Filtering

---
+++ LHDS.AdminPortal.Web/ClientApp/src/services/views/ingestionTrackingHomeViewService.ts
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { IngestionTracking } from "../../models/ingestionTrackings/ingestionTracking";
+import { IngestionTrackingHomeView } from "../../models/ingestionTrackings/ingestionTrackingHomeView";
+import { ingestionTrackingService } from "../foundations/ingestionTrackingService";
+
+
+export const IngestionTrackingHomeViewService = {
+
+    useGetAllIngestionTrackings: (searchTerm?: string) => {
+        try {
+            let query = `?$expand=borough&$expand=sites($select=*&$expand=propertyStatus;$orderby=siteName desc )&$orderby=isActive desc,IngestionTrackingName`;
+
+            if (searchTerm) {
+                query = query + `&$filter=contains(IngestionTrackingName,'${searchTerm}')`
+            }
+
+            const response = ingestionTrackingService.useGetAllIngestionTrackingPages(query);
+            const [mappedIngestionTrackings, setMappedIngestionTrackings] = useState<Array<IngestionTrackingHomeView>>();
+            const [pages, setPages] = useState<any>([]);
+
+            useEffect(() => {
+                if (response.data && response.data.pages) {
+                    const IngestionTrackings: Array<IngestionTrackingHomeView> = [];
+                    response.data.pages.forEach(x => {
+                        x.data.forEach((ingestionTracking: IngestionTracking) => {
+                            IngestionTrackings.push(new IngestionTrackingHomeView(
+                                ingestionTracking.id,
+                                ingestionTracking.source,
+                                ingestionTracking.encryptedFileName,
+                                ingestionTracking.decryptedFileName,
+                                ingestionTracking.decrypted,
+                                ingestionTracking.lastSeen,
+                                ingestionTracking.fileDeleted,
+                                ingestionTracking.recordCount,
+                                ingestionTracking.encryptedFileSize,
+                                ingestionTracking.decryptedFileSize,
+                                ingestionTracking.audit,
+                            ));
+                        })
+                        setMappedIngestionTrackings(IngestionTrackings);
+                    });
+                    setPages(response.data.pages);
+                }
+            }, [response.data]);
+
+            return {
+                mappedIngestionTrackings, pages, ...response
+            }
+
+        } catch (err) {
+            throw err;
+        }
+    },
+}

6cd287b0526e0ccd6309d238c036c55fecc53bfd
Christo du Toit <christo@dutoit.co.uk>
2023-03-01 12:30:52 +0000

    DATA: Added DecryptionManualTriggerUrl to Supplier

--- LHDS.Core/Models/Foundations/Suppliers/Supplier.cs
+++ LHDS.Core/Models/Foundations/Suppliers/Supplier.cs
@@ -16,6 +16,7 @@ namespace LHDS.Core.Models.Foundations.Suppliers
         public string FriendlyName { get; set; }
         public string Description { get; set; }
         public string LandingManualTriggerUrl { get; set; }
+        public string DecryptionManualTriggerUrl { get; set; }
         public string CreatedBy { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
         public string UpdatedBy { get; set; }

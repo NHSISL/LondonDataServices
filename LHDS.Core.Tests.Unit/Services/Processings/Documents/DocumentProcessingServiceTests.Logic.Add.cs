6dae18ea3ab825b3b4612000aef674ea4d80b2f5
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-03-08 15:59:41 +0000

    ShouldAddFileAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/Documents/DocumentProcessingServiceTests.Logic.Add.cs
@@ -0,0 +1,38 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Text;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Documents;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.Documents
+{
+    public partial class DocumentProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddFileAsync()
+        {
+            // Given
+            var randomFileName = GetRandomString();
+            var randomfileData = Encoding.ASCII.GetBytes(GetRandomString());
+
+            Document document = new Document
+            {
+                FileName = randomFileName,
+                DocumentData = randomfileData
+            };
+
+            // When
+            await this.documentProcessingService.AddDocumentAsync(document);
+
+            // Then
+            this.documentServiceMock.Verify(service => 
+                service.AddDocumentAsync(It.Is<Document>(doc =>
+                    doc.FileName == randomFileName && doc.DocumentData == randomfileData)), 
+                    Times.Once);
+        }
+    }
+}

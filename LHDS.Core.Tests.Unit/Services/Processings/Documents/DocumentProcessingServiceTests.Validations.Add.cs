322c7f1698155622bce57a7672eac5955f57a7e8
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-03-09 14:59:29 +0000

    ShouldThrowValidationExceptionsOnAddIfDocumentProcessingIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/Documents/DocumentProcessingServiceTests.Validations.Add.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.Documents;
+using LHDS.Core.Models.Processings.Documents.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.Documents
+{
+    public partial class DocumentProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionsOnAddIfDocumentProcessingIsNullAndLogItAsync()
+        {
+            // given
+            Document nullDocument = null;
+
+            var nullDocumentProcessingException =
+                new NullDocumentProcessingException();
+
+            var expectedDocumentProcessingValidationException =
+                new DocumentProcessingValidationException(nullDocumentProcessingException);
+
+            this.documentServiceMock.Setup(service =>
+                service.AddDocumentAsync(nullDocument))
+                    .Throws(expectedDocumentProcessingValidationException);
+
+            // when
+            ValueTask AddDocumentTask =
+                this.documentProcessingService.AddDocumentAsync(nullDocument);
+
+            DocumentProcessingValidationException actualDocumentProcessingValidationException =
+                await Assert.ThrowsAsync<DocumentProcessingValidationException>(AddDocumentTask.AsTask);
+
+            //then
+            actualDocumentProcessingValidationException.Should()
+                .BeEquivalentTo(expectedDocumentProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedDocumentProcessingValidationException))),
+                        Times.Once);
+
+            this.documentServiceMock.Verify(service =>
+                service.AddDocumentAsync(nullDocument),
+                    Times.Once); 
+
+            this.documentServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LHDS.Core/Models/Processings/Documents/Exceptions/NullDocumentProcessingException.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Processings.Documents.Exceptions
+{
+    public class NullDocumentProcessingException : Xeption
+    {
+        public NullDocumentProcessingException()
+            : base(message: $"Document processing is Null") { }
+    }
+}

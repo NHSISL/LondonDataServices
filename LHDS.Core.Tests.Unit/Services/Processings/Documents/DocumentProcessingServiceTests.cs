8a9ed52487588b5425da583810d2ad6dce4e412b
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-03-09 14:59:29 +0000

    ShouldThrowValidationExceptionsOnAddIfDocumentProcessingIsNullAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Processings/Documents/DocumentProcessingServiceTests.cs
+++ LHDS.Core.Tests.Unit/Services/Processings/Documents/DocumentProcessingServiceTests.cs
@@ -2,23 +2,32 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
-
+using System.Linq.Expressions;
+using System;
+using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Loggings;
 using LHDS.Core.Services.Foundations.Documents;
 using LHDS.Core.Services.Processings.Documents;
 using Moq;
 using Tynamix.ObjectFiller;
+using Xeptions;
 
 namespace LHDS.Core.Tests.Unit.Services.Processings.Documents
 {
     public partial class DocumentProcessingServiceTests
     {
         private readonly Mock<IDocumentService> documentServiceMock = new Mock<IDocumentService>();
+        private readonly Mock<ILoggingBroker> loggingBrokerMock = new Mock<ILoggingBroker>();
         private readonly IDocumentProcessingService documentProcessingService;
 
         public DocumentProcessingServiceTests()
         {
-            documentProcessingService = new DocumentProcessingService(documentServiceMock.Object);
+            this.documentProcessingService = new DocumentProcessingService(
+                this.documentServiceMock.Object);
         }
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+           actualException => actualException.SameExceptionAs(expectedException);
 
         private static string GetRandomString() =>
             new MnemonicString().GetValue();
---
+++ LHDS.Core/Models/Processings/Documents/Exceptions/DocumentProcessingValidationException.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Processings.Documents.Exceptions
+{
+    public class DocumentProcessingValidationException : Xeption
+    {
+        private const string validationMessage = "Document processing validation errors occured, please try again";
+
+        public DocumentProcessingValidationException(Xeption innerException, string validationSummary = "")
+            : base(
+                  message: validationSummary.Length > 0
+                    ? $"{validationMessage}  Validation errors: {validationSummary}"
+                    : validationMessage,
+                  innerException)
+        { }
+    }
+}

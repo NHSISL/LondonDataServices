a5aade7f7e0ac53740b7be1ddd941448e9e8564c
Christo du Toit <christo@dutoit.co.uk>
2023-03-10 14:05:01 +0000

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/OptOuts/OptOutServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,53 @@
+using System;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using LHDS.Core.Models.OptOuts.Exceptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.OptOuts
+{
+    public partial class OptOutServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedStorageException =
+                new FailedOptOutStorageException(sqlException);
+
+            var expectedOptOutDependencyException =
+                new OptOutDependencyException(failedStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllOptOuts())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllOptOutsAction = () =>
+                this.optOutService.RetrieveAllOptOuts();
+
+            OptOutDependencyException actualOptOutDependencyException =
+                Assert.Throws<OptOutDependencyException>(retrieveAllOptOutsAction);
+
+            // then
+            actualOptOutDependencyException.Should()
+                .BeEquivalentTo(expectedOptOutDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllOptOuts(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedOptOutDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

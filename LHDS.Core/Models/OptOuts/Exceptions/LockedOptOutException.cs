50b3cb38d067dc4a139f36d69d49714171168d7e
Christo du Toit <christo@dutoit.co.uk>
2023-03-10 14:05:33 +0000

    ShouldThrowDependencyValidationExceptionOnModifyIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/OptOuts/Exceptions/LockedOptOutException.cs
@@ -0,0 +1,13 @@
+using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.OptOuts.Exceptions
+{
+    public class LockedOptOutException : Xeption
+    {
+        public LockedOptOutException(Exception innerException)
+            : base(message: "Locked optOut record exception, please try again later", innerException)
+        {
+        }
+    }
+}

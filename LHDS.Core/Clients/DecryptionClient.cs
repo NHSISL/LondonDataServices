7894fde84e300d4d38bc30b974c669e72fe72d7b
Christo du Toit <christo@dutoit.co.uk>
2023-03-10 11:28:35 +0000

    CODE RUB: Extended Error Details

--- LHDS.Core/Clients/DecryptionClient.cs
+++ LHDS.Core/Clients/DecryptionClient.cs
@@ -2,52 +2,67 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using LHDS.Core.Models.Clients.DecryptClient.Exceptions;
 using LHDS.Core.Models.Orchestrations.Decryptions.Exceptions;
 using LHDS.Core.Services.Orchestrations.Decryptions;
 using Xeptions;
 
-namespace LHDS.Core.Clients
-{
-    public class DecryptionClient : IDecryptionClient
-    {
+namespace LHDS.Core.Clients {
+    public class DecryptionClient : IDecryptionClient {
         private readonly IDecryptionOrchestrationService decryptionOrchestrationService;
 
-        public DecryptionClient(IDecryptionOrchestrationService decryptionOrchestrationService)
-        {
+        public DecryptionClient(IDecryptionOrchestrationService decryptionOrchestrationService) {
             this.decryptionOrchestrationService = decryptionOrchestrationService;
         }
 
-        public async ValueTask DecryptAsync(string fileName)
-        {
-            try
-            {
+        public async ValueTask DecryptAsync(string fileName) {
+            try {
                 await decryptionOrchestrationService.DecryptAsync(fileName);
             }
-            catch (DecryptionOrchestrationValidationException decryptionOrchestrationValidationException)
-            {
+            catch (DecryptionOrchestrationValidationException decryptionOrchestrationValidationException) {
+                string validationSummary = GetValidationSummary(
+                    decryptionOrchestrationValidationException.InnerException.Data);
+
                 throw new DecryptionClientValidationException(
-                    decryptionOrchestrationValidationException.InnerException as Xeption);
+                    decryptionOrchestrationValidationException.InnerException as Xeption, validationSummary);
             }
             catch (DecryptionOrchestrationDependencyValidationException
-                decryptionOrchestrationDependencyValidationException)
-            {
+                decryptionOrchestrationDependencyValidationException) {
+                string validationSummary = GetValidationSummary(
+                    decryptionOrchestrationDependencyValidationException.InnerException.Data);
+
                 throw new DecryptionClientValidationException(
-                    decryptionOrchestrationDependencyValidationException.InnerException as Xeption);
+                    decryptionOrchestrationDependencyValidationException.InnerException as Xeption, validationSummary);
             }
             catch (DecryptionOrchestrationDependencyException
-                decryptionOrchestrationDependencyException)
-            {
+                decryptionOrchestrationDependencyException) {
                 throw new DecryptionClientDependencyException(
                     decryptionOrchestrationDependencyException.InnerException as Xeption);
             }
             catch (DecryptionOrchestrationServiceException
-                decryptionOrchestrationServiceException)
-            {
+                decryptionOrchestrationServiceException) {
                 throw new DecryptionClientServiceException(
                     decryptionOrchestrationServiceException.InnerException as Xeption);
             }
+        }
+
+        private string GetValidationSummary(IDictionary data) {
+            StringBuilder validationSummary = new StringBuilder();
+
+            foreach (DictionaryEntry entry in data) {
+                string errorSummary = ((List<string>)entry.Value)
+                    .Select((string value) => value)
+                    .Aggregate((string current, string next) => current + ", " + next);
+
+                validationSummary.Append($"{entry.Key} => {errorSummary};  ");
+            }
+
+            return validationSummary.ToString();
         }
     }
 }

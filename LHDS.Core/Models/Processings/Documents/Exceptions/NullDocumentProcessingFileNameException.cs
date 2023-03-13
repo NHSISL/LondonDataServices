f7147b4bf43e206cf55d22d10b03d3dc7f7321b2
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-03-13 16:39:50 +0000

    ShouldThrowValidationExceptionsOnRetrieveIfDocumentProcessingIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Processings/Documents/Exceptions/NullDocumentProcessingFileNameException.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Processings.Documents.Exceptions
+{
+    public class NullDocumentProcessingFileNameException : Xeption
+    {
+        public NullDocumentProcessingFileNameException()
+            : base(message: "Null document processing file name. Please correct the errors and try again.") { }
+    }
+}

1d1ef63085d41a2d56a4f9d080bb47fbd4eceef2
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-03-13 12:05:46 +0000

    ShouldThrowDependencyOnAddIfDependencyErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Processings/Documents/Exceptions/DocumentProcessingDependencyException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Processings.Documents.Exceptions
+{
+    public class DocumentProcessingDependencyException : Xeption
+    {
+        public DocumentProcessingDependencyException(Xeption innerException) :
+            base(message: "Document processing dependency error occurred, contact support.", innerException)
+        { }
+    }
+}

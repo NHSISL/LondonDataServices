29487a2b6da66a00233ad36591eb1d1faf52b3a2
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-03-13 12:17:51 +0000

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAsync -> FAIL

---
+++ LHDS.Core/Models/Processings/Documents/Exceptions/DocumentProcessingServiceException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Processings.Documents.Exceptions
+{
+    public class DocumentProcessingServiceException : Xeption
+    {
+        public DocumentProcessingServiceException(Xeption innerException)
+          : base(message: "Document processing service error occurred, contact support.",
+                innerException)
+        { }
+    }
+}

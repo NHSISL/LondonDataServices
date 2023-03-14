96536eb8ed86faaf2d4e6e56801b0b46432d935f
David Hayes <david.hayes17@nhs.net>
2023-03-14 15:48:20 +0000

    ShouldReturnAknowledgeMessageByIdAsync -> FAIL

--- LHDS.Core/Services/Foundations/Mesh/IMeshService.cs
+++ LHDS.Core/Services/Foundations/Mesh/IMeshService.cs
@@ -9,5 +9,7 @@ namespace LHDS.Core.Services.Foundations.Mesh
     public interface IMeshService
     {
         ValueTask<bool> ValidateAccessAsync();
+
+        ValueTask<bool> AcknowledgeMessageByIdAsync(string inputMailboxId, string inputMessageId);
     }
 }

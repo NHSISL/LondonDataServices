f252009b47888b77a2de5c11efd8c37c7e215a97
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-03-14 11:16:56 +0000

    ShouldThrowValidationExceptionsOnGetDownloadLinkIfDocumentProcessingIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/Documents/DocumentProcessingServiceTests.Validations.GetDownloadLink.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.Documents;
+using LHDS.Core.Models.Processings.Documents.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.Documents
+{
+    public partial class DocumentProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionsOnGetDownloadLinkIfDocumentProcessingIsNullAndLogItAsync()
+        {
+            // given
+            string nullFileName = null;
+
+            Document document = new Document
+            {
+                FileName = nullFileName
+            };
+
+            var nullDocumentProcessingFileNameException =
+                new NullDocumentProcessingFileNameException();
+
+            var expectedDocumentProcessingValidationException =
+                new DocumentProcessingValidationException(nullDocumentProcessingFileNameException);
+
+            // when
+            ValueTask<string> GetDownloadLinkTask =
+                this.documentProcessingService.GetDownloadLinkAsync(document.FileName);
+
+            DocumentProcessingValidationException actualDocumentProcessingValidationException =
+                await Assert.ThrowsAsync<DocumentProcessingValidationException>(GetDownloadLinkTask.AsTask);
+
+            //then
+            actualDocumentProcessingValidationException.Should()
+                .BeEquivalentTo(expectedDocumentProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedDocumentProcessingValidationException))),
+                        Times.Once);
+
+            this.documentServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

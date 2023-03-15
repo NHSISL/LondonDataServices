e8cb5f33031c00e9ed1cc31b48568d8470080218
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-03-15 16:38:49 +0000

    ShouldThrowValidationExceptionsOnAddIfOptOutProcessingIsNullAndLogItAsync -> PASS

---
+++ LHDS.Core/Services/Processings/OptOuts/OptOutProcessingService.Exceptions.cs
@@ -0,0 +1,41 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.OptOuts;
+using LHDS.Core.Models.Processings.OptOuts.Exceptions;
+using Xeptions;
+
+namespace LHDS.Core.Services.Processings.OptOuts
+{
+    public partial class OptOutProcessingService
+    {
+        private delegate ValueTask<OptOut> ReturningOptOutFunction();
+
+        private async ValueTask<OptOut> TryCatch(ReturningOptOutFunction returningOptOutFunction)
+        {
+            try
+            {
+                return await returningOptOutFunction();
+            }
+            catch (NullOptOutProcessingException nullOptOutProcessingException)
+            {
+                throw CreateAndLogValidationException(nullOptOutProcessingException);
+            }
+        }
+
+        private OptOutProcessingValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var optOutProcessingValidationException =
+                new OptOutProcessingValidationException(exception);
+
+            this.loggingBroker.LogError(optOutProcessingValidationException);
+
+            return optOutProcessingValidationException;
+        }
+    }
+}
+
---
+++ LHDS.Core/Services/Processings/OptOuts/OptOutProcessingService.Validations.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using LHDS.Core.Models.Foundations.OptOuts;
+using LHDS.Core.Models.Processings.OptOuts.Exceptions;
+using LHDS.Core.Services.Foundations.OptOuts;
+
+namespace LHDS.Core.Services.Processings.OptOuts
+{
+    public partial class OptOutProcessingService
+    {
+        private static void ValidateOptOutProcessingOnRetrieveOrAdd(OptOut optOut)
+        {
+            ValidateOptOutProcessingIsNotNull(optOut);
+        }
+
+        private static void ValidateOptOutProcessingIsNotNull(OptOut optOut)
+        {
+            if (optOut is null)
+            {
+                throw new NullOptOutProcessingException();
+            }
+        }
+    }
+}
+

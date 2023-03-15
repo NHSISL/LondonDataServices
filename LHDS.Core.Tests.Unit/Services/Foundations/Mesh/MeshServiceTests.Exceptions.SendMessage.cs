8573c5a7db1de0d0fad774937d86b1a0be79b350
David Hayes <david.hayes17@nhs.net>
2023-03-15 14:43:17 +0000

    ShouldThrowServiceExceptionOnSendMessageIfServiceErrorOccursAndLogItAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.Exceptions.SendMessage.cs
@@ -0,0 +1,58 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.MeshItems.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Mesh
+{
+    public partial class MeshServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnSendMessageIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            string messageId = GetRandomMessage();
+            var serviceException = new Exception();
+
+            var failedMeshServiceException =
+               new FailedMeshServiceException(serviceException);
+
+            var expectedMeshServiceException =
+               new MeshServiceException(failedMeshServiceException);
+
+            this.meshBrokerMock.Setup(broker =>
+                broker.SendMessageAsync(It.IsAny<string>()))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<string> RetrieveSendMessageTask =
+                this.meshService.SendMessageAsync(messageId);
+
+            MeshServiceException actualMeshServiceException =
+                await Assert.ThrowsAsync<MeshServiceException>
+                    (RetrieveSendMessageTask.AsTask);
+
+            // then
+            actualMeshServiceException.Should()
+                .BeEquivalentTo(expectedMeshServiceException);
+
+            this.meshBrokerMock.Verify(broker =>
+                broker.SendMessageAsync(It.IsAny<string>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(
+                   expectedMeshServiceException))),
+                       Times.Once);
+
+            this.meshBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

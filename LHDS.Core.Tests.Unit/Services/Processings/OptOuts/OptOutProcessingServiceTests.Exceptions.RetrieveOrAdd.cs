8f0ed32d819dde080b5f61884c0fb73a5558a99c
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-03-15 18:27:43 +0000

    ShouldThrowDependencyValidationExceptionOnAddIfDependencyValidationErrorOccursAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Processings/OptOuts/OptOutProcessingServiceTests.Exceptions.RetrieveOrAdd.cs
+++ LHDS.Core.Tests.Unit/Services/Processings/OptOuts/OptOutProcessingServiceTests.Exceptions.RetrieveOrAdd.cs
@@ -59,5 +59,50 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.OptOuts
             this.optOutServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnAddIfDependencyValidationErrorOccursAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            OptOut someOptOut = CreateRandomOptOut();
+            OptOut inputOptOut = someOptOut;
+
+            var expectedOptOutProcessingDependencyValidationException =
+                new OptOutProcessingDependencyValidationException(
+                    dependencyValidationException.InnerException as Xeption);
+
+            this.optOutServiceMock.Setup(service =>
+                service.AddOptOutAsync(inputOptOut))
+                    .Throws(dependencyValidationException);
+
+            // when
+            ValueTask<OptOut> optOutRetrieveOrAddTask =
+                this.optOutProcessingService.RetrieveOrAddOptOutAsync(inputOptOut);
+
+            OptOutProcessingDependencyValidationException actualException =
+                await Assert.ThrowsAsync<OptOutProcessingDependencyValidationException>(optOutRetrieveOrAddTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(
+                expectedOptOutProcessingDependencyValidationException);
+
+            this.optOutServiceMock.Verify(service =>
+                service.RetrieveOptOutByIdAsync(inputOptOut.Id),
+                    Times.Once);
+
+            this.optOutServiceMock.Verify(service =>
+                service.AddOptOutAsync(inputOptOut),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedOptOutProcessingDependencyValidationException))),
+                         Times.Once);
+
+            this.optOutServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

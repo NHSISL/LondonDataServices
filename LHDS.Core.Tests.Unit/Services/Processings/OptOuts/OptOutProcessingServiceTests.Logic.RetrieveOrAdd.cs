cd87a4dda974bfa38b31494305f742cba05f35be
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-03-15 14:47:54 +0000

    ShouldNotAddOptOutWhenExistingOptOutIsFoundAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Processings/OptOuts/OptOutProcessingServiceTests.Logic.RetrieveOrAdd.cs
+++ LHDS.Core.Tests.Unit/Services/Processings/OptOuts/OptOutProcessingServiceTests.Logic.RetrieveOrAdd.cs
@@ -3,10 +3,11 @@
 // ---------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
-using LHDS.Core.Models.Foundations.Documents;
 using LHDS.Core.Models.Foundations.OptOuts;
 using Moq;
 using Xunit;
@@ -32,6 +33,36 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.OptOuts
             this.optOutServiceMock.Verify(service =>
                 service.AddOptOutAsync(inputOptOut),
                     Times.Once);
+
+            this.optOutServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldNotAddOptOutWhenExistingOptOutIsFoundAsync()
+        {
+            // given
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+            OptOut existingOptOut = CreateRandomOptOut(randomDateTimeOffset);
+            OptOut existingOptOutFound = existingOptOut;
+
+            this.optOutServiceMock.Setup(service => 
+                service.RetrieveOptOutByIdAsync(existingOptOut.Id))
+                    .ReturnsAsync(existingOptOutFound);
+
+            // when
+            OptOut retrievedOptOut = await this.optOutProcessingService.RetrieveOrAddOptOutAsync(existingOptOut);
+
+            // then
+            retrievedOptOut.Should().BeEquivalentTo(existingOptOutFound);
+
+            this.optOutServiceMock.Verify(service => 
+                service.RetrieveOptOutByIdAsync(existingOptOut.Id),
+                    Times.Once);
+
+            this.optOutServiceMock.Verify(service => 
+                service.AddOptOutAsync(It.IsAny<OptOut>()), 
+                    Times.Never);
 
             this.optOutServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();

6612a99f0ecb92fb28bacdec424d8076afbc417e
David Hayes <david.hayes17@nhs.net>
2023-03-15 14:22:00 +0000

    ShouldReturnSendMessageAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.Logic.SendMessage.cs
@@ -0,0 +1,42 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Mesh
+{
+    public partial class MeshServiceTests
+    {
+        [Fact]
+        public async Task ShouldReturnSendMessageAsync()
+        {
+            // given
+            string randomMessageId = GetRandomMessage();
+            string inputMessageId = randomMessageId;
+            string outputValidationResult = "";
+            string expectedValidationResult = outputValidationResult;
+
+            this.meshBrokerMock.Setup(broker =>
+                broker.SendMessageAsync(inputMessageId))
+                    .ReturnsAsync(outputValidationResult);
+
+            // when
+            string actualMeshValidation =
+                await this.meshService.SendMessageAsync(inputMessageId);
+
+            // then
+            actualMeshValidation.Should().Be(expectedValidationResult);
+
+            this.meshBrokerMock.Verify(broker =>
+                broker.SendMessageAsync(inputMessageId),
+                    Times.Once());
+
+            this.meshBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

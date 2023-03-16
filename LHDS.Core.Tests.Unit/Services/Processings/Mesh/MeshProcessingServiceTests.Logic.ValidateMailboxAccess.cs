a7272f3f45e4038d26fa2d2d3567d9a009bae126
David Hayes <david.hayes17@nhs.net>
2023-03-16 11:27:43 +0000

    ShouldReturnMeshValidationAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/Mesh/MeshProcessingServiceTests.Logic.ValidateMailboxAccess.cs
@@ -0,0 +1,36 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.Mesh
+{
+    public partial class MeshProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldReturnMeshValidationAsync()
+        {
+            // given
+            bool outputValidationResult = true;
+            bool expectedValidationResult = outputValidationResult;
+
+            // when
+            bool actualMeshValidationResult =
+                await this.meshProcessingService.ValidateMailboxAccessAsync();
+
+            // then
+            actualMeshValidationResult.Should().Be(expectedValidationResult);
+
+            this.meshServiceMock.Verify(service =>
+                service.ValidateMailboxAccessAsync(),
+                    Times.Once());
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LHDS.Core.Tests.Unit/Services/Processings/Mesh/MeshProcessingServiceTests.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Services.Foundations.Mesh;
+using LHDS.Core.Services.Processings.Mesh;
+using Moq;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.Mesh
+{
+    public partial class MeshProcessingServiceTests
+    {
+        private readonly Mock<IMeshService> meshServiceMock = new Mock<IMeshService>();
+        private readonly Mock<ILoggingBroker> loggingBrokerMock = new Mock<ILoggingBroker>();
+        private readonly IMeshProcessingService meshProcessingService;
+
+        public MeshProcessingServiceTests()
+        {
+            this.meshProcessingService = new MeshProcessingService(
+                this.meshServiceMock.Object,
+                this.loggingBrokerMock.Object);
+        }
+    }
+}

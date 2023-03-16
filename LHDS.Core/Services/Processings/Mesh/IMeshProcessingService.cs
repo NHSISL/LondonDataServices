88a742c1394887623d3b913dcc8e265e8de8cdee
David Hayes <david.hayes17@nhs.net>
2023-03-16 11:27:43 +0000

    ShouldReturnMeshValidationAsync -> FAIL

---
+++ LHDS.Core/Services/Processings/Mesh/IMeshProcessingService.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LHDS.Core.Services.Processings.Mesh
+{
+    public interface IMeshProcessingService
+    {
+        ValueTask<bool> ValidateMailboxAccessAsync();
+        ValueTask<List<string>> RetrieveMessageIdsFromInboxAsync(string mailboxId);
+        ValueTask<string> RetrieveAndAcknowledgeMessageByIdAsync(string mailboxId, string messageId);
+        ValueTask<string> SendMessageAsync(string mailboxId, string messageId);
+    }
+}

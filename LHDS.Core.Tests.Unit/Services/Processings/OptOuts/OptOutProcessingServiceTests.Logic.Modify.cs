7852d7ad2d3640c58bebeafc39be8b1e175d037e
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-03-16 14:07:14 +0000

    ShouldModifyOptOutProcessingAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Processings/OptOuts/OptOutProcessingServiceTests.Logic.Modify.cs
+++ LHDS.Core.Tests.Unit/Services/Processings/OptOuts/OptOutProcessingServiceTests.Logic.Modify.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Force.DeepCloner;
 using LHDS.Core.Models.Foundations.OptOuts;
 using Moq;
 using Xunit;
@@ -25,11 +26,20 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.OptOuts
 
             OptOut randomOptOut = CreateRandomOptOut(randomDateTimeOffset);
             OptOut inputOptOut = randomOptOut;
+            OptOut updatedOptOut = inputOptOut;
+            OptOut expectedOptOut = updatedOptOut.DeepClone();
+
+            this.optOutServiceMock.Setup(service =>
+                service.ModifyOptOutAsync(inputOptOut))
+                    .ReturnsAsync(inputOptOut);
 
             // when
-            await this.optOutProcessingService.ModifyOptOutAsync(inputOptOut);
+            OptOut actualOptOut = 
+                await this.optOutProcessingService.ModifyOptOutAsync(inputOptOut);
 
             // then
+            actualOptOut.Should().BeEquivalentTo(expectedOptOut);
+
             this.optOutServiceMock.Verify(service =>
                 service.ModifyOptOutAsync(inputOptOut),
                     Times.Once);

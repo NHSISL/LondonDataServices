f0b063ba32d1e3a3fd4caac787569644ae29d418
David Hayes <david.hayes17@nhs.net>
2023-03-16 12:15:57 +0000

    ShouldThrowDependencyOnValidateAccessIfDependencyErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/MeshItems/Exceptions/MeshDependencyException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.MeshItems.Exceptions
+{
+    public class MeshDependencyException : Xeption
+    {
+        public MeshDependencyException(Xeption innerException) :
+            base(message: "Mesh dependency error occurred, contact support.", innerException)
+        { }
+    }
+}

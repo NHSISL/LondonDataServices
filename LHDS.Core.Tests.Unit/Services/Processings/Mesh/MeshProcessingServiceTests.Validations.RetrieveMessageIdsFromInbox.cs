d7ce47cd32ba1796d86f0681f4e75a82989b9902
David Hayes <david.hayes17@nhs.net>
2023-03-17 11:06:37 +0000

    ShouldThrowValidationExceptionOnRetrieveMessageIdsFromInboxIfArgsIsInvalidAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/Mesh/MeshProcessingServiceTests.Validations.RetrieveMessageIdsFromInbox.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Processings.Mesh.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.Mesh
+{
+    public partial class MeshProcessingServiceTests
+    {
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnRetrieveMessageIdsFromInboxIfArgsIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            string mailboxId = invalidText;
+
+            var invalidMeshProcessingArgumentException =
+                new InvalidMeshProcessingArgumentException();
+
+            invalidMeshProcessingArgumentException.AddData(
+                key: nameof(mailboxId),
+                values: "Text is required");
+
+            var expectedMeshProcessingValidationException =
+            new MeshProcessingValidationException(
+                   innerException: invalidMeshProcessingArgumentException,
+                   validationSummary: GetValidationSummary(invalidMeshProcessingArgumentException.Data));
+
+            // when
+            ValueTask<List<string>> retrieveMessageIdsFromInboxTask =
+                this.meshProcessingService.RetrieveMessageIdsFromInboxAsync(mailboxId);
+
+            MeshProcessingValidationException actualMeshProcessingValidationException =
+                await Assert.ThrowsAsync<MeshProcessingValidationException>(retrieveMessageIdsFromInboxTask.AsTask);
+
+            //then
+            actualMeshProcessingValidationException.Should()
+                .BeEquivalentTo(expectedMeshProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMeshProcessingValidationException))),
+                        Times.Once);
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

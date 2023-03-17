e039fde1e1c249f29f3514ce17ec6403b1e2ce6e
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-03-17 09:59:04 +0000

    ShouldThrowValidationExceptionsOnRemoveIfOptOutProcessingIdIsNullAndLogItAsync -> PASS

--- LHDS.Core/Services/Processings/OptOuts/OptOutProcessingService.Validations.cs
+++ LHDS.Core/Services/Processings/OptOuts/OptOutProcessingService.Validations.cs
@@ -9,6 +9,9 @@ using LHDS.Core.Models.Foundations.OptOuts;
 using LHDS.Core.Models.Processings.OptOuts.Exceptions;
 using LHDS.Core.Services.Foundations.OptOuts;
 using System.Linq;
+using LHDS.Core.Models.Foundations.OptOuts.Exceptions;
+using LHDS.Core.Models.Foundations.OptOuts;
+using System;
 
 namespace LHDS.Core.Services.Processings.OptOuts
 {
@@ -30,6 +33,32 @@ namespace LHDS.Core.Services.Processings.OptOuts
             {
                 throw new NullOptOutProcessingException();
             }
+        }
+
+        public void ValidateOptOutId(Guid optOutId) =>
+            Validate((Rule: IsInvalid(optOutId), Parameter: nameof(OptOut.Id)));
+
+        private static dynamic IsInvalid(Guid id) => new
+        {
+            Condition = id == Guid.Empty,
+            Message = "Id is required"
+        };
+
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidOptOutProcessingIdException = new InvalidOptOutProcessingIdException();
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidOptOutProcessingIdException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidOptOutProcessingIdException.ThrowIfContainsErrors();
         }
 
         private string GetValidationSummary(IDictionary data)

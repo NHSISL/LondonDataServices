7edade74534eedd7162ebc6c11ffedab2e96a9a8
David Hayes <david.hayes17@nhs.net>
2023-03-17 11:06:37 +0000

    ShouldThrowValidationExceptionOnRetrieveMessageIdsFromInboxIfArgsIsInvalidAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Processings/Mesh/Exceptions/InvalidMeshProcessingArgumentException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Processings.Mesh.Exceptions
+{
+    public class InvalidMeshProcessingArgumentException : Xeption
+    {
+        public InvalidMeshProcessingArgumentException()
+               : base(message: "Invalid mesh processing argument. Please correct the errors and try again.")
+        { }
+    }
+}

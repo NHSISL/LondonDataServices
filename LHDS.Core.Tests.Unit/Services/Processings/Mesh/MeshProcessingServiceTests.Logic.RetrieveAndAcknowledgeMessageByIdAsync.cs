89008f66c3c81043c7bdc32603fe67b29d42fb9c
David Hayes <david.hayes17@nhs.net>
2023-03-17 12:34:38 +0000

    ShouldReturnRetrieveMessageIdAndAcknowledgeAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/Mesh/MeshProcessingServiceTests.Logic.RetrieveAndAcknowledgeMessageByIdAsync.cs
@@ -0,0 +1,38 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.Mesh
+{
+    public partial class MeshProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldReturnRetrieveMessageIdAndAcknowledgeAsync()
+        {
+            // given
+            string randomMailboxId = GetRandomString();
+            string inputMailboxId = randomMailboxId;
+            string randomMessageId = GetRandomString();
+            string inputMessageId = randomMessageId;
+
+            // when
+            await this.meshProcessingService.RetrieveAndAcknowledgeMessageByIdAsync(inputMailboxId, inputMessageId);
+
+            // then
+            this.meshServiceMock.Verify(service =>
+                service.RetrieveMessageByIdAsync(inputMailboxId, inputMessageId),
+                    Times.Once());
+
+            this.meshServiceMock.Verify(service =>
+               service.AcknowledgeMessageByIdAsync(inputMailboxId, inputMessageId),
+                   Times.Once());
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

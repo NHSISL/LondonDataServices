4f4d56e342d9191b037357ece48985b5c59eeca7
David Hayes <david.hayes17@nhs.net>
2023-03-17 15:37:20 +0000

    ShouldThrowDependencyOnSendMessageIfDependencyErrorOccursAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Processings/Mesh/MeshProcessingServiceTests.Exceptions.SendMessage.cs
+++ LHDS.Core.Tests.Unit/Services/Processings/Mesh/MeshProcessingServiceTests.Exceptions.SendMessage.cs
@@ -60,5 +60,54 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.Mesh
             this.meshServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task ShouldThrowDependencyOnSendMessageIfDependencyErrorOccursAndLogItAsync(
+         Xeption dependencyException)
+        {
+            // given
+            string mailboxId = GetRandomString();
+            string messageId = GetRandomString();
+
+            var expectedMeshProcessingDependencyException =
+                new MeshProcessingDependencyException(
+                    dependencyException.InnerException as Xeption);
+
+            this.meshServiceMock.Setup(service =>
+             service.RetrieveTrackingStatusAsync(mailboxId, messageId))
+                 .Throws(dependencyException);
+
+            this.meshServiceMock.Setup(service =>
+                service.SendMessageAsync(messageId))
+                    .Throws(dependencyException);
+
+            // when
+            ValueTask<string> retrieveMessageAndAcknowledgeTask =
+                this.meshProcessingService.SendMessageAsync(mailboxId, messageId);
+
+            MeshProcessingDependencyException actualException =
+                await Assert.ThrowsAsync<MeshProcessingDependencyException>(retrieveMessageAndAcknowledgeTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedMeshProcessingDependencyException);
+
+            this.meshServiceMock.Verify(service =>
+                service.RetrieveTrackingStatusAsync(mailboxId, messageId),
+                    Times.Once);
+
+            this.meshServiceMock.Verify(service =>
+               service.SendMessageAsync(messageId),
+                   Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedMeshProcessingDependencyException))),
+                         Times.Once);
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }

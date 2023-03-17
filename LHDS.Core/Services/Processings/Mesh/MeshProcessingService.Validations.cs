05347fea0490115a9383dfe504c85eb0b06f588b
David Hayes <david.hayes17@nhs.net>
2023-03-17 13:45:12 +0000

    ShouldThrowValidationExceptionOnRetrieveMessagAndkAcknowledgeIfArgsIsInvalidAndLogItAsync -> FAIL

--- LHDS.Core/Services/Processings/Mesh/MeshProcessingService.Validations.cs
+++ LHDS.Core/Services/Processings/Mesh/MeshProcessingService.Validations.cs
@@ -12,6 +12,11 @@ namespace LHDS.Core.Services.Processings.Mesh
 {
     public partial class MeshProcessingService
     {
+        public void ValidateMeshArgs(string mailboxId, string messageId) =>
+           Validate(
+               (Rule: IsInvalid(mailboxId), Parameter: nameof(mailboxId)),
+               (Rule: IsInvalid(messageId), Parameter: nameof(messageId)));
+
         private static void ValidateGetArguments(string mailboxId)
         {
             Validate(

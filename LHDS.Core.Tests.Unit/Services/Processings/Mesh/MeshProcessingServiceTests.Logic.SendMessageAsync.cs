42fc430b02a29b021a3c2fcf0f892cb32aabeebd
David Hayes <david.hayes17@nhs.net>
2023-03-17 14:18:26 +0000

    ShouldReturnSendMessageAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/Mesh/MeshProcessingServiceTests.Logic.SendMessageAsync.cs
@@ -0,0 +1,34 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.Mesh
+{
+    public partial class MeshProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldReturnSendMessageAsync()
+        {
+            // given
+            string randomMailboxId = GetRandomString();
+            string inputMailboxId = randomMailboxId;
+            string randomMessageId = GetRandomString();
+            string inputMessageId = randomMessageId;
+
+            // when
+            await this.meshProcessingService.SendMessageAsync(inputMailboxId, inputMessageId);
+
+            // then
+            this.meshServiceMock.Verify(service =>
+                service.SendMessageAsync(inputMessageId),
+                    Times.Once());
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

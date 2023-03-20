9abded314859a71399a24e06ab0caf9db6672375
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-03-20 09:52:01 +0000

    ShouldThrowValidationExceptionsOnRetrieveIfOptOutProcessingNhsNumberIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Processings/OptOuts/Exceptions/InvalidArgumentOptOutProcessingException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Processings.OptOuts.Exceptions
+{
+    public class InvalidArgumentOptOutProcessingException : Xeption
+    {
+        public InvalidArgumentOptOutProcessingException()
+            : base(message: "Invalid opt out processing argument. Please correct the errors and try again.") 
+        { }
+    }
+}

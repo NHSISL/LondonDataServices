4b83c3a5ccf55faeb1ca14c086a2fc7ead673a8a
Christo du Toit <christo@dutoit.co.uk>
2023-03-20 13:15:37 +0000

    CODE RUB: Stubbed Exceptions

---
+++ LHDS.Core/Models/Foundations/Mesh/DeleteMe/MeshClientServiceException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.Mesh.DeleteMe
+{
+    internal class MeshClientServiceException : Xeption
+    {
+        public MeshClientServiceException(Xeption innerException)
+            : base(message: "Mesh client dependency validation error occurred, contact support.", innerException)
+        { }
+    }
+}

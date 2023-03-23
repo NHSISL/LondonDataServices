3cb7c90717f2f95a31e062d276529824270a0dba
David Hayes <david.hayes17@nhs.net>
2023-03-23 10:18:05 +0000

    BROKERS: Add Crud methods

---
+++ LHDS.AdminPortal.Web/ClientApp/src/brokers/apiBroker.optout.ts
@@ -0,0 +1,41 @@
+import { Guid } from "guid-typescript";
+import { OptOut } from "../models/optout/optout";
+import ApiBroker from "./apiBroker";
+
+class OptOutBroker {
+    relativeOptOutUrl = '/api/optout';
+
+    private apiBroker: ApiBroker = new ApiBroker();
+
+    async PostOptOutAsync(optout: OptOut) {
+        return await this.apiBroker.PostAsync(this.relativeOptOutUrl, optout)
+            .then(result => new OptOut(result.data));
+    }
+
+    async GetAllOptOutsAsync(queryString: string) {
+        var url = this.relativeOptOutUrl + queryString;
+
+        return await this.apiBroker.GetAsync(url)
+            .then(result => result.data.map((optOut: any) => new OptOut(optOut)));
+    }
+
+    async GetOptOutByNhsNumberAsync(nhsNumber: string) {
+        const url = `${this.relativeOptOutUrl}/${nhsNumber}`;
+
+        return await this.apiBroker.GetAsync(url)
+            .then(result => new OptOut(result.data));
+    }
+
+    async PutOptOutAsync(optout: OptOut) {
+        return await this.apiBroker.PutAsync(this.relativeOptOutUrl, optout)
+            .then(result => new OptOut(result.data));
+    }
+
+    async DeleteOptOutByIdAsync(id: Guid) {
+        const url = `${this.relativeOptOutUrl}/${id}`;
+
+        return await this.apiBroker.DeleteAsync(url)
+            .then(result => new OptOut(result.data));
+    }
+}
+export default OptOutBroker;

c7efa797828abebd455a389e7537184a7bbd7507
David Hayes <david.hayes17@nhs.net>
2023-03-27 13:01:49 +0100

    CODE RUB: Remove line breaks

--- LHDS.AdminPortal.Web/ClientApp/src/components/NavigationBar.tsx
+++ LHDS.AdminPortal.Web/ClientApp/src/components/NavigationBar.tsx
@@ -10,7 +10,6 @@ export const NavigationBar = () => {
                     <Navbar.Brand href="#"></Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
-
                         <Nav className="me-auto my-2 my-lg-0"
                             style={{ maxHeight: '100px' }}
                             navbarScroll>
@@ -18,12 +17,9 @@ export const NavigationBar = () => {
                             <SecuredLink to="/ingestionTracking">Supplier Data</SecuredLink>
                             <NavDropdown title="Opt Out">
                                 <SecuredLink to="/optOut">  Search Opt-Out</SecuredLink>
-
                                 <SecuredLink to="/optOut">  Upload Opt-Out</SecuredLink> 
                             </NavDropdown>
                         </Nav>
-
-                        
                     </Navbar.Collapse>
                 </Container>
             </Navbar>

fd608a14f2a83be72cad7e046516843148eaa8c8
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-04-17 22:28:50 +0100

    ShouldThrowValidationExceptionOnSendFileIfRequiredMessageItemsAreNullAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.Validations.SendFile.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.Validations.SendFile.cs
@@ -3,6 +3,7 @@
 // ---------------------------------------------------------------
 
 using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
 using LHDS.Core.Models.Foundations.Mesh;
@@ -42,6 +43,66 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Mesh
 
             this.meshBrokerMock.Verify(broker =>
                 broker.SendFileAsync(nullMessage),
+                        Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMeshValidationException))),
+                        Times.Once);
+
+            this.meshBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnSendFileIfRequiredMessageItemsAreNullAsync(
+            string invalidInput)
+        {
+            // given
+            string inputMessageId = GetRandomString();
+
+            MeshMessage randomMeshMessage = new MeshMessage
+            {
+                MessageId = inputMessageId,
+                Headers = null,
+                FileContent = null,
+            };
+
+            var inputMeshMessage = randomMeshMessage;
+
+            var invalidMeshMessageException =
+                new InvalidMeshMessageException();
+
+            invalidMeshMessageException.AddData(
+                key: nameof(Message.FileContent),
+                values: "Content is required");
+
+            invalidMeshMessageException.AddData(
+                key: "Headers",
+                values: "Values is required");
+
+            var expectedMeshValidationException =
+                new MeshValidationException(
+                innerException: invalidMeshMessageException,
+                validationSummary: GetValidationSummary(invalidMeshMessageException.Data));
+
+            // when
+            ValueTask<MeshMessage> sendFileTask =
+                this.meshService.SendFileAsync(inputMeshMessage);
+
+            MeshValidationException actualMeshValidationException =
+                await Assert.ThrowsAsync<MeshValidationException>(() =>
+                    sendFileTask.AsTask());
+
+            // then
+            actualMeshValidationException.Should()
+                .BeEquivalentTo(expectedMeshValidationException);
+
+            this.meshBrokerMock.Verify(broker =>
+                broker.SendFileAsync(It.IsAny<Message>()),
                         Times.Never);
 
             this.loggingBrokerMock.Verify(broker =>

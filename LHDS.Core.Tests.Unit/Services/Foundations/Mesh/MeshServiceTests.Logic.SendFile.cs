233938cbefdc29f665aaacbf91a247c3ba8a8471
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-04-17 22:05:11 +0100

    ShouldReturnSendFileAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.Logic.SendFile.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.Logic.SendFile.cs
@@ -1,69 +1,69 @@
-﻿//// ---------------------------------------------------------------
-//// Copyright (c) North East London ICB. All rights reserved.
-//// ---------------------------------------------------------------
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
 
-//using System.Net.Http;
-//using System.Reflection.PortableExecutable;
-//using System.Runtime.InteropServices;
-//using System.Threading.Tasks;
-//using FluentAssertions;
-//using LHDS.Core.Models.Foundations.Mesh;
-//using Moq;
-//using NEL.MESH.Models.Foundations.Mesh;
-//using Xunit;
+using System.Net.Http;
+using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.Mesh;
+using Moq;
+using NEL.MESH.Models.Foundations.Mesh;
+using Xunit;
 
-//namespace LHDS.Core.Tests.Unit.Services.Foundations.Mesh
-//{
-//    public partial class MeshServiceTests
-//    {
-//        [Fact]
-//        public async Task ShouldReturnSendFileAsync()
-//        {
-//            // given
-//            dynamic dynamicMeshMessageProperties =
-//                CreateRandomMeshMessageProperties();
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Mesh
+{
+    public partial class MeshServiceTests
+    {
+        [Fact]
+        public async Task ShouldReturnSendFileAsync()
+        {
+            // given
+            dynamic dynamicMeshMessageProperties =
+                CreateRandomMeshMessageProperties();
 
-//            var randomMessage = new Message
-//            {
-//                MessageId = dynamicMeshMessageProperties.MessageId,
-//                Headers = dynamicMeshMessageProperties.Headers,
-//                StringContent = dynamicMeshMessageProperties.StringContent,
-//                FileContent = dynamicMeshMessageProperties.FileContent,
-//                TrackingInfo = MaptToMessageTrackingInfo(dynamicMeshMessageProperties.TrackingInfo)
-//            };
+            var randomMessage = new Message
+            {
+                MessageId = dynamicMeshMessageProperties.MessageId,
+                Headers = dynamicMeshMessageProperties.Headers,
+                StringContent = dynamicMeshMessageProperties.StringContent,
+                FileContent = dynamicMeshMessageProperties.FileContent,
+                TrackingInfo = MaptToMessageTrackingInfo(dynamicMeshMessageProperties.TrackingInfo)
+            };
 
-//            var inputMessage = randomMessage;
-//            var outputMessage = inputMessage;
+            var inputMessage = randomMessage;
+            var outputMessage = inputMessage;
 
-//            MeshMessage randomMeshMessage = new MeshMessage
-//            {
-//                MessageId = dynamicMeshMessageProperties.MessageId,
-//                Headers = dynamicMeshMessageProperties.Headers,
-//                StringContent = dynamicMeshMessageProperties.StringContent,
-//                FileContent = dynamicMeshMessageProperties.FileContent,
-//                TrackingInfo = MaptToMeshMessageTrackingInfo(dynamicMeshMessageProperties.TrackingInfo)
-//            };
+            MeshMessage randomMeshMessage = new MeshMessage
+            {
+                MessageId = dynamicMeshMessageProperties.MessageId,
+                Headers = dynamicMeshMessageProperties.Headers,
+                StringContent = dynamicMeshMessageProperties.StringContent,
+                FileContent = dynamicMeshMessageProperties.FileContent,
+                TrackingInfo = MaptToMeshMessageTrackingInfo(dynamicMeshMessageProperties.TrackingInfo)
+            };
 
-//            var inputMeshMessage = randomMeshMessage;
-//            var expectedMeshMessage = randomMeshMessage;
+            var inputMeshMessage = randomMeshMessage;
+            var expectedMeshMessage = randomMeshMessage;
 
-//            this.meshBrokerMock.Setup(broker =>
-//                broker.SendMessageAsync(It.Is(SameMessageAs(inputMessage))))
-//                    .ReturnsAsync(outputMessage);
+            this.meshBrokerMock.Setup(broker =>
+                broker.SendFileAsync(It.Is(SameMessageAs(inputMessage))))
+                    .ReturnsAsync(outputMessage);
 
-//            // when
-//            MeshMessage actualMeshMessage =
-//                await this.meshService.SendFileAsync(inputMeshMessage);
+            // when
+            MeshMessage actualMeshMessage =
+                await this.meshService.SendFileAsync(inputMeshMessage);
 
-//            // then
-//            actualMeshMessage.Should().BeEquivalentTo(expectedMeshMessage);
+            // then
+            actualMeshMessage.Should().BeEquivalentTo(expectedMeshMessage);
 
-//            this.meshBrokerMock.Verify(broker =>
-//                broker.SendFileAsync(It.Is(SameMessageAs(inputMessage))),
-//                    Times.Once());
+            this.meshBrokerMock.Verify(broker =>
+                broker.SendFileAsync(It.Is(SameMessageAs(inputMessage))),
+                    Times.Once());
 
-//            this.meshBrokerMock.VerifyNoOtherCalls();
-//            this.loggingBrokerMock.VerifyNoOtherCalls();
-//        }
-//    }
-//}
+            this.meshBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

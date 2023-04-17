7b5ff35868720d294342b9fc2d0732ba5141c60f
Christo du Toit <christo@dutoit.co.uk>
2023-04-17 18:09:34 +0100

    CODE RUB:  Method stubs for CSV Mapper

---
+++ LHDS.Core/Services/Processings/CsvMappers/CsvMapperProcessingService.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.CsvMappers
+{
+    internal class CsvMapperProcessingService : ICsvMapperProcessingService
+    {
+        public ValueTask<List<T>> MapCsvDataToObjectAsync<T>(byte[] data) =>
+            throw new NotImplementedException();
+
+        public ValueTask<byte[]> MapObjectToCsvDataAsync<T>(List<T> @object) =>
+            throw new NotImplementedException();
+    }
+}

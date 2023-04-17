75587132dbbb6df3e35fd6bca4b417d542c9d176
Christo du Toit <christo@dutoit.co.uk>
2023-04-17 14:52:28 +0100

    ShouldRetrieveOptOutStatusAsync -> WIP

---
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/OptOuts/OptOutOrchestrationTests.Logic.RetrieveOptOutStatus.cs
@@ -0,0 +1,29 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LHDS.Core.Tests.Unit.Services.Orchestrations.OptOuts
+{
+    public class OptOutOrchestrationTests
+    {
+        public async Task ShouldRetrieveOptOutStatusAsync()
+        {
+            // given
+            var randomBytes = Encoding.ASCII.GetBytes(GetRandomString());
+            var inputBytes = randomBytes;
+
+
+            // when
+            await this.optOutOrchestrationService.RetrieveOptOutStatusAsync(inputBytes);
+
+
+            // then
+            this.optOutProcessingServiceMock.VerifyNoOtherCalls();
+            this.meshProcessingServiceMock.VerifyNoOtherCalls();
+            this.documentProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LHDS.Core/Services/Orchestrations/OptOuts/IOptOutOrchestrationService.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace LHDS.Core.Services.Orchestrations.OptOuts
+{
+    public interface IOptOutOrchestrationService
+    {
+        ValueTask RetrieveOptOutStatusAsync(byte[] optOutFile);
+        ValueTask PushExpiredOptOutsToMeshForRenewalAsync();
+        ValueTask RetrieveUpdatedMeshOptOutStatusChangesAsync();
+    }
+}

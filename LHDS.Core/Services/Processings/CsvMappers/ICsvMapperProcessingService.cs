853cb769f59895c29dc0108ce1c66122ccebb54c
David Hayes <david.hayes17@nhs.net>
2023-04-17 16:48:07 +0100

    ShouldRetrieveOptOutStatusAsync -> FAIL

---
+++ LHDS.Core/Services/Processings/CsvMappers/ICsvMapperProcessingService.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.CsvMappers
+{
+    public interface ICsvMapperProcessingService
+    {
+        ValueTask<List<T>> MapCsvDataToObjectAsync<T>(byte[] data);
+        ValueTask<byte[]> MapObjectToCsvDataAsync<T>(List<T> @object);
+    }
+}

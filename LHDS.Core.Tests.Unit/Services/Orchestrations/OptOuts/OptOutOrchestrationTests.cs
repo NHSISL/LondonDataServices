81dda3218daf3e4464016b8929eb3407e1e57c55
Christo du Toit <christo@dutoit.co.uk>
2023-04-17 14:52:28 +0100

    ShouldRetrieveOptOutStatusAsync -> WIP

---
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/OptOuts/OptOutOrchestrationTests.cs
@@ -0,0 +1,36 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using LHDS.Core.Services.Orchestrations.OptOuts;
+using LHDS.Core.Services.Processings.Documents;
+using LHDS.Core.Services.Processings.Mesh;
+using LHDS.Core.Services.Processings.OptOuts;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace LHDS.Core.Tests.Unit.Services.Orchestrations.OptOuts
+{
+    public partial class OptOutOrchestrationTests
+    {
+        private readonly Mock<IOptOutProcessingService> optOutProcessingServiceMock;
+        private readonly Mock<IDocumentProcessingService> documentProcessingServiceMock;
+        private readonly Mock<IMeshProcessingService> meshProcessingServiceMock;
+        private readonly OptOutOrchestrationService optOutOrchestrationService;
+
+        public OptOutOrchestrationTests()
+        {
+            this.optOutProcessingServiceMock = new Mock<IOptOutProcessingService>();
+            this.documentProcessingServiceMock = new Mock<IDocumentProcessingService>();
+            this.meshProcessingServiceMock = new Mock<IMeshProcessingService>();
+
+            this.optOutOrchestrationService = new OptOutOrchestrationService(
+                optOutProcessingService: optOutProcessingServiceMock.Object,
+                documentProcessingService: documentProcessingServiceMock.Object,
+                meshProcessingService: meshProcessingServiceMock.Object);
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+    }
+}
---
+++ LHDS.Core/Services/Orchestrations/OptOuts/OptOutOrchestrationService.cs
@@ -0,0 +1,38 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using LHDS.Core.Services.Processings.Documents;
+using LHDS.Core.Services.Processings.Mesh;
+using LHDS.Core.Services.Processings.OptOuts;
+
+namespace LHDS.Core.Services.Orchestrations.OptOuts
+{
+    public partial class OptOutOrchestrationService : IOptOutOrchestrationService
+    {
+        private readonly IOptOutProcessingService optOutProcessingService;
+        private readonly IDocumentProcessingService documentProcessingService;
+        private readonly IMeshProcessingService meshProcessingService;
+
+        public OptOutOrchestrationService(
+            IOptOutProcessingService optOutProcessingService,
+            IDocumentProcessingService documentProcessingService,
+            IMeshProcessingService meshProcessingService)
+        {
+            this.optOutProcessingService = optOutProcessingService;
+            this.documentProcessingService = documentProcessingService;
+            this.meshProcessingService = meshProcessingService;
+        }
+
+        public ValueTask RetrieveOptOutStatusAsync(byte[] optOutFile) =>
+            throw new NotImplementedException();
+
+        public ValueTask PushExpiredOptOutsToMeshForRenewalAsync() =>
+            throw new NotImplementedException();
+
+        public ValueTask RetrieveUpdatedMeshOptOutStatusChangesAsync() =>
+            throw new NotImplementedException();
+    }
+}

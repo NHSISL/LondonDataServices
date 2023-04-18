025115b713782196b90d46e70ce9129df58c11c3
David Hayes <david.hayes17@nhs.net>
2023-04-18 14:26:17 +0100

    ShouldThrowValidationExceptionOnRetrieveOptOutIfRequestIdIsNullAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Orchestrations/OptOuts/OptOutOrchestrationTests.Validations.RetrieveOptOutStatus.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/OptOuts/OptOutOrchestrationTests.Validations.RetrieveOptOutStatus.cs
@@ -2,6 +2,7 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
 using LHDS.Core.Models.Orchestrations.OptOuts.Exceptions;
@@ -34,6 +35,50 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.OptOuts
             // when
             ValueTask RetrieveOptOutStatusTask =
                 this.optOutOrchestrationService.RetrieveOptOutStatusAsync(invalidData, randomString);
+
+            RetrieveOptOutStatusOrchestrationValidationException actualException =
+               await Assert.ThrowsAsync<RetrieveOptOutStatusOrchestrationValidationException>(RetrieveOptOutStatusTask.AsTask);
+
+            // then
+            actualException.Should()
+                .BeEquivalentTo(expectedRetrieveOptOutStatusOrchestrationOptOutFileValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedRetrieveOptOutStatusOrchestrationOptOutFileValidationException))),
+                        Times.Once);
+
+            this.optOutProcessingServiceMock.VerifyNoOtherCalls();
+            this.csvMapperProcessingServiceMock.VerifyNoOtherCalls();
+            this.meshProcessingServiceMock.VerifyNoOtherCalls();
+            this.documentProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnRetrieveOptOutIfRequestIdIsNullAndLogItAsync(string invalidText)
+        {
+            var randomString = GetRandomString();
+            byte[] randomBytes = Encoding.UTF8.GetBytes(randomString);
+
+            var invalidArgumentRetieveOptOutStatusOrchestrationException =
+               new InvalidArgumentRetieveOptOutStatusOrchestrationException();
+
+            invalidArgumentRetieveOptOutStatusOrchestrationException.AddData(
+               key: "RequestId",
+               values: "Text is required");
+
+            var expectedRetrieveOptOutStatusOrchestrationOptOutFileValidationException =
+               new RetrieveOptOutStatusOrchestrationValidationException(
+                   innerException: invalidArgumentRetieveOptOutStatusOrchestrationException,
+                   validationSummary: GetValidationSummary(invalidArgumentRetieveOptOutStatusOrchestrationException.Data));
+
+            // when
+            ValueTask RetrieveOptOutStatusTask =
+                this.optOutOrchestrationService.RetrieveOptOutStatusAsync(randomBytes, invalidText);
 
             RetrieveOptOutStatusOrchestrationValidationException actualException =
                await Assert.ThrowsAsync<RetrieveOptOutStatusOrchestrationValidationException>(RetrieveOptOutStatusTask.AsTask);

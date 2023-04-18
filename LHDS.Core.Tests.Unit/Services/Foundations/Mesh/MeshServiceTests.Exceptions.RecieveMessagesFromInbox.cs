b73a0f4eee199def51a77b79f8ef9892b9009762
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-04-18 14:28:11 +0100

    ShouldThrowMeshServiceDependencyValidationExceptionOnRecieveMessagesFromInboxIfValidationFailsAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.Exceptions.RecieveMessagesFromInbox.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Mesh/MeshServiceTests.Exceptions.RecieveMessagesFromInbox.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using LHDS.Core.Models.Foundations.Mesh.Exceptions;
 using Moq;
+using NEL.MESH.Models.Clients.Mesh.Exceptions;
+using Xeptions;
 using Xunit;
 
 namespace LHDS.Core.Tests.Unit.Services.Foundations.Mesh
@@ -15,7 +17,50 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Mesh
     public partial class MeshServiceTests
     {
         [Fact]
-        public async Task ShouldThrowServiceExceptionOnRecieveMessageIdsFromInboxIfServiceErrorOccursAndLogItAsync()
+        public async Task ShouldThrowMeshServiceDependencyValidationExceptionOnRecieveMessagesFromInboxIfValidationFailsAndLogItAsync()
+        {
+            // given
+            string randomMessage = GetRandomMessage();
+            var validationException = new Exception(randomMessage);
+
+            var meshClientValidationException =
+                new MeshClientValidationException(validationException as Xeption);
+
+            var expectedDependencyValidationException =
+                new MeshServiceDependencyValidationException(meshClientValidationException);
+
+            this.meshBrokerMock.Setup(broker =>
+                broker.RetrieveMessagesAsync())
+                    .ThrowsAsync(meshClientValidationException);
+
+            // when
+            ValueTask<List<string>> retrieveMessagesFromInboxTask =
+                this.meshService.RetrieveMessagesFromInboxAsync();
+
+            MeshServiceDependencyValidationException actualValidationException =
+                await Assert.ThrowsAsync<MeshServiceDependencyValidationException>(retrieveMessagesFromInboxTask.AsTask);
+
+            // then
+            MeshServiceDependencyValidationException actualMeshServiceDependencyValidationException =
+                await Assert.ThrowsAsync<MeshServiceDependencyValidationException>(
+                    retrieveMessagesFromInboxTask.AsTask);
+
+            actualValidationException.Should().BeEquivalentTo(expectedDependencyValidationException);
+
+            this.meshBrokerMock.Verify(broker =>
+                broker.RetrieveMessagesAsync(),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(expectedDependencyValidationException))),
+                   Times.Once);
+
+            this.meshBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRecieveMessagesFromInboxIfServiceErrorOccursAndLogItAsync()
         {
             // given
             string messageId = GetRandomMessage();
@@ -32,12 +77,12 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Mesh
                     .ThrowsAsync(serviceException);
 
             // when
-            ValueTask<List<string>> RetrieveMessageIdsFromInboxTask =
+            ValueTask<List<string>> retrieveMessagesFromInboxTask =
                 this.meshService.RetrieveMessagesFromInboxAsync();
 
             MeshServiceException actualMeshServiceException =
                 await Assert.ThrowsAsync<MeshServiceException>
-                    (RetrieveMessageIdsFromInboxTask.AsTask);
+                    (retrieveMessagesFromInboxTask.AsTask);
 
             // then
             actualMeshServiceException.Should()

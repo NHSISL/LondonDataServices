7848e174a1b80abb9a8edca3caf270cfec2b7ec7
Christo du Toit <christo@dutoit.co.uk>
2023-04-18 14:26:45 +0100

    ShouldThrowValidationExceptionOnMapCsvToObjectIfInputsIsInvalidAndLogItAsync -> PASS

---
+++ LHDS.Core/Services/Foundations/CsvMappers/CsvMapperService.Exceptions.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.CsvMappers.Exceptions;
+using Xeptions;
+
+namespace LHDS.Core.Brokers.CsvMappers
+{
+    public partial class CsvMapperService : ICsvMapperService
+    {
+        private delegate ValueTask<T> ReturningObjectFunction<T>();
+
+        private async ValueTask<T> TryCatch<T>(ReturningObjectFunction<T> returningObjectFunction)
+        {
+            try
+            {
+                return await returningObjectFunction();
+            }
+            catch (InvalidCsvMapperArgumentsException invalidCsvMapperArgumentsException)
+            {
+                throw CreateAndLogValidationException(invalidCsvMapperArgumentsException);
+            }
+        }
+
+        private CsvMapperValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var csvMapperValidationException = new CsvMapperValidationException(exception);
+            this.loggingBroker.LogError(csvMapperValidationException);
+
+            return csvMapperValidationException;
+        }
+    }
+}

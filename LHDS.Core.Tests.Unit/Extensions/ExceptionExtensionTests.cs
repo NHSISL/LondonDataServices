bb234344ad87c75221bec923c93183a4f726b27d
Christo du Toit <christo@dutoit.co.uk>
2023-04-18 16:56:26 +0100

    FOUNDATIONS:  Validation Summary Extension

---
+++ LHDS.Core.Tests.Unit/Extensions/ExceptionExtensionTests.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Tynamix.ObjectFiller;
+
+namespace LHDS.Core.Tests.Unit.Extensions
+{
+    public partial class ExceptionExtensionTests
+    {
+        private static string GetRandomString() =>
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+    }
+}

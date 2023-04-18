f3396efb6a45938a946f0eebfb3bf9ed49e79268
Christo du Toit <christo@dutoit.co.uk>
2023-04-18 11:19:26 +0100

    BROKERS: CSV Mapping Broker

---
+++ LHDS.Core/Brokers/CsvMappers/ICsvMapperBroker.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LHDS.Core.Brokers.CsvMappers
+{
+    public interface ICsvMapperBroker
+    {
+        ValueTask<List<T>> MapCsvDataToObjectAsync<T>(string data, bool hasHeaderRecord);
+        ValueTask<string> MapObjectToCsvDataAsync<T>(List<T> @object, bool addHeaderRecord);
+    }
+}

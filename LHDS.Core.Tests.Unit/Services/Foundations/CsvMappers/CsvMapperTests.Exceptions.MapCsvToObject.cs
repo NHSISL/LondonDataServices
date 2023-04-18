3a5c26a372b828183bf9fb758408db896cde038e
Christo du Toit <christo@dutoit.co.uk>
2023-04-18 14:47:37 +0100

    ShouldThrowServiceExceptionOnMapCsvToObjectIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/CsvMappers/CsvMapperTests.Exceptions.MapCsvToObject.cs
@@ -0,0 +1,61 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.CsvMappers.Exceptions;
+using LHDS.Core.Models.Foundations.OptOuts;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.CsvMappers
+{
+    public partial class CsvMapperTests
+    {
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnMapCsvToObjectIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            string randomCsvFormattedOptOutData = GetRandomString();
+            string inputCsvFormattedOptOutData = randomCsvFormattedOptOutData;
+            bool withHeaderRecord = true;
+            var serviceException = new Exception();
+
+            var failedCsvMapperServiceException =
+                new FailedCsvMapperServiceException(serviceException);
+
+            var expectedCsvMapperServiceException =
+                new CsvMapperServiceException(failedCsvMapperServiceException);
+
+            this.csvMapperBrokerMock.Setup(broker =>
+                broker.MapCsvToObjectAsync<OptOut>(inputCsvFormattedOptOutData, withHeaderRecord))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<List<OptOut>> mapCsvToObjectTask = this.csvMapperService.MapCsvToObjectAsync<OptOut>(
+                data: inputCsvFormattedOptOutData,
+                hasHeaderRecord: withHeaderRecord);
+
+            CsvMapperServiceException actualCsvMapperServiceException =
+                await Assert.ThrowsAsync<CsvMapperServiceException>(mapCsvToObjectTask.AsTask);
+
+            // then
+            actualCsvMapperServiceException.Should().BeEquivalentTo(expectedCsvMapperServiceException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedCsvMapperServiceException))),
+                        Times.Once);
+
+            this.csvMapperBrokerMock.Verify(broker =>
+                broker.MapCsvToObjectAsync<OptOut>(inputCsvFormattedOptOutData, withHeaderRecord),
+                    Times.Never());
+
+            this.csvMapperBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

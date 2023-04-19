29c5dcf078b7a2c6bdf9c5eb4cb0441424c1b46e
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-04-19 12:21:35 +0100

    CLIENTS: Opt Out Client Push Expired Opt Outs To Mesh For Renewal

--- LHDS.Core/Clients/OptOutClient.cs
+++ LHDS.Core/Clients/OptOutClient.cs
@@ -3,12 +3,8 @@
 // ---------------------------------------------------------------
 
 using System.Threading.Tasks;
-using LHDS.Core.Models.Clients.LandingClient.Exceptions;
 using LHDS.Core.Models.Clients.OptOutClient.Exceptions;
-using LHDS.Core.Models.Orchestrations.Decryptions.Exceptions;
-using LHDS.Core.Models.Orchestrations.Downloads.Exceptions;
 using LHDS.Core.Models.Orchestrations.OptOuts.Exceptions;
-using LHDS.Core.Services.Orchestrations.Downloads;
 using LHDS.Core.Services.Orchestrations.OptOuts;
 using Xeptions;
 
@@ -29,6 +25,37 @@ namespace LHDS.Core.Clients
             try
             {
                 await this.optOutOrchestrationService.RetrieveOptOutStatusAsync(optOutFile, requestId);
+            }
+            catch (OptOutOrchestrationValidationException optOutOrchestrationValidationException)
+            {
+                throw new OptOutClientValidationException(
+                    optOutOrchestrationValidationException.InnerException as Xeption);
+            }
+            catch (OptOutOrchestrationDependencyValidationException
+                optOutOrchestrationDependencyValidationException)
+            {
+                throw new OptOutClientValidationException(
+                    optOutOrchestrationDependencyValidationException.InnerException as Xeption);
+            }
+            catch (OptOutOrchestrationDependencyException
+                optOutOrchestrationDependencyException)
+            {
+                throw new OptOutClientDependencyException(
+                    optOutOrchestrationDependencyException.InnerException as Xeption);
+            }
+            catch (OptOutOrchestrationServiceException
+                optOutOrchestrationServiceException)
+            {
+                throw new OptOutClientServiceException(
+                    optOutOrchestrationServiceException.InnerException as Xeption);
+            }
+        }
+
+        public async ValueTask PushExpiredOptOutsToMeshForRenewalAsync()
+        {
+            try
+            {
+                await this.optOutOrchestrationService.PushExpiredOptOutsToMeshForRenewalAsync();
             }
             catch (OptOutOrchestrationValidationException optOutOrchestrationValidationException)
             {

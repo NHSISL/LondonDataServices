b82111a1a0711fa840a27d6d283ec188bc513a50
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-04-20 15:55:10 +0100

    ShouldThrowDependencyValidationExceptionOnRetrieveAllExpiredOptOutsIfDependencyValidationErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/OptOuts/OptOutProcessingServiceTests.Exceptions.RetrieveAllExpiredOptOuts.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.OptOuts;
+using LHDS.Core.Models.Processings.OptOuts.Exceptions;
+using Moq;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.OptOuts
+{
+    public partial class OptOutProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnRetrieveAllExpiredOptOutsIfDependencyValidationErrorOccursAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            int randomNumber = GetRandomNumber();
+
+            var expectedOptOutProcessingDependencyValidationException =
+                new OptOutProcessingDependencyValidationException(
+                    dependencyValidationException.InnerException as Xeption);
+
+            this.optOutServiceMock.Setup(service =>
+                service.RetrieveAllOptOuts())
+                    .Throws(dependencyValidationException);
+
+            // when
+            ValueTask<List<OptOut>> retrieveAllExpiredOptOutsTask =
+                this.optOutProcessingService.RetrieveAllExpiredOptOutsAsync(randomNumber);
+
+            OptOutProcessingDependencyValidationException actualException =
+                await Assert.ThrowsAsync<OptOutProcessingDependencyValidationException>(retrieveAllExpiredOptOutsTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(
+                expectedOptOutProcessingDependencyValidationException);
+
+            this.optOutServiceMock.Verify(service =>
+                service.RetrieveAllOptOuts(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedOptOutProcessingDependencyValidationException))),
+                         Times.Once);
+
+            this.optOutServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

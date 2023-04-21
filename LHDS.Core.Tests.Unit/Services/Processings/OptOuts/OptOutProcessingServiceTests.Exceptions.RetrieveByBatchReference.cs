31c74cad313f8a7881422e3e4e5d10766a4ccb68
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-04-21 12:50:06 +0100

    ShouldThrowServiceExceptionOnRetrieveByBatchReferenceIfServiceErrorOccursAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Processings/OptOuts/OptOutProcessingServiceTests.Exceptions.RetrieveByBatchReference.cs
@@ -0,0 +1,139 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.OptOuts;
+using LHDS.Core.Models.Processings.OptOuts.Exceptions;
+using Moq;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.OptOuts
+{
+    public partial class OptOutProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnRetrieveByBatchReferenceIfDependencyValidationErrorOccursAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            string batchRefernce = GetRandomString();
+
+            var expectedOptOutProcessingDependencyValidationException =
+                new OptOutProcessingDependencyValidationException(
+                    dependencyValidationException.InnerException as Xeption);
+
+            this.optOutServiceMock.Setup(service =>
+                service.RetrieveAllOptOuts())
+                    .Throws(dependencyValidationException);
+
+            // when
+            ValueTask<OptOut> optOutRetrieveByNhsNumberTask =
+                this.optOutProcessingService.RetrieveOptOutByNhsNumberAsync(batchRefernce);
+
+            OptOutProcessingDependencyValidationException actualException =
+                await Assert.ThrowsAsync<OptOutProcessingDependencyValidationException>(optOutRetrieveByNhsNumberTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(
+                expectedOptOutProcessingDependencyValidationException);
+
+            this.optOutServiceMock.Verify(service =>
+                service.RetrieveAllOptOuts(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedOptOutProcessingDependencyValidationException))),
+                         Times.Once);
+
+            this.optOutServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task ShouldThrowDependencyOnRetrieveByBatchReferenceIfDependencyErrorOccursAndLogItAsync(
+        Xeption dependencyException)
+        {
+            // given
+            string batchRefernce = GetRandomString();
+
+            var expectedOptOutProcessingDependencyException =
+                new OptOutProcessingDependencyException(
+                    dependencyException.InnerException as Xeption);
+
+            this.optOutServiceMock.Setup(service =>
+                service.RetrieveAllOptOuts())
+                    .Throws(dependencyException);
+
+            // when
+            ValueTask<OptOut> optOutRetrieveTask =
+                this.optOutProcessingService.RetrieveOptOutByNhsNumberAsync(batchRefernce);
+
+            OptOutProcessingDependencyException actualException =
+                await Assert.ThrowsAsync<OptOutProcessingDependencyException>(optOutRetrieveTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedOptOutProcessingDependencyException);
+
+            this.optOutServiceMock.Verify(service =>
+                service.RetrieveAllOptOuts(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedOptOutProcessingDependencyException))),
+                         Times.Once);
+
+            this.optOutServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveByBatchReferenceIfServiceErrorOccursAsync()
+        {
+            // given
+            string batchRefernce = GetRandomString();
+
+            var serviceException = new Exception();
+
+            var failedOptOutProcessingServiceException =
+                new FailedOptOutProcessingServiceException(serviceException);
+
+            var expectedOptOutProcessingServiveException =
+                new OptOutProcessingServiceException(
+                    failedOptOutProcessingServiceException);
+
+            this.optOutServiceMock.Setup(service =>
+                service.RetrieveAllOptOuts())
+                    .Throws(serviceException);
+
+            // when
+            ValueTask<OptOut> optOutRetrieveTask =
+                this.optOutProcessingService.RetrieveOptOutByNhsNumberAsync(batchRefernce);
+
+            OptOutProcessingServiceException actualException =
+                await Assert.ThrowsAsync<OptOutProcessingServiceException>(optOutRetrieveTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedOptOutProcessingServiveException);
+
+            this.optOutServiceMock.Verify(service =>
+                service.RetrieveAllOptOuts(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedOptOutProcessingServiveException))),
+                         Times.Once);
+
+            this.optOutServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

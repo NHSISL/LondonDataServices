c74434ad8b2cf4c4e1dcec31d93f7344cd719236
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-04-21 10:12:18 +0100

    ShouldRetrieveAllOptOutsByBatchReferenceAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/OptOuts/OptOutProcessingServiceTests.Logic.RetrieveAllByBatchReference.cs
@@ -0,0 +1,52 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LHDS.Core.Models.Foundations.OptOuts;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.OptOuts
+{
+    public partial class OptOutProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveAllOptOutsByBatchReferenceAsync()
+        {
+            // given
+            string randomString = GetRandomString();
+            string inputBatchReference = randomString;
+            IQueryable<OptOut> randomOptOuts = CreateRandomOptOuts(inputBatchReference);
+            IQueryable<OptOut> retrievedOptOuts = randomOptOuts.DeepClone();
+
+            List<OptOut> expectedOptOuts = retrievedOptOuts
+                .Where(optOut => optOut.BatchReference == inputBatchReference)
+                    .ToList();
+
+            this.optOutServiceMock.Setup(service =>
+                service.RetrieveAllOptOuts())
+                    .Returns(retrievedOptOuts);
+
+            // when
+            List<OptOut> actualOptOuts =
+                await this.optOutProcessingService.RetrieveAllOptOutsByBatchReferenceAsync(inputBatchReference);
+
+            // then
+            actualOptOuts.Count().Should().Be(3);
+            actualOptOuts.Should().BeEquivalentTo(expectedOptOuts);
+
+            this.optOutServiceMock.Verify(service =>
+                service.RetrieveAllOptOuts(),
+                    Times.Once);
+
+            this.optOutServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

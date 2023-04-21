0f97f4fea7a227e2a89cc3702a22545d159c6a95
David Hayes <david.hayes17@nhs.net>
2023-04-21 09:25:33 +0100

    CLIENT:  PushExpiredOptOutsToMeshForRenewalAsync implement

--- LHDS.Core/Clients/IOptOutClient.cs
+++ LHDS.Core/Clients/IOptOutClient.cs
@@ -10,5 +10,6 @@ namespace LHDS.Core.Clients
     {
         ValueTask RetrieveOptOutStatusAsync(byte[] optOutFile, string requestId);
         ValueTask PushExpiredOptOutsToMeshForRenewalAsync();
+        ValueTask RetrieveUpdatedMeshOptOutStatusChangesAsync();
     }
 }

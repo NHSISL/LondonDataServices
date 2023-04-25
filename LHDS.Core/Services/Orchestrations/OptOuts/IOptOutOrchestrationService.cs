4a9549fbb3003ad0d6c9eadc26de64b2b07d35a6
David Hayes <david.hayes17@nhs.net>
2023-04-25 12:30:01 +0100

    ShouldPushExpiredOptOutsToMeshForRenewalStatusAsync -> PASS

--- LHDS.Core/Services/Orchestrations/OptOuts/IOptOutOrchestrationService.cs
+++ LHDS.Core/Services/Orchestrations/OptOuts/IOptOutOrchestrationService.cs
@@ -3,13 +3,14 @@
 // ---------------------------------------------------------------
 
 using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Mesh;
 
 namespace LHDS.Core.Services.Orchestrations.OptOuts
 {
     public interface IOptOutOrchestrationService
     {
         ValueTask RetrieveOptOutStatusAsync(byte[] optOutFile, string fileName);
-        ValueTask PushExpiredOptOutsToMeshForRenewalAsync();
+        ValueTask<MeshMessage> PushExpiredOptOutsToMeshForRenewalAsync();
         ValueTask RetrieveUpdatedMeshConsentStatusesChangesAsync();
     }
 }

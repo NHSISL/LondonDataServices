b82da6fb2a0ec5c93f050349833fc43d3c5fd5aa
David Hayes <david.hayes17@nhs.net>
2023-04-26 09:28:37 +0100

    ShouldRetreiveOptOutStatusAsync -> PASS,
    Clean up and delete

--- LHDS.Core.Tests.Manual.OptOut/OptOutTests.RetrieveOptOutStatus.cs
+++ LHDS.Core.Tests.Manual.OptOut/OptOutTests.RetrieveOptOutStatus.cs
@@ -21,10 +21,16 @@ namespace LHDS.Core.Tests.Manual.OptOut
                 byte[] fileBytes = File.ReadAllBytes(@"Resources\testfile.csv");
                 FileInfo fi = new FileInfo(@"Resources\testfile.csv");
                 var fileName = fi.Name.Substring(0, fi.Name.Length - fi.Extension.Length);
-                string DocumentFileName = await optOutClient.RetrieveOptOutStatusAsync(optOutFile: fileBytes, fileName: fileName);
+
+                string DocumentFileName = await optOutClient
+                    .RetrieveOptOutStatusAsync(optOutFile: fileBytes, fileName: fileName);
                 DocumentFileName.Should().NotBeNullOrWhiteSpace();
-                Document document = await documentService.RetrieveDocumentByFileNameAsync(DocumentFileName);
-                document.DocumentData.Should().NotBeNull();
+
+                Document retreiveDocument =
+                    await documentService.RetrieveDocumentByFileNameAsync(DocumentFileName);
+                retreiveDocument.DocumentData.Should().NotBeNull();
+
+                await documentService.RemoveDocumentByFileNameAsync(DocumentFileName);
             }
             catch (Exception ex)
             {

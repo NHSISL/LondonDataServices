c7dddf82ceaa08468b5ed129b580aa81fd146c74
Hassan Raza <49044825+Hassaero110@users.noreply.github.com>
2023-04-28 15:57:25 +0100

    EXPOSERS: Azure Functions Opt Out

--- LHDS.Core.Tests.Integration.OptOuts/OptOutTests.PushExpiredOptOutsToMeshForRenewalAsync.cs
+++ LHDS.Core.Tests.Integration.OptOuts/OptOutTests.PushExpiredOptOutsToMeshForRenewalAsync.cs
@@ -1,31 +1,31 @@
-﻿// ---------------------------------------------------------------
-// Copyright (c) North East London ICB. All rights reserved.
-// ---------------------------------------------------------------
+﻿//// ---------------------------------------------------------------
+//// Copyright (c) North East London ICB. All rights reserved.
+//// ---------------------------------------------------------------
 
-using System;
-using System.Threading.Tasks;
-using Xunit;
+//using System;
+//using System.Threading.Tasks;
+//using Xunit;
 
-namespace LHDS.Core.Tests.Manual.OptOut
-{
-    public partial class OptOutTests
-    {
-        [Fact]
-        public async Task ShouldPushExpiredOptOutsToMeshForRenewalAsyncsAsync()
-        {
-            try
-            {
-                //MeshMessage message = await optOutClient.PushExpiredOptOutsToMeshForRenewalAsync();
-                //message.Should().NotBeNull();
-                //message.MessageId.Should().NotBeNullOrWhiteSpace();
-                //var messageId = message.MessageId;
-                //bool messageAcked = await meshService.AcknowledgeMessageByIdAsync(messageId);
-                //messageAcked.Should().BeTrue();
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail($"{ex.Message} {ex?.InnerException?.Message} {ex.StackTrace}");
-            }
-        }
-    }
-}
+//namespace LHDS.Core.Tests.Manual.OptOut
+//{
+//    public partial class OptOutTests
+//    {
+//        [Fact]
+//        public async Task ShouldPushExpiredOptOutsToMeshForRenewalAsyncsAsync()
+//        {
+//            try
+//            {
+//                //MeshMessage message = await optOutClient.PushExpiredOptOutsToMeshForRenewalAsync();
+//                //message.Should().NotBeNull();
+//                //message.MessageId.Should().NotBeNullOrWhiteSpace();
+//                //var messageId = message.MessageId;
+//                //bool messageAcked = await meshService.AcknowledgeMessageByIdAsync(messageId);
+//                //messageAcked.Should().BeTrue();
+//            }
+//            catch (Exception ex)
+//            {
+//                Assert.Fail($"{ex.Message} {ex?.InnerException?.Message} {ex.StackTrace}");
+//            }
+//        }
+//    }
+//}
--- LHDS.Core.Tests.Integration.OptOuts/OptOutTests.RetrieveOptOutStatus.cs
+++ LHDS.Core.Tests.Integration.OptOuts/OptOutTests.RetrieveOptOutStatus.cs
@@ -1,45 +1,45 @@
-﻿// ---------------------------------------------------------------
-// Copyright (c) North East London ICB. All rights reserved.
-// ---------------------------------------------------------------
+﻿//// ---------------------------------------------------------------
+//// Copyright (c) North East London ICB. All rights reserved.
+//// ---------------------------------------------------------------
 
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using FluentAssertions;
-using LHDS.Core.Models.Foundations.Documents;
-using Xunit;
+//using System;
+//using System.IO;
+//using System.Threading.Tasks;
+//using FluentAssertions;
+//using LHDS.Core.Models.Foundations.Documents;
+//using Xunit;
 
-namespace LHDS.Core.Tests.Manual.OptOut
-{
-    public partial class OptOutTests
-    {
-        [Fact]
-        public async Task ShouldRetreiveOptOutStatusAsync()
-        {
-            try
-            {
-                byte[] fileBytes = File.ReadAllBytes(@"Resources\testfile.csv");
-                FileInfo fi = new FileInfo(@"Resources\testfile.csv");
-                var fileName = fi.Name.Substring(0, fi.Name.Length - fi.Extension.Length);
+//namespace LHDS.Core.Tests.Manual.OptOut
+//{
+//    public partial class OptOutTests
+//    {
+//        [Fact]
+//        public async Task ShouldRetreiveOptOutStatusAsync()
+//        {
+//            try
+//            {
+//                byte[] fileBytes = File.ReadAllBytes(@"Resources\testfile.csv");
+//                FileInfo fi = new FileInfo(@"Resources\testfile.csv");
+//                var fileName = fi.Name.Substring(0, fi.Name.Length - fi.Extension.Length);
 
-                string DocumentFileName = await optOutClient
-                    .RetrieveOptOutStatusAsync(optOutFile: fileBytes, fileName: fileName);
+//                string DocumentFileName = await optOutClient
+//                    .RetrieveOptOutStatusAsync(optOutFile: fileBytes, fileName: fileName);
 
-                DocumentFileName.Should().NotBeNullOrWhiteSpace();
+//                DocumentFileName.Should().NotBeNullOrWhiteSpace();
 
-                Document retreiveDocument =
-                    await documentService
-                    .RetrieveDocumentByFileNameAsync(DocumentFileName);
+//                Document retreiveDocument =
+//                    await documentService
+//                    .RetrieveDocumentByFileNameAsync(DocumentFileName);
 
-                retreiveDocument.DocumentData.Should().NotBeNull();
+//                retreiveDocument.DocumentData.Should().NotBeNull();
 
-                await documentService
-                    .RemoveDocumentByFileNameAsync(DocumentFileName);
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail($"{ex.Message} {ex?.InnerException?.Message} {ex.StackTrace}");
-            }
-        }
-    }
-}
+//                await documentService
+//                    .RemoveDocumentByFileNameAsync(DocumentFileName);
+//            }
+//            catch (Exception ex)
+//            {
+//                Assert.Fail($"{ex.Message} {ex?.InnerException?.Message} {ex.StackTrace}");
+//            }
+//        }
+//    }
+//}

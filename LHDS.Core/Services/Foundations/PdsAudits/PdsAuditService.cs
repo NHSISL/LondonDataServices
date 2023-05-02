9b286f70afb1518561e9218e75ed1dd42009a9ad
Christo du Toit <christo@dutoit.co.uk>
2023-05-02 15:45:13 +0100

    ShouldReturnPdsAudit -> FAIL

--- LHDS.Core/Services/Foundations/PdsAudits/PdsAuditService.cs
+++ LHDS.Core/Services/Foundations/PdsAudits/PdsAuditService.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.DateTimes;
 using LHDS.Core.Brokers.Loggings;
@@ -29,5 +30,8 @@ namespace LHDS.Core.Services.Foundations.PdsAudits
 
                 return await this.storageBroker.InsertPdsAuditAsync(pdsAudit);
             });
+
+        public IQueryable<PdsAudit> RetrieveAllPdsAudits() =>
+            throw new System.NotImplementedException();
     }
 }

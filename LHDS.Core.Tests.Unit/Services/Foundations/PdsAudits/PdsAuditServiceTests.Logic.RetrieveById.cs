95c07fc77ffbf6a2e5be96c4ee50ac4f07f6352d
Christo du Toit <christo@dutoit.co.uk>
2023-05-02 15:45:25 +0100

    ShouldRetrievePdsAuditByIdAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/PdsAudits/PdsAuditServiceTests.Logic.RetrieveById.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using LHDS.Core.Models.PdsAudits;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.PdsAudits
+{
+    public partial class PdsAuditServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrievePdsAuditByIdAsync()
+        {
+            // given
+            PdsAudit randomPdsAudit = CreateRandomPdsAudit();
+            PdsAudit inputPdsAudit = randomPdsAudit;
+            PdsAudit storagePdsAudit = randomPdsAudit;
+            PdsAudit expectedPdsAudit = storagePdsAudit.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectPdsAuditByIdAsync(inputPdsAudit.Id))
+                    .ReturnsAsync(storagePdsAudit);
+
+            // when
+            PdsAudit actualPdsAudit =
+                await this.pdsAuditService.RetrievePdsAuditByIdAsync(inputPdsAudit.Id);
+
+            // then
+            actualPdsAudit.Should().BeEquivalentTo(expectedPdsAudit);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectPdsAuditByIdAsync(inputPdsAudit.Id),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

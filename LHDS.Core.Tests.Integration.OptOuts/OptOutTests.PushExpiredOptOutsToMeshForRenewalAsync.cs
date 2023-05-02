88b0fe3a9a51431341a16b3597d2b81393971033
Christo du Toit <christo@dutoit.co.uk>
2023-05-02 15:13:15 +0100

    DATA: PDS Model

--- LHDS.Core.Tests.Integration.OptOuts/OptOutTests.PushExpiredOptOutsToMeshForRenewalAsync.cs
+++ LHDS.Core.Tests.Integration.OptOuts/OptOutTests.PushExpiredOptOutsToMeshForRenewalAsync.cs
@@ -2,7 +2,6 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
-using System;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -10,22 +9,23 @@ namespace LHDS.Core.Tests.Manual.OptOut
 {
     public partial class OptOutTests
     {
-        [Fact]
+        [Fact(Skip = "Excluded from pipeline")]
         public async Task ShouldPushExpiredOptOutsToMeshForRenewalAsyncsAsync()
         {
-            try
-            {
-                //MeshMessage message = await optOutClient.PushExpiredOptOutsToMeshForRenewalAsync();
-                //message.Should().NotBeNull();
-                //message.MessageId.Should().NotBeNullOrWhiteSpace();
-                //var messageId = message.MessageId;
-                //bool messageAcked = await meshService.AcknowledgeMessageByIdAsync(messageId);
-                //messageAcked.Should().BeTrue();
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail($"{ex.Message} {ex?.InnerException?.Message} {ex.StackTrace}");
-            }
+            //TODO:  fix from DH to replace this tests
+            //try
+            //{
+            //    MeshMessage message = await optOutClient.PushExpiredOptOutsToMeshForRenewalAsync();
+            //    message.Should().NotBeNull();
+            //    message.MessageId.Should().NotBeNullOrWhiteSpace();
+            //    var messageId = message.MessageId;
+            //    bool messageAcked = await meshService.AcknowledgeMessageByIdAsync(messageId);
+            //    messageAcked.Should().BeTrue();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Assert.Fail($"{ex.Message} {ex?.InnerException?.Message} {ex.StackTrace}");
+            //}
         }
     }
 }

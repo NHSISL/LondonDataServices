9c182971b0b4715a8a32876edd321ecd0e6101a2
David Hayes <david.hayes17@nhs.net>
2023-05-04 08:55:07 +0100

    CODE RUB: Code Review Changes

--- LHDS.Functions.OptOut/PushExpiredOptOutsToMeshForRenewalFunction.cs
+++ LHDS.Functions.OptOut/PushExpiredOptOutsToMeshForRenewalFunction.cs
@@ -3,7 +3,6 @@
 // ---------------------------------------------------------------
 
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.Loggings;
 using LHDS.Core.Clients;
@@ -50,19 +49,17 @@ namespace LHDS.Functions.OptOut
             this.loggingBroker.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
             this.logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
         }
+
         public class MyInfo
         {
             public MyScheduleStatus ScheduleStatus { get; set; }
-
             public bool IsPastDue { get; set; }
         }
 
         public class MyScheduleStatus
         {
             public DateTime Last { get; set; }
-
             public DateTime Next { get; set; }
-
             public DateTime LastUpdated { get; set; }
         }
     }

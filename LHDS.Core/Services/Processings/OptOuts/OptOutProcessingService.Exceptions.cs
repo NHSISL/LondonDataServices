3ee8bbab32f158e7ab3ad712d8733a3c9b2bb03a
Hassan Raza <h_raza110@outlook.com>
2023-05-31 12:43:52 +0100

    ShouldThrowDependencyValidationExceptionOnConsolidateChangesOptOutsIfDependencyValidationErrorOccursAndLogItAsync -> PASS

--- LHDS.Core/Services/Processings/OptOuts/OptOutProcessingService.Exceptions.cs
+++ LHDS.Core/Services/Processings/OptOuts/OptOutProcessingService.Exceptions.cs
@@ -78,10 +78,6 @@ namespace LHDS.Core.Services.Processings.OptOuts
             {
                 throw CreateAndLogValidationException(nullOptOutConsentedItemsListProcessingException);
             }
-            catch (InvalidOptOutItemInConsentedListProcessingException invalidOptOutItemInConsentedListProcessingException)
-            {
-                throw CreateAndLogValidationException(invalidOptOutItemInConsentedListProcessingException);
-            }
             catch (OptOutDependencyValidationException optOutDependencyValidationException)
             {
                 throw CreateAndLogDependencyValidationException(optOutDependencyValidationException);

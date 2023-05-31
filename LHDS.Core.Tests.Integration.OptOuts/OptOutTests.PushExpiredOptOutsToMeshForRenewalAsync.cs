66f0e3257744cd69fc6ccfd5503e60e6ad9194fc
Hassan Raza <h_raza110@outlook.com>
2023-05-31 13:35:29 +0100

    CODE RUB: Skipping Integration Tests

--- LHDS.Core.Tests.Integration.OptOuts/OptOutTests.PushExpiredOptOutsToMeshForRenewalAsync.cs
+++ LHDS.Core.Tests.Integration.OptOuts/OptOutTests.PushExpiredOptOutsToMeshForRenewalAsync.cs
@@ -12,7 +12,7 @@ namespace LHDS.Core.Tests.Integration.OptOuts
 {
     public partial class OptOutTests
     {
-        [Fact]
+        [Fact(Skip = "Reason for skipping the test")]
         public async Task ShouldPushExpiredOptOutsToMeshForRenewalAsyncsAsync()
         {
             try

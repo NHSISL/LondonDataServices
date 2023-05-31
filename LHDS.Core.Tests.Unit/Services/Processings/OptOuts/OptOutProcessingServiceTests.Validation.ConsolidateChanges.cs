a240ace805d7456082ba80499204928837312cb2
Hassan Raza <h_raza110@outlook.com>
2023-05-31 11:01:51 +0100

    ShouldThrowValidationExceptionsOnConsolidateChangesIfOptOutProcessingListIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/OptOuts/OptOutProcessingServiceTests.Validation.ConsolidateChanges.cs
@@ -0,0 +1,51 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.OptOuts;
+using LHDS.Core.Models.Processings.OptOuts.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.OptOuts
+{
+    public partial class OptOutProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionsOnConsolidateChangesIfOptOutProcessingListIsNullAndLogItAsync()
+        {
+            // given
+            int randomNumber = GetRandomNumber();
+            List<OptOut> nullOptOutList = null;
+            List<string> randomStringList = RandomStringList(randomNumber);
+
+            var nullOptOutListProcessingException =
+                new NullOptOutListProcessingException();
+
+            var expectedOptOutProcessingValidationException =
+                new OptOutProcessingValidationException(innerException: nullOptOutListProcessingException);
+
+            // when
+            ValueTask<List<OptOut>> ConsolidateChangesOptOutTask =
+                this.optOutProcessingService.ConsolidateOptOutChangesAndReturnChangesOnly(nullOptOutList, randomStringList);
+
+            OptOutProcessingValidationException actualOptOutProcessingValidationException =
+                await Assert.ThrowsAsync<OptOutProcessingValidationException>(ConsolidateChangesOptOutTask.AsTask);
+
+            //then
+            actualOptOutProcessingValidationException.Should()
+                .BeEquivalentTo(expectedOptOutProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedOptOutProcessingValidationException))),
+                        Times.Once);
+
+            this.optOutServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

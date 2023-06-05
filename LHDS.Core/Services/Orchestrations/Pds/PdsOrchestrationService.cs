6f894b4eb092e914cf09a0251862c3a6249360c7
David Hayes <davidhayes03@googlemail.com>
2023-06-05 11:52:34 +0100

    ORCHESTRAIONS: Implement service interface

---
+++ LHDS.Core/Services/Orchestrations/Pds/PdsOrchestrationService.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.PdsAudits;
+
+namespace LHDS.Core.Services.Orchestrations.Pds
+{
+    public partial class PdsOrchestrationService : IPdsOrchestrationService
+    {
+        public ValueTask<PdsAudit> PickupFileAndSendToMesh(byte[] pdsFile) =>
+            throw new NotImplementedException();
+
+        public ValueTask<List<PdsAudit>> RetreiveMessagesFromMeshAndUpdateStorage() =>
+            throw new NotImplementedException();
+    }

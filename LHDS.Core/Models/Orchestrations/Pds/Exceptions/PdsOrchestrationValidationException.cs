8758247f2bce5c48e2670c81e788ba71f4ed30b6
Christo du Toit <christo@dutoit.co.uk>
2023-06-05 13:28:42 +0100

    EXPOSERS: PDS Client

---
+++ LHDS.Core/Models/Orchestrations/Pds/Exceptions/PdsOrchestrationValidationException.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Orchestrations.Pds.Exceptions
+{
+    public class PdsOrchestrationValidationException : Xeption
+    {
+        public PdsOrchestrationValidationException(Xeption innerException)
+            : base(
+                message: "PDS orchestration validation errors occurred, please try again.",
+                innerException)
+        { }
+    }
+}

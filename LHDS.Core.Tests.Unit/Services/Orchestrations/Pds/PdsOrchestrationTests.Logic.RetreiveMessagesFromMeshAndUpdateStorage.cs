172ebafa1f1958d0123c12d3f27fb8174008a1b6
Hassan Raza <h_raza110@outlook.com>
2023-06-06 09:25:08 +0100

    ShouldRetreiveMessagesFromMeshAsync -> WIP

---
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/Pds/PdsOrchestrationTests.Logic.RetreiveMessagesFromMeshAndUpdateStorage.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Orchestrations.Pds
+{
+    public partial class PdsOrchestrationTests
+    {
+        [Fact]
+        public async Task ShouldRetreiveMessagesFromMeshAsync()
+        {
+            var randomString = GetRandomString();
+            var inputString = randomString;
+            var inputBytes = Encoding.ASCII.GetBytes(inputString);
+        }
+    }
+}

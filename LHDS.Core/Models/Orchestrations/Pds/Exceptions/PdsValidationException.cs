ba76759e80b9d64ec245dedbb99bb41f63ccf954
David Hayes <davidhayes03@googlemail.com>
2023-06-06 12:46:01 +0100

    ShouldThrowValidationExceptionOnPickupFileAndSendIfArgsAreInvalidAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Orchestrations/Pds/Exceptions/PdsValidationException.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Orchestrations.Pds.Exceptions
+{
+    public class PdsValidationException : Xeption
+    {
+        public PdsValidationException(Xeption innerException)
+            : base(
+                message: "Pds validation errors occurred, please try again.",
+                innerException)
+        { }
+    }
+}

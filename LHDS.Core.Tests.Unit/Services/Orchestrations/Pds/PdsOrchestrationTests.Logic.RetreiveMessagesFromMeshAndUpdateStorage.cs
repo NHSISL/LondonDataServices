ce77cc8d3419e7532110516fd5128bf98e780802
Hassan Raza <h_raza110@outlook.com>
2023-06-06 16:02:35 +0100

    ShouldRetreiveMessagesForNonMatchingPdsWorkflowIdFromMeshAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Orchestrations/Pds/PdsOrchestrationTests.Logic.RetreiveMessagesFromMeshAndUpdateStorage.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/Pds/PdsOrchestrationTests.Logic.RetreiveMessagesFromMeshAndUpdateStorage.cs
@@ -142,8 +142,150 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.Pds
                 pdsAuditsList.Add(pdsAudit);
             };
 
-            this.pdsAuditServiceMock.VerifyNoOtherCalls();
             this.meshServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+            this.documentServiceMock.VerifyNoOtherCalls();
+            this.pdsAuditServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldRetreiveMessagesForNonMatchingPdsWorkflowIdFromMeshAsync()
+        {
+            // given
+            DateTimeOffset randomDate = GetRandomDateTimeOffset();
+            int randomNumber = GetRandomNumber();
+            List<string> randomMessageIds = GetRandomStrings(randomNumber);
+            string mexWorkflowId = GetRandomString();
+            List<MeshMessage> retrievedMessages = GetRandomMessages(randomMessageIds, mexWorkflowId);
+            Guid identifier = Guid.NewGuid();
+
+            this.dateTimeBrokerMock.Setup(service =>
+                service.GetCurrentDateTimeOffset())
+                    .Returns(randomDate);
+
+            this.identifierBrokerMock.Setup(service =>
+                service.GetIdentifier())
+                    .Returns(identifier);
+
+            this.meshServiceMock.Setup(service =>
+                service.RetrieveMessageIdsFromInboxAsync())
+                    .ReturnsAsync(randomMessageIds);
+
+            List<PdsAudit> pdsAuditsList = new List<PdsAudit>();
+
+            foreach (var message in retrievedMessages)
+            { 
+                this.meshServiceMock.Setup(service =>
+                    service.RetrieveMessageByIdAsync(message.MessageId))
+                        .ReturnsAsync(message);
+
+                if (message.Headers["Mex-WorkflowId"].FirstOrDefault() != this.pdsConfiguration.WorkflowId)
+                {
+                    continue;
+                }
+
+                Document document = new Document
+                {
+                    FileName = message.Headers["Mex-FileName"].FirstOrDefault().ToString(),
+                    DocumentData = message.FileContent,
+                };
+
+                this.documentServiceMock.Setup(service =>
+                    service.AddDocumentAsync(document));
+
+                Guid correlationId = Guid.Parse(message.Headers["Mex-LocalID"].FirstOrDefault());
+                string fileName = message.Headers["Mex-FileName"].FirstOrDefault();
+
+                PdsAudit pdsAudit = new PdsAudit
+                {
+                    Id = identifier,
+                    CorrelationId = correlationId,
+                    FileName = fileName,
+                    Message = $"Received message from mesh with id {message.MessageId}",
+                    CreatedDate = randomDate,
+                    UpdatedDate = randomDate,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                };
+
+                this.pdsAuditServiceMock.Setup(service =>
+                    service.AddPdsAuditAsync(pdsAudit))
+                        .ReturnsAsync(pdsAudit);
+
+                pdsAuditsList.Add(pdsAudit);
+            };
+
+            List<PdsAudit> expectedPdsAudits = pdsAuditsList.DeepClone();
+
+            //when
+            List<PdsAudit> actualPdsAudits =
+                await this.pdsOrchestrationService.RetreiveMessagesFromMeshAndUpdateStorage();
+
+            //then
+            actualPdsAudits.Should().BeEquivalentTo(expectedPdsAudits);
+
+            this.dateTimeBrokerMock.Verify(service =>
+                service.GetCurrentDateTimeOffset(),
+                    Times.Exactly(retrievedMessages.Count));
+
+            this.identifierBrokerMock.Verify(service =>
+                service.GetIdentifier(),
+                    Times.Exactly(retrievedMessages.Count));
+
+            this.meshServiceMock.Verify(service =>
+              service.RetrieveMessageIdsFromInboxAsync(),
+                        Times.Once);
+
+            foreach (var message in retrievedMessages)
+            {
+
+                this.meshServiceMock.Verify(service =>
+                    service.RetrieveMessageByIdAsync(message.MessageId),
+                        Times.Once);
+
+                if (message.Headers["Mex-WorkflowId"].FirstOrDefault() != this.pdsConfiguration.WorkflowId)
+                {
+                    continue;
+                }
+
+                Document document = new Document
+                {
+                    FileName = message.Headers["Mex-FileName"].FirstOrDefault().ToString(),
+                    DocumentData = message.FileContent,
+                };
+
+                this.documentServiceMock.Verify(service =>
+                    service.AddDocumentAsync(It.Is(SameDocumentAs(document))),
+                        Times.Once);
+
+                Guid correlationId = Guid.Parse(message.Headers["Mex-LocalID"].FirstOrDefault());
+                string fileName = message.Headers["Mex-FileName"].FirstOrDefault();
+
+                PdsAudit pdsAudit = new PdsAudit
+                {
+                    Id = identifier,
+                    CorrelationId = correlationId,
+                    FileName = fileName,
+                    Message = $"Received message from mesh with id {message.MessageId}",
+                    CreatedDate = randomDate,
+                    UpdatedDate = randomDate,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                };
+
+                this.pdsAuditServiceMock.Verify(service =>
+                    service.AddPdsAuditAsync(It.Is(SamePdsAuditAs(pdsAudit))),
+                        Times.Once);
+
+                pdsAuditsList.Add(pdsAudit);
+            };
+
+            this.meshServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+            this.documentServiceMock.VerifyNoOtherCalls();
+            this.pdsAuditServiceMock.VerifyNoOtherCalls();
         }
     }
 }

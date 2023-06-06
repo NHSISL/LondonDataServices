05d4806b34b39d9f51ab078a9094999fbf847dd2
Hassan Raza <h_raza110@outlook.com>
2023-06-06 15:54:10 +0100

    ShouldRetreiveMessagesForMatchingPdsWorkflowIdFromMeshAsync -> PASS

--- LHDS.Core/Services/Orchestrations/Pds/PdsOrchestrationService.cs
+++ LHDS.Core/Services/Orchestrations/Pds/PdsOrchestrationService.cs
@@ -4,10 +4,12 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.DateTimes;
 using LHDS.Core.Brokers.Identifiers;
 using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Models.Foundations.Documents;
 using LHDS.Core.Models.Foundations.PdsAudits;
 using LHDS.Core.Models.Orchestrations.Pds;
 using LHDS.Core.Services.Foundations.Documents;
@@ -48,7 +50,44 @@ namespace LHDS.Core.Services.Orchestrations.Pds
         public ValueTask<PdsAudit> PickupFileAndSendToMesh(byte[] pdsFile, string fileName) =>
             throw new NotImplementedException();
 
-        public ValueTask<List<PdsAudit>> RetreiveMessagesFromMeshAndUpdateStorage() =>
-            throw new NotImplementedException();
+        public async ValueTask<List<PdsAudit>> RetreiveMessagesFromMeshAndUpdateStorage()
+        {
+            var messageIds = await this.meshService.RetrieveMessageIdsFromInboxAsync();
+            var pdsAudits = new List<PdsAudit>();
+
+            foreach (var id in messageIds)
+            {
+                var message = await this.meshService.RetrieveMessageByIdAsync(id);
+
+                var document = new Document
+                {
+                    FileName = message.Headers["Mex-FileName"].FirstOrDefault(),
+                    DocumentData = message.FileContent,
+                };
+
+                await this.documentService.AddDocumentAsync(document);
+
+                var correlationId = Guid.Parse(message.Headers["Mex-LocalID"].FirstOrDefault());
+                var fileName = message.Headers["Mex-FileName"].FirstOrDefault();
+                DateTimeOffset currentDate = this.dateTimeBroker.GetCurrentDateTimeOffset();
+
+                var pdsAudit = new PdsAudit
+                {
+                    Id = this.identifierBroker.GetIdentifier(),
+                    CorrelationId = correlationId,
+                    FileName = fileName,
+                    Message = $"Received message from mesh with id {message.MessageId}",
+                    CreatedDate = currentDate,
+                    UpdatedDate = currentDate,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                };
+
+                await this.pdsAuditService.AddPdsAuditAsync(pdsAudit);
+                pdsAudits.Add(pdsAudit);
+            }
+
+            return pdsAudits;
+        }
     }
 }

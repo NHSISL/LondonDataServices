98f18ea4307955bc136b792054b7db9dddf81b35
Hassan Raza <h_raza110@outlook.com>
2023-06-07 12:58:01 +0100

    ShouldMapCsvWithTrailingCommaToObjectAsync -> WIP

---
+++ LHDS.Core.Tests.Acceptance/Clients/Pds/PdsTests.Logic.RetrieveMessageFromMeshAndUpdateStorage.cs
@@ -0,0 +1,29 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.PdsAudits;
+using LHDS.Core.Tests.Acceptance.Brokers.CsvMappers.Models;
+using Xunit;
+
+namespace LHDS.Core.Tests.Acceptance.Clients.Pds
+{
+    public partial class PdsTests
+    {
+        [Fact]
+        public async Task ShouldMapCsvWithTrailingCommaToObjectAsync()
+        {
+            // given
+            List<PdsAudit> expectedList = new List<PdsAudit>();
+
+            // when
+            List<PdsAudit> actualList = await this.pdsClient.;
+
+            // then
+            actualList.Should().BeEquivalentTo(expectedList);
+        }
+    }
+}

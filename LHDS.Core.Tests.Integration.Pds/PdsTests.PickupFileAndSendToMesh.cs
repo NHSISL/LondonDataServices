5e8aa183c270c4d05c737c46697f33150fe0ed5d
David Hayes <davidhayes03@googlemail.com>
2023-06-07 14:38:50 +0100

    WIP: Wire up main Test File

---
+++ LHDS.Core.Tests.Integration.Pds/PdsTests.PickupFileAndSendToMesh.cs
@@ -0,0 +1,10 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace LHDS.Core.Tests.Integration.Pds
+{
+    public partial class PdsTests
+    {
+    }
+}

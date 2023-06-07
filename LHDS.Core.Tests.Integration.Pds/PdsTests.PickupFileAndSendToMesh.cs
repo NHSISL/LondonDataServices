3d23d2dd7ed46e07fead77285d17ebe4e00e4bd8
David Hayes <davidhayes03@googlemail.com>
2023-06-07 14:59:51 +0100

    WIP: Intergration setup WIP: Add Data engineering csv

--- LHDS.Core.Tests.Integration.Pds/PdsTests.PickupFileAndSendToMesh.cs
+++ LHDS.Core.Tests.Integration.Pds/PdsTests.PickupFileAndSendToMesh.cs
@@ -2,9 +2,34 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.IO;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.PdsAudits;
+using Xunit;
+
 namespace LHDS.Core.Tests.Integration.Pds
 {
     public partial class PdsTests
     {
+        [Fact]
+        public async Task ShouldPickupFileAndSendToMeshAsync()
+        {
+            // Given
+            byte[] fileBytes =
+                 File.ReadAllBytes(@"Resources\EmisPDSPatientExtract_247BB600-213A-494E-8E90-A4F9190F07DF_20230601T130544.csv");
+
+            FileInfo fi =
+                new FileInfo(@"Resources\EmisPDSPatientExtract_247BB600-213A-494E-8E90-A4F9190F07DF_20230601T130544.csv");
+
+            var fileName = fi.Name.Substring(0, fi.Name.Length - fi.Extension.Length);
+
+            // When
+            PdsAudit pdsAudit = await pdsClient.PickupFileAndSendToMesh(pdsFile: fileBytes, fileName: fileName);
+
+            // Then
+            pdsAudit.Should().NotBeNull();
+
+        }
     }
 }

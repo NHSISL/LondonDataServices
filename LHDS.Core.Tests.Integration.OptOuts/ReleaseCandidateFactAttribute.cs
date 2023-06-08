901b2a39a1b200df08ec9ace39c41e05742aa91c
Christo du Toit <christo@dutoit.co.uk>
2023-06-08 10:17:27 +0100

    INTEGRATIONS:  Addition of Dynamic Test Filters

---
+++ LHDS.Core.Tests.Integration.OptOuts/ReleaseCandidateFactAttribute.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using Xunit;
+using Xunit.Sdk;
+
+namespace LHDS.Core.Tests.Integration.OptOuts
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    [XunitTestCaseDiscoverer(
+        typeName: "LHDS.Core.Tests.Integration.OptOuts.ReleaseCandidateTestCaseDiscoverer",
+        assemblyName: "LHDS.Core.Tests.Integration.OptOuts")]
+    public class ReleaseCandidateFactAttribute : FactAttribute { }
+}

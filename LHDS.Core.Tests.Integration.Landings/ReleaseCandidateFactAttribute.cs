4d3779ac70f3a92848e8acc3965f64305263aa9e
Christo du Toit <christo@dutoit.co.uk>
2023-06-08 10:17:27 +0100

    INTEGRATIONS:  Addition of Dynamic Test Filters

---
+++ LHDS.Core.Tests.Integration.Landings/ReleaseCandidateFactAttribute.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xunit;
+using Xunit.Sdk;
+
+namespace LHDS.Core.Tests.Integration.Landings
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    [XunitTestCaseDiscoverer(
+        typeName: "LHDS.Core.Tests.Integration.Landings.ReleaseCandidateTestCaseDiscoverer",
+        assemblyName: "LHDS.Core.Tests.Integration.Landings")]
+    public class ReleaseCandidateFactAttribute : FactAttribute { }
+}

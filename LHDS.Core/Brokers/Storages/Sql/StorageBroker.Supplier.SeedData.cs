0fe7d0a97841f07b12df149f58189c125a26c04b
Christo du Toit <christo@dutoit.co.uk>
2023-06-19 13:15:37 +0100

    CODE RUB:  Updated seed data and changed the retrieve logic for ingestion tracking.

---
+++ LHDS.Core/Brokers/Storages/Sql/StorageBroker.Supplier.SeedData.cs
@@ -0,0 +1,36 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using LHDS.Core.Models.Foundations.Suppliers;
+using Microsoft.EntityFrameworkCore;
+
+namespace LHDS.Core.Brokers.Storages.Sql
+{
+    public partial class StorageBroker
+    {
+        private static void AddSupplierSeedData(ModelBuilder modelBuilder)
+        {
+            List<Supplier> suppliers = new List<Supplier>
+            {
+                new Supplier
+                {
+                    Id = Guid.Parse("67680f17-9d0c-4474-8b35-56ca8f9df1f6"),
+                    Name = "EMIS",
+                    FriendlyName = "EMIS",
+                    Description = "Emis Supplier",
+                    LandingManualTriggerUrl = "Update this => environment specific",
+                    DecryptionManualTriggerUrl = "Update this => environment specific",
+                    CreatedBy = "System",
+                    CreatedDate = DateTime.UtcNow,
+                    UpdatedBy = "System",
+                    UpdatedDate = DateTime.UtcNow
+                }
+            };
+
+            modelBuilder.Entity<Supplier>().HasData(suppliers);
+        }
+    }
+}

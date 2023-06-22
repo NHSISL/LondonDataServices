01b656a414e9a57a2082c1f12665c981dec23232
Hassan Raza <h_raza110@outlook.com>
2023-06-22 10:27:12 +0100

    ShouldReturnEmptyStringForNullIngestionTrackingOnProcessAsync -> WIP

---
+++ LHDS.Core.Tests.Acceptance/Clients/Landings/LandingTests.Logic.ProcessAsyncReturningString.cs
@@ -0,0 +1,38 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.IngestionTrackings;
+using LHDS.Core.Models.Foundations.OptOuts;
+using LHDS.Core.Models.Orchestrations.OptOuts;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Acceptance.Clients.Landings
+{
+    public partial class LandingTests
+    {
+        [Fact]
+        public async Task ShouldReturnEmptyStringForNullIngestionTrackingOnProcessAsync()
+        {
+            //Given
+            string fileName = GetRandomString();
+
+            //When
+            var actualString = await this.landingClient.ProcessAsync(fileName);
+
+            //Then
+            actualString.Should().Be(null);
+
+            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+            this.meshBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

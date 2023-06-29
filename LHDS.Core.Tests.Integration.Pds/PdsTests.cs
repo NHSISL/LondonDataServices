2b3acaf8089118931fe6841a52667b540a8dfade
Christo du Toit <christo@dutoit.co.uk>
2023-06-29 15:14:04 +0100

    CODE RUB: Updated config

--- LHDS.Core.Tests.Integration.Pds/PdsTests.cs
+++ LHDS.Core.Tests.Integration.Pds/PdsTests.cs
@@ -42,7 +42,7 @@ namespace LHDS.Core.Tests.Integration.Pds
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
                 .AddJsonFile("local.appsettings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables();
+                .AddEnvironmentVariables("LHDS_INTEGRATION_");
 
             IConfiguration configuration = configurationBuilder.Build();
 

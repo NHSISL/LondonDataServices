cf3b8c93c5fca86efacb028e3f2afa50fc45f649
Christo du Toit <christo@dutoit.co.uk>
2023-07-04 16:54:59 +0100

    CODE RUB: Updated config

--- LHDS.Core/Providers/Downloads/Extensions/FtpDownloadProviderServiceCollectionExtensions.cs
+++ LHDS.Core/Providers/Downloads/Extensions/FtpDownloadProviderServiceCollectionExtensions.cs
@@ -3,6 +3,7 @@
 // ---------------------------------------------------------------
 
 using System;
+using LHDS.Core.Models.Configurations;
 using LHDS.Core.Providers.Downloads.Builders;
 using LHDS.Core.Providers.Downloads.FtpDownloads;
 using Microsoft.Extensions.Configuration;
@@ -18,7 +19,9 @@ namespace LHDS.Core.Providers.Downloads.Extensions
             Action<FtpProviderRegistrationBuilder> builderAction)
         {
             IFtpDownloadProviderSettings ftpDownloadProviderSettings =
-                new FtpDownloadProviderSettings(configuration);
+                configuration.GetSection("cryptography").Get<FtpDownloadProviderSettings>();
+
+            ValidateFtpProviderSettings(ftpDownloadProviderSettings);
 
             FtpProviderRegistrationBuilder builder =
                 new FtpProviderRegistrationBuilder(ftpDownloadProviderSettings);
@@ -29,6 +32,57 @@ namespace LHDS.Core.Providers.Downloads.Extensions
             services.AddTransient<IDownloadProvider, FtpDownloadProvider>();
 
             return services;
+        }
+
+        private static void ValidateFtpProviderSettings(IFtpDownloadProviderSettings ftpDownloadProviderSettings)
+        {
+            Validate(
+                (Rule: IsInvalid(ftpDownloadProviderSettings.FtpPort),
+                    Parameter: "ftpDownload__ftpPort"),
+
+                (Rule: IsInvalid(ftpDownloadProviderSettings.FtpUserName),
+                    Parameter: "ftpDownload__ftpUserName"),
+
+                (Rule: IsInvalid(ftpDownloadProviderSettings.TempFolder),
+                    Parameter: "ftpDownload__tempFolder"),
+
+                (Rule: IsInvalid(ftpDownloadProviderSettings.IncludeSubDirectories),
+                    Parameter: "ftpDownload__includeSubDirectories"));
+        }
+
+        private static dynamic IsInvalid(int value) => new
+        {
+            Condition = value == 0,
+            Message = "Configuration value does not exist"
+        };
+
+        private static dynamic IsInvalid(bool value) => new
+        {
+            Condition = value == null,
+            Message = "Configuration value does not exist"
+        };
+
+        private static dynamic IsInvalid(string text) => new
+        {
+            Condition = string.IsNullOrWhiteSpace(text),
+            Message = "Configuration value does not exist"
+        };
+
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidConfigurationException = new InvalidConfigurationException();
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidConfigurationException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidConfigurationException.ThrowIfContainsErrors();
         }
     }
 }

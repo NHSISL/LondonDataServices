19942a7fdcf91d1f5fdd7334659bddab105c624a
Christo du Toit <christo@dutoit.co.uk>
2023-07-05 10:21:54 +0100

    CODE RUB: Code Cleanup

--- LHDS.Core/Clients/Extensions/DecryptionClientServiceCollectionExtensions.cs
+++ LHDS.Core/Clients/Extensions/DecryptionClientServiceCollectionExtensions.cs
@@ -52,7 +52,7 @@ namespace LHDS.Core.Clients.Extensions
             bool acceptanceTest)
         {
             services.AddSingleton<IConfiguration>(_ => configuration);
-            services.Configure<LandingConfiguration>(configuration.GetSection("LandingSettings"));
+            services.Configure<LandingConfiguration>(configuration.GetSection("landingSettings"));
 
             AddProviders(services);
             AddBrokers(services, configuration, acceptanceTest);

fec5fca7e5fc271bfd77dce5264bf062cc3bfc3a
Hassan Raza <h_raza110@outlook.com>
2023-07-06 11:47:53 +0100

    ShouldGetLandingDocumentByFileNameAsync -> WIP

---
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Apis/LandingsApiTests.Logic.cs
@@ -0,0 +1,56 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.AdminPortal.Api.Tests.Acceptance.Models.Audits;
+using LHDS.AdminPortal.Api.Tests.Acceptance.Models.IngestionTrackings;
+using LHDS.AdminPortal.Api.Tests.Acceptance.Models.Suppliers;
+using LHDS.Core.Models.Foundations.Documents;
+using RESTFulSense.Exceptions;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Acceptance.Apis.Audits
+{
+    public partial class LandingsApiTests
+    {
+        [Fact]
+        public async Task ShouldGetLandingDocumentByFileNameAsync()
+        {
+            // given
+            Supplier randomSupplier = await PostRandomSupplierAsync();
+            IngestionTracking randomIngestionTracking = await PostRandomIngestionTrackingAsync(randomSupplier.Id);
+            string randomFileName = GetRandomString();
+            byte[] documentData = Encoding.ASCII.GetBytes(GetRandomString());
+
+            Document document = new Document
+            {
+                DocumentData = documentData,
+                FileName = randomFileName
+            };
+
+            await this.apiBroker.documentService.AddDocumentAsync(document);
+
+            IngestionTracking ingestionTracking = CreateRandomIngestionTracking(
+                supplierId: randomSupplier.Id);
+
+            ingestionTracking.DecryptedFileName = document.FileName;
+
+            // when
+            await this.apiBroker.GetLandingDocumentByFileNameAsync(randomFileName);
+
+            // then
+            IngestionTracking createdIngestionTracking =
+                await this.apiBroker.GetIngestionTrackingByIdAsync(ingestionTracking.Id);
+
+            createdIngestionTracking.FileName = document.FileName;
+
+            await this.apiBroker.DeleteIngestionTrackingByIdAsync(randomIngestionTracking.Id);
+            await this.apiBroker.DeleteSupplierByIdAsync(randomSupplier.Id);
+        }
+    }
+}
---
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Brokers/ApiBroker.Landings.cs
@@ -0,0 +1,21 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.AdminPortal.Api.Tests.Acceptance.Models.Audits;
+using LHDS.AdminPortal.Api.Tests.Acceptance.Models.IngestionTrackings;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LHDS.AdminPortal.Api.Tests.Acceptance.Brokers
+{
+    public partial class ApiBroker
+    {
+        private const string LandingsRelativeUrl = "api/landings";
+
+        public async ValueTask<ActionResult<IngestionTracking>> GetLandingDocumentByFileNameAsync(string fileName) =>
+            await this.apiFactoryClient.GetContentAsync<IngestionTracking>($"{LandingsRelativeUrl}/{fileName}");
+    }
+}

94afd9ad7ed5e2d6f7bb5f3706c51bbc0f824092
David Hayes <davidhayes03@googlemail.com>
2023-07-13 14:59:49 +0100

    EXPOSERS: Update security Matrix

--- LHDS.AdminPortal.Web/ClientApp/src/SecurityMatrix.tsx
+++ LHDS.AdminPortal.Web/ClientApp/src/SecurityMatrix.tsx
@@ -3,15 +3,31 @@ const securityPoints = {
         view: [""]
     },
     suppliers: {
-        add: ["", ""],
-        delete: ["", ""],
-        edit: ["", ""],
-        view: ["", ""]
+        add: ["ISL.LDS.AdminSpa.Suppliers", "ISL.LDS.AdminSpa.Administrators"],
+        delete: ["ISL.LDS.AdminSpa.Suppliers", "ISL.LDS.AdminSpa.Administrators"],
+        edit: ["ISL.LDS.AdminSpa.Suppliers", "ISL.LDS.AdminSpa.Administrators"],
+        view: ["ISL.LDS.AdminSpa.Suppliers", "ISL.LDS.AdminSpa.Administrators"]
+    },
+    configNavigation: {
+        view: ["ISL.LDS.AdminSpa.Administrators"]
     },
     pdsNavigation: {
-        view: ["lhdsApi.Pds"]
+        view: ["ISL.LDS.AdminSpa.PDS", "ISL.LDS.AdminSpa.Administrators", "ISL.LDS.AdminSpa.ReadOnly"]
     },
-
+    pds: {
+        upload: ["ISL.LDS.AdminSpa.OptOut", "ISL.LDS.AdminSpa.Administrators"]
+    },
+    optOutNavigation: {
+        view: ["ISL.LDS.AdminSpa.OptOut", "ISL.LDS.AdminSpa.Administrators","ISL.LDS.AdminSpa.ReadOnly"]
+    },
+    optOut: {
+        add: ["ISL.LDS.AdminSpa.OptOut", "ISL.LDS.AdminSpa.Administrators"],
+        upload: ["ISL.LDS.AdminSpa.OptOut", "ISL.LDS.AdminSpa.Administrators"],
+        readonly: ["ISL.LDS.AdminSpa.ReadOnly"]
+    },
+    ingestionTrackingNavigation: {
+        view: ["ISL.LDS.AdminSpa.IngestionTracking", "ISL.LDS.AdminSpa.Administrators", "ISL.LDS.AdminSpa.ReadOnly"]
+    },
 }
 
 export default securityPoints

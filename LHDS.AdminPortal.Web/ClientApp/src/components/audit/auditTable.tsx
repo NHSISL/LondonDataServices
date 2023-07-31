5e13d798f76d5ce2abdb3c334a5b42df1799dcf7
David Hayes <davidhayes03@googlemail.com>
2023-07-31 16:04:45 +0100

    CODE RUB: Add Audit screen

--- LHDS.AdminPortal.Web/ClientApp/src/components/audit/auditTable.tsx
+++ LHDS.AdminPortal.Web/ClientApp/src/components/audit/auditTable.tsx
@@ -1,18 +1,12 @@
 import React, { FunctionComponent } from "react";
-import CardBase from "../bases/components/Card/CardBase";
-import CardBaseBody from "../bases/components/Card/CardBase.Body";
-import CardBaseContent from "../bases/components/Card/CardBase.Content";
-import CardBaseTitle from "../bases/components/Card/CardBase.Title";
 import TableBase from "../bases/components/Table/TableBase";
 import TableBaseTbody from "../bases/components/Table/TableBase.Tbody";
 import AuditRow from "./auditRow";
-import { auditService } from "../../services/foundations/auditService";
 import { auditViewService } from "../../services/views/auditViewService";
 import TableBaseRow from "../bases/components/Table/TableBase.Row";
 import TableBaseData from "../bases/components/Table/TableBase.Data";
 import TableBaseThead from "../bases/components/Table/TableBase.Thead";
 import { AuditView } from "../../models/views/components/audit/auditView";
-import { IngestionTrackingView } from "../../models/views/components/ingestionTracking/ingestionTrackingView";
 
 interface AuditTableProps {
     ingestionTrackingId: string;
@@ -23,7 +17,7 @@ const AuditTable: FunctionComponent<AuditTableProps> = (props) => {
         ingestionTrackingId
     } = props;
 
-    const { mappedAudits: auditsRetrieved, isLoading } =
+    const { mappedAudits: auditsRetrieved } =
         auditViewService.useGetAllAudits(ingestionTrackingId);
 
     return (

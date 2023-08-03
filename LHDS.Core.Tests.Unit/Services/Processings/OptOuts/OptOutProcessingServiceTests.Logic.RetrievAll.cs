1e7bf0bf42d9da5a6ba6c8157cf66f44abf6b7c8
David Hayes <davidhayes03@googlemail.com>
2023-08-03 13:44:20 +0100

    ShouldRetrieveOptOutProcessingAllAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/OptOuts/OptOutProcessingServiceTests.Logic.RetrievAll.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.OptOuts;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.OptOuts
+{
+    public partial class OptOutProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveOptOutProcessingAllAsync()
+        {
+            // given
+            IQueryable<OptOut> randomOptOuts = CreateRandomOptOuts();
+            IQueryable<OptOut> storageOptOuts = randomOptOuts;
+            IQueryable<OptOut> expectedOptOuts = storageOptOuts;
+
+            // when
+            OptOut actualOptOut =
+                await this.optOutProcessingService.RetrieveAllOptOutsAsync();
+
+            // then
+            actualOptOut.Should().BeEquivalentTo(expectedOptOuts);
+
+
+            this.optOutServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

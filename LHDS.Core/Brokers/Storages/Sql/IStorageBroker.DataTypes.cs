5f262ad7f1d226f97bd45d628beb90d0ab98f6b9
Christo du Toit <christo@dutoit.co.uk>
2023-08-21 14:30:41 +0100

    DATA: Added Data Types migration

---
+++ LHDS.Core/Brokers/Storages/Sql/IStorageBroker.DataTypes.cs
@@ -0,0 +1,20 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.DataTypes;
+
+namespace LHDS.Core.Brokers.Storages.Sql
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<DataType> InsertDataTypeAsync(DataType audit);
+        IQueryable<DataType> SelectAllDataTypes();
+        ValueTask<DataType> SelectDataTypeByIdAsync(Guid auditId);
+        ValueTask<DataType> UpdateDataTypeAsync(DataType audit);
+        ValueTask<DataType> DeleteDataTypeAsync(DataType audit);
+    }
+}
---
+++ LHDS.Core/Models/Foundations/DataTypes/DataType.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using LHDS.Core.Models.Bases;
+
+namespace LHDS.Core.Models.Foundations.DataTypes
+{
+    public class DataType : IKey, IAudit
+    {
+        public Guid Id { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}

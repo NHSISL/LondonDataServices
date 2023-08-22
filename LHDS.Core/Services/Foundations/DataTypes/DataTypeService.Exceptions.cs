66262edff5e32fd0af9b16746ad859a37ad664f8
Christo du Toit <christo@dutoit.co.uk>
2023-08-22 18:25:55 +0100

    ShouldThrowValidationExceptionOnAddIfDataTypeIsNullAndLogItAsync -> PASS

---
+++ LHDS.Core/Services/Foundations/DataTypes/DataTypeService.Exceptions.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.DataTypes;
+using LHDS.Core.Models.Foundations.DataTypes.Exceptions;
+using Xeptions;
+
+namespace LHDS.Core.Services.Foundations.DataTypes
+{
+    public partial class DataTypeService
+    {
+        private delegate ValueTask<DataType> ReturningDataTypeFunction();
+
+        private async ValueTask<DataType> TryCatch(ReturningDataTypeFunction returningDataTypeFunction)
+        {
+            try
+            {
+                return await returningDataTypeFunction();
+            }
+            catch (NullDataTypeException nullDataTypeException)
+            {
+                throw CreateAndLogValidationException(nullDataTypeException);
+            }
+        }
+
+        private DataTypeValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var dataTypeValidationException =
+                new DataTypeValidationException(
+                    message: "DataType validation errors occurred, please try again.",
+                    innerException: exception);
+
+            this.loggingBroker.LogError(dataTypeValidationException);
+
+            return dataTypeValidationException;
+        }
+    }
+}

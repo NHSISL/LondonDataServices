b045715327abe60624b5b2dc5df2ae7790445064
Christo du Toit <christo@dutoit.co.uk>
2023-08-22 16:36:53 +0100

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/DataTypes/Exceptions/FailedDataTypeStorageException.cs
@@ -0,0 +1,12 @@
+using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.DataTypes.Exceptions
+{
+    public class FailedDataTypeStorageException : Xeption
+    {
+        public FailedDataTypeStorageException(string message: Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}

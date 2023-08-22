649c21e3dea9dd70aa0f8ebfd3b72e60beae7c40
Christo du Toit <christo@dutoit.co.uk>
2023-08-22 13:46:53 +0100

    ShouldThrowValidationExceptionOnAddIfDataTypeIsNullAndLogItAsync -> PASS

---
+++ LHDS.Core/Services/Foundations/DataTypes/DataTypeService.Validations.cs
@@ -0,0 +1,21 @@
+using LHDS.Core.Models.Foundations.DataTypes;
+using LHDS.Core.Models.Foundations.DataTypes.Exceptions;
+
+namespace LHDS.Core.Services.Foundations.DataTypes
+{
+    public partial class DataTypeService
+    {
+        private void ValidateDataTypeOnAdd(DataType dataType)
+        {
+            ValidateDataTypeIsNotNull(dataType);
+        }
+
+        private static void ValidateDataTypeIsNotNull(DataType dataType)
+        {
+            if (dataType is null)
+            {
+                throw new NullDataTypeException();
+            }
+        }
+    }
+}

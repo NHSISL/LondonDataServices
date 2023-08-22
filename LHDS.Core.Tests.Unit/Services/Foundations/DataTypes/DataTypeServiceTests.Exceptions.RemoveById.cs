d074991f16ca684374a47b28c10273c4f2795775
Christo du Toit <christo@dutoit.co.uk>
2023-08-22 13:48:18 +0100

    ShouldThrowCriticalDependencyExceptionOnRemoveIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/DataTypes/DataTypeServiceTests.Exceptions.RemoveById.cs
@@ -0,0 +1,64 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using LHDS.Core.Models.Foundations.DataTypes;
+using LHDS.Core.Models.Foundations.DataTypes.Exceptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.DataTypes
+{
+    public partial class DataTypeServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRemoveIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            DataType randomDataType = CreateRandomDataType();
+            SqlException sqlException = GetSqlException();
+
+            var failedDataTypeStorageException =
+                new FailedDataTypeStorageException(sqlException);
+
+            var expectedDataTypeDependencyException =
+                new DataTypeDependencyException(failedDataTypeStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectDataTypeByIdAsync(randomDataType.Id))
+                    .Throws(sqlException);
+
+            // when
+            ValueTask<DataType> addDataTypeTask =
+                this.dataTypeService.RemoveDataTypeByIdAsync(randomDataType.Id);
+
+            DataTypeDependencyException actualDataTypeDependencyException =
+                await Assert.ThrowsAsync<DataTypeDependencyException>(
+                    addDataTypeTask.AsTask);
+
+            // then
+            actualDataTypeDependencyException.Should()
+                .BeEquivalentTo(expectedDataTypeDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectDataTypeByIdAsync(randomDataType.Id),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedDataTypeDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteDataTypeAsync(It.IsAny<DataType>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

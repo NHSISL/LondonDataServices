f2ee6ee2d9346560131c6574262d1c4ec7038b38
Christo du Toit <christo@dutoit.co.uk>
2023-08-22 18:26:55 +0100

    ShouldThrowValidationExceptionOnModifyIfDataTypeIsInvalidAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/DataTypes/DataTypeServiceTests.Validations.Modify.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/DataTypes/DataTypeServiceTests.Validations.Modify.cs
@@ -50,5 +50,77 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.DataTypes
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfDataTypeIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given 
+            var invalidDataType = new DataType
+            {
+                // TODO:  Add default values for your properties i.e. Name = invalidText
+            };
+
+            var invalidDataTypeException = 
+                new InvalidDataTypeException(
+                        message: "Invalid dataType. Please correct the errors and try again.");
+
+            invalidDataTypeException.AddData(
+                key: nameof(DataType.Id),
+                values: "Id is required");
+
+            //invalidDataTypeException.AddData(
+            //    key: nameof(DataType.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the DataType model
+
+            invalidDataTypeException.AddData(
+                key: nameof(DataType.CreatedDate),
+                values: "Date is required");
+
+            invalidDataTypeException.AddData(
+                key: nameof(DataType.CreatedBy),
+                values: "Text is required");
+
+            invalidDataTypeException.AddData(
+                key: nameof(DataType.UpdatedDate),
+                values: "Date is required");
+
+            invalidDataTypeException.AddData(
+                key: nameof(DataType.UpdatedBy),
+                values: "Text is required");
+
+            var expectedDataTypeValidationException =
+                new DataTypeValidationException(
+                    message: "DataType validation errors occurred, please try again.",
+                    innerException: invalidDataTypeException);
+
+            // when
+            ValueTask<DataType> modifyDataTypeTask =
+                this.dataTypeService.ModifyDataTypeAsync(invalidDataType);
+
+            DataTypeValidationException actualDataTypeValidationException =
+                await Assert.ThrowsAsync<DataTypeValidationException>(
+                    modifyDataTypeTask.AsTask);
+
+            //then
+            actualDataTypeValidationException.Should().BeEquivalentTo(expectedDataTypeValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedDataTypeValidationException))),
+                        Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateDataTypeAsync(It.IsAny<DataType>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

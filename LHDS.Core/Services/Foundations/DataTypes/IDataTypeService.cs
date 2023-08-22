cf14f2ebedb5a55f187fe926652bb02ce734ab83
Christo du Toit <christo@dutoit.co.uk>
2023-08-22 16:36:42 +0100

    ShouldAddDataTypeAsync -> FAIL

---
+++ LHDS.Core/Services/Foundations/DataTypes/IDataTypeService.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.DataTypes;
+
+namespace LHDS.Core.Services.Foundations.DataTypes
+{
+    public interface IDataTypeService
+    {
+        ValueTask<DataType> AddDataTypeAsync(DataType dataType);
+    }
+}

3585a47040823c0a0adbd52459cc6177ebd4341a
Christo du Toit <christo@dutoit.co.uk>
2023-08-22 18:26:12 +0100

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/DataTypes/Exceptions/DataTypeServiceException.cs
@@ -0,0 +1,12 @@
+using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.DataTypes.Exceptions
+{
+    public class DataTypeServiceException : Xeption
+    {
+        public DataTypeServiceException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}

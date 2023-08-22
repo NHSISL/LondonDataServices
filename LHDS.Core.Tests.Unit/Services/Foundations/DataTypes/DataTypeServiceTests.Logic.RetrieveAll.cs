0fdced55934275a16c3df38e8dcb43c91ae8675d
Christo du Toit <christo@dutoit.co.uk>
2023-08-22 16:37:10 +0100

    ShouldReturnDataType -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/DataTypes/DataTypeServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,38 @@
+using System.Linq;
+using FluentAssertions;
+using Moq;
+using LHDS.Core.Models.Foundations.DataTypes;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.DataTypes
+{
+    public partial class DataTypeServiceTests
+    {
+        [Fact]
+        public void ShouldReturnDataTypes()
+        {
+            // given
+            IQueryable<DataType> randomDataTypes = CreateRandomDataTypes();
+            IQueryable<DataType> storageDataTypes = randomDataTypes;
+            IQueryable<DataType> expectedDataTypes = storageDataTypes;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllDataTypes())
+                    .Returns(storageDataTypes);
+
+            // when
+            IQueryable<DataType> actualDataTypes =
+                this.dataTypeService.RetrieveAllDataTypes();
+
+            // then
+            actualDataTypes.Should().BeEquivalentTo(expectedDataTypes);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllDataTypes(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

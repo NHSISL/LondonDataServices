c92135ca0b8f6927aa4617138ad7692c92afe024
Christo du Toit <christo@dutoit.co.uk>
2023-08-22 18:26:08 +0100

    ShouldThrowValidationExceptionOnAddIfReferenceErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/DataTypes/Exceptions/InvalidDataTypeReferenceException.cs
@@ -0,0 +1,11 @@
+using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.DataTypes.Exceptions
+{
+    public class InvalidDataTypeReferenceException : Xeption
+    {
+        public InvalidDataTypeReferenceException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}

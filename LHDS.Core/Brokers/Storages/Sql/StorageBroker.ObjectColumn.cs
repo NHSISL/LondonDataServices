1c1e4fce391ea8cf9e25159799efb7fbf24e852f
Christo du Toit <christo@dutoit.co.uk>
2023-08-25 11:18:36 +0100

    DATA:  Renamed ColumnDefinitions to ObjectColumns

---
+++ LHDS.Core/Brokers/Storages/Sql/StorageBroker.ObjectColumn.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.ColumnDefinitions;
+using Microsoft.EntityFrameworkCore;
+
+namespace LHDS.Core.Brokers.Storages.Sql
+{
+    public partial class StorageBroker
+    {
+        public DbSet<ObjectColumn> ColumnDefinitions { get; set; }
+
+        public async ValueTask<ObjectColumn> InsertColumnDefinitionAsync(ObjectColumn columnDefinition) =>
+            await InsertAsync(columnDefinition);
+
+        public IQueryable<ObjectColumn> SelectAllColumnDefinitions() => ReadAll<ObjectColumn>();
+
+        public async ValueTask<ObjectColumn> SelectColumnDefinitionByIdAsync(Guid columnDefinitionId) =>
+            await ReadAsync<ObjectColumn>(columnDefinitionId);
+
+        public async ValueTask<ObjectColumn> UpdateColumnDefinitionAsync(ObjectColumn columnDefinition) =>
+            await UpdateAsync(columnDefinition);
+
+        public async ValueTask<ObjectColumn> DeleteColumnDefinitionAsync(ObjectColumn columnDefinition) =>
+            await DeleteAsync(columnDefinition);
+    }
+}
--- LHDS.Core/Brokers/Storages/Sql/StorageBroker.ObjectColumns.Configurations.cs
+++ LHDS.Core/Brokers/Storages/Sql/StorageBroker.ObjectColumns.Configurations.cs
@@ -2,22 +2,22 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
-using LHDS.Core.Models.Foundations.SchemaDefinitions;
+using LHDS.Core.Models.Foundations.ColumnDefinitions;
 using Microsoft.EntityFrameworkCore;
 
 namespace LHDS.Core.Brokers.Storages.Sql
 {
     public partial class StorageBroker
     {
-        private static void AddSchemaDefinitionConfigurations(ModelBuilder modelBuilder)
+        private static void AddColumnDefinitionConfigurations(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<DatasetObject>()
+            modelBuilder.Entity<ObjectColumn>()
                 .ToTable(columnDefinition => columnDefinition.IsTemporal());
 
-            modelBuilder.Entity<DatasetObject>()
-                .HasOne(schemaDefinition => schemaDefinition.DataSet)
-                .WithMany(dataSet => dataSet.SchemaDefinitions)
-                .HasForeignKey(schemaDefinition => schemaDefinition.DataSetId)
+            modelBuilder.Entity<ObjectColumn>()
+                .HasOne(columnDefinition => columnDefinition.DatasetObject)
+                .WithMany(schemaDefinition => schemaDefinition.DatasetObjects)
+                .HasForeignKey(columnDefinition => columnDefinition.DatasetObjectId)
                 .OnDelete(DeleteBehavior.NoAction);
         }
     }

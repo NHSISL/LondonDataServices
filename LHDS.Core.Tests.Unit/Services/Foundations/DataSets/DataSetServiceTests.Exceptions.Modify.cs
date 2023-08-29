197f3d95aa62938e6de5061b6943a634893048a6
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 11:02:19 +0100

    ShouldThrowValidationExceptionOnModifyIfReferenceErrorOccursAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Foundations/DataSets/DataSetServiceTests.Exceptions.Modify.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/DataSets/DataSetServiceTests.Exceptions.Modify.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
 using Moq;
@@ -58,6 +59,64 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.DataSets
 
             this.storageBrokerMock.Verify(broker =>
                 broker.UpdateDataSetAsync(randomDataSet),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnModifyIfReferenceErrorOccursAndLogItAsync()
+        {
+            // given
+            DataSet someDataSet = CreateRandomDataSet();
+            string randomMessage = GetRandomString();
+            string exceptionMessage = randomMessage;
+
+            var foreignKeyConstraintConflictException =
+                new ForeignKeyConstraintConflictException(exceptionMessage);
+
+            var invalidDataSetReferenceException =
+                new InvalidDataSetReferenceException(
+                    message: "Invalid dataSet reference error occurred.", 
+                    innerException: foreignKeyConstraintConflictException);
+
+            DataSetDependencyValidationException expectedDataSetDependencyValidationException =
+                new DataSetDependencyValidationException(
+                    message: "DataSet dependency validation occurred, please try again.",
+                    innerException: invalidDataSetReferenceException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(foreignKeyConstraintConflictException);
+
+            // when
+            ValueTask<DataSet> modifyDataSetTask =
+                this.dataSetService.ModifyDataSetAsync(someDataSet);
+
+            DataSetDependencyValidationException actualDataSetDependencyValidationException =
+                await Assert.ThrowsAsync<DataSetDependencyValidationException>(
+                    modifyDataSetTask.AsTask);
+
+            // then
+            actualDataSetDependencyValidationException.Should()
+                .BeEquivalentTo(expectedDataSetDependencyValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectDataSetByIdAsync(someDataSet.Id),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedDataSetDependencyValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateDataSetAsync(someDataSet),
                     Times.Never);
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();

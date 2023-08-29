219f6e6b717ae2159c26f812d8036c4ccc56c501
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 10:57:27 +0100

    ShouldThrowValidationExceptionOnModifyIfDataSetObjectIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/DataSetObjects/DataSetObjectServiceTests.Validations.Modify.cs
@@ -0,0 +1,54 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using LHDS.Core.Models.Foundations.DataSetObjects;
+using LHDS.Core.Models.Foundations.DataSetObjects.Exceptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.DataSetObjects
+{
+    public partial class DataSetObjectServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfDataSetObjectIsNullAndLogItAsync()
+        {
+            // given
+            DataSetObject nullDataSetObject = null;
+            var nullDataSetObjectException = new NullDataSetObjectException(message: "DataSetObject is null.");
+
+            var expectedDataSetObjectValidationException =
+                new DataSetObjectValidationException(
+                    message: "DataSetObject validation errors occurred, please try again.",
+                    innerException: nullDataSetObjectException);
+
+            // when
+            ValueTask<DataSetObject> modifyDataSetObjectTask =
+                this.dataSetObjectService.ModifyDataSetObjectAsync(nullDataSetObject);
+
+            DataSetObjectValidationException actualDataSetObjectValidationException =
+                await Assert.ThrowsAsync<DataSetObjectValidationException>(
+                    modifyDataSetObjectTask.AsTask);
+
+            // then
+            actualDataSetObjectValidationException.Should()
+                .BeEquivalentTo(expectedDataSetObjectValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedDataSetObjectValidationException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateDataSetObjectAsync(It.IsAny<DataSetObject>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

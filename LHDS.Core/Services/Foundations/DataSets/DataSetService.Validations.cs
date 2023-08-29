6e28f874bc69a6ba2dda7efaf4afcb381fac2c65
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 11:01:14 +0100

    ShouldThrowValidationExceptionOnAddIfDataSetIsInvalidAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/DataSets/DataSetService.Validations.cs
+++ LHDS.Core/Services/Foundations/DataSets/DataSetService.Validations.cs
@@ -1,3 +1,4 @@
+using System;
 using LHDS.Core.Models.Foundations.DataSets;
 using LHDS.Core.Models.Foundations.DataSets.Exceptions;
 
@@ -8,6 +9,16 @@ namespace LHDS.Core.Services.Foundations.DataSets
         private void ValidateDataSetOnAdd(DataSet dataSet)
         {
             ValidateDataSetIsNotNull(dataSet);
+
+            Validate(
+                (Rule: IsInvalid(dataSet.Id), Parameter: nameof(DataSet.Id)),
+
+                // TODO: Add any other required validation rules
+
+                (Rule: IsInvalid(dataSet.CreatedDate), Parameter: nameof(DataSet.CreatedDate)),
+                (Rule: IsInvalid(dataSet.CreatedBy), Parameter: nameof(DataSet.CreatedBy)),
+                (Rule: IsInvalid(dataSet.UpdatedDate), Parameter: nameof(DataSet.UpdatedDate)),
+                (Rule: IsInvalid(dataSet.UpdatedBy), Parameter: nameof(DataSet.UpdatedBy)));
         }
 
         private static void ValidateDataSetIsNotNull(DataSet dataSet)
@@ -16,6 +27,43 @@ namespace LHDS.Core.Services.Foundations.DataSets
             {
                 throw new NullDataSetException(message: "DataSet is null.");
             }
+        }
+
+        private static dynamic IsInvalid(Guid id) => new
+        {
+            Condition = id == Guid.Empty,
+            Message = "Id is required"
+        };
+
+        private static dynamic IsInvalid(string text) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(text),
+            Message = "Text is required"
+        };
+
+        private static dynamic IsInvalid(DateTimeOffset date) => new
+        {
+            Condition = date == default,
+            Message = "Date is required"
+        };
+
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidDataSetException = 
+                new InvalidDataSetException(
+                    message: "Invalid dataSet. Please correct the errors and try again.");
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidDataSetException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidDataSetException.ThrowIfContainsErrors();
         }
     }
 }

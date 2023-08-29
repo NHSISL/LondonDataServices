ceebc3b9be02466453d1280e8cdf457c46a8f432
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 09:47:54 +0100

    ShouldThrowValidationExceptionOnAddIfObjectColumnIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/ObjectColumns/ObjectColumnServiceTests.Validations.Add.cs
@@ -0,0 +1,47 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using LHDS.Core.Models.Foundations.ObjectColumns;
+using LHDS.Core.Models.Foundations.ObjectColumns.Exceptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.ObjectColumns
+{
+    public partial class ObjectColumnServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfObjectColumnIsNullAndLogItAsync()
+        {
+            // given
+            ObjectColumn nullObjectColumn = null;
+
+            var nullObjectColumnException =
+                new NullObjectColumnException(message: "ObjectColumn is null.");
+
+            var expectedObjectColumnValidationException =
+                new ObjectColumnValidationException(
+                    message: "ObjectColumn validation errors occurred, please try again.",
+                    innerException: nullObjectColumnException);
+
+            // when
+            ValueTask<ObjectColumn> addObjectColumnTask =
+                this.objectColumnService.AddObjectColumnAsync(nullObjectColumn);
+
+            ObjectColumnValidationException actualObjectColumnValidationException =
+                await Assert.ThrowsAsync<ObjectColumnValidationException>(
+                    addObjectColumnTask.AsTask);
+
+            // then
+            actualObjectColumnValidationException.Should().BeEquivalentTo(expectedObjectColumnValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedObjectColumnValidationException))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

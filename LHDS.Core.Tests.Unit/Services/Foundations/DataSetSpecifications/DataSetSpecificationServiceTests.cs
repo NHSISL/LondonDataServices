8d9b4926c9b078293418a39396c5ca75f01834db
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 11:06:38 +0100

    ShouldReturnDataSetSpecification -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/DataSetSpecifications/DataSetSpecificationServiceTests.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/DataSetSpecifications/DataSetSpecificationServiceTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.Serialization;
 using Microsoft.Data.SqlClient;
@@ -62,6 +63,13 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.DataSetSpecifications
 
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static IQueryable<DataSetSpecification> CreateRandomDataSetSpecifications()
+        {
+            return CreateDataSetSpecificationFiller(dateTimeOffset: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber())
+                    .AsQueryable();
+        }
 
         private static DataSetSpecification CreateRandomDataSetSpecification() =>
             CreateDataSetSpecificationFiller(dateTimeOffset: GetRandomDateTimeOffset()).Create();

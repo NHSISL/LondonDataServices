02d318f1d77ca7225c7762db373ff75075030333
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 09:48:00 +0100

    ShouldThrowValidationExceptionOnAddIfCreateAndUpdateUsersIsNotSameAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/ObjectColumns/ObjectColumnService.Validations.cs
+++ LHDS.Core/Services/Foundations/ObjectColumns/ObjectColumnService.Validations.cs
@@ -24,7 +24,13 @@ namespace LHDS.Core.Services.Foundations.ObjectColumns
                     firstDate: objectColumn.UpdatedDate,
                     secondDate: objectColumn.CreatedDate,
                     secondDateName: nameof(ObjectColumn.CreatedDate)),
-                Parameter: nameof(ObjectColumn.UpdatedDate)));
+                Parameter: nameof(ObjectColumn.UpdatedDate)),
+
+                (Rule: IsNotSame(
+                    firstId: objectColumn.UpdatedBy,
+                    secondId: objectColumn.CreatedBy,
+                    secondIdName: nameof(ObjectColumn.CreatedBy)),
+                Parameter: nameof(ObjectColumn.UpdatedBy)));
         }
 
         private static void ValidateObjectColumnIsNotNull(ObjectColumn objectColumn)
@@ -61,6 +67,24 @@ namespace LHDS.Core.Services.Foundations.ObjectColumns
                 Condition = firstDate != secondDate,
                 Message = $"Date is not the same as {secondDateName}"
             };
+
+        private static dynamic IsNotSame(
+            Guid firstId,
+            Guid secondId,
+            string secondIdName) => new
+            {
+                Condition = firstId != secondId,
+                Message = $"Id is not the same as {secondIdName}"
+            };
+
+        private static dynamic IsNotSame(
+           string first,
+           string second,
+           string secondName) => new
+           {
+               Condition = first != second,
+               Message = $"Text is not the same as {secondName}"
+           };
 
         private static void Validate(params (dynamic Rule, string Parameter)[] validations)
         {

1914034024aa98aa64abba3c32c20da79e9922e2
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 09:49:35 +0100

    ShouldThrowCriticalDependencyExceptionOnRemoveIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/ObjectColumns/ObjectColumnServiceTests.Exceptions.RemoveById.cs
@@ -0,0 +1,68 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using LHDS.Core.Models.Foundations.ObjectColumns;
+using LHDS.Core.Models.Foundations.ObjectColumns.Exceptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.ObjectColumns
+{
+    public partial class ObjectColumnServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRemoveIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            ObjectColumn randomObjectColumn = CreateRandomObjectColumn();
+            SqlException sqlException = GetSqlException();
+
+            var failedObjectColumnStorageException =
+                new FailedObjectColumnStorageException(
+                    message: "Failed objectColumn storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedObjectColumnDependencyException =
+                new ObjectColumnDependencyException(
+                    message: "ObjectColumn dependency error occurred, contact support.",
+                    innerException: failedObjectColumnStorageException); 
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectObjectColumnByIdAsync(randomObjectColumn.Id))
+                    .Throws(sqlException);
+
+            // when
+            ValueTask<ObjectColumn> addObjectColumnTask =
+                this.objectColumnService.RemoveObjectColumnByIdAsync(randomObjectColumn.Id);
+
+            ObjectColumnDependencyException actualObjectColumnDependencyException =
+                await Assert.ThrowsAsync<ObjectColumnDependencyException>(
+                    addObjectColumnTask.AsTask);
+
+            // then
+            actualObjectColumnDependencyException.Should()
+                .BeEquivalentTo(expectedObjectColumnDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectObjectColumnByIdAsync(randomObjectColumn.Id),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedObjectColumnDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteObjectColumnAsync(It.IsAny<ObjectColumn>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

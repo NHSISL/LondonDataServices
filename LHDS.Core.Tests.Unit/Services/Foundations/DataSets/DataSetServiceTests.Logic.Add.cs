e1775da5bf90e752b943704aa52c135a798850da
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 11:01:19 +0100

    ShouldThrowValidationExceptionOnAddIfCreatedDateIsNotRecentAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Foundations/DataSets/DataSetServiceTests.Logic.Add.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/DataSets/DataSetServiceTests.Logic.Add.cs
@@ -22,6 +22,10 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.DataSets
             DataSet storageDataSet = inputDataSet;
             DataSet expectedDataSet = storageDataSet.DeepClone();
 
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Returns(randomDateTimeOffset);
+
             this.storageBrokerMock.Setup(broker =>
                 broker.InsertDataSetAsync(inputDataSet))
                     .ReturnsAsync(storageDataSet);
@@ -33,13 +37,17 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.DataSets
             // then
             actualDataSet.Should().BeEquivalentTo(expectedDataSet);
 
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once());
+
             this.storageBrokerMock.Verify(broker =>
                 broker.InsertDataSetAsync(inputDataSet),
                     Times.Once);
 
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
--- LHDS.Core/Services/Foundations/DataSets/DataSetService.Validations.cs
+++ LHDS.Core/Services/Foundations/DataSets/DataSetService.Validations.cs
@@ -27,10 +27,12 @@ namespace LHDS.Core.Services.Foundations.DataSets
                 Parameter: nameof(DataSet.UpdatedDate)),
 
                 (Rule: IsNotSame(
-                    firstId: dataSet.UpdatedBy,
-                    secondId: dataSet.CreatedBy,
-                    secondIdName: nameof(DataSet.CreatedBy)),
-                Parameter: nameof(DataSet.UpdatedBy)));
+                    first: dataSet.UpdatedBy,
+                    second: dataSet.CreatedBy,
+                    secondName: nameof(DataSet.CreatedBy)),
+                Parameter: nameof(DataSet.UpdatedBy)),
+
+                (Rule: IsNotRecent(dataSet.CreatedDate), Parameter: nameof(DataSet.CreatedDate)));
         }
 
         private static void ValidateDataSetIsNotNull(DataSet dataSet)
@@ -85,6 +87,23 @@ namespace LHDS.Core.Services.Foundations.DataSets
                Condition = first != second,
                Message = $"Text is not the same as {secondName}"
            };
+
+        private dynamic IsNotRecent(DateTimeOffset date) => new
+        {
+            Condition = IsDateNotRecent(date),
+            Message = "Date is not recent"
+        };
+
+        private bool IsDateNotRecent(DateTimeOffset date)
+        {
+            DateTimeOffset currentDateTime =
+                this.dateTimeBroker.GetCurrentDateTimeOffset();
+
+            TimeSpan timeDifference = currentDateTime.Subtract(date);
+            TimeSpan oneMinute = TimeSpan.FromMinutes(1);
+
+            return timeDifference.Duration() > oneMinute;
+        }
 
         private static void Validate(params (dynamic Rule, string Parameter)[] validations)
         {

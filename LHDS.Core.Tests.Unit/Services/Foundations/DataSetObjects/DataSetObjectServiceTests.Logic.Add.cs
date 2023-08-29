359cbfcec883f5f65caf23c1f8f6594ed6d2f689
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 10:56:34 +0100

    ShouldAddDataSetObjectAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/DataSetObjects/DataSetObjectServiceTests.Logic.Add.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using LHDS.Core.Models.Foundations.DataSetObjects;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.DataSetObjects
+{
+    public partial class DataSetObjectServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddDataSetObjectAsync()
+        {
+            // given
+            DateTimeOffset randomDateTimeOffset =
+                GetRandomDateTimeOffset();
+
+            DataSetObject randomDataSetObject = CreateRandomDataSetObject(randomDateTimeOffset);
+            DataSetObject inputDataSetObject = randomDataSetObject;
+            DataSetObject storageDataSetObject = inputDataSetObject;
+            DataSetObject expectedDataSetObject = storageDataSetObject.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertDataSetObjectAsync(inputDataSetObject))
+                    .ReturnsAsync(storageDataSetObject);
+
+            // when
+            DataSetObject actualDataSetObject = await this.dataSetObjectService
+                .AddDataSetObjectAsync(inputDataSetObject);
+
+            // then
+            actualDataSetObject.Should().BeEquivalentTo(expectedDataSetObject);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertDataSetObjectAsync(inputDataSetObject),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

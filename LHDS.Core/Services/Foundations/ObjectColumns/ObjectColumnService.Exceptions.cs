7086b81a91a50d188bdf04e262230aedce261e7f
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 09:47:55 +0100

    ShouldThrowValidationExceptionOnAddIfObjectColumnIsNullAndLogItAsync -> PASS

---
+++ LHDS.Core/Services/Foundations/ObjectColumns/ObjectColumnService.Exceptions.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.ObjectColumns;
+using LHDS.Core.Models.Foundations.ObjectColumns.Exceptions;
+using Xeptions;
+
+namespace LHDS.Core.Services.Foundations.ObjectColumns
+{
+    public partial class ObjectColumnService
+    {
+        private delegate ValueTask<ObjectColumn> ReturningObjectColumnFunction();
+
+        private async ValueTask<ObjectColumn> TryCatch(ReturningObjectColumnFunction returningObjectColumnFunction)
+        {
+            try
+            {
+                return await returningObjectColumnFunction();
+            }
+            catch (NullObjectColumnException nullObjectColumnException)
+            {
+                throw CreateAndLogValidationException(nullObjectColumnException);
+            }
+        }
+
+        private ObjectColumnValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var objectColumnValidationException =
+                new ObjectColumnValidationException(
+                    message: "ObjectColumn validation errors occurred, please try again.",
+                    innerException: exception);
+
+            this.loggingBroker.LogError(objectColumnValidationException);
+
+            return objectColumnValidationException;
+        }
+    }
+}

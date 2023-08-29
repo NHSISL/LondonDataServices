0d942a6111f417930a02cec2fb22e926a86784fa
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 10:56:38 +0100

    ShouldThrowValidationExceptionOnAddIfDataSetObjectIsInvalidAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/DataSetObjects/DataSetObjectService.Validations.cs
+++ LHDS.Core/Services/Foundations/DataSetObjects/DataSetObjectService.Validations.cs
@@ -1,3 +1,4 @@
+using System;
 using LHDS.Core.Models.Foundations.DataSetObjects;
 using LHDS.Core.Models.Foundations.DataSetObjects.Exceptions;
 
@@ -8,6 +9,16 @@ namespace LHDS.Core.Services.Foundations.DataSetObjects
         private void ValidateDataSetObjectOnAdd(DataSetObject dataSetObject)
         {
             ValidateDataSetObjectIsNotNull(dataSetObject);
+
+            Validate(
+                (Rule: IsInvalid(dataSetObject.Id), Parameter: nameof(DataSetObject.Id)),
+
+                // TODO: Add any other required validation rules
+
+                (Rule: IsInvalid(dataSetObject.CreatedDate), Parameter: nameof(DataSetObject.CreatedDate)),
+                (Rule: IsInvalid(dataSetObject.CreatedBy), Parameter: nameof(DataSetObject.CreatedBy)),
+                (Rule: IsInvalid(dataSetObject.UpdatedDate), Parameter: nameof(DataSetObject.UpdatedDate)),
+                (Rule: IsInvalid(dataSetObject.UpdatedBy), Parameter: nameof(DataSetObject.UpdatedBy)));
         }
 
         private static void ValidateDataSetObjectIsNotNull(DataSetObject dataSetObject)
@@ -16,6 +27,43 @@ namespace LHDS.Core.Services.Foundations.DataSetObjects
             {
                 throw new NullDataSetObjectException(message: "DataSetObject is null.");
             }
+        }
+
+        private static dynamic IsInvalid(Guid id) => new
+        {
+            Condition = id == Guid.Empty,
+            Message = "Id is required"
+        };
+
+        private static dynamic IsInvalid(string text) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(text),
+            Message = "Text is required"
+        };
+
+        private static dynamic IsInvalid(DateTimeOffset date) => new
+        {
+            Condition = date == default,
+            Message = "Date is required"
+        };
+
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidDataSetObjectException = 
+                new InvalidDataSetObjectException(
+                    message: "Invalid dataSetObject. Please correct the errors and try again.");
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidDataSetObjectException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidDataSetObjectException.ThrowIfContainsErrors();
         }
     }
 }

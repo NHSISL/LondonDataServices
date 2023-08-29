7812b6696fe4e4fb53d5c3b4ec9b81f56cfe9a85
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 09:48:20 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/ObjectColumns/ObjectColumnServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,57 @@
+using System;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using LHDS.Core.Models.Foundations.ObjectColumns.Exceptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.ObjectColumns
+{
+    public partial class ObjectColumnServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedObjectColumnStorageException =
+                new FailedObjectColumnStorageException(
+                    message: "Failed objectColumn storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedObjectColumnDependencyException =
+                new ObjectColumnDependencyException(
+                    message: "ObjectColumn dependency error occurred, contact support.",
+                    innerException: failedObjectColumnStorageException); 
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllObjectColumns())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllObjectColumnsAction = () =>
+                this.objectColumnService.RetrieveAllObjectColumns();
+
+            ObjectColumnDependencyException actualObjectColumnDependencyException =
+                Assert.Throws<ObjectColumnDependencyException>(retrieveAllObjectColumnsAction);
+
+            // then
+            actualObjectColumnDependencyException.Should()
+                .BeEquivalentTo(expectedObjectColumnDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllObjectColumns(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedObjectColumnDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

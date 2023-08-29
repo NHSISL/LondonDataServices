63061ae347a6133efc6e49768b6e6f94bb933ba0
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 11:01:48 +0100

    ShouldRetrieveDataSetByIdAsync -> FAIL

--- LHDS.Core/Services/Foundations/DataSets/IDataSetService.cs
+++ LHDS.Core/Services/Foundations/DataSets/IDataSetService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Models.Foundations.DataSets;
@@ -8,5 +9,6 @@ namespace LHDS.Core.Services.Foundations.DataSets
     {
         ValueTask<DataSet> AddDataSetAsync(DataSet dataSet);
         IQueryable<DataSet> RetrieveAllDataSets();
+        ValueTask<DataSet> RetrieveDataSetByIdAsync(Guid dataSetId);
     }
 }

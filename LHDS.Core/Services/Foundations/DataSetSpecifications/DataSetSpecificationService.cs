10bba06027dcc948a2e5ccf3d56631a3b7bd3e5b
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 11:07:14 +0100

    ShouldThrowValidationExceptionOnModifyIfDataSetSpecificationDoesNotExistAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/DataSetSpecifications/DataSetSpecificationService.cs
+++ LHDS.Core/Services/Foundations/DataSetSpecifications/DataSetSpecificationService.cs
@@ -1,59 +1,62 @@
 using System;
-using System.Linq;
 using System.Threading.Tasks;
-using LHDS.Core.Brokers.DateTimes;
-using LHDS.Core.Brokers.Loggings;
-using LHDS.Core.Brokers.Storages.Sql;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
 using LHDS.Core.Models.Foundations.DataSetSpecifications;
+using Xunit;
 
-namespace LHDS.Core.Services.Foundations.DataSetSpecifications
+namespace LHDS.Core.Tests.Unit.Services.Foundations.DataSetSpecifications
 {
-    public partial class DataSetSpecificationService : IDataSetSpecificationService
+    public partial class DataSetSpecificationServiceTests
     {
-        private readonly IStorageBroker storageBroker;
-        private readonly IDateTimeBroker dateTimeBroker;
-        private readonly ILoggingBroker loggingBroker;
-
-        public DataSetSpecificationService(
-            IStorageBroker storageBroker,
-            IDateTimeBroker dateTimeBroker,
-            ILoggingBroker loggingBroker)
+        [Fact]
+        public async Task ShouldModifyDataSetSpecificationAsync()
         {
-            this.storageBroker = storageBroker;
-            this.dateTimeBroker = dateTimeBroker;
-            this.loggingBroker = loggingBroker;
+            // given
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+            DataSetSpecification randomDataSetSpecification = CreateRandomModifyDataSetSpecification(randomDateTimeOffset);
+            DataSetSpecification inputDataSetSpecification = randomDataSetSpecification;
+            DataSetSpecification storageDataSetSpecification = inputDataSetSpecification.DeepClone();
+            storageDataSetSpecification.UpdatedDate = randomDataSetSpecification.CreatedDate;
+            DataSetSpecification updatedDataSetSpecification = inputDataSetSpecification;
+            DataSetSpecification expectedDataSetSpecification = updatedDataSetSpecification.DeepClone();
+            Guid dataSetSpecificationId = inputDataSetSpecification.Id;
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Returns(randomDateTimeOffset);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectDataSetSpecificationByIdAsync(dataSetSpecificationId))
+                    .ReturnsAsync(storageDataSetSpecification);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.UpdateDataSetSpecificationAsync(inputDataSetSpecification))
+                    .ReturnsAsync(updatedDataSetSpecification);
+
+            // when
+            DataSetSpecification actualDataSetSpecification =
+                await this.dataSetSpecificationService.ModifyDataSetSpecificationAsync(inputDataSetSpecification);
+
+            // then
+            actualDataSetSpecification.Should().BeEquivalentTo(expectedDataSetSpecification);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectDataSetSpecificationByIdAsync(inputDataSetSpecification.Id),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateDataSetSpecificationAsync(inputDataSetSpecification),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
-
-        public ValueTask<DataSetSpecification> AddDataSetSpecificationAsync(DataSetSpecification dataSetSpecification) =>
-            TryCatch(async () =>
-            {
-                ValidateDataSetSpecificationOnAdd(dataSetSpecification);
-
-                return await this.storageBroker.InsertDataSetSpecificationAsync(dataSetSpecification);
-            });
-
-        public IQueryable<DataSetSpecification> RetrieveAllDataSetSpecifications() =>
-            TryCatch(() => this.storageBroker.SelectAllDataSetSpecifications());
-
-        public ValueTask<DataSetSpecification> RetrieveDataSetSpecificationByIdAsync(Guid dataSetSpecificationId) =>
-            TryCatch(async () =>
-            {
-                ValidateDataSetSpecificationId(dataSetSpecificationId);
-
-                DataSetSpecification maybeDataSetSpecification = await this.storageBroker
-                    .SelectDataSetSpecificationByIdAsync(dataSetSpecificationId);
-
-                ValidateStorageDataSetSpecification(maybeDataSetSpecification, dataSetSpecificationId);
-
-                return maybeDataSetSpecification;
-            });
-
-        public ValueTask<DataSetSpecification> ModifyDataSetSpecificationAsync(DataSetSpecification dataSetSpecification) =>
-            TryCatch(async () =>
-            {
-                ValidateDataSetSpecificationOnModify(dataSetSpecification);
-
-                return await this.storageBroker.UpdateDataSetSpecificationAsync(dataSetSpecification);
-            });
     }
 }

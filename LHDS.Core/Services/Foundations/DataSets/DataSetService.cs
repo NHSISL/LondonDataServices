f30810db874d7fde530ebf1033863253ae5c9c75
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 11:01:37 +0100

    ShouldReturnDataSet -> PASS

--- LHDS.Core/Services/Foundations/DataSets/DataSetService.cs
+++ LHDS.Core/Services/Foundations/DataSets/DataSetService.cs
@@ -32,6 +32,6 @@ namespace LHDS.Core.Services.Foundations.DataSets
             });
 
         public IQueryable<DataSet> RetrieveAllDataSets() =>
-            throw new System.NotImplementedException();
+            this.storageBroker.SelectAllDataSets();
     }
 }

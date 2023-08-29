a9618f267ffa967911b2a05232407fed7df55f0c
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 11:02:05 +0100

    ShouldThrowValidationExceptionOnModifyIfDataSetIsInvalidAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/DataSets/DataSetServiceTests.Validations.Modify.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/DataSets/DataSetServiceTests.Validations.Modify.cs
@@ -50,5 +50,77 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.DataSets
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfDataSetIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given 
+            var invalidDataSet = new DataSet
+            {
+                // TODO:  Add default values for your properties i.e. Name = invalidText
+            };
+
+            var invalidDataSetException = 
+                new InvalidDataSetException(
+                        message: "Invalid dataSet. Please correct the errors and try again.");
+
+            invalidDataSetException.AddData(
+                key: nameof(DataSet.Id),
+                values: "Id is required");
+
+            //invalidDataSetException.AddData(
+            //    key: nameof(DataSet.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the DataSet model
+
+            invalidDataSetException.AddData(
+                key: nameof(DataSet.CreatedDate),
+                values: "Date is required");
+
+            invalidDataSetException.AddData(
+                key: nameof(DataSet.CreatedBy),
+                values: "Text is required");
+
+            invalidDataSetException.AddData(
+                key: nameof(DataSet.UpdatedDate),
+                values: "Date is required");
+
+            invalidDataSetException.AddData(
+                key: nameof(DataSet.UpdatedBy),
+                values: "Text is required");
+
+            var expectedDataSetValidationException =
+                new DataSetValidationException(
+                    message: "DataSet validation errors occurred, please try again.",
+                    innerException: invalidDataSetException);
+
+            // when
+            ValueTask<DataSet> modifyDataSetTask =
+                this.dataSetService.ModifyDataSetAsync(invalidDataSet);
+
+            DataSetValidationException actualDataSetValidationException =
+                await Assert.ThrowsAsync<DataSetValidationException>(
+                    modifyDataSetTask.AsTask);
+
+            //then
+            actualDataSetValidationException.Should().BeEquivalentTo(expectedDataSetValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedDataSetValidationException))),
+                        Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateDataSetAsync(It.IsAny<DataSet>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

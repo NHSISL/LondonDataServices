6561020de5c06b4e250c9721069e34575b1a2813
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 11:07:25 +0100

    ShouldThrowValidationExceptionOnModifyIfStorageUpdatedDateSameAsUpdatedDateAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Foundations/DataSetSpecifications/DataSetSpecificationServiceTests.Exceptions.Modify.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/DataSetSpecifications/DataSetSpecificationServiceTests.Exceptions.Modify.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using EFxceptions.Models.Exceptions;
 using FluentAssertions;
@@ -224,6 +225,61 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.DataSetSpecifications
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
                     expectedDataSetSpecificationDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateDataSetSpecificationAsync(randomDataSetSpecification),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnModifyIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            DataSetSpecification randomDataSetSpecification = CreateRandomDataSetSpecification();
+            var serviceException = new Exception();
+
+            var failedDataSetSpecificationServiceException =
+                new FailedDataSetSpecificationServiceException(
+                    message: "Failed dataSetSpecification service occurred, please contact support", 
+                    innerException: serviceException);
+
+            var expectedDataSetSpecificationServiceException =
+                new DataSetSpecificationServiceException(
+                    message: "DataSetSpecification service error occurred, contact support.",
+                    innerException: failedDataSetSpecificationServiceException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(serviceException);
+
+            // when
+            ValueTask<DataSetSpecification> modifyDataSetSpecificationTask =
+                this.dataSetSpecificationService.ModifyDataSetSpecificationAsync(randomDataSetSpecification);
+
+            DataSetSpecificationServiceException actualDataSetSpecificationServiceException =
+                await Assert.ThrowsAsync<DataSetSpecificationServiceException>(
+                    modifyDataSetSpecificationTask.AsTask);
+
+            // then
+            actualDataSetSpecificationServiceException.Should()
+                .BeEquivalentTo(expectedDataSetSpecificationServiceException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectDataSetSpecificationByIdAsync(randomDataSetSpecification.Id),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedDataSetSpecificationServiceException))),
                         Times.Once);
 
             this.storageBrokerMock.Verify(broker =>

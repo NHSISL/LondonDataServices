4e849784e73a9832a1235bcf1a00893fd89eca18
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 10:56:37 +0100

    ShouldThrowValidationExceptionOnAddIfDataSetObjectIsInvalidAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/DataSetObjects/DataSetObjectServiceTests.Validations.Add.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/DataSetObjects/DataSetObjectServiceTests.Validations.Add.cs
@@ -32,7 +32,8 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.DataSetObjects
                     addDataSetObjectTask.AsTask);
 
             // then
-            actualDataSetObjectValidationException.Should().BeEquivalentTo(expectedDataSetObjectValidationException);
+            actualDataSetObjectValidationException.Should()
+                .BeEquivalentTo(expectedDataSetObjectValidationException);
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
@@ -42,6 +43,79 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.DataSetObjects
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfDataSetObjectIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given
+            var invalidDataSetObject = new DataSetObject
+            {
+                // TODO:  Add default values for your properties i.e. Name = invalidText
+            };
+
+            var invalidDataSetObjectException =
+                new InvalidDataSetObjectException(
+                    message: "Invalid dataSetObject. Please correct the errors and try again.");
+
+            invalidDataSetObjectException.AddData(
+                key: nameof(DataSetObject.Id),
+                values: "Id is required");
+
+            //invalidDataSetObjectException.AddData(
+            //    key: nameof(DataSetObject.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the DataSetObject model
+
+            invalidDataSetObjectException.AddData(
+                key: nameof(DataSetObject.CreatedDate),
+                values: "Date is required");
+
+            invalidDataSetObjectException.AddData(
+                key: nameof(DataSetObject.CreatedBy),
+                values: "Text is required");
+
+            invalidDataSetObjectException.AddData(
+                key: nameof(DataSetObject.UpdatedDate),
+                values: "Date is required");
+
+            invalidDataSetObjectException.AddData(
+                key: nameof(DataSetObject.UpdatedBy),
+                values: "Text is required");
+
+            var expectedDataSetObjectValidationException =
+                new DataSetObjectValidationException(
+                    message: "DataSetObject validation errors occurred, please try again.",
+                    innerException: invalidDataSetObjectException);
+
+            // when
+            ValueTask<DataSetObject> addDataSetObjectTask =
+                this.dataSetObjectService.AddDataSetObjectAsync(invalidDataSetObject);
+
+            DataSetObjectValidationException actualDataSetObjectValidationException =
+                await Assert.ThrowsAsync<DataSetObjectValidationException>(
+                    addDataSetObjectTask.AsTask);
+
+            // then
+            actualDataSetObjectValidationException.Should()
+                .BeEquivalentTo(expectedDataSetObjectValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedDataSetObjectValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertDataSetObjectAsync(It.IsAny<DataSetObject>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }
 }

f97ef607e06a95a73c97e84923275f996a7767e7
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 10:57:00 +0100

    ShouldReturnDataSetObject -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/DataSetObjects/DataSetObjectServiceTests.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/DataSetObjects/DataSetObjectServiceTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.Serialization;
 using Microsoft.Data.SqlClient;
@@ -62,6 +63,13 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.DataSetObjects
 
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static IQueryable<DataSetObject> CreateRandomDataSetObjects()
+        {
+            return CreateDataSetObjectFiller(dateTimeOffset: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber())
+                    .AsQueryable();
+        }
 
         private static DataSetObject CreateRandomDataSetObject() =>
             CreateDataSetObjectFiller(dateTimeOffset: GetRandomDateTimeOffset()).Create();

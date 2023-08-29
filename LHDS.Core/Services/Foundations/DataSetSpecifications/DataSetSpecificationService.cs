c09118fb84c15b31b836224e79a07fd835409799
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 11:06:11 +0100

    ShouldAddDataSetSpecificationAsync -> FAIL

---
+++ LHDS.Core/Services/Foundations/DataSetSpecifications/DataSetSpecificationService.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Brokers.Storages.Sql;
+using LHDS.Core.Models.Foundations.DataSetSpecifications;
+
+namespace LHDS.Core.Services.Foundations.DataSetSpecifications
+{
+    public partial class DataSetSpecificationService : IDataSetSpecificationService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public DataSetSpecificationService(
+            IStorageBroker storageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<DataSetSpecification> AddDataSetSpecificationAsync(DataSetSpecification dataSetSpecification) =>
+            throw new System.NotImplementedException();
+    }
+}

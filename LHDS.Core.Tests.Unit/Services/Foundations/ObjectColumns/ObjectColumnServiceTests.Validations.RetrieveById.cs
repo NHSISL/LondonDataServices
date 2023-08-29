5a0cf522a8c152dc43686868298ac52dcab319bb
Christo du Toit <christo@dutoit.co.uk>
2023-08-29 09:48:34 +0100

    ShouldThrowNotFoundExceptionOnRetrieveByIdIfObjectColumnIsNotFoundAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/ObjectColumns/ObjectColumnServiceTests.Validations.RetrieveById.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/ObjectColumns/ObjectColumnServiceTests.Validations.RetrieveById.cs
@@ -54,5 +54,49 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.ObjectColumns
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRetrieveByIdIfObjectColumnIsNotFoundAndLogItAsync()
+        {
+            //given
+            Guid someObjectColumnId = Guid.NewGuid();
+            ObjectColumn noObjectColumn = null;
+
+            var notFoundObjectColumnException =
+                new NotFoundObjectColumnException(someObjectColumnId);
+
+            var expectedObjectColumnValidationException =
+                new ObjectColumnValidationException(
+                    message: "ObjectColumn validation errors occurred, please try again.",
+                    innerException: notFoundObjectColumnException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectObjectColumnByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noObjectColumn);
+
+            //when
+            ValueTask<ObjectColumn> retrieveObjectColumnByIdTask =
+                this.objectColumnService.RetrieveObjectColumnByIdAsync(someObjectColumnId);
+
+            ObjectColumnValidationException actualObjectColumnValidationException =
+                await Assert.ThrowsAsync<ObjectColumnValidationException>(
+                    retrieveObjectColumnByIdTask.AsTask);
+
+            //then
+            actualObjectColumnValidationException.Should().BeEquivalentTo(expectedObjectColumnValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectObjectColumnByIdAsync(It.IsAny<Guid>()),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedObjectColumnValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

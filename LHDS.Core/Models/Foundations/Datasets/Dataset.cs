81c5fb2aa790777b6dff5fac9101444992a6442f
David Hayes <davidhayes03@googlemail.com>
2023-08-30 14:18:41 +0100

    BROKERS: Add DataSet Fields

--- LHDS.Core/Models/Foundations/Datasets/Dataset.cs
+++ LHDS.Core/Models/Foundations/Datasets/Dataset.cs
@@ -13,11 +13,12 @@ namespace LHDS.Core.Models.Foundations.DataSets
         public Guid Id { get; set; }
         public string DataSetName { get; set; }
         public string DataSetAliasses { get; set; }
+        public string DataSetSupplier { get; set; }
         public string DataSetAuthor { get; set; }
         public string DataSourceType { get; set; }
         public bool IsActive { get; set; }
-        public DateTimeOffset ActiveFrom { get; set; }
-        public DateTimeOffset ActiveTo { get; set; }
+        public DateTimeOffset? ActiveFrom { get; set; }
+        public DateTimeOffset? ActiveTo { get; set; }
         public string CreatedBy { get; set; }
         public string UpdatedBy { get; set; }
         public DateTimeOffset UpdatedDate { get; set; }

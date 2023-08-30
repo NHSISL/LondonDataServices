bfca29d3ee5916d7b9dabf9cbcf5de598dec4565
Christo du Toit <christo@dutoit.co.uk>
2023-08-30 20:21:30 +0100

    CODE RUB: Code Cleanup

--- LHDS.Core/Brokers/Storages/Sql/StorageBroker.DatasetSpecifications.Configurations.cs
+++ LHDS.Core/Brokers/Storages/Sql/StorageBroker.DatasetSpecifications.Configurations.cs
@@ -15,70 +15,70 @@ namespace LHDS.Core.Brokers.Storages.Sql
                 .ToTable(dataSet => dataSet.IsTemporal());
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.Id)
+                .Property(dataSetSpecification => dataSetSpecification.Id)
                 .IsRequired();
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.DataSetId)
+                .Property(dataSetSpecification => dataSetSpecification.DataSetId)
                 .IsRequired();
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.SupplierSpecificationVersion)
+                .Property(dataSetSpecification => dataSetSpecification.SupplierSpecificationVersion)
                 .HasMaxLength(10)
                 .IsRequired();
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.OurSpecificationVersion)
+                .Property(dataSetSpecification => dataSetSpecification.OurSpecificationVersion)
                 .HasMaxLength(10)
                 .IsRequired();
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.IsMultiSender)
+                .Property(dataSetSpecification => dataSetSpecification.IsMultiSender)
                 .HasDefaultValue(false)
                 .IsRequired();
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.DateReleased)
+                .Property(dataSetSpecification => dataSetSpecification.DateReleased)
                 .IsRequired(false);
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.DateImplemented)
+                .Property(dataSetSpecification => dataSetSpecification.DateImplemented)
                 .IsRequired(false);
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.DateSuperseded)
+                .Property(dataSetSpecification => dataSetSpecification.DateSuperseded)
                 .IsRequired(false);
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.SupersededBy)
+                .Property(dataSetSpecification => dataSetSpecification.SupersededBy)
                 .IsRequired(false);
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.PresededBy)
+                .Property(dataSetSpecification => dataSetSpecification.PresededBy)
                 .HasMaxLength(255)
                 .IsRequired();
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.IsPublished)
+                .Property(dataSetSpecification => dataSetSpecification.IsPublished)
                 .HasDefaultValue(false)
                 .IsRequired();
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.IsActive)
+                .Property(dataSetSpecification => dataSetSpecification.IsActive)
                 .HasDefaultValue(false)
                 .IsRequired();
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.CreatedDate)
+                .Property(dataSetSpecification => dataSetSpecification.CreatedDate)
                 .IsRequired();
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.UpdatedBy)
+                .Property(dataSetSpecification => dataSetSpecification.UpdatedBy)
                 .HasMaxLength(255)
                 .IsRequired();
 
             modelBuilder.Entity<DataSetSpecification>()
-                .Property(objectColumn => objectColumn.UpdatedDate)
+                .Property(dataSetSpecification => dataSetSpecification.UpdatedDate)
                 .IsRequired();
 
             modelBuilder.Entity<DataSetSpecification>()

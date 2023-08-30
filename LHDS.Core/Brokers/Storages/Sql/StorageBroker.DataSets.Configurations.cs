a8d2431b0a6f3f29aa4dae13cba081a50917470f
David Hayes <davidhayes03@googlemail.com>
2023-08-30 14:18:41 +0100

    BROKERS: Add DataSet Fields

--- LHDS.Core/Brokers/Storages/Sql/StorageBroker.DataSets.Configurations.cs
+++ LHDS.Core/Brokers/Storages/Sql/StorageBroker.DataSets.Configurations.cs
@@ -13,6 +13,61 @@ namespace LHDS.Core.Brokers.Storages.Sql
         {
             modelBuilder.Entity<DataSet>()
                 .ToTable(dataSet => dataSet.IsTemporal());
+
+            modelBuilder.Entity<DataSet>()
+               .Property(objectColumn => objectColumn.Id)
+               .IsRequired();
+
+            modelBuilder.Entity<DataSet>()
+                .Property(objectColumn => objectColumn.DataSetName)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSet>()
+                .Property(objectColumn => objectColumn.DataSetAliasses)
+                .HasMaxLength(250)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSet>()
+                .Property(objectColumn => objectColumn.DataSetSupplier)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSet>()
+                .Property(objectColumn => objectColumn.DataSetAuthor)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSet>()
+               .Property(objectColumn => objectColumn.DataSourceType)
+               .HasMaxLength(50)
+               .IsRequired();
+
+            modelBuilder.Entity<DataSet>()
+                .Property(objectColumn => objectColumn.ActiveFrom)
+                .IsRequired(false);
+
+            modelBuilder.Entity<DataSet>()
+                .Property(objectColumn => objectColumn.ActiveTo)
+                .IsRequired(false);
+
+            modelBuilder.Entity<DataSet>()
+                .Property(objectColumn => objectColumn.CreatedBy)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSet>()
+                .Property(objectColumn => objectColumn.CreatedDate)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSet>()
+                .Property(objectColumn => objectColumn.UpdatedBy)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSet>()
+                .Property(objectColumn => objectColumn.UpdatedDate)
+                .IsRequired();
         }
     }
 }

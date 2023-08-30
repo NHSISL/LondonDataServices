b24a035c86e37d435d52ec6b3204f4358deff01d
David Hayes <davidhayes03@googlemail.com>
2023-08-30 11:30:00 +0100

    ShouldThrowValidationExceptionOnAddIfObjectColumnsIsInvalidLenghtAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/DataSetObjects/DataSetObjectServiceTests.Validations.Add.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/DataSetObjects/DataSetObjectServiceTests.Validations.Add.cs
@@ -140,6 +140,81 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.DataSetObjects
         }
 
         [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfObjectColumnsIsInvalidLenghtAndLogItAsync()
+        {
+            // given
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+            DataSetObject invalidDataSetObject = CreateRandomDataSetObject(randomDateTimeOffset);
+            invalidDataSetObject.SupplierObjectName = GetRandomString(256);
+            invalidDataSetObject.OurObjectName = GetRandomString(256);
+            invalidDataSetObject.PushOrPull = GetRandomString(11);
+            invalidDataSetObject.CreatedBy = GetRandomString(256);
+            invalidDataSetObject.UpdatedBy = invalidDataSetObject.CreatedBy;
+
+            var invalidDataSetObjectException =
+                new InvalidDataSetObjectException(
+                    message: "Invalid dataSetObject. Please correct the errors and try again.");
+
+            invalidDataSetObjectException.AddData(
+                key: nameof(DataSetObject.SupplierObjectName),
+                values: "Text is exceeding max length");
+
+            invalidDataSetObjectException.AddData(
+                 key: nameof(DataSetObject.OurObjectName),
+                 values: "Text is exceeding max length");
+
+            invalidDataSetObjectException.AddData(
+                 key: nameof(DataSetObject.PushOrPull),
+                 values: "Text is exceeding max length");
+
+            invalidDataSetObjectException.AddData(
+                key: nameof(DataSetObject.CreatedBy),
+                values: "Text is exceeding max length");
+
+            invalidDataSetObjectException.AddData(
+                key: nameof(DataSetObject.UpdatedBy),
+                values: "Text is exceeding max length");
+
+            var expectedDataSetObjectValidationException =
+                new DataSetObjectValidationException(
+                    message: "DataSetObject validation errors occurred, please try again.",
+                    innerException: invalidDataSetObjectException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Returns(randomDateTimeOffset);
+
+            // when
+            ValueTask<DataSetObject> addDataSetObjectTask =
+                this.dataSetObjectService.AddDataSetObjectAsync(invalidDataSetObject);
+
+            DataSetObjectValidationException actualDataSetObjectValidationException =
+                await Assert.ThrowsAsync<DataSetObjectValidationException>(() =>
+                    addDataSetObjectTask.AsTask());
+
+            // then
+            actualDataSetObjectValidationException.Should()
+                .BeEquivalentTo(expectedDataSetObjectValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedDataSetObjectValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertDataSetObjectAsync(It.IsAny<DataSetObject>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
         public async Task ShouldThrowValidationExceptionOnAddIfCreateAndUpdateDatesIsNotSameAndLogItAsync()
         {
             // given

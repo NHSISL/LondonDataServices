dadeef4b884999542893ded7e6d8204be026a607
Christo du Toit <christo@dutoit.co.uk>
2023-08-31 11:50:57 +0100

    CONTROLLERS: POST DataSet

---
+++ LHDS.AdminPortal.Api/Controllers/DataSetsController.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using LHDS.AdminPortal.Api.Models.Foundations.DataSets;
+using LHDS.AdminPortal.Api.Models.Foundations.DataSets.Exceptions;
+using LHDS.AdminPortal.Api.Services.Foundations.DataSets;
+
+namespace LHDS.AdminPortal.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DataSetsController : RESTFulController
+    {
+        private readonly IDataSetService dataSetService;
+
+        public DataSetsController(IDataSetService dataSetService) =>
+            this.dataSetService = dataSetService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<DataSet>> PostDataSetAsync(DataSet dataSet)
+        {
+            try
+            {
+                DataSet addedDataSet =
+                    await this.dataSetService.AddDataSetAsync(dataSet);
+
+                return Created(addedDataSet);
+            }
+            catch (DataSetValidationException dataSetValidationException)
+            {
+                return BadRequest(dataSetValidationException.InnerException);
+            }
+            catch (DataSetDependencyValidationException dataSetValidationException)
+                when (dataSetValidationException.InnerException is InvalidDataSetReferenceException)
+            {
+                return FailedDependency(dataSetValidationException.InnerException);
+            }
+            catch (DataSetDependencyValidationException dataSetDependencyValidationException)
+               when (dataSetDependencyValidationException.InnerException is AlreadyExistsDataSetException)
+            {
+                return Conflict(dataSetDependencyValidationException.InnerException);
+            }
+            catch (DataSetDependencyException dataSetDependencyException)
+            {
+                return InternalServerError(dataSetDependencyException);
+            }
+            catch (DataSetServiceException dataSetServiceException)
+            {
+                return InternalServerError(dataSetServiceException);
+            }
+        }
+    }
+}

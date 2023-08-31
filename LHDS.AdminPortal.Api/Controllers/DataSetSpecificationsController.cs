95ce03c200ea7eb9909f7ca62467d3b4b1294899
Christo du Toit <christo@dutoit.co.uk>
2023-08-31 11:24:27 +0100

    CONTROLLERS: POST DataSetSpecification

---
+++ LHDS.AdminPortal.Api/Controllers/DataSetSpecificationsController.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using LHDS.AdminPortal.Api.Models.Foundations.DataSetSpecifications;
+using LHDS.AdminPortal.Api.Models.Foundations.DataSetSpecifications.Exceptions;
+using LHDS.AdminPortal.Api.Services.Foundations.DataSetSpecifications;
+
+namespace LHDS.AdminPortal.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DataSetSpecificationsController : RESTFulController
+    {
+        private readonly IDataSetSpecificationService dataSetSpecificationService;
+
+        public DataSetSpecificationsController(IDataSetSpecificationService dataSetSpecificationService) =>
+            this.dataSetSpecificationService = dataSetSpecificationService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<DataSetSpecification>> PostDataSetSpecificationAsync(DataSetSpecification dataSetSpecification)
+        {
+            try
+            {
+                DataSetSpecification addedDataSetSpecification =
+                    await this.dataSetSpecificationService.AddDataSetSpecificationAsync(dataSetSpecification);
+
+                return Created(addedDataSetSpecification);
+            }
+            catch (DataSetSpecificationValidationException dataSetSpecificationValidationException)
+            {
+                return BadRequest(dataSetSpecificationValidationException.InnerException);
+            }
+            catch (DataSetSpecificationDependencyValidationException dataSetSpecificationValidationException)
+                when (dataSetSpecificationValidationException.InnerException is InvalidDataSetSpecificationReferenceException)
+            {
+                return FailedDependency(dataSetSpecificationValidationException.InnerException);
+            }
+            catch (DataSetSpecificationDependencyValidationException dataSetSpecificationDependencyValidationException)
+               when (dataSetSpecificationDependencyValidationException.InnerException is AlreadyExistsDataSetSpecificationException)
+            {
+                return Conflict(dataSetSpecificationDependencyValidationException.InnerException);
+            }
+            catch (DataSetSpecificationDependencyException dataSetSpecificationDependencyException)
+            {
+                return InternalServerError(dataSetSpecificationDependencyException);
+            }
+            catch (DataSetSpecificationServiceException dataSetSpecificationServiceException)
+            {
+                return InternalServerError(dataSetSpecificationServiceException);
+            }
+        }
+    }
+}

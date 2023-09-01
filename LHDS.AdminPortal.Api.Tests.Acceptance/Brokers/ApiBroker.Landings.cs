3ad4ea13f849f9a8c5b80b8eccd4214b8a9eefc1
Christo du Toit <christo@dutoit.co.uk>
2023-09-01 16:15:25 +0100

    WIP:  Trying to resolve landing issue.

--- LHDS.AdminPortal.Api.Tests.Acceptance/Brokers/ApiBroker.Landings.cs
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Brokers/ApiBroker.Landings.cs
@@ -4,7 +4,6 @@
 
 using System.Threading.Tasks;
 using System.Web;
-using Newtonsoft.Json;
 
 namespace LHDS.AdminPortal.Api.Tests.Acceptance.Brokers
 {
@@ -14,11 +13,18 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Brokers
 
         public async ValueTask<string> GetLandingDocumentByFileNameAsync(string fileName)
         {
-            var jsonResponse = await this.apiFactoryClient.GetContentAsync<string>(
-                        $"{LandingsRelativeUrl}/{HttpUtility.UrlEncode(fileName)}");
+            try
+            {
+                var response = await this.apiFactoryClient.GetContentAsync<object>(
+                            $"{LandingsRelativeUrl}/{HttpUtility.UrlEncode(fileName)}");
 
-            var responseObject = JsonConvert.DeserializeObject<dynamic>(jsonResponse);
-            return responseObject.path;
+                return "test";
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+
         }
     }
 }

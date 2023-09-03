a02b8c630be95fe914509e30991828c1b2e73b0a
Hassan Raza <h_raza110@outlook.com>
2023-09-03 21:32:33 +0100

    ShouldLandDocumentByFileNameForNewIngestionTrackingAsync -> PASS

---
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Brokers/LandingConfigurationFixture.cs
@@ -0,0 +1,28 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.IO;
+using LHDS.Core.Models.Orchestrations.Downloads;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace LHDS.AdminPortal.Api.Tests.Acceptance.Brokers
+{
+    public class LandingConfigurationFixture
+    {
+        public IOptions<LandingConfiguration> LandingConfigOptions { get; }
+
+        public LandingConfigurationFixture()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("local.appsettings.json")
+                .Build();
+
+            var landingConfig = configuration.GetSection("landingSettings").Get<LandingConfiguration>();
+            LandingConfigOptions = Options.Create(landingConfig);
+        }
+    }
+
+}

9cd77f7c471e4692cbc0e123e2e40e9c15352223
Christo du Toit <christo@dutoit.co.uk>
2023-09-05 18:55:47 +0100

    FOUNDATIONS: Create - dataType

---
+++ LHDS.AdminPortal.Web/ClientApp/src/services/foundations/dataTypeService.ts
@@ -0,0 +1,27 @@
+import { useMsal } from "@azure/msal-react";
+import { Guid } from "guid-typescript";
+import { useInfiniteQuery, useMutation, useQuery, useQueryClient } from "react-query";
+import dataTypeBroker from "../../brokers/apiBroker.datatypes";
+import { dataType } from "../../models/dataTypes/dataType";
+
+export const Service = {
+    useCreatedataType: () => {
+        const broker = new dataTypeBroker();
+        const queryClient = useQueryClient();
+        const msal = useMsal();
+
+        return useMutation((dataType: dataType) => {
+            const date = new Date();
+            dataType.createdDate = dataType.updatedDate = date;
+            dataType.createdBy = dataType.updatedBy = msal.accounts[0].username;
+
+            return broker.PostdataTypeAsync(dataType);
+        },
+            {
+                onSuccess: (variables) => {
+                    queryClient.invalidateQueries("dataTypeGetAll");
+                    queryClient.invalidateQueries(["dataTypeGetById", { id: variables.id }]);
+                }
+            });
+    },
+}

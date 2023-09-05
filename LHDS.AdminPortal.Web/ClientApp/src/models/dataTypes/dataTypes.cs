737e6b2c8f4ceaea6d74a64aff6a5d24441c8eb6
Christo du Toit <christo@dutoit.co.uk>
2023-09-05 10:11:27 +0100

    DATA: DatType Model

---
+++ LHDS.AdminPortal.Web/ClientApp/src/models/dataTypes/dataTypes.cs
@@ -0,0 +1,21 @@
+﻿import { Guid } from 'guid-typescript';
+
+export class DataType
+{
+    public id: Guid;
+    public name: string;
+    public createdDate: Date;
+    public createdBy: string;
+    public updatedDate: Date;
+    public updatedBy: string;
+
+    constructor(dataType: any)
+    {
+        this.id = dataType.id ? Guid.parse(dataType.id) : Guid.parse(Guid.EMPTY);
+        this.name = dataType.name || "";
+        this.createdDate = dataType.createdDate ? new Date(dataType.createdDate) : new Date();
+        this.createdBy = dataType.createdBy || "";
+        this.updatedDate = dataType.updatedDate ? new Date(dataType.updatedDate) : new Date();
+        this.updatedBy = dataType.updatedBy || "";
+    }
+}

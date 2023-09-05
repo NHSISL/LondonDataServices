b9394e29029557b6ed45f16a7aa2623e9acc38db
Hassan Raza <h_raza110@outlook.com>
2023-09-05 15:29:14 +0100

    ShouldGetDataTypeByIdAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Acceptance/Apis/DataTypesApiTests.Logic.cs
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Apis/DataTypesApiTests.Logic.cs
@@ -56,5 +56,25 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Apis.DataTypes
                 await this.apiBroker.DeleteDataTypeByIdAsync(inputDataType.Id);
             }
         }
+
+        [Fact]
+        public async Task ShouldGetDataTypeByIdAsync()
+        {
+            // Given
+            DataType randomDataType = CreateRandomDataType();
+            DataType inputDataType = randomDataType;
+            DataType expectedDataType = inputDataType;
+            await this.apiBroker.PostDataTypeAsync(inputDataType);
+
+            // When
+            DataType actualDataType =
+                await this.apiBroker.GetDataTypeByIdAsync(inputDataType.Id);
+
+            // Then
+            actualDataType.Should().BeEquivalentTo(expectedDataType);
+
+            // Cleanup
+            await this.apiBroker.DeleteDataTypeByIdAsync(inputDataType.Id);
+        }
     }
 }

42c6be1b0186650cd3036b3c9aa5d5d3fe42a8c2
David Hayes <davidhayes03@googlemail.com>
2023-09-05 12:13:17 +0100

    MODELS: DataSetSpecification Model and View Model

---
+++ LHDS.AdminPortal.Web/ClientApp/src/models/dataSetSpecifications/dataSetSpecification.ts
@@ -0,0 +1,48 @@
+import { Guid } from 'guid-typescript';
+
+export class DataSetSpecification {
+    public id: Guid;
+    public dataSetId: Guid;
+    public supplierSpecificationVersion: string;
+    public ourSpecificationVersion: string;
+    public notes: string;
+    public isMultiSender: boolean;
+    public entityChangeSynchronisation: string;
+    public dateReleased?: Date;
+    public dateImplemented?: Date;
+    public dateSuperseded?: Date;
+    public presededBy: string;
+    public supersededBy: string;
+    public isPublished: boolean;
+    public isActive: boolean;
+    public activeFrom: Date;
+    public activeTo: Date;
+    public createdBy?: string;
+    public createdDate?: Date;
+    public updatedBy?: string;
+    public updatedDate?: Date;
+
+    constructor(dataSetSpecification: any) {
+        this.id = dataSetSpecification.id ? Guid.parse(dataSetSpecification.id) : Guid.parse(Guid.EMPTY);
+        this.dataSetId = dataSetSpecification.dataSetId;
+        this.supplierSpecificationVersion = dataSetSpecification.name;
+        this.ourSpecificationVersion = dataSetSpecification.ourSpecificationVersion;
+        this.notes = dataSetSpecification.notes;
+        this.isMultiSender = dataSetSpecification.isMultiSender;
+        this.entityChangeSynchronisation = dataSetSpecification.entityChangeSynchronisation;
+        this.dateReleased = dataSetSpecification.dateReleased;
+        this.dateImplemented = dataSetSpecification.dateImplemented;
+        this.dateSuperseded = dataSetSpecification.dateSuperseded;
+        this.presededBy = dataSetSpecification.presededBy;
+        this.supersededBy = dataSetSpecification.dataSourceType;
+        this.isPublished = dataSetSpecification.isPublished;
+        this.isActive = dataSetSpecification.isActive;
+        this.activeFrom = dataSetSpecification.activeFrom;
+        this.activeTo = dataSetSpecification.activeTo;
+        this.createdDate = dataSetSpecification.createdDate;
+        this.createdBy = dataSetSpecification.createdBy;
+        this.createdDate = new Date(dataSetSpecification.createdDate);
+        this.updatedBy = dataSetSpecification.updatedBy;
+        this.updatedDate = new Date(dataSetSpecification.updatedDate);
+    }
+}

55cd33fc57ea480e7093f1d4a96015bdcee433e7
Christo du Toit <christo@dutoit.co.uk>
2023-09-05 15:02:54 +0100

    BROKERS: GET - All DataTypes

--- LHDS.AdminPortal.Web/ClientApp/src/brokers/apiBroker.datatypes.ts
+++ LHDS.AdminPortal.Web/ClientApp/src/brokers/apiBroker.datatypes.ts
@@ -11,6 +11,17 @@ class DataTypeBroker {
         return await this.apiBroker.PostAsync(this.relativeDataTypeUrl, dataType)
             .then(result => new DataType(result.data));
     }
+
+    async GetAllDataTypesAsync(queryString: string) {
+        const url = this.relativeDataTypeUrl + queryString;
+        
+        if (queryString === "/") {
+            return undefined;
+        }
+        
+        return await this.apiBroker.GetAsync(url)
+            .then(result => result.data.map((optOut: any) => new DataType(optOut)));
+    }
 }
 
 export default DataTypeBroker;

d2c6bc23773ca49fcaab9a2779a72433ef0b6e1f
Christo du Toit <christo@dutoit.co.uk>
2023-09-05 13:30:09 +0100

    FOUNDATIONS: Create - DataSet

---
+++ LHDS.AdminPortal.Web/ClientApp/src/services/foundations/dataSetService.ts
@@ -0,0 +1,27 @@
+import { useMsal } from "@azure/msal-react";
+import { Guid } from "guid-typescript";
+import { useInfiniteQuery, useMutation, useQuery, useQueryClient } from "react-query";
+import DataSetBroker from "../../brokers/apiBroker.datasets";
+import { DataSet } from "../../models/dataSets/";
+
+export const Service = {
+    useCreateDataSet: () => {
+        const Broker = new DataSetBroker();
+        const queryClient = useQueryClient();
+        const msal = useMsal();
+
+        return useMutation((: DataSet) => {
+            const date = new Date();
+            .createdDate = .updatedDate = date;
+            .createdBy = .updatedBy = msal.accounts[0].username;
+
+            return Broker.PostDataSetAsync();
+        },
+            {
+                onSuccess: (variables) => {
+                    queryClient.invalidateQueries("DataSetGetAll");
+                    queryClient.invalidateQueries(["DataSetGetById", { id: variables.id }]);
+                }
+            });
+    },
+}

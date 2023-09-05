1821a0ebe2645ab015c2bc660ca963e945a26d9e
Christo du Toit <christo@dutoit.co.uk>
2023-09-05 13:30:31 +0100

    FOUNDATIONS: Retrieve All DataSets - Paging

--- LHDS.AdminPortal.Web/ClientApp/src/services/foundations/dataSetService.ts
+++ LHDS.AdminPortal.Web/ClientApp/src/services/foundations/dataSetService.ts
@@ -33,4 +33,21 @@ export const Service = {
             () => Broker.GetAllDataSetAsync(query),
             { staleTime: Infinity });
     },
+
+    useRetrieveAllDataSetPages: (query: string) => {
+        const Broker = new DataSetBroker();
+
+        return useInfiniteQuery(
+            ["DataSetGetAll", { query: query }],
+            ({ pageParam }) => {
+                if (!pageParam) {
+                    return Broker.GetDataSetFirstPagesAsync(query)
+                }
+                return Broker.GetDataSetSubsequentPagesAsync(pageParam)
+            },
+            {
+                getNextPageParam: (lastPage) => lastPage.nextPage,
+                staleTime: Infinity
+            });
+    },
 }

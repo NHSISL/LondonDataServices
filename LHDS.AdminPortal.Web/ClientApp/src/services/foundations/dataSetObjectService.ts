0354374338c1a1758b7a01c84a16423c9e445179
Christo du Toit <christo@dutoit.co.uk>
2023-09-05 19:07:12 +0100

    FOUNDATIONS: Create - DataSetObject

---
+++ LHDS.AdminPortal.Web/ClientApp/src/services/foundations/dataSetObjectService.ts
@@ -0,0 +1,27 @@
+import { useMsal } from "@azure/msal-react";
+import { Guid } from "guid-typescript";
+import { useInfiniteQuery, useMutation, useQuery, useQueryClient } from "react-query";
+import DataSetObjectBroker from "../../brokers/apiBroker.datasetobjects";
+import { DataSetObject } from "../../models/dataSetObjects/dataSetObject";
+
+export const Service = {
+    useCreateDataSetObject: () => {
+        const broker = new DataSetObjectBroker();
+        const queryClient = useQueryClient();
+        const msal = useMsal();
+
+        return useMutation((dataSetObject: DataSetObject) => {
+            const date = new Date();
+            dataSetObject.createdDate = dataSetObject.updatedDate = date;
+            dataSetObject.createdBy = dataSetObject.updatedBy = msal.accounts[0].username;
+
+            return broker.PostDataSetObjectAsync(dataSetObject);
+        },
+            {
+                onSuccess: (variables) => {
+                    queryClient.invalidateQueries("DataSetObjectGetAll");
+                    queryClient.invalidateQueries(["DataSetObjectGetById", { id: variables.id }]);
+                }
+            });
+    },
+}

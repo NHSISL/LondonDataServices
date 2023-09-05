72402b823eb8ee7c40066366c04305ebf13bf11d
Christo du Toit <christo@dutoit.co.uk>
2023-09-05 19:09:29 +0100

    FOUNDATIONS: Create - ObjectColumn

---
+++ LHDS.AdminPortal.Web/ClientApp/src/services/foundations/objectColumnService.ts
@@ -0,0 +1,27 @@
+import { useMsal } from "@azure/msal-react";
+import { Guid } from "guid-typescript";
+import { useInfiniteQuery, useMutation, useQuery, useQueryClient } from "react-query";
+import ObjectColumnBroker from "../../brokers/apiBroker.objectcolumns";
+import { ObjectColumn } from "../../models/objectColumns/objectColumn";
+
+export const Service = {
+    useCreateObjectColumn: () => {
+        const broker = new ObjectColumnBroker();
+        const queryClient = useQueryClient();
+        const msal = useMsal();
+
+        return useMutation((objectColumn: ObjectColumn) => {
+            const date = new Date();
+            objectColumn.createdDate = objectColumn.updatedDate = date;
+            objectColumn.createdBy = objectColumn.updatedBy = msal.accounts[0].username;
+
+            return broker.PostObjectColumnAsync(objectColumn);
+        },
+            {
+                onSuccess: (variables) => {
+                    queryClient.invalidateQueries("ObjectColumnGetAll");
+                    queryClient.invalidateQueries(["ObjectColumnGetById", { id: variables.id }]);
+                }
+            });
+    },
+}

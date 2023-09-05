fa1afa587578befbaa85cb7e994031aaabd629c1
Christo du Toit <christo@dutoit.co.uk>
2023-09-05 17:12:36 +0100

    BROKERS: GET - DataSets OData Subsequent Pages

--- LHDS.AdminPortal.Web/ClientApp/src/brokers/apiBroker.datasets.ts
+++ LHDS.AdminPortal.Web/ClientApp/src/brokers/apiBroker.datasets.ts
@@ -27,7 +27,7 @@ class DataSetBroker {
         if (queryString === "/") {
             return undefined;
         }
-
+        
         return await this.apiBroker.GetAsync(url)
             .then(result => result.data.map((optOut: any) => new DataSet(optOut)));
     }
@@ -35,6 +35,10 @@ class DataSetBroker {
     async GetDataSetFirstPagesAsync(query: string) {
         var url = this.relativeDataSetOdataUrl + query;
         return this.processOdataResult(await this.apiBroker.GetAsync(url));
+    }
+
+    async GetDataSetSubsequentPagesAsync(absoluteUri: string) {
+        return this.processOdataResult(await this.apiBroker.GetAsyncAbsolute(absoluteUri));
     }
 }
 

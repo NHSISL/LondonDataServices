8cc202bf7dc95df583958d790dd3d78c5277c1c4
Hassan Raza <h_raza110@outlook.com>
2023-09-07 13:13:29 +0100

    ShouldPostDataSetSpecificationAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Acceptance/Apis/DataSetSpecificationsApiTests.cs
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Apis/DataSetSpecificationsApiTests.cs
@@ -53,8 +53,9 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Apis.DataSetSpecifications
                 .OnProperty(DataSetSpecification => 
                     DataSetSpecification.SupplierSpecificationVersion).Use(GetRandomString(10))
 
-                .OnProperty(DataSetSpecification => DataSetSpecification.SupersededBy).Use(GetRandomString(255))
-                .OnProperty(DataSetSpecification => DataSetSpecification.PresededBy).Use(GetRandomString(255))
+                .OnProperty(DataSetSpecification => DataSetSpecification.PresededById).IgnoreIt()
+                .OnProperty(DataSetSpecification => DataSetSpecification.SupersededById).IgnoreIt()
+                .OnProperty(DataSetSpecification => DataSetSpecification.CreatedBy).Use(user)
                 .OnProperty(DataSetSpecification => DataSetSpecification.CreatedBy).Use(user)
                 .OnProperty(DataSetSpecification => DataSetSpecification.UpdatedBy).Use(user);
 

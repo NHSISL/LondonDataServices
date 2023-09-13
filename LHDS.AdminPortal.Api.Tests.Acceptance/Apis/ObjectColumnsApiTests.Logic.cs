0aa0643c5e6e99da30b85a660ddd25b6a5823829
Hassan Raza <h_raza110@outlook.com>
2023-09-13 12:43:23 +0100

    ACCEPTANCE: Object column delete api

--- LHDS.AdminPortal.Api.Tests.Acceptance/Apis/ObjectColumnsApiTests.Logic.cs
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Apis/ObjectColumnsApiTests.Logic.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
 using LHDS.AdminPortal.Api.Tests.Acceptance.Models.ObjectColumns;
+using RESTFulSense.Exceptions;
 using Xunit;
 
 namespace LHDS.AdminPortal.Api.Tests.Acceptance.Apis.ObjectColumns
@@ -29,7 +30,7 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Apis.ObjectColumns
             actualObjectColumn.Should().BeEquivalentTo(expectedObjectColumn);
 
             // Cleanup
-            await CleanupTask(actualObjectColumn);
+            await CleanupTask(actualObjectColumn, false);
         }
 
         [Fact]
@@ -56,7 +57,7 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Apis.ObjectColumns
                     actualObjectColumns.Single(approval => approval.Id == expectedObjectColumn.Id);
 
                 actualObjectColumn.Should().BeEquivalentTo(expectedObjectColumn);
-                await CleanupTask(actualObjectColumn);
+                await CleanupTask(actualObjectColumn, false);
             }
         }
 
@@ -77,7 +78,7 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Apis.ObjectColumns
             actualObjectColumn.Should().BeEquivalentTo(expectedObjectColumn);
 
             // Cleanup
-            await CleanupTask(actualObjectColumn);
+            await CleanupTask(actualObjectColumn, false);
         }
 
         [Fact]
@@ -99,7 +100,33 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Apis.ObjectColumns
             actualObjectColumn.Should().BeEquivalentTo(modifiedObjectColumn);
 
             // Cleanup
-            await CleanupTask(actualObjectColumn);
+            await CleanupTask(actualObjectColumn, false);
+        }
+
+        [Fact]
+        public async Task ShouldDeleteSpecificationObjectAsync()
+        {
+            // Given
+            ObjectColumn randomObjectColumn = await CreateRandomObjectColumnAsync();
+            ObjectColumn inputObjectColumn = randomObjectColumn;
+            ObjectColumn expectedObjectColumn = inputObjectColumn;
+            await this.apiBroker.PostObjectColumnAsync(inputObjectColumn);
+
+            // when
+            ObjectColumn deletedObjectColumn =
+                await this.apiBroker.DeleteObjectColumnByIdAsync(inputObjectColumn.Id);
+
+            ValueTask<ObjectColumn> getObjectColumnbyIdTask =
+                this.apiBroker.GetObjectColumnByIdAsync(inputObjectColumn.Id);
+
+            // then
+            deletedObjectColumn.Should().BeEquivalentTo(expectedObjectColumn);
+
+            await Assert.ThrowsAsync<HttpResponseNotFoundException>(() =>
+                getObjectColumnbyIdTask.AsTask());
+
+            // Cleanup
+            await CleanupTask(expectedObjectColumn, true);
         }
     }
 }

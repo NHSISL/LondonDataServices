be95561f202ec1a98e1bf577b8641c8c5c3df7b5
Christo du Toit <christo@dutoit.co.uk>
2023-09-20 14:42:54 +0100

    ShouldThrowDependencyValidationExceptionOnAddIfDependencyValidationErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/DataSets/DataSetProcessingServiceTests.Exceptions.Add.cs
@@ -0,0 +1,58 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.DataSets;
+using LHDS.Core.Models.Processings.DataSets.Exceptions;
+using Moq;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.DataSets
+{
+    public partial class DataSetProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnAddIfDependencyValidationErrorOccursAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            DataSet someDataSet = CreateRandomDataSet();
+            DataSet inputDataSet = someDataSet;
+
+            var expectedDataSetProcessingDependencyValidationException =
+                new DataSetProcessingDependencyValidationException(
+                    message: "DataSet processing dependency validation occurred, please try again.",
+                    innerException: dependencyValidationException.InnerException as Xeption);
+
+            this.dataSetServiceMock.Setup(service =>
+                service.AddDataSetAsync(inputDataSet))
+                    .Throws(dependencyValidationException);
+
+            // when
+            ValueTask<DataSet> dataSetAddTask =
+                this.dataSetProcessingService.AddDataSetAsync(inputDataSet);
+
+            DataSetProcessingDependencyValidationException actualException =
+                await Assert.ThrowsAsync<DataSetProcessingDependencyValidationException>(dataSetAddTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedDataSetProcessingDependencyValidationException);
+
+            this.dataSetServiceMock.Verify(service =>
+                service.AddDataSetAsync(inputDataSet),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedDataSetProcessingDependencyValidationException))),
+                         Times.Once);
+
+            this.dataSetServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

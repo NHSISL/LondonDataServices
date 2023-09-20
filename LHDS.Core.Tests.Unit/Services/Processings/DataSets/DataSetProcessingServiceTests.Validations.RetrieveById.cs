b4664d3d706e0ed278d595a8742da72176fec7f3
Christo du Toit <christo@dutoit.co.uk>
2023-09-20 17:11:03 +0100

    ShouldThrowValidationExceptionsOnRetrieveIfDataSetProcessingIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/DataSets/DataSetProcessingServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.DataSets;
+using LHDS.Core.Models.Processings.DataSets.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.DataSets
+{
+    public partial class DataSetProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionsOnRetrieveIfDataSetProcessingIsNullAndLogItAsync()
+        {
+            // given
+            Guid invalidId = Guid.Empty;
+
+            var invalidDataSetProcessingFileNameException =
+                new InvalidDataSetProcessingException(
+                    message: "Invalid dataSet. Please correct the errors and try again.");
+
+            invalidDataSetProcessingFileNameException.AddData(
+                key: "Id",
+                values: "Id is required");
+
+            var expectedDataSetProcessingValidationException =
+                new DataSetProcessingValidationException(
+                    message: "DataSet processing validation error occurred, please try again.",
+                    innerException: invalidDataSetProcessingFileNameException);
+
+            // when
+            ValueTask<DataSet> RetrieveDataSetTask =
+                this.dataSetProcessingService.RetrieveDataSetByIdAsync(invalidId);
+
+            DataSetProcessingValidationException actualDataSetProcessingValidationException =
+                await Assert.ThrowsAsync<DataSetProcessingValidationException>(RetrieveDataSetTask.AsTask);
+
+            //then
+            actualDataSetProcessingValidationException.Should()
+                .BeEquivalentTo(expectedDataSetProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedDataSetProcessingValidationException))),
+                        Times.Once);
+
+            this.dataSetServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

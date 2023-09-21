b6976b13c9ef14d91181271f02e3c0eb05d8b0ef
Christo du Toit <christo@dutoit.co.uk>
2023-09-21 10:07:13 +0100

    ShouldRetrieveDataSetIfOneExistsAndNotAddAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/DataSets/DataSetProcessingServiceTests.Logic.RetrieveOrAdd.cs
@@ -0,0 +1,44 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Force.DeepCloner;
+using LHDS.Core.Models.Foundations.DataSets;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.DataSets
+{
+    public partial class DataSetProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveDataSetIfOneExistsAndNotAddAsync()
+        {
+            // Given
+            DataSet randomDataSet = CreateRandomDataSet();
+            DataSet inputDataSet = randomDataSet;
+            DataSet storageDataSet = inputDataSet;
+            DataSet expectedDataSet = storageDataSet.DeepClone();
+
+            this.dataSetServiceMock.Setup(service =>
+                service.RetrieveDataSetByIdAsync(inputDataSet.Id))
+                    .ReturnsAsync(storageDataSet);
+
+            // When
+            await this.dataSetProcessingService.RetrieveOrAddDataSetAsync(inputDataSet);
+
+            // Then
+            this.dataSetServiceMock.Verify(service =>
+                service.RetrieveDataSetByIdAsync(inputDataSet.Id),
+                    Times.Once);
+
+            this.dataSetServiceMock.Verify(service =>
+                service.AddDataSetAsync(inputDataSet),
+                    Times.Never);
+
+            this.dataSetServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

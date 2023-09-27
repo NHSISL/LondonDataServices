ad68c6687ac4f8993c107e5ff4aa8b76a26800dd
Christo du Toit <christo@dutoit.co.uk>
2023-09-27 16:33:12 +0100

    01 - ShouldModifyObjectColumnAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/ObjectColumns/ObjectColumnProcessingServiceTests.Logic.Modify.cs
@@ -0,0 +1,40 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Force.DeepCloner;
+using LHDS.Core.Models.Foundations.ObjectColumns;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.ObjectColumns
+{
+    public partial class ObjectColumnProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldModifyObjectColumnAsync()
+        {
+            // Given
+            ObjectColumn randomObjectColumn = CreateRandomObjectColumn();
+            ObjectColumn inputObjectColumn = randomObjectColumn;
+            ObjectColumn storageObjectColumn = inputObjectColumn;
+            ObjectColumn expectedObjectColumn = storageObjectColumn.DeepClone();
+
+            this.objectColumnServiceMock.Setup(service =>
+                service.ModifyObjectColumnAsync(inputObjectColumn))
+                    .ReturnsAsync(storageObjectColumn);
+
+            // When
+            await this.objectColumnProcessingService.ModifyObjectColumnAsync(inputObjectColumn);
+
+            // Then
+            this.objectColumnServiceMock.Verify(service =>
+                service.ModifyObjectColumnAsync(inputObjectColumn),
+                    Times.Once);
+
+            this.objectColumnServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

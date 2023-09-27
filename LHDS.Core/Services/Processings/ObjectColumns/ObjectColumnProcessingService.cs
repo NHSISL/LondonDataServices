fe6f3edb9d09d3449fee4b638a7ad33db6bbfe7b
Christo du Toit <christo@dutoit.co.uk>
2023-09-27 10:50:34 +0100

    001 - ShouldAddObjectColumnAsync -> FAIL

---
+++ LHDS.Core/Services/Processings/ObjectColumns/ObjectColumnProcessingService.cs
@@ -0,0 +1,28 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Models.Foundations.ObjectColumns;
+using LHDS.Core.Services.Foundations.ObjectColumns;
+
+namespace LHDS.Core.Services.Processings.ObjectColumns
+{
+    public partial class ObjectColumnProcessingService : IObjectColumnProcessingService
+    {
+        private readonly IObjectColumnService objectColumnService;
+        private readonly ILoggingBroker loggingBroker;
+
+        public ObjectColumnProcessingService(
+            IObjectColumnService objectColumnService,
+            ILoggingBroker loggingBroker)
+        {
+            this.objectColumnService = objectColumnService;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<ObjectColumn> AddObjectColumnAsync(ObjectColumn objectColumn) =>
+            throw new System.NotImplementedException();
+    }
+}

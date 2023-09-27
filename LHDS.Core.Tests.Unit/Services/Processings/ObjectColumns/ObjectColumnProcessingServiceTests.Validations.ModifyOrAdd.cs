47fe171409d99d09a515da941229d6b6050fe05d
Christo du Toit <christo@dutoit.co.uk>
2023-09-27 13:49:34 +0100

    007 - ShouldThrowValidationExceptionsOnModifyOrAddIfIdIsInvalidAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Processings/ObjectColumns/ObjectColumnProcessingServiceTests.Validations.ModifyOrAdd.cs
+++ LHDS.Core.Tests.Unit/Services/Processings/ObjectColumns/ObjectColumnProcessingServiceTests.Validations.ModifyOrAdd.cs
@@ -46,5 +46,44 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.ObjectColumns
             this.objectColumnServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionsOnModifyOrAddIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            ObjectColumn invalidObjectColumn = new ObjectColumn();
+
+            var invalidArgumentObjectColumnProcessingException =
+                new InvalidArgumentObjectColumnProcessingException(
+                    message: "Invalid argument(s). Please correct the errors and try again.");
+
+            invalidArgumentObjectColumnProcessingException.AddData(
+                key: "Id",
+                values: "Id is required");
+
+            var expectedObjectColumnProcessingValidationException =
+                new ObjectColumnProcessingValidationException(
+                    message: "ObjectColumn processing validation error occurred, please try again.",
+                    innerException: invalidArgumentObjectColumnProcessingException);
+
+            // when
+            ValueTask<ObjectColumn> RetrieveObjectColumnTask =
+                this.objectColumnProcessingService.ModifyOrAddObjectColumnAsync(invalidObjectColumn);
+
+            ObjectColumnProcessingValidationException actualObjectColumnProcessingValidationException =
+                await Assert.ThrowsAsync<ObjectColumnProcessingValidationException>(RetrieveObjectColumnTask.AsTask);
+
+            //then
+            actualObjectColumnProcessingValidationException.Should()
+                .BeEquivalentTo(expectedObjectColumnProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedObjectColumnProcessingValidationException))),
+                        Times.Once);
+
+            this.objectColumnServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

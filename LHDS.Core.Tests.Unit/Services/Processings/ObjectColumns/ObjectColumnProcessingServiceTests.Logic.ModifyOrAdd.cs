6bdf588f6202db7e596dad5078fa5c02f2a24b90
Christo du Toit <christo@dutoit.co.uk>
2023-09-27 13:40:57 +0100

    001 - ShouldModifyObjectColumnIfOneExistsAndNotAddAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/ObjectColumns/ObjectColumnProcessingServiceTests.Logic.ModifyOrAdd.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LHDS.Core.Models.Foundations.ObjectColumns;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.ObjectColumns
+{
+    public partial class ObjectColumnProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldModifyObjectColumnIfOneExistsAndNotAddAsync()
+        {
+            // Given
+            ObjectColumn randomObjectColumn = CreateRandomObjectColumn();
+            ObjectColumn storageObjectColumn = randomObjectColumn;
+            ObjectColumn modifiedObjectColumn = storageObjectColumn.DeepClone();
+            modifiedObjectColumn.UpdatedDate = DateTimeOffset.UtcNow;
+            ObjectColumn updatedObjectColumn = modifiedObjectColumn.DeepClone();
+            ObjectColumn expectedObjectColumn = updatedObjectColumn;
+
+            this.objectColumnServiceMock.Setup(service =>
+                service.RetrieveObjectColumnByIdAsync(modifiedObjectColumn.Id))
+                    .ReturnsAsync(value: storageObjectColumn);
+
+            this.objectColumnServiceMock.Setup(service =>
+                service.ModifyObjectColumnAsync(modifiedObjectColumn))
+                    .ReturnsAsync(value: updatedObjectColumn);
+
+            // When
+            ObjectColumn actualObjectColumn = await this.objectColumnProcessingService
+                .ModifyOrAddObjectColumnAsync(modifiedObjectColumn);
+
+            // Then
+            actualObjectColumn.Should().BeEquivalentTo(expectedObjectColumn);
+
+            this.objectColumnServiceMock.Verify(service =>
+                service.RetrieveObjectColumnByIdAsync(modifiedObjectColumn.Id),
+                    Times.Once);
+
+            this.objectColumnServiceMock.Verify(service =>
+                service.ModifyObjectColumnAsync(modifiedObjectColumn),
+                    Times.Once);
+
+            this.objectColumnServiceMock.Verify(service =>
+                service.AddObjectColumnAsync(modifiedObjectColumn),
+                    Times.Never);
+
+            this.objectColumnServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

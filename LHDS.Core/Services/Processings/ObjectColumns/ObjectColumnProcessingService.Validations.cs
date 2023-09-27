7636fa5b27dbd2f448fa085430c040263948840c
Christo du Toit <christo@dutoit.co.uk>
2023-09-27 11:05:07 +0100

    004 - ShouldThrowValidationExceptionsOnAddIfObjectColumnProcessingIsNullAndLogItAsync -> PASS

---
+++ LHDS.Core/Services/Processings/ObjectColumns/ObjectColumnProcessingService.Validations.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using LHDS.Core.Models.Foundations.ObjectColumns;
+using LHDS.Core.Models.Processings.ObjectColumns.Exceptions;
+
+namespace LHDS.Core.Services.Processings.ObjectColumns
+{
+    public partial class ObjectColumnProcessingService : IObjectColumnProcessingService
+    {
+        private void ValidateObjectColumn(ObjectColumn objectColumn)
+        {
+            ValidateObjectColumnIsNotNull(objectColumn);
+        }
+
+        private static void ValidateObjectColumnIsNotNull(ObjectColumn objectColumn)
+        {
+            if (objectColumn is null)
+            {
+                throw new NullObjectColumnProcessingException(message: "ObjectColumn is null.");
+            }
+        }
+    }
+}

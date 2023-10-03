90a5340064b35f354e029b98fba1c7c41471b773
Hassan Raza <h_raza110@outlook.com>
2023-10-03 13:56:03 +0100

    Fixing DataSet api model

--- LHDS.AdminPortal.Api.Tests.Acceptance/Models/DataSets/DataSet.cs
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Models/DataSets/DataSet.cs
@@ -14,7 +14,6 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Models.DataSets
         public Guid SupplierId { get; set; }
         public string DataSetName { get; set; } = string.Empty;
         public string DataSetAliases { get; set; } = string.Empty;
-        public string DataSetSupplier { get; set; } = string.Empty;
         public string DataSetAuthor { get; set; } = string.Empty;
         public string SpecifiedBy { get; set; } = string.Empty;
         public bool IsNationallySpecified { get; set; }

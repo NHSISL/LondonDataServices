de67f7bb147816db8da0d5906a0c7f0f0890098b
Christo du Toit <christo@dutoit.co.uk>
2023-10-03 11:50:44 +0100

    ShouldThrowValidationExceptionOnAddIfStudentIsInvalidAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/Students/StudentService.Validations.cs
+++ LHDS.Core/Services/Foundations/Students/StudentService.Validations.cs
@@ -1,3 +1,4 @@
+using System;
 using LHDS.Core.Models.Foundations.Students;
 using LHDS.Core.Models.Foundations.Students.Exceptions;
 
@@ -8,6 +9,16 @@ namespace LHDS.Core.Services.Foundations.Students
         private void ValidateStudentOnAdd(Student student)
         {
             ValidateStudentIsNotNull(student);
+
+            Validate(
+                (Rule: IsInvalid(student.Id), Parameter: nameof(Student.Id)),
+
+                // TODO: Add any other required validation rules
+
+                (Rule: IsInvalid(student.CreatedDate), Parameter: nameof(Student.CreatedDate)),
+                (Rule: IsInvalid(student.CreatedBy), Parameter: nameof(Student.CreatedBy)),
+                (Rule: IsInvalid(student.UpdatedDate), Parameter: nameof(Student.UpdatedDate)),
+                (Rule: IsInvalid(student.UpdatedBy), Parameter: nameof(Student.UpdatedBy)));
         }
 
         private static void ValidateStudentIsNotNull(Student student)
@@ -16,6 +27,43 @@ namespace LHDS.Core.Services.Foundations.Students
             {
                 throw new NullStudentException(message: "Student is null.");
             }
+        }
+
+        private static dynamic IsInvalid(Guid id) => new
+        {
+            Condition = id == Guid.Empty,
+            Message = "Id is required"
+        };
+
+        private static dynamic IsInvalid(string text) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(text),
+            Message = "Text is required"
+        };
+
+        private static dynamic IsInvalid(DateTimeOffset date) => new
+        {
+            Condition = date == default,
+            Message = "Date is required"
+        };
+
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidStudentException = 
+                new InvalidStudentException(
+                    message: "Invalid student. Please correct the errors and try again.");
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidStudentException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidStudentException.ThrowIfContainsErrors();
         }
     }
 }

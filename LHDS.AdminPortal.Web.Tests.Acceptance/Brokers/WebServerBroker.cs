b048756065290cc3967c57e61628c89dab7aed89
David Hayes <davidhayes03@googlemail.com>
2023-10-03 13:27:25 +0100

    WIP: build ReactApp

--- LHDS.AdminPortal.Web.Tests.Acceptance/Brokers/WebServerBroker.cs
+++ LHDS.AdminPortal.Web.Tests.Acceptance/Brokers/WebServerBroker.cs
@@ -3,9 +3,12 @@
 // ---------------------------------------------------------------
 
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +24,8 @@ namespace LHDS.AdminPortal.Web.Tests.Acceptance.Brokers
         private readonly IHost frontendHost;
         private IPlaywright playwright { get; set; }
         public IBrowser browser { get; private set; }
+        private IPage page;
+        private Process reactAppProcess;
         public string ApiBaseUrl { get; } = $"https://localhost:{GetRandomUnusedPort()}";
         public string FrontendBaseUrl { get; } = $"https://localhost:{GetRandomUnusedPort()}";
         public string FrontendProxyBaseUrl { get; } = $"https://localhost:44405/";
@@ -56,6 +61,11 @@ namespace LHDS.AdminPortal.Web.Tests.Acceptance.Brokers
 
         public async Task InitializeAsync()
         {
+            Task reactAppTask = StartReactAppAsync();
+
+            // Sleep for a moment to ensure the React app is started
+            Thread.Sleep(10000);
+
             playwright = await Playwright.CreateAsync();
 
             browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
@@ -65,6 +75,32 @@ namespace LHDS.AdminPortal.Web.Tests.Acceptance.Brokers
             });
 
             await Task.WhenAll(apiHost.StartAsync(), frontendHost.StartAsync());
+            await reactAppTask;
+        }
+
+        public async Task StartReactAppAsync()
+        {
+            string solutionDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..");
+            solutionDirectory = Path.GetFullPath(solutionDirectory);
+            string clientAppPath = Path.Combine(solutionDirectory, "LHDS.AdminPortal.Web", "ClientApp");
+
+            using (var reactAppProcess = new Process())
+            {
+                reactAppProcess.StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/C start npm run start",
+                    UseShellExecute = true,
+                    CreateNoWindow = true,
+                    WorkingDirectory = clientAppPath,
+                };
+
+                await Task.Run(() =>
+                {
+                    reactAppProcess.Start();
+                    reactAppProcess.WaitForExit(); // Wait for the process to exit
+                });
+            }
         }
 
         public async Task DisposeAsync()
@@ -89,10 +125,6 @@ namespace LHDS.AdminPortal.Web.Tests.Acceptance.Brokers
             var port = ((IPEndPoint)listener.LocalEndpoint).Port;
             listener.Stop();
             return port;
-        }
-
-        public interface IWebServerBroker
-        {
         }
     }
 }

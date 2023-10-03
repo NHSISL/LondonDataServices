3e7142fe5eeb5567a4dcc1c05a51cc227c89981f
Christo du Toit <christo@dutoit.co.uk>
2023-10-03 17:10:18 +0100

    DATA: Changes to procedure to overcome STRING_SPLIT limitation

---
+++ LHDS.Core/Brokers/Storages/Sql/Functions/Metadata/SplitString.sql
@@ -0,0 +1,18 @@
+﻿CREATE OR ALTER FUNCTION [LDS].[SplitString]
+(
+    @InputString NVARCHAR(MAX),
+    @Delimiter NVARCHAR(1)
+)
+RETURNS TABLE
+AS
+RETURN (
+    WITH SplitStrings AS
+    (
+        SELECT 
+            value AS SplitValue,
+            ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS SplitOrdinal
+        FROM STRING_SPLIT(@InputString, @Delimiter)
+    )
+    SELECT SplitValue, SplitOrdinal
+    FROM SplitStrings
+);

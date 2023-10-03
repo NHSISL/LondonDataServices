2ae1947f3468945724995926311dfd4ca6442d06
Christo du Toit <christo@dutoit.co.uk>
2023-10-03 11:50:45 +0100

    ShouldThrowValidationExceptionOnAddIfCreateAndUpdateDatesIsNotSameAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/Students/StudentServiceTests.Validations.Add.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Students/StudentServiceTests.Validations.Add.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
@@ -86,6 +87,57 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Students
             invalidStudentException.AddData(
                 key: nameof(Student.UpdatedBy),
                 values: "Text is required");
+
+            var expectedStudentValidationException =
+                new StudentValidationException(
+                    message: "Student validation errors occurred, please try again.",
+                    innerException: invalidStudentException);
+
+            // when
+            ValueTask<Student> addStudentTask =
+                this.studentService.AddStudentAsync(invalidStudent);
+
+            StudentValidationException actualStudentValidationException =
+                await Assert.ThrowsAsync<StudentValidationException>(
+                    addStudentTask.AsTask);
+
+            // then
+            actualStudentValidationException.Should()
+                .BeEquivalentTo(expectedStudentValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedStudentValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertStudentAsync(It.IsAny<Student>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfCreateAndUpdateDatesIsNotSameAndLogItAsync()
+        {
+            // given
+            int randomNumber = GetRandomNumber();
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+            Student randomStudent = CreateRandomStudent(randomDateTimeOffset);
+            Student invalidStudent = randomStudent;
+
+            invalidStudent.UpdatedDate =
+                invalidStudent.CreatedDate.AddDays(randomNumber);
+
+            var invalidStudentException = 
+                new InvalidStudentException(
+                    message: "Invalid student. Please correct the errors and try again.");
+
+            invalidStudentException.AddData(
+                key: nameof(Student.UpdatedDate),
+                values: $"Date is not the same as {nameof(Student.CreatedDate)}");
 
             var expectedStudentValidationException =
                 new StudentValidationException(

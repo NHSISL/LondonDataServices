35725d5d626f10d42884329219e55b10f4bbb42a
Hassan Raza <h_raza110@outlook.com>
2023-10-06 15:06:07 +0100

    Fixing odata tests

---
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Brokers/ApiBroker.OData.cs
@@ -0,0 +1,21 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.AdminPortal.Api.Tests.Acceptance.Models.OdataResponses;
+
+namespace LHDS.AdminPortal.Api.Tests.Acceptance.Brokers
+{
+    public partial class ApiBroker
+    {
+        public async ValueTask<List<T>> GetAllItemsAsync<T>(string relativeOdataUrl)
+        {
+            OdataResponse<T> response =
+                await this.apiFactoryClient.GetContentAsync<OdataResponse<T>>($"{relativeOdataUrl}/");
+
+            return response.Items;
+        }
+    }
+}

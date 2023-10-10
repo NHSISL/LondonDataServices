99602e09b8fc919660d24421de4fae2d839d22fd
Christo du Toit <christo@dutoit.co.uk>
2023-10-10 14:34:11 +0100

    PROCESSINGS: DataSetSpecification Processing Service

---
+++ LHDS.Core.Tests.Unit/Services/Processings/DataSetSpecifications/DataSetSpecificationServiceTests.Exceptions.Add.cs
@@ -0,0 +1,149 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.DataSetSpecifications;
+using LHDS.Core.Models.Processings.DataSetSpecifications.Exceptions;
+using Moq;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.DataSetSpecifications
+{
+    public partial class DataSetSpecificationProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnAddIfErrorOccursAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            DataSetSpecification someDataSetSpecification = CreateRandomDataSetSpecification();
+            DataSetSpecification inputDataSetSpecification = someDataSetSpecification;
+
+            var expectedDataSetSpecificationProcessingDependencyValidationException =
+                new DataSetSpecificationProcessingDependencyValidationException(
+                    message: "DataSetSpecification processing dependency validation error occurred, please try again.",
+                    innerException: dependencyValidationException.InnerException as Xeption);
+
+            this.dataSetSpecificationServiceMock.Setup(service =>
+                service.AddDataSetSpecificationAsync(inputDataSetSpecification))
+                    .Throws(dependencyValidationException);
+
+            // when
+            ValueTask<DataSetSpecification> dataSetSpecificationAddTask =
+                this.dataSetSpecificationProcessingService.AddDataSetSpecificationAsync(inputDataSetSpecification);
+
+            DataSetSpecificationProcessingDependencyValidationException actualException =
+                await Assert.ThrowsAsync<DataSetSpecificationProcessingDependencyValidationException>(
+                    dataSetSpecificationAddTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedDataSetSpecificationProcessingDependencyValidationException);
+
+            this.dataSetSpecificationServiceMock.Verify(service =>
+                service.AddDataSetSpecificationAsync(inputDataSetSpecification),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedDataSetSpecificationProcessingDependencyValidationException))),
+                         Times.Once);
+
+            this.dataSetSpecificationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task ShouldThrowDependencyExceptionOnAddIfDependencyErrorOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            DataSetSpecification someDataSetSpecification = CreateRandomDataSetSpecification();
+            DataSetSpecification inputDataSetSpecification = someDataSetSpecification;
+
+            var expectedDataSetSpecificationProcessingDependencyException =
+                new DataSetSpecificationProcessingDependencyException(
+                    message: "DataSetSpecification processing dependency error occurred, please try again.",
+                    innerException: dependencyException.InnerException as Xeption);
+
+            this.dataSetSpecificationServiceMock.Setup(service =>
+                service.AddDataSetSpecificationAsync(inputDataSetSpecification))
+                    .Throws(dependencyException);
+
+            // when
+            ValueTask<DataSetSpecification> dataSetSpecificationAddTask =
+                this.dataSetSpecificationProcessingService.AddDataSetSpecificationAsync(inputDataSetSpecification);
+
+            DataSetSpecificationProcessingDependencyException actualException =
+                await Assert.ThrowsAsync<DataSetSpecificationProcessingDependencyException>(
+                    dataSetSpecificationAddTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedDataSetSpecificationProcessingDependencyException);
+
+            this.dataSetSpecificationServiceMock.Verify(service =>
+                service.AddDataSetSpecificationAsync(inputDataSetSpecification),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedDataSetSpecificationProcessingDependencyException))),
+                         Times.Once);
+
+            this.dataSetSpecificationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAsync()
+        {
+            // given
+            DataSetSpecification someDataSetSpecification = CreateRandomDataSetSpecification();
+            DataSetSpecification inputDataSetSpecification = someDataSetSpecification;
+
+            var serviceException = new Exception();
+
+            var failedDataSetSpecificationProcessingServiceException =
+                new FailedDataSetSpecificationProcessingServiceException(
+                    message: "Failed DataSetSpecification processing service error occurred, contact support.",
+                    innerException: serviceException);
+
+            var expectedDataSetSpecificationProcessingServiveException =
+                new DataSetSpecificationProcessingServiceException(
+                    message: "DataSetSpecification processing service error occurred, contact support.",
+                    innerException: failedDataSetSpecificationProcessingServiceException);
+
+            this.dataSetSpecificationServiceMock.Setup(service =>
+                service.AddDataSetSpecificationAsync(inputDataSetSpecification))
+                    .Throws(serviceException);
+
+            // when
+            ValueTask<DataSetSpecification> addDataSetSpecificationTask =
+                this.dataSetSpecificationProcessingService.AddDataSetSpecificationAsync(inputDataSetSpecification);
+
+            DataSetSpecificationProcessingServiceException actualException =
+                await Assert.ThrowsAsync<DataSetSpecificationProcessingServiceException>(
+                    addDataSetSpecificationTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedDataSetSpecificationProcessingServiveException);
+
+            this.dataSetSpecificationServiceMock.Verify(service =>
+                service.AddDataSetSpecificationAsync(inputDataSetSpecification),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedDataSetSpecificationProcessingServiveException))),
+                         Times.Once);
+
+            this.dataSetSpecificationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

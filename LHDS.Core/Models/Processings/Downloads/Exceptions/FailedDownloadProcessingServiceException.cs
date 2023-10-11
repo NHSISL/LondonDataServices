ae6bee05536b11cb891f3df1885880bd1fbf5351
Christo du Toit <christo@dutoit.co.uk>
2023-10-11 13:02:53 +0100

    ShouldThrowServiceExceptionOnRetrieveByFileNameIfServiceErrorOccursAsync -> FAIL

---
+++ LHDS.Core/Models/Processings/Downloads/Exceptions/FailedDownloadProcessingServiceException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Processings.Downloads.Exceptions
+{
+    public class FailedDownloadProcessingServiceException : Xeption
+    {
+        public FailedDownloadProcessingServiceException(string message, Exception innerException)
+          : base(message, innerException)
+        { }
+    }
+}

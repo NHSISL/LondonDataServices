6457501d6d021797b385c547886f4577a9e696ca
Hassan Raza <h_raza110@outlook.com>
2023-10-12 11:29:16 +0100

    ShouldThrowValidationExceptionsOnGetActiveIfCountIsZeroAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Processings/DataSetSpecifications/Exceptions/InvalidCountDataSetSpecificationProcessingException.cs
@@ -0,0 +1,15 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Processings.DataSetSpecifications.Exceptions
+{
+    public class InvalidCountDataSetSpecificationProcessingException : Xeption
+    {
+        public InvalidCountDataSetSpecificationProcessingException(string message)
+            : base(message)
+        { }
+    }
+}

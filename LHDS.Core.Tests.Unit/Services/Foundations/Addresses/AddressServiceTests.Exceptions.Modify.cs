0f692162e89c31a50582b8e4de5c0166dc31bcef
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 09:33:12 +0100

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/Addresses/AddressServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,68 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Foundations.Addresses.Exceptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Addresses
+{
+    public partial class AddressServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Address randomAddress = CreateRandomAddress();
+            SqlException sqlException = GetSqlException();
+
+            var failedAddressStorageException =
+                new FailedAddressStorageException(
+                    message: "Failed address storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedAddressDependencyException =
+                new AddressDependencyException(
+                    message: "Address dependency error occurred, contact support.",
+                    innerException: failedAddressStorageException); 
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(sqlException);
+
+            // when
+            ValueTask<Address> modifyAddressTask =
+                this.addressService.ModifyAddressAsync(randomAddress);
+
+            AddressDependencyException actualAddressDependencyException =
+                await Assert.ThrowsAsync<AddressDependencyException>(
+                    modifyAddressTask.AsTask);
+
+            // then
+            actualAddressDependencyException.Should()
+                .BeEquivalentTo(expectedAddressDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAddressByIdAsync(randomAddress.Id),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedAddressDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateAddressAsync(randomAddress),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

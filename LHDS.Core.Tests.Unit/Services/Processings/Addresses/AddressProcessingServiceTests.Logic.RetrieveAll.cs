b3ebd7958085cfd683ec8dffef1e5dbcc5db3440
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 15:41:46 +0100

    PROCESSINGS: Address Processing Service

---
+++ LHDS.Core.Tests.Unit/Services/Processings/Addresses/AddressProcessingServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,42 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Linq;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.Addresses;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.Addresses
+{
+    public partial class AddressProcessingServiceTests
+    {
+        [Fact]
+        public void ShouldRetrieveAllAddresses()
+        {
+            // given
+            IQueryable<Address> randomAddresses = CreateRandomAddresses();
+            IQueryable<Address> storageAddresses = randomAddresses;
+            IQueryable<Address> expectedAddresses = storageAddresses;
+
+            this.addressServiceMock.Setup(broker =>
+                broker.RetrieveAllAddresses())
+                    .Returns(storageAddresses);
+
+            // when
+            IQueryable<Address> actualAddresses =
+                this.addressProcessingService.RetrieveAllAddresses();
+
+            // then
+            actualAddresses.Should().BeEquivalentTo(expectedAddresses);
+
+            this.addressServiceMock.Verify(broker =>
+                broker.RetrieveAllAddresses(),
+                    Times.Once);
+
+            this.addressServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LHDS.Core.Tests.Unit/Services/Processings/Addresses/AddressProcessingServiceTests.cs
@@ -0,0 +1,104 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Foundations.Addresses.Exceptions;
+using LHDS.Core.Services.Foundations.Addresses;
+using LHDS.Core.Services.Processings.Addresses;
+using Moq;
+using Tynamix.ObjectFiller;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.Addresses
+{
+    public partial class AddressProcessingServiceTests
+    {
+        private readonly Mock<IAddressService> addressServiceMock = new Mock<IAddressService>();
+        private readonly Mock<ILoggingBroker> loggingBrokerMock = new Mock<ILoggingBroker>();
+        private readonly IAddressProcessingService addressProcessingService;
+
+        public AddressProcessingServiceTests()
+        {
+            addressProcessingService = new AddressProcessingService(
+                addressService: addressServiceMock.Object,
+                loggingBroker: loggingBrokerMock.Object);
+        }
+
+        public static TheoryData DependencyValidationExceptions()
+        {
+            string randomMessage = GetRandomString();
+            string exceptionMessage = randomMessage;
+            var innerException = new Xeption(exceptionMessage);
+
+            return new TheoryData<Xeption>
+            {
+                new AddressValidationException(
+                    message: "Address validation errors occurred, please try again.", innerException),
+
+                new AddressDependencyValidationException(
+                    message: "Address dependency validation occurred, please try again.", innerException)
+            };
+        }
+
+        public static TheoryData DependencyExceptions()
+        {
+            string randomMessage = GetRandomString();
+            string exceptionMessage = randomMessage;
+            var innerException = new Xeption(exceptionMessage);
+
+            return new TheoryData<Xeption>
+            {
+                new AddressDependencyException(
+                    message: "Address validation errors occurred, please try again.", innerException),
+
+                new AddressServiceException(
+                    message : "Address service error occurred, contact support.", innerException)
+            };
+        }
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+
+        private static string GetRandomString(int length) =>
+            new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Address CreateRandomAddress() =>
+            CreateAddressFiller(dateTimeOffset: GetRandomDateTimeOffset()).Create();
+
+        private static IQueryable<Address> CreateRandomAddresses()
+        {
+            return CreateAddressFiller(dateTimeOffset: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber())
+                    .AsQueryable();
+        }
+
+        private static Filler<Address> CreateAddressFiller(DateTimeOffset dateTimeOffset)
+        {
+            string user = GetRandomString(length: 255).ToString();
+            var filler = new Filler<Address>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(address => address.CreatedBy).Use(user)
+                .OnProperty(address => address.UpdatedBy).Use(user);
+
+            return filler;
+        }
+    }
+}

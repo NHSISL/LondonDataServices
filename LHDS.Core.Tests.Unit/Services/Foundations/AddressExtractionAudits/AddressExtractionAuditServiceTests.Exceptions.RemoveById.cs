bf383e34036cd157a917e5565f45f98212ddb557
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 10:25:54 +0100

    ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Foundations/AddressExtractionAudits/AddressExtractionAuditServiceTests.Exceptions.RemoveById.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/AddressExtractionAudits/AddressExtractionAuditServiceTests.Exceptions.RemoveById.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using LHDS.Core.Models.Foundations.AddressExtractionAudits;
 using LHDS.Core.Models.Foundations.AddressExtractionAudits.Exceptions;
@@ -58,6 +60,59 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.AddressExtractionAudits
 
             this.dateTimeBrokerMock.Verify(broker =>
                 broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someAddressExtractionAuditId = Guid.NewGuid();
+
+            var databaseUpdateConcurrencyException =
+                new DbUpdateConcurrencyException();
+
+            var lockedAddressExtractionAuditException =
+                new LockedAddressExtractionAuditException(
+                    message: "Locked addressExtractionAudit record exception, please try again later",
+                    innerException: databaseUpdateConcurrencyException);
+
+            var expectedAddressExtractionAuditDependencyValidationException =
+                new AddressExtractionAuditDependencyValidationException(
+                    message: "AddressExtractionAudit dependency validation occurred, please try again.",
+                    innerException: lockedAddressExtractionAuditException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAddressExtractionAuditByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(databaseUpdateConcurrencyException);
+
+            // when
+            ValueTask<AddressExtractionAudit> removeAddressExtractionAuditByIdTask =
+                this.addressExtractionAuditService.RemoveAddressExtractionAuditByIdAsync(someAddressExtractionAuditId);
+
+            AddressExtractionAuditDependencyValidationException actualAddressExtractionAuditDependencyValidationException =
+                await Assert.ThrowsAsync<AddressExtractionAuditDependencyValidationException>(
+                    removeAddressExtractionAuditByIdTask.AsTask);
+
+            // then
+            actualAddressExtractionAuditDependencyValidationException.Should()
+                .BeEquivalentTo(expectedAddressExtractionAuditDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAddressExtractionAuditByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAddressExtractionAuditDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteAddressExtractionAuditAsync(It.IsAny<AddressExtractionAudit>()),
                     Times.Never);
 
             this.storageBrokerMock.VerifyNoOtherCalls();

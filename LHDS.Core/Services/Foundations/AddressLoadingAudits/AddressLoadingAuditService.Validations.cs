fe7685e6bcb090012e55dfd01c698017a846d986
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 09:52:57 +0100

    ShouldThrowValidationExceptionOnModifyIfAddressLoadingAuditIsNullAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/AddressLoadingAudits/AddressLoadingAuditService.Validations.cs
+++ LHDS.Core/Services/Foundations/AddressLoadingAudits/AddressLoadingAuditService.Validations.cs
@@ -35,6 +35,11 @@ namespace LHDS.Core.Services.Foundations.AddressLoadingAudits
                 (Rule: IsNotRecent(addressLoadingAudit.CreatedDate), Parameter: nameof(AddressLoadingAudit.CreatedDate)));
         }
 
+        private void ValidateAddressLoadingAuditOnModify(AddressLoadingAudit addressLoadingAudit)
+        {
+            ValidateAddressLoadingAuditIsNotNull(addressLoadingAudit);
+        }
+
         public void ValidateAddressLoadingAuditId(Guid addressLoadingAuditId) =>
             Validate((Rule: IsInvalid(addressLoadingAuditId), Parameter: nameof(AddressLoadingAudit.Id)));
 
@@ -89,7 +94,7 @@ namespace LHDS.Core.Services.Foundations.AddressLoadingAudits
                 Condition = firstId != secondId,
                 Message = $"Id is not the same as {secondIdName}"
             };
-
+        
         private static dynamic IsNotSame(
            string first,
            string second,

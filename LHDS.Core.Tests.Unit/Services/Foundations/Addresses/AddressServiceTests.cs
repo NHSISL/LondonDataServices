4d1293bb6e92b85b37b0fb300af5a44125742895
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 09:32:20 +0100

    ShouldReturnAddress -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/Addresses/AddressServiceTests.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Addresses/AddressServiceTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.Serialization;
 using Microsoft.Data.SqlClient;
@@ -62,6 +63,13 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Addresses
 
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static IQueryable<Address> CreateRandomAddresses()
+        {
+            return CreateAddressFiller(dateTimeOffset: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber())
+                    .AsQueryable();
+        }
 
         private static Address CreateRandomAddress() =>
             CreateAddressFiller(dateTimeOffset: GetRandomDateTimeOffset()).Create();
--- LHDS.Core/Services/Foundations/Addresses/AddressService.cs
+++ LHDS.Core/Services/Foundations/Addresses/AddressService.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.DateTimes;
 using LHDS.Core.Brokers.Loggings;
@@ -29,5 +30,8 @@ namespace LHDS.Core.Services.Foundations.Addresses
 
                 return await this.storageBroker.InsertAddressAsync(address);
             });
+
+        public IQueryable<Address> RetrieveAllAddresses() =>
+            throw new System.NotImplementedException();
     }
 }

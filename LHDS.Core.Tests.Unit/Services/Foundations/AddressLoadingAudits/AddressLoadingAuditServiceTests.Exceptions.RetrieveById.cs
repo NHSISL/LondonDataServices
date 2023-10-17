bd356a8ee788173063615e97eb129d3f1a6d553a
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 09:52:43 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/AddressLoadingAudits/AddressLoadingAuditServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using LHDS.Core.Models.Foundations.AddressLoadingAudits;
+using LHDS.Core.Models.Foundations.AddressLoadingAudits.Exceptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.AddressLoadingAudits
+{
+    public partial class AddressLoadingAuditServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedAddressLoadingAuditStorageException =
+                new FailedAddressLoadingAuditStorageException(
+                    message: "Failed addressLoadingAudit storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedAddressLoadingAuditDependencyException =
+                new AddressLoadingAuditDependencyException(
+                    message: "AddressLoadingAudit dependency error occurred, contact support.",
+                    innerException: failedAddressLoadingAuditStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAddressLoadingAuditByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<AddressLoadingAudit> retrieveAddressLoadingAuditByIdTask =
+                this.addressLoadingAuditService.RetrieveAddressLoadingAuditByIdAsync(someId);
+
+            AddressLoadingAuditDependencyException actualAddressLoadingAuditDependencyException =
+                await Assert.ThrowsAsync<AddressLoadingAuditDependencyException>(
+                    retrieveAddressLoadingAuditByIdTask.AsTask);
+
+            // then
+            actualAddressLoadingAuditDependencyException.Should()
+                .BeEquivalentTo(expectedAddressLoadingAuditDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAddressLoadingAuditByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedAddressLoadingAuditDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

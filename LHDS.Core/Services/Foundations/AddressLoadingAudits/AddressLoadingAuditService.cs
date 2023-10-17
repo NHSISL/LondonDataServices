a60e0f9b7bb889578fa330eaa7a76411bb2a0948
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 09:52:37 +0100

    ShouldRetrieveAddressLoadingAuditByIdAsync -> FAIL

--- LHDS.Core/Services/Foundations/AddressLoadingAudits/AddressLoadingAuditService.cs
+++ LHDS.Core/Services/Foundations/AddressLoadingAudits/AddressLoadingAuditService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.DateTimes;
@@ -33,5 +34,8 @@ namespace LHDS.Core.Services.Foundations.AddressLoadingAudits
 
         public IQueryable<AddressLoadingAudit> RetrieveAllAddressLoadingAudits() =>
             TryCatch(() => this.storageBroker.SelectAllAddressLoadingAudits());
+
+        public ValueTask<AddressLoadingAudit> RetrieveAddressLoadingAuditByIdAsync(Guid addressLoadingAuditId) =>
+            throw new NotImplementedException();
     }
 }

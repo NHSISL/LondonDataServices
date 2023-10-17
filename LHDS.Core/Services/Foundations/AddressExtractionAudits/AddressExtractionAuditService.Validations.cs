2b7368142efbd85d23af52e82f2dc00d131a9855
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 10:24:11 +0100

    ShouldThrowValidationExceptionOnAddIfAddressExtractionAuditIsInvalidAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/AddressExtractionAudits/AddressExtractionAuditService.Validations.cs
+++ LHDS.Core/Services/Foundations/AddressExtractionAudits/AddressExtractionAuditService.Validations.cs
@@ -1,3 +1,4 @@
+using System;
 using LHDS.Core.Models.Foundations.AddressExtractionAudits;
 using LHDS.Core.Models.Foundations.AddressExtractionAudits.Exceptions;
 
@@ -8,6 +9,16 @@ namespace LHDS.Core.Services.Foundations.AddressExtractionAudits
         private void ValidateAddressExtractionAuditOnAdd(AddressExtractionAudit addressExtractionAudit)
         {
             ValidateAddressExtractionAuditIsNotNull(addressExtractionAudit);
+
+            Validate(
+                (Rule: IsInvalid(addressExtractionAudit.Id), Parameter: nameof(AddressExtractionAudit.Id)),
+
+                // TODO: Add any other required validation rules
+
+                (Rule: IsInvalid(addressExtractionAudit.CreatedDate), Parameter: nameof(AddressExtractionAudit.CreatedDate)),
+                (Rule: IsInvalid(addressExtractionAudit.CreatedBy), Parameter: nameof(AddressExtractionAudit.CreatedBy)),
+                (Rule: IsInvalid(addressExtractionAudit.UpdatedDate), Parameter: nameof(AddressExtractionAudit.UpdatedDate)),
+                (Rule: IsInvalid(addressExtractionAudit.UpdatedBy), Parameter: nameof(AddressExtractionAudit.UpdatedBy)));
         }
 
         private static void ValidateAddressExtractionAuditIsNotNull(AddressExtractionAudit addressExtractionAudit)
@@ -16,6 +27,43 @@ namespace LHDS.Core.Services.Foundations.AddressExtractionAudits
             {
                 throw new NullAddressExtractionAuditException(message: "AddressExtractionAudit is null.");
             }
+        }
+
+        private static dynamic IsInvalid(Guid id) => new
+        {
+            Condition = id == Guid.Empty,
+            Message = "Id is required"
+        };
+
+        private static dynamic IsInvalid(string text) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(text),
+            Message = "Text is required"
+        };
+
+        private static dynamic IsInvalid(DateTimeOffset date) => new
+        {
+            Condition = date == default,
+            Message = "Date is required"
+        };
+
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidAddressExtractionAuditException = 
+                new InvalidAddressExtractionAuditException(
+                    message: "Invalid addressExtractionAudit. Please correct the errors and try again.");
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidAddressExtractionAuditException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidAddressExtractionAuditException.ThrowIfContainsErrors();
         }
     }
 }

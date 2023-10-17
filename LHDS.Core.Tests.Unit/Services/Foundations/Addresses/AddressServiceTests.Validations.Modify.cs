0a091615db23386b32557571a3912ecf7b9e007a
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 09:32:58 +0100

    ShouldThrowValidationExceptionOnModifyIfAddressIsInvalidAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/Addresses/AddressServiceTests.Validations.Modify.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Addresses/AddressServiceTests.Validations.Modify.cs
@@ -50,5 +50,77 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Addresses
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfAddressIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given 
+            var invalidAddress = new Address
+            {
+                // TODO:  Add default values for your properties i.e. Name = invalidText
+            };
+
+            var invalidAddressException = 
+                new InvalidAddressException(
+                        message: "Invalid address. Please correct the errors and try again.");
+
+            invalidAddressException.AddData(
+                key: nameof(Address.Id),
+                values: "Id is required");
+
+            //invalidAddressException.AddData(
+            //    key: nameof(Address.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the Address model
+
+            invalidAddressException.AddData(
+                key: nameof(Address.CreatedDate),
+                values: "Date is required");
+
+            invalidAddressException.AddData(
+                key: nameof(Address.CreatedBy),
+                values: "Text is required");
+
+            invalidAddressException.AddData(
+                key: nameof(Address.UpdatedDate),
+                values: "Date is required");
+
+            invalidAddressException.AddData(
+                key: nameof(Address.UpdatedBy),
+                values: "Text is required");
+
+            var expectedAddressValidationException =
+                new AddressValidationException(
+                    message: "Address validation errors occurred, please try again.",
+                    innerException: invalidAddressException);
+
+            // when
+            ValueTask<Address> modifyAddressTask =
+                this.addressService.ModifyAddressAsync(invalidAddress);
+
+            AddressValidationException actualAddressValidationException =
+                await Assert.ThrowsAsync<AddressValidationException>(
+                    modifyAddressTask.AsTask);
+
+            //then
+            actualAddressValidationException.Should().BeEquivalentTo(expectedAddressValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAddressValidationException))),
+                        Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateAddressAsync(It.IsAny<Address>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

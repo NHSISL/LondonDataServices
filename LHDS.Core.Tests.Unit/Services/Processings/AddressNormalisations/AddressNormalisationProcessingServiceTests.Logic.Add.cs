6dbbfce555812fe5e2932558fd8f77f079f072a7
Hassan Raza <h_raza110@outlook.com>
2023-10-17 16:03:51 +0100

    ShouldGetNormalisedAddress -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/AddressNormalisations/AddressNormalisationProcessingServiceTests.Logic.Add.cs
@@ -0,0 +1,48 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.AddressNormalisation;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.AddressNormalisations
+{
+    public partial class AddressNormalisationProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldGetNormalisedAddress()
+        {
+            // Given
+            var randomAddress = GetRandomString();
+            string inputAddress = randomAddress;
+
+            AddressNormalisation addressNormalisation = new AddressNormalisation
+            {
+                PostalAddress = GetRandomString(),
+                JsonPostalAddress = GetRandomString()
+            };
+
+            this.addressNormalisationServiceMock.Setup(service =>
+                service.GetNormalisedAddress(inputAddress)).ReturnsAsync(addressNormalisation);
+
+            var expectedNormalisedAddress = addressNormalisation;
+
+            // When
+            AddressNormalisation actualNormalisedAddress = 
+                await this.addressNormalisationProcessingService.GetNormalisedAddress(inputAddress);
+
+            // Then
+            actualNormalisedAddress.Should().BeEquivalentTo(expectedNormalisedAddress);
+
+            this.addressNormalisationServiceMock.Verify(service =>
+                service.GetNormalisedAddress(It.IsAny<string>()),
+                    Times.Once);
+
+            this.addressNormalisationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LHDS.Core.Tests.Unit/Services/Processings/AddressNormalisations/AddressNormalisationProcessingServiceTests.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Services.Foundations.AddressNormalisations;
+using LHDS.Core.Services.Processings.AddressNormalisations;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.AddressNormalisations
+{
+    public partial class AddressNormalisationProcessingServiceTests
+    {
+        private readonly Mock<IAddressNormalisationService> addressNormalisationServiceMock =
+            new Mock<IAddressNormalisationService>();
+
+        private readonly Mock<ILoggingBroker> loggingBrokerMock = new Mock<ILoggingBroker>();
+        private readonly IAddressNormalisationProcessingService addressNormalisationProcessingService;
+
+        public AddressNormalisationProcessingServiceTests()
+        {
+            this.addressNormalisationProcessingService = new AddressNormalisationProcessingService(
+                this.addressNormalisationServiceMock.Object,
+                this.loggingBrokerMock.Object);
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+    }
+}

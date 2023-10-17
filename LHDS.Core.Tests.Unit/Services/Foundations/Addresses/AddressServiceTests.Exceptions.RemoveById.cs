4a666fb9afefae53e04b94c4d126b98e66e1669d
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 09:33:35 +0100

    ShouldThrowDependencyExceptionOnRemoveWhenSqlExceptionOccursAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Foundations/Addresses/AddressServiceTests.Exceptions.RemoveById.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Addresses/AddressServiceTests.Exceptions.RemoveById.cs
@@ -119,5 +119,52 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Addresses
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRemoveWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid someAddressId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedAddressStorageException =
+                new FailedAddressStorageException(
+                    message: "Failed address storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedAddressDependencyException =
+                new AddressDependencyException(
+                    message: "Address dependency error occurred, contact support.",
+                    innerException: failedAddressStorageException); 
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAddressByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Address> deleteAddressTask =
+                this.addressService.RemoveAddressByIdAsync(someAddressId);
+
+            AddressDependencyException actualAddressDependencyException =
+                await Assert.ThrowsAsync<AddressDependencyException>(
+                    deleteAddressTask.AsTask);
+
+            // then
+            actualAddressDependencyException.Should()
+                .BeEquivalentTo(expectedAddressDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAddressByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedAddressDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

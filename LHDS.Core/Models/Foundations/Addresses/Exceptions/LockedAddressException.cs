376d69495a78d480e0af9d7599ec249d99bdb5fd
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 09:33:15 +0100

    ShouldThrowDependencyValidationExceptionOnModifyIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/Addresses/Exceptions/LockedAddressException.cs
@@ -0,0 +1,12 @@
+using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.Addresses.Exceptions
+{
+    public class LockedAddressException : Xeption
+    {
+        public LockedAddressException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}

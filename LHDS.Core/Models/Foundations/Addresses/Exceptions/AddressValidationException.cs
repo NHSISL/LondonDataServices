88e13f473f48aedaeb6cabb4793cc5fbcc9ed02e
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 09:31:48 +0100

    ShouldThrowValidationExceptionOnAddIfAddressIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/Addresses/Exceptions/AddressValidationException.cs
@@ -0,0 +1,11 @@
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.Addresses.Exceptions
+{
+    public class AddressValidationException : Xeption
+    {
+        public AddressValidationException(string message, Xeption innerException)
+            : base(message,innerException)
+        { }
+    }
+}

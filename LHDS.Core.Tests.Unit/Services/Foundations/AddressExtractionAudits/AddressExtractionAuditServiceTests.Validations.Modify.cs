99e38a1a5634e1f12b86fc4b72c701a09e4436c6
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 10:25:15 +0100

    ShouldThrowValidationExceptionOnModifyIfAddressExtractionAuditIsInvalidAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/AddressExtractionAudits/AddressExtractionAuditServiceTests.Validations.Modify.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/AddressExtractionAudits/AddressExtractionAuditServiceTests.Validations.Modify.cs
@@ -50,5 +50,77 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.AddressExtractionAudits
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfAddressExtractionAuditIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given 
+            var invalidAddressExtractionAudit = new AddressExtractionAudit
+            {
+                // TODO:  Add default values for your properties i.e. Name = invalidText
+            };
+
+            var invalidAddressExtractionAuditException = 
+                new InvalidAddressExtractionAuditException(
+                        message: "Invalid addressExtractionAudit. Please correct the errors and try again.");
+
+            invalidAddressExtractionAuditException.AddData(
+                key: nameof(AddressExtractionAudit.Id),
+                values: "Id is required");
+
+            //invalidAddressExtractionAuditException.AddData(
+            //    key: nameof(AddressExtractionAudit.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the AddressExtractionAudit model
+
+            invalidAddressExtractionAuditException.AddData(
+                key: nameof(AddressExtractionAudit.CreatedDate),
+                values: "Date is required");
+
+            invalidAddressExtractionAuditException.AddData(
+                key: nameof(AddressExtractionAudit.CreatedBy),
+                values: "Text is required");
+
+            invalidAddressExtractionAuditException.AddData(
+                key: nameof(AddressExtractionAudit.UpdatedDate),
+                values: "Date is required");
+
+            invalidAddressExtractionAuditException.AddData(
+                key: nameof(AddressExtractionAudit.UpdatedBy),
+                values: "Text is required");
+
+            var expectedAddressExtractionAuditValidationException =
+                new AddressExtractionAuditValidationException(
+                    message: "AddressExtractionAudit validation errors occurred, please try again.",
+                    innerException: invalidAddressExtractionAuditException);
+
+            // when
+            ValueTask<AddressExtractionAudit> modifyAddressExtractionAuditTask =
+                this.addressExtractionAuditService.ModifyAddressExtractionAuditAsync(invalidAddressExtractionAudit);
+
+            AddressExtractionAuditValidationException actualAddressExtractionAuditValidationException =
+                await Assert.ThrowsAsync<AddressExtractionAuditValidationException>(
+                    modifyAddressExtractionAuditTask.AsTask);
+
+            //then
+            actualAddressExtractionAuditValidationException.Should().BeEquivalentTo(expectedAddressExtractionAuditValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAddressExtractionAuditValidationException))),
+                        Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateAddressExtractionAuditAsync(It.IsAny<AddressExtractionAudit>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

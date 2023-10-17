b1976a9546197a3206957c205bf273d3db4a27bc
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 09:52:41 +0100

    ShouldThrowNotFoundExceptionOnRetrieveByIdIfAddressLoadingAuditIsNotFoundAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/AddressLoadingAudits/AddressLoadingAuditServiceTests.Validations.RetrieveById.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/AddressLoadingAudits/AddressLoadingAuditServiceTests.Validations.RetrieveById.cs
@@ -54,5 +54,49 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.AddressLoadingAudits
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRetrieveByIdIfAddressLoadingAuditIsNotFoundAndLogItAsync()
+        {
+            //given
+            Guid someAddressLoadingAuditId = Guid.NewGuid();
+            AddressLoadingAudit noAddressLoadingAudit = null;
+
+            var notFoundAddressLoadingAuditException =
+                new NotFoundAddressLoadingAuditException(someAddressLoadingAuditId);
+
+            var expectedAddressLoadingAuditValidationException =
+                new AddressLoadingAuditValidationException(
+                    message: "AddressLoadingAudit validation errors occurred, please try again.",
+                    innerException: notFoundAddressLoadingAuditException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAddressLoadingAuditByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noAddressLoadingAudit);
+
+            //when
+            ValueTask<AddressLoadingAudit> retrieveAddressLoadingAuditByIdTask =
+                this.addressLoadingAuditService.RetrieveAddressLoadingAuditByIdAsync(someAddressLoadingAuditId);
+
+            AddressLoadingAuditValidationException actualAddressLoadingAuditValidationException =
+                await Assert.ThrowsAsync<AddressLoadingAuditValidationException>(
+                    retrieveAddressLoadingAuditByIdTask.AsTask);
+
+            //then
+            actualAddressLoadingAuditValidationException.Should().BeEquivalentTo(expectedAddressLoadingAuditValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAddressLoadingAuditByIdAsync(It.IsAny<Guid>()),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAddressLoadingAuditValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

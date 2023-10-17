51aa3061ad885d804c7fe2d9ac76c9f4c41fd98c
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 10:24:39 +0100

    ShouldReturnAddressExtractionAudit -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/AddressExtractionAudits/AddressExtractionAuditServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,38 @@
+using System.Linq;
+using FluentAssertions;
+using Moq;
+using LHDS.Core.Models.Foundations.AddressExtractionAudits;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.AddressExtractionAudits
+{
+    public partial class AddressExtractionAuditServiceTests
+    {
+        [Fact]
+        public void ShouldReturnAddressExtractionAudits()
+        {
+            // given
+            IQueryable<AddressExtractionAudit> randomAddressExtractionAudits = CreateRandomAddressExtractionAudits();
+            IQueryable<AddressExtractionAudit> storageAddressExtractionAudits = randomAddressExtractionAudits;
+            IQueryable<AddressExtractionAudit> expectedAddressExtractionAudits = storageAddressExtractionAudits;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllAddressExtractionAudits())
+                    .Returns(storageAddressExtractionAudits);
+
+            // when
+            IQueryable<AddressExtractionAudit> actualAddressExtractionAudits =
+                this.addressExtractionAuditService.RetrieveAllAddressExtractionAudits();
+
+            // then
+            actualAddressExtractionAudits.Should().BeEquivalentTo(expectedAddressExtractionAudits);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllAddressExtractionAudits(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- LHDS.Core.Tests.Unit/Services/Foundations/AddressExtractionAudits/AddressExtractionAuditServiceTests.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/AddressExtractionAudits/AddressExtractionAuditServiceTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.Serialization;
 using Microsoft.Data.SqlClient;
@@ -62,6 +63,13 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.AddressExtractionAudits
 
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static IQueryable<AddressExtractionAudit> CreateRandomAddressExtractionAudits()
+        {
+            return CreateAddressExtractionAuditFiller(dateTimeOffset: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber())
+                    .AsQueryable();
+        }
 
         private static AddressExtractionAudit CreateRandomAddressExtractionAudit() =>
             CreateAddressExtractionAuditFiller(dateTimeOffset: GetRandomDateTimeOffset()).Create();

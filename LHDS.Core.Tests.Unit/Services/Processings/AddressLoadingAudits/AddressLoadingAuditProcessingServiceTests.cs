440da1ff88673f6118a0f53b4f7952b4aec5c427
Hassan Raza <h_raza110@outlook.com>
2023-10-18 09:54:23 +0100

    ShouldAddAddressLoadingAuditAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/AddressLoadingAudits/AddressLoadingAuditProcessingServiceTests.cs
@@ -0,0 +1,55 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Models.Foundations.AddressLoadingAudits;
+using LHDS.Core.Services.Foundations.AddressLoadingAudits;
+using LHDS.Core.Services.Processings.AddressLoadingAudits;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.AddressLoadingAudits
+{
+    public partial class AddressLoadingAuditProcessingServiceTests
+    {
+        private readonly Mock<IAddressLoadingAuditService> addressLoadingAuditServiceMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IAddressLoadingAuditProcessingService addressLoadingAuditProcessingService;
+
+        public AddressLoadingAuditProcessingServiceTests()
+        {
+            this.addressLoadingAuditServiceMock = new Mock<IAddressLoadingAuditService>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.addressLoadingAuditProcessingService = new AddressLoadingAuditProcessingService(
+                addressLoadingAuditService: this.addressLoadingAuditServiceMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+
+        private static AddressLoadingAudit CreateRandomAddressLoadingAudit(DateTimeOffset dateTimeOffset) =>
+            CreateAddressLoadingAuditFiller(dateTimeOffset).Create();
+
+        private static Filler<AddressLoadingAudit> CreateAddressLoadingAuditFiller(DateTimeOffset dateTimeOffset)
+        {
+            string user = Guid.NewGuid().ToString();
+            var filler = new Filler<AddressLoadingAudit>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnProperty(addressLoadingAudit => addressLoadingAudit.CreatedBy).Use(user)
+                .OnProperty(addressLoadingAudit => addressLoadingAudit.UpdatedBy).Use(user);
+
+            return filler;
+        }
+    }
+}

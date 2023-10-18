5df7c2f0eaf9cf750650d426df4e2d98a572c1d2
Hassan Raza <h_raza110@outlook.com>
2023-10-18 11:18:01 +0100

    ShouldThrowValidationExceptionOnAddIfAddressLoadingAuditIsInvalidAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Processings/AddressLoadingAudits/AddressLoadingAuditServiceTests.Validations.Add.cs
+++ LHDS.Core.Tests.Unit/Services/Processings/AddressLoadingAudits/AddressLoadingAuditServiceTests.Validations.Add.cs
@@ -46,5 +46,72 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.AddressLoadingAudits
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfAddressLoadingAuditIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given
+            var invalidAddressLoadingAudit = new AddressLoadingAudit 
+            {
+                CreatedBy = invalidText,
+                UpdatedBy = invalidText
+            };
+
+            var invalidAddressLoadingAuditException =
+                new InvalidAddressLoadingAuditException(
+                    message: "Invalid address loading audit. Please correct the errors and try again.");
+
+            invalidAddressLoadingAuditException.AddData(
+                key: nameof(AddressLoadingAudit.Id),
+                values: "Id is required");
+
+            invalidAddressLoadingAuditException.AddData(
+                key: nameof(AddressLoadingAudit.CreatedDate),
+                values: "Date is required");
+
+            invalidAddressLoadingAuditException.AddData(
+                key: nameof(AddressLoadingAudit.CreatedBy),
+                values: "Text is required");
+
+            invalidAddressLoadingAuditException.AddData(
+                key: nameof(AddressLoadingAudit.UpdatedDate),
+                values: "Date is required");
+
+            invalidAddressLoadingAuditException.AddData(
+                key: nameof(AddressLoadingAudit.UpdatedBy),
+                values: "Text is required");
+
+            var expectedAddressLoadingAuditValidationException =
+                new AddressLoadingAuditValidationException(
+                    message: "Address loading audit validation errors occurred, please try again.",
+                    innerException: invalidAddressLoadingAuditException);
+
+            // when
+            ValueTask<AddressLoadingAudit> addAddressLoadingAuditTask =
+                this.addressLoadingAuditProcessingService.AddAddressLoadingAuditAsync(invalidAddressLoadingAudit);
+
+            AddressLoadingAuditValidationException actualAddressLoadingAuditValidationException =
+                await Assert.ThrowsAsync<AddressLoadingAuditValidationException>(() =>
+                    addAddressLoadingAuditTask.AsTask());
+
+            // then
+            actualAddressLoadingAuditValidationException.Should()
+                .BeEquivalentTo(expectedAddressLoadingAuditValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAddressLoadingAuditValidationException))),
+                        Times.Once);
+
+            this.addressLoadingAuditServiceMock.Verify(service =>
+                service.AddAddressLoadingAuditAsync(It.IsAny<AddressLoadingAudit>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.addressLoadingAuditServiceMock.VerifyNoOtherCalls();
+        }
     }
 }

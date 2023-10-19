058793f5fc3bc82aba638d8224503607da637f3f
Hassan Raza <h_raza110@outlook.com>
2023-10-19 14:40:10 +0100

    ShouldThrowValidationExceptionsOnProcessIfAddressListIsNullAndLogItAsync -> PASS

---
+++ LHDS.Core/Services/Orchestrations/AddressPersistances/AddressPersistanceOrchestrationService.Exceptions.cs
@@ -0,0 +1,41 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Orchestrations.AddressPersistances.Exceptions;
+using Xeptions;
+
+namespace LHDS.Core.Services.Orchestrations.AddressPersistances
+{
+    public partial class AddressPersistanceOrchestrationService
+    {
+        private delegate ValueTask<List<Address>> ReturningAddressListFunction();
+
+        private async ValueTask<List<Address>> TryCatch(ReturningAddressListFunction returningAddressListFunction)
+        {
+            try
+            {
+                return await returningAddressListFunction();
+            }
+            catch (InvalidArgumentAddressPersistanceOrchestrationException
+                invalidArgumentAddressPersistanceOrchestrationException)
+            {
+                throw CreateAndLogValidationException(invalidArgumentAddressPersistanceOrchestrationException);
+            }
+        }
+        private AddressPersistanceOrchestrationValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var addressPersistanceOrchestrationValidationException =
+                new AddressPersistanceOrchestrationValidationException(
+                    message: "Address persistance orchestration validation errors occured, please try again",
+                    innerException: exception);
+
+            this.loggingBroker.LogError(addressPersistanceOrchestrationValidationException);
+
+            return addressPersistanceOrchestrationValidationException;
+        }
+    }
+}

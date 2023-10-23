721972003dea47f37fc668c9f9833d384a11eda5
Hassan Raza <h_raza110@outlook.com>
2023-10-23 16:55:17 +0100

    ShouldProcessAddressCSVAsync -> PASS

--- LHDS.Core/Services/Foundations/AddressParser/AddressParserService.cs
+++ LHDS.Core/Services/Foundations/AddressParser/AddressParserService.cs
@@ -4,9 +4,11 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using LHDS.Core.Brokers.Loggings;
 using LHDS.Core.Models.Foundations.Addresses;
 using LHDS.Core.Services.Foundations.AddressParsers;
+using Org.BouncyCastle.Utilities;
 
 namespace LHDS.Core.Services.Foundations.Addresses
 {
@@ -20,7 +22,41 @@ namespace LHDS.Core.Services.Foundations.Addresses
             this.loggingBroker = loggingBroker;
         }
 
-        public List<Address> ProcessCSV(byte[] data) =>
-            throw new NotImplementedException();
+        public List<Address> ProcessCSV(byte[] data)
+        {
+            string stringData = data.ToString();
+            List<string> recods = stringData.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None).ToList();
+            List<Address> expectedAddresses = new List<Address>();
+
+            foreach (string record in recods)
+            {
+                if (record.StartsWith("28,"))
+                {
+                    string[] index = record.Split(",");
+
+                    Address address = new Address
+                    {
+                        Id = Guid.NewGuid(),
+                        UPRN = index[3],
+                        UPSN = index[4],
+                        OrganisationName = index[5],
+                        DepartmentName = index[6],
+                        SubBuildingName = index[7],
+                        BuildingName = index[8],
+                        BuildingNumber = index[9],
+                        DependentThoroughfare = index[10],
+                        Thoroughfare = index[12],
+                        DoubleDependentLocality = index[13],
+                        DependentLocality = index[14],
+                        PostTown = index[15],
+                        PostCode = index[16],
+                    };
+
+                    expectedAddresses.Add(address);
+                }
+            }
+
+            return expectedAddresses;
+        }
     }
 }

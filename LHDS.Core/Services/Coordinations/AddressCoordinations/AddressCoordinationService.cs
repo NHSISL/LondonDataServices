4a7b9fae3a8bfdaf4c45b3926ed2ed2737886426
Hassan Raza <h_raza110@outlook.com>
2023-10-25 16:24:29 +0100

    ShouldProcessDataAndLogAsync -> PASS

--- LHDS.Core/Services/Coordinations/AddressCoordinations/AddressCoordinationService.cs
+++ LHDS.Core/Services/Coordinations/AddressCoordinations/AddressCoordinationService.cs
@@ -2,8 +2,8 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
-using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using LHDS.Core.Brokers.Loggings;
 using LHDS.Core.Models.Foundations.Addresses;
 using LHDS.Core.Services.Orchestrations.AddressExtractions;
@@ -13,14 +13,25 @@ namespace LHDS.Core.Services.Coordinations.AddressCoordinations
 {
     public class AddressCoordinationService : IAddressCoordinationService
     {
+        private readonly IAddressExtractionOrchestrationService addressExtractionOrchestrationService;
+        private readonly IAddressPersistanceOrchestrationService addressPersistanceOrchestrationService;
+        private readonly ILoggingBroker loggingBroker;
+
         public AddressCoordinationService(
             IAddressExtractionOrchestrationService addressExtractionOrchestrationService,
             IAddressPersistanceOrchestrationService addressPersistanceOrchestrationService,
             ILoggingBroker loggingBroker)
         {
+            this.addressExtractionOrchestrationService = addressExtractionOrchestrationService;
+            this.addressPersistanceOrchestrationService = addressPersistanceOrchestrationService;
+            this.loggingBroker = loggingBroker;
         }
 
-        public List<Address> ProcessData(byte[] data) =>
-            throw new NotImplementedException();
+        public async ValueTask<List<Address>> ProcessData(byte[] data)
+        {
+            List<Address> extractedAddress = this.addressExtractionOrchestrationService.ProcessData(data);
+
+            return await this.addressPersistanceOrchestrationService.ProcessAsync(extractedAddress);
+        }
     }
 }

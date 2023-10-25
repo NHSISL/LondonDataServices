2d8c9dd4738e19997a28498bf237e2d6527dd884
Hassan Raza <h_raza110@outlook.com>
2023-10-25 11:53:31 +0100

    ShouldThrowValidationExceptionOnProcessDataIfDataIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Orchestrations/AddressExtractions/AddressExtractionOrchestrationValidationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Orchestrations.AddressExtractions.Exceptions
+{
+    public class AddressExtractionOrchestrationValidationException : Xeption
+    {
+        public AddressExtractionOrchestrationValidationException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}

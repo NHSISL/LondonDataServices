dc00223a6d050a29947133670a7739bcba13ada1
Hassan Raza <h_raza110@outlook.com>
2023-10-25 13:08:07 +0100

    ShouldThrowServiceExceptionOnAddressExtractionIfServiceErrorOccursAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Orchestrations/AddressExtractions/AddressExtractionOrchestrationTests.Exceptions.ProcessAsync.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/AddressExtractions/AddressExtractionOrchestrationTests.Exceptions.ProcessAsync.cs
@@ -2,6 +2,7 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using FluentAssertions;
 using LHDS.Core.Models.Foundations.Addresses;
 using LHDS.Core.Models.Foundations.AddressExtractionAudits;
 using LHDS.Core.Models.Orchestrations.AddressExtractions.Exceptions;
+using LHDS.Core.Models.Orchestrations.AddressPersistances.Exceptions;
 using Moq;
 using Xeptions;
 using Xunit;
@@ -109,6 +111,58 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.AddressExtractions
             this.loggingBrokerMock.Verify(broker =>
                broker.LogError(It.Is(SameExceptionAs(
                    expectedDependencyException))),
+                       Times.Once);
+
+            this.addressParserServiceMock.VerifyNoOtherCalls();
+            this.addressExtractionAuditServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        public async Task ShouldThrowServiceExceptionOnAddressExtractionIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            string inputFilePath = @"c:\temp\TestNestedZip.zip";
+            byte[] inputData = await File.ReadAllBytesAsync(inputFilePath);
+            var serviceException = new Exception();
+
+            var failedAddressPersistanceOrchestrationServiceException =
+                new FailedAddressExtractionOrchestrationServiceException(
+                    message: "Failed address extraction orchestration service occurred, please contact support",
+                    innerException: serviceException);
+
+            var expectedAddressExtractionOrchestrationServiceException =
+                new AddressExtractionOrchestrationServiceException(
+                    message: "Address extraction orchestration service error occurred, contact support.",
+                    innerException: failedAddressPersistanceOrchestrationServiceException);
+
+            this.addressParserServiceMock.Setup(service =>
+                service.ProcessCsvAsync(It.IsAny<byte[]>()))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<List<Address>> processDataTask =
+                this.addressExtractionOrchestrationService.ProcessDataAsync(inputData);
+
+            AddressExtractionOrchestrationServiceException actualException =
+                await Assert.ThrowsAsync<AddressExtractionOrchestrationServiceException>(
+                    processDataTask.AsTask);
+
+            // then
+            actualException.Should()
+                 .BeEquivalentTo(expectedAddressExtractionOrchestrationServiceException);
+
+            this.addressParserServiceMock.Verify(service =>
+             service.ProcessCsvAsync(It.IsAny<byte[]>()),
+                 Times.Once);
+
+            this.addressExtractionAuditServiceMock.Verify(service =>
+             service.AddAddressExtractionAuditAsync(It.IsAny<AddressExtractionAudit>()),
+                 Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(
+                   expectedAddressExtractionOrchestrationServiceException))),
                        Times.Once);
 
             this.addressParserServiceMock.VerifyNoOtherCalls();

8379c9d2e067e3534dd479758656d137699cbeb7
Hassan Raza <h_raza110@outlook.com>
2023-10-30 15:50:28 +0000

    CODE RUB: Correcting Relative Path Assembly

--- LHDS.Core.Tests.Unit/Services/Orchestrations/AddressExtractions/AddressExtractionOrchestrationTests.Logic.ProcessData.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/AddressExtractions/AddressExtractionOrchestrationTests.Logic.ProcessData.cs
@@ -2,11 +2,11 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
-using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
-using System.Text;
+using System.Reflection;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Force.DeepCloner;
@@ -23,63 +23,30 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.AddressExtractions
         public async Task ShouldProcessAddressesDataAndLogAsync()
         {
             // Given
-            string inputFilePath = @"./Resources/TestNestedZip.zip";
+            string assembly = Assembly.GetExecutingAssembly().Location;
+            string inputFilePath = Path.Combine(Path.GetDirectoryName(assembly), @"Resources/TestNestedZip.zip");
             byte[] inputData = await File.ReadAllBytesAsync(inputFilePath);
 
             List<string> unZippedInputFilePaths =
-                new List<string> { @"./Resources/TestCsv1.csv", @"./Resources/TestCsv2.csv" };
+                new List<string> {
+                    Path.Combine(Path.GetDirectoryName(assembly), @"./Resources/TestCsv1.csv"),
+                    Path.Combine(Path.GetDirectoryName(assembly), @"./Resources/TestCsv2.csv")
+                };
 
-            List<Address> extractedAddresses = new List<Address>();
+            List<Address> outputAddresses = new List<Address>();
 
             foreach (string filePath in unZippedInputFilePaths)
             {
-                List<Address> currentCsvAddresses = new List<Address>();
-
-                string randomCsvFormattedAddresses;
-
-                using (StreamReader reader = new StreamReader(filePath))
-                {
-                    randomCsvFormattedAddresses = reader.ReadToEnd();
-                }
-
-                string inputCsvFormattedAddresses = randomCsvFormattedAddresses;
-                byte[] byteAddressesCsv = Encoding.GetEncoding("UTF-8").GetBytes(inputCsvFormattedAddresses);
-
-                List<string> lines =
-                    inputCsvFormattedAddresses.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None).ToList();
-
-                foreach (string line in lines)
-                {
-                    string[] index = line.Split(",");
-
-                    Address address = new Address
-                    {
-                        Id = Guid.NewGuid(),
-                        UPRN = index[3],
-                        UPSN = index[4],
-                        OrganisationName = index[5],
-                        DepartmentName = index[6],
-                        SubBuildingName = index[7],
-                        BuildingName = index[8],
-                        BuildingNumber = index[9],
-                        DependentThoroughfare = index[10],
-                        Thoroughfare = index[11],
-                        DoubleDependentLocality = index[12],
-                        DependentLocality = index[13],
-                        PostTown = index[14],
-                        PostCode = index[15],
-                    };
-
-                    currentCsvAddresses.Add(address);
-                    extractedAddresses.Add(address);
-                }
+                List<Address> randomAddresses = CreateRandomAddresses().ToList();
+                outputAddresses.AddRange(randomAddresses);
+                byte[] csvData = await File.ReadAllBytesAsync(filePath);
 
                 this.addressParserServiceMock.Setup(service =>
-                    service.ProcessCsvAsync(byteAddressesCsv))
-                        .ReturnsAsync(currentCsvAddresses);
+                    service.ProcessCsvAsync(csvData))
+                        .ReturnsAsync(randomAddresses);
             }
 
-            List<Address> expectedAddresses = extractedAddresses.DeepClone();
+            List<Address> expectedAddresses = outputAddresses.DeepClone();
 
             // When
             List<Address> actualAddresses =

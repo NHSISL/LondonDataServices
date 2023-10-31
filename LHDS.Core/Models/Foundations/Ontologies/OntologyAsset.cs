cc5063c60c736b1afeeb2aa651fd6cbd689bddeb
Christo du Toit <christo@dutoit.co.uk>
2023-10-31 14:24:51 +0000

    ShouldRetrieveAllCodingSystemsByRelativeUrlAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/Ontologies/OntologyAsset.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+
+namespace LHDS.Core.Models.Foundations.Ontologies
+{
+    internal class OntologyAsset
+    {
+        public string FullUrl { get; set; }
+        public string ResourceType { get; set; }
+        public string Version { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Status { get; set; }
+        public DateTimeOffset LastUpdated { get; set; }
+    }
+}
---
+++ LHDS.Core/Services/Foundations/Ontologies/OntologyService.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Brokers.Ontologies;
+using LHDS.Core.Models.Foundations.Ontologies;
+
+namespace LHDS.Core.Services.Foundations.Ontologies
+{
+    internal class OntologyService : IOntologyService
+    {
+        private readonly IOntologyBroker ontologyBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public OntologyService(
+            IOntologyBroker ontologyBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.ontologyBroker = ontologyBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<OntologyAssets> RetrieveAllCodingSystemsAsync(string? relativeUrl) =>
+            throw new NotImplementedException();
+
+        public ValueTask<OntologyAssets> RetrieveAllConceptMapsAsync(string? relativeUrl) =>
+            throw new NotImplementedException();
+
+        public ValueTask<OntologyAssets> RetrieveAllValueSetsAsync(string? relativeUrl) =>
+            throw new NotImplementedException();
+    }
+}

a1d9ce8e8931c06acb9a4ce54c20861d7b4f91f6
Christo du Toit <christo@dutoit.co.uk>
2023-11-01 15:42:56 +0000

    ShouldThrowValidationExceptionOnModifyIfOntologyConceptMapIsInvalidAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/OntologyConceptMaps/OntologyConceptMapServiceTests.Validations.Modify.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/OntologyConceptMaps/OntologyConceptMapServiceTests.Validations.Modify.cs
@@ -50,5 +50,77 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.OntologyConceptMaps
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfOntologyConceptMapIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given 
+            var invalidOntologyConceptMap = new OntologyConceptMap
+            {
+                // TODO:  Add default values for your properties i.e. Name = invalidText
+            };
+
+            var invalidOntologyConceptMapException = 
+                new InvalidOntologyConceptMapException(
+                        message: "Invalid ontologyConceptMap. Please correct the errors and try again.");
+
+            invalidOntologyConceptMapException.AddData(
+                key: nameof(OntologyConceptMap.Id),
+                values: "Id is required");
+
+            //invalidOntologyConceptMapException.AddData(
+            //    key: nameof(OntologyConceptMap.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the OntologyConceptMap model
+
+            invalidOntologyConceptMapException.AddData(
+                key: nameof(OntologyConceptMap.CreatedDate),
+                values: "Date is required");
+
+            invalidOntologyConceptMapException.AddData(
+                key: nameof(OntologyConceptMap.CreatedBy),
+                values: "Text is required");
+
+            invalidOntologyConceptMapException.AddData(
+                key: nameof(OntologyConceptMap.UpdatedDate),
+                values: "Date is required");
+
+            invalidOntologyConceptMapException.AddData(
+                key: nameof(OntologyConceptMap.UpdatedBy),
+                values: "Text is required");
+
+            var expectedOntologyConceptMapValidationException =
+                new OntologyConceptMapValidationException(
+                    message: "OntologyConceptMap validation errors occurred, please try again.",
+                    innerException: invalidOntologyConceptMapException);
+
+            // when
+            ValueTask<OntologyConceptMap> modifyOntologyConceptMapTask =
+                this.ontologyConceptMapService.ModifyOntologyConceptMapAsync(invalidOntologyConceptMap);
+
+            OntologyConceptMapValidationException actualOntologyConceptMapValidationException =
+                await Assert.ThrowsAsync<OntologyConceptMapValidationException>(
+                    modifyOntologyConceptMapTask.AsTask);
+
+            //then
+            actualOntologyConceptMapValidationException.Should().BeEquivalentTo(expectedOntologyConceptMapValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedOntologyConceptMapValidationException))),
+                        Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateOntologyConceptMapAsync(It.IsAny<OntologyConceptMap>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

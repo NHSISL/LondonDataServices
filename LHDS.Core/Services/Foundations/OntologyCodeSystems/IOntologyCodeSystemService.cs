8484c104beff4f74fd62b76cd1492f9c060eeb73
Christo du Toit <christo@dutoit.co.uk>
2023-11-01 13:38:40 +0000

    ShouldRetrieveOntologyCodeSystemByIdAsync -> FAIL

--- LHDS.Core/Services/Foundations/OntologyCodeSystems/IOntologyCodeSystemService.cs
+++ LHDS.Core/Services/Foundations/OntologyCodeSystems/IOntologyCodeSystemService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Models.Foundations.OntologyCodeSystems;
@@ -8,5 +9,6 @@ namespace LHDS.Core.Services.Foundations.OntologyCodeSystems
     {
         ValueTask<OntologyCodeSystem> AddOntologyCodeSystemAsync(OntologyCodeSystem ontologyCodeSystem);
         IQueryable<OntologyCodeSystem> RetrieveAllOntologyCodeSystems();
+        ValueTask<OntologyCodeSystem> RetrieveOntologyCodeSystemByIdAsync(Guid ontologyCodeSystemId);
     }
 }

de4b2c13d0cf91b552476cf4f512469fbba93996
Christo du Toit <christo@dutoit.co.uk>
2023-11-01 13:37:55 +0000

    ShouldAddOntologyCodeSystemAsync -> FAIL

---
+++ LHDS.Core/Services/Foundations/OntologyCodeSystems/OntologyCodeSystemService.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Brokers.Storages.Sql;
+using LHDS.Core.Models.Foundations.OntologyCodeSystems;
+
+namespace LHDS.Core.Services.Foundations.OntologyCodeSystems
+{
+    public partial class OntologyCodeSystemService : IOntologyCodeSystemService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public OntologyCodeSystemService(
+            IStorageBroker storageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<OntologyCodeSystem> AddOntologyCodeSystemAsync(OntologyCodeSystem ontologyCodeSystem) =>
+            throw new System.NotImplementedException();
+    }
+}

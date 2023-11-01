40b85de94426adc214e950cf1ef7b673fbd737dd
Christo du Toit <christo@dutoit.co.uk>
2023-11-01 15:42:12 +0000

    ShouldThrowDependencyValidationExceptionOnAddIfOntologyConceptMapAlreadyExsitsAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/OntologyConceptMaps/OntologyConceptMapServiceTests.Exceptions.Add.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/OntologyConceptMaps/OntologyConceptMapServiceTests.Exceptions.Add.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
 using Moq;
@@ -25,7 +26,7 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.OntologyConceptMaps
             var expectedOntologyConceptMapDependencyException =
                 new OntologyConceptMapDependencyException(
                     message: "OntologyConceptMap dependency error occurred, contact support.",
-                    innerException: failedOntologyConceptMapStorageException);
+                    innerException: failedOntologyConceptMapStorageException);             
 
             this.dateTimeBrokerMock.Setup(broker =>
                 broker.GetCurrentDateTimeOffset())
@@ -54,6 +55,61 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.OntologyConceptMaps
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(
                     expectedOntologyConceptMapDependencyException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnAddIfOntologyConceptMapAlreadyExsitsAndLogItAsync()
+        {
+            // given
+            OntologyConceptMap randomOntologyConceptMap = CreateRandomOntologyConceptMap();
+            OntologyConceptMap alreadyExistsOntologyConceptMap = randomOntologyConceptMap;
+            string randomMessage = GetRandomString();
+
+            var duplicateKeyException =
+                new DuplicateKeyException(randomMessage);
+
+            var alreadyExistsOntologyConceptMapException =
+                new AlreadyExistsOntologyConceptMapException(
+                    message: "OntologyConceptMap with the same Id already exists.",
+                    innerException: duplicateKeyException);
+
+            var expectedOntologyConceptMapDependencyValidationException =
+                new OntologyConceptMapDependencyValidationException(
+                    message: "OntologyConceptMap dependency validation occurred, please try again.",
+                    innerException: alreadyExistsOntologyConceptMapException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(duplicateKeyException);
+
+            // when
+            ValueTask<OntologyConceptMap> addOntologyConceptMapTask =
+                this.ontologyConceptMapService.AddOntologyConceptMapAsync(alreadyExistsOntologyConceptMap);
+
+            // then
+            OntologyConceptMapDependencyValidationException actualOntologyConceptMapDependencyValidationException =
+                await Assert.ThrowsAsync<OntologyConceptMapDependencyValidationException>(
+                    addOntologyConceptMapTask.AsTask);
+
+            actualOntologyConceptMapDependencyValidationException.Should()
+                .BeEquivalentTo(expectedOntologyConceptMapDependencyValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertOntologyConceptMapAsync(It.IsAny<OntologyConceptMap>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedOntologyConceptMapDependencyValidationException))),
                         Times.Once);
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();

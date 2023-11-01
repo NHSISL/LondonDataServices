c156d8463ff20fbb322fa0571064982b6fa2d7ca
Christo du Toit <christo@dutoit.co.uk>
2023-11-01 10:46:09 +0000

    ShouldThrowServiceExceptionOnRetrieveAllCodingSystemsIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/Ontologies/OntologyServiceTests.RetrieveAllCodingSystems.Exceptions.cs
@@ -0,0 +1,63 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.Ontologies;
+using LHDS.Core.Models.Foundations.Suppliers.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Ontologys
+{
+    public partial class OntologyServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveAllCodingSystemsIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            string someRelativeUrl = GetRandomString();
+            var serviceException = new Exception();
+
+            var failedOntologyServiceException =
+                new FailedOntologyServiceException(
+                    message: "Failed ontology service occurred, please contact support",
+                    innerException: serviceException);
+
+            var expectedOntologyServiceException =
+                new OntologyServiceException(
+                    message: "Ontology service error occurred, contact support.",
+                    innerException: failedOntologyServiceException);
+
+            this.ontologyBrokerMock.Setup(broker =>
+                broker.GetAllCodingSystemsAsync(It.IsAny<string>()))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<OntologyAssets> retrieveOntologyByIdTask =
+                this.ontologyService.RetrieveAllCodingSystemsAsync(someRelativeUrl);
+
+            OntologyServiceException actualOntologyServiceException =
+                await Assert.ThrowsAsync<OntologyServiceException>(
+                    retrieveOntologyByIdTask.AsTask);
+
+            // then
+            actualOntologyServiceException.Should()
+                .BeEquivalentTo(expectedOntologyServiceException);
+
+            this.ontologyBrokerMock.Verify(broker =>
+                broker.GetAllCodingSystemsAsync(It.IsAny<string>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(
+                   expectedOntologyServiceException))),
+                        Times.Once);
+
+            this.ontologyBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

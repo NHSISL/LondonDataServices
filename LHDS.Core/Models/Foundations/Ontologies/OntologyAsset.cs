d4fc7369b3502ae84a293551c418b92baed8e024
Christo du Toit <christo@dutoit.co.uk>
2023-11-01 10:55:40 +0000

    CODE RUB: Code Cleanup

--- LHDS.Core/Models/Foundations/Ontologies/OntologyAsset.cs
+++ LHDS.Core/Models/Foundations/Ontologies/OntologyAsset.cs
@@ -8,8 +8,8 @@ namespace LHDS.Core.Models.Foundations.Ontologies
 {
     internal class OntologyAsset
     {
-        public string? FullUrl { get; set; }
-        public string? ResourceType { get; set; }
+        public string FullUrl { get; set; }
+        public string ResourceType { get; set; }
         public string? Version { get; set; }
         public string? Name { get; set; }
         public string? Title { get; set; }

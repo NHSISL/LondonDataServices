1599ffe8855755fec41ec854dafa0437bc4a4060
Christo du Toit <christo@dutoit.co.uk>
2023-11-02 15:22:39 +0000

    ShouldRetrieveTerminologyPollByIdAsync -> FAIL

--- LHDS.Core/Services/Foundations/TerminologyPolls/ITerminologyPollService.cs
+++ LHDS.Core/Services/Foundations/TerminologyPolls/ITerminologyPollService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Models.Foundations.TerminologyPolls;
@@ -8,5 +9,6 @@ namespace LHDS.Core.Services.Foundations.TerminologyPolls
     {
         ValueTask<TerminologyPoll> AddTerminologyPollAsync(TerminologyPoll terminologyPoll);
         IQueryable<TerminologyPoll> RetrieveAllTerminologyPolls();
+        ValueTask<TerminologyPoll> RetrieveTerminologyPollByIdAsync(Guid terminologyPollId);
     }
 }

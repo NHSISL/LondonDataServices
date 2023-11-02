358265853d0f13460f8b52fb40f683d16bdfb45f
Christo du Toit <christo@dutoit.co.uk>
2023-11-02 15:23:15 +0000

    ShouldThrowDependencyExceptionOnRemoveWhenSqlExceptionOccursAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Foundations/TerminologyPolls/TerminologyPollServiceTests.Exceptions.RemoveById.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/TerminologyPolls/TerminologyPollServiceTests.Exceptions.RemoveById.cs
@@ -119,5 +119,52 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.TerminologyPolls
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRemoveWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid someTerminologyPollId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedTerminologyPollStorageException =
+                new FailedTerminologyPollStorageException(
+                    message: "Failed terminologyPoll storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedTerminologyPollDependencyException =
+                new TerminologyPollDependencyException(
+                    message: "TerminologyPoll dependency error occurred, contact support.",
+                    innerException: failedTerminologyPollStorageException); 
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectTerminologyPollByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<TerminologyPoll> deleteTerminologyPollTask =
+                this.terminologyPollService.RemoveTerminologyPollByIdAsync(someTerminologyPollId);
+
+            TerminologyPollDependencyException actualTerminologyPollDependencyException =
+                await Assert.ThrowsAsync<TerminologyPollDependencyException>(
+                    deleteTerminologyPollTask.AsTask);
+
+            // then
+            actualTerminologyPollDependencyException.Should()
+                .BeEquivalentTo(expectedTerminologyPollDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTerminologyPollByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedTerminologyPollDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

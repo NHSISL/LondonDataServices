b2d6c32cf9858ab4f171c5935db8cc137dd95909
Christo du Toit <christo@dutoit.co.uk>
2023-11-02 15:22:39 +0000

    ShouldRetrieveTerminologyPollByIdAsync -> FAIL

--- LHDS.Core/Services/Foundations/TerminologyPolls/TerminologyPollService.cs
+++ LHDS.Core/Services/Foundations/TerminologyPolls/TerminologyPollService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.DateTimes;
@@ -33,5 +34,8 @@ namespace LHDS.Core.Services.Foundations.TerminologyPolls
 
         public IQueryable<TerminologyPoll> RetrieveAllTerminologyPolls() =>
             TryCatch(() => this.storageBroker.SelectAllTerminologyPolls());
+
+        public ValueTask<TerminologyPoll> RetrieveTerminologyPollByIdAsync(Guid terminologyPollId) =>
+            throw new NotImplementedException();
     }
 }

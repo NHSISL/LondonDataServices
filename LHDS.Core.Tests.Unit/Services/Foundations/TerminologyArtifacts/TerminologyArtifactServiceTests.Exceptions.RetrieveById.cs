81732ad9588e21825cf5223cc002481523e33a21
Christo du Toit <christo@dutoit.co.uk>
2023-11-02 15:06:32 +0000

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/TerminologyArtifacts/TerminologyArtifactServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using LHDS.Core.Models.Foundations.TerminologyArtifacts;
+using LHDS.Core.Models.Foundations.TerminologyArtifacts.Exceptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.TerminologyArtifacts
+{
+    public partial class TerminologyArtifactServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedTerminologyArtifactStorageException =
+                new FailedTerminologyArtifactStorageException(
+                    message: "Failed terminologyArtifact storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedTerminologyArtifactDependencyException =
+                new TerminologyArtifactDependencyException(
+                    message: "TerminologyArtifact dependency error occurred, contact support.",
+                    innerException: failedTerminologyArtifactStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectTerminologyArtifactByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<TerminologyArtifact> retrieveTerminologyArtifactByIdTask =
+                this.terminologyArtifactService.RetrieveTerminologyArtifactByIdAsync(someId);
+
+            TerminologyArtifactDependencyException actualTerminologyArtifactDependencyException =
+                await Assert.ThrowsAsync<TerminologyArtifactDependencyException>(
+                    retrieveTerminologyArtifactByIdTask.AsTask);
+
+            // then
+            actualTerminologyArtifactDependencyException.Should()
+                .BeEquivalentTo(expectedTerminologyArtifactDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTerminologyArtifactByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedTerminologyArtifactDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

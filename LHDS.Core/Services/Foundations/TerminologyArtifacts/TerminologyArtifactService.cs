9139916c5987367bd627a88c914cf8448d3cab25
Christo du Toit <christo@dutoit.co.uk>
2023-11-02 15:06:28 +0000

    ShouldRetrieveTerminologyArtifactByIdAsync -> FAIL

--- LHDS.Core/Services/Foundations/TerminologyArtifacts/TerminologyArtifactService.cs
+++ LHDS.Core/Services/Foundations/TerminologyArtifacts/TerminologyArtifactService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.DateTimes;
@@ -33,5 +34,8 @@ namespace LHDS.Core.Services.Foundations.TerminologyArtifacts
 
         public IQueryable<TerminologyArtifact> RetrieveAllTerminologyArtifacts() =>
             TryCatch(() => this.storageBroker.SelectAllTerminologyArtifacts());
+
+        public ValueTask<TerminologyArtifact> RetrieveTerminologyArtifactByIdAsync(Guid terminologyArtifactId) =>
+            throw new NotImplementedException();
     }
 }

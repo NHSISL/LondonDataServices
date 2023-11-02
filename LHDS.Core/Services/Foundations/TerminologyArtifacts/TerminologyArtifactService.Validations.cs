a6e951fcab65cccd500e32f15dd9bbf55f84d0c1
Christo du Toit <christo@dutoit.co.uk>
2023-11-02 15:06:05 +0000

    ShouldThrowValidationExceptionOnAddIfCreateAndUpdateUsersIsNotSameAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/TerminologyArtifacts/TerminologyArtifactService.Validations.cs
+++ LHDS.Core/Services/Foundations/TerminologyArtifacts/TerminologyArtifactService.Validations.cs
@@ -24,7 +24,13 @@ namespace LHDS.Core.Services.Foundations.TerminologyArtifacts
                     firstDate: terminologyArtifact.UpdatedDate,
                     secondDate: terminologyArtifact.CreatedDate,
                     secondDateName: nameof(TerminologyArtifact.CreatedDate)),
-                Parameter: nameof(TerminologyArtifact.UpdatedDate)));
+                Parameter: nameof(TerminologyArtifact.UpdatedDate)),
+
+                (Rule: IsNotSame(
+                    firstId: terminologyArtifact.UpdatedBy,
+                    secondId: terminologyArtifact.CreatedBy,
+                    secondIdName: nameof(TerminologyArtifact.CreatedBy)),
+                Parameter: nameof(TerminologyArtifact.UpdatedBy)));
         }
 
         private static void ValidateTerminologyArtifactIsNotNull(TerminologyArtifact terminologyArtifact)
@@ -61,6 +67,24 @@ namespace LHDS.Core.Services.Foundations.TerminologyArtifacts
                 Condition = firstDate != secondDate,
                 Message = $"Date is not the same as {secondDateName}"
             };
+
+        private static dynamic IsNotSame(
+            Guid firstId,
+            Guid secondId,
+            string secondIdName) => new
+            {
+                Condition = firstId != secondId,
+                Message = $"Id is not the same as {secondIdName}"
+            };
+
+        private static dynamic IsNotSame(
+           string first,
+           string second,
+           string secondName) => new
+           {
+               Condition = first != second,
+               Message = $"Text is not the same as {secondName}"
+           };
 
         private static void Validate(params (dynamic Rule, string Parameter)[] validations)
         {

886a5c5d7a78d4ec7ae8ec5225af8503583c60d1
Christo du Toit <christo@dutoit.co.uk>
2023-11-02 15:06:08 +0000

    ShouldThrowDependencyValidationExceptionOnAddIfTerminologyArtifactAlreadyExsitsAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/TerminologyArtifacts/TerminologyArtifactServiceTests.Exceptions.Add.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/TerminologyArtifacts/TerminologyArtifactServiceTests.Exceptions.Add.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
 using Moq;
@@ -25,7 +26,7 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.TerminologyArtifacts
             var expectedTerminologyArtifactDependencyException =
                 new TerminologyArtifactDependencyException(
                     message: "TerminologyArtifact dependency error occurred, contact support.",
-                    innerException: failedTerminologyArtifactStorageException);
+                    innerException: failedTerminologyArtifactStorageException);             
 
             this.dateTimeBrokerMock.Setup(broker =>
                 broker.GetCurrentDateTimeOffset())
@@ -54,6 +55,61 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.TerminologyArtifacts
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(
                     expectedTerminologyArtifactDependencyException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnAddIfTerminologyArtifactAlreadyExsitsAndLogItAsync()
+        {
+            // given
+            TerminologyArtifact randomTerminologyArtifact = CreateRandomTerminologyArtifact();
+            TerminologyArtifact alreadyExistsTerminologyArtifact = randomTerminologyArtifact;
+            string randomMessage = GetRandomString();
+
+            var duplicateKeyException =
+                new DuplicateKeyException(randomMessage);
+
+            var alreadyExistsTerminologyArtifactException =
+                new AlreadyExistsTerminologyArtifactException(
+                    message: "TerminologyArtifact with the same Id already exists.",
+                    innerException: duplicateKeyException);
+
+            var expectedTerminologyArtifactDependencyValidationException =
+                new TerminologyArtifactDependencyValidationException(
+                    message: "TerminologyArtifact dependency validation occurred, please try again.",
+                    innerException: alreadyExistsTerminologyArtifactException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(duplicateKeyException);
+
+            // when
+            ValueTask<TerminologyArtifact> addTerminologyArtifactTask =
+                this.terminologyArtifactService.AddTerminologyArtifactAsync(alreadyExistsTerminologyArtifact);
+
+            // then
+            TerminologyArtifactDependencyValidationException actualTerminologyArtifactDependencyValidationException =
+                await Assert.ThrowsAsync<TerminologyArtifactDependencyValidationException>(
+                    addTerminologyArtifactTask.AsTask);
+
+            actualTerminologyArtifactDependencyValidationException.Should()
+                .BeEquivalentTo(expectedTerminologyArtifactDependencyValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertTerminologyArtifactAsync(It.IsAny<TerminologyArtifact>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTerminologyArtifactDependencyValidationException))),
                         Times.Once);
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();

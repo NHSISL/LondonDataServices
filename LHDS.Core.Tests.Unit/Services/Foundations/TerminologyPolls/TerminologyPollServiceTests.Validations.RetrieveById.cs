1d28243795a5319155aa45825892883b44469677
Christo du Toit <christo@dutoit.co.uk>
2023-11-02 15:22:41 +0000

    ShouldThrowNotFoundExceptionOnRetrieveByIdIfTerminologyPollIsNotFoundAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/TerminologyPolls/TerminologyPollServiceTests.Validations.RetrieveById.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/TerminologyPolls/TerminologyPollServiceTests.Validations.RetrieveById.cs
@@ -54,5 +54,49 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.TerminologyPolls
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRetrieveByIdIfTerminologyPollIsNotFoundAndLogItAsync()
+        {
+            //given
+            Guid someTerminologyPollId = Guid.NewGuid();
+            TerminologyPoll noTerminologyPoll = null;
+
+            var notFoundTerminologyPollException =
+                new NotFoundTerminologyPollException(someTerminologyPollId);
+
+            var expectedTerminologyPollValidationException =
+                new TerminologyPollValidationException(
+                    message: "TerminologyPoll validation errors occurred, please try again.",
+                    innerException: notFoundTerminologyPollException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectTerminologyPollByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noTerminologyPoll);
+
+            //when
+            ValueTask<TerminologyPoll> retrieveTerminologyPollByIdTask =
+                this.terminologyPollService.RetrieveTerminologyPollByIdAsync(someTerminologyPollId);
+
+            TerminologyPollValidationException actualTerminologyPollValidationException =
+                await Assert.ThrowsAsync<TerminologyPollValidationException>(
+                    retrieveTerminologyPollByIdTask.AsTask);
+
+            //then
+            actualTerminologyPollValidationException.Should().BeEquivalentTo(expectedTerminologyPollValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTerminologyPollByIdAsync(It.IsAny<Guid>()),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTerminologyPollValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

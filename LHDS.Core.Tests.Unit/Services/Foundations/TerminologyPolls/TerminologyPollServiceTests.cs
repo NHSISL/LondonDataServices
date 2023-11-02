b8742516b623d30805879d09f6aa3f17308e5c3a
Christo du Toit <christo@dutoit.co.uk>
2023-11-02 15:22:10 +0000

    ShouldAddTerminologyPollAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/TerminologyPolls/TerminologyPollServiceTests.cs
@@ -0,0 +1,52 @@
+using System;
+using Moq;
+using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Brokers.Storages.Sql;
+using LHDS.Core.Models.Foundations.TerminologyPolls;
+using LHDS.Core.Services.Foundations.TerminologyPolls;
+using Tynamix.ObjectFiller;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.TerminologyPolls
+{
+    public partial class TerminologyPollServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ITerminologyPollService terminologyPollService;
+
+        public TerminologyPollServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.terminologyPollService = new TerminologyPollService(
+                storageBroker: this.storageBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static TerminologyPoll CreateRandomTerminologyPoll(DateTimeOffset dateTimeOffset) =>
+            CreateTerminologyPollFiller(dateTimeOffset).Create();
+
+        private static Filler<TerminologyPoll> CreateTerminologyPollFiller(DateTimeOffset dateTimeOffset)
+        {
+            string user = Guid.NewGuid().ToString();
+            var filler = new Filler<TerminologyPoll>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnProperty(terminologyPoll => terminologyPoll.CreatedBy).Use(user)
+                .OnProperty(terminologyPoll => terminologyPoll.UpdatedBy).Use(user);
+
+            // TODO: Complete the filler setup e.g. ignore related properties etc...
+
+            return filler;
+        }
+    }
+}
---
+++ LHDS.Core/Services/Foundations/TerminologyPolls/TerminologyPollService.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Brokers.Storages.Sql;
+using LHDS.Core.Models.Foundations.TerminologyPolls;
+
+namespace LHDS.Core.Services.Foundations.TerminologyPolls
+{
+    public partial class TerminologyPollService : ITerminologyPollService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public TerminologyPollService(
+            IStorageBroker storageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<TerminologyPoll> AddTerminologyPollAsync(TerminologyPoll terminologyPoll) =>
+            throw new System.NotImplementedException();
+    }
+}

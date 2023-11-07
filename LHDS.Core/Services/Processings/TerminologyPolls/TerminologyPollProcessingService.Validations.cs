218a196ea7c78e8d1846b680987272761a5f213c
Hassan Raza <h_raza110@outlook.com>
2023-11-07 15:58:49 +0000

    ShouldThrowValidationExceptionOnAddIfTerminologyPollIsInvalidAndLogItAsync -> PASS

--- LHDS.Core/Services/Processings/TerminologyPolls/TerminologyPollProcessingService.Validations.cs
+++ LHDS.Core/Services/Processings/TerminologyPolls/TerminologyPollProcessingService.Validations.cs
@@ -13,7 +13,18 @@ namespace LHDS.Core.Services.Processings.TerminologyPolls
         private void ValidateTerminologyPollOnAdd(TerminologyPoll terminologyPoll)
         {
             ValidateTerminologyPollIsNotNull(terminologyPoll);
+
+            Validate(
+                (Rule: IsInvalid(terminologyPoll.Id), Parameter: nameof(TerminologyPoll.Id)),
+                (Rule: IsInvalid(terminologyPoll.ResourceType), Parameter: nameof(TerminologyPoll.ResourceType)),
+                (Rule: IsInvalid(terminologyPoll.CreatedDate), Parameter: nameof(TerminologyPoll.CreatedDate)),
+                (Rule: IsInvalid(terminologyPoll.CreatedBy), Parameter: nameof(TerminologyPoll.CreatedBy)),
+                (Rule: IsInvalid(terminologyPoll.UpdatedDate), Parameter: nameof(TerminologyPoll.UpdatedDate)),
+                (Rule: IsInvalid(terminologyPoll.UpdatedBy), Parameter: nameof(TerminologyPoll.UpdatedBy)));
         }
+
+        public void ValidateTerminologyPollId(Guid terminologyPollId) =>
+            Validate((Rule: IsInvalid(terminologyPollId), Parameter: nameof(TerminologyPoll.Id)));
 
         private static void ValidateTerminologyPollIsNotNull(TerminologyPoll terminologyPoll)
         {
@@ -22,5 +33,43 @@ namespace LHDS.Core.Services.Processings.TerminologyPolls
                 throw new NullTerminologyPollException(message: "Terminology poll is null.");
             }
         }
-    }
+
+        private static dynamic IsInvalid(Guid id) => new
+        {
+            Condition = id == Guid.Empty,
+            Message = "Id is required"
+        };
+
+        private static dynamic IsInvalid(string text) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(text),
+            Message = "Text is required"
+        };
+
+        private static dynamic IsInvalid(DateTimeOffset date) => new
+        {
+            Condition = date == default,
+            Message = "Date is required"
+
+        };
+
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidTerminologyPollException =
+                new InvalidTerminologyPollException(
+                    message: "Invalid terminology poll. Please correct the errors and try again.");
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidTerminologyPollException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidTerminologyPollException.ThrowIfContainsErrors();
+        }
+    } 
 }

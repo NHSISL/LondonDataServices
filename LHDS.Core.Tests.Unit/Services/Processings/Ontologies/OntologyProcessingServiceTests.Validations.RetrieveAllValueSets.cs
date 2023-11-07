77979d8b465a253f72df2f1d467bd727df934f14
Hassan Raza <h_raza110@outlook.com>
2023-11-07 12:15:37 +0000

    ShouldThrowValidationExceptionOnRetrieveAllValueSetsIfUrlIsInvalidAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/Ontologies/OntologyProcessingServiceTests.Validations.RetrieveAllValueSets.cs
@@ -0,0 +1,59 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.Ontologies;
+using LHDS.Core.Models.Processings.Ontologies.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.Ontologies
+{
+    public partial class OntologyProcessingServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnRetrieveAllValueSetsIfUrlIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            string invalidFileName = invalidText;
+
+            var invalidArgumentOntologyProcessingException =
+                new InvalidArgumentOntologyProcessingException(
+                    message: "Invalid ontology processing arguments. Please correct the error and try again.");
+
+            invalidArgumentOntologyProcessingException.AddData(
+                key: "relativeUrl",
+                values: "Text is required");
+
+            var expectedOntologyValidationException =
+                new OntologyProcessingValidationException(
+                    message: "Ontology processing validation error occurred, please try again.",
+                    innerException: invalidArgumentOntologyProcessingException);
+
+            // when
+            ValueTask<OntologyAssets> retrieveOntologyByRelativeUrlTask =
+                this.ontologyProcessingService.RetrieveAllValueSetsAsync(invalidFileName);
+
+            OntologyProcessingValidationException actualOntologyValidationException =
+                await Assert.ThrowsAsync<OntologyProcessingValidationException>(
+                    retrieveOntologyByRelativeUrlTask.AsTask);
+
+            // then
+            actualOntologyValidationException.Should()
+                .BeEquivalentTo(expectedOntologyValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedOntologyValidationException))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.ontologyServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}

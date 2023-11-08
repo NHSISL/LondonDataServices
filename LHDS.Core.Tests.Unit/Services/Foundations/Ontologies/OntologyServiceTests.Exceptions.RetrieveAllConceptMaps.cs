17efe7fa78fe2bde63564314d18f306b9cff70e7
Hassan Raza <h_raza110@outlook.com>
2023-11-08 10:38:21 +0000

    CODE RUB: Fixing comments

--- LHDS.Core.Tests.Unit/Services/Foundations/Ontologies/OntologyServiceTests.Exceptions.RetrieveAllConceptMaps.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Ontologies/OntologyServiceTests.Exceptions.RetrieveAllConceptMaps.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using LHDS.Core.Models.Foundations.Ontologies;
 using LHDS.Core.Models.Foundations.Ontologies.Exceptions;
-using LHDS.Core.Models.Foundations.Suppliers.Exceptions;
 using Moq;
 using Xunit;
 

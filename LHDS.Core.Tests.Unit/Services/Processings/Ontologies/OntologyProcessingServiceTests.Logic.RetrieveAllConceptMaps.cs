c62e347c5ec1db572c973a67f20e88de0e6b14cc
Hassan Raza <h_raza110@outlook.com>
2023-11-09 11:17:22 +0000

    CODE RUB: Fixing Comments

--- LHDS.Core.Tests.Unit/Services/Processings/Ontologies/OntologyProcessingServiceTests.Logic.RetrieveAllConceptMaps.cs
+++ LHDS.Core.Tests.Unit/Services/Processings/Ontologies/OntologyProcessingServiceTests.Logic.RetrieveAllConceptMaps.cs
@@ -24,7 +24,7 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.Ontologies
 
             this.ontologyServiceMock.Setup(service =>
                 service.RetrieveAllConceptMapsAsync(inputRelativeUrl))
-                    .ReturnsAsync(expectedOntologyAssets);
+                    .ReturnsAsync(createdOntologyAssets);
 
             // when
             OntologyAssets actualOntologyAssets =

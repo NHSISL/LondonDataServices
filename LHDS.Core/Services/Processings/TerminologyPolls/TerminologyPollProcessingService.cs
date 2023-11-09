5bb91e90c8a7efd52f0f4e6b574eac99f95761b5
Hassan Raza <h_raza110@outlook.com>
2023-11-09 13:04:01 +0000

    ShouldRetrieveTerminologyPollByIdAsync -> FAIL

--- LHDS.Core/Services/Processings/TerminologyPolls/TerminologyPollProcessingService.cs
+++ LHDS.Core/Services/Processings/TerminologyPolls/TerminologyPollProcessingService.cs
@@ -2,9 +2,9 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
-using LHDS.Core.Brokers.DateTimes;
 using LHDS.Core.Brokers.Loggings;
 using LHDS.Core.Models.Foundations.TerminologyPolls;
 using LHDS.Core.Services.Foundations.TerminologyPolls;
@@ -37,5 +37,8 @@ namespace LHDS.Core.Services.Processings.TerminologyPolls
             {
                 return this.terminologyPollService.RetrieveAllTerminologyPolls();
             });
+
+        public ValueTask<TerminologyPoll> RetrieveTerminologyPollByIdAsync(Guid terminologyPollId) =>
+            throw new System.NotImplementedException();
     }
 }

6232e3f0e3e35e0a4bbdf113a1f81d0ec27a352a
David Hayes <davidhayes03@googlemail.com>
2023-11-09 09:07:09 +0000

    WIP: Terminology Artifact Service

---
+++ LHDS.Core/Services/Processings/TerminologyArtifacts/ITerminologyArtifactProcessingService.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Ontologies;
+using LHDS.Core.Models.Foundations.TerminologyArtifacts;
+
+namespace LHDS.Core.Services.Processings.TerminologyArtifacts
+{
+    internal interface ITerminologyArtifactProcessingService
+    {
+        ValueTask<TerminologyArtifact> RetrieveAllTerminologyArtifactsAsync(OntologyAsset ontologyAsset);
+    }
+}

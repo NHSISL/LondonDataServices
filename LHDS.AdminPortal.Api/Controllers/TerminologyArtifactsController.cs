a6fcaa51f064ba82a1ae76ae1c70b76fead3a70f
Christo du Toit <christo@dutoit.co.uk>
2023-11-13 16:42:02 +0000

    CONTROLLERS: POST TerminologyArtifact

---
+++ LHDS.AdminPortal.Api/Controllers/TerminologyArtifactsController.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using LHDS.AdminPortal.Api.Models.Foundations.TerminologyArtifacts;
+using LHDS.AdminPortal.Api.Models.Foundations.TerminologyArtifacts.Exceptions;
+using LHDS.AdminPortal.Api.Services.Foundations.TerminologyArtifacts;
+
+namespace LHDS.AdminPortal.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TerminologyArtifactsController : RESTFulController
+    {
+        private readonly ITerminologyArtifactService terminologyArtifactService;
+
+        public TerminologyArtifactsController(ITerminologyArtifactService terminologyArtifactService) =>
+            this.terminologyArtifactService = terminologyArtifactService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<TerminologyArtifact>> PostTerminologyArtifactAsync(TerminologyArtifact terminologyArtifact)
+        {
+            try
+            {
+                TerminologyArtifact addedTerminologyArtifact =
+                    await this.terminologyArtifactService.AddTerminologyArtifactAsync(terminologyArtifact);
+
+                return Created(addedTerminologyArtifact);
+            }
+            catch (TerminologyArtifactValidationException terminologyArtifactValidationException)
+            {
+                return BadRequest(terminologyArtifactValidationException.InnerException);
+            }
+            catch (TerminologyArtifactDependencyValidationException terminologyArtifactValidationException)
+                when (terminologyArtifactValidationException.InnerException is InvalidTerminologyArtifactReferenceException)
+            {
+                return FailedDependency(terminologyArtifactValidationException.InnerException);
+            }
+            catch (TerminologyArtifactDependencyValidationException terminologyArtifactDependencyValidationException)
+               when (terminologyArtifactDependencyValidationException.InnerException is AlreadyExistsTerminologyArtifactException)
+            {
+                return Conflict(terminologyArtifactDependencyValidationException.InnerException);
+            }
+            catch (TerminologyArtifactDependencyException terminologyArtifactDependencyException)
+            {
+                return InternalServerError(terminologyArtifactDependencyException);
+            }
+            catch (TerminologyArtifactServiceException terminologyArtifactServiceException)
+            {
+                return InternalServerError(terminologyArtifactServiceException);
+            }
+        }
+    }
+}

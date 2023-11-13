4b4416287d9ce394b87ad34659fa53ac0e716da6
Hassan Raza <h_raza110@outlook.com>
2023-11-13 15:40:28 +0000

    ShouldThrowServiceExceptionOnRemoveveByIdIfServiceErrorOccursAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Processings/TerminologyPolls/TerminologyPollProcessingServiceTests.Exceptions.RemoveById.cs
@@ -0,0 +1,145 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.TerminologyPolls;
+using LHDS.Core.Models.Processings.TerminologyPolls.Exceptions;
+using Moq;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.TerminologyPolls
+{
+    public partial class TerminologyPollProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnRemoveByIdIfErrorOccursAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            Guid terminologyPollId = Guid.NewGuid();
+
+            var expectedTerminologyPollProcessingDependencyValidationException =
+                new TerminologyPollProcessingDependencyValidationException(
+                    message: "Terminology poll processing dependency validation error occurred, please try again.",
+                    innerException: dependencyValidationException.InnerException as Xeption);
+
+            this.terminologyPollServiceMock.Setup(service =>
+                service.RemoveTerminologyPollByIdAsync(terminologyPollId))
+                    .ThrowsAsync(dependencyValidationException);
+
+            // when
+            ValueTask<TerminologyPoll> terminologyRetrieveTask =
+                this.terminologyPollProcessingService.RemoveTerminologyPollByIdAsync(terminologyPollId);
+
+            TerminologyPollProcessingDependencyValidationException actualException =
+                await Assert.ThrowsAsync<TerminologyPollProcessingDependencyValidationException>(
+                    terminologyRetrieveTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedTerminologyPollProcessingDependencyValidationException);
+
+            this.terminologyPollServiceMock.Verify(service =>
+                service.RemoveTerminologyPollByIdAsync(terminologyPollId),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedTerminologyPollProcessingDependencyValidationException))),
+                         Times.Once);
+
+            this.terminologyPollServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task ShouldThrowDependencyExceptionOnRemoveveByIdIfErrorOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            Guid terminologyPollId = Guid.NewGuid();
+
+            var expectedTerminologyPollProcessingDependencyException =
+                new TerminologyPollProcessingDependencyException(
+                    message: "Terminology poll processing dependency error occurred, please try again.",
+                    innerException: dependencyException.InnerException as Xeption);
+
+            this.terminologyPollServiceMock.Setup(service =>
+                 service.RemoveTerminologyPollByIdAsync(terminologyPollId))
+                     .ThrowsAsync(dependencyException);
+
+            // when
+            ValueTask<TerminologyPoll> terminologyRetrieveTask =
+                this.terminologyPollProcessingService.RemoveTerminologyPollByIdAsync(terminologyPollId);
+
+            TerminologyPollProcessingDependencyException actualException =
+                await Assert.ThrowsAsync<TerminologyPollProcessingDependencyException>(
+                    terminologyRetrieveTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedTerminologyPollProcessingDependencyException);
+
+            this.terminologyPollServiceMock.Verify(service =>
+                service.RemoveTerminologyPollByIdAsync(terminologyPollId),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedTerminologyPollProcessingDependencyException))),
+                         Times.Once);
+
+            this.terminologyPollServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRemoveveByIdIfServiceErrorOccursAsync()
+        {
+            // given
+            Guid terminologyPollId = Guid.NewGuid();
+            var serviceException = new Exception();
+
+            var failedTerminologyPollProcessingServiceException =
+                new FailedTerminologyPollProcessingServiceException(
+                    message: "Failed terminology poll processing service error occurred, contact support.",
+                    innerException: serviceException);
+
+            var expectedTerminologyPollProcessingServiceException =
+                new TerminologyPollProcessingServiceException(
+                    message: "Terminology poll processing service error occurred, contact support.",
+                    innerException: failedTerminologyPollProcessingServiceException);
+
+            this.terminologyPollServiceMock.Setup(service =>
+                 service.RemoveTerminologyPollByIdAsync(terminologyPollId))
+                     .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<TerminologyPoll> terminologyRetrieveTask =
+                this.terminologyPollProcessingService.RemoveTerminologyPollByIdAsync(terminologyPollId);
+
+            TerminologyPollProcessingServiceException actualException =
+                await Assert.ThrowsAsync<TerminologyPollProcessingServiceException>(
+                    terminologyRetrieveTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedTerminologyPollProcessingServiceException);
+
+            this.terminologyPollServiceMock.Verify(service =>
+                service.RemoveTerminologyPollByIdAsync(terminologyPollId),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedTerminologyPollProcessingServiceException))),
+                         Times.Once);
+
+            this.terminologyPollServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

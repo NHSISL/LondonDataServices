c0c028384761c4fc2490b60a81a85692878fb99c
David Hayes <davidhayes03@googlemail.com>
2023-11-13 11:03:42 +0000

    ShouldThrowDependencyValidationExceptionOnRetrieveAllIfErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Processings/TerminologyArtifacts/Exceptions/TerminologyArtifactProcessingDependencyValidationException.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Processings.TerminologyPolls.Exceptions;
+
+public class TerminologyArtifactProcessingDependencyValidationException : Xeption
+{
+    public TerminologyArtifactProcessingDependencyValidationException(string message, Xeption innerException)
+        : base(message, innerException)
+    { }
+}

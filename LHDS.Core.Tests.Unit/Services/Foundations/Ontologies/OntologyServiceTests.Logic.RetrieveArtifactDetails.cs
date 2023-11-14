bd1dcc420525b16fc02a2c6ae7d99e826648b278
Hassan Raza <h_raza110@outlook.com>
2023-11-14 16:26:34 +0000

    WIP

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/Ontologies/OntologyServiceTests.Logic.RetrieveArtifactDetails.cs
@@ -0,0 +1,49 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.Ontologies;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Ontologys
+{
+    public partial class OntologyServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveArtifactDetailsByRelativeUrlAsync()
+        {
+            // given
+            string randomRelativeUrl = GetRandomString();
+            string inputRelativeUrl = randomRelativeUrl;
+            string nextPageUrl = "http://localhost:5000/api/fhir/ValueSet?_page=2";
+            string artifactType = "ValueSet";
+
+            List<dynamic> randomArtifactProperties = CreateRandomArtifactProperties(artifactType);
+
+            var remoteValueSetBundle = CreateValueSetBundleFromRandomData(randomArtifactProperties, nextPageUrl);
+            var expectedOntologyAssets = CreateArtiFactFromRandomData(randomArtifactProperties, nextPageUrl);
+
+            this.ontologyBrokerMock.Setup(broker =>
+                broker.GetAllValueSetsAsync(inputRelativeUrl))
+                    .ReturnsAsync(remoteValueSetBundle);
+
+            // when
+            OntologyAssets actualOntologyAssets =
+                await this.ontologyService.RetrieveAllValueSetsAsync(inputRelativeUrl);
+
+            // then
+            actualOntologyAssets.Should().BeEquivalentTo(expectedOntologyAssets);
+
+            this.ontologyBrokerMock.Verify(broker =>
+                broker.GetAllValueSetsAsync(inputRelativeUrl),
+                    Times.Once);
+
+            this.ontologyBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

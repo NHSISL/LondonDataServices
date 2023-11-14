dd4677bedc246bb0fd9e94e4d17a83da65bed7c2
Hassan Raza <h_raza110@outlook.com>
2023-11-14 13:12:44 +0000

    ShouldThrowDependencyExceptionOnAddressNormalisationIfDependencyErrorOccursAndLogItAsync -> PASS

---
+++ LHDS.Core/Models/Clients/LibPostalClient/Exceptions/InvalidLibPostalDependencyException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Clients.LibPostalClient.Exceptions
+{
+    public class InvalidLibPostalDependencyException : Xeption
+    {
+        public InvalidLibPostalDependencyException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}

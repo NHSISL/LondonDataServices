e69de17e4a001669959b4e7330010e8c12b2e744
Hassan Raza <h_raza110@outlook.com>
2023-11-15 09:31:47 +0000

    ShouldThrowValidationExceptionOnRetrieveArtifactDetailsIfUrlIsInvalidAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/Ontologies/OntologyServiceTests.Exceptions.RetrieveAllCodingSystems.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Ontologies/OntologyServiceTests.Exceptions.RetrieveAllCodingSystems.cs
@@ -10,7 +10,7 @@ using LHDS.Core.Models.Foundations.Ontologies.Exceptions;
 using Moq;
 using Xunit;
 
-namespace LHDS.Core.Tests.Unit.Services.Foundations.Ontologys
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Ontologies
 {
     public partial class OntologyServiceTests
     {

4afc52eeb2637abbf47a06c6d7a3fc251779f36b
David Hayes <davidhayes03@googlemail.com>
2023-11-15 12:44:19 +0000

    UI FOUNDATIONS: Terminology Artifact

---
+++ LHDS.AdminPortal.Web/ClientApp/src/services/foundations/terminologyArtifactService.ts
@@ -0,0 +1,81 @@
+import { useMsal } from "@azure/msal-react";
+import { Guid } from "guid-typescript";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import TerminologyArtifactBroker from "../../brokers/apiBroker.terminologyartifacts";
+import { TerminologyArtifact } from "../../models/terminologyArtifacts/terminologyArtifact";
+
+export const terminologyArtifactService = {
+
+    useCreateTerminologyArtifact: () => {
+        const terminologyArtifactBroker = new TerminologyArtifactBroker();
+        const queryClient = useQueryClient();
+        const msal = useMsal();
+
+        return useMutation((terminologyArtifact: TerminologyArtifact) => {
+            const date = new Date();
+            terminologyArtifact.createdDate = terminologyArtifact.updatedDate = date;
+            terminologyArtifact.createdBy = terminologyArtifact.updatedBy = msal.accounts[0].username;
+
+            return terminologyArtifactBroker.PostTerminologyArtifactAsync(terminologyArtifact);
+        },
+            {
+                onSuccess: (variables) => {
+                    queryClient.invalidateQueries("TerminologyArtifactGetAll");
+                    queryClient.invalidateQueries(["TerminologyArtifactGetById", { id: variables.id }]);
+                }
+            });
+    },
+
+    useGetAlTerminologyArtifacts: (query: string) => {
+        const terminologyArtifactBroker = new TerminologyArtifactBroker();
+
+        return useQuery(
+            ["TerminologyArtifactGetAll", { query: query }],
+            () => terminologyArtifactBroker.GetAllTerminologyArtifactsAsync(query),
+            { staleTime: Infinity });
+    },
+
+    useGetTerminologyArtifactById: (id: Guid) => {
+        const terminologyArtifactBroker = new TerminologyArtifactBroker();
+
+        return useQuery(
+            ["TerminologyArtifactGetById", { id: id }],
+            () => terminologyArtifactBroker.GetTerminologyArtifactByIdAsync(id),
+            { staleTime: Infinity });
+    },
+
+    useUpdateTerminologyArtifact: () => {
+        const terminologyArtifactBroker = new TerminologyArtifactBroker();
+        const queryClient = useQueryClient();
+        const msal = useMsal();
+
+        return useMutation((terminologyArtifact: TerminologyArtifact) => {
+            const date = new Date();
+            terminologyArtifact.updatedDate = date;
+            terminologyArtifact.updatedBy = msal.accounts[0].username;
+
+            return terminologyArtifactBroker.PutTerminologyArtifactAsync(terminologyArtifact);
+        },
+            {
+                onSuccess: (data) => {
+                    queryClient.invalidateQueries("TerminologyArtifactGetAll");
+                    queryClient.invalidateQueries(["TerminologyArtifactGetById", { id: data.id }]);
+                }
+            });
+    },
+
+    useDeleteTerminologyArtifact: () => {
+        const terminologyArtifactBroker = new TerminologyArtifactBroker();
+        const queryClient = useQueryClient();
+
+        return useMutation((id: Guid) => {
+            return terminologyArtifactBroker.DeleteTerminologyArtifactByIdAsync(id);
+        },
+            {
+                onSuccess: (data) => {
+                    queryClient.invalidateQueries("TerminologyArtifactGetAll");
+                    queryClient.invalidateQueries(["TerminologyArtifactGetById", { id: data.id }]);
+                }
+            });
+    },
+}

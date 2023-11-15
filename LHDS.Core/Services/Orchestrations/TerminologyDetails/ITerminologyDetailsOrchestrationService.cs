10953ff134cd77baabef77c276af7b09f757be11
Hassan Raza <h_raza110@outlook.com>
2023-11-15 11:43:35 +0000

    WIP

---
+++ LHDS.Core/Services/Orchestrations/TerminologyDetails/ITerminologyDetailsOrchestrationService.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace LHDS.Core.Services.Orchestrations.TerminologyDetails
+{
+    public interface ITerminologyDetailsOrchestrationService
+    {
+        ValueTask<string> RetrieveArtifactDetailsAsync();
+    }
+}

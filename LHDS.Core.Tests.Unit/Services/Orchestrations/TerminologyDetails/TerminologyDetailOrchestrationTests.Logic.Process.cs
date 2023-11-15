a8b40479fde372b8b367c1118a855570ee92a652
Hassan Raza <h_raza110@outlook.com>
2023-11-15 16:15:53 +0000

    ShouldProcessRetrieveArtifactDetailsAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/TerminologyDetails/TerminologyDetailOrchestrationTests.Logic.Process.cs
@@ -0,0 +1,78 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Force.DeepCloner;
+using LHDS.Core.Models.Foundations.Documents;
+using LHDS.Core.Models.Foundations.IngestionTrackingAudits;
+using LHDS.Core.Models.Foundations.IngestionTrackings;
+using LHDS.Core.Models.Foundations.TerminologyArtifacts;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Orchestrations.TerminologyDetails
+{
+    public partial class TerminologyDetailOrchestrationTests
+    {
+        [Fact]
+        public async Task ShouldProcessRetrieveArtifactDetailsAsync()
+        {
+            // given
+            IQueryable<TerminologyArtifact> randomTerminologyArtifacts =
+                CreateRandomUndownloadedTerminologyArtifacts();
+
+            IQueryable<TerminologyArtifact> inputTerminologyArtifacts = randomTerminologyArtifacts;
+            IQueryable<TerminologyArtifact> undownloadedTerminologyArtifacts = inputTerminologyArtifacts;
+            TerminologyArtifact undownloadedTerminologyArtifact = undownloadedTerminologyArtifacts.First();
+            string inputFullUrl = undownloadedTerminologyArtifact.FullUrl;
+            string inputFileName = GetRandomString();
+            string outputFileName = inputFileName;
+            string outputArtifactDetail = GetRandomString();
+
+            this.terminologyArtifactProcessingServiceMock.SetupSequence(service =>
+                service.GetNonDownloadedArtifactAsync())
+                    .ReturnsAsync(undownloadedTerminologyArtifact)
+                    .ReturnsAsync((TerminologyArtifact)null);
+
+            this.ontologyProcessingServiceMock.Setup(service =>
+                service.RetrieveArtifactDetailsAsync(inputFullUrl))
+                    .ReturnsAsync(outputArtifactDetail);
+
+            byte[] outputArtifactDetailData = Encoding.UTF8.GetBytes(outputArtifactDetail);
+
+            Document artifactDetailDocument = new Document
+            {
+                FileName = inputFileName,
+                DocumentData = outputArtifactDetailData
+            };
+
+            this.documentProcessingServiceMock.Setup(service =>
+                service.AddDocumentAsync(artifactDetailDocument, It.IsAny<string>()))
+                    .ReturnsAsync(outputFileName);
+
+            // when
+            await this.terminologyDetailOrchestrationService.RetrieveArtifactDetailsAsync();
+
+            // then
+            this.terminologyArtifactProcessingServiceMock.Verify(service =>
+                service.GetNonDownloadedArtifactAsync(), 
+                    Times.Exactly(2));
+
+            this.ontologyProcessingServiceMock.Verify(service =>
+                service.RetrieveArtifactDetailsAsync(inputFullUrl), 
+                    Times.Once());
+
+            this.documentProcessingServiceMock.Verify(service =>
+                service.AddDocumentAsync(artifactDetailDocument, It.IsAny<string>()),
+                    Times.Once());
+
+            this.terminologyArtifactProcessingServiceMock.VerifyNoOtherCalls();
+            this.ontologyProcessingServiceMock.VerifyNoOtherCalls();
+            this.documentProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/TerminologyDetails/TerminologyDetailOrchestrationTests.cs
@@ -0,0 +1,127 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using KellermanSoftware.CompareNetObjects;
+using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Models.Brokers.Storages.Blobs;
+using LHDS.Core.Models.Foundations.Documents;
+using LHDS.Core.Models.Foundations.TerminologyArtifacts;
+using LHDS.Core.Services.Orchestrations.TerminologyDetails;
+using LHDS.Core.Services.Processings.Documents;
+using LHDS.Core.Services.Processings.Ontologies;
+using LHDS.Core.Services.Processings.TerminologyArtifacts;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace LHDS.Core.Tests.Unit.Services.Orchestrations.TerminologyDetails
+{
+    public partial class TerminologyDetailOrchestrationTests
+    {
+        private readonly Mock<ITerminologyArtifactProcessingService> terminologyArtifactProcessingServiceMock;
+        private readonly Mock<IOntologyProcessingService> ontologyProcessingServiceMock;
+        private readonly Mock<IDocumentProcessingService> documentProcessingServiceMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly BlobContainers blobContainers;
+        private readonly ITerminologyDetailOrchestrationService terminologyDetailOrchestrationService;
+        private readonly ICompareLogic compareLogic;
+
+        public TerminologyDetailOrchestrationTests()
+        {
+            documentProcessingServiceMock = new Mock<IDocumentProcessingService>();
+            terminologyArtifactProcessingServiceMock = new Mock<ITerminologyArtifactProcessingService>();
+            ontologyProcessingServiceMock = new Mock<IOntologyProcessingService>();
+            loggingBrokerMock = new Mock<ILoggingBroker>();
+            dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            compareLogic = new CompareLogic();
+
+            terminologyDetailOrchestrationService = new TerminologyDetailOrchestrationService(
+                terminologyArtifactProcessingService: terminologyArtifactProcessingServiceMock.Object,
+                ontologyProcessingService: ontologyProcessingServiceMock.Object,
+                documentProcessingService: documentProcessingServiceMock.Object,
+                loggingBroker: loggingBrokerMock.Object,
+                dateTimeBroker: dateTimeBrokerMock.Object);
+
+            blobContainers = new BlobContainers
+            {
+                EmisLanding = "emislanding",
+                Versioner = "versioner",
+                OptOut = "optout",
+                Pds = "pds",
+                Terminology = "terminology",
+            };
+        }
+
+    private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static List<Document> CreateRandomDocuments()
+        {
+            return CreateDocumentFiller()
+                .Create(count: 1)
+                    .ToList();
+        }
+
+        private static Document CreateRandomDocument() =>
+            CreateDocumentFiller().Create();
+
+        private static Filler<Document> CreateDocumentFiller()
+        {
+            var filler = new Filler<Document>();
+            string filename = GetRandomString(10);
+
+            for (int i = 0; i < 6; i++)
+            {
+                filename = $"{filename}_{GetRandomString(10)}";
+            }
+
+            filler.Setup()
+                .OnProperty(dataSet => dataSet.FileName).Use(filename);
+
+            return filler;
+        }
+
+        private static string GetRandomString() =>
+          new MnemonicString().GetValue();
+
+        private static string GetRandomString(int length) =>
+            new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
+        private static string GetRandomMessage() =>
+           new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static IQueryable<TerminologyArtifact> CreateRandomUndownloadedTerminologyArtifacts()
+        {
+            return CreateUndownloadedTerminologyArtifactFiller()
+                .Create(count: 1)
+                    .AsQueryable();
+        }
+
+        private static TerminologyArtifact CreateRandomTerminologyArtifact() =>
+            CreateUndownloadedTerminologyArtifactFiller().Create();
+
+        private static Filler<TerminologyArtifact> CreateUndownloadedTerminologyArtifactFiller()
+        {
+            string user = Guid.NewGuid().ToString();
+            DateTimeOffset dateTimeOffset = DateTimeOffset.Now;
+            var filler = new Filler<TerminologyArtifact>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(terminologyArtifact => terminologyArtifact.IsDownloaded).Use(false)
+                .OnProperty(terminologyArtifact => terminologyArtifact.CreatedBy).Use(user)
+                .OnProperty(terminologyArtifact => terminologyArtifact.UpdatedBy).Use(user);
+
+            return filler;
+        }
+    }
+}

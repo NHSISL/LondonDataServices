f978f7d1e5dd1d6f189fad1fc072582d88758215
Hassan Raza <h_raza110@outlook.com>
2023-11-17 12:22:31 +0000

    ShouldGetNonDownloadedTerminologyArtifact -> PASS

--- LHDS.Core/Services/Processings/TerminologyArtifacts/TerminologyArtifactProcessingService.cs
+++ LHDS.Core/Services/Processings/TerminologyArtifacts/TerminologyArtifactProcessingService.cs
@@ -3,12 +3,14 @@
 // ---------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.DateTimes;
 using LHDS.Core.Brokers.Loggings;
 using LHDS.Core.Models.Foundations.TerminologyArtifacts;
 using LHDS.Core.Services.Foundations.TerminologyArtifacts;
+using Microsoft.EntityFrameworkCore;
 
 namespace LHDS.Core.Services.Processings.TerminologyArtifacts
 {
@@ -74,9 +76,14 @@ namespace LHDS.Core.Services.Processings.TerminologyArtifacts
                 return await this.terminologyArtifactService.RemoveTerminologyArtifactByIdAsync(Id);
             });
 
-        public ValueTask<TerminologyArtifact> GetNonDownloadedArtifactAsync()
+        public async ValueTask<TerminologyArtifact?> GetNonDownloadedArtifactAsync()
         {
-            return this.terminologyArtifactService.RetrieveAllTerminologyArtifacts(where terminologyArtifact.IsDownloaded == true);
+            List<TerminologyArtifact> nonDownloadedArtifacts = 
+                 this.terminologyArtifactService.RetrieveAllTerminologyArtifacts()
+                    .Where(terminologyArtifact => !terminologyArtifact.IsDownloaded)
+                        .ToList();
+
+            return nonDownloadedArtifacts.FirstOrDefault();
         }
     }
 }

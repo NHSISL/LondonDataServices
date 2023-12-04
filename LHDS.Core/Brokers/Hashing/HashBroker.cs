dc7cec1c66e28033a5bdb5ef73e7bf7f2aaff54b
Christo du Toit <christo@dutoit.co.uk>
2023-12-04 10:30:07 +0000

    BROKERS: Hash Broker

---
+++ LHDS.Core/Brokers/Hashing/HashBroker.cs
@@ -0,0 +1,34 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Security.Cryptography;
+
+namespace LHDS.Core.Brokers.Hashing
+{
+    public class HashBroker : IHashBroker
+    {
+        public string GenerateMd5Hash(byte[] data)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] hashBytes = md5.ComputeHash(data);
+                var md5Hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+
+                return md5Hash;
+            }
+        }
+
+        public string GenerateSha256Hash(byte[] data)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(data);
+                var sha256Hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+
+                return sha256Hash;
+            }
+        }
+    }
+}

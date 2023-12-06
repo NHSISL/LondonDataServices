382b2eb6d7f8bcccfc8a1bcc4fae7d2c186678a7
Hassan Raza <h_raza110@outlook.com>
2023-12-06 12:03:12 +0000

    ShouldProcessRetrieveArtifactMetadataAsync -> WIP

--- LHDS.Core.Tests.Unit/Services/Orchestrations/TerminologyMetadata/TerminologyMetadataOrchestrationTests.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/TerminologyMetadata/TerminologyMetadataOrchestrationTests.cs
@@ -3,10 +3,15 @@
 // ---------------------------------------------------------------
 
 using System;
-using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Linq;
 using KellermanSoftware.CompareNetObjects;
 using LHDS.Core.Brokers.DateTimes;
 using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Models.Foundations.Ontologies;
+using LHDS.Core.Models.Foundations.TerminologyArtifacts;
+using LHDS.Core.Models.Foundations.TerminologyPolls;
+using LHDS.Core.Models.Orchestrations.Ontologies;
 using LHDS.Core.Services.Orchestrations.TerminologyMetadata;
 using LHDS.Core.Services.Processings.Ontologies;
 using LHDS.Core.Services.Processings.TerminologyArtifacts;
@@ -23,6 +28,7 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.TerminologyMetadata
         private readonly Mock<IOntologyProcessingService> ontologyProcessingServiceMock;
         private readonly Mock<ILoggingBroker> loggingBrokerMock;
         private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly OntologyConfiguration ontologyConfiguration;
         private readonly ITerminologyMetadataOrchestrationService terminologyMetadataOrchestrationService;
         private readonly ICompareLogic compareLogic;
 
@@ -40,7 +46,11 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.TerminologyMetadata
                 terminologyArtifactProcessingService: terminologyArtifactProcessingServiceMock.Object,
                 ontologyProcessingService: ontologyProcessingServiceMock.Object,
                 loggingBroker: loggingBrokerMock.Object,
-                dateTimeBroker: dateTimeBrokerMock.Object);
+                dateTimeBroker: dateTimeBrokerMock.Object,
+
+            ontologyConfiguration = new OntologyConfiguration(
+                resourceURL: $"{{terminology-server}}/{resourceType}?_lastUpdated=ge{{datestamp}}" +
+                        "&_name=dm+dCOMBINATION_PACK_IND&_elements=name,title,url,version,status&_count=10"));
         }
 
         private static int GetRandomNumber() =>
@@ -54,5 +64,117 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.TerminologyMetadata
 
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static IQueryable<TerminologyPoll> CreateRandomTerminologyPolls(string resourceType)
+        {
+            return CreateTerminologyPollFiller(resourceType)
+                .Create(count: 1)
+                    .AsQueryable();
+        }
+
+        private static TerminologyPoll CreateRandomTerminologyPoll(string resourceType) =>
+            CreateTerminologyPollFiller(resourceType).Create();
+
+        private static Filler<TerminologyPoll> CreateTerminologyPollFiller(string resourceType)
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.UtcNow;
+            var filler = new Filler<TerminologyPoll>();
+
+            filler.Setup()
+                .OnProperty(terminologyMetadata => terminologyMetadata.ResourceType).Use(resourceType)
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset);
+
+            return filler;
+        }
+
+        private static List<OntologyAsset> CreateRandomOntologyAssets() =>
+            CreateOntologyFiller().Create(count: GetRandomNumber()).ToList();
+
+        private static Filler<OntologyAsset> CreateOntologyFiller()
+        {
+            string user = Guid.NewGuid().ToString();
+            DateTimeOffset dateTimeOffset = GetRandomDateTimeOffset();
+
+            var filler = new Filler<OntologyAsset>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset);
+
+            return filler;
+        }
+
+        private static dynamic CreateRandomArtifactProperties(string resourceType)
+        {
+            string user = GetRandomString();
+            DateTimeOffset dateTimeOffset = GetRandomDateTimeOffset();
+            return new
+            {
+                FullUrl = GetRandomString(),
+                ResourceType = resourceType,
+                Version = GetRandomString(),
+                Name = GetRandomString(),
+                Title = GetRandomString(),
+                Status = "active",
+                LastUpdated = dateTimeOffset,
+                IsCore = false,
+                IsDownloaded = true,
+                CreatedBy = user,
+                UpdateBy = user,
+                UpdatedDate = dateTimeOffset,
+                CreatedDate = dateTimeOffset
+            };
+        }
+
+        private static OntologyAsset CreateOntologyArtiFactFromRandomData(
+            dynamic randomArtifactProperties,
+            string nextPageUrl)
+        {
+            var ontologyAssets = new OntologyAssets
+            {
+                Assets = new List<OntologyAsset>(),
+                NextPage = nextPageUrl
+            };
+
+
+            ontologyAssets.Assets.Add(
+                new OntologyAsset
+                {
+                    FullUrl = item.FullUrl,
+                    ResourceType = item.ResourceType,
+                    Version = item.Version,
+                    Name = item.Name,
+                    Title = item.Title,
+                    Status = item.Status,
+                    LastUpdated = item.LastUpdated
+                });
+
+            return ontologyAssets;
+        }
+
+        private static TerminologyArtifact CreateTerminologyArtiFactFromRandomData(
+            dynamic randomArtifactProperties)
+        {
+            TerminologyArtifact terminologyArtifact =
+                new TerminologyArtifact
+                {
+                    FullUrl = randomArtifactProperties.FullUrl,
+                    ResourceType = randomArtifactProperties.ResourceType,
+                    Version = randomArtifactProperties.Version,
+                    Name = randomArtifactProperties.Name,
+                    Title = randomArtifactProperties.Title,
+                    Status = randomArtifactProperties.Status,
+                    LastUpdated = randomArtifactProperties.LastUpdated,
+                    IsCore = randomArtifactProperties.IsCore,
+                    IsDownloaded = randomArtifactProperties.IsDownloaded,
+                    CreatedBy = randomArtifactProperties.LastUpdated,
+                    UpdatedBy = randomArtifactProperties.UpdatedBy,
+                    UpdatedDate = randomArtifactProperties.UpdatedDate,
+                    CreatedDate = randomArtifactProperties.CreatedDate
+                });
+
+            return terminologyArtifact;
+        }
     }
 }

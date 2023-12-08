8ec1c2e3a0806048d763cd32d1b77ed422257b40
Hassan Raza <h_raza110@outlook.com>
2023-12-08 14:11:17 +0000

    ShouldThrowValidationExceptionOnRetrieveOrAddIfResourceTypeIsInvalidAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/TerminologyPolls/TerminologyPollProcessingServiceTests.Validations.RetrieveOrAdd.cs
@@ -0,0 +1,66 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.CsvMappers.Exceptions;
+using LHDS.Core.Models.Foundations.Mesh;
+using LHDS.Core.Models.Foundations.OptOuts;
+using LHDS.Core.Models.Foundations.TerminologyPolls;
+using LHDS.Core.Models.Processings.Mesh.Exceptions;
+using LHDS.Core.Models.Processings.TerminologyPolls.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.TerminologyPolls
+{
+    public partial class TerminologyPollProcessingServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnRetrieveOrAddIfResourceTypeIsInvalidAndLogItAsync(
+            string invalidString)
+        {
+            // given
+            string inputString = invalidString;
+
+            var invalidArgumentTerminologyPollsProcessingException =
+                new InvalidArgumentTerminologyPollsProcessingException(
+                    message: "Invalid terminology poll processing argument. Please correct the errors and try again.");
+
+            invalidArgumentTerminologyPollsProcessingException.AddData(
+                key: "resourceType",
+                values: "Text is required");
+
+            var expectedTerminologyPollProcessingValidationException =
+            new TerminologyPollProcessingValidationException(
+                message: "Terminology poll processing validation errors occured, please try again",
+                innerException: invalidArgumentTerminologyPollsProcessingException);
+
+            // when
+            ValueTask<TerminologyPoll> retrieveOrAddTerminologyPollTask =
+                this.terminologyPollProcessingService.RetrieveOrAddTerminologyPollAsync(inputString);
+
+            TerminologyPollProcessingValidationException actualTerminologyPollProcessingValidationException =
+                await Assert.ThrowsAsync<TerminologyPollProcessingValidationException>(() =>
+                    retrieveOrAddTerminologyPollTask.AsTask());
+
+            //then
+            actualTerminologyPollProcessingValidationException.Should()
+                .BeEquivalentTo(expectedTerminologyPollProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTerminologyPollProcessingValidationException))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.terminologyPollServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}

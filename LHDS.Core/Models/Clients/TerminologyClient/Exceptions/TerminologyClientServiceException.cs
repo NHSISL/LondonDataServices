07e95719539c1aed9b8f6efef2443ef684eedd31
Hassan Raza <h_raza110@outlook.com>
2023-12-12 22:36:30 +0000

    CLIENT: Terminology RetrieveArtifactMetadataAsync

---
+++ LHDS.Core/Models/Clients/TerminologyClient/Exceptions/TerminologyClientServiceException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Clients.TerminologyClient.Exceptions
+{
+    public class TerminologyClientServiceException : Xeption
+    {
+        public TerminologyClientServiceException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}

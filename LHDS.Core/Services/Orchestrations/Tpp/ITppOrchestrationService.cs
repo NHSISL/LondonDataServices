79ab804f4dcf74d8e20ad064650f8d50cce7a6e0
David Hayes <davidhayes03@googlemail.com>
2023-12-15 14:20:40 +0000

    WIP: TPP Orc

---
+++ LHDS.Core/Services/Orchestrations/Tpp/ITppOrchestrationService.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Documents;
+
+namespace LHDS.Core.Services.Orchestrations.Tpp
+{
+    public interface ITppOrchestrationService
+    {
+        ValueTask<string> ProcessAsync(Document fileName);
+    }
+}

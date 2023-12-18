75b4d247d3590c1c44107802a865b56cd8d08003
David Hayes <davidhayes03@googlemail.com>
2023-12-18 14:44:51 +0000

    ShouldThrowValidationExceptionIfDocumentIsNullAndLogItAsync -> PASS

---
+++ LHDS.Core/Models/Orchestrations/Tpp/Exceptions/NullTppDocumentException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.Tpp.Exceptions
+{
+    public class NullTppDocumentException : Xeption
+    {
+        public NullTppDocumentException(string message)
+            : base(message)
+        { }
+    }
+}

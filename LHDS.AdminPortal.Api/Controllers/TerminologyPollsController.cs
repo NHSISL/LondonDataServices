b5f709001fb63bc157c68f82ab2f20831e4477c8
Christo du Toit <christo@dutoit.co.uk>
2024-01-05 10:08:28 +0000

    CONTROLLERS: POST TerminologyPoll

---
+++ LHDS.AdminPortal.Api/Controllers/TerminologyPollsController.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using LHDS.AdminPortal.Api.Models.Foundations.TerminologyPolls;
+using LHDS.AdminPortal.Api.Models.Foundations.TerminologyPolls.Exceptions;
+using LHDS.AdminPortal.Api.Services.Foundations.TerminologyPolls;
+
+namespace LHDS.AdminPortal.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TerminologyPollsController : RESTFulController
+    {
+        private readonly ITerminologyPollService terminologyPollService;
+
+        public TerminologyPollsController(ITerminologyPollService terminologyPollService) =>
+            this.terminologyPollService = terminologyPollService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<TerminologyPoll>> PostTerminologyPollAsync(TerminologyPoll terminologyPoll)
+        {
+            try
+            {
+                TerminologyPoll addedTerminologyPoll =
+                    await this.terminologyPollService.AddTerminologyPollAsync(terminologyPoll);
+
+                return Created(addedTerminologyPoll);
+            }
+            catch (TerminologyPollValidationException terminologyPollValidationException)
+            {
+                return BadRequest(terminologyPollValidationException.InnerException);
+            }
+            catch (TerminologyPollDependencyValidationException terminologyPollValidationException)
+                when (terminologyPollValidationException.InnerException is InvalidTerminologyPollReferenceException)
+            {
+                return FailedDependency(terminologyPollValidationException.InnerException);
+            }
+            catch (TerminologyPollDependencyValidationException terminologyPollDependencyValidationException)
+               when (terminologyPollDependencyValidationException.InnerException is AlreadyExistsTerminologyPollException)
+            {
+                return Conflict(terminologyPollDependencyValidationException.InnerException);
+            }
+            catch (TerminologyPollDependencyException terminologyPollDependencyException)
+            {
+                return InternalServerError(terminologyPollDependencyException);
+            }
+            catch (TerminologyPollServiceException terminologyPollServiceException)
+            {
+                return InternalServerError(terminologyPollServiceException);
+            }
+        }
+    }
+}

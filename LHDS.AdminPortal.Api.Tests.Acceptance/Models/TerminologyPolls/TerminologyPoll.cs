ec937b82b323bcb173e6eaf6d05264b302c827f6
Christo du Toit <christo@dutoit.co.uk>
2024-01-05 10:12:34 +0000

    DATA: TerminologyPoll Model

---
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Models/TerminologyPolls/TerminologyPoll.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace LHDS.AdminPortal.Api.Tests.Acceptance.Models.TerminologyPolls
+{
+    public class TerminologyPoll
+    {
+        public Guid Id { get; set; }
+
+        // TODO:  Add your properties here
+
+        public string CreatedBy { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}

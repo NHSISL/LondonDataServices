b7aaf3b81971e03073952b22fdd3fc2caec18162
Christo du Toit <christo@dutoit.co.uk>
2024-01-05 10:44:06 +0000

    CODE RUB: Updated model

--- LHDS.AdminPortal.Api.Tests.Acceptance/Models/TerminologyArtifacts/TerminologyArtifact.cs
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Models/TerminologyArtifacts/TerminologyArtifact.cs
@@ -1,3 +1,7 @@
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
 using System;
 
 namespace LHDS.AdminPortal.Api.Tests.Acceptance.Models.TerminologyArtifacts
@@ -5,12 +9,18 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Models.TerminologyArtifacts
     public class TerminologyArtifact
     {
         public Guid Id { get; set; }
-
-        // TODO:  Add your properties here
-
-        public string CreatedBy { get; set; }
-        public DateTimeOffset CreatedDate { get; set; }
-        public string UpdatedBy { get; set; }
+        public string FullUrl { get; set; }
+        public string ResourceType { get; set; }
+        public string? Version { get; set; }
+        public string? Name { get; set; }
+        public string? Title { get; set; }
+        public string? Status { get; set; }
+        public DateTimeOffset? LastUpdated { get; set; }
+        public bool IsCore { get; set; }
+        public bool IsDownloaded { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public string UpdatedBy { get; set; } = string.Empty;
         public DateTimeOffset UpdatedDate { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
     }
 }

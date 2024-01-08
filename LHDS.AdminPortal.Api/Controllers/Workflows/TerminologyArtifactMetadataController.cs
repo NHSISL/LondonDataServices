3f0ffab5587c1c7a47cf8f15a69e8b7a0eb97da6
Hassan Raza <h_raza110@outlook.com>
2024-01-08 10:27:36 +0000

    ProcessArtifactMetadataAsync

--- LHDS.AdminPortal.Api/Controllers/Workflows/TerminologyArtifactMetadataController.cs
+++ LHDS.AdminPortal.Api/Controllers/Workflows/TerminologyArtifactMetadataController.cs
@@ -4,7 +4,9 @@
 
 using System.Threading.Tasks;
 using System.Web;
+using LHDS.Core.Models.Orchestrations.TerminologyMedata.Exceptions;
 using LHDS.Core.Models.Orchestrations.TerminologyMetadata.Exceptions;
+using LHDS.Core.Services.Orchestrations.TerminologyMetadata;
 using Microsoft.AspNetCore.Mvc;
 using RESTFulSense.Controllers;
 #if RELEASE
@@ -14,31 +16,34 @@ using Microsoft.AspNetCore.Authorization;
 namespace LHDS.AdminPortal.Api.Controllers.Workflows
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/workflows/[controller]")]
     public class TerminologyArtifactMetadataController : RESTFulController
     {
         private readonly ITerminologyMetadataOrchestrationService terminologyMetadataOrchestrationService;
 
-        public LandingsController(ITerminologyMetadataOrchestrationService terminologyMetadataOrchestrationService) =>
+        public TerminologyArtifactMetadataController(ITerminologyMetadataOrchestrationService 
+                terminologyMetadataOrchestrationService) =>
             this.terminologyMetadataOrchestrationService = terminologyMetadataOrchestrationService;
 
         [HttpPost]
 #if RELEASE
         [Authorize(Roles = "ISL.LDS.AdminApi.Administrators, lhds.Api.TerminologyArtifact, ISL.LDS.AdminApi.ReadOnly")]
 #endif
-        public async ValueTask<ActionResult> RetrieveArtifactMetadataAsync()
+        public async ValueTask<ActionResult> ProcessArtifactMetadataAsync([FromBody] string resrouceType)
         {
             try
             {
-                await this.terminologyMetadataOrchestrationService.RetrieveArtifactMetadataAsync();
+                await this.terminologyMetadataOrchestrationService.RetrieveArtifactMetadataAsync(resrouceType);
 
                 return Ok();
             }
-            catch (TerminologyMetadataOrchestrationValidationException terminologyMetadataOrchestrationValidationException)
+            catch (TerminologyMetadataOrchestrationValidationException 
+                terminologyMetadataOrchestrationValidationException)
             {
                 return BadRequest(terminologyMetadataOrchestrationValidationException.InnerException);
             }
-            catch (TerminologyMetadataOrchestrationDependencyException terminologyMetadataOrchestrationDependencyException)
+            catch (TerminologyMetadataOrchestrationDependencyException 
+                terminologyMetadataOrchestrationDependencyException)
             {
                 return InternalServerError(terminologyMetadataOrchestrationDependencyException);
             }

d955a9871b7d5121c54c4956c152088fa2bf81fa
Christo du Toit <christo@dutoit.co.uk>
2024-01-09 16:24:13 +0000

    CODE RUB: Refactored decryption service to cryptography

--- LHDS.Core/Models/Foundations/Cryptographies/Exceptions/CryptographyServiceException.cs
+++ LHDS.Core/Models/Foundations/Cryptographies/Exceptions/CryptographyServiceException.cs
@@ -2,13 +2,14 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
+using System;
 using Xeptions;
 
-namespace LHDS.Core.Models.Foundations.Decryptions.Exceptions
+namespace LHDS.Core.Models.Foundations.Cryptographies.Exceptions
 {
-    public class DecryptionValidationException : Xeption
+    public class CryptographyServiceException : Xeption
     {
-        public DecryptionValidationException(string message, Xeption innerException)
+        public CryptographyServiceException(string message, Exception innerException)
             : base(message, innerException)
         { }
     }

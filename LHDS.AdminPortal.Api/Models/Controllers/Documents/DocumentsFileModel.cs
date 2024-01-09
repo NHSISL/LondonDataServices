a6a4cb4e087df17b08d9205b4355c93eeb3a34eb
Hassan Raza <h_raza110@outlook.com>
2024-01-09 11:59:00 +0000

    CODE RUB: Creating Documents Model

---
+++ LHDS.AdminPortal.Api/Models/Controllers/Documents/DocumentsFileModel.cs
@@ -0,0 +1,12 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace LHDS.AdminPortal.Api.Models.Controllers.Documents
+{
+    public class DocumentsFileModel
+    {
+        public string FileName { get; set; }
+        public string Container { get; set; }
+    }
+}

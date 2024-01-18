2b396ed6886906b240a034150fbca72102bcce19
Hassan Raza <h_raza110@outlook.com>
2024-01-18 15:26:28 +0000

    OptOutDependencyValidationException

--- LHDS.Core.Tests.Unit/Services/Foundations/OptOuts/OptOutServiceTests.Exceptions.RemoveById.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/OptOuts/OptOutServiceTests.Exceptions.RemoveById.cs
@@ -82,7 +82,7 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.OptOuts
                 message: "Locked optOut record exception, please try again later",
                 innerException: databaseUpdateConcurrencyException);
 
-            var expectedOptOutValidationException = new OptOutDependencyValidationException(
+            var expectedOptOutDependencyValidationException = new OptOutDependencyValidationException(
                 message: "OptOut dependency validation occurred, please try again.",
                 innerException: lockedOptOutException);
 

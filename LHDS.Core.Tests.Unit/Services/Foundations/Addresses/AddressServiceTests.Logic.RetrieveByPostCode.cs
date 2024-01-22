14321909d4d43be06e50f8f5554bc4289a5efd65
Hassan Raza <h_raza110@outlook.com>
2024-01-22 12:35:12 +0000

    ShouldReturnAddressesByPostCode -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/Addresses/AddressServiceTests.Logic.RetrieveByPostCode.cs
@@ -0,0 +1,52 @@
+using System.Linq;
+using FluentAssertions;
+using Moq;
+using LHDS.Core.Models.Foundations.Addresses;
+using Xunit;
+using System.Collections.Generic;
+using Force.DeepCloner;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Addresses
+{
+    public partial class AddressServiceTests
+    {
+        [Fact]
+        public void ShouldReturnAddressesByPostCode()
+        {
+            // given
+            List<Address> randomAddresses = CreateRandomAddresses().ToList();
+            string randomString = GetRandomString();
+            string inputPostCode = randomString;
+
+            foreach (var address in randomAddresses)
+            {
+                address.PostCode = inputPostCode;
+            }
+
+            List<Address> expectedAddresses = randomAddresses.DeepClone();
+            List<Address> extraRandomAddresses = CreateRandomAddresses().ToList();
+            List<Address> allRandomAddresses = new List<Address>();
+            allRandomAddresses.AddRange(randomAddresses);
+            allRandomAddresses.AddRange(extraRandomAddresses);
+
+            IQueryable<Address> storageAddresses = allRandomAddresses.AsQueryable();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllAddresses())
+                    .Returns(storageAddresses);
+
+            // when
+            List<Address> actualAddresses = this.addressService.RetrieveAddressesByPostCode(inputPostCode);
+
+            // then
+            actualAddresses.Should().BeEquivalentTo(expectedAddresses);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllAddresses(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

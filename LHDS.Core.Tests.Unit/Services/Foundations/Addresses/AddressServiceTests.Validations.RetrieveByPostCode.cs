575561070767d27c13647605cd9cd194ce3ca89f
Hassan Raza <h_raza110@outlook.com>
2024-01-22 15:23:04 +0000

    ShouldThrowValidationExceptionOnRetrieveByPostCodeIfPostCodeIsInvalidAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Foundations/Addresses/AddressServiceTests.Validations.RetrieveByPostCode.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Addresses/AddressServiceTests.Validations.RetrieveByPostCode.cs
@@ -1,28 +1,37 @@
-using System;
+// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
-using Moq;
 using LHDS.Core.Models.Foundations.Addresses;
 using LHDS.Core.Models.Foundations.Addresses.Exceptions;
+using Moq;
 using Xunit;
 
 namespace LHDS.Core.Tests.Unit.Services.Foundations.Addresses
 {
     public partial class AddressServiceTests
     {
-        [Fact]
-        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+
+        public async Task ShouldThrowValidationExceptionOnRetrieveByPostCodeIfPostCodeIsInvalidAndLogItAsync(
+            string invalidString)
         {
             // given
-            var invalidAddressId = Guid.Empty;
+            var invalidPostCode = invalidString;
 
-            var invalidAddressException = 
+            var invalidAddressException =
                 new InvalidAddressException(
                     message: "Invalid address. Please correct the errors and try again.");
 
             invalidAddressException.AddData(
-                key: nameof(Address.Id),
-                values: "Id is required");
+                key: "postCode",
+                values: "Text is required");
 
             var expectedAddressValidationException =
                 new AddressValidationException(
@@ -30,12 +39,12 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Addresses
                     innerException: invalidAddressException);
 
             // when
-            ValueTask<Address> retrieveAddressByIdTask =
-                this.addressService.RetrieveAddressByIdAsync(invalidAddressId);
+            ValueTask<List<Address>> retrieveAddressByPostCodeTask =
+                this.addressService.RetrieveAddressesByPostCodeAsync(invalidPostCode);
 
             AddressValidationException actualAddressValidationException =
                 await Assert.ThrowsAsync<AddressValidationException>(
-                    retrieveAddressByIdTask.AsTask);
+                    retrieveAddressByPostCodeTask.AsTask);
 
             // then
             actualAddressValidationException.Should()
@@ -47,55 +56,11 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Addresses
                         Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
-                broker.SelectAddressByIdAsync(It.IsAny<Guid>()),
+                broker.SelectAllAddresses(),
                     Times.Never);
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public async Task ShouldThrowNotFoundExceptionOnRetrieveByIdIfAddressIsNotFoundAndLogItAsync()
-        {
-            //given
-            Guid someAddressId = Guid.NewGuid();
-            Address noAddress = null;
-
-            var notFoundAddressException =
-                new NotFoundAddressException(someAddressId);
-
-            var expectedAddressValidationException =
-                new AddressValidationException(
-                    message: "Address validation errors occurred, please try again.",
-                    innerException: notFoundAddressException);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectAddressByIdAsync(It.IsAny<Guid>()))
-                    .ReturnsAsync(noAddress);
-
-            //when
-            ValueTask<Address> retrieveAddressByIdTask =
-                this.addressService.RetrieveAddressByIdAsync(someAddressId);
-
-            AddressValidationException actualAddressValidationException =
-                await Assert.ThrowsAsync<AddressValidationException>(
-                    retrieveAddressByIdTask.AsTask);
-
-            //then
-            actualAddressValidationException.Should().BeEquivalentTo(expectedAddressValidationException);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectAddressByIdAsync(It.IsAny<Guid>()),
-                    Times.Once());
-
-            this.loggingBrokerMock.Verify(broker =>
-                broker.LogError(It.Is(SameExceptionAs(
-                    expectedAddressValidationException))),
-                        Times.Once);
-
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }

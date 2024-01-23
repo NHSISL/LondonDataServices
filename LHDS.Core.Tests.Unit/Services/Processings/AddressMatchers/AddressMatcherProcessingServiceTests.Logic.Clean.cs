e183fe3e31c1856b2cfcf852361d8b8e1a444e25
Hassan Raza <h_raza110@outlook.com>
2024-01-23 14:56:18 +0000

    ShouldCleanAddress -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/AddressMatchers/AddressMatcherProcessingServiceTests.Logic.Clean.cs
@@ -0,0 +1,29 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.AddressMatchers
+{
+    public partial class AddressMatcherProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(UncleanedAddressString))]
+        public void ShouldCleanAddress(string uncleanedAddressString)
+        {
+            // given
+            string uncleanedAddress = uncleanedAddressString;
+            string expectedCleanedAddress = "a, a, a";
+
+            // when
+            string actualAddress = this.addressMatcherProcessingService.CleanAddress(uncleanedAddress);
+
+            // then
+            actualAddress.Should().BeEquivalentTo(expectedCleanedAddress);
+        }
+
+
+    }
+}

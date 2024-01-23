f6ce69b952a2363019ec130fbd164b312c0d6b5c
Christo du Toit <christo@dutoit.co.uk>
2024-01-23 09:43:01 +0000

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/ResolvedAddresses/Exceptions/ResolvedAddressServiceException.cs
@@ -0,0 +1,12 @@
+using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.ResolvedAddresses.Exceptions
+{
+    public class ResolvedAddressServiceException : Xeption
+    {
+        public ResolvedAddressServiceException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}

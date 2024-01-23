e33290617f5f898ba7bfbde24cd01757059b5a19
Christo du Toit <christo@dutoit.co.uk>
2024-01-23 09:44:16 +0000

    ShouldThrowDependencyValidationExceptionOnModifyIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/ResolvedAddresses/ResolvedAddressService.Exceptions.cs
+++ LHDS.Core/Services/Foundations/ResolvedAddresses/ResolvedAddressService.Exceptions.cs
@@ -60,6 +60,15 @@ namespace LHDS.Core.Services.Foundations.ResolvedAddresses
 
                 throw CreateAndLogDependencyValidationException(invalidResolvedAddressReferenceException);
             }
+            catch (DbUpdateConcurrencyException dbUpdateConcurrencyException)
+            {
+                var lockedResolvedAddressException = 
+                    new LockedResolvedAddressException(
+                        message: "Locked resolvedAddress record exception, please try again later",
+                        innerException: dbUpdateConcurrencyException);
+
+                throw CreateAndLogDependencyValidationException(lockedResolvedAddressException);
+            }
             catch (DbUpdateException databaseUpdateException)
             {
                 var failedResolvedAddressStorageException =
@@ -123,7 +132,7 @@ namespace LHDS.Core.Services.Foundations.ResolvedAddresses
             var resolvedAddressDependencyException = 
                 new ResolvedAddressDependencyException(
                     message: "ResolvedAddress dependency error occurred, contact support.",
-                    innerException: exception);
+                    innerException: exception); 
 
             this.loggingBroker.LogCritical(resolvedAddressDependencyException);
 
@@ -148,7 +157,7 @@ namespace LHDS.Core.Services.Foundations.ResolvedAddresses
             var resolvedAddressDependencyException = 
                 new ResolvedAddressDependencyException(
                     message: "ResolvedAddress dependency error occurred, contact support.",
-                    innerException: exception);
+                    innerException: exception); 
 
             this.loggingBroker.LogError(resolvedAddressDependencyException);
 

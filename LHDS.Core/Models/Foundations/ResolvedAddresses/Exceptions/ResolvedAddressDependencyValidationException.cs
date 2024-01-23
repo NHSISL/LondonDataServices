16527dcf65072b350e2d7cd4672340a49388f82a
Christo du Toit <christo@dutoit.co.uk>
2024-01-23 09:42:56 +0000

    ShouldThrowDependencyValidationExceptionOnAddIfResolvedAddressAlreadyExsitsAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/ResolvedAddresses/Exceptions/ResolvedAddressDependencyValidationException.cs
@@ -0,0 +1,11 @@
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.ResolvedAddresses.Exceptions
+{
+    public class ResolvedAddressDependencyValidationException : Xeption
+    {
+        public ResolvedAddressDependencyValidationException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}

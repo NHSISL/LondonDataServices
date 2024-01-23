1b40a66599a9effbf97cd4895d0afe1cc4b9f42c
Hassan Raza <h_raza110@outlook.com>
2024-01-23 16:01:30 +0000

    ShouldThrowValidationExceptionOnCleanAddressIfArgsIsInvalidAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/AddressMatchers/AddressMatcherProcessingServiceTests.Validations.CleanAddress.cs
@@ -0,0 +1,58 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Processings.AddressMatchers.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.AddressMatchers
+{
+    public partial class AddressMatcherProcessingServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnCleanAddressIfArgsIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            string address = invalidText;
+
+            var invalidArgumentAddressMatcherException =
+                new InvalidArgumentAddressMatcherException(
+                    message: "Invalid Address Matcher argument(s), please correct the errors and try again.");
+
+            invalidArgumentAddressMatcherException.AddData(
+                key: "address",
+                values: "Text is required");
+
+            var expectedAddressMatcherValidationException =
+                new AddressMatcherValidationException(
+                    message: "Address matcher validation errors occurred, please try again.",
+                    innerException: invalidArgumentAddressMatcherException);
+
+            // when
+            Action cleanAddressAction = () =>
+                addressMatcherProcessingService.CleanAddress(address);
+
+            AddressMatcherValidationException actualAddressMatcherValidationException =
+                Assert.Throws<AddressMatcherValidationException>(cleanAddressAction);
+
+            // then
+            actualAddressMatcherValidationException.Should()
+                .BeEquivalentTo(expectedAddressMatcherValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAddressMatcherValidationException))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

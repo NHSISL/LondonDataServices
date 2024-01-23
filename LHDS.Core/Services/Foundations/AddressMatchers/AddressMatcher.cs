81d4f75c8adf62bec092ec92fa9ffdb7d9a60acb
Christo du Toit <christo@dutoit.co.uk>
2024-01-23 11:20:54 +0000

    CODE RUB: Interface stubs

---
+++ LHDS.Core/Services/Foundations/AddressMatchers/AddressMatcher.cs
@@ -0,0 +1,24 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LHDS.Core.Brokers.Loggings;
+
+namespace LHDS.Core.Services.Foundations.AddressMatchers
+{
+    internal class AddressMatcher : IAddressMatcher
+    {
+        private readonly ILoggingBroker loggingBroker;
+
+        public AddressMatcher(ILoggingBroker loggingBroker)
+        {
+            this.loggingBroker = loggingBroker;
+        }
+
+        public string CleanAddress(string address) =>
+            throw new System.NotImplementedException();
+
+        public string ExtractPostCode(string address) =>
+            throw new System.NotImplementedException();
+    }
+}

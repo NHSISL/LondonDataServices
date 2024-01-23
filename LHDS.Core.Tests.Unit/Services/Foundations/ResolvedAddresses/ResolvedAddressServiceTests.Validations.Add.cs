2fda8fdd153d157d548e4b3c5675d66eb6332c9c
Christo du Toit <christo@dutoit.co.uk>
2024-01-23 09:42:46 +0000

    ShouldThrowValidationExceptionOnAddIfResolvedAddressIsInvalidAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/ResolvedAddresses/ResolvedAddressServiceTests.Validations.Add.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/ResolvedAddresses/ResolvedAddressServiceTests.Validations.Add.cs
@@ -32,7 +32,8 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.ResolvedAddresses
                     addResolvedAddressTask.AsTask);
 
             // then
-            actualResolvedAddressValidationException.Should().BeEquivalentTo(expectedResolvedAddressValidationException);
+            actualResolvedAddressValidationException.Should()
+                .BeEquivalentTo(expectedResolvedAddressValidationException);
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
@@ -42,6 +43,79 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.ResolvedAddresses
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfResolvedAddressIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given
+            var invalidResolvedAddress = new ResolvedAddress
+            {
+                // TODO:  Add default values for your properties i.e. Name = invalidText
+            };
+
+            var invalidResolvedAddressException =
+                new InvalidResolvedAddressException(
+                    message: "Invalid resolvedAddress. Please correct the errors and try again.");
+
+            invalidResolvedAddressException.AddData(
+                key: nameof(ResolvedAddress.Id),
+                values: "Id is required");
+
+            //invalidResolvedAddressException.AddData(
+            //    key: nameof(ResolvedAddress.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the ResolvedAddress model
+
+            invalidResolvedAddressException.AddData(
+                key: nameof(ResolvedAddress.CreatedDate),
+                values: "Date is required");
+
+            invalidResolvedAddressException.AddData(
+                key: nameof(ResolvedAddress.CreatedBy),
+                values: "Text is required");
+
+            invalidResolvedAddressException.AddData(
+                key: nameof(ResolvedAddress.UpdatedDate),
+                values: "Date is required");
+
+            invalidResolvedAddressException.AddData(
+                key: nameof(ResolvedAddress.UpdatedBy),
+                values: "Text is required");
+
+            var expectedResolvedAddressValidationException =
+                new ResolvedAddressValidationException(
+                    message: "ResolvedAddress validation errors occurred, please try again.",
+                    innerException: invalidResolvedAddressException);
+
+            // when
+            ValueTask<ResolvedAddress> addResolvedAddressTask =
+                this.resolvedAddressService.AddResolvedAddressAsync(invalidResolvedAddress);
+
+            ResolvedAddressValidationException actualResolvedAddressValidationException =
+                await Assert.ThrowsAsync<ResolvedAddressValidationException>(
+                    addResolvedAddressTask.AsTask);
+
+            // then
+            actualResolvedAddressValidationException.Should()
+                .BeEquivalentTo(expectedResolvedAddressValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedResolvedAddressValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertResolvedAddressAsync(It.IsAny<ResolvedAddress>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }
 }

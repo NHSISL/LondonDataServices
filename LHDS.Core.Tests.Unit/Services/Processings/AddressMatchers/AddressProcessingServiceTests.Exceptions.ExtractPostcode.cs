8fa3b6966bc18d254d0ed3504e0702842618899f
David Hayes <davidhayes03@googlemail.com>
2024-01-24 13:16:33 +0000

    ShouldThrowServiceExceptionOnExtractPostcodeIfServiceErrorOccursAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/AddressMatchers/AddressProcessingServiceTests.Exceptions.ExtractPostcode.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Processings.AddressMatchers.Exceptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.AddressMatchers
+{
+    public partial class AddressMatcherProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnExtractPostcodeIfServiceErrorOccursAsync()
+        {
+            // given
+            string someAddress = GetRandomString();
+            var serviceException = new Exception();
+
+            var failedAddressMartcherProcessingServiceException =
+                new FailedAddressMatcherProcessingServiceException(
+                    message: "Failed Address matcher processing service error occurred, contact support.",
+                    innerException: serviceException);
+
+            var expectedAddressMatcherProcessingServiveException =
+                new AddressMatcherProcessingServiceException(
+                    message: "Address matcher processing service error occurred, contact support.",
+                    innerException: failedAddressMartcherProcessingServiceException);
+
+            // when
+            Action addAddressMatcherTask = () =>
+                addressMatcherProcessingService.ExtractPostCode(someAddress);
+
+            AddressMatcherProcessingServiceException actualAddressMatcherProcessingServiceException =
+                Assert.Throws<AddressMatcherProcessingServiceException>(addAddressMatcherTask);
+
+            // then
+            actualAddressMatcherProcessingServiceException.Should()
+                .BeEquivalentTo(expectedAddressMatcherProcessingServiveException);
+        }
+    }
+}

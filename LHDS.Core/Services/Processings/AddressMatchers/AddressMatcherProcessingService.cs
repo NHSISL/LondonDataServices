708e9825a6cd2115650229655f1715850071a12e
David Hayes <davidhayes03@googlemail.com>
2024-01-24 11:19:43 +0000

    ShouldThrowValidationExceptionOnExtractPostcodeIfArgsIsInvalidOrEmptyStringAndLogItAsync -> PASS

--- LHDS.Core/Services/Processings/AddressMatchers/AddressMatcherProcessingService.cs
+++ LHDS.Core/Services/Processings/AddressMatchers/AddressMatcherProcessingService.cs
@@ -21,14 +21,16 @@ namespace LHDS.Core.Services.Processings.AddressMatchers
         public string CleanAddress(string address) =>
             throw new System.NotImplementedException();
 
-        public string ExtractPostCode(string address)
-        {
-            string pattern = @"\b([A-Z]{1,2}\d{1,2}[A-Z]?\s*\d[A-Z]{2})\b";
-            MatchCollection matches = Regex.Matches(address, pattern);
+        public string ExtractPostCode(string address) =>
+            TryCatch(() =>
+            {
+                ValidateAddress(address);
+                string pattern = @"\b([A-Z]{1,2}\d{1,2}[A-Z]?\s*\d[A-Z]{2})\b";
+                MatchCollection matches = Regex.Matches(address, pattern);
 
-            string extractedPostCode = matches[0].Groups[1].Value;
+                string extractedPostCode = matches[0].Groups[1].Value;
 
-            return extractedPostCode;
-        }
+                return extractedPostCode;
+            });
     }
 }

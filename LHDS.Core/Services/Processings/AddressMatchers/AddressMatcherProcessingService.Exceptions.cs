06ead6d042d68fdc2e8fdacb9a47ce9c67075a21
David Hayes <davidhayes03@googlemail.com>
2024-01-24 11:19:43 +0000

    ShouldThrowValidationExceptionOnExtractPostcodeIfArgsIsInvalidOrEmptyStringAndLogItAsync -> PASS

---
+++ LHDS.Core/Services/Processings/AddressMatchers/AddressMatcherProcessingService.Exceptions.cs
@@ -0,0 +1,38 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LHDS.Core.Models.Processings.AddressMatcher.Exceptions;
+using Xeptions;
+
+namespace LHDS.Core.Services.Processings.AddressMatchers
+{
+    public partial class AddressMatcherProcessingService : IAddressMatcherProcessingService
+    {
+        private delegate string ReturningExtractPostcodeFunction();
+
+        private string TryCatch(ReturningExtractPostcodeFunction returningExtractPostcodeFunction)
+        {
+            try
+            {
+                return returningExtractPostcodeFunction();
+            }
+            catch (InvalidArgumentAddressMatcherProcessingException invalidArgumentAddressMatcherProcessingException)
+            {
+                throw CreateAndLogValidationException(invalidArgumentAddressMatcherProcessingException);
+            }
+        }
+
+        private AddressMatcherProcessingValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var addressMatcherProcessingValidationException =
+                new AddressMatcherProcessingValidationException(
+                    message: "Address matcher processing validation errors occurred, please try again.",
+                    innerException: exception);
+
+            this.loggingBroker.LogError(addressMatcherProcessingValidationException);
+
+            return addressMatcherProcessingValidationException;
+        }
+    }
+}
---
+++ LHDS.Core/Services/Processings/AddressMatchers/AddressMatcherProcessingService.Validations.cs
@@ -0,0 +1,43 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using LHDS.Core.Models.Processings.AddressMatcher.Exceptions;
+using Xeptions;
+
+namespace LHDS.Core.Services.Processings.AddressMatchers
+{
+    public partial class AddressMatcherProcessingService : IAddressMatcherProcessingService
+    {
+
+        public void ValidateAddress(string address) =>
+            Validate<InvalidArgumentAddressMatcherProcessingException>(
+                message: "Invalid address matcher processing argument(s), please correct the errors and try again.",
+                (Rule: IsInvalid(address), Parameter: "address"));
+
+        private static dynamic IsInvalid(string text) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(text),
+            Message = "Text is required"
+        };
+
+        private static void Validate<T>(string message, params (dynamic Rule, string Parameter)[] validations)
+            where T : Xeption
+        {
+            var invalidDataException = (T)Activator.CreateInstance(typeof(T), message);
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidDataException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidDataException.ThrowIfContainsErrors();
+        }
+    }
+}

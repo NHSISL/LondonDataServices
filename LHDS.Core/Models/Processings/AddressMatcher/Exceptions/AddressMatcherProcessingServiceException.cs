d939f53e45293665f1e753475fbc174698429cde
David Hayes <davidhayes03@googlemail.com>
2024-01-24 13:16:33 +0000

    ShouldThrowServiceExceptionOnExtractPostcodeIfServiceErrorOccursAsync -> FAIL

---
+++ LHDS.Core/Models/Processings/AddressMatcher/Exceptions/AddressMatcherProcessingServiceException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Processings.AddressMatchers.Exceptions
+{
+    public class AddressMatcherProcessingServiceException : Xeption
+    {
+        public AddressMatcherProcessingServiceException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}

923a2ffa6f48a835d9fd54bbf57c9ef02ac83dfd
Christo du Toit <christo@dutoit.co.uk>
2024-01-26 09:07:09 +0000

    FOUNDATIONS: Extended normalisation to include the components

--- LHDS.Core/Models/Foundations/AddressNormalisations/AddressNormalisation.cs
+++ LHDS.Core/Models/Foundations/AddressNormalisations/AddressNormalisation.cs
@@ -1,6 +1,8 @@
-﻿// ---------------------------------------------------------------
+﻿// ---------------------------------------------------------
 // Copyright (c) North East London ICB. All rights reserved.
-// ---------------------------------------------------------------
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
 
 namespace LHDS.Core.Models.Foundations.AddressNormalisations
 {
@@ -8,5 +10,7 @@ namespace LHDS.Core.Models.Foundations.AddressNormalisations
     {
         public string? PostalAddress { get; set; }
         public string? JsonPostalAddress { get; set; }
+        public IList<KeyValuePair<string, string>> AddressComponents { get; set; }
+            = new List<KeyValuePair<string, string>>();
     }
 }

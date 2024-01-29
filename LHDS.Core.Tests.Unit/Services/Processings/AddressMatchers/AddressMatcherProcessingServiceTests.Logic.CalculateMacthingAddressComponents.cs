a60d1101165b861ab53096b04f0e1542135baad8
Christo du Toit <christo@dutoit.co.uk>
2024-01-29 14:40:03 +0000

    ShouldCalculateMacthingAddressComponents -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/AddressMatchers/AddressMatcherProcessingServiceTests.Logic.CalculateMacthingAddressComponents.cs
@@ -0,0 +1,40 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Processings.AddressMatchers;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.AddressMatchers
+{
+    public partial class AddressMatcherProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(AddressToMatch))]
+        public async Task ShouldCalculateMacthingAddressComponents(
+            List<KeyValuePair<string, string>> inputAddress,
+            AddressMatch potentialMatch,
+            int matchedComponents,
+            bool matchingCoreComponents
+            )
+        {
+            // given
+            List<KeyValuePair<string, string>> incomingAddress = inputAddress;
+            HashSet<AddressMatch> possibleAddresses = new HashSet<AddressMatch> { potentialMatch };
+
+            // when
+            HashSet<AddressMatch> actualAddressMatches = await addressMatcherProcessingService
+                .CalculateMacthingAddressComponents(incomingAddress, possibleAddresses);
+
+            AddressMatch actualAddressMatch = actualAddressMatches.First();
+
+            // then
+            actualAddressMatch.MatchedComponents.Should().Be(matchedComponents);
+            actualAddressMatch.MatchingCoreComponents.Should().Be(matchingCoreComponents);
+        }
+    }
+}
--- LHDS.Core/Services/Processings/AddressMatchers/IAddressMatcherProcessingService.cs
+++ LHDS.Core/Services/Processings/AddressMatchers/IAddressMatcherProcessingService.cs
@@ -2,11 +2,19 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Processings.AddressMatchers;
+
 namespace LHDS.Core.Services.Processings.AddressMatchers
 {
     public interface IAddressMatcherProcessingService
     {
         string CleanAddress(string address);
         string ExtractPostCode(string address);
+
+        ValueTask<HashSet<AddressMatch>> CalculateMacthingAddressComponents(
+            IList<KeyValuePair<string, string>> incomingAddress,
+            HashSet<AddressMatch> possibleAddresses);
     }
 }

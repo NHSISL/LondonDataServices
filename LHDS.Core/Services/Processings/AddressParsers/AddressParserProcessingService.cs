b851286051c9de0b402e53435aec95be40c90f14
David Hayes <davidhayes03@googlemail.com>
2024-01-30 12:12:11 +0000

    ShouldGetParsedAddressByByte -> FAIL

---
+++ LHDS.Core/Services/Processings/AddressParsers/AddressParserProcessingService.cs
@@ -0,0 +1,34 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Foundations.AddressNormalisations;
+using LHDS.Core.Services.Foundations.AddressNormalisations;
+using LHDS.Core.Services.Foundations.AddressParsers;
+
+namespace LHDS.Core.Services.Processings.AddressParsers
+{
+    public partial class AddressParserProcessingService : IAddressParserProcessingService
+    {
+        private readonly IAddressParserService addressParserService;
+        private readonly ILoggingBroker loggingBroker;
+
+        public AddressParserProcessingService(
+            IAddressParserService addressParserService,
+            ILoggingBroker loggingBroker)
+        {
+            this.addressParserService = addressParserService;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<List<Address>> ProcessCsvAsync(byte[] data) =>
+            throw new System.NotImplementedException();
+
+        public ValueTask<List<Address>> ProcessCsvAsync(string data) =>
+            throw new System.NotImplementedException();
+    }
+}
---
+++ LHDS.Core/Services/Processings/AddressParsers/IAddressParserProcessingService.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Addresses;
+
+namespace LHDS.Core.Services.Processings.AddressParsers
+{
+    public interface IAddressParserProcessingService
+    {
+        ValueTask<List<Address>> ProcessCsvAsync(byte[] data);
+        ValueTask<List<Address>> ProcessCsvAsync(string data);
+    }
+}

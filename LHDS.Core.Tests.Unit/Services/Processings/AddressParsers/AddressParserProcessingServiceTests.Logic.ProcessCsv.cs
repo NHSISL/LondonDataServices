5453ef6925b21c485881952d2f8f94c4caa46472
David Hayes <davidhayes03@googlemail.com>
2024-01-30 15:11:42 +0000

    ShouldGetParsedAddressByByte -> PASS

--- LHDS.Core.Tests.Unit/Services/Processings/AddressParsers/AddressParserProcessingServiceTests.Logic.ProcessCsv.cs
+++ LHDS.Core.Tests.Unit/Services/Processings/AddressParsers/AddressParserProcessingServiceTests.Logic.ProcessCsv.cs
@@ -3,6 +3,7 @@
 // ---------------------------------------------------------------
 
 using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Force.DeepCloner;
@@ -15,6 +16,36 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.AddressParsers
 {
     public partial class AddressParserProcessingServiceTests
     {
+        [Fact]
+        public async Task ShouldGetParsedAddressByByte()
+        {
+            // Given
+            var randomAddress = Encoding.GetEncoding("UTF-8").GetBytes(GetRandomString());
+            byte[] inputAddress = randomAddress;
+
+            List<Address> randomAddresses = new List<Address>();
+            List<Address> storageAddresses = randomAddresses;
+            List<Address> expectedAdresses = storageAddresses.DeepClone();
+
+            this.addressParserServiceMock.Setup(service =>
+                service.ProcessCsvAsync(inputAddress))
+                    .ReturnsAsync(randomAddresses);
+
+            // When
+            List<Address> actualParserAddress = 
+                await this.addressParserProcessingService.ProcessCsvAsync(inputAddress);
+
+            // Then
+            actualParserAddress.Should().BeEquivalentTo(expectedAdresses);
+
+            this.addressParserServiceMock.Verify(service =>
+                service.ProcessCsvAsync(It.IsAny<byte[]>()),
+                    Times.Once);
+
+            this.addressParserServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
         [Fact]
         public async Task ShouldGetParsedAddressByString()
         {
@@ -31,7 +62,7 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.AddressParsers
                     .ReturnsAsync(randomAddresses);
 
             // When
-            List<Address> actualParserAddress = 
+            List<Address> actualParserAddress =
                 await this.addressParserProcessingService.ProcessCsvAsync(inputAddress);
 
             // Then

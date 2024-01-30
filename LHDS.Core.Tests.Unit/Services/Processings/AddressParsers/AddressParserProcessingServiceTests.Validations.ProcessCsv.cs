a7b5b836719c4d64e61dfa4e3034614bf02428c5
David Hayes <davidhayes03@googlemail.com>
2024-01-30 15:14:57 +0000

    ShouldThrowValidationExceptionOnProcessCsvIfDataIsNullAndLogItAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Processings/AddressParsers/AddressParserProcessingServiceTests.Validations.ProcessCsv.cs
+++ LHDS.Core.Tests.Unit/Services/Processings/AddressParsers/AddressParserProcessingServiceTests.Validations.ProcessCsv.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
 using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Foundations.AddressParsers.Exceptions;
 using LHDS.Core.Models.Processings.AddressParsers.Exceptions;
 using Moq;
 using Xunit;
@@ -15,12 +16,45 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.AddressParsers
 {
     public partial class AddressParserProcessingServiceTests
     {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnProcessCsvIfDataIsNullAndLogItAsync()
+        {
+            // given
+            byte[] nullAddresses = null;
+
+            var invalidArgumentAddressParserProcessingException =
+                new InvalidArgumentAddressParserProcessingException(message: "Address parser is null.");
+
+            var expectedAddressParserProcessingValidationException =
+                new AddressParserProcessingValidationException(
+                    message: "Address parser processing validation errors occurred, please try again.",
+                    innerException: invalidArgumentAddressParserProcessingException);
+
+            // when
+            ValueTask<List<Address>> processCSVAddressTask =
+                this.addressParserProcessingService.ProcessCsvAsync(nullAddresses);
+
+            AddressParserProcessingValidationException actualAddressParserProcessingValidationException =
+                await Assert.ThrowsAsync<AddressParserProcessingValidationException>(async () =>
+                    await processCSVAddressTask);
+
+            // then
+            actualAddressParserProcessingValidationException.Should().BeEquivalentTo(expectedAddressParserProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAddressParserProcessingValidationException))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
         [Theory]
         [InlineData(null)]
         [InlineData("")]
         [InlineData(" ")]
-        public async Task ShouldThrowValidationExceptionOnProcessIfAddressIsNullAndLogItAsync(
-            string invalidText)
+        public async Task ShouldThrowValidationExceptionOnProcessIDataIsInvalidAndLogItAsync(
+        string invalidText)
         {
             // given
             string invalidAddress = invalidText;
@@ -43,7 +77,7 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.AddressParsers
                 addressParserProcessingService.ProcessCsvAsync(invalidAddress);
 
             AddressParserProcessingValidationException actualAddressParserValidationException =
-                await Assert.ThrowsAsync<AddressParserProcessingValidationException>(() => 
+                await Assert.ThrowsAsync<AddressParserProcessingValidationException>(() =>
                 addressParserTask.AsTask());
 
             // then

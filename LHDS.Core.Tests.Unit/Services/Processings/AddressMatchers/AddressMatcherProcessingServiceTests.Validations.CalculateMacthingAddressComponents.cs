698f96db52e8ad3a04407ecc2b97797fafd22689
Christo du Toit <christo@dutoit.co.uk>
2024-01-30 11:57:36 +0000

    ShouldThrowValidationExceptionOnCalculateMacthingAddressIfArgsIsInvalidAndLogItAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Processings/AddressMatchers/AddressMatcherProcessingServiceTests.Validations.CalculateMacthingAddressComponents.cs
@@ -0,0 +1,66 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Processings.AddressMatchers;
+using LHDS.Core.Models.Processings.AddressMatchers.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.AddressMatchers
+{
+    public partial class AddressMatcherProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnCalculateMacthingAddressIfArgsIsInvalidAndLogItAsync()
+        {
+            // given
+            List<KeyValuePair<string, string>> invalidIncomingAddressComponents =
+                new List<KeyValuePair<string, string>>();
+
+            HashSet<AddressMatch> invalidPossibleAddresses = new HashSet<AddressMatch>();
+
+            var invalidArgumentAddressMatcherException =
+                new InvalidArgumentAddressMatcherProcessingException(
+                    message: "Invalid address matcher processing argument(s), " +
+                    "please correct the errors and try again.");
+
+            invalidArgumentAddressMatcherException.AddData(
+                key: "IncomingAddressComponents",
+                values: "Values is required");
+
+            invalidArgumentAddressMatcherException.AddData(
+                key: "PossibleAddresses",
+                values: "Values is required");
+
+            var expectedAddressMatcherValidationException =
+                new AddressMatcherProcessingValidationException(
+                    message: "Address matcher processing validation errors occurred, please try again.",
+                    innerException: invalidArgumentAddressMatcherException);
+
+            // when
+            ValueTask<HashSet<AddressMatch>> calculateMacthingAddressComponentsAction =
+                addressMatcherProcessingService.CalculateMacthingAddressComponents(
+                    incomingAddressComponents: invalidIncomingAddressComponents,
+                    possibleAddresses: invalidPossibleAddresses);
+
+            AddressMatcherProcessingValidationException actualAddressMatcherValidationException =
+                await Assert.ThrowsAsync<AddressMatcherProcessingValidationException>(
+                    calculateMacthingAddressComponentsAction.AsTask);
+
+            // then
+            actualAddressMatcherValidationException.Should()
+                .BeEquivalentTo(expectedAddressMatcherValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAddressMatcherValidationException))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

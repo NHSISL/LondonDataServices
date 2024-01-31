8a9f7890428bd0f5fef7002a192c3eda5af85acc
David Hayes <davidhayes03@googlemail.com>
2024-01-31 15:39:32 +0000

    ORCHESTRATION: Implement Interface

---
+++ LHDS.Core/Services/Orchestrations/AddressResolving/AddressResolvingOrchestrationService.cs
@@ -0,0 +1,43 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Foundations.AddressNormalisations;
+using LHDS.Core.Services.Processings.Addresses;
+using LHDS.Core.Services.Processings.AddressMatchers;
+using LHDS.Core.Services.Processings.ResolvedAddresses;
+
+namespace LHDS.Core.Services.Orchestrations.AddressResolvings
+{
+    internal partial class AddressResolvingOrchestrationService : IAddressResolvingOrchestrationService
+    {
+        private readonly IAddressProcessingService addressProcessingService;
+        private readonly IAddressMatcherProcessingService addressMatcherProcessingService;
+        private readonly IResolvedAddressProcessingService resolvedAddressProcessingService;
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+
+        public AddressResolvingOrchestrationService(
+            IAddressProcessingService addressProcessingService,
+            IAddressMatcherProcessingService addressMatcherProcessingService,
+            IResolvedAddressProcessingService resolvedAddressProcessingService,
+            ILoggingBroker loggingBroker,
+            IDateTimeBroker dateTimeBroker)
+        {
+            this.addressProcessingService = addressProcessingService;
+            this.addressMatcherProcessingService = addressMatcherProcessingService;
+            this.resolvedAddressProcessingService = resolvedAddressProcessingService;
+            this.loggingBroker = loggingBroker;
+            this.dateTimeBroker = dateTimeBroker;
+        }
+
+        public ValueTask<List<Address>> ProcessAsync(List<AddressNormalisation> addressNormalisation) =>
+            throw new NotImplementedException();
+    }
+}
---
+++ LHDS.Core/Services/Orchestrations/AddressResolving/IAddressResolvingOrchestrationService.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Foundations.AddressNormalisations;
+
+namespace LHDS.Core.Services.Orchestrations.AddressResolvings
+{
+    public interface IAddressResolvingOrchestrationService
+    {
+        ValueTask<List<Address>> ProcessAsync(List<AddressNormalisation> addressNormalisation);
+    }
+}

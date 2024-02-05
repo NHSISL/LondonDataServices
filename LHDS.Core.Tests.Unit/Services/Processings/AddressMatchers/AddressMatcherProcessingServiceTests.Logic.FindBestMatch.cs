2114ac893bb78715f7e6af24a13d733391a9c7a4
Christo du Toit <christo@dutoit.co.uk>
2024-02-05 15:32:09 +0000

    ShouldAttemptFindBestMatchOnMultipleWithSuccess -> FAIL

--- LHDS.Core.Tests.Unit/Services/Processings/AddressMatchers/AddressMatcherProcessingServiceTests.Logic.FindBestMatch.cs
+++ LHDS.Core.Tests.Unit/Services/Processings/AddressMatchers/AddressMatcherProcessingServiceTests.Logic.FindBestMatch.cs
@@ -15,14 +15,136 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.AddressMatchers
     public partial class AddressMatcherProcessingServiceTests
     {
         [Fact]
-        public async Task ShouldAttemptFindBestMatchOnNone()
+        public async Task ShouldAttemptFindBestMatchOnNoneWithSuccess()
         {
+            // given
+            HashSet<AddressMatch> inputAddressMatches = CreateNoBestMatch();
+            HashSet<AddressMatch> outputAddressMatches = inputAddressMatches;
+            HashSet<AddressMatch> cleanedOutputAddressMatches = CreateSingleBestMatch();
 
+            AddressMatch expectedAddressMatch = cleanedOutputAddressMatches.FirstOrDefault();
+            List<KeyValuePair<string, string>> inputAddressComponents = CreateRandomKeyValuePairList();
+            List<KeyValuePair<string, string>> outputAddressComponents = CreateRandomKeyValuePairList();
+
+            this.addressMatcherServiceMock.Setup(service =>
+                service.CalculateMatchingAddressComponents(inputAddressComponents, inputAddressMatches))
+                    .Returns(outputAddressMatches);
+
+            this.addressMatcherServiceMock.Setup(service =>
+                service.CheckForBestMatch(outputAddressMatches))
+                    .Returns(BestMatchEnum.None);
+
+            this.addressMatcherServiceMock.Setup(service =>
+                service.RemoveNonDigitCharactersFromHouseNumber(inputAddressComponents))
+                    .Returns(outputAddressComponents);
+
+            this.addressMatcherServiceMock.Setup(service =>
+                service.CalculateMatchingAddressComponents(outputAddressComponents, inputAddressMatches))
+                    .Returns(cleanedOutputAddressMatches);
+
+            this.addressMatcherServiceMock.Setup(service =>
+                service.CheckForBestMatch(cleanedOutputAddressMatches))
+                    .Returns(BestMatchEnum.Single);
+
+            // when
+            AddressMatch actualAddressMatch = await this.addressMatcherProcessingService
+                .FindBestMatch(inputAddressMatches, inputAddressComponents);
+
+            // then
+            actualAddressMatch.Should().BeEquivalentTo(expectedAddressMatch);
+
+            this.addressMatcherServiceMock.Verify(service =>
+                service.CalculateMatchingAddressComponents(inputAddressComponents, inputAddressMatches),
+                    Times.Once);
+
+            this.addressMatcherServiceMock.Verify(service =>
+                service.CheckForBestMatch(outputAddressMatches),
+                    Times.Once);
+
+            this.addressMatcherServiceMock.Verify(service =>
+                service.RemoveNonDigitCharactersFromHouseNumber(inputAddressComponents),
+                    Times.Once);
+
+            this.addressMatcherServiceMock.Verify(service =>
+                service.CalculateMatchingAddressComponents(outputAddressComponents, inputAddressMatches),
+                    Times.Once);
+
+            this.addressMatcherServiceMock.Verify(service =>
+                service.CheckForBestMatch(cleanedOutputAddressMatches),
+                    Times.Once);
+
+            this.addressMatcherServiceMock.VerifyNoOtherCalls();
         }
 
+        [Fact]
+        public async Task ShouldAttemptFindBestMatchOnNoneWithoutSuccess()
+        {
+            // given
+            HashSet<AddressMatch> inputAddressMatches = CreateNoBestMatch();
+            HashSet<AddressMatch> outputAddressMatches = inputAddressMatches;
+            HashSet<AddressMatch> cleanedOutputAddressMatches = CreateSingleBestMatch();
+
+            AddressMatch expectedAddressMatch = new AddressMatch
+            {
+                IsMatched = false,
+                BestMatch = BestMatchEnum.None
+            };
+
+            List<KeyValuePair<string, string>> inputAddressComponents = CreateRandomKeyValuePairList();
+            List<KeyValuePair<string, string>> outputAddressComponents = CreateRandomKeyValuePairList();
+
+            this.addressMatcherServiceMock.Setup(service =>
+                service.CalculateMatchingAddressComponents(inputAddressComponents, inputAddressMatches))
+                    .Returns(outputAddressMatches);
+
+            this.addressMatcherServiceMock.Setup(service =>
+                service.CheckForBestMatch(outputAddressMatches))
+                    .Returns(BestMatchEnum.None);
+
+            this.addressMatcherServiceMock.Setup(service =>
+                service.RemoveNonDigitCharactersFromHouseNumber(inputAddressComponents))
+                    .Returns(outputAddressComponents);
+
+            this.addressMatcherServiceMock.Setup(service =>
+                service.CalculateMatchingAddressComponents(outputAddressComponents, inputAddressMatches))
+                    .Returns(cleanedOutputAddressMatches);
+
+            this.addressMatcherServiceMock.Setup(service =>
+                service.CheckForBestMatch(cleanedOutputAddressMatches))
+                    .Returns(BestMatchEnum.None);
+
+            // when
+            AddressMatch actualAddressMatch = await this.addressMatcherProcessingService
+                .FindBestMatch(inputAddressMatches, inputAddressComponents);
+
+            // then
+            actualAddressMatch.Should().BeEquivalentTo(expectedAddressMatch);
+
+            this.addressMatcherServiceMock.Verify(service =>
+                service.CalculateMatchingAddressComponents(inputAddressComponents, inputAddressMatches),
+                    Times.Once);
+
+            this.addressMatcherServiceMock.Verify(service =>
+                service.CheckForBestMatch(outputAddressMatches),
+                    Times.Once);
+
+            this.addressMatcherServiceMock.Verify(service =>
+                service.RemoveNonDigitCharactersFromHouseNumber(inputAddressComponents),
+                    Times.Once);
+
+            this.addressMatcherServiceMock.Verify(service =>
+                service.CalculateMatchingAddressComponents(outputAddressComponents, inputAddressMatches),
+                    Times.Once);
+
+            this.addressMatcherServiceMock.Verify(service =>
+                service.CheckForBestMatch(cleanedOutputAddressMatches),
+                    Times.Once);
+
+            this.addressMatcherServiceMock.VerifyNoOtherCalls();
+        }
 
         [Fact]
-        public async Task ShouldAttemptFindBestMatchOnSingle()
+        public async Task ShouldAttemptFindBestMatchOnSingleWithSuccess()
         {
             // given
             HashSet<AddressMatch> inputAddressMatches = CreateSingleBestMatch();
@@ -58,12 +180,18 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.AddressMatchers
         }
 
         [Fact]
-        public async Task ShouldAttemptFindBestMatchOnMultiple()
+        public async Task ShouldAttemptFindBestMatchOnMultipleWithSuccess()
         {
             // given
-            HashSet<AddressMatch> inputAddressMatches = CreateMultipleBestMatch();
-            HashSet<AddressMatch> outputAddressMatches = CreateMultipleBestMatch();
-            AddressMatch expectedAddressMatch = outputAddressMatches.FirstOrDefault();
+            HashSet<AddressMatch> inputAddressMatches = CreateSingleBestMatch();
+            HashSet<AddressMatch> outputAddressMatches = inputAddressMatches;
+
+            AddressMatch expectedAddressMatch = new AddressMatch
+            {
+                IsMatched = false,
+                BestMatch = BestMatchEnum.None
+            };
+
             List<KeyValuePair<string, string>> randomAddressComponents = CreateKeyValuePairList();
             List<KeyValuePair<string, string>> inputAddressComponents = randomAddressComponents;
 
@@ -71,12 +199,18 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.AddressMatchers
                 service.CalculateMatchingAddressComponents(inputAddressComponents, inputAddressMatches))
                     .Returns(outputAddressMatches);
 
+            this.addressMatcherServiceMock.Setup(service =>
+                service.CheckForBestMatch(outputAddressMatches))
+                    .Returns(BestMatchEnum.Multiple);
+
             // when
             AddressMatch actualAddressMatch = await this.addressMatcherProcessingService
                 .FindBestMatch(inputAddressMatches, inputAddressComponents);
 
             // then
             actualAddressMatch.Should().BeEquivalentTo(expectedAddressMatch);
+
+            this.addressMatcherServiceMock.VerifyNoOtherCalls();
         }
     }
 }

5743cb8eac1418250fecdf484dc5c7cb32c950fa
Hassan Raza <h_raza110@outlook.com>
2024-02-05 10:58:27 +0000

    MODELS: Subscriber Agreements

---
+++ LHDS.Core/Brokers/Storages/Sql/StorageBroker.SubscriberAgreement.Configurations.cs
@@ -0,0 +1,76 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using LHDS.Core.Models.Foundations.SubscriberAgreements;
+using Microsoft.EntityFrameworkCore;
+
+namespace LHDS.Core.Brokers.Storages.Sql
+{
+    public partial class StorageBroker
+    {
+        private static void AddSubscriberAgreementConfigurations(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SubscriberAgreement>()
+                .ToTable("SharingAgreements", "Configurations");
+
+            modelBuilder.Entity<SubscriberAgreement>()
+                .Property(subscriberAgreement => subscriberAgreement.SupplierSharingAgreementId)
+                .IsRequired();
+
+            modelBuilder.Entity<SubscriberAgreement>()
+                .HasIndex(subscriberAgreement => subscriberAgreement.SupplierSharingAgreementId)
+                .IsUnique();
+
+            modelBuilder.Entity<SubscriberAgreement>()
+                .Property(subscriberAgreement => subscriberAgreement.SupplierSharingAgreementGuid)
+                .IsRequired();
+
+            modelBuilder.Entity<SubscriberAgreement>()
+                .Property(subscriberAgreement => subscriberAgreement.FtpUserName)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            modelBuilder.Entity<SubscriberAgreement>()
+                .Property(subscriberAgreement => subscriberAgreement.FtpPublicKey)
+                .HasMaxLength(128)
+                .IsRequired();
+
+            modelBuilder.Entity<SubscriberAgreement>()
+                .Property(subscriberAgreement => subscriberAgreement.GpgPublicKey)
+                .HasMaxLength(128)
+                .IsRequired();
+
+            modelBuilder.Entity<SubscriberAgreement>()
+                .Property(dataSet => dataSet.IsActive)
+                .HasDefaultValue(false)
+                .IsRequired();
+
+            modelBuilder.Entity<SubscriberAgreement>()
+                .Property(dataSet => dataSet.LastPollStartDate)
+                .IsRequired();
+
+            modelBuilder.Entity<SubscriberAgreement>()
+                .Property(dataSet => dataSet.LastPollEndDate)
+                .IsRequired();
+
+            modelBuilder.Entity<SubscriberAgreement>()
+                .Property(subscriberAgreement => subscriberAgreement.CreatedBy)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            modelBuilder.Entity<SubscriberAgreement>()
+                .Property(subscriberAgreement => subscriberAgreement.CreatedDate)
+                .IsRequired();
+
+            modelBuilder.Entity<SubscriberAgreement>()
+                .Property(subscriberAgreement => subscriberAgreement.UpdatedBy)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            modelBuilder.Entity<SubscriberAgreement>()
+                .Property(subscriberAgreement => subscriberAgreement.UpdatedDate)
+                .IsRequired();
+        }
+    }
+}

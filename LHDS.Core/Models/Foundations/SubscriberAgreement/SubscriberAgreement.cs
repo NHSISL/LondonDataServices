e34b4958a8572e0d1c1a16841424d215ef753750
Hassan Raza <h_raza110@outlook.com>
2024-02-05 10:58:27 +0000

    MODELS: Subscriber Agreements

---
+++ LHDS.Core/Models/Foundations/SubscriberAgreement/SubscriberAgreement.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using LHDS.Core.Models.Bases;
+
+namespace LHDS.Core.Models.Foundations.SubscriberAgreements
+{
+    public class SubscriberAgreement : IKey, IAudit
+    {
+        public Guid Id { get; set; }
+        public Guid SupplierSharingAgreementId { get; set; }
+        public Guid SupplierSharingAgreementGuid { get; set; }
+        public string FtpUserName { get; set; }
+        public string FtpPublicKey { get; set; }
+        public string GpgPublicKey { get; set; }
+        public bool IsActive { get; set; }
+        public DateTimeOffset LastPollStartDate { get; set; }
+        public DateTimeOffset LastPollEndDate { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public string UpdatedBy { get; set; } = string.Empty;
+        public DateTimeOffset UpdatedDate { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}

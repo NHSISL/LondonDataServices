1af244e8ab25a33fae29f1fc632c4ea3129faf3d
Christo du Toit <christo@dutoit.co.uk>
2024-02-06 09:17:14 +0000

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/SubscriberAgreements/Exceptions/SubscriberAgreementDependencyException.cs
@@ -0,0 +1,11 @@
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.SubscriberAgreements.Exceptions
+{
+    public class SubscriberAgreementDependencyException : Xeption
+    {
+        public SubscriberAgreementDependencyException(string message, Xeption innerException) 
+            : base(message, innerException)
+        { }
+    }
+}

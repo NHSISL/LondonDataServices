06e15d24f9fbba443b346d20c82004e7c8eca15d
Christo du Toit <christo@dutoit.co.uk>
2024-02-06 09:17:17 +0000

    ShouldThrowValidationExceptionOnAddIfReferenceErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/SubscriberAgreements/Exceptions/InvalidSubscriberAgreementReferenceException.cs
@@ -0,0 +1,11 @@
+using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.SubscriberAgreements.Exceptions
+{
+    public class InvalidSubscriberAgreementReferenceException : Xeption
+    {
+        public InvalidSubscriberAgreementReferenceException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}

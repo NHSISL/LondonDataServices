ca7d495529bc090cc4b97829aefad6626fa9c8a0
Hassan Raza <h_raza110@outlook.com>
2024-02-06 11:12:59 +0000

    Correcting to Secure Data broker

---
+++ LHDS.Core/Brokers/SecureData/SecureDataBroker.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using LHDS.Core.Models.Brokers.SecureData;
+
+namespace LHDS.Core.Brokers.KeyVaults
+{
+    public class SecureDataBroker : ISecureDataBroker
+    {
+        private readonly SecretClient secretClient;
+
+        public SecureDataBroker(string keyVaultUri)
+        {
+            secretClient = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential());
+        }
+
+        public async ValueTask<KeyVaultSecret> CreateOrUpdateKeyVaultSecretAsync(SecureData keyVaultSecret) =>
+            this.secretClient.SetSecret(keyVaultSecret.Name, keyVaultSecret.Value);
+    }
+}

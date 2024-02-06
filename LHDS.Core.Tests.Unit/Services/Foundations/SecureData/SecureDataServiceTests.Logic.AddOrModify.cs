ebb1d6bb9cd1b6e8a21cee0c3221c23c649a058f
Hassan Raza <h_raza110@outlook.com>
2024-02-06 15:21:43 +0000

    ShouldAddOrModifySecureDataAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/SecureData/SecureDataServiceTests.Logic.AddOrModify.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using LHDS.Core.Models.Foundations.Addresses;
+using Xunit;
+using Azure.Security.KeyVault.Secrets;
+using LHDS.Core.Models.Foundations.SecureData;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.SecureDatas
+{
+    public partial class SecureDataServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddOrModifySecureDataAsync()
+        {
+            // given
+            dynamic randomSecret = CreateRandomSecret();
+            KeyVaultSecret inputKeyVaultSecret = CreateKeyVaultSecretFromRandomSecret(randomSecret);
+            KeyVaultSecret outputKeyVaultSecret = inputKeyVaultSecret.DeepClone();
+            SecureData inputSecureData = CreateSecureDataFromRandomSecret(randomSecret);
+            SecureData expectedSecureData = CreateSecureDataFromRandomSecret(randomSecret);
+
+            this.secureDataBrokerMock.Setup(broker =>
+                broker.CreateOrUpdateKeyVaultSecretAsync(inputKeyVaultSecret))
+                    .ReturnsAsync(outputKeyVaultSecret);
+
+            // when
+            SecureData actualSecureData = await this.secureDataService
+                .AddOrModifySecureData(inputSecureData);
+
+            // then
+            actualSecureData.Should().BeEquivalentTo(expectedSecureData);
+
+            this.secureDataBrokerMock.Verify(broker =>
+                broker.CreateOrUpdateKeyVaultSecretAsync(inputKeyVaultSecret),
+                    Times.Once());
+
+            this.secureDataBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

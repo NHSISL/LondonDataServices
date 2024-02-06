049ccd3e618e36382073a0b8182a6a2e56d0d30c
Christo du Toit <christo@dutoit.co.uk>
2024-02-06 09:17:46 +0000

    ShouldThrowNotFoundExceptionOnRetrieveByIdIfSubscriberAgreementIsNotFoundAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/SubscriberAgreements/Exceptions/NotFoundSubscriberAgreementException.cs
@@ -0,0 +1,12 @@
+using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.SubscriberAgreements.Exceptions
+{
+    public class NotFoundSubscriberAgreementException : Xeption
+    {
+        public NotFoundSubscriberAgreementException(Guid subscriberAgreementId)
+            : base(message: $"Couldn't find subscriberAgreement with subscriberAgreementId: {subscriberAgreementId}.")
+        { }
+    }
+}

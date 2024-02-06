ad44786b0f4702cb71d8e600007e46c5ec410fc8
Hassan Raza <h_raza110@outlook.com>
2024-02-06 17:42:55 +0000

    ShouldThrowValidationExceptionAddOrModifyIfSecureDataIsInvalidAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/SecureData/SecureDataServiceTests.Validations.AddOrModify.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/SecureData/SecureDataServiceTests.Validations.AddOrModify.cs
@@ -1,14 +1,14 @@
-using System;
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
 using System.Threading.Tasks;
 using FluentAssertions;
-using Force.DeepCloner;
-using Moq;
-using LHDS.Core.Models.Foundations.Addresses;
-using Xunit;
-using Azure.Security.KeyVault.Secrets;
-using LHDS.Core.Models.Foundations.SecureData;
 using LHDS.Core.Models.Foundations.Addresses.Exceptions;
+using LHDS.Core.Models.Foundations.SecureData;
 using LHDS.Core.Models.Foundations.SecureData.Exceptions;
+using Moq;
+using Xunit;
 
 namespace LHDS.Core.Tests.Unit.Services.Foundations.SecureDatas
 {
@@ -40,6 +40,57 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.SecureDatas
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(expectedSecureDataValidationException))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.secureDataBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionAddOrModifyIfSecureDataIsInvalidAsync(string invalidText)
+        {
+            // given
+            var invalidSecureData = new SecureData
+            {
+                Name = invalidText,
+                Value = invalidText
+            };
+
+            var invalidAddressException =
+                new InvalidSecureDataException(
+                    message: "Invalid secure data. Please correct the errors and try again.");
+
+            invalidAddressException.AddData(
+                key: nameof(SecureData.Name),
+                values: "Text is required");
+
+            invalidAddressException.AddData(
+                key: nameof(SecureData.Value),
+                values: "Text is required");
+
+            var expectedSecureDataValidationException =
+                new SecureDataValidationException(
+                    message: "Secure data validation errors occurred, please try again.",
+                    innerException: invalidAddressException);
+
+            // when
+            ValueTask<SecureData> addSecureDataTask =
+                this.secureDataService.AddOrModifySecureData(invalidSecureData);
+
+            SecureDataValidationException actualSecureDataValidationException =
+                await Assert.ThrowsAsync<SecureDataValidationException>(() =>
+                    addSecureDataTask.AsTask());
+
+            // then
+            actualSecureDataValidationException.Should()
+                .BeEquivalentTo(expectedSecureDataValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSecureDataValidationException))),
                         Times.Once);
 
             this.loggingBrokerMock.VerifyNoOtherCalls();

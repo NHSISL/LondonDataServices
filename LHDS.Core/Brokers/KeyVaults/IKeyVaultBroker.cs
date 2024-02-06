7a7016e263245cf64a680981b35d586ae9b9e3c5
Hassan Raza <h_raza110@outlook.com>
2024-02-06 10:47:47 +0000

    BROKER: Create Or Update Key Vault Secret Async

---
+++ LHDS.Core/Brokers/KeyVaults/IKeyVaultBroker.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using LHDS.Core.Models.Brokers.KeyVaults;
+
+namespace LHDS.Core.Brokers.KeyVaults
+{
+    public interface IKeyVaultBroker
+    {
+        ValueTask<KeyVaultSecret> CreateOrUpdateKeyVaultSecretAsync(KeyVaultSecret keyVaultSecret);
+    }
+}

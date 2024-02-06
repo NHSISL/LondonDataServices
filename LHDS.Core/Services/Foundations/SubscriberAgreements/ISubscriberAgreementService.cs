a2c91657d402621f96f82dfaac9fcd84974b089d
Christo du Toit <christo@dutoit.co.uk>
2024-02-06 09:17:30 +0000

    ShouldReturnSubscriberAgreement -> FAIL

--- LHDS.Core/Services/Foundations/SubscriberAgreements/ISubscriberAgreementService.cs
+++ LHDS.Core/Services/Foundations/SubscriberAgreements/ISubscriberAgreementService.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Models.Foundations.SubscriberAgreements;
 
@@ -6,5 +7,6 @@ namespace LHDS.Core.Services.Foundations.SubscriberAgreements
     public interface ISubscriberAgreementService
     {
         ValueTask<SubscriberAgreement> AddSubscriberAgreementAsync(SubscriberAgreement subscriberAgreement);
+        IQueryable<SubscriberAgreement> RetrieveAllSubscriberAgreements();
     }
 }

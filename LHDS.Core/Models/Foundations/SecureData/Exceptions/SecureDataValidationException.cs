07d0d666c922eaffaa89e3771d40c3dec18adb3b
Hassan Raza <h_raza110@outlook.com>
2024-02-07 14:12:18 +0000

    Adding copyright

--- LHDS.Core/Models/Foundations/SecureData/Exceptions/SecureDataValidationException.cs
+++ LHDS.Core/Models/Foundations/SecureData/Exceptions/SecureDataValidationException.cs
@@ -1,3 +1,7 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
 using Xeptions;
 
 namespace LHDS.Core.Models.Foundations.SecureData.Exceptions
@@ -5,7 +9,7 @@ namespace LHDS.Core.Models.Foundations.SecureData.Exceptions
     public class SecureDataValidationException : Xeption
     {
         public SecureDataValidationException(string message, Xeption innerException)
-            : base(message,innerException)
+            : base(message, innerException)
         { }
     }
 }

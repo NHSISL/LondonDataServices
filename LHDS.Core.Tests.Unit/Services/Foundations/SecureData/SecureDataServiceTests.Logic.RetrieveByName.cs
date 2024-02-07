9a947ed24de5edf01126e6dea2e836ced3509ead
Hassan Raza <h_raza110@outlook.com>
2024-02-07 15:22:29 +0000

    ShouldRetrieveSecureDataByNameAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/SecureData/SecureDataServiceTests.Logic.RetrieveByName.cs
@@ -0,0 +1,44 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using Azure.Security.KeyVault.Secrets;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.SecureData;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.SecureDatas
+{
+    public partial class SecureDataServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveSecureDataByNameAsync()
+        {
+            // given
+            dynamic randomSecret = CreateRandomSecret();
+            string secretName = randomSecret.Name;
+            KeyVaultSecret outputKeyVaultSecret = CreateKeyVaultSecretFromRandomSecret(randomSecret);
+            SecureData expectedSecureData = CreateSecureDataFromRandomSecret(randomSecret);
+
+            this.secureDataBrokerMock.Setup(broker =>
+                broker.GetKeyVaultSecretAsync(secretName))
+                    .ReturnsAsync(outputKeyVaultSecret);
+
+            // when
+            SecureData actualSecureData = await this.secureDataService
+                .RetrieveSecretDataByName(secretName);
+
+            // then
+            actualSecureData.Should().BeEquivalentTo(expectedSecureData);
+
+            this.secureDataBrokerMock.Verify(broker =>
+                broker.GetKeyVaultSecretAsync(secretName),
+                    Times.Once());
+
+            this.secureDataBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

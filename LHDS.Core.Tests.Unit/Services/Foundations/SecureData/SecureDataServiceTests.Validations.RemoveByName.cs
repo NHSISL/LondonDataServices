59132ecc7f631af29ceb0d1a07f31d0e3d21f8fa
Hassan Raza <h_raza110@outlook.com>
2024-02-12 17:20:34 +0000

    ShouldThrowValidationExceptionOnRemoveByNameIfSecretNameIsInvalidAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/SecureData/SecureDataServiceTests.Validations.RemoveByName.cs
@@ -0,0 +1,58 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.Documents.Exceptions;
+using LHDS.Core.Models.Foundations.SecureData.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.SecureDatas
+{
+    public partial class SecureDataServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnRemoveByNameIfSecretNameIsInvalidAsync(string invalidText)
+        {
+            // given
+            string invalidSecretName = invalidText;
+
+            var invalidArgumentSecureDataException =
+                new InvalidArgumentSecureDataException(
+                    message: "Invalid secure data argument. Please correct the errors and try again.");
+
+            invalidArgumentSecureDataException.AddData(
+                key: "secretName",
+                values: "Text is required");
+
+            var expectedSecureDataValidationException =
+                new SecureDataValidationException(
+                    message: "Secure data validation errors occurred, please try again.",
+                    innerException: invalidArgumentSecureDataException);
+
+            // when
+            ValueTask removeSecureDataTask = this.secureDataService.RemoveSecureDataAsync(invalidSecretName);
+
+            var actualSecureDataValidationException =
+                 await Assert.ThrowsAsync<SecureDataValidationException>(removeSecureDataTask.AsTask);
+
+            // then
+            actualSecureDataValidationException.Should()
+                .BeEquivalentTo(expectedSecureDataValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSecureDataValidationException))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.keyVaultSecretBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

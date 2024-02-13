2b9397154b9831a983e953ea960d86f847c2fa19
Hassan Raza <h_raza110@outlook.com>
2024-02-13 16:50:35 +0000

    ShouldAddOrModifySecureData -> WIP

--- LHDS.Core.Tests.Unit/Services/Processings/SecureDatas/SecureDataProcessingServiceTests.Logic.Add.cs
+++ LHDS.Core.Tests.Unit/Services/Processings/SecureDatas/SecureDataProcessingServiceTests.Logic.Add.cs
@@ -48,7 +48,7 @@ namespace LHDS.Core.Tests.Unit.Services.Processings.SecureDatas
 
             // when
             SubscriberCredential actualSubscriberCredential =
-                await this.secureDataProcessingService.AddOrModifySecureData(inputSubscriberCredential);
+                await this.secureDataProcessingService.AddOrModifySecureDataAsync(inputSubscriberCredential);
 
             // then
             actualSubscriberCredential.Should().BeEquivalentTo(expectedSubscriberCredential);

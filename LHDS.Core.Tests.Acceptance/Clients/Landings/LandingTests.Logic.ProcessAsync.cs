33d61031877d1f8f9ea846793fabcc056380a7a5
Christo du Toit <christo@dutoit.co.uk>
2024-02-15 14:02:20 +0000

    CODE RUB: Commented out some stuff as stack needs to be updated 1st for us to be able to fix this.

--- LHDS.Core.Tests.Acceptance/Clients/Landings/LandingTests.Logic.ProcessAsync.cs
+++ LHDS.Core.Tests.Acceptance/Clients/Landings/LandingTests.Logic.ProcessAsync.cs
@@ -1,19 +1,9 @@
-﻿// ---------------------------------------------------------------
+﻿// ---------------------------------------------------------
 // Copyright (c) North East London ICB. All rights reserved.
-// ---------------------------------------------------------------
+// ---------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using FluentAssertions;
-using LHDS.Core.Models.Foundations.DataSets;
-using LHDS.Core.Models.Foundations.DataSetSpecifications;
-using LHDS.Core.Models.Foundations.Documents;
-using LHDS.Core.Models.Foundations.IngestionTrackings;
-using Moq;
 using Xunit;
 
 namespace LHDS.Core.Tests.Acceptance.Clients.Landings
@@ -23,140 +13,143 @@ namespace LHDS.Core.Tests.Acceptance.Clients.Landings
         [Fact(Skip = "Ftp Down")]
         public async Task ShouldProcessNewDocumentsAsync()
         {
-            //Given
-            DateTimeOffset randomDateTime = this.dateTimeBroker.GetCurrentDateTimeOffset();
-            Guid supplierId = landingConfiguration.LandingSupplierId;
-            string fileName = GetRandomFileName();
-            byte[] documentData = Encoding.UTF8.GetBytes(GetRandomString());
-            DataSet activeDataSet = CreateRandomDataSet(supplierId);
-            DataSetSpecification activeDataSetSpecification = CreateRandomDataSetSpecification(activeDataSet);
+            throw new NotImplementedException();
+            ////Given
+            //DateTimeOffset randomDateTime = this.dateTimeBroker.GetCurrentDateTimeOffset();
+            //Guid supplierId = landingConfiguration.LandingSupplierId;
+            //string fileName = GetRandomFileName();
+            //byte[] documentData = Encoding.UTF8.GetBytes(GetRandomString());
+            //DataSet activeDataSet = CreateRandomDataSet(supplierId);
+            //DataSetSpecification activeDataSetSpecification = CreateRandomDataSetSpecification(activeDataSet);
 
-            Document document = new Document
-            {
-                DocumentData = documentData,
-                FileName = fileName
-            };
+            //Document document = new Document
+            //{
+            //    DocumentData = documentData,
+            //    FileName = fileName
+            //};
 
-            List<Document> documents = new List<Document> { document };
+            //List<Document> documents = new List<Document> { document };
 
-            this.downloadBrokerMock.Setup(broker =>
-                broker.GetListOfDocumentsToProcessAsync())
-                    .ReturnsAsync(documents);
+            //this.downloadBrokerMock.Setup(broker =>
+            //    broker.GetListOfDocumentsToProcessAsync())
+            //        .ReturnsAsync(documents);
 
-            this.downloadBrokerMock.Setup(broker =>
-                broker.GetDocumentByFileNameAsync(fileName))
-                    .ReturnsAsync(document);
+            //this.downloadBrokerMock.Setup(broker =>
+            //    broker.GetDocumentByFileNameAsync(fileName))
+            //        .ReturnsAsync(document);
 
-            await this.dataSetService.AddDataSetAsync(activeDataSet);
-            await this.dataSetSpecificationProcessingService.AddDataSetSpecificationAsync(activeDataSetSpecification);
+            //await this.dataSetService.AddDataSetAsync(activeDataSet);
+            //await this.dataSetSpecificationProcessingService.AddDataSetSpecificationAsync(activeDataSetSpecification);
 
-            DataSetSpecification retrievedDataSetSpecification =
-                await this.dataSetSpecificationProcessingService.GetActiveDataSetSpecification(supplierId);
+            //DataSetSpecification retrievedDataSetSpecification =
+            //    await this.dataSetSpecificationProcessingService.GetActiveDataSetSpecification(supplierId);
 
-            //When
-            var actualStringList = await this.landingClient.ProcessAsync();
+            ////When
+            //var actualStringList = await this.landingClient.ProcessAsync();
 
-            //Then
-            this.downloadBrokerMock.Verify(broker =>
-                broker.GetListOfDocumentsToProcessAsync(),
-                    Times.Once);
+            ////Then
+            //this.downloadBrokerMock.Verify(broker =>
+            //    broker.GetListOfDocumentsToProcessAsync(),
+            //        Times.Once);
 
-            foreach (var actualFile in actualStringList)
-            {
-                this.downloadBrokerMock.Verify(broker =>
-                    broker.GetDocumentByFileNameAsync(fileName),
-                        Times.Once);
+            //foreach (var actualFile in actualStringList)
+            //{
+            //    this.downloadBrokerMock.Verify(broker =>
+            //        broker.GetDocumentByFileNameAsync(fileName),
+            //            Times.Once);
 
-                string expectedFile =
-                    $"/{landingConfiguration.DecryptedFolder}"
-                    + $"/{activeDataSet.DataSetName}"
-                    + $"/{activeDataSetSpecification.Id}"
-                    + $"/{fileName.Split('_')[3]}"
-                    + $"/{fileName.Replace(".gpg", "", StringComparison.InvariantCultureIgnoreCase)}";
+            //    string expectedFile =
+            //        $"/{landingConfiguration.DecryptedFolder}"
+            //        + $"/{activeDataSet.DataSetName}"
+            //        + $"/{activeDataSetSpecification.Id}"
+            //        + $"/{fileName.Split('_')[3]}"
+            //        + $"/{fileName.Replace(".gpg", "", StringComparison.InvariantCultureIgnoreCase)}";
 
-                actualFile.Should().BeEquivalentTo(expectedFile);
+            //    actualFile.Should().BeEquivalentTo(expectedFile);
 
-                IngestionTracking ingestionTracking = this.ingestionTrackingService.RetrieveAllIngestionTrackings()
-                    .FirstOrDefault(ingestionTracking => ingestionTracking.DecryptedFileName == actualFile);
+            //    IngestionTracking ingestionTracking = this.ingestionTrackingService.RetrieveAllIngestionTrackings()
+            //        .FirstOrDefault(ingestionTracking => ingestionTracking.DecryptedFileName == actualFile);
 
-                ingestionTracking.Should().NotBeNull();
+            //    ingestionTracking.Should().NotBeNull();
 
-                var audits = this.auditService.RetrieveAllIngestionTrackingAudits()
-                    .Where(audit => audit.IngestionTrackingId == ingestionTracking.Id);
+            //    var audits = this.auditService.RetrieveAllIngestionTrackingAudits()
+            //        .Where(audit => audit.IngestionTrackingId == ingestionTracking.Id);
 
-                foreach (var audit in audits)
-                {
-                    await this.auditService.RemoveIngestionTrackingAuditByIdAsync(audit.Id);
-                }
+            //    foreach (var audit in audits)
+            //    {
+            //        await this.auditService.RemoveIngestionTrackingAuditByIdAsync(audit.Id);
+            //    }
 
-                await this.ingestionTrackingService.RemoveIngestionTrackingByIdAsync(ingestionTracking.Id);
+            //    await this.ingestionTrackingService.RemoveIngestionTrackingByIdAsync(ingestionTracking.Id);
 
-                this.blobStorageBrokerMock.Verify(broker =>
-                    broker.InsertFileAsync(
-                        ingestionTracking.EncryptedFileName,
-                        It.IsAny<Stream>(),
-                        It.IsAny<string>()),
-                            Times.Once());
-            }
+            //    this.blobStorageBrokerMock.Verify(broker =>
+            //        broker.InsertFileAsync(
+            //            ingestionTracking.EncryptedFileName,
+            //            It.IsAny<Stream>(),
+            //            It.IsAny<string>()),
+            //                Times.Once());
+            //}
 
-            await this.dataSetSpecificationProcessingService
-                .RemoveDataSetSpecificationByIdAsync(activeDataSetSpecification.Id);
+            //await this.dataSetSpecificationProcessingService
+            //    .RemoveDataSetSpecificationByIdAsync(activeDataSetSpecification.Id);
 
-            await this.dataSetService.RemoveDataSetByIdAsync(activeDataSet.Id);
-            this.downloadBrokerMock.VerifyNoOtherCalls();
-            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+            //await this.dataSetService.RemoveDataSetByIdAsync(activeDataSet.Id);
+            //this.downloadBrokerMock.VerifyNoOtherCalls();
+            //this.blobStorageBrokerMock.VerifyNoOtherCalls();
         }
 
         [Fact(Skip = "Ftp Down")]
         public async Task ShouldNotProcessExistingDocumentsAsync()
         {
-            //Given
-            DateTimeOffset randomDateTime = this.dateTimeBroker.GetCurrentDateTimeOffset();
-            string fileName = GetRandomString();
-            byte[] documentData = Encoding.UTF8.GetBytes(GetRandomString());
+            throw new NotImplementedException();
 
-            Document document = new Document
-            {
-                DocumentData = documentData,
-                FileName = fileName
-            };
+            ////Given
+            //DateTimeOffset randomDateTime = this.dateTimeBroker.GetCurrentDateTimeOffset();
+            //string fileName = GetRandomString();
+            //byte[] documentData = Encoding.UTF8.GetBytes(GetRandomString());
 
-            List<Document> documents = new List<Document> { document };
+            //Document document = new Document
+            //{
+            //    DocumentData = documentData,
+            //    FileName = fileName
+            //};
 
-            this.downloadBrokerMock.Setup(broker =>
-                broker.GetListOfDocumentsToProcessAsync())
-                    .ReturnsAsync(documents);
+            //List<Document> documents = new List<Document> { document };
 
-            List<IngestionTracking> ingestionTrackings = await CreateRandomIngestionTrackings(
-                dateTimeOffset: this.dateTimeBroker.GetCurrentDateTimeOffset(),
-                documents,
-                supplierId: landingConfiguration.LandingSupplierId);
+            //this.downloadBrokerMock.Setup(broker =>
+            //    broker.GetListOfDocumentsToProcessAsync())
+            //        .ReturnsAsync(documents);
 
-            //When
-            var actualStringList = await this.landingClient.ProcessAsync();
+            //List<IngestionTracking> ingestionTrackings = await CreateRandomIngestionTrackings(
+            //    dateTimeOffset: this.dateTimeBroker.GetCurrentDateTimeOffset(),
+            //    documents,
+            //    supplierId: landingConfiguration.LandingSupplierId);
 
-            //Then
-            actualStringList.Should().HaveCount(0);
+            ////When
+            //var actualStringList = await this.landingClient.ProcessAsync();
 
-            this.downloadBrokerMock.Verify(broker =>
-                broker.GetListOfDocumentsToProcessAsync(),
-                    Times.Once);
+            ////Then
+            //actualStringList.Should().HaveCount(0);
 
-            foreach (var tracking in ingestionTrackings)
-            {
-                var audits = this.auditService.RetrieveAllIngestionTrackingAudits()
-                    .Where(audit => audit.IngestionTrackingId == tracking.Id);
+            //this.downloadBrokerMock.Verify(broker =>
+            //    broker.GetListOfDocumentsToProcessAsync(),
+            //        Times.Once);
 
-                foreach (var audit in audits)
-                {
-                    await this.auditService.RemoveIngestionTrackingAuditByIdAsync(audit.Id);
-                }
+            //foreach (var tracking in ingestionTrackings)
+            //{
+            //    var audits = this.auditService.RetrieveAllIngestionTrackingAudits()
+            //        .Where(audit => audit.IngestionTrackingId == tracking.Id);
 
-                await this.ingestionTrackingService.RemoveIngestionTrackingByIdAsync(tracking.Id);
-            }
+            //    foreach (var audit in audits)
+            //    {
+            //        await this.auditService.RemoveIngestionTrackingAuditByIdAsync(audit.Id);
+            //    }
 
-            this.downloadBrokerMock.VerifyNoOtherCalls();
-            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+            //    await this.ingestionTrackingService.RemoveIngestionTrackingByIdAsync(tracking.Id);
+            //}
+
+            //this.downloadBrokerMock.VerifyNoOtherCalls();
+            //this.blobStorageBrokerMock.VerifyNoOtherCalls();
         }
     }
 }

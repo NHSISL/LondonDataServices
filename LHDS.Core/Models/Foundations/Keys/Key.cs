997da06844c9630e0f6c2d8dc071788936d09a97
Hassan Raza <h_raza110@outlook.com>
2024-02-20 12:38:20 +0000

    BROKER: Cryptography Keys

---
+++ LHDS.Core/Models/Foundations/Keys/Key.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+namespace LHDS.Core.Models.Foundations.Keys
+{
+    public class Key
+    {
+        public string? Base64PublicKey { get; set; }
+        public string? Base64PrivateKey { get; set; }
+        public string? Passphrase { get; set; }
+    }
+}

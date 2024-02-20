5c3d09cd8e5845ccadd8c825ea75ff69994be9a5
Hassan Raza <h_raza110@outlook.com>
2024-02-20 13:01:19 +0000

    Draft commit

---
+++ LHDS.Core/Brokers/CryptographyKeys/GpgKeyBroker.cs
@@ -0,0 +1,84 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Keys;
+using Org.BouncyCastle.Bcpg;
+using Org.BouncyCastle.Bcpg.OpenPgp;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Security;
+
+namespace LHDS.Core.Brokers.CryptographyKeys
+{
+    public class GpgKeyBroker : ICryptographyKeyBroker
+    {
+        public ValueTask<Key> GenerateKeys(string publicKeyComment)
+        {
+            var publicKeyPath = @"C:\Users\d_cun\OneDrive\Desktop\AZ SSH Files\pub.asc";
+            var privateKeyPath = @"C:\Users\d_cun\OneDrive\Desktop\AZ SSH Files\priv.asc";
+            RsaKeyPairGenerator rsaKeyPairGenerator = new RsaKeyPairGenerator();
+            rsaKeyPairGenerator.Init(new KeyGenerationParameters(new SecureRandom(), 2048));
+            AsymmetricCipherKeyPair keyPair = rsaKeyPairGenerator.GenerateKeyPair();
+
+            // Create the PGP key pair
+            PgpKeyPair pgpKeyPair = new PgpKeyPair(PublicKeyAlgorithmTag.RsaGeneral, keyPair, DateTime.UtcNow);
+
+            // Create the PGP key ring generator
+            PgpSignatureSubpacketGenerator subpacketGenerator = new PgpSignatureSubpacketGenerator();
+
+            subpacketGenerator.SetKeyFlags(
+                false, PgpKeyFlags.CanSign | PgpKeyFlags.CanEncryptCommunications | PgpKeyFlags.CanEncryptStorage);
+
+            subpacketGenerator.SetPreferredSymmetricAlgorithms(false, new[] { (int)SymmetricKeyAlgorithmTag.Aes256 });
+            subpacketGenerator.SetPreferredHashAlgorithms(false, new[] { (int)HashAlgorithmTag.Sha256 });
+            PgpSignatureSubpacketVector subpacketVector = subpacketGenerator.Generate();
+
+            PgpKeyRingGenerator keyRingGenerator = new PgpKeyRingGenerator(
+                PgpSignature.DefaultCertification, 
+                pgpKeyPair, "test", SymmetricKeyAlgorithmTag.Aes256, "test".ToCharArray(), true, 
+                subpacketVector, null, new SecureRandom());
+
+            PgpPublicKeyRing publicKeyRing = keyRingGenerator.GeneratePublicKeyRing();
+            PgpSecretKeyRing secretKeyRing = keyRingGenerator.GenerateSecretKeyRing();
+
+            string publicKey;
+            using (MemoryStream outputStream = new MemoryStream())
+            {
+                using (ArmoredOutputStream armoredStream = new ArmoredOutputStream(outputStream))
+                {
+                    publicKeyRing.Encode(armoredStream);
+                }
+                publicKey = Encoding.UTF8.GetString(outputStream.ToArray());
+            }
+
+            // Write the private key to a string
+            string privateKey;
+            using (MemoryStream outputStream = new MemoryStream())
+            {
+                using (ArmoredOutputStream armoredStream = new ArmoredOutputStream(outputStream))
+                {
+                    secretKeyRing.Encode(armoredStream);
+                }
+                privateKey = Encoding.UTF8.GetString(outputStream.ToArray());
+            }
+
+            Key returnedKey =  new Key
+            {
+                Base64PrivateKey = publicKey,
+                Base64PublicKey = privateKey
+            };
+
+            return ValueTask.FromResult(returnedKey);
+        }
+
+        public ValueTask<Key> GenerateKeys() =>
+            this.GenerateKeys("");
+
+
+    }
+}
---
+++ LHDS.Core/Brokers/CryptographyKeys/SshKeyBroker.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Keys;
+
+namespace LHDS.Core.Brokers.CryptographyKeys
+{
+    public class SSHKeys : ICryptographyKeyBroker
+    {
+        public ValueTask<Key> GenerateKeys(string publicKeyComment)
+        {
+            int keyBits = 2048;
+
+            var keygen = new SshKeyGenerator.SshKeyGenerator(keyBits);
+
+            Key key = new Key
+            {
+                Base64PublicKey = keygen.ToRfcPublicKey(publicKeyComment),
+                Base64PrivateKey = keygen.ToPrivateKey(),
+            };
+
+            return ValueTask.FromResult(key);
+        }
+
+        public ValueTask<Key> GenerateKeys() =>
+            this.GenerateKeys($"Auto Generated Comment {DateTimeOffset.UtcNow} ");
+    }
+}

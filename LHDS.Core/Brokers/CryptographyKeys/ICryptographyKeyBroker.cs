534548804c4ca110ff1dc7c437c3d9e748c58344
Hassan Raza <h_raza110@outlook.com>
2024-02-20 12:38:20 +0000

    BROKER: Cryptography Keys

---
+++ LHDS.Core/Brokers/CryptographyKeys/ICryptographyKeyBroker.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Keys;
+
+namespace LHDS.Core.Brokers.Decryptions
+{
+    public interface ICryptographyKeyBroker
+    {
+        ValueTask<Key> GenerateKeys(string publicKeyComment);
+        ValueTask<Key> GenerateKeys();
+    }
+}

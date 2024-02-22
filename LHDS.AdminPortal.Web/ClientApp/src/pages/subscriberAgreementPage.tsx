63cd8417147ee3aecc65c7492eaa6fe3ac97f128
David Hayes <davidhayes03@googlemail.com>
2024-02-22 14:59:53 +0000

    UI: Page containers

---
+++ LHDS.AdminPortal.Web/ClientApp/src/pages/subscriberAgreementPage.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { PageLayout } from '../components/pageLayout';
+
+export const SubscriberAgreementPage = () => {
+    return <PageLayout>
+        <section>
+            <div className="container-fluis">
+                <main id="maincontent" className="NELTopPadding" role="main">
+                    <>
+                        
+                    </>
+                </main>
+            </div>
+        </section>
+    </PageLayout>
+}

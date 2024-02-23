87b01de3877942d1dffb3ce25223eaf3f47e5bb1
Hassan Raza <h_raza110@outlook.com>
2024-02-23 10:38:58 +0000

    ShouldModifyOrAddSubscriberCredentialAndLogAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/SubscriberCredentials/SubscricberCredentialOrchestrationTests.Logic.AddOrModify.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Foundations.AddressExtractionAudits;
+using LHDS.Core.Models.Processings.SubscriberCredentials;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Orchestrations.SubscriberCredentials
+{
+    public partial class SubscriberCredentialOrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldModifyOrAddSubscriberCredentialAndLogAsync()
+        {
+            // Given
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+
+            SubscriberCredential randomSubscriberCredential = CreateRandomCreateSubscriberCredential(
+                randomDateTimeOffset);
+
+            SubscriberCredential inputSubscriberCredential = randomSubscriberCredential;
+            SubscriberCredential outputSubscriberCredential = inputSubscriberCredential;
+            SubscriberCredential expectedSubscriberCredential = outputSubscriberCredential.DeepClone();
+
+            this.secureDataProcessingServiceMock.Setup(service =>
+                service.AddOrModifySecureDataAsync(inputSubscriberCredential))
+                    .ReturnsAsync(outputSubscriberCredential);
+
+            // When
+            SubscriberCredential actualSubscriberCredential = await this.subscriberCredentialOrchestration
+                .ModifyOrAddSubscriberCredentialAsync(inputSubscriberCredential);
+
+            // Then
+            actualSubscriberCredential.Should().BeEquivalentTo(expectedSubscriberCredential);
+
+            this.secureDataProcessingServiceMock.Verify(service =>
+                service.AddOrModifySecureDataAsync(inputSubscriberCredential),
+                    Times.Once);
+
+            this.secureDataProcessingServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.subscriberAgreementProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
+

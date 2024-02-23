eca5533bc74952f3663d8b49ace1ac18454f8596
Hassan Raza <h_raza110@outlook.com>
2024-02-23 11:18:30 +0000

    ShouldThrowValidationExceptionOnModifyOrAddIfSubscriberCredentialIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/SubscriberCredentials/SubscricberCredentialOrchestrationTests.Validations.AddOrModify.cs
@@ -0,0 +1,60 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Orchestrations.AddressExtractions.Exceptions;
+using LHDS.Core.Models.Orchestrations.SubscriberCredentials.Exceptions;
+using LHDS.Core.Models.Processings.SubscriberCredentials;
+using LHDS.Core.Models.Processings.SubscriberCredentials.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Orchestrations.SubscriberCredentials
+{
+    public partial class SubscriberCredentialOrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyOrAddIfSubscriberCredentialIsNullAndLogItAsync()
+        {
+            // given
+            SubscriberCredential nullSubscriberCredential = null;
+
+            var invalidArgumentSubscriberCredentialProcessingException =
+                new InvalidArgumentSubscriberCredentialProcessingException(
+                    message: "Invalid argument subscriber credential orchestration exception, " +
+                        "please correct the errors and try again.");
+
+            var expectedSubscriberCredentialValidationOrchestrationException =
+                new SubscriberCredentialValidationOrchestrationException(
+                    message: "Subscriber credential orchestration validation error occurred, please try again.",
+                    innerException: invalidArgumentSubscriberCredentialProcessingException);
+
+            // when
+            ValueTask<SubscriberCredential> addOrModifySubscriberCredentialTask =
+                this.subscriberCredentialOrchestration.ModifyOrAddSubscriberCredentialAsync(nullSubscriberCredential);
+
+            SubscriberCredentialValidationOrchestrationException actualException =
+                await Assert.ThrowsAsync<SubscriberCredentialValidationOrchestrationException>(
+                    addOrModifySubscriberCredentialTask.AsTask);
+
+            // then
+            actualException.Should()
+                .BeEquivalentTo(expectedSubscriberCredentialValidationOrchestrationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSubscriberCredentialValidationOrchestrationException))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.secureDataProcessingServiceMock.VerifyNoOtherCalls();
+            this.subscriberAgreementProcessingServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

c0cda7371002d7581b4e49440214ba4128a21ab6
Hassan Raza <h_raza110@outlook.com>
2024-02-27 14:27:43 +0000

    ShouldThrowServiceExceptionOnModifyOrAddSubscriberCredentialIfServiceErrorOccursAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Orchestrations/SubscriberCredentials/SubscricberCredentialOrchestrationTests.Exceptions.AddOrModify.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/SubscriberCredentials/SubscricberCredentialOrchestrationTests.Exceptions.AddOrModify.cs
@@ -1,163 +1,164 @@
-﻿//// ---------------------------------------------------------
-//// Copyright (c) North East London ICB. All rights reserved.
-//// ---------------------------------------------------------
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
 
-//using System;
-//using System.Threading.Tasks;
-//using FluentAssertions;
-//using LHDS.Core.Models.Orchestrations.SubscriberCredentials.Exceptions;
-//using LHDS.Core.Models.Processings.SubscriberCredentials;
-//using Moq;
-//using Xeptions;
-//using Xunit;
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LHDS.Core.Models.Foundations.SubscriberAgreements;
+using LHDS.Core.Models.Orchestrations.SubscriberCredentials.Exceptions;
+using LHDS.Core.Models.Processings.SubscriberCredentials;
+using Moq;
+using Xeptions;
+using Xunit;
 
-//namespace LHDS.Core.Tests.Unit.Services.Orchestrations.SubscriberCredentials
-//{
-//    public partial class SubscriberCredentialOrchestrationServiceTests
-//    {
-//        [Theory]
-//        [MemberData(nameof(SubscriberCredentialOrchestrationDependencyValidationExceptions))]
-//        public async Task
-//            ShouldThrowDependencyValidationOnModifyOrAddSubscriberCredentialIfDependencyValidationOccursAndLogItAsync(
-//            Xeption dependencyValidationException)
-//        {
-//            // given
-//            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
-//            SubscriberCredential subscriberCredential = CreateRandomCreateSubscriberCredential(randomDateTimeOffset);
+namespace LHDS.Core.Tests.Unit.Services.Orchestrations.SubscriberCredentials
+{
+    public partial class SubscriberCredentialOrchestrationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(SubscriberCredentialOrchestrationDependencyValidationExceptions))]
+        public async Task
+            ShouldThrowDependencyValidationOnModifyOrAddSubscriberCredentialIfDependencyValidationOccursAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            dynamic randomDynamic = CreateRandomDynamicSubscriberAgreementCredential();
+            SubscriberCredential inputSubscriberCredential = CreateSubscriberCredentialFromDynamic(randomDynamic);
 
-//            var expectedDependencyException =
-//                new SubscriberCredentialOrchestrationDependencyValidationException(
-//                    message: "Subscriber credential orchestration dependency validation error occurred, " +
-//                        "fix the errors and try again.",
-//                    innerException: dependencyValidationException.InnerException as Xeption);
+            var expectedDependencyException =
+                new SubscriberCredentialOrchestrationDependencyValidationException(
+                    message: "Subscriber credential orchestration dependency validation error occurred, " +
+                        "fix the errors and try again.",
+                    innerException: dependencyValidationException.InnerException as Xeption);
 
-//            this.secureDataProcessingServiceMock.Setup(service =>
-//                service.AddOrModifySecureDataAsync(It.IsAny<SubscriberCredential>()))
-//                    .ThrowsAsync(dependencyValidationException);
+            this.subscriberAgreementProcessingServiceMock.Setup(service =>
+                service.ModifyOrAddSubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()))
+                    .ThrowsAsync(dependencyValidationException);
 
-//            // when
-//            ValueTask<SubscriberCredential> modifyOrAddSubscriberCredentialTask =
-//                this.subscriberCredentialOrchestration.ModifyOrAddSubscriberCredentialAsync(subscriberCredential);
+            // when
+            ValueTask<SubscriberCredential> modifyOrAddSubscriberCredentialTask =
+                this.subscriberCredentialOrchestration.ModifyOrAddSubscriberCredentialAsync(inputSubscriberCredential);
 
-//            SubscriberCredentialOrchestrationDependencyValidationException actualException =
-//                await Assert.ThrowsAsync<SubscriberCredentialOrchestrationDependencyValidationException>(
-//                    modifyOrAddSubscriberCredentialTask.AsTask);
+            SubscriberCredentialOrchestrationDependencyValidationException actualException =
+                await Assert.ThrowsAsync<SubscriberCredentialOrchestrationDependencyValidationException>(
+                    modifyOrAddSubscriberCredentialTask.AsTask);
 
-//            // then
-//            actualException.Should()
-//                 .BeEquivalentTo(expectedDependencyException);
+            // then
+            actualException.Should()
+                 .BeEquivalentTo(expectedDependencyException);
 
-//            this.secureDataProcessingServiceMock.Verify(service =>
-//             service.AddOrModifySecureDataAsync(It.IsAny<SubscriberCredential>()),
-//                 Times.Once);
+            this.subscriberAgreementProcessingServiceMock.Verify(service =>
+             service.ModifyOrAddSubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()),
+                 Times.Once);
 
-//            this.loggingBrokerMock.Verify(broker =>
-//               broker.LogError(It.Is(SameExceptionAs(
-//                   expectedDependencyException))),
-//                       Times.Once);
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(
+                   expectedDependencyException))),
+                       Times.Once);
 
-//            this.secureDataProcessingServiceMock.VerifyNoOtherCalls();
-//            this.loggingBrokerMock.VerifyNoOtherCalls();
-//            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-//            this.subscriberAgreementProcessingServiceMock.VerifyNoOtherCalls();
-//        }
+            this.secureDataProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.subscriberAgreementProcessingServiceMock.VerifyNoOtherCalls();
+        }
 
-//        [Theory]
-//        [MemberData(nameof(SubscriberCredentialOrchestrationDependencyExceptions))]
-//        public async Task
-//            ShouldThrowDependencyExceptionOnModifyOrAddSubscriberCredentialIfDependencyErrorOccursAndLogItAsync(
-//            Xeption dependencyException)
-//        {
-//            // given
-//            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
-//            SubscriberCredential subscriberCredential = CreateRandomCreateSubscriberCredential(randomDateTimeOffset);
+        [Theory]
+        [MemberData(nameof(SubscriberCredentialOrchestrationDependencyExceptions))]
+        public async Task
+            ShouldThrowDependencyExceptionOnModifyOrAddSubscriberCredentialIfDependencyErrorOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            dynamic randomDynamic = CreateRandomDynamicSubscriberAgreementCredential();
+            SubscriberCredential inputSubscriberCredential = CreateSubscriberCredentialFromDynamic(randomDynamic);
 
-//            var expectedDependencyException =
-//                new SubscriberCredentialDependencyOrchestrationException(
-//                    message: "Subscriber credential orchestration dependency error occurred, " +
-//                        "fix the errors and try again.",
-//                    innerException: dependencyException.InnerException as Xeption);
+            var expectedDependencyException =
+                new SubscriberCredentialDependencyOrchestrationException(
+                    message: "Subscriber credential orchestration dependency error occurred, " +
+                        "fix the errors and try again.",
+                    innerException: dependencyException.InnerException as Xeption);
 
-//            this.secureDataProcessingServiceMock.Setup(service =>
-//                service.AddOrModifySecureDataAsync(It.IsAny<SubscriberCredential>()))
-//                    .ThrowsAsync(dependencyException);
+            this.subscriberAgreementProcessingServiceMock.Setup(service =>
+                service.ModifyOrAddSubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()))
+                    .ThrowsAsync(dependencyException);
 
-//            // when
-//            ValueTask<SubscriberCredential> modifyOrAddSubscriberCredentialTask =
-//                this.subscriberCredentialOrchestration.ModifyOrAddSubscriberCredentialAsync(subscriberCredential);
+            // when
+            ValueTask<SubscriberCredential> modifyOrAddSubscriberCredentialTask =
+                this.subscriberCredentialOrchestration.ModifyOrAddSubscriberCredentialAsync(inputSubscriberCredential);
 
-//            SubscriberCredentialDependencyOrchestrationException actualException =
-//                await Assert.ThrowsAsync<SubscriberCredentialDependencyOrchestrationException>(
-//                    modifyOrAddSubscriberCredentialTask.AsTask);
+            SubscriberCredentialDependencyOrchestrationException actualException =
+                await Assert.ThrowsAsync<SubscriberCredentialDependencyOrchestrationException>(
+                    modifyOrAddSubscriberCredentialTask.AsTask);
 
-//            // then
-//            actualException.Should()
-//                 .BeEquivalentTo(expectedDependencyException);
+            // then
+            actualException.Should()
+                 .BeEquivalentTo(expectedDependencyException);
 
-//            this.secureDataProcessingServiceMock.Verify(service =>
-//             service.AddOrModifySecureDataAsync(It.IsAny<SubscriberCredential>()),
-//                 Times.Once);
+            this.subscriberAgreementProcessingServiceMock.Verify(service =>
+             service.ModifyOrAddSubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()),
+                 Times.Once);
 
-//            this.loggingBrokerMock.Verify(broker =>
-//               broker.LogError(It.Is(SameExceptionAs(
-//                   expectedDependencyException))),
-//                       Times.Once);
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(
+                   expectedDependencyException))),
+                       Times.Once);
 
-//            this.secureDataProcessingServiceMock.VerifyNoOtherCalls();
-//            this.loggingBrokerMock.VerifyNoOtherCalls();
-//            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-//            this.subscriberAgreementProcessingServiceMock.VerifyNoOtherCalls();
-//        }
+            this.secureDataProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.subscriberAgreementProcessingServiceMock.VerifyNoOtherCalls();
+        }
 
-//        [Fact]
-//        public async Task
-//            ShouldThrowServiceExceptionOnModifyOrAddSubscriberCredentialIfServiceErrorOccursAndLogItAsync()
-//        {
-//            //given
-//            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
-//            SubscriberCredential subscriberCredential = CreateRandomCreateSubscriberCredential(randomDateTimeOffset);
-//            var serviceException = new Exception();
+        [Fact]
+        public async Task
+            ShouldThrowServiceExceptionOnModifyOrAddSubscriberCredentialIfServiceErrorOccursAndLogItAsync()
+        {
+            //given
+            dynamic randomDynamic = CreateRandomDynamicSubscriberAgreementCredential();
+            SubscriberCredential inputSubscriberCredential = CreateSubscriberCredentialFromDynamic(randomDynamic);
+            var serviceException = new Exception();
 
-//            var failedSubscriberCredentialOrchestrationServiceException =
-//                new FailedSubscriberCredentialOrchestrationServiceException(
-//                    message: "Failed subscriber credential orchestration service error occurred, please contact support.",
-//                    innerException: serviceException);
+            var failedSubscriberCredentialOrchestrationServiceException =
+                new FailedSubscriberCredentialOrchestrationServiceException(
+                    message: "Failed subscriber credential orchestration service error occurred, please contact support.",
+                    innerException: serviceException);
 
-//            var expectedDependencyException =
-//                new SubscriberCredentialOrchestrationServiceException(
-//                    message: "Subscriber credential orchestration service error occurred, " +
-//                        "fix the errors and try again.",
-//                    innerException: failedSubscriberCredentialOrchestrationServiceException);
+            var expectedDependencyException =
+                new SubscriberCredentialOrchestrationServiceException(
+                    message: "Subscriber credential orchestration service error occurred, " +
+                        "fix the errors and try again.",
+                    innerException: failedSubscriberCredentialOrchestrationServiceException);
 
-//            this.secureDataProcessingServiceMock.Setup(service =>
-//                service.AddOrModifySecureDataAsync(It.IsAny<SubscriberCredential>()))
-//                    .ThrowsAsync(serviceException);
+            this.subscriberAgreementProcessingServiceMock.Setup(service =>
+                service.ModifyOrAddSubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()))
+                    .ThrowsAsync(serviceException);
 
-//            // when
-//            ValueTask<SubscriberCredential> modifyOrAddSubscriberCredentialTask =
-//                this.subscriberCredentialOrchestration.ModifyOrAddSubscriberCredentialAsync(subscriberCredential);
+            // when
+            ValueTask<SubscriberCredential> modifyOrAddSubscriberCredentialTask =
+                this.subscriberCredentialOrchestration.ModifyOrAddSubscriberCredentialAsync(inputSubscriberCredential);
 
-//            SubscriberCredentialOrchestrationServiceException actualException =
-//                await Assert.ThrowsAsync<SubscriberCredentialOrchestrationServiceException>(
-//                    modifyOrAddSubscriberCredentialTask.AsTask);
+            SubscriberCredentialOrchestrationServiceException actualException =
+                await Assert.ThrowsAsync<SubscriberCredentialOrchestrationServiceException>(
+                    modifyOrAddSubscriberCredentialTask.AsTask);
 
-//            // then
-//            actualException.Should()
-//                 .BeEquivalentTo(expectedDependencyException);
+            // then
+            actualException.Should()
+                 .BeEquivalentTo(expectedDependencyException);
 
-//            this.secureDataProcessingServiceMock.Verify(service =>
-//             service.AddOrModifySecureDataAsync(It.IsAny<SubscriberCredential>()),
-//                 Times.Once);
+            this.subscriberAgreementProcessingServiceMock.Verify(service =>
+             service.ModifyOrAddSubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()),
+                 Times.Once);
 
-//            this.loggingBrokerMock.Verify(broker =>
-//               broker.LogError(It.Is(SameExceptionAs(
-//                   expectedDependencyException))),
-//                       Times.Once);
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(
+                   expectedDependencyException))),
+                       Times.Once);
 
-//            this.secureDataProcessingServiceMock.VerifyNoOtherCalls();
-//            this.loggingBrokerMock.VerifyNoOtherCalls();
-//            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-//            this.subscriberAgreementProcessingServiceMock.VerifyNoOtherCalls();
-//        }
-//    }
-//}
+            this.secureDataProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.subscriberAgreementProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}

eb1b694d3c4d4c9feea4a87f6769837059cfe8f6
Hassan Raza <h_raza110@outlook.com>
2024-02-28 14:06:51 +0000

    CODE RUB: Removing Validations

--- LHDS.Core/Services/Processings/SecureDatas/SecureDataProcessingService.Validations.cs
+++ LHDS.Core/Services/Processings/SecureDatas/SecureDataProcessingService.Validations.cs
@@ -76,37 +76,6 @@ namespace LHDS.Core.Services.Processings.SecureDatas
         private void ValidateSubscriberCredentialOnRemove(SubscriberCredential subscriberCredential)
         {
             ValidateSubscriberCredentialIsNotNull(subscriberCredential);
-
-            Validate<InvalidSubscriberCredentialException>(
-               message: "Invalid subscriber credential errors occured. Please correct the errors and try again.",
-               (Rule: IsInvalid(subscriberCredential.Id), Parameter: nameof(SubscriberCredential.Id)),
-
-               (Rule: IsInvalid(subscriberCredential.SupplierSharingAgreementShortName), Parameter: nameof(
-                   SubscriberCredential.SupplierSharingAgreementShortName)),
-
-               (Rule: IsInvalid(subscriberCredential.FtpUserName), Parameter: nameof(
-                   SubscriberCredential.FtpUserName)),
-
-               (Rule: IsInvalid(subscriberCredential.FtpPassword), Parameter: nameof(
-                   SubscriberCredential.FtpPassword)),
-
-               (Rule: IsInvalid(subscriberCredential.FtpPassPhrase), Parameter: nameof(
-                   SubscriberCredential.FtpPassPhrase)),
-
-               (Rule: IsInvalid(subscriberCredential.FtpPrivateKey), Parameter: nameof(
-                   SubscriberCredential.FtpPrivateKey)),
-
-               (Rule: IsInvalid(subscriberCredential.FtpPublicKey), Parameter: nameof(
-                   SubscriberCredential.FtpPublicKey)),
-
-               (Rule: IsInvalid(subscriberCredential.GpgPassPhrase), Parameter: nameof(
-                   SubscriberCredential.GpgPassPhrase)),
-
-               (Rule: IsInvalid(subscriberCredential.GpgPrivateKey), Parameter: nameof(
-                   SubscriberCredential.GpgPrivateKey)),
-
-               (Rule: IsInvalid(subscriberCredential.GpgPublicKey), Parameter: nameof(
-                   SubscriberCredential.GpgPublicKey)));
         }
 
             private void ValidateSecureData(SecureData secureData)

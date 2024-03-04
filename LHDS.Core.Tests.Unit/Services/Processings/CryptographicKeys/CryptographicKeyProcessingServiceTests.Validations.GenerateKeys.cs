48de4a1182fc51b42954411610991a1e89ed5660
Christo du Toit <christo@dutoit.co.uk>
2024-03-04 17:30:16 +0000

    ShouldThrowValidationExceptionOnGenerateKeysIfSubscriberCredentialIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/CryptographicKeys/CryptographicKeyProcessingServiceTests.Validations.GenerateKeys.cs
@@ -0,0 +1,46 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.CryptographicKeys.Exceptions;
+using LHDS.Core.Models.Processings.SubscriberCredentials;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.CryptographicKeys
+{
+    public partial class CryptographicKeyProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnGenerateKeysIfSubscriberCredentialIsNullAndLogItAsync()
+        {
+            //given
+            SubscriberCredential nullSubscriberCredential = null;
+
+            var nullSubscriberCredentialProcessingException =
+                new NullSubscriberCredentialCryptographicKeyProcessingException(
+                    message: "Null subscriber credential processing exception, " +
+                        "please correct the errors and try again.");
+
+            var expectedSubscriberCredentialValidationProcessingException =
+                new CryptographicKeyValidationProcessingException(
+                    message: "Cryptography key processing validation error occurred, please try again.",
+                    innerException: nullSubscriberCredentialProcessingException);
+
+            // when
+            ValueTask<SubscriberCredential> generateKeysTask =
+                this.cryptographyKeyProcessingService.GenerateKeysAsync(nullSubscriberCredential);
+
+            CryptographicKeyValidationProcessingException actualException =
+                await Assert.ThrowsAsync<CryptographicKeyValidationProcessingException>(
+                    generateKeysTask.AsTask);
+
+            // then
+            actualException.Should()
+                .BeEquivalentTo(expectedSubscriberCredentialValidationProcessingException);
+
+            this.cryptographyKeyServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}

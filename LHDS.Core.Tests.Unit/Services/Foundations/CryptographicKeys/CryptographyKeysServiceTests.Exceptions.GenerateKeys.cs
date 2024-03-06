56c827811d2c011f84ae40a4d1d6f07e97e7d791
Hassan Raza <h_raza110@outlook.com>
2024-03-06 12:59:51 +0000

    Fixing comments

--- LHDS.Core.Tests.Unit/Services/Foundations/CryptographicKeys/CryptographyKeysServiceTests.Exceptions.GenerateKeys.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/CryptographicKeys/CryptographyKeysServiceTests.Exceptions.GenerateKeys.cs
@@ -38,7 +38,11 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.CryptographicKeys
                    .Returns(cryptographyType);
 
             this.cryptographyKeyBrokerMock.Setup(broker =>
-                broker.GenerateKeys(It.IsAny<string>(), "", "", ""))
+                broker.GenerateKeys(
+                    It.IsAny<string>(), 
+                    It.IsAny<string>(), 
+                    It.IsAny<string>(), 
+                    It.IsAny<string>()))
                     .ThrowsAsync(serviceException);
 
             // when
@@ -60,7 +64,11 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.CryptographicKeys
                     Times.Once);
 
             this.cryptographyKeyBrokerMock.Verify(broker =>
-                broker.GenerateKeys(It.IsAny<string>(), "", "", ""),
+                broker.GenerateKeys(
+                    It.IsAny<string>(), 
+                    It.IsAny<string>(), 
+                    It.IsAny<string>(), 
+                    It.IsAny<string>()),
                     Times.Once);
 
             loggingBrokerMock.Verify(broker =>

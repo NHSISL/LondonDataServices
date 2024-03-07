05f9289b15c143ed94147134259a2b561abb3a45
Hassan Raza <h_raza110@outlook.com>
2024-03-07 09:41:54 +0000

    Adding subscriber Credentials

--- LHDS.AdminPortal.Api.Tests.Acceptance/Brokers/ApiBroker.Downloads.cs
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Brokers/ApiBroker.Downloads.cs
@@ -1,10 +1,9 @@
-// ---------------------------------------------------------------
+// ---------------------------------------------------------
 // Copyright (c) North East London ICB. All rights reserved.
-// ---------------------------------------------------------------
+// ---------------------------------------------------------
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using LHDS.Core.Models.Foundations.Documents;
 using LHDS.Core.Models.Foundations.Downloads;
 
 namespace LHDS.AdminPortal.Api.Tests.Acceptance.Brokers

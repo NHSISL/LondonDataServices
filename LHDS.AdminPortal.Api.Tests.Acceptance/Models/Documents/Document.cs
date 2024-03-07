8256e7afd2a8a85a10426ce610a1047b7b2232f2
Hassan Raza <h_raza110@outlook.com>
2024-03-07 11:02:08 +0000

    Adding api models

---
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Models/Documents/Document.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace LHDS.AdminPortal.Api.Tests.Acceptance.Models.Documents
+{
+    public class Document
+    {
+        public string FileName { get; set; } = string.Empty;
+        public byte[] DocumentData { get; set; } = new byte[0];
+        public string SHA256Hash { get; set; } = string.Empty;
+    }
+}

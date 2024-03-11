d8c7936b6a4dc62b59eb8e5bb750d9f1061ab5e8
Christo du Toit <christo@dutoit.co.uk>
2024-03-11 14:12:44 +0000

    ShouldThrowValidationExceptionOnRetrieveFileListIfFileNameIsNullAndLogItAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Coordinations/EmisLandings/EmisLandingCoordinationTests.Validations.RetrieveListOfDocumentsToProcess.cs
@@ -0,0 +1,58 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Coordinations.EmisLandings.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Coordinations.EmisLandings
+{
+    public partial class EmisLandingCoordinationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveFileListIfFileNameIsNullAndLogItAsync()
+        {
+            // given
+            Guid invalidSubscriberAgreementId = Guid.Empty;
+
+            var invalidArgumentEmisLandingCoordinationException =
+                new InvalidArgumentEmisLandingCoordinationException(
+                    message: "Invalid Emis Landing coordination argument, please correct the errors and try again.");
+
+            invalidArgumentEmisLandingCoordinationException.AddData(
+                key: "SubscriberAgreementId",
+                values: "Id is required");
+
+            var expectedEmisLandingCoordinationValidationException =
+                new EmisLandingCoordinationValidationException(
+                    message: "Emis Landing coordination validation error occurred, please try again.",
+                    innerException: invalidArgumentEmisLandingCoordinationException);
+
+            // when
+            ValueTask<List<string>> processDataTask =
+                this.emisLandingCoordinationService.RetrieveListOfDocumentsToProcessAsync(invalidSubscriberAgreementId);
+
+            EmisLandingCoordinationValidationException actualEmisLandingCoordinationValidationException =
+                await Assert.ThrowsAsync<EmisLandingCoordinationValidationException>(async () =>
+                    await processDataTask);
+
+            // then
+            actualEmisLandingCoordinationValidationException.Should()
+                .BeEquivalentTo(expectedEmisLandingCoordinationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedEmisLandingCoordinationValidationException))),
+                        Times.Once);
+
+            this.subscriberCredentialOrchestrationMock.VerifyNoOtherCalls();
+            this.emisLandingExtractionOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

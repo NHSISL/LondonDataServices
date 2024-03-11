0e4f4924101fdb850ac8c9bd6efa2d21e4f75b91
Christo du Toit <christo@dutoit.co.uk>
2024-03-11 14:19:21 +0000

    ShouldThrowDependencyValidationExceptionOnRetrieveFileListIfErrorsAndLogItAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Coordinations/EmisLandings/EmisLandingCoordinationTests.Exceptions.RetrieveListOfDocumentsToProcess.cs
@@ -0,0 +1,64 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Coordinations.EmisLandings.Exceptions;
+using Moq;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Coordinations.EmisLandings
+{
+    public partial class EmisLandingCoordinationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnRetrieveFileListIfErrorsAndLogItAsync(
+            Xeption dependancyValidationException)
+        {
+            // Given
+            Guid subscriberCredentialId = Guid.NewGuid();
+
+            var expectedEmisLandingCoordinationDependencyValidationException =
+                new EmisLandingCoordinationDependencyValidationException(
+                    message: "EMIS landing coordination dependency validation error occurred, please try again.",
+                    innerException: dependancyValidationException.InnerException as Xeption);
+
+            this.subscriberCredentialOrchestrationMock.Setup(service =>
+                service.RetrieveSubscriberCredentialByIdAsync(It.IsAny<Guid>(), It.IsAny<bool>()))
+                    .ThrowsAsync(dependancyValidationException);
+
+            // When
+            ValueTask<List<string>> retrieveListOfDocumentsToProcessTask =
+                this.emisLandingCoordinationService
+                    .RetrieveListOfDocumentsToProcessAsync(subscriberAgreementId: subscriberCredentialId);
+
+            EmisLandingCoordinationDependencyValidationException
+                actuaEmisLandingCoordinationDependencyValidationException =
+                    await Assert.ThrowsAsync<EmisLandingCoordinationDependencyValidationException>(
+                        retrieveListOfDocumentsToProcessTask.AsTask);
+
+            // Then
+            actuaEmisLandingCoordinationDependencyValidationException.Should()
+                .BeEquivalentTo(expectedEmisLandingCoordinationDependencyValidationException);
+
+            this.subscriberCredentialOrchestrationMock.Verify(service =>
+                service.RetrieveSubscriberCredentialByIdAsync(It.IsAny<Guid>(), It.IsAny<bool>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(IsSameExceptionAs(
+                     expectedEmisLandingCoordinationDependencyValidationException))),
+                         Times.Once);
+
+            this.subscriberCredentialOrchestrationMock.VerifyNoOtherCalls();
+            this.emisLandingExtractionOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
+

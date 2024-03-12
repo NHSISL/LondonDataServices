a3d876903fa5d4e2a37d0995dc3937f31e9ef19d
Christo du Toit <christo@dutoit.co.uk>
2024-03-12 13:24:55 +0000

    CONTROLLERS: POST SubscriberAgreement

---
+++ LHDS.AdminPortal.Api/Controllers/SubscriberAgreementsController.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using LHDS.AdminPortal.Api.Models.Foundations.SubscriberAgreements;
+using LHDS.AdminPortal.Api.Models.Foundations.SubscriberAgreements.Exceptions;
+using LHDS.AdminPortal.Api.Services.Foundations.SubscriberAgreements;
+
+namespace LHDS.AdminPortal.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SubscriberAgreementsController : RESTFulController
+    {
+        private readonly ISubscriberAgreementService subscriberAgreementService;
+
+        public SubscriberAgreementsController(ISubscriberAgreementService subscriberAgreementService) =>
+            this.subscriberAgreementService = subscriberAgreementService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<SubscriberAgreement>> PostSubscriberAgreementAsync(SubscriberAgreement subscriberAgreement)
+        {
+            try
+            {
+                SubscriberAgreement addedSubscriberAgreement =
+                    await this.subscriberAgreementService.AddSubscriberAgreementAsync(subscriberAgreement);
+
+                return Created(addedSubscriberAgreement);
+            }
+            catch (SubscriberAgreementValidationException subscriberAgreementValidationException)
+            {
+                return BadRequest(subscriberAgreementValidationException.InnerException);
+            }
+            catch (SubscriberAgreementDependencyValidationException subscriberAgreementValidationException)
+                when (subscriberAgreementValidationException.InnerException is InvalidSubscriberAgreementReferenceException)
+            {
+                return FailedDependency(subscriberAgreementValidationException.InnerException);
+            }
+            catch (SubscriberAgreementDependencyValidationException subscriberAgreementDependencyValidationException)
+               when (subscriberAgreementDependencyValidationException.InnerException is AlreadyExistsSubscriberAgreementException)
+            {
+                return Conflict(subscriberAgreementDependencyValidationException.InnerException);
+            }
+            catch (SubscriberAgreementDependencyException subscriberAgreementDependencyException)
+            {
+                return InternalServerError(subscriberAgreementDependencyException);
+            }
+            catch (SubscriberAgreementServiceException subscriberAgreementServiceException)
+            {
+                return InternalServerError(subscriberAgreementServiceException);
+            }
+        }
+    }
+}

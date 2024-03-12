4e13416d98d65743a05352bd035aed21e6076737
Hassan Raza <h_raza110@outlook.com>
2024-03-12 16:14:15 +0000

    Adding models

--- LHDS.AdminPortal.Api.Tests.Acceptance/Models/SubscriberAgreements/SubscriberAgreement.cs
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Models/SubscriberAgreements/SubscriberAgreement.cs
@@ -5,9 +5,14 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Models.SubscriberAgreements
     public class SubscriberAgreement
     {
         public Guid Id { get; set; }
-
-        // TODO:  Add your properties here
-
+        public string SupplierSharingAgreementShortName { get; set; }
+        public Guid? SupplierSharingAgreementGuid { get; set; }
+        public string? FtpUserName { get; set; }
+        public string? FtpPublicKey { get; set; }
+        public string? GpgPublicKey { get; set; }
+        public bool IsActive { get; set; }
+        public DateTimeOffset? LastPollStartDate { get; set; }
+        public DateTimeOffset? LastPollEndDate { get; set; }
         public string CreatedBy { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
         public string UpdatedBy { get; set; }

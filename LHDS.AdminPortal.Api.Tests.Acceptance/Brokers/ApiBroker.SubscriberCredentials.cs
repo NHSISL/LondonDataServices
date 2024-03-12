5be434f8727ebfcf09a054e11f69bc163a7d2b2e
Hassan Raza <h_raza110@outlook.com>
2024-03-12 15:34:10 +0000

    Fixing api broker

--- LHDS.AdminPortal.Api.Tests.Acceptance/Brokers/ApiBroker.SubscriberCredentials.cs
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Brokers/ApiBroker.SubscriberCredentials.cs
@@ -9,19 +9,24 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Brokers
     {
         private const string SubscriberCredentialsRelativeUrl = "api/subscriberCredentials";
 
-        public async ValueTask<SubscriberCredential> PostSubscriberCredentialAsync(SubscriberCredential subscriberCredential) =>
-            await this.apiFactoryClient.PostContentAsync(SubscriberCredentialsRelativeUrl, subscriberCredential);
+        public async ValueTask<SubscriberCredential> PostSubscriberCredentialAsync(
+            SubscriberCredential subscriberCredential) =>
+                await this.apiFactoryClient.PostContentAsync(SubscriberCredentialsRelativeUrl, subscriberCredential);
 
         public async ValueTask<SubscriberCredential> GetSubscriberCredentialByIdAsync(Guid subscriberCredentialId) =>
-            await this.apiFactoryClient.GetContentAsync<SubscriberCredential>($"{SubscriberCredentialsRelativeUrl}/{subscriberCredentialId}");
+            await this.apiFactoryClient.GetContentAsync<SubscriberCredential>(
+                $"{SubscriberCredentialsRelativeUrl}/{subscriberCredentialId}");
 
         public async ValueTask<List<SubscriberCredential>> GetAllSubscriberCredentialsAsync() =>
-          await this.apiFactoryClient.GetContentAsync<List<SubscriberCredential>>($"{SubscriberCredentialsRelativeUrl}/");
+          await this.apiFactoryClient.GetContentAsync<List<SubscriberCredential>>(
+              $"{SubscriberCredentialsRelativeUrl}/");
 
-        public async ValueTask<SubscriberCredential> PutSubscriberCredentialAsync(SubscriberCredential subscriberCredential) =>
-            await this.apiFactoryClient.PutContentAsync(SubscriberCredentialsRelativeUrl, subscriberCredential);
+        public async ValueTask<SubscriberCredential> PutSubscriberCredentialAsync(
+            SubscriberCredential subscriberCredential) =>
+                await this.apiFactoryClient.PutContentAsync(SubscriberCredentialsRelativeUrl, subscriberCredential);
 
         public async ValueTask<SubscriberCredential> DeleteSubscriberCredentialByIdAsync(Guid subscriberCredentialId) =>
-            await this.apiFactoryClient.DeleteContentAsync<SubscriberCredential>($"{SubscriberCredentialsRelativeUrl}/{subscriberCredentialId}");
+            await this.apiFactoryClient.DeleteContentAsync<SubscriberCredential>(
+                $"{SubscriberCredentialsRelativeUrl}/{subscriberCredentialId}");
     }
 }

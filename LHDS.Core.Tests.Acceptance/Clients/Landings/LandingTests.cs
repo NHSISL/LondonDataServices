e51232547b9fb0f8209776bc3da08c0cfa40ac8f
Christo du Toit <christo@dutoit.co.uk>
2024-03-13 17:23:59 +0000

    WIP: Landing tests

--- LHDS.Core.Tests.Acceptance/Clients/Landings/LandingTests.cs
+++ LHDS.Core.Tests.Acceptance/Clients/Landings/LandingTests.cs
@@ -1,10 +1,9 @@
-﻿// ---------------------------------------------------------------
+﻿// ---------------------------------------------------------
 // Copyright (c) North East London ICB. All rights reserved.
-// ---------------------------------------------------------------
+// ---------------------------------------------------------
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.DateTimes;
 using LHDS.Core.Brokers.Downloads;
@@ -18,6 +17,7 @@ using LHDS.Core.Models.Foundations.Documents;
 using LHDS.Core.Models.Foundations.IngestionTrackings;
 using LHDS.Core.Models.Foundations.Suppliers;
 using LHDS.Core.Models.Orchestrations.EmisLandings;
+using LHDS.Core.Models.Processings.SubscriberCredentials;
 using LHDS.Core.Services.Foundations.DataSets;
 using LHDS.Core.Services.Foundations.DataSetSpecifications;
 using LHDS.Core.Services.Foundations.IngestionTrackingAudits;
@@ -83,7 +83,7 @@ namespace LHDS.Core.Tests.Acceptance.Clients.Landings
             this.dataSetService = serviceProvider.GetService<IDataSetService>();
             this.dataSetSpecificationService = serviceProvider.GetService<IDataSetSpecificationService>();
 
-            this.dataSetSpecificationProcessingService = 
+            this.dataSetSpecificationProcessingService =
                 serviceProvider.GetService<IDataSetSpecificationProcessingService>();
 
             this.auditService = serviceProvider.GetService<IIngestionTrackingAuditService>();
@@ -99,7 +99,7 @@ namespace LHDS.Core.Tests.Acceptance.Clients.Landings
         {
             string filename = GetRandomString();
 
-            for (int i = 0; i< 6; i++)
+            for (int i = 0; i < 6; i++)
             {
                 filename = $"{filename}_{GetRandomString(10)}";
             }
@@ -107,8 +107,8 @@ namespace LHDS.Core.Tests.Acceptance.Clients.Landings
             return filename;
         }
 
-    private static string GetRandomString(int length) =>
-           new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+        private static string GetRandomString(int length) =>
+               new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
 
         private static int GetRandomNumber(int min = 2, int max = 10) =>
             new IntRange(min, max).GetValue();
@@ -170,7 +170,7 @@ namespace LHDS.Core.Tests.Acceptance.Clients.Landings
         }
 
         private static Supplier CreateRandomSupplier(Guid supplierId, DateTimeOffset dateTimeOffset) =>
-            CreateSupplierFiller(supplierId ,dateTimeOffset).Create();
+            CreateSupplierFiller(supplierId, dateTimeOffset).Create();
 
         private static Filler<Supplier> CreateSupplierFiller(Guid supplierId, DateTimeOffset dateTimeOffset)
         {
@@ -242,6 +242,24 @@ namespace LHDS.Core.Tests.Acceptance.Clients.Landings
                 .OnProperty(dataSetSpecification => dataSetSpecification.CreatedBy).Use(user)
                 .OnProperty(dataSetSpecification => dataSetSpecification.CreatedBy).Use(user)
                 .OnProperty(dataSetSpecification => dataSetSpecification.UpdatedBy).Use(user);
+
+            return filler;
+        }
+
+        private static SubscriberCredential CreateRandomSubscriberCredential() =>
+            CreateSubscriberCredentialFiller().Create();
+
+        private static Filler<SubscriberCredential> CreateSubscriberCredentialFiller()
+        {
+            var filler = new Filler<SubscriberCredential>();
+            string user = Guid.NewGuid().ToString();
+            var now = DateTimeOffset.UtcNow;
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(now)
+                .OnType<DateTimeOffset?>().Use(now)
+                .OnProperty(subscriberCredential => subscriberCredential.CreatedBy).Use(user)
+                .OnProperty(subscriberCredential => subscriberCredential.UpdatedBy).Use(user);
 
             return filler;
         }

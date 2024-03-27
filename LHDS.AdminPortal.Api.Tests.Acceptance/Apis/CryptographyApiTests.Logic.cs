d4b4abb5d55f9c112944df4eaa54f5588981d0b9
Christo du Toit <christo@dutoit.co.uk>
2024-03-27 11:08:28 +0000

    CODE RUB: Added code comments for broken tests

--- LHDS.AdminPortal.Api.Tests.Acceptance/Apis/CryptographyApiTests.Logic.cs
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Apis/CryptographyApiTests.Logic.cs
@@ -2,6 +2,7 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -15,17 +16,24 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Apis.Cryptographies
         public async Task ShouldEncryptAndDecryptAsync()
         {
             // given
+            // TODO:  @Hassan -> Setup a subscriber agreement
+            Guid subscriberAgreemnetId = Guid.NewGuid();
             string randomString = GetRandomString();
             byte[] inputBytes = Encoding.ASCII.GetBytes(randomString);
             string expectedString = randomString;
 
             //When
-            byte[] encryptedData = await this.apiBroker.PostEncryptDataAsync(inputBytes);
-            byte[] decryptedData = await this.apiBroker.PostDecryptDataAsync(encryptedData);
+            byte[] encryptedData = await this.apiBroker
+                .PostEncryptDataAsync(subscriberAgreemnetId, data: inputBytes);
+
+            byte[] decryptedData = await this.apiBroker
+                .PostDecryptDataAsync(subscriberAgreemnetId, data: encryptedData);
+
             string actualString = Encoding.ASCII.GetString(decryptedData);
 
             //Then
             actualString.Should().BeEquivalentTo(expectedString);
+            // TODO:  @Hassan -> Remove the subscriber agreement
         }
     }
 }

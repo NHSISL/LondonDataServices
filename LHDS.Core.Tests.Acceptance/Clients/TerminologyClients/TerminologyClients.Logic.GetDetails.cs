a25458e3f10387e49f8942f825a1c95940714c91
Christo du Toit <christo@dutoit.co.uk>
2024-04-11 12:32:46 +0100

    ShouldRetrieveArtifactDetailsAsync ->WIP

---
+++ LHDS.Core.Tests.Acceptance/Clients/TerminologyClients/TerminologyClients.Logic.GetDetails.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LHDS.Core.Tests.Acceptance.Clients.Terminologies
+{
+    public partial class TerminologyClients
+    {
+        [Fact]
+        public async Task ShouldRetrieveArtifactDetailsAsync()
+        {
+            //Given
+
+            //When
+            await terminologyClient.RetrieveArtifactDetailsAsync();
+
+            //Then
+        }
+    }
+}

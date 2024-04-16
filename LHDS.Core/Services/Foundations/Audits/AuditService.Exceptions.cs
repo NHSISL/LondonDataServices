143ba0505839bf9ec94f654f21e9e9ec5176bab6
Christo du Toit <christo@dutoit.co.uk>
2024-04-16 12:26:54 +0100

    ShouldThrowDependencyExceptionOnAddIfDatabaseUpdateErrorOccursAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/Audits/AuditService.Exceptions.cs
+++ LHDS.Core/Services/Foundations/Audits/AuditService.Exceptions.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using EFxceptions.Models.Exceptions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using LHDS.Core.Models.Foundations.Audits;
 using LHDS.Core.Models.Foundations.Audits.Exceptions;
 using Xeptions;
@@ -52,6 +53,15 @@ namespace LHDS.Core.Services.Foundations.Audits
 
                 throw CreateAndLogDependencyValidationException(invalidAuditReferenceException);
             }
+            catch (DbUpdateException databaseUpdateException)
+            {
+                var failedAuditStorageException =
+                    new FailedAuditStorageException(
+                    message: "Failed audit storage error occurred, contact support.",
+                    innerException: databaseUpdateException);
+
+                throw CreateAndLogDependencyException(failedAuditStorageException);
+            }
         }
 
         private AuditValidationException CreateAndLogValidationException(Xeption exception)
@@ -88,6 +98,19 @@ namespace LHDS.Core.Services.Foundations.Audits
             this.loggingBroker.LogError(auditDependencyValidationException);
 
             return auditDependencyValidationException;
+        }
+
+        private AuditDependencyException CreateAndLogDependencyException(
+            Xeption exception)
+        {
+            var auditDependencyException = 
+                new AuditDependencyException(
+                    message: "Audit dependency error occurred, contact support.",
+                    innerException: exception); 
+
+            this.loggingBroker.LogError(auditDependencyException);
+
+            return auditDependencyException;
         }
     }
 }

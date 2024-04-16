e2d6582efe421b4672b5bd42538a1ed589a2c1f9
Christo du Toit <christo@dutoit.co.uk>
2024-04-16 12:28:09 +0100

    ShouldThrowNotFoundExceptionOnRemoveIfAuditIsNotFoundAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/Audits/AuditService.cs
+++ LHDS.Core/Services/Foundations/Audits/AuditService.cs
@@ -70,6 +70,8 @@ namespace LHDS.Core.Services.Foundations.Audits
                 Audit maybeAudit = await this.storageBroker
                     .SelectAuditByIdAsync(auditId);
 
+                ValidateStorageAudit(maybeAudit, auditId);
+
                 return await this.storageBroker.DeleteAuditAsync(maybeAudit);
             });
     }

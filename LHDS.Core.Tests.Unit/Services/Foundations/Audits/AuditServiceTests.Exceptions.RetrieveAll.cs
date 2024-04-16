94b64453ebac565df038e4952500fd6778c07d2e
Christo du Toit <christo@dutoit.co.uk>
2024-04-16 12:27:06 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,57 @@
+using System;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using LHDS.Core.Models.Foundations.Audits.Exceptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Audits
+{
+    public partial class AuditServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedAuditStorageException =
+                new FailedAuditStorageException(
+                    message: "Failed audit storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedAuditDependencyException =
+                new AuditDependencyException(
+                    message: "Audit dependency error occurred, contact support.",
+                    innerException: failedAuditStorageException); 
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllAudits())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllAuditsAction = () =>
+                this.auditService.RetrieveAllAudits();
+
+            AuditDependencyException actualAuditDependencyException =
+                Assert.Throws<AuditDependencyException>(retrieveAllAuditsAction);
+
+            // then
+            actualAuditDependencyException.Should()
+                .BeEquivalentTo(expectedAuditDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllAudits(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedAuditDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

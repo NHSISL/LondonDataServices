eb6e5eac1d5e399f743aa7b4c6011c5b3e20f04f
Christo du Toit <christo@dutoit.co.uk>
2024-04-16 12:26:48 +0100

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/Audits/AuditService.Exceptions.cs
+++ LHDS.Core/Services/Foundations/Audits/AuditService.Exceptions.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using LHDS.Core.Models.Foundations.Audits;
 using LHDS.Core.Models.Foundations.Audits.Exceptions;
 using Xeptions;
@@ -23,6 +24,15 @@ namespace LHDS.Core.Services.Foundations.Audits
             {
                 throw CreateAndLogValidationException(invalidAuditException);
             }
+            catch (SqlException sqlException)
+            {
+                var failedAuditStorageException =
+                    new FailedAuditStorageException(
+                        message: "Failed audit storage error occurred, contact support.",
+                        innerException: sqlException);
+
+                throw CreateAndLogCriticalDependencyException(failedAuditStorageException);
+            }
         }
 
         private AuditValidationException CreateAndLogValidationException(Xeption exception)
@@ -35,6 +45,18 @@ namespace LHDS.Core.Services.Foundations.Audits
             this.loggingBroker.LogError(auditValidationException);
 
             return auditValidationException;
+        }
+
+        private AuditDependencyException CreateAndLogCriticalDependencyException(Xeption exception)
+        {
+            var auditDependencyException = 
+                new AuditDependencyException(
+                    message: "Audit dependency error occurred, contact support.",
+                    innerException: exception);
+
+            this.loggingBroker.LogCritical(auditDependencyException);
+
+            return auditDependencyException;
         }
     }
 }

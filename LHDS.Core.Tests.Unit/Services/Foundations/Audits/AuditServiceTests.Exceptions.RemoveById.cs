577a75e9dee1f871710f230903caa92225873057
Christo du Toit <christo@dutoit.co.uk>
2024-04-16 12:28:11 +0100

    ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.Exceptions.RemoveById.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.Exceptions.RemoveById.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using LHDS.Core.Models.Foundations.Audits;
 using LHDS.Core.Models.Foundations.Audits.Exceptions;
@@ -58,6 +60,59 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Audits
 
             this.dateTimeBrokerMock.Verify(broker =>
                 broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someAuditId = Guid.NewGuid();
+
+            var databaseUpdateConcurrencyException =
+                new DbUpdateConcurrencyException();
+
+            var lockedAuditException =
+                new LockedAuditException(
+                    message: "Locked audit record exception, please try again later",
+                    innerException: databaseUpdateConcurrencyException);
+
+            var expectedAuditDependencyValidationException =
+                new AuditDependencyValidationException(
+                    message: "Audit dependency validation occurred, please try again.",
+                    innerException: lockedAuditException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAuditByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(databaseUpdateConcurrencyException);
+
+            // when
+            ValueTask<Audit> removeAuditByIdTask =
+                this.auditService.RemoveAuditByIdAsync(someAuditId);
+
+            AuditDependencyValidationException actualAuditDependencyValidationException =
+                await Assert.ThrowsAsync<AuditDependencyValidationException>(
+                    removeAuditByIdTask.AsTask);
+
+            // then
+            actualAuditDependencyValidationException.Should()
+                .BeEquivalentTo(expectedAuditDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAuditByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAuditDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteAuditAsync(It.IsAny<Audit>()),
                     Times.Never);
 
             this.storageBrokerMock.VerifyNoOtherCalls();

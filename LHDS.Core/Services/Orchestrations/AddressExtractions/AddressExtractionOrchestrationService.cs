16380a81e748e6eed12c52dae3c5aa03789e6e6a
Hassan Raza <h_raza110@outlook.com>
2024-04-16 12:24:46 +0100

    Stubbing Address Extraction Orchestration

--- LHDS.Core/Services/Orchestrations/AddressExtractions/AddressExtractionOrchestrationService.cs
+++ LHDS.Core/Services/Orchestrations/AddressExtractions/AddressExtractionOrchestrationService.cs
@@ -4,13 +4,12 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.DateTimes;
-using LHDS.Core.Brokers.Identifiers;
 using LHDS.Core.Brokers.Loggings;
 using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Foundations.ResolvedAddresses;
+using LHDS.Core.Services.Foundations.AddressNormalisations;
 using LHDS.Core.Services.Foundations.AddressParsers;
 
 namespace LHDS.Core.Services.Orchestrations.AddressExtractions
@@ -18,77 +17,26 @@ namespace LHDS.Core.Services.Orchestrations.AddressExtractions
     public partial class AddressExtractionOrchestrationService : IAddressExtractionOrchestrationService
     {
         private readonly IAddressParserService addressParserService;
+        private readonly IAddressNormalisationService addressNormalisationService;
         private readonly ILoggingBroker loggingBroker;
         private readonly IDateTimeBroker dateTimeBroker;
-        private readonly IIdentifierBroker identifierBroker;
 
         public AddressExtractionOrchestrationService(
             IAddressParserService addressParserService,
+            IAddressNormalisationService addressNormalisationService,
             ILoggingBroker loggingBroker,
-            IDateTimeBroker dateTimeBroker,
-            IIdentifierBroker identifierBroker)
+            IDateTimeBroker dateTimeBroker)
         {
             this.addressParserService = addressParserService;
+            this.addressNormalisationService = addressNormalisationService;
             this.loggingBroker = loggingBroker;
             this.dateTimeBroker = dateTimeBroker;
-            this.identifierBroker = identifierBroker;
         }
 
-        public ValueTask<List<Address>> ProcessDataAsync(byte[] data) =>
-            TryCatch(async () =>
-            {
-                ValidateDataOnProcessData(data);
-                return await ProcessAddressDataAsync(data);
-            });
+        public async ValueTask<List<Address>> ProcessAddressesAsync(byte[] data) =>
+            throw new NotImplementedException();
 
-        private async ValueTask<List<Address>> ProcessAddressDataAsync(byte[] data)
-        {
-            List<Address> addresses = new List<Address>();
-
-            using (MemoryStream memoryStream = new MemoryStream(data))
-            using (ZipArchive archive = new ZipArchive(memoryStream))
-            {
-                foreach (ZipArchiveEntry entry in archive.Entries)
-                {
-                    if (entry.Name.EndsWith(".csv", StringComparison.OrdinalIgnoreCase))
-                    {
-                        using (var entryStream = entry.Open())
-                        using (var tempMemoryStream = new MemoryStream())
-                        {
-                            await entryStream.CopyToAsync(tempMemoryStream);
-                            byte[] csvData = tempMemoryStream.ToArray();
-
-                            List<Address> csvAddresses =
-                                await this.addressParserService.ProcessCsvAsync(csvData);
-
-                            addresses.AddRange(csvAddresses);
-                            var dateStamp = this.dateTimeBroker.GetCurrentDateTimeOffset();
-                        }
-                    }
-                    else if (entry.Name.EndsWith(".zip", StringComparison.OrdinalIgnoreCase))
-                    {
-                        byte[] nestedZipData;
-
-                        using (MemoryStream nestedMemoryStream = new MemoryStream())
-                        using (Stream entryStream = entry.Open())
-                        {
-                            entryStream.CopyTo(nestedMemoryStream);
-                            nestedZipData = nestedMemoryStream.ToArray();
-                        }
-
-                        if (nestedZipData.Length <= 0)
-                        {
-                            Console.WriteLine($"nestedZipData is null");
-                            throw new Exception($"nestedZipData is null");
-                        }
-
-                        List<Address> nestedAddresses = await ProcessAddressDataAsync(nestedZipData);
-                        addresses.AddRange(nestedAddresses);
-                    }
-                }
-            }
-
-            return addresses;
-        }
+        public ValueTask<List<ResolvedAddress>> ProcessResolvedAddressesAsync(byte[] data) =>
+            throw new NotImplementedException(); 
     }
 }

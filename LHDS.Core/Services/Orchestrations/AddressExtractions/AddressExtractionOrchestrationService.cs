cd367723a25b9a7050a798793d1bfaa2c42f640c
Hassan Raza <h_raza110@outlook.com>
2024-04-17 12:40:12 +0100

    Adding resource file

--- LHDS.Core/Services/Orchestrations/AddressExtractions/AddressExtractionOrchestrationService.cs
+++ LHDS.Core/Services/Orchestrations/AddressExtractions/AddressExtractionOrchestrationService.cs
@@ -12,6 +12,7 @@ using LHDS.Core.Brokers.Identifiers;
 using LHDS.Core.Brokers.Loggings;
 using LHDS.Core.Models.Foundations.Addresses;
 using LHDS.Core.Models.Foundations.AddressExtractionAudits;
+using LHDS.Core.Models.Foundations.ResolvedAddresses;
 using LHDS.Core.Services.Foundations.AddressExtractionAudits;
 using LHDS.Core.Services.Foundations.AddressParsers;
 
@@ -39,76 +40,10 @@ namespace LHDS.Core.Services.Orchestrations.AddressExtractions
             this.identifierBroker = identifierBroker;
         }
 
-        public ValueTask<List<Address>> ProcessDataAsync(byte[] data) =>
-            TryCatch(async () =>
-            {
-                ValidateDataOnProcessData(data);
-                return await ProcessAddressDataAsync(data);
-            });
+        public async ValueTask<List<Address>> ProcessAddressesAsync(byte[] data) =>
+           throw new NotImplementedException();
 
-        private async ValueTask<List<Address>> ProcessAddressDataAsync(byte[] data)
-        {
-            List<Address> addresses = new List<Address>();
-
-            using (MemoryStream memoryStream = new MemoryStream(data))
-            using (ZipArchive archive = new ZipArchive(memoryStream))
-            {
-                foreach (ZipArchiveEntry entry in archive.Entries)
-                {
-                    if (entry.Name.EndsWith(".csv", StringComparison.OrdinalIgnoreCase))
-                    {
-                        using (var entryStream = entry.Open())
-                        using (var tempMemoryStream = new MemoryStream())
-                        {
-                            await entryStream.CopyToAsync(tempMemoryStream);
-                            byte[] csvData = tempMemoryStream.ToArray();
-
-                            List<Address> csvAddresses =
-                                await this.addressParserService.ProcessCsvAsync(csvData);
-
-                            addresses.AddRange(csvAddresses);
-                            var dateStamp = this.dateTimeBroker.GetCurrentDateTimeOffset();
-
-                            var audit = new AddressExtractionAudit
-                            {
-                                Id = this.identifierBroker.GetIdentifier(),
-                                CorrelationId = this.identifierBroker.GetIdentifier(),
-                                FileName = $"{entry}",
-                                Message = "Success",
-                                MessageId = "",
-                                CreatedBy = "System",
-                                UpdatedBy = "System",
-                                UpdatedDate = dateStamp,
-                                CreatedDate = dateStamp,
-                            };
-
-                            await this.addressExtractionAuditService.AddAddressExtractionAuditAsync(audit);
-                        }
-                    }
-                    else if (entry.Name.EndsWith(".zip", StringComparison.OrdinalIgnoreCase))
-                    {
-                        byte[] nestedZipData;
-
-                        using (MemoryStream nestedMemoryStream = new MemoryStream())
-                        using (Stream entryStream = entry.Open())
-                        {
-                            entryStream.CopyTo(nestedMemoryStream);
-                            nestedZipData = nestedMemoryStream.ToArray();
-                        }
-
-                        if (nestedZipData.Length <= 0)
-                        {
-                            Console.WriteLine($"nestedZipData is null");
-                            throw new Exception($"nestedZipData is null");
-                        }
-
-                        List<Address> nestedAddresses = await ProcessAddressDataAsync(nestedZipData);
-                        addresses.AddRange(nestedAddresses);
-                    }
-                }
-            }
-
-            return addresses;
-        }
+        public async ValueTask<List<ResolvedAddress>> ProcessResolvedAddressesAsync(byte[] data) =>
+            throw new NotImplementedException();
     }
 }

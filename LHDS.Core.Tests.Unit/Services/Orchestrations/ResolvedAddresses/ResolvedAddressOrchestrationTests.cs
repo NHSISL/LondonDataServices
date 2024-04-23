7397004a2097353cc610ac85ef1867137d663d2b
Christo du Toit <christo@dutoit.co.uk>
2024-04-23 16:54:34 +0100

    ShouldThrowDependencyValidationExceptionOnAddIfDependencyValidationErrorOccursAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Orchestrations/ResolvedAddresses/ResolvedAddressOrchestrationTests.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/ResolvedAddresses/ResolvedAddressOrchestrationTests.cs
@@ -6,12 +6,15 @@ using System;
 using System.Linq.Expressions;
 using LHDS.Core.Brokers.DateTimes;
 using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Models.Processings.Documents.Exceptions;
+using LHDS.Core.Models.Processings.ResolvedAddresses.Exceptions;
 using LHDS.Core.Services.Orchestrations.ResolvedAddresses;
 using LHDS.Core.Services.Processings.Documents;
 using LHDS.Core.Services.Processings.ResolvedAddresses;
 using Moq;
 using Tynamix.ObjectFiller;
 using Xeptions;
+using Xunit;
 
 namespace LHDS.Core.Tests.Unit.Services.Orchestrations.ResolvedAddresses
 {
@@ -42,5 +45,58 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.ResolvedAddresses
 
         private static string GetRandomString() =>
             new MnemonicString().GetValue();
+
+        public static TheoryData DependencyValidationExceptions()
+        {
+            string randomMessage = GetRandomString();
+            string exceptionMessage = randomMessage;
+            var innerException = new Xeption(exceptionMessage);
+
+            return new TheoryData<Xeption>
+            {
+                new DocumentProcessingValidationException(
+                    message: "Document processing validation error occured, please try again",
+                    innerException),
+
+                new DocumentProcessingDependencyValidationException(
+                    message: "Document processing dependency validation error occurred, please try again.",
+                    innerException),
+
+                new ResolvedAddressProcessingValidationException(
+                    message: "Resolved address processing validation error occured, please try again",
+                    innerException),
+
+                new ResolvedAddressProcessingDependencyValidationException(
+                    message: "Resolved address processing dependency validation error occurred, please try again.",
+                    innerException),
+            };
+        }
+
+        public static TheoryData DependencyExceptions()
+        {
+            string randomMessage = GetRandomString();
+            string exceptionMessage = randomMessage;
+            var innerException = new Xeption(exceptionMessage);
+
+            return new TheoryData<Xeption>
+            {
+                new DocumentProcessingDependencyException(
+                    message: "Document processing dependency error occurred, contact support.",
+                    innerException),
+
+                new DocumentProcessingServiceException(
+                    message: "Document processing service error occurred, contact support.",
+                    innerException),
+
+                new ResolvedAddressProcessingDependencyException(
+                    message: "Resolved address processing dependency error occurred, contact support.",
+                    innerException),
+
+                new ResolvedAddressProcessingServiceException(
+                    message: "Resolved address processing service error occurred, contact support.",
+                    innerException),
+            };
+        }
+
     }
 }

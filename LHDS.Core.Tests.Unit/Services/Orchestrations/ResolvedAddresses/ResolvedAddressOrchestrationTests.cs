70ada19c5ef3e16fec91cf71f0f23485feb7083a
Christo du Toit <christo@dutoit.co.uk>
2024-04-23 14:40:15 +0100

    ShouldAddFileAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/ResolvedAddresses/ResolvedAddressOrchestrationTests.cs
@@ -0,0 +1,40 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Services.Orchestrations.ResolvedAddresses;
+using LHDS.Core.Services.Processings.Documents;
+using LHDS.Core.Services.Processings.ResolvedAddresses;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace LHDS.Core.Tests.Unit.Services.Orchestrations.ResolvedAddresses
+{
+    public partial class ResolvedAddressOrchestrationTests
+    {
+        private readonly Mock<IDocumentProcessingService> documentProcessingServiceMock;
+        private readonly Mock<IResolvedAddressProcessingService> resolvedAddressProcessingServiceMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IResolvedAddressOrchestrationService resolvedAddressOrchestrationService;
+
+        public ResolvedAddressOrchestrationTests()
+        {
+            this.documentProcessingServiceMock = new Mock<IDocumentProcessingService>();
+            this.resolvedAddressProcessingServiceMock = new Mock<IResolvedAddressProcessingService>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.resolvedAddressOrchestrationService = new ResolvedAddressOrchestrationService(
+                documentProcessingService: this.documentProcessingServiceMock.Object,
+                resolvedAddressProcessingService: this.resolvedAddressProcessingServiceMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+    }
+}

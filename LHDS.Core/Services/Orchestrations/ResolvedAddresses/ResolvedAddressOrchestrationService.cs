ac8d7fe5930d9a8163f4f5fd358ce24a9ed8b18e
Christo du Toit <christo@dutoit.co.uk>
2024-04-23 17:46:41 +0100

    ShouldRemoveDocumentAsync -> PASS

--- LHDS.Core/Services/Orchestrations/ResolvedAddresses/ResolvedAddressOrchestrationService.cs
+++ LHDS.Core/Services/Orchestrations/ResolvedAddresses/ResolvedAddressOrchestrationService.cs
@@ -46,7 +46,10 @@ namespace LHDS.Core.Services.Orchestrations.ResolvedAddresses
             });
 
         public ValueTask RemoveDocumentByFileNameAsync(string fileName, string container) =>
-            throw new NotImplementedException();
+            TryCatch(async () =>
+            {
+                await this.documentProcessingService.RemoveDocumentByFileNameAsync(fileName, container);
+            });
 
         public ValueTask<Guid> UploadResolvedAddressesAsync() =>
             throw new NotImplementedException();

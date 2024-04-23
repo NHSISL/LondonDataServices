63ad9daef54a7331b1b04bdb964859e9f0871820
Christo du Toit <christo@dutoit.co.uk>
2024-04-23 12:35:50 +0100

    CODE RUB: Stubbed interfaces

--- LHDS.Core/Services/Orchestrations/ResolvedAddresses/IResolvedAddressOrchestrationService.cs
+++ LHDS.Core/Services/Orchestrations/ResolvedAddresses/IResolvedAddressOrchestrationService.cs
@@ -4,12 +4,14 @@
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Foundations.ResolvedAddresses;
 
 namespace LHDS.Core.Services.Orchestrations.ResolvedAddresses
 {
     public interface IResolvedAddressOrchestrationService
     {
-        ValueTask<List<Address>> UploadResolvedAddressesAsync();
+        ValueTask<List<ResolvedAddress>> UploadResolvedAddressesAsync();
+        ValueTask AddDocumentAsync(byte[] data, string fileName, string container);
+        ValueTask RemoveDocumentByFileNameAsync(string fileName, string container);
     }
 }

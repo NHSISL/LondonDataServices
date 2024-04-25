256509173aa9c3e59ed8f0d2d934bec08688f142
Christo du Toit <christo@dutoit.co.uk>
2024-04-25 15:24:45 +0100

    CODE RUB: Stub interfaces

---
+++ LHDS.Core/Services/Foundations/CsvParsers/ICsvParserService.cs
@@ -0,0 +1,23 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LHDS.Core.Services.Foundations.CsvParsers
+{
+    public interface ICsvParserService
+    {
+        ValueTask<List<T>> MapCsvToObjectAsync<T>(
+            string data,
+            bool hasHeaderRecord,
+            Dictionary<string, int>? fieldMappings);
+
+        ValueTask<string> MapObjectToCsvAsync<T>(
+            List<T> @object,
+            bool addHeaderRecord = true,
+            Dictionary<string, int>? fieldMappings = null,
+            bool? shouldAddTrailingComma = false);
+    }
+}

209632ac14a62eb94220c4754b780e740cb2ccc4
Hassan Raza <h_raza110@outlook.com>
2024-04-29 16:34:20 +0100

    ShouldThrowServiceExceptionOnUploadIfServiceErrorOccursAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/ResolvedAddresses/ResolvedAddressOrchestrationTests.Exceptions.UploadResolvedAddress.cs
@@ -0,0 +1,154 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Orchestrations.ResolvedAddresses.Exceptions;
+using Moq;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Orchestrations.ResolvedAddresses
+{
+    public partial class ResolvedAddressOrchestrationTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task
+            ShouldThrowDependencyValidationExceptionOnUploadIfDependencyValidationErrorOccursAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            var expectedResolvedAddressOrchestrationDependencyValidationException =
+                new ResolvedAddressOrchestrationDependencyValidationException(
+                    message: "Resolved address orchestration dependency validation occurred, please try again.",
+                    innerException: dependencyValidationException.InnerException as Xeption);
+
+            this.resolvedAddressProcessingServiceMock.Setup(service =>
+                service.RetrieveAllResolvedAddresses())
+                    .Throws(dependencyValidationException);
+
+            // when
+            ValueTask<Guid> documentAddTask =
+                this.resolvedAddressOrchestrationService.UploadResolvedAddressesAsync();
+
+            ResolvedAddressOrchestrationDependencyException actualException =
+                await Assert.ThrowsAsync<ResolvedAddressOrchestrationDependencyException>(documentAddTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedResolvedAddressOrchestrationDependencyValidationException);
+
+            this.resolvedAddressProcessingServiceMock.Verify(service =>
+                service.RetrieveAllResolvedAddresses(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedResolvedAddressOrchestrationDependencyValidationException))),
+                         Times.Once);
+
+            this.resolvedAddressProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.documentProcessingServiceMock.VerifyNoOtherCalls();
+            this.csvMapperProcessingServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task ShouldThrowDependencyOnUploadIfDependencyErrorOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            var expectedResolvedAddressOrchestrationDependencyException =
+                new ResolvedAddressOrchestrationDependencyException(
+                    message: "Resolved address orchestration dependency error occurred, please try again.",
+                    innerException: dependencyException.InnerException as Xeption);
+
+            this.resolvedAddressProcessingServiceMock.Setup(service =>
+                service.RetrieveAllResolvedAddresses())
+                    .Throws(dependencyException);
+
+            // when
+            ValueTask<Guid> documentAddTask =
+                this.resolvedAddressOrchestrationService.UploadResolvedAddressesAsync();
+
+            ResolvedAddressOrchestrationDependencyException actualException =
+                await Assert.ThrowsAsync<ResolvedAddressOrchestrationDependencyException>(documentAddTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedResolvedAddressOrchestrationDependencyException);
+
+            this.resolvedAddressProcessingServiceMock.Verify(service =>
+                service.RetrieveAllResolvedAddresses(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedResolvedAddressOrchestrationDependencyException))),
+                         Times.Once);
+
+            this.resolvedAddressProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.documentProcessingServiceMock.VerifyNoOtherCalls();
+            this.csvMapperProcessingServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnUploadIfServiceErrorOccursAsync()
+        {
+            // given
+            var randomContainer = GetRandomString();
+            var randomFileName = GetRandomString();
+            var randomData = Encoding.ASCII.GetBytes(GetRandomString());
+            var serviceException = new Exception();
+
+            var failedResolvedAddressOrchestrationServiceException =
+                new FailedResolvedAddressOrchestrationServiceException(
+                    message: "Failed resolved address orchestration service occurred, please contact support.",
+                    innerException: serviceException);
+
+            var expectedResolvedAddressOrchestrationServiveException =
+                new ResolvedAddressOrchestrationServiceException(
+                    message: "Resolved address orchestration service error occurred, contact support.",
+                    innerException: failedResolvedAddressOrchestrationServiceException);
+
+            this.resolvedAddressProcessingServiceMock.Setup(service =>
+                service.RetrieveAllResolvedAddresses())
+                    .Throws(serviceException);
+
+            // when
+            ValueTask<Guid> uploadResolvedAddressTask =
+                 this.resolvedAddressOrchestrationService.UploadResolvedAddressesAsync();
+
+            ResolvedAddressOrchestrationServiceException actualException =
+                await Assert.ThrowsAsync<ResolvedAddressOrchestrationServiceException>(
+                    uploadResolvedAddressTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedResolvedAddressOrchestrationServiveException);
+
+            this.resolvedAddressProcessingServiceMock.Verify(service =>
+                service.RetrieveAllResolvedAddresses(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                 broker.LogError(It.Is(SameExceptionAs(
+                     expectedResolvedAddressOrchestrationServiveException))),
+                         Times.Once);
+
+            this.resolvedAddressProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.documentProcessingServiceMock.VerifyNoOtherCalls();
+            this.csvMapperProcessingServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

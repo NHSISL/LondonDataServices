c46e72a0c8798bc371ee326eb0315f306630570e
David Hayes <david.hayes17@nhs.net>
2024-04-30 14:49:51 +0100

    CODE RUB: Add Logic placeholder

---
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/AddressPersistances/AddressPersistanceOrchestrationTests.Logic.MatchAndPersistResolvedAddressAsync.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LHDS.Core.Models.Foundations.Addresses;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Orchestrations.AddressPersistances
+{
+    public partial class AddressPersistanceOrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldMatchAndPersistResolvedAddressAndLogAsync()
+        {
+            // Given
+           
+
+            // Where
+           
+
+            // Then
+        }
+    }
+}
+

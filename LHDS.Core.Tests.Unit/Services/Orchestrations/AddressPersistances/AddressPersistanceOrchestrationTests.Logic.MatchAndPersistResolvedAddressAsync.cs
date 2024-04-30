b29f052869bf387e3f18748c5c7520749881a141
David Hayes <david.hayes17@nhs.net>
2024-04-30 15:46:33 +0100

    WIP: Pow

--- LHDS.Core.Tests.Unit/Services/Orchestrations/AddressPersistances/AddressPersistanceOrchestrationTests.Logic.MatchAndPersistResolvedAddressAsync.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/AddressPersistances/AddressPersistanceOrchestrationTests.Logic.MatchAndPersistResolvedAddressAsync.cs
@@ -2,12 +2,14 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Force.DeepCloner;
 using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Foundations.ResolvedAddresses;
 using Moq;
 using Xunit;
 
@@ -19,12 +21,32 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.AddressPersistances
         public async Task ShouldMatchAndPersistResolvedAddressAndLogAsync()
         {
             // Given
-           
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+            ResolvedAddress randomResolvedAddress = CreateRandomResolvedAddress(randomDateTimeOffset);
+            ResolvedAddress inputResolvedAddress = randomResolvedAddress;
+            List<Address> randomAddresses = CreateRandomAddressList();
+            List<Address> storageAddresses = randomAddresses;
 
-            // Where
-           
+            this.addressProcessingServiceMock.Setup(processing =>
+                processing.RetrieveAddressByPostCodeAsync(inputResolvedAddress.PostCode))
+                    .ReturnsAsync(storageAddresses);
+
+            this.addressMatcherProcessingServiceMock.Setup(processing =>
+                processing.FindBestMatch())
+                    .ReturnsAsync(inputResolvedAddress);
+
+            // When
+            ResolvedAddress actualResolvedAddress =
+                await this.addressPersistanceOrchestrationService.MatchAndPersistResolvedAddressAsync(inputResolvedAddress);
 
             // Then
+
+
+
+            addressProcessingServiceMock.VerifyNoOtherCalls();
+            addressMatcherProcessingServiceMock.VerifyNoOtherCalls();
+            loggingBrokerMock.VerifyNoOtherCalls();
+            dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }
 }

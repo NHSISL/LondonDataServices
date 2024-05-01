31f406b91100ea7dc803aed16252d84975d11c4e
Christo du Toit <christo@dutoit.co.uk>
2024-05-01 14:29:30 +0100

    BROKERS: CsvHelperBroker

---
+++ LHDS.Core/Brokers/CsvHelpers/CsvHelperBroker.cs
@@ -0,0 +1,39 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CsvHelperClient.Clients;
+
+namespace LHDS.Core.Brokers.CsvHelpers
+{
+    public class CsvHelperBroker : ICsvHelperBroker
+    {
+        private readonly ICsvHelperClient csvHelperClient;
+
+        public CsvHelperBroker(ICsvHelperClient csvHelperClient)
+        {
+            this.csvHelperClient = csvHelperClient;
+        }
+
+        public async ValueTask<List<T>> MapCsvToObjectAsync<T>(
+            string data,
+            bool hasHeaderRecord,
+            Dictionary<string, int>? fieldMappings = null)
+        {
+            return await this.csvHelperClient
+                .MapCsvToObjectAsync<T>(data, hasHeaderRecord, fieldMappings);
+        }
+
+        public async ValueTask<string> MapObjectToCsvAsync<T>(
+            List<T> @object,
+            bool addHeaderRecord,
+            Dictionary<string, int>? fieldMappings = null,
+            bool? shouldAddTrailingComma = false)
+        {
+            return await this.csvHelperClient
+                .MapObjectToCsvAsync(@object, addHeaderRecord, fieldMappings, shouldAddTrailingComma);
+        }
+    }
+}

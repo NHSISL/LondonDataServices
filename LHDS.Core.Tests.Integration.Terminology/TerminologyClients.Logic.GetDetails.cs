50a9a8a075b3d337adfcb2e0d4cdeec0a27c55f0
Christo du Toit <christo@dutoit.co.uk>
2024-05-14 11:07:18 +0100

    CODE RUB: Fixed namespaces

---
+++ LHDS.Core.Tests.Integration.Terminology/TerminologyClients.Logic.GetDetails.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LHDS.Core.Tests.Integration.Terminology
+{
+    public partial class TerminologyClients
+    {
+        [Fact]
+        public async Task ShouldRetrieveArtifactDetailsAsync()
+        {
+            //Given
+
+            //When
+            await terminologyClient.RetrieveArtifactDetailsAsync();
+
+            //Then
+        }
+    }
+}

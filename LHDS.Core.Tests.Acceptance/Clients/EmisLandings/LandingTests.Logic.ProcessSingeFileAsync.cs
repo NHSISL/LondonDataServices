77bb69bffa088954c7781086546f26c8f6076c36
Hassan Raza <h_raza110@outlook.com>
2024-05-28 16:18:05 +0100

    ShouldProcessExistingDocumentAsync -> PASS

--- LHDS.Core.Tests.Acceptance/Clients/EmisLandings/LandingTests.Logic.ProcessSingeFileAsync.cs
+++ LHDS.Core.Tests.Acceptance/Clients/EmisLandings/LandingTests.Logic.ProcessSingeFileAsync.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
+using LHDS.Core.Models.Foundations.DataSets;
 using LHDS.Core.Models.Foundations.DataSetSpecifications;
 using LHDS.Core.Models.Foundations.IngestionTrackings;
 using LHDS.Core.Models.Processings.SubscriberCredentials;
@@ -74,6 +75,10 @@ namespace LHDS.Core.Tests.Acceptance.Clients.EmisLandings
             CleanupDownloadFolder();
             Guid supplierId = landingConfiguration.LandingSupplierId;
             SubscriberCredential randomSubscriberCredential = CreateRandomSubscriberCredential();
+            DataSet randomDataSet = CreateRandomDataSet(supplierId);
+            DataSetSpecification activeDataSetSpecifications = CreateRandomDataSetSpecification(randomDataSet);
+            await this.dataSetService.AddDataSetAsync(randomDataSet);
+            await this.dataSetSpecificationProcessingService.AddDataSetSpecificationAsync(activeDataSetSpecifications);
 
             SubscriberCredential inputSubscriberCredential = await this.subscriberCredentialOrchestration
                 .ModifyOrAddSubscriberCredentialAsync(
@@ -114,6 +119,11 @@ namespace LHDS.Core.Tests.Acceptance.Clients.EmisLandings
             {
                 await this.ingestionTrackingAuditService.RemoveIngestionTrackingAuditByIdAsync(audit.Id);
             }
+
+            await this.dataSetSpecificationProcessingService
+               .RemoveDataSetSpecificationByIdAsync(activeDataSetSpecifications.Id);
+
+            await this.dataSetService.RemoveDataSetByIdAsync(randomDataSet.Id);
 
             await this.subscriberCredentialOrchestration
                 .RemoveSubscriberCredentialByIdAsync(subscriberCredentialId: inputSubscriberCredential.Id);

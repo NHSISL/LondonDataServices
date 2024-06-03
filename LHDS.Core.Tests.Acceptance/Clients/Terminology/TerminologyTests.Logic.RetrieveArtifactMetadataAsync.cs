60aa86545bd1f78f47299e26ec4a1fb635823cd6
Hassan Raza <h_raza110@outlook.com>
2024-06-03 10:17:33 +0100

    ShouldRetrieveArtifactMetadataAsync -> WIP

---
+++ LHDS.Core.Tests.Acceptance/Clients/Terminology/TerminologyTests.Logic.RetrieveArtifactMetadataAsync.cs
@@ -0,0 +1,36 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Foundations.ResolvedAddresses;
+using Xunit;
+
+namespace LHDS.Core.Tests.Acceptance.Clients.Terminology
+{
+    public partial class TerminologyTests
+    {
+        [Theory]
+        [InlineData("CodeSystem")]
+        [InlineData("ValueSet")]
+        [InlineData("ConceptMap")]
+        public async Task ShouldRetrieveArtifactMetadataAsync(string resourceType)
+        {
+            //Given
+            DateTimeOffset randomDateTime = this.dateTimeBroker.GetCurrentDateTimeOffset();
+            string[] resourceTypes = new string[] { resourceType };
+
+            //When
+            await this.terminologyClient.RetrieveArtifactMetadataAsync(resourceTypes);
+
+            //Then
+            await this.addressService.RemoveAddressByIdAsync(address.Id);
+            }
+        }
+    }
+}

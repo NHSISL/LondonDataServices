5c532b927ce49d9bbefaf2082ec90113319865c6
Hassan Raza <h_raza110@outlook.com>
2024-06-03 16:11:01 +0100

    Wiremock -> WIP

--- LHDS.Core.Tests.Acceptance/Clients/Terminology/TerminologyTests.Logic.RetrieveArtifactMetadataAsync.cs
+++ LHDS.Core.Tests.Acceptance/Clients/Terminology/TerminologyTests.Logic.RetrieveArtifactMetadataAsync.cs
@@ -2,10 +2,17 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
+using LHDS.Core.Models.Foundations.Ontologies;
 using LHDS.Core.Models.Foundations.TerminologyPolls;
+using Newtonsoft.Json;
+using Tynamix.ObjectFiller;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
 using Xunit;
 
 namespace LHDS.Core.Tests.Acceptance.Clients.Terminology
@@ -13,27 +20,50 @@ namespace LHDS.Core.Tests.Acceptance.Clients.Terminology
     public partial class TerminologyTests
     {
         [Theory]
-        [InlineData("CodeSystem")]
+        //[InlineData("CodeSystem")]
         [InlineData("ValueSet")]
-        [InlineData("ConceptMap")]
+        //[InlineData("ConceptMap")]
         public async Task ShouldRetrieveArtifactMetadataAsync(string resourceType)
         {
             //Given
+            DateTimeOffset dateTimeOffset = GetRandomDateTimeOffset();
             string[] resourceTypes = new string[] { resourceType };
+
+            OntologyAsset ontologyAsset = new OntologyAsset
+            {
+                FullUrl = GetRandomString(),
+                ResourceType = resourceType,
+                Version = GetRandomString(),
+                Name = GetRandomString(),
+                Title = GetRandomString(),
+                Status = GetRandomString(),
+                LastUpdated = dateTimeOffset
+            };
+
+            string serialisedResponseMessage = JsonConvert.SerializeObject(ontologyAsset);
+
+            this.wireMockServer
+                .Given(
+                    Request.Create()
+                        .UsingGet())
+                .RespondWith(
+                    Response.Create()
+                        .WithSuccess()
+                        .WithBody(serialisedResponseMessage));
 
             //When
             await this.terminologyClient.RetrieveArtifactMetadataAsync(resourceTypes);
 
             //Then
-            IQueryable<TerminologyPoll> retrievedTerminologyPolls =
-                this.terminologyPollService.RetrieveAllTerminologyPolls();
+            //IQueryable<TerminologyPoll> retrievedTerminologyPolls =
+            //    this.terminologyPollService.RetrieveAllTerminologyPolls();
 
-            retrievedTerminologyPolls.Count().Should().BeGreaterThan(0);
+            //retrievedTerminologyPolls.Count().Should().BeGreaterThan(0);
 
-            foreach (TerminologyPoll poll in retrievedTerminologyPolls)
-            {
-                await this.terminologyPollService.RemoveTerminologyPollByIdAsync(poll.Id);
-            }
+            //foreach (TerminologyPoll poll in retrievedTerminologyPolls)
+            //{
+            //    await this.terminologyPollService.RemoveTerminologyPollByIdAsync(poll.Id);
+            //}
         }
     }
 }
--- LHDS.Core.Tests.Acceptance/Clients/Terminology/TerminologyTests.cs
+++ LHDS.Core.Tests.Acceptance/Clients/Terminology/TerminologyTests.cs
@@ -2,14 +2,24 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using KellermanSoftware.CompareNetObjects;
 using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Identifiers;
+using LHDS.Core.Brokers.Loggings;
 using LHDS.Core.Clients;
 using LHDS.Core.Clients.Extensions;
-using LHDS.Core.Services.Foundations.TerminologyPolls;
+using LHDS.Core.Models.Brokers.Ontologies;
+using LHDS.Core.Services.Orchestrations.TerminologyMetadata;
+using LHDS.Core.Services.Processings.Ontologies;
+using LHDS.Core.Services.Processings.TerminologyArtifacts;
+using LHDS.Core.Services.Processings.TerminologyPolls;
 using LHDS.Core.Tests.Acceptance.Brokers.DependencyBrokers;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Tynamix.ObjectFiller;
+using WireMock.Server;
 using Xunit;
 
 namespace LHDS.Core.Tests.Acceptance.Clients.Terminology
@@ -18,30 +28,81 @@ namespace LHDS.Core.Tests.Acceptance.Clients.Terminology
     public partial class TerminologyTests
     {
         private readonly DependencyBroker dependencyBroker;
-        private readonly ITerminologyPollService terminologyPollService;
+        private readonly ITerminologyMetadataOrchestrationService terminologyMetadataOrchestrationService;
+        private readonly ITerminologyArtifactProcessingService terminologyArtifactProcessingService;
+        private readonly ITerminologyPollProcessingService terminologyPollProcessingService;
+        private readonly IOntologyProcessingService ontologyProcessingService;
         private readonly ITerminologyClient terminologyClient;
+        private readonly OntologyConfiguration ontologyConfigurations;
         private readonly ICompareLogic compareLogic;
         private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IIdentifierBroker identifierBroker;
+        private readonly WireMockServer wireMockServer;
 
         public TerminologyTests(DependencyBroker dependencyBroker)
         {
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("local.appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables("LHDS_TERMINOLOGY_CLIENT_ACCEPTANCE_");
+
+            IConfiguration configuration = configurationBuilder.Build();
+            this.wireMockServer = WireMockServer.Start();
+            bool RunAcceptanceTests = configuration.GetSection("RunAcceptanceTests").Get<bool>();
+            bool RunIntegrationTests = configuration.GetSection("RunIntegrationTests").Get<bool>();
+            var terminologyServerBaseUrl = configuration["ontologySettings:terminologyServerBaseUrl"];
+            var terminologyServerAuthenticationRelativeUrl = configuration["ontologySettings:terminologyServerAuthenticationRelativeUrl"];
+            var terminologyServerResourceRelativeUrl = configuration["ontologySettings:terminologyServerResourceRelativeUrl"];
+            var clientId = configuration["ontologySettings:ClientId"];
+            var clientSecret = configuration["ontologySettings:ClientSecret"];
+
             this.dependencyBroker = dependencyBroker;
             this.compareLogic = new CompareLogic();
             var serviceCollection = new ServiceCollection();
 
             serviceCollection
-                .AddTransient<ITerminologyPollService, TerminologyPollService>();
+                .AddTransient<ITerminologyPollProcessingService, TerminologyPollProcessingService>()
+                .AddTransient<ITerminologyMetadataOrchestrationService, TerminologyMetadataOrchestrationService>()
+                .AddTransient<ITerminologyArtifactProcessingService, TerminologyArtifactProcessingService>()
+                .AddTransient<IOntologyProcessingService, OntologyProcessingService>();
 
             serviceCollection.AddLogging(builder =>
             {
                 builder.AddConsole();
             });
 
+            this.ontologyConfigurations = new OntologyConfiguration
+            {
+                TerminologyServerBaseUrl = this.wireMockServer.Url,
+                TerminologyServerAuthenticationRelativeUrl = terminologyServerAuthenticationRelativeUrl,
+                TerminologyServerResourceRelativeUrl = terminologyServerResourceRelativeUrl,
+                ClientId = clientId,
+                ClientSecret = clientSecret,
+            };
+
             serviceCollection.AddTerminologyClient(this.dependencyBroker.Configuration);
             var serviceProvider = serviceCollection.BuildServiceProvider();
-            this.terminologyPollService = serviceProvider.GetService<ITerminologyPollService>();
             this.dateTimeBroker = serviceProvider.GetService<IDateTimeBroker>();
+            this.identifierBroker = serviceProvider.GetService<IIdentifierBroker>();
+            this.loggingBroker = serviceProvider.GetService<ILoggingBroker>();
             terminologyClient = serviceProvider.GetService<ITerminologyClient>();
+
+            this.terminologyMetadataOrchestrationService = new TerminologyMetadataOrchestrationService(
+                ontologyConfiguration: this.ontologyConfigurations,
+                terminologyPollProcessingService: this.terminologyPollProcessingService,
+                ontologyProcessingService: this.ontologyProcessingService,
+                terminologyArtifactProcessingService: this.terminologyArtifactProcessingService,
+                dateTimeBroker: this.dateTimeBroker,
+                loggingBroker: this.loggingBroker,
+                identifierBroker: this.identifierBroker
+                );
         }
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
     }
 }

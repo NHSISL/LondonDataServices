f44a6b4e73ba1cca65812b653d7dffc83af6cb44
Christo du Toit <christo@dutoit.co.uk>
2024-06-04 13:35:58 +0100

    ShouldNormaliseAddressesAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/AddressExtractions/AddressExtractionOrchestrationTests.Logic.NormaliseAddresses.cs
@@ -0,0 +1,90 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Force.DeepCloner;
+using LHDS.Core.Extensions.Addresses;
+using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Foundations.AddressNormalisations;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Orchestrations.AddressExtractions
+{
+    public partial class AddressExtractionOrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldNormaliseAddressesAsync()
+        {
+            // Given
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+            Address randomAddress = CreateRandomAddress();
+            randomAddress.IsErrored = false;
+            randomAddress.IsNormalised = false;
+            List<Address> randomAddresses = new List<Address> { randomAddress };
+
+            AddressNormalisation addressNormalisation = new AddressNormalisation
+            {
+                PostalAddress = GetRandomString(),
+                JsonPostalAddress = GetRandomString()
+            };
+
+            dateTimeBrokerMock
+                .Setup(broker => broker.GetCurrentDateTimeOffset())
+                    .Returns(randomDateTimeOffset);
+
+            addressProcessingServiceMock
+                .SetupSequence(service => service.RetrieveAllAddresses())
+                    .Returns(randomAddresses.AsQueryable())
+                    .Returns(new List<Address>().AsQueryable());
+
+            addressNormalisationProcessingServiceMock
+                .Setup(service => service.GetNormalisedAddress(randomAddress.GetFormattedAddress()))
+                    .ReturnsAsync(addressNormalisation);
+
+            Address modifiedAddress = randomAddress.DeepClone();
+            modifiedAddress.JsonPostalAddress = addressNormalisation.JsonPostalAddress;
+            modifiedAddress.PostalAddress = addressNormalisation.PostalAddress;
+            modifiedAddress.IsErrored = false;
+            modifiedAddress.IsNormalised = true;
+            modifiedAddress.UpdatedDate = randomDateTimeOffset;
+
+            addressProcessingServiceMock
+                .Setup(service => service.ModifyAddressAsync(modifiedAddress))
+                    .ReturnsAsync(modifiedAddress);
+
+            // When
+            await this.addressExtractionOrchestrationService.NormaliseAddresses();
+
+            // Then
+            addressProcessingServiceMock
+                .Verify(service => service.RetrieveAllAddresses(),
+                    Times.Exactly(2));
+
+            addressNormalisationProcessingServiceMock
+                .Verify(service => service.GetNormalisedAddress(randomAddress.GetFormattedAddress()),
+                    Times.Once);
+
+            dateTimeBrokerMock
+                .Verify(broker => broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            addressProcessingServiceMock
+                .Verify(service => service.ModifyAddressAsync(It.Is(SameAddressAs(modifiedAddress))),
+                    Times.Once);
+
+            this.csvHelperBrokerMock.VerifyNoOtherCalls();
+            this.auditBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.addressNormalisationProcessingServiceMock.VerifyNoOtherCalls();
+            this.addressProcessingServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
+

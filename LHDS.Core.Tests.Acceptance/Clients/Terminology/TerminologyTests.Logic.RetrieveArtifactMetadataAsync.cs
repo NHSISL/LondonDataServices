a8b00c27cb7259097cdc4a18788279009a07293c
Hassan Raza <h_raza110@outlook.com>
2024-06-04 13:47:27 +0100

    WIP

--- LHDS.Core.Tests.Acceptance/Clients/Terminology/TerminologyTests.Logic.RetrieveArtifactMetadataAsync.cs
+++ LHDS.Core.Tests.Acceptance/Clients/Terminology/TerminologyTests.Logic.RetrieveArtifactMetadataAsync.cs
@@ -3,73 +3,91 @@
 // ---------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Reflection.PortableExecutable;
 using System.Threading.Tasks;
 using FluentAssertions;
 using LHDS.Core.Models.Brokers.Ontologies;
-using LHDS.Core.Models.Foundations.Ontologies;
+using LHDS.Core.Models.Foundations.TerminologyArtifacts;
 using LHDS.Core.Models.Foundations.TerminologyPolls;
-using Newtonsoft.Json;
-using Org.BouncyCastle.Pqc.Crypto.Lms;
-using Tynamix.ObjectFiller;
 using WireMock.RequestBuilders;
 using WireMock.ResponseBuilders;
 using Xunit;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace LHDS.Core.Tests.Acceptance.Clients.Terminology
 {
     public partial class TerminologyTests
     {
         [Theory]
-        //[InlineData("CodeSystem")]
+        [InlineData("CodeSystem")]
         [InlineData("ValueSet")]
-        //[InlineData("ConceptMap")]
+        [InlineData("ConceptMap")]
         public async Task ShouldRetrieveArtifactMetadataAsync(string resourceType)
         {
             //Given
             DateTimeOffset dateTimeOffset = GetRandomDateTimeOffset();
             string[] resourceTypes = new string[] { resourceType };
+            string relativeUrl = $"/authoring/fhir/{resourceType}";
+            string authUri = $"{ontologyConfiguration.TerminologyServerAuthenticationRelativeUrl}";
 
-            string relativeUrl = $"{ontologyConfiguration.TerminologyServerBaseUrl}" +
-                $"{this.ontologyConfiguration.TerminologyServerResourceRelativeUrl}";
-
-            string authUri = $"{ontologyConfiguration.TerminologyServerBaseUrl}" +
-                $"{ontologyConfiguration.TerminologyServerAuthenticationRelativeUrl}";
-
-            OntologyAsset ontologyAsset = new OntologyAsset
+            OntologyAccessToken token = new OntologyAccessToken
             {
-                FullUrl = GetRandomString(),
-                ResourceType = resourceType,
-                Version = GetRandomString(),
-                Name = GetRandomString(),
-                Title = GetRandomString(),
-                Status = GetRandomString(),
-                LastUpdated = dateTimeOffset
+                AccessToken = GetRandomString(),
+                ExpiresIn = 1800,
+                TokenType = "Bearer",
+                NotBeforePolicy = 0,
+                Scope = ""
             };
-
-            string serialisedResponseMessage = JsonConvert.SerializeObject(ontologyAsset);
-
-            var requestContent = new FormUrlEncodedContent(new[]
-                {
-                    new KeyValuePair<string, string>("grant_type", "client_credentials"),
-                    new KeyValuePair<string, string>("client_id", ontologyConfiguration.ClientId),
-                    new KeyValuePair<string, string>("client_secret", ontologyConfiguration.ClientSecret)
-                });
 
             this.wireMockServer.Given(
                 Request.Create()
-                        .UsingPost()
-                        .WithPath($"{authUri}"))
+                        .WithPath($"{authUri}")
+                        .UsingPost())
                     .RespondWith(
                         Response.Create()
                             .WithStatusCode(HttpStatusCode.OK)
-                            .WithBodyAsJson(GetRandomString()));
-            
+                            .WithBodyAsJson(token));
+
+            string returnedJsonString = $@"{{
+                ""resourceType"": ""Bundle"",
+                ""id"": ""{Guid.NewGuid}"",
+                ""meta"": {{
+                    ""lastUpdated"": ""2024-06-04T10:59:29.125+00:00""
+                }},
+                ""type"": ""searchset"",
+                ""total"": 18,
+                ""link"": [
+                    {{
+                        ""relation"": ""self"",
+                        ""url"": ""https://ontology.onelondon.online/authoring/fhir/ValueSet?_count=1&_elements=name%2Ctitle%2Curl%2Cversion&_lastUpdated=ge2023-06-20T09%3A16%3A18.872%2000%3A00""
+                    }}
+                ],
+                ""entry"": [
+                    {{
+                        ""fullUrl"": ""https://ontology.onelondon.online/authoring/fhir/ValueSet/6d5030f7-5575-4500-beb5-2a72d0c6e10e"",
+                        ""resource"": {{
+                            ""resourceType"": ""{resourceType}"",
+                            ""id"": ""{Guid.NewGuid}"",
+                            ""meta"": {{
+                                ""versionId"": ""1"",
+                                ""lastUpdated"": ""2023-07-01T13:18:41.902+00:00"",
+                                ""tag"": [
+                                    {{
+                                        ""system"": ""http://terminology.hl7.org/CodeSystem/v3-ObservationValue"",
+                                        ""code"": ""SUBSETTED"",
+                                        ""display"": ""Resource encoded in summary mode""
+                                    }}
+                                ]
+                            }},
+                            ""url"": ""https://kch.htndisorderltc"",
+                            ""version"": ""1.0"",
+                            ""name"": ""Kch_htndisorderltc"",
+                            ""title"": ""kch-htndisorderltc""
+                        }}
+                    }}
+                ]
+            }}";
+
             this.wireMockServer.Given(
                 Request.Create()
                         .UsingGet()
@@ -77,21 +95,21 @@ namespace LHDS.Core.Tests.Acceptance.Clients.Terminology
                     .RespondWith(
                         Response.Create()
                             .WithStatusCode(HttpStatusCode.OK)
-                            .WithBodyAsJson(serialisedResponseMessage));
+                            .WithBody(returnedJsonString));
 
             //When
             await this.terminologyClient.RetrieveArtifactMetadataAsync(resourceTypes);
 
             //Then
-            //IQueryable<TerminologyPoll> retrievedTerminologyPolls =
-            //    this.terminologyPollService.RetrieveAllTerminologyPolls();
+            IQueryable<TerminologyPoll> retrievedTerminologyPolls =
+                this.terminologyPollService.RetrieveAllTerminologyPolls();
 
-            //retrievedTerminologyPolls.Count().Should().BeGreaterThan(0);
+            retrievedTerminologyPolls.Count().Should().BeGreaterThan(0);
 
-            //foreach (TerminologyPoll poll in retrievedTerminologyPolls)
-            //{
-            //    await this.terminologyPollService.RemoveTerminologyPollByIdAsync(poll.Id);
-            //}
+            foreach (TerminologyPoll poll in retrievedTerminologyPolls)
+            {
+                await this.terminologyPollService.RemoveTerminologyPollByIdAsync(poll.Id);
+            }
         }
     }
 }

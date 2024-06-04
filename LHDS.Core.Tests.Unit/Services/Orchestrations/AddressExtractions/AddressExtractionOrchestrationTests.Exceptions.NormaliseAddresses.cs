7facbc4f2679401192fd57ed71b5a3428d2663ef
Christo du Toit <christo@dutoit.co.uk>
2024-06-04 13:35:58 +0100

    ShouldNormaliseAddressesAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/AddressExtractions/AddressExtractionOrchestrationTests.Exceptions.NormaliseAddresses.cs
@@ -0,0 +1,153 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Orchestrations.AddressExtractions.Exceptions;
+using Moq;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Orchestrations.AddressExtractions
+{
+    public partial class AddressExtractionOrchestrationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(AddressExtractionOrchestrationDependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationOnNormaliseAddressesIfDependencyValidationOccursAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            var expectedDependencyException =
+                new AddressExtractionOrchestrationDependencyValidationException(
+                    message: "Address extraction orchestration dependency validation error occurred, " +
+                        "fix the errors and try again.",
+                    innerException: dependencyValidationException.InnerException as Xeption);
+
+            this.addressProcessingServiceMock.Setup(service =>
+                service.RetrieveAllAddresses())
+                    .Throws(dependencyValidationException);
+
+            // when
+            ValueTask normaliseTask = this.addressExtractionOrchestrationService.NormaliseAddresses();
+
+            AddressExtractionOrchestrationDependencyValidationException actualException =
+                await Assert.ThrowsAsync<AddressExtractionOrchestrationDependencyValidationException>(
+                    normaliseTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedDependencyException);
+
+            this.addressProcessingServiceMock.Verify(service =>
+                service.RetrieveAllAddresses(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(
+                   expectedDependencyException))),
+                       Times.Once);
+
+            this.addressNormalisationProcessingServiceMock.VerifyNoOtherCalls();
+            this.addressProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.csvHelperBrokerMock.VerifyNoOtherCalls();
+            this.auditBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(AddressExtractionDependencyExceptions))]
+        public async Task ShouldThrowDependencyExceptionOnNormaliseAddressesIfDependencyErrorOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            var expectedDependencyException =
+                new AddressExtractionOrchestrationDependencyException(
+                    message: "Address extraction orchestration dependency error occurred, " +
+                        "fix the errors and try again.",
+                    innerException: dependencyException.InnerException as Xeption);
+
+            this.addressProcessingServiceMock.Setup(service =>
+                service.RetrieveAllAddresses())
+                    .Throws(dependencyException);
+
+            // when
+            ValueTask normaliseTask = this.addressExtractionOrchestrationService.NormaliseAddresses();
+
+            AddressExtractionOrchestrationDependencyException actualException =
+                await Assert.ThrowsAsync<AddressExtractionOrchestrationDependencyException>(
+                    normaliseTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedDependencyException);
+
+            this.addressProcessingServiceMock.Verify(service =>
+                service.RetrieveAllAddresses(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(
+                   expectedDependencyException))),
+                       Times.Once);
+
+            this.addressNormalisationProcessingServiceMock.VerifyNoOtherCalls();
+            this.addressProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.csvHelperBrokerMock.VerifyNoOtherCalls();
+            this.auditBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnNormaliseAddressesIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            var serviceException = new Exception();
+
+            var failedAddressPersistanceOrchestrationServiceException =
+                new FailedAddressExtractionOrchestrationServiceException(
+                    message: "Failed address extraction orchestration service error occurred, please contact support.",
+                    innerException: serviceException);
+
+            var expectedException =
+                new AddressExtractionOrchestrationServiceException(
+                    message: "Address extraction orchestration service error occurred, please contact support.",
+                    innerException: failedAddressPersistanceOrchestrationServiceException);
+
+            this.addressProcessingServiceMock.Setup(service =>
+                service.RetrieveAllAddresses())
+                    .Throws(serviceException);
+
+            // when
+            ValueTask normaliseTask = this.addressExtractionOrchestrationService.NormaliseAddresses();
+
+            AddressExtractionOrchestrationServiceException actualException =
+                await Assert.ThrowsAsync<AddressExtractionOrchestrationServiceException>(
+                    normaliseTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedException);
+
+            this.addressProcessingServiceMock.Verify(service =>
+                service.RetrieveAllAddresses(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(
+                   expectedException))),
+                       Times.Once);
+
+            this.addressNormalisationProcessingServiceMock.VerifyNoOtherCalls();
+            this.addressProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.csvHelperBrokerMock.VerifyNoOtherCalls();
+            this.auditBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

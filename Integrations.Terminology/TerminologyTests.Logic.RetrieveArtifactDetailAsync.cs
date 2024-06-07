bdea06c2a87428ac9454121bac179ce0376ba19b
Hassan Raza <h_raza110@outlook.com>
2024-06-07 13:59:34 +0100

    INTEGRATIONS: Terminology Tests

---
+++ Integrations.Terminology/TerminologyTests.Logic.RetrieveArtifactDetailAsync.cs
@@ -0,0 +1,36 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.TerminologyArtifacts;
+using Xunit;
+
+namespace LHDS.Core.Tests.Integration.Terminology
+{
+    public partial class TerminologyTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveArtifactDetailsAsync()
+        {
+            //Given
+            DateTimeOffset dateTimeOffset = this.dateTimeBroker.GetCurrentDateTimeOffset();
+            TerminologyArtifact terminologyArtifact = CreateRandomTerminologyArtifact(dateTimeOffset);
+            await this.terminologyArtifactService.AddTerminologyArtifactAsync(terminologyArtifact);
+
+            //When
+            await this.terminologyClient.RetrieveArtifactDetailsAsync();
+
+            //Then
+            TerminologyArtifact retrievedTerminologyArtifact =
+                await this.terminologyArtifactService.RetrieveTerminologyArtifactByIdAsync(terminologyArtifact.Id);
+
+            retrievedTerminologyArtifact.IsDownloaded.Should().BeTrue();
+            string fileName = $"{retrievedTerminologyArtifact.ResourceType}/{retrievedTerminologyArtifact.Name}.json";
+            await this.documentService.RemoveDocumentByFileNameAsync(fileName, "terminology");
+            await this.terminologyArtifactService.RemoveTerminologyArtifactByIdAsync(retrievedTerminologyArtifact.Id);
+        }
+    }
+}

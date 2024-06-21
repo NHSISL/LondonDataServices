cc6c755f3ef3ba39dc712d0e0ed5bda95cade15b
Christo du Toit <christo@dutoit.co.uk>
2024-06-21 16:48:24 +0100

    PROVIDERS:  Added support to encrypt / decrypt a stream

--- LHDS.Core/Brokers/Cryptographies/CryptographyBroker.cs
+++ LHDS.Core/Brokers/Cryptographies/CryptographyBroker.cs
@@ -2,6 +2,8 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using LHDS.Core.Models.Processings.SubscriberCredentials;
 using LHDS.Core.Providers.Cryptography;
@@ -17,10 +19,18 @@ namespace LHDS.Core.Brokers.Cryptographies
             this.decryptionAbstractProvider = decryptionAbstractProvider;
         }
 
+        [Obsolete]
         public ValueTask<byte[]> EncryptAsync(byte[] data, SubscriberCredential subscriberCredential) =>
             decryptionAbstractProvider.EncryptAsync(data, subscriberCredential);
 
+        [Obsolete]
         public ValueTask<byte[]> DecryptAsync(byte[] data, SubscriberCredential subscriberCredential) =>
             decryptionAbstractProvider.DecryptAsync(data, subscriberCredential);
+
+        public async ValueTask EncryptAsync(Stream input, Stream output, SubscriberCredential subscriberCredential) =>
+            await decryptionAbstractProvider.EncryptAsync(input, output, subscriberCredential);
+
+        public async ValueTask DecryptAsync(Stream input, Stream output, SubscriberCredential subscriberCredential) =>
+            await decryptionAbstractProvider.DecryptAsync(input, output, subscriberCredential);
     }
 }

7690755a3b5c8a5b9338619409fbfd9874f1cf97
Christo du Toit <christo@dutoit.co.uk>
2024-06-28 16:47:54 +0100

    ShouldProcessRetrieveArtifactDetailsAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Orchestrations/TerminologyDetails/TerminologyDetailOrchestrationTests.Logic.Process.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/TerminologyDetails/TerminologyDetailOrchestrationTests.Logic.Process.cs
@@ -3,10 +3,10 @@
 // ---------------------------------------------------------
 
 using System;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Force.DeepCloner;
-using LHDS.Core.Models.Foundations.Documents;
 using LHDS.Core.Models.Foundations.TerminologyArtifacts;
 using Moq;
 using Xunit;
@@ -19,11 +19,10 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.TerminologyDetails
         public async Task ShouldProcessRetrieveArtifactDetailsAsync()
         {
             // given
+            string inputContainer = blobContainers.Terminology;
             DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
             TerminologyArtifact randomTerminologyArtifacts = CreateRandomUndownloadedTerminologyArtifact();
             TerminologyArtifact undownloadedTerminologyArtifact = randomTerminologyArtifacts;
-            string inputFileName = undownloadedTerminologyArtifact.Id.ToString();
-            string outputFileName = inputFileName;
             string outputArtifactDetail = GetRandomString();
 
             this.terminologyArtifactProcessingServiceMock.SetupSequence(service =>
@@ -36,17 +35,20 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.TerminologyDetails
                     .ReturnsAsync(outputArtifactDetail);
 
             byte[] outputArtifactDetailData = Encoding.UTF8.GetBytes(outputArtifactDetail);
+            Stream inputStream = new MemoryStream(outputArtifactDetailData);
+            Stream expectedStream = inputStream;
+            Stream actualStream = new MemoryStream();
+            string inputFileName = $"{undownloadedTerminologyArtifact.ResourceType}/" +
+                    $"{undownloadedTerminologyArtifact.Name}.json";
 
-            Document artifactDetailDocument = new Document
-            {
-                FileName = $"{undownloadedTerminologyArtifact.ResourceType}/" +
-                    $"{undownloadedTerminologyArtifact.Name}.json",
-                //DocumentData = outputArtifactDetailData
-            };
-
-            //this.documentProcessingServiceMock.Setup(service =>
-            //    service.AddDocumentAsync(artifactDetailDocument, blobContainers.Terminology))
-            //        .ReturnsAsync(outputFileName);
+            this.documentProcessingServiceMock
+                .Setup(service => service.AddDocumentAsync(It.Is(SameStreamAs(inputStream)), inputFileName, inputContainer))
+                .Callback<Stream, string, string>((input, fileName, container) =>
+                {
+                    input.Position = 0;
+                    input.CopyTo(actualStream);
+                })
+                .Returns(ValueTask.CompletedTask);
 
             this.dateTimeBrokerMock.Setup(broker =>
                 broker.GetCurrentDateTimeOffset())
@@ -63,6 +65,8 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.TerminologyDetails
             await this.terminologyDetailOrchestrationService.RetrieveArtifactDetailsAsync();
 
             // then
+            Assert.True(IsSameStream(expectedStream, actualStream));
+
             this.terminologyArtifactProcessingServiceMock.Verify(service =>
                 service.GetNonDownloadedArtifactAsync(),
                     Times.Exactly(2));
@@ -71,9 +75,9 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.TerminologyDetails
                 service.RetrieveArtifactDetailsAsync(undownloadedTerminologyArtifact.FullUrl),
                     Times.Once());
 
-            //this.documentProcessingServiceMock.Verify(service =>
-            //    service.AddDocumentAsync(It.Is(SameDocumentAs(artifactDetailDocument)), blobContainers.Terminology),
-            //        Times.Once);
+            this.documentProcessingServiceMock.Verify(service =>
+                service.AddDocumentAsync(It.IsAny<Stream>(), inputFileName, inputContainer),
+                    Times.Once);
 
             this.dateTimeBrokerMock.Verify(broker =>
                 broker.GetCurrentDateTimeOffset(),

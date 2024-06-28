4ca43d982425bbfbe7b6c0d02dece93a3595fffd
Christo du Toit <christo@dutoit.co.uk>
2024-06-28 17:19:44 +0100

    ShouldAddFileAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Orchestrations/ResolvedAddresses/ResolvedAddressOrchestrationTests.Logic.AddDocument.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/ResolvedAddresses/ResolvedAddressOrchestrationTests.Logic.AddDocument.cs
@@ -2,8 +2,10 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 using Xunit;
 
 namespace LHDS.Core.Tests.Unit.Services.Orchestrations.ResolvedAddresses
@@ -14,19 +16,32 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.ResolvedAddresses
         public async Task ShouldAddFileAsync()
         {
             // Given
-            var randomContainer = GetRandomString();
-            var randomFileName = GetRandomString();
-            var randomfileData = Encoding.UTF8.GetBytes(GetRandomString());
+            var inputContainer = GetRandomString();
+            var inputFileName = GetRandomString();
+            var inputFileData = Encoding.UTF8.GetBytes(GetRandomString());
+            Stream inputStream = new MemoryStream(inputFileData);
+            Stream expectedStream = inputStream;
+            Stream actualStream = new MemoryStream();
+
+            this.documentProcessingServiceMock
+                .Setup(service => service.AddDocumentAsync(It.IsAny<Stream>(), inputFileName, inputContainer))
+                .Callback<Stream, string, string>((input, fileName, container) =>
+                {
+                    input.Position = 0;
+                    input.CopyTo(actualStream);
+                })
+                .Returns(ValueTask.CompletedTask);
 
             // When
             await this.resolvedAddressOrchestrationService
-                .AddDocumentAsync(data: randomfileData, fileName: randomFileName, container: randomContainer);
+                .AddDocumentAsync(data: inputFileData, fileName: inputFileName, container: inputContainer);
 
             // Then
-            //this.documentProcessingServiceMock.Verify(service =>
-            //    service.AddDocumentAsync(It.Is<Document>(doc =>
-            //        doc.FileName == randomFileName && doc.DocumentData == randomfileData), randomContainer),
-            //        Times.Once);
+            Assert.True(IsSameStream(expectedStream, actualStream));
+
+            this.documentProcessingServiceMock.Verify(service =>
+                service.AddDocumentAsync(It.IsAny<Stream>(), inputFileName, inputContainer),
+                    Times.Once);
 
             this.documentProcessingServiceMock.VerifyNoOtherCalls();
             this.resolvedAddressProcessingServiceMock.VerifyNoOtherCalls();

d4bc50f83c04975e983e9f2b07cf4d2d764ec3df
Christo du Toit <christo@dutoit.co.uk>
2024-06-28 16:16:01 +0100

    ShouldThrowValidationExceptionOnRetrieveOptOutIfArgumentsIsIbvalidAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Orchestrations/OptOuts/OptOutOrchestrationTests.Validations.RetrieveOptOutStatus.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/OptOuts/OptOutOrchestrationTests.Validations.RetrieveOptOutStatus.cs
@@ -3,7 +3,6 @@
 // ---------------------------------------------------------
 
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
 using LHDS.Core.Models.Orchestrations.OptOuts.Exceptions;
@@ -14,78 +13,30 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.OptOuts
 {
     public partial class OptOutOrchestrationTests
     {
-
         [Theory]
-        [InlineData("null")]
-        [InlineData("empty")]
-        public async Task ShouldThrowValidationExceptionOnRetrieveOptOutIfOptOutFileIsNullAndLogItAsync(
-            string type)
+        [InlineData(null, "null")]
+        [InlineData("", "empty")]
+        [InlineData(" ", "")]
+        public async Task ShouldThrowValidationExceptionOnRetrieveOptOutIfArgumentsIsIbvalidAndLogItAsync(
+            string invalidText, string streamType)
         {
-            Stream invalidStream = type switch
+            Stream invalidStream = streamType switch
             {
                 "null" => null,
                 _ => new MemoryStream()
             };
 
-            var randomString = GetRandomString();
-
             var invalidArgumentRetieveOptOutStatusOrchestrationException =
                 new InvalidArgumentOptOutOrchestrationException(
                     message: "Invalid Retrieve Opt Out Status orchestration argument(s), " +
                         "please correct the errors and try again.");
 
             invalidArgumentRetieveOptOutStatusOrchestrationException.AddData(
-               key: "OptOutFile",
-               values: "Data is required");
-
-            var expectedRetrieveOptOutStatusOrchestrationOptOutFileValidationException =
-                new OptOutOrchestrationValidationException(
-                    message: "Opt Out orchestration validation errors occurred, please try again.",
-                    innerException: invalidArgumentRetieveOptOutStatusOrchestrationException);
-
-            // when
-            ValueTask<string> RetrieveOptOutStatusTask =
-                this.optOutOrchestrationService.RetrieveOptOutStatusAsync(invalidStream, randomString);
-
-            OptOutOrchestrationValidationException actualException =
-                await Assert.ThrowsAsync<OptOutOrchestrationValidationException>(RetrieveOptOutStatusTask.AsTask);
-
-            // then
-            actualException.Should()
-                .BeEquivalentTo(expectedRetrieveOptOutStatusOrchestrationOptOutFileValidationException);
-
-            this.loggingBrokerMock.Verify(broker =>
-                broker.LogError(It.Is(SameExceptionAs(
-                    expectedRetrieveOptOutStatusOrchestrationOptOutFileValidationException))),
-                        Times.Once);
-
-            this.optOutProcessingServiceMock.VerifyNoOtherCalls();
-            this.csvHelperBrokerMock.VerifyNoOtherCalls();
-            this.meshProcessingServiceMock.VerifyNoOtherCalls();
-            this.documentProcessingServiceMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.identifierBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData(" ")]
-        public async Task ShouldThrowValidationExceptionOnRetrieveOptOutIfRequestIdIsNullAndLogItAsync(
-            string invalidText)
-        {
-            var randomString = GetRandomString();
-            byte[] randomBytes = Encoding.UTF8.GetBytes(randomString);
-            Stream inputStream = new MemoryStream(randomBytes);
-
-            var invalidArgumentRetieveOptOutStatusOrchestrationException =
-                new InvalidArgumentOptOutOrchestrationException(
-                    message: "Invalid Retrieve Opt Out Status orchestration argument(s), " +
-                        "please correct the errors and try again.");
+               key: "Input",
+               values: "Stream is required");
 
             invalidArgumentRetieveOptOutStatusOrchestrationException.AddData(
-               key: "RequestId",
+               key: "FileName",
                values: "Text is required");
 
             var expectedRetrieveOptOutStatusOrchestrationOptOutFileValidationException =
@@ -95,7 +46,7 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.OptOuts
 
             // when
             ValueTask<string> RetrieveOptOutStatusTask =
-                this.optOutOrchestrationService.RetrieveOptOutStatusAsync(inputStream, invalidText);
+                this.optOutOrchestrationService.RetrieveOptOutStatusAsync(invalidStream, invalidText);
 
             OptOutOrchestrationValidationException actualException =
                 await Assert.ThrowsAsync<OptOutOrchestrationValidationException>(RetrieveOptOutStatusTask.AsTask);

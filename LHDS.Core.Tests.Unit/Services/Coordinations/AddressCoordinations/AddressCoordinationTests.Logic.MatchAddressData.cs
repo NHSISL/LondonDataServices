ed6eed996f835bc8242e8c90b4607254f9900329
Christo du Toit <christo@dutoit.co.uk>
2024-07-09 18:35:59 +0100

    ShouldMatchAddressDataAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Coordinations/AddressCoordinations/AddressCoordinationTests.Logic.MatchAddressData.cs
+++ LHDS.Core.Tests.Unit/Services/Coordinations/AddressCoordinations/AddressCoordinationTests.Logic.MatchAddressData.cs
@@ -2,49 +2,31 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
 namespace LHDS.Core.Tests.Unit.Services.Coordinations.AddressCoordinations
 {
     public partial class AddressCoordinationServiceTests
     {
-        //[Fact]
-        //public async Task ShouldMatchAddressDataAsync()
-        //{
-        //    // Given
-        //    string someFilename = GetRandomString();
-        //    byte[] inputData = Encoding.UTF8.GetBytes(GetRandomString());
-        //    List<ResolvedAddress> randomAddresses = CreateRandomResolvedAddresses();
-        //    List<ResolvedAddress> extractedAddresses = randomAddresses.DeepClone();
+        [Fact]
+        public async Task ShouldMatchAddressDataAsync()
+        {
+            // Given
 
-        //    this.addressExtractionOrchestrationServiceMock.Setup(service =>
-        //        service.ProcessResolvedAddressesAsync(inputData, someFilename))
-        //            .ReturnsAsync(extractedAddresses);
+            // When
+            await this.addressCoordinationService.MatchAddressDataAsync();
 
-        //    foreach (var address in extractedAddresses)
-        //    {
-        //        this.addressPersistanceOrchestrationServiceMock.Setup(service =>
-        //            service.MatchAndPersistResolvedAddressAsync(address))
-        //                .ReturnsAsync(address);
-        //    }
+            // Then
+            this.resolvedAddressOrchestrationServiceMock.Verify(service =>
+                service.MatchAddressDataAsync(),
+                    Times.Once());
 
-        //    // When
-        //    await this.addressCoordinationService.MatchAddressDataAsync(inputData, someFilename);
-
-        //    // Then
-        //    this.addressExtractionOrchestrationServiceMock.Verify(service =>
-        //        service.ProcessResolvedAddressesAsync(inputData, someFilename),
-        //            Times.Once());
-
-        //    foreach (var address in extractedAddresses)
-        //    {
-        //        this.addressPersistanceOrchestrationServiceMock.Verify(service =>
-        //            service.MatchAndPersistResolvedAddressAsync(address),
-        //                Times.Once());
-        //    }
-
-        //    this.addressExtractionOrchestrationServiceMock.VerifyNoOtherCalls();
-        //    this.addressPersistanceOrchestrationServiceMock.VerifyNoOtherCalls();
-        //    this.resolvedAddressOrchestrationServiceMock.VerifyNoOtherCalls();
-        //    this.loggingBrokerMock.VerifyNoOtherCalls();
-        //}
+            this.addressOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.resolvedAddressOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
+

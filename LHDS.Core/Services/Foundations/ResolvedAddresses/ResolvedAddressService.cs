852c459dedceea21e032b9a84d87659d8d80376a
Christo du Toit <christo@dutoit.co.uk>
2024-07-10 13:50:54 +0100

    ShouldBulkAddResolvedAddressesAsync -> FAIL

--- LHDS.Core/Services/Foundations/ResolvedAddresses/ResolvedAddressService.cs
+++ LHDS.Core/Services/Foundations/ResolvedAddresses/ResolvedAddressService.cs
@@ -1,7 +1,13 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Identifiers;
 using LHDS.Core.Brokers.Loggings;
 using LHDS.Core.Brokers.Storages.Sql;
 using LHDS.Core.Models.Foundations.ResolvedAddresses;
@@ -11,15 +17,18 @@ namespace LHDS.Core.Services.Foundations.ResolvedAddresses
     public partial class ResolvedAddressService : IResolvedAddressService
     {
         private readonly IStorageBroker storageBroker;
+        private readonly IIdentifierBroker identifierBroker;
         private readonly IDateTimeBroker dateTimeBroker;
         private readonly ILoggingBroker loggingBroker;
 
         public ResolvedAddressService(
             IStorageBroker storageBroker,
+            IIdentifierBroker identifierBroker,
             IDateTimeBroker dateTimeBroker,
             ILoggingBroker loggingBroker)
         {
             this.storageBroker = storageBroker;
+            this.identifierBroker = identifierBroker;
             this.dateTimeBroker = dateTimeBroker;
             this.loggingBroker = loggingBroker;
         }
@@ -31,6 +40,9 @@ namespace LHDS.Core.Services.Foundations.ResolvedAddresses
 
                 return await this.storageBroker.InsertResolvedAddressAsync(resolvedAddress);
             });
+
+        public ValueTask BulkAddResolvedAddressesAsync(List<ResolvedAddress> resolvedAddresses, string fileName) =>
+            throw new NotImplementedException();
 
         public IQueryable<ResolvedAddress> RetrieveAllResolvedAddresses() =>
             TryCatch(() => this.storageBroker.SelectAllResolvedAddresses());

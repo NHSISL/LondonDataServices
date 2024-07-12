9cab061d6ed54b853ecb7676e401cbb8893d9019
Hassan Raza <h_raza110@outlook.com>
2024-07-12 14:31:22 +0100

    ShouldThrowAggregateServiceExceptionOnUpdateIfErrorsInLoopAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Orchestrations/OptOuts/OptOutOrchestrationTests.Exceptions.RetrieveUpdatedOptOutStatus.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/OptOuts/OptOutOrchestrationTests.Exceptions.RetrieveUpdatedOptOutStatus.cs
@@ -202,6 +202,95 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.OptOuts
             this.optOutProcessingServiceMock.VerifyNoOtherCalls();
         }
 
+        [Fact]
+        public async Task v()
+        {
+            // Given
+            List<string> randomMessageIds = GetRandomStrings(1);
+            List<Exception> exceptions = new List<Exception>();
+            var serviceException = new Exception();
+
+            this.meshProcessingServiceMock.SetupSequence(service =>
+                service.RetrieveMessageIdsFromInboxAsync())
+                    .ReturnsAsync(randomMessageIds)
+                    .ReturnsAsync(new List<string>());
+
+            var innerFailedOptOutOrchestrationServiceException =
+                new FailedOptOutOrchestrationServiceException(
+                    message: "Failed opt out orchestration service error occurred, please contact support.",
+                    innerException: serviceException);
+
+            var innerOptOutOrchestrationServiceException =
+                new OptOutOrchestrationServiceException(
+                    message: "Opt Out orchestration service error occurred, please contact support.",
+                    innerException: innerFailedOptOutOrchestrationServiceException);
+
+            foreach (var id in randomMessageIds)
+            {
+                this.meshProcessingServiceMock.Setup(service =>
+                    service.RetrieveMessageByIdAsync(id))
+                        .ThrowsAsync(serviceException);
+
+                exceptions.Add(innerOptOutOrchestrationServiceException);
+            }
+
+            var aggregateException =
+                new AggregateException(
+                    $"Unable to retrieve message for {exceptions.Count} message IDs",
+                    exceptions);
+
+            var failedOptOutOrchestrationServiceException =
+                new FailedOptOutOrchestrationServiceException(
+                    message: "Failed opt out aggregate orchestration service error occurred, " +
+                        "please contact support.",
+                    innerException: aggregateException);
+
+            var expectedOptOutOrchestrationServiceException =
+                new OptOutOrchestrationServiceException(
+                    message: "Opt Out orchestration service error occurred, please contact support.",
+                    innerException: failedOptOutOrchestrationServiceException);
+
+            // When
+            ValueTask<List<MeshMessage>> actualMeshMessages =
+                this.optOutOrchestrationService.RetrieveUpdatedMeshConsentStatusesChangesAsync();
+
+            OptOutOrchestrationServiceException actualOptOutOrchestrationServiceException =
+                await Assert.ThrowsAsync<OptOutOrchestrationServiceException>(async () =>
+                    await actualMeshMessages);
+
+            // Then
+            actualOptOutOrchestrationServiceException.Should()
+                .BeEquivalentTo(expectedOptOutOrchestrationServiceException);
+
+            this.meshProcessingServiceMock.Verify(service =>
+                service.RetrieveMessageIdsFromInboxAsync(),
+                    Times.Once);
+
+            foreach (var id in randomMessageIds)
+            {
+                this.meshProcessingServiceMock.Verify(service =>
+                    service.RetrieveMessageByIdAsync(id),
+                        Times.Once);
+            }
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    innerOptOutOrchestrationServiceException))),
+                        Times.Exactly(randomMessageIds.Count));
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedOptOutOrchestrationServiceException))),
+                        Times.Once);
+
+            this.meshProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.documentProcessingServiceMock.VerifyNoOtherCalls();
+            this.optOutProcessingServiceMock.VerifyNoOtherCalls();
+        }
+
         [Theory]
         [MemberData(nameof(OptOutDependencyValidationExceptions))]
         public async Task ShouldThrowDependencyValidationOnUpdateIfDependencyValidationOccursAndLogItAsync(

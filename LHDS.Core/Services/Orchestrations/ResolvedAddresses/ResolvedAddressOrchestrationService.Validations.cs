858d70bf19b50340862e9a1d9a9c9369463bfb97
Christo du Toit <christo@dutoit.co.uk>
2024-07-15 14:16:23 +0100

    ShouldUploadResolvedAddressAsync -> PASS

--- LHDS.Core/Services/Orchestrations/ResolvedAddresses/ResolvedAddressOrchestrationService.Validations.cs
+++ LHDS.Core/Services/Orchestrations/ResolvedAddresses/ResolvedAddressOrchestrationService.Validations.cs
@@ -3,6 +3,7 @@
 // ---------------------------------------------------------
 
 using System;
+using System.IO;
 using LHDS.Core.Models.Orchestrations.ResolvedAddresses.Exceptions;
 using Xeptions;
 
@@ -15,17 +16,31 @@ namespace LHDS.Core.Services.Orchestrations.ResolvedAddresses
             Validate<InvalidArgumentResolvedAddressOrchestrationException>(
                 message: "Invalid resolved address orchestration argument.  Please correct the errors and try again.",
                 (Rule: IsInvalid(data), Parameter: "data"),
-                (Rule: IsInvalid(fileName), Parameter: "fileName"),
-                (Rule: IsInvalid(container), Parameter: "container"));
+                (Rule: IsInvalid(fileName), Parameter: nameof(fileName)),
+                (Rule: IsInvalid(container), Parameter: nameof(container)));
         }
 
         public void ValidateResolvedAddressArgsOnRemove(string fileName, string container)
         {
             Validate<InvalidArgumentResolvedAddressOrchestrationException>(
                 message: "Invalid resolved address orchestration argument.  Please correct the errors and try again.",
-                (Rule: IsInvalid(fileName), Parameter: "fileName"),
-                (Rule: IsInvalid(container), Parameter: "container"));
+                (Rule: IsInvalid(fileName), Parameter: nameof(fileName)),
+                (Rule: IsInvalid(container), Parameter: nameof(container)));
         }
+
+        public void ValidateOnUploadAddressesToReslve(Stream input, string container)
+        {
+            Validate<InvalidArgumentResolvedAddressOrchestrationException>(
+                message: "Invalid resolved address orchestration argument.  Please correct the errors and try again.",
+                (Rule: IsInvalidInputStream(input), Parameter: nameof(input)),
+                (Rule: IsInvalid(container), Parameter: nameof(container)));
+        }
+
+        private static dynamic IsInvalidInputStream(Stream? stream) => new
+        {
+            Condition = stream is null || stream.Length == 0,
+            Message = "Stream is required"
+        };
 
         private static dynamic IsInvalid(string? text) => new
         {

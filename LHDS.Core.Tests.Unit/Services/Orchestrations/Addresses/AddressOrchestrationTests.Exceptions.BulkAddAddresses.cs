2d0af1801d6ade1112be3b1e6328d06e40719333
Hassan Raza <hassan.raza3@nhs.net>
2024-07-16 09:18:08 +0100

    ShouldThrowAggregateDependencyExceptionOnAddressIfErrorsInLoopAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Orchestrations/Addresses/AddressOrchestrationTests.Exceptions.BulkAddAddresses.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/Addresses/AddressOrchestrationTests.Exceptions.BulkAddAddresses.cs
@@ -69,7 +69,7 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.Addresses
                 };
 
             Guid randomId = Guid.NewGuid();
-            List<Address> randomAddresses = CreateRandomAddresses(count: 1).ToList();//change to random number
+            List<Address> randomAddresses = CreateRandomAddresses(count: GetRandomNumber()).ToList();//change to random number
             List<Address> outputAddresses = randomAddresses.DeepClone();
             string stringRecords = string.Join(Environment.NewLine, filteredRecords);
             bool hasHeaderRecord = false;
@@ -172,6 +172,175 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.Addresses
                 this.loggingBrokerMock.Verify(broker =>
                     broker.LogError(It.Is(SameExceptionAs(
                         addressOrchestrationDependencyValidationLoggingException))),
+                            Times.Once);
+            }
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    actualAddressOrchestrationServiceException))),
+                        Times.Once);
+
+            this.fileBrokerMock.VerifyNoOtherCalls();
+            this.csvHelperBrokerMock.VerifyNoOtherCalls();
+            this.auditBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.addressProcessingServiceMock.VerifyNoOtherCalls();
+            this.assignProcessingServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(AddressDependencyExceptions))]
+        public async Task
+            ShouldThrowAggregateDependencyExceptionOnAddressIfErrorsInLoopAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // Given
+            List<Exception> exceptions = new List<Exception>();
+            string assembly = Assembly.GetExecutingAssembly().Location;
+            string zipFileName = "ShouldProcessZipFileWithZippedCsvAddressesData.zip";
+            string csvFileName = "ShouldProcessZipFileWithOnlyCsvAddressesData.zip";
+
+            string inputFilePath = Path.Combine(
+                Path.GetDirectoryName(assembly),
+                $"Resources/Services/Orchestrations/Addresses/{zipFileName}");
+
+            byte[] inputData = await File.ReadAllBytesAsync(inputFilePath);
+            Stream inputStream = new MemoryStream(inputData);
+
+            string csvFilePath = Path.Combine(
+                Path.GetDirectoryName(assembly),
+                $"Resources/Services/Orchestrations/Addresses/{csvFileName}");
+
+            byte[] csvData = await File.ReadAllBytesAsync(csvFilePath);
+            string stringData = Encoding.UTF8.GetString(csvData);
+            List<string> records = stringData.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None).ToList();
+
+            List<string> filteredRecords = records.Where(record =>
+               record.StartsWith("28,") || record.StartsWith("\"28\",")).ToList();
+
+            Dictionary<string, int> fieldMappings = new Dictionary<string, int>
+                {
+                    { "UPRN", 3 },
+                    { "UPSN", 4 },
+                    { "OrganisationName", 5 },
+                    { "DepartmentName", 6 },
+                    { "SubBuildingName", 7 },
+                    { "BuildingName", 8 },
+                    { "BuildingNumber", 9 },
+                    { "DependentThoroughfare", 10 },
+                    { "Thoroughfare", 11 },
+                    { "DoubleDependentLocality", 12 },
+                    { "DependentLocality", 13 },
+                    { "PostTown", 14 },
+                    { "PostCode", 15 }
+                };
+
+            Guid randomId = Guid.NewGuid();
+            List<Address> randomAddresses = CreateRandomAddresses(count: GetRandomNumber()).ToList();//change to random number
+            List<Address> outputAddresses = randomAddresses.DeepClone();
+            string stringRecords = string.Join(Environment.NewLine, filteredRecords);
+            bool hasHeaderRecord = false;
+            string inputFileName = zipFileName;
+            string randomTempPath = Path.GetTempPath();
+            string ordinanceTempFolder = Path.Combine(randomTempPath, "OrdinanceData");
+
+            string ordinanceTempCsvFile =
+                Path.Combine(ordinanceTempFolder, "ShouldProcessZipFileWithOnlyCsvAddressesData.csv");
+
+            this.fileBrokerMock.Setup(service =>
+                service.GetTempPath())
+                    .ReturnsAsync(randomTempPath);
+
+            this.fileBrokerMock.Setup(service =>
+                service.CheckIfDirectoryExistsAsync(ordinanceTempFolder))
+                    .ReturnsAsync(false);
+
+            List<string> csvFiles = new List<string> { ordinanceTempCsvFile };
+
+            this.fileBrokerMock.Setup(service =>
+                service.GetListOfFilesAsync(ordinanceTempFolder, "*.csv"))
+                    .ReturnsAsync(csvFiles);
+
+            foreach (var csvFile in csvFiles)
+            {
+                this.fileBrokerMock.Setup(service =>
+                    service.ReadFileAsync(csvFile))
+                        .ThrowsAsync(dependencyException);
+
+                var addressOrchestrationDependencyException =
+                    new AddressOrchestrationDependencyException(
+                        message: "Address orchestration dependency validation error occurred, " +
+                            "fix the errors and try again.",
+                        innerException: dependencyException.InnerException as Xeption);
+
+                //addressOrchestrationDependencyValidationException.Data.Add("ExtractionError", csvFile);
+                exceptions.Add(addressOrchestrationDependencyException);
+            }
+
+            var aggregateException =
+                new AggregateException(
+                    message: $"Unable to extract {exceptions.Count} address files. " +
+                        "File has been moved to the error folder.",
+                    exceptions);
+
+            var failedAddressOrchestrationServiceException =
+                new FailedAddressOrchestrationServiceException(
+                    message: "Failed address aggregate orchestration service error occurred, " +
+                        "please contact support.",
+                    innerException: aggregateException);
+
+            var expectedAddressOrchestrationServiceException =
+                new AddressOrchestrationServiceException(
+                    message: "Address orchestration service error occurred, please contact support.",
+                    innerException: failedAddressOrchestrationServiceException);
+
+            // When
+            ValueTask processDataTask = this.addressOrchestrationService
+                .BulkAddAddressesAsync(input: inputStream, fileName: zipFileName);
+
+            AddressOrchestrationServiceException actualAddressOrchestrationServiceException =
+                await Assert.ThrowsAsync<AddressOrchestrationServiceException>(
+                    processDataTask.AsTask);
+
+            // Then
+            actualAddressOrchestrationServiceException.Should()
+                .BeEquivalentTo(expectedAddressOrchestrationServiceException);
+
+            this.fileBrokerMock.Verify(service =>
+                service.GetTempPath(),
+                    Times.Once());
+
+            this.fileBrokerMock.Verify(service =>
+                service.CheckIfDirectoryExistsAsync(ordinanceTempFolder),
+                    Times.Once());
+
+            this.fileBrokerMock.Verify(service =>
+                service.CreateDirectoryAsync(ordinanceTempFolder),
+                    Times.Once);
+
+            this.fileBrokerMock.Verify(service =>
+                service.GetListOfFilesAsync(ordinanceTempFolder, "*.csv"),
+                    Times.Once());
+
+            foreach (var csvFile in csvFiles)
+            {
+                this.fileBrokerMock.Verify(service =>
+                    service.ReadFileAsync(csvFile),
+                        Times.Once());
+
+                var addressOrchestrationDependencyLoggingException =
+                    new AddressOrchestrationDependencyException(
+                        message: "Address orchestration dependency error occurred, " +
+                            "fix the errors and try again.",
+                        innerException: dependencyException.InnerException as Xeption);
+
+                //addressOrchestrationDependencyValidationLoggingException.Data.Add("ExtractionError", csvFile);
+
+                this.loggingBrokerMock.Verify(broker =>
+                    broker.LogError(It.Is(SameExceptionAs(
+                        addressOrchestrationDependencyLoggingException))),
                             Times.Once);
             }
 

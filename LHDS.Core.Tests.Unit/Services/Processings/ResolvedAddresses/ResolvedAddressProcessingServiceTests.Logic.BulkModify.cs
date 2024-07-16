5ff7130eaf048df611cf8344330b107ab53f7874
Christo du Toit <christo@dutoit.co.uk>
2024-07-16 09:34:14 +0100

    ShouldBulkModifyResolvedAddressesAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Processings/ResolvedAddresses/ResolvedAddressProcessingServiceTests.Logic.BulkModify.cs
@@ -0,0 +1,36 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.ResolvedAddresses;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.ResolvedAddresses
+{
+    public partial class ResolvedAddressProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldBulkModifyResolvedAddressesAsync()
+        {
+            // Given
+            List<ResolvedAddress> randomResolvedAddresses = new List<ResolvedAddress> { CreateRandomResolvedAddress() };
+            List<ResolvedAddress> inputResolvedAddresses = randomResolvedAddresses;
+
+            // When
+            await this.resolvedAddressProcessingService
+                .BulkModifyResolvedAddressesAsync(resolvedAddresses: inputResolvedAddresses);
+
+            // Then
+
+            this.resolvedAddressServiceMock.Verify(service =>
+                service.BulkModifyResolvedAddressesAsync(inputResolvedAddresses),
+                    Times.Once);
+
+            this.resolvedAddressServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

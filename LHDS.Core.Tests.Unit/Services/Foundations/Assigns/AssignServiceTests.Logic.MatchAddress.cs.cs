93039564624f6fa890db50400738083afb58ceda
Christo du Toit <christo@dutoit.co.uk>
2024-07-17 10:27:57 +0100

    ShouldMatchAddressAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/Assigns/AssignServiceTests.Logic.MatchAddress.cs.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.AssignAddresses;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Assigns
+{
+    public partial class AssignServiceTests
+    {
+        [Fact]
+        public async Task ShouldMatchAddressAsync()
+        {
+            // Given
+            string randomAddress = GetRandomString();
+            string inputAddress = randomAddress;
+            AssignAddress randomAssignAddress = CreateRandomAssignAddress();
+            AssignAddress storageAssignAddress = randomAssignAddress;
+            AssignAddress expectedAssignAddress = storageAssignAddress;
+
+            this.assignBrokerMock.Setup(service =>
+                service.MatchAddressAsync(inputAddress))
+                    .ReturnsAsync(storageAssignAddress);
+
+            // When
+            AssignAddress actualAssignAddress = await this.assignService
+                .MatchAddressAsync(inputAddress);
+
+            // Then
+            actualAssignAddress.Should().BeEquivalentTo(expectedAssignAddress);
+
+            this.assignBrokerMock.Verify(service =>
+                service.MatchAddressAsync(inputAddress),
+                    Times.Once);
+
+            this.assignBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+
+        }
+    }
+}
---
+++ LHDS.Core.Tests.Unit/Services/Foundations/Assigns/AssignServiceTests.cs.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Linq.Expressions;
+using LHDS.Core.Brokers.Assigns;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Models.Foundations.AssignAddresses;
+using LHDS.Core.Services.Foundations.Assigns;
+using Moq;
+using Tynamix.ObjectFiller;
+using Xeptions;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Assigns
+{
+    public partial class AssignServiceTests
+    {
+        private readonly Mock<IAssignBroker> assignBrokerMock = new Mock<IAssignBroker>();
+        private readonly Mock<ILoggingBroker> loggingBrokerMock = new Mock<ILoggingBroker>();
+        private readonly IAssignService assignService;
+
+        public AssignServiceTests()
+        {
+            this.assignBrokerMock = new Mock<IAssignBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.assignService = new AssignService(
+                assignBroker: this.assignBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+
+        private static string GetRandomString(int length) =>
+            new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
+        private static AssignAddress CreateRandomAssignAddress() =>
+            CreateAssignAddressFiller().Create();
+
+        private static Filler<AssignAddress> CreateAssignAddressFiller()
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.UtcNow;
+            var filler = new Filler<AssignAddress>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset);
+
+            return filler;
+        }
+    }
+}

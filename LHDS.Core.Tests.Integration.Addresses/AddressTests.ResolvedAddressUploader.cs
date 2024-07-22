7a02a134bac3a5e8dd31e94303994b1aeaa16e1a
Hassan Raza <h_raza110@outlook.com>
2024-07-22 12:35:22 +0100

    ShouldLoadAddressesToResolveAsync -> WIP

--- LHDS.Core.Tests.Integration.Addresses/AddressTests.ResolvedAddressUploader.cs
+++ LHDS.Core.Tests.Integration.Addresses/AddressTests.ResolvedAddressUploader.cs
@@ -3,8 +3,11 @@
 // ---------------------------------------------------------
 
 using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
-using LHDS.Core.Models.Foundations.Documents;
+using LHDS.Core.Models.Foundations.ResolvedAddresses;
 using Xunit;
 
 namespace LHDS.Core.Tests.Integration.Addresses
@@ -12,33 +15,27 @@ namespace LHDS.Core.Tests.Integration.Addresses
     public partial class AddressTests
     {
         [Fact]
-        public async Task ProcessResolvedAddressDataAsync()
+        public async Task ShouldLoadAddressesToResolveAsync()
         {
             // Given
-            string addressContainer = this.blobContainers.Addresses;
+            string inputFilename = "ShouldUploadAddressesSetup";
+            string assembly = Assembly.GetExecutingAssembly().Location;
+            string projectRoot = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(assembly), @"..\..\.."));
+            Guid expectedUniqueRef = Guid.Parse("7b41335a-f2cf-4949-8b83-c5b210446631");
+
+            string inputFilePath = Path.Combine(
+                projectRoot,
+                @"Resource/Clients/Address/ShouldUploadAddressesSetup.csv");
+
+            byte[] inputData = await File.ReadAllBytesAsync(inputFilePath);
+            Stream inputStream = new MemoryStream(inputData);
 
             // When
-            Guid? returnedBatchGuid = await addressClient.ProcessResolvedAddressDataAsync();
+            await this.addressClient.LoadAddressesToResolveAsync(inputStream, inputFilename);
 
             // Then
-            Assert.True(returnedBatchGuid == null || returnedBatchGuid != Guid.Empty,
-               "The returned GUID should be either null or a valid GUID.");
-
-            if (returnedBatchGuid != null)
-            {
-                string fileName = $"{returnedBatchGuid.ToString()}.csv";
-
-                Document uploadedDocument =
-                    await this.documentService.RetrieveDocumentByFileNameAsync(fileName, addressContainer);
-
-                Assert.NotNull(uploadedDocument);
-                await this.documentService.RemoveDocumentByFileNameAsync(fileName, addressContainer);
-
-                Document uploadedDocumentDeleteCheck =
-                   await this.documentService.RetrieveDocumentByFileNameAsync(fileName, addressContainer);
-
-                Assert.Null(uploadedDocumentDeleteCheck);
-            }
+            ResolvedAddress retrievedAddress = this.resolvedAddressService.RetrieveAllResolvedAddresses().
+                Where(resolvedAddress => resolvedAddress.UniqueReference == expectedUniqueRef).FirstOrDefault();
         }
     }
 }

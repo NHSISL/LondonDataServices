159ceab7034e47f2627d6eab7a3e4eb2f92a3e0b
Christo du Toit <christo@dutoit.co.uk>
2024-07-25 12:06:54 +0100

    BROKERS: Addign - MatchAddressAsync

---
+++ LHDS.Core/Models/Brokers/Assigns/AssignConfiguration.cs
@@ -0,0 +1,11 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+namespace LHDS.Core.Models.Brokers.Assigns
+{
+    public class AssignConfiguration
+    {
+        public string ApiUrl { get; set; }
+    }
+}

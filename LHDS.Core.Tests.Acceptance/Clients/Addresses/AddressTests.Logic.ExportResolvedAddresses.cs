7f642f44f6d45a87b50eb1ca897ea8eafc4cbaff
Hassan Raza <h_raza110@outlook.com>
2024-08-01 11:06:07 +0100

    ShouldExportResolvedAddressesAsync -> WIP

--- LHDS.Core.Tests.Acceptance/Clients/Addresses/AddressTests.Logic.ExportResolvedAddresses.cs
+++ LHDS.Core.Tests.Acceptance/Clients/Addresses/AddressTests.Logic.ExportResolvedAddresses.cs
@@ -26,43 +26,38 @@ namespace LHDS.Core.Tests.Acceptance.Clients.Addresses
             List<ResolvedAddress> randomResolvedAddresses = CreateRandomUnmatchedAddresses(count: 1);
             List<ResolvedAddress> expectedResolvedAddresses = new List<ResolvedAddress>();
             List<Address> addedAddresses = new List<Address>();
-            Guid batchReferenceId = Guid.NewGuid();
+            Guid? batchReference = Guid.NewGuid();
 
             foreach (ResolvedAddress resolvedAddress in randomResolvedAddresses)
             {
                 await this.resolvedAddressService.AddResolvedAddressAsync(resolvedAddress);
             }
 
-            this.identifierBrokerMock.Setup(broker =>
-                broker.GetIdentifier())
-                    .Returns(batchReferenceId);
-
             //When
             await this.addressClient.ExportResolvedAddressesAsync();
 
             //Then
-            this.identifierBrokerMock.Verify(broker =>
-                broker.GetIdentifier(),
-                    Times.Once());
-
             foreach (ResolvedAddress resolvedAddress in randomResolvedAddresses)
             {
                 ResolvedAddress retrievedResolvedAddress =
                     await this.resolvedAddressService.RetrieveResolvedAddressByIdAsync(resolvedAddress.Id);
 
-                retrievedResolvedAddress.BatchReference.Should().Be(batchReferenceId);
                 retrievedResolvedAddress.IsProcessing.Should().Be(false);
                 retrievedResolvedAddress.RetryCount.Should().Be(0);
                 retrievedResolvedAddress.IsExported.Should().Be(true);
                 retrievedResolvedAddress.IsProcessed.Should().Be(true);
 
                 await this.resolvedAddressService.RemoveResolvedAddressByIdAsync(resolvedAddress.Id);
+                batchReference = retrievedResolvedAddress.BatchReference;
             }
 
             Stream retrievedDocumentStream = new MemoryStream();
-            string csvFileName = $"out/{batchReferenceId}.csv";
-            await this.documentService.RetrieveDocumentByFileNameAsync(retrievedDocumentStream, csvFileName, "address");
-            await this.documentService.RemoveDocumentByFileNameAsync(csvFileName, "address");
+            string csvFileName = $"out/{batchReference}.csv";
+
+            await this.documentService.RetrieveDocumentByFileNameAsync(
+                retrievedDocumentStream, csvFileName, blobContainers.Addresses);
+
+            await this.documentService.RemoveDocumentByFileNameAsync(csvFileName, blobContainers.Addresses);
 
             this.identifierBrokerMock.VerifyNoOtherCalls();
         }

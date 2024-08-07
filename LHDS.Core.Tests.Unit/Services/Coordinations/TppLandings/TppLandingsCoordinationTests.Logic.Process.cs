a283596d6abbbbd90eb17fdffe3b05f7a7b093ef
Christo du Toit <christo@dutoit.co.uk>
2024-08-07 10:59:24 +0100

    ShouldProcessAndLogAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Coordinations/TppLandings/TppLandingsCoordinationTests.Logic.Process.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Coordinations.Decryptions
+{
+    public partial class TppLandingsCoordinationTests
+    {
+        [Fact]
+        public async Task ShouldProcessAndLogAsync()
+        {
+            // given
+            Stream randomData = new MemoryStream(Encoding.UTF8.GetBytes(GetRandomString()));
+            Stream inputData = randomData;
+            string inputFileName = GetRandomString();
+            Guid inputSupplierId = Guid.NewGuid();
+            Guid ingestionTrackingId = Guid.NewGuid();
+            Guid expectedIngestionTrackingId = ingestionTrackingId;
+
+            this.tppLandingOrchestrationServiceMock.Setup(service =>
+                service.ProcessAsync(inputData, inputFileName, inputSupplierId))
+                    .ReturnsAsync(ingestionTrackingId);
+
+            // when
+            ValueTask<Guid> actualIngestionTrackingId = this.tppLandingCoordinationService.ProcessAsync(
+                input: inputData,
+                fileName: inputFileName,
+                supplierId: inputSupplierId);
+
+            // then
+            actualIngestionTrackingId.Should().BeEquivalentTo(expectedIngestionTrackingId);
+
+            this.tppLandingOrchestrationServiceMock.Setup(service =>
+                service.ProcessAsync(inputData, inputFileName, inputSupplierId))
+                    .ReturnsAsync(ingestionTrackingId);
+
+            this.ingressOrchestrationServiceMock.Verify(service =>
+                service.CheckForTPPBatchCompleteAsync(inputFileName),
+                    Times.Once);
+
+            this.tppLandingOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.ingressOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LHDS.Core.Tests.Unit/Services/Coordinations/TppLandings/TppLandingsCoordinationTests.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Linq.Expressions;
+using KellermanSoftware.CompareNetObjects;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Extensions.Exceptions;
+using LHDS.Core.Services.Coordinations.TppLandings;
+using LHDS.Core.Services.Orchestrations.Ingress;
+using LHDS.Core.Services.Orchestrations.TppLandings;
+using Moq;
+using Tynamix.ObjectFiller;
+using Xeptions;
+
+namespace LHDS.Core.Tests.Unit.Services.Coordinations.Decryptions
+{
+    public partial class TppLandingsCoordinationTests
+    {
+        private readonly Mock<ITppLandingOrchestrationService> tppLandingOrchestrationServiceMock;
+        private readonly Mock<IIngressOrchestrationService> ingressOrchestrationServiceMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ICompareLogic compareLogic;
+        private readonly ITppLandingCoordinationService tppLandingCoordinationService;
+
+        public TppLandingsCoordinationTests()
+        {
+            this.tppLandingOrchestrationServiceMock = new Mock<ITppLandingOrchestrationService>();
+            this.ingressOrchestrationServiceMock = new Mock<IIngressOrchestrationService>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.compareLogic = new CompareLogic();
+
+            this.tppLandingCoordinationService = new TppLandingCoordinationService(
+                tppOrchestrationService: tppLandingOrchestrationServiceMock.Object,
+                ingressOrchestrationService: ingressOrchestrationServiceMock.Object,
+                loggingBroker: loggingBrokerMock.Object);
+        }
+
+        private static string GetRandomString() =>
+           new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+          new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+         actualException => actualException.SameExceptionAs(expectedException);
+
+        private static Expression<Func<Xeption, bool>> IsSameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.IsSameExceptionAs(expectedException);
+    }
+}

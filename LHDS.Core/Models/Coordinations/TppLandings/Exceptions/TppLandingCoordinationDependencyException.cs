bee0625058f6686f386d8ebf97385e49291f45e2
Christo du Toit <christo@dutoit.co.uk>
2024-08-07 11:41:36 +0100

    ShouldThrowValidationExceptionOnProcessIfArgumentsInvalidAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Coordinations/TppLandings/Exceptions/TppLandingCoordinationDependencyException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Coordinations.TppLandings.Exceptions
+{
+    public class TppLandingCoordinationDependencyException : Xeption
+    {
+        public TppLandingCoordinationDependencyException(string message, Xeption? innerException)
+            : base(message, innerException)
+        { }
+    }
+}

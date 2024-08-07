342be3cd365b1ed98cc02bf5b77dc51314da79f5
Christo du Toit <christo@dutoit.co.uk>
2024-08-07 10:59:24 +0100

    ShouldProcessAndLogAsync -> FAIL

---
+++ LHDS.Core/Services/Coordinations/TppLandings/TppLandingCoordinationService.Exceptions.cs
@@ -0,0 +1,10 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+namespace LHDS.Core.Services.Coordinations.TppLandings
+{
+    public partial class TppLandingCoordinationService
+    {
+    }
+}

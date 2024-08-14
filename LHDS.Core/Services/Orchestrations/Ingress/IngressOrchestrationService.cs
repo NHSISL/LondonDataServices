eb53429ef0387ace8288a819eab400b536e32db9
Christo du Toit <christo@dutoit.co.uk>
2024-08-14 16:27:34 +0100

    ShouldCheckForBatchCompleteAndCreateBatchReadyFileAsync -> PASS

--- LHDS.Core/Services/Orchestrations/Ingress/IngressOrchestrationService.cs
+++ LHDS.Core/Services/Orchestrations/Ingress/IngressOrchestrationService.cs
@@ -3,9 +3,12 @@
 // ---------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.Audits;
 using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Models.Foundations.IngestionTrackings;
 using LHDS.Core.Services.Processings.Documents;
 using LHDS.Core.Services.Processings.IngestionTrackings;
 using LHDS.Core.Services.Processings.SpecificationObjects;
@@ -29,11 +32,51 @@ namespace LHDS.Core.Services.Orchestrations.Ingress
         {
             this.ingestionTrackingProcessingService = ingestionTrackingProcessingService;
             this.specificationObjectProcessingService = specificationObjectProcessingService;
+            this.documentProcessingService = documentProcessingService;
             this.loggingBroker = loggingBroker;
             this.auditBroker = auditBroker;
         }
 
-        public ValueTask CheckForBatchCompleteAsync(Guid ingestionTrackingId) =>
-            ValueTask.CompletedTask;
+        public async ValueTask CheckForBatchCompleteAsync(Guid ingestionTrackingId)
+        {
+            IngestionTracking ingestionTracking =
+                await this.ingestionTrackingProcessingService
+                    .RetrieveIngestionTrackingByIdAsync(ingestionTrackingId);
+
+            List<string> specificationObjectIds = await this.specificationObjectProcessingService
+                .RetrieveSpecificationObjectsByDataSetSpecificationId(ingestionTracking.DataSetSpecificationId);
+
+            List<string> ingestiontrackingObject = await this.ingestionTrackingProcessingService
+                .RetrieveObjectsInBatchByBatchReference(ingestionTracking.Batch);
+
+            bool isBatchComplete = true;
+
+            foreach (string specificationObjectId in specificationObjectIds)
+            {
+                if (!ingestiontrackingObject.Contains(specificationObjectId))
+                {
+                    isBatchComplete = false;
+                    break;
+                }
+            }
+
+            if (isBatchComplete)
+            {
+                string batchCompleteFileName =
+                    $"{Path.GetDirectoryName(ingestionTracking.DecryptedFileName)}/BatchReady.txt"
+                    .Replace("\\", "/");
+
+                string batchComplete =
+                    $"All specification object files present for dataset specification id: " +
+                    $"{ingestionTracking.DataSetSpecificationId}";
+
+                Stream data = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(batchComplete));
+
+                await this.documentProcessingService.AddDocumentAsync(
+                    input: data,
+                    fileName: batchCompleteFileName,
+                    container: ingestionTracking.Container);
+            }
+        }
     }
 }

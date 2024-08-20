88b23892cdebcb801d234eaeba642503b7bb955e
davidhayes03 <davidhayes03@googlemail.com>
2024-08-20 12:24:43 +0100

    INTEGRATION: Add Integratioin Test

---
+++ LHDS.Core.Tests.Integration.Terminology/TerminologyClients.Logic.GetUserDetails.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace LHDS.Core.Tests.Integration.Terminology
+{
+    public partial class TerminologyClients
+    {
+        [ReleaseCandidateFact]
+        public async Task ShouldRetrieveUserArtifactDetailsAsync()
+        {
+            //Given
+
+            //When
+            await terminologyClient.RetrieveUserArtifactDetailsAsync();
+
+            //Then
+        }
+    }
+}

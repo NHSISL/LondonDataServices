de4be3a587d93fcf6551fb2b31d982689e6caec2
davidhayes03 <davidhayes03@googlemail.com>
2024-08-20 10:33:17 +0100

    ShouldGetNonDownloadedUserTerminologyArtifact -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/TerminologyArtifacts/TerminologyArtifactProcessingServiceTests.Logic.GetUserUndownloaded.cs
@@ -0,0 +1,48 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.TerminologyArtifacts;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.TerminologyArtifacts
+{
+    public partial class TerminologyArtifactProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldGetNonDownloadedUserTerminologyArtifact()
+        {
+            // given
+            List<TerminologyArtifact> randomTerminologyArtifacts = CreateRandomTerminologyArtifacts();
+            List<TerminologyArtifact> artifactsList = randomTerminologyArtifacts.ToList();
+            artifactsList.Last().IsForUser = true;
+            artifactsList.Last().IsDownloadedForUser = false;
+            artifactsList.Last().IsError = false;
+            TerminologyArtifact expectedTerminologyArtifact = artifactsList.Last();
+            List<TerminologyArtifact> outputTerminologyArtifacts = artifactsList;
+
+            this.terminologyArtifactServiceMock.Setup(service =>
+                service.RetrieveAllTerminologyArtifacts())
+                    .Returns(outputTerminologyArtifacts.AsQueryable());
+
+            // when
+            TerminologyArtifact? actualTerminologyArtifact =
+                await this.terminologyArtifactProcessingService.GetNonDownloadedUserArtifactAsync();
+
+            // then
+            actualTerminologyArtifact.Should().BeEquivalentTo(expectedTerminologyArtifact);
+
+            this.terminologyArtifactServiceMock.Verify(service =>
+                service.RetrieveAllTerminologyArtifacts(),
+                    Times.Once());
+
+            this.terminologyArtifactServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

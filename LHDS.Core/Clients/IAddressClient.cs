067ba9077731afce6e23d99d7348072ccbe5f877
Hassan Raza <h_raza110@outlook.com>
2024-09-12 10:36:49 +0100

    CODE RUB: Removing Sync WIth Assign

--- LHDS.Core/Clients/IAddressClient.cs
+++ LHDS.Core/Clients/IAddressClient.cs
@@ -12,7 +12,6 @@ namespace LHDS.Core.Clients
     public interface IAddressClient
     {
         public ValueTask LoadAddressDataAsync(Stream data, string filename);
-        public ValueTask SyncAddressesWithAssign();
         public ValueTask LoadAddressesToResolveAsync(Stream data, string filename);
         public ValueTask MatchAddressDataAsync();
         public ValueTask<List<Guid>> ExportResolvedAddressesAsync();

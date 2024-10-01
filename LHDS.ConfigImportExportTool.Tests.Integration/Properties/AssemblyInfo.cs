dd67ba570784cbe701947713a49e12f4db903864
Hassan Raza <h_raza110@outlook.com>
2024-10-01 16:38:21 +0100

    Adding Integration and Acceptance test project

---
+++ LHDS.ConfigImportExportTool.Tests.Integration/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LHDS.ConfigImportExportTool.Tests.Integration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NEL ICB")]
+[assembly: AssemblyProduct("LHDS.ConfigImportExportTool.Tests.Integration")]
+[assembly: AssemblyCopyright("Copyright © NEL ICB 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("08017380-62ba-4f7f-ac37-e17d558adebb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

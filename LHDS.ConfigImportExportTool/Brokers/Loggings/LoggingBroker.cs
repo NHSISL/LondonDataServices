ad248e01959f6627d8faba1206e3a76af6a4db63
Christo du Toit <christo@dutoit.co.uk>
2024-10-01 16:50:22 +0100

    CODE RUB: code cleanup

--- LHDS.ConfigImportExportTool/Brokers/Loggings/LoggingBroker.cs
+++ LHDS.ConfigImportExportTool/Brokers/Loggings/LoggingBroker.cs
@@ -16,38 +16,20 @@ namespace LHDS.ConfigImportExportTool.Brokers.Loggings
         public LoggingBroker(ILogger<LoggingBroker> logger) =>
             this.logger = logger;
 
-        public void LogInformation(string message) =>
-            logger.LogInformation(message);
-
         public async ValueTask LogInformationAsync(string message) =>
             logger.LogInformation(message);
-
-        public void LogTrace(string message) =>
-            logger.LogTrace(message);
 
         public async ValueTask LogTraceAsync(string message) =>
             logger.LogTrace(message);
 
-        public void LogDebug(string message) =>
-            logger.LogDebug(message);
-
         public async ValueTask LogDebugAsync(string message) =>
             logger.LogDebug(message);
-
-        public void LogWarning(string message) =>
-            logger.LogWarning(message);
 
         public async ValueTask LogWarningAsync(string message) =>
             logger.LogWarning(message);
 
-        public void LogError(Exception exception) =>
-            logger.LogError(exception, $"{exception.Message} {exception.GetValidationSummary()}");
-
         public async ValueTask LogErrorAsync(Exception exception) =>
             logger.LogError(exception, $"{exception.Message} {exception.GetValidationSummary()}");
-
-        public void LogCritical(Exception exception) =>
-            logger.LogCritical(exception, $"{exception.Message} {exception.GetValidationSummary()}");
 
         public async ValueTask LogCriticalAsync(Exception exception) =>
             logger.LogCritical(exception, $"{exception.Message} {exception.GetValidationSummary()}");

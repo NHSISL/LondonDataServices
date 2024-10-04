aca9bae0623a97596d3ea614d42514336d26e70e
Christo du Toit <christo@dutoit.co.uk>
2024-10-04 11:51:37 +0100

    CODE RUB: Code cleanup

--- LHDS.Core.Tests.Acceptance/Clients/TppLandings/TppLandingTests.Logic.ProcessAsync.cs
+++ LHDS.Core.Tests.Acceptance/Clients/TppLandings/TppLandingTests.Logic.ProcessAsync.cs
@@ -21,7 +21,7 @@ namespace LHDS.Core.Tests.Acceptance.Clients.TppLandings
 {
     public partial class TppLandingTests
     {
-        [Fact(Skip = "Hassan to fix. make sure there is no missing awaits")]
+        [Fact]
         public async Task ShouldProcessNewDocumentAndAddHashAsync()
         {
             //Given
@@ -74,7 +74,7 @@ namespace LHDS.Core.Tests.Acceptance.Clients.TppLandings
                 blobContainers.Ingress);
         }
 
-        [Fact(Skip = "Hassan to fix. make sure there is no missing awaits")]
+        [Fact]
         public async Task ShouldNotProcessExistingDocumentAsync()
         {
             //Given
@@ -136,7 +136,7 @@ namespace LHDS.Core.Tests.Acceptance.Clients.TppLandings
                 blobContainers.Ingress);
         }
 
-        [Fact(Skip = "Hassan to fix. make sure there is no missing awaits")]
+        [Fact]
         public async Task ShouldProcessExistingDocumentAndUpdateHashAsync()
         {
             //Given

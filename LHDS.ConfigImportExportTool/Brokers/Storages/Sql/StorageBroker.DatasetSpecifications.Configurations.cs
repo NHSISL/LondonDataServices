a16299861994494538fcf2e3a656afd376cc65f7
Christo du Toit <christo@dutoit.co.uk>
2024-10-04 10:49:37 +0100

    IMPORTS:  Imported Storage Broker and updated async operations

---
+++ LHDS.ConfigImportExportTool/Brokers/Storages/Sql/StorageBroker.DatasetSpecifications.Configurations.cs
@@ -0,0 +1,126 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LHDS.ConfigImportExportTool.Models.Foundations.DatasetSpecifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace LHDS.ConfigImportExportTool.Brokers.Storages.Sql
+{
+    public partial class StorageBroker
+    {
+        private static void AddDataSetSpecificationConfigurations(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DataSetSpecification>()
+                .ToTable("DataSetSpecifications", "Configuration");
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .ToTable(dataSet => dataSet.IsTemporal());
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.Id)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.DataSetId)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.SupplierSpecificationVersion)
+                .HasMaxLength(10)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .HasIndex(dataSet => new { dataSet.DataSetId, dataSet.SupplierSpecificationVersion })
+                .IsUnique();
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.OurSpecificationVersion)
+                .HasMaxLength(10)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .HasIndex(dataSet => new { dataSet.DataSetId, dataSet.OurSpecificationVersion })
+                .IsUnique();
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.IsMultiAuthorPerBatch)
+                .HasDefaultValue(false)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.DateReleased)
+                .IsRequired(false);
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.DateImplemented)
+                .IsRequired(false);
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.DateSuperseded)
+                .IsRequired(false);
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.SupersededById)
+                .IsRequired(false);
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.PresededById)
+                .IsRequired(false);
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.IsPublished)
+                .HasDefaultValue(false)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.IsActive)
+                .HasDefaultValue(false)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.CreatedDate)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.CreatedBy)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.UpdatedBy)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .Property(dataSetSpecification => dataSetSpecification.UpdatedDate)
+                .IsRequired();
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .HasOne(columnDefinition => columnDefinition.DataSet)
+                .WithMany(schemaDefinition => schemaDefinition.DataSetSpecifications)
+                .HasForeignKey(columnDefinition => columnDefinition.DataSetId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .HasIndex(ds => ds.SupersededById)
+                .IsUnique(false);
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .HasIndex(ds => ds.PresededById)
+                .IsUnique(false);
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .HasMany(ds => ds.SupersededBy)
+                .WithOne()
+                .HasForeignKey(ds => ds.SupersededById)
+                .IsRequired(false);
+
+            modelBuilder.Entity<DataSetSpecification>()
+                .HasMany(ds => ds.PresededBy)
+                .WithOne()
+                .HasForeignKey(ds => ds.PresededById)
+                .IsRequired(false);
+        }
+    }
+}
--- LHDS.Core/Brokers/Storages/Sql/StorageBroker.cs
+++ LHDS.Core/Brokers/Storages/Sql/StorageBroker.cs
@@ -8,17 +8,20 @@ using System.Threading.Tasks;
 using EFxceptions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using STX.EFCore.Client.Clients;
 
 namespace LHDS.Core.Brokers.Storages.Sql
 {
     public partial class StorageBroker : EFxceptionsContext, IStorageBroker
     {
         private readonly IConfiguration configuration;
+        private readonly IEFCoreClient efCoreClient;
 
         public StorageBroker(IConfiguration configuration)
         {
             this.configuration = configuration;
             this.Database.Migrate();
+            this.efCoreClient = new EFCoreClient(this);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -53,54 +56,30 @@ namespace LHDS.Core.Brokers.Storages.Sql
             optionsBuilder.UseSqlServer(connectionString);
         }
 
-        private async ValueTask BulkInsertAsync<T>(IEnumerable<T> objects) where T : class
-        {
-            objects.ToList();
-            this.AddRange(objects);
-            await this.SaveChangesAsync();
-            objects.ToList().ForEach(@object => this.Entry(@object).State = EntityState.Detached);
-        }
+        private async ValueTask<T> InsertAsync<T>(T @object) where T : class =>
+            await efCoreClient.InsertAsync(@object);
 
-        private async ValueTask<T> InsertAsync<T>(T @object)
-        {
-            this.Entry(@object).State = EntityState.Added;
-            await this.SaveChangesAsync();
-            this.Entry(@object).State = EntityState.Detached;
+        private IQueryable<T> SelectAll<T>() where T : class => this.Set<T>();
 
-            return @object;
-        }
+        private async ValueTask<IQueryable<T>> SelectAllAsync<T>() where T : class =>
+            await efCoreClient.SelectAllAsync<T>();
 
-        private IQueryable<T> ReadAll<T>() where T : class => this.Set<T>();
+        private async ValueTask<T?> SelectAsync<T>(params object[] @objectIds) where T : class =>
+            await efCoreClient.SelectAsync<T>(@objectIds);
 
-        private async ValueTask<IQueryable<T>> ReadAllAsync<T>() where T : class =>
-            this.Set<T>();
+        private async ValueTask<T> UpdateAsync<T>(T @object) where T : class =>
+            await efCoreClient.UpdateAsync(@object);
 
-        private async ValueTask<T?> ReadAsync<T>(params object[] @objectIds) where T : class =>
-            await this.FindAsync<T>(objectIds);
+        private async ValueTask<T> DeleteAsync<T>(T @object) where T : class =>
+            await efCoreClient.UpdateAsync(@object);
 
-        private async ValueTask<T> UpdateAsync<T>(T @object)
-        {
-            this.Entry(@object).State = EntityState.Modified;
-            await this.SaveChangesAsync();
-            this.Entry(@object).State = EntityState.Detached;
+        private async ValueTask BulkInsertAsync<T>(IEnumerable<T> objects) where T : class =>
+            await efCoreClient.BulkInsertAsync<T>(objects);
 
-            return @object;
-        }
+        private async ValueTask BulkUpdateAsync<T>(IEnumerable<T> objects) where T : class =>
+            await efCoreClient.BulkUpdateAsync<T>(objects);
 
-        private async ValueTask BulkUpdateAsync<T>(IEnumerable<T> objects) where T : class
-        {
-            objects.ToList().ForEach(@object => this.Entry(@object).State = EntityState.Modified);
-            await this.SaveChangesAsync();
-            objects.ToList().ForEach(@object => this.Entry(@object).State = EntityState.Detached);
-        }
-
-        private async ValueTask<T> DeleteAsync<T>(T @object)
-        {
-            this.Entry(@object).State = EntityState.Deleted;
-            await this.SaveChangesAsync();
-            this.Entry(@object).State = EntityState.Detached;
-
-            return @object;
-        }
+        private async ValueTask BulkDeleteAsync<T>(IEnumerable<T> objects) where T : class =>
+            await efCoreClient.BulkDeleteAsync<T>(objects);
     }
 }

80c5cdd5cc2c1e532cc8c9bb191a2ca033ba90ef
Hassan Raza <h_raza110@outlook.com>
2024-10-14 10:31:34 +0100

    ORCHESTRATIONS: Read Schema File

---
+++ LHDS.ConfigImportExportTool/Services/Orchestrations/ReadSchema/IReadSchemaOrchestrationService.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.ConfigImportExportTool.Models.Foundations.ObjectColumns;
+
+namespace LHDS.ConfigImportExportTool.Services.Foundations.Files
+{
+    internal interface IReadSchemaOrchestrationService
+    {
+        ValueTask<IQueryable<ObjectColumn>> ProcessSchemaFile(string path);
+    }
+}
---
+++ LHDS.ConfigImportExportTool/Services/Orchestrations/ReadSchema/ReadSchemaOrchestrationService.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LHDS.ConfigImportExportTool.Brokers.CsvHelpers;
+using LHDS.ConfigImportExportTool.Models.Foundations.ObjectColumns;
+using LHDS.ConfigImportExportTool.Services.Foundations.Files;
+
+namespace LHDS.ConfigImportExportTool.Services.Orchestrations.ReadSchema
+{
+    internal partial class ReadSchemaOrchestrationService : IReadSchemaOrchestrationService
+    {
+        private readonly IFileService fileService;
+        private readonly ICsvHelperBroker csvHelperBroker;
+
+        public ReadSchemaOrchestrationService(IFileService fileService, ICsvHelperBroker csvHelperBroker)
+        {
+            this.fileService = fileService;
+            this.csvHelperBroker = csvHelperBroker;
+        }
+
+        public ValueTask<IQueryable<ObjectColumn>> ProcessSchemaFile(string path) =>
+            throw new NotImplementedException();
+    }
+}

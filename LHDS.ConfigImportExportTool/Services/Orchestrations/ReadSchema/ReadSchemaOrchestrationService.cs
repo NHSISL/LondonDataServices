d83f1e7dc3b2cc733f27dd1101ef5728e7c569d9
Hassan Raza <h_raza110@outlook.com>
2024-10-15 14:16:08 +0100

    ShouldProcessSchemaFileAsync -> PASS

--- LHDS.ConfigImportExportTool/Services/Orchestrations/ReadSchema/ReadSchemaOrchestrationService.cs
+++ LHDS.ConfigImportExportTool/Services/Orchestrations/ReadSchema/ReadSchemaOrchestrationService.cs
@@ -2,6 +2,8 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System.Runtime.InteropServices;
+using System.Text;
 using LHDS.ConfigImportExportTool.Brokers.CsvHelpers;
 using LHDS.ConfigImportExportTool.Models.Foundations.ObjectColumns;
 using LHDS.ConfigImportExportTool.Services.Foundations.Files;
@@ -19,7 +21,12 @@ namespace LHDS.ConfigImportExportTool.Services.Orchestrations.ReadSchema
             this.csvHelperBroker = csvHelperBroker;
         }
 
-        public ValueTask<List<ObjectColumn>> ProcessSchemaFile(string path) =>
-            throw new NotImplementedException();
+        public async ValueTask<List<ObjectColumn>> ProcessSchemaFile(string path)
+        {
+            byte[] csvData = await this.fileService.ReadFromFileAsync(path);
+            string csvString = ASCIIEncoding.UTF8.GetString(csvData);
+
+            return await this.csvHelperBroker.MapCsvToObjectAsync<ObjectColumn>(csvString, true);
+        }
     }
 }

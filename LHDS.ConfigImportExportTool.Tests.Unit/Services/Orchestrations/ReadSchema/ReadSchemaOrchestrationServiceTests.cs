86e50ec1ede66f8ad910e8cb79dd861cd14f5d7f
Hassan Raza <h_raza110@outlook.com>
2024-10-16 12:20:23 +0100

    ShouldThrowServiceExceptionOnProcessSchemaFileIfServiceErrorOccursAndLogItAsync -> PASS

--- LHDS.ConfigImportExportTool.Tests.Unit/Services/Orchestrations/ReadSchema/ReadSchemaOrchestrationServiceTests.cs
+++ LHDS.ConfigImportExportTool.Tests.Unit/Services/Orchestrations/ReadSchema/ReadSchemaOrchestrationServiceTests.cs
@@ -7,13 +7,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using LHDS.ConfigImportExportTool.Brokers.CsvHelpers;
+using LHDS.ConfigImportExportTool.Brokers.Loggings;
+using LHDS.ConfigImportExportTool.Models.Foundations.Files.Exceptions;
 using LHDS.ConfigImportExportTool.Models.Foundations.ObjectColumns;
 using LHDS.ConfigImportExportTool.Services.Foundations.CsvHelpers;
 using LHDS.ConfigImportExportTool.Services.Foundations.Files;
 using LHDS.ConfigImportExportTool.Services.Orchestrations.ReadSchema;
 using Moq;
+using NHSISL.CsvHelperClient.Models.Clients.CsvHelpers.Exceptions;
 using Tynamix.ObjectFiller;
 using Xeptions;
+using Xunit;
 
 namespace LHDS.ConfigImportExportTool.Tests.Unit.Services.Orchestrations.ReadSchema
 {
@@ -21,16 +25,19 @@ namespace LHDS.ConfigImportExportTool.Tests.Unit.Services.Orchestrations.ReadSch
     {
         private readonly Mock<IFileService> fileServiceMock;
         private readonly Mock<ICsvHelperService> csvHelperServiceMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
         private readonly IReadSchemaOrchestrationService readSchemaOrchestrationService;
 
         public ReadSchemaOrchestrationServiceTests()
         {
             this.fileServiceMock = new Mock<IFileService>();
             this.csvHelperServiceMock = new Mock<ICsvHelperService>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
 
             this.readSchemaOrchestrationService = new ReadSchemaOrchestrationService(
                 fileService: this.fileServiceMock.Object,
-                csvHelperService : this.csvHelperServiceMock.Object);
+                csvHelperService : this.csvHelperServiceMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
         }
 
         private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
@@ -70,6 +77,47 @@ namespace LHDS.ConfigImportExportTool.Tests.Unit.Services.Orchestrations.ReadSch
                 .OnProperty(objectColumn => objectColumn.UpdatedBy).Use(user);
 
             return filler;
+        }
+
+        public static TheoryData<Xeption> ReadSchemaOrchestrationDependencyValidationExceptions()
+        {
+            string randomMessage = GetRandomString();
+            string exceptionMessage = randomMessage;
+            var innerException = new Xeption(exceptionMessage);
+
+            return new TheoryData<Xeption>
+            {
+                new FileValidationException(
+                    message: "File validation error occured, please contact support.",
+                    innerException),
+
+                new FileDependencyValidationException(
+                    message: "File dependency validation error occurred, please contact support.",
+                    innerException),
+
+                new CsvHelperClientValidationException(innerException),
+            };
+        }
+
+        public static TheoryData<Xeption> ReadSchemaOrchestrationDependencyExceptions()
+        {
+            string randomMessage = GetRandomString();
+            string exceptionMessage = randomMessage;
+            var innerException = new Xeption(exceptionMessage);
+
+            return new TheoryData<Xeption>
+            {
+                new FileDependencyException(
+                    message: "File dependency error occurred, please contact support.",
+                    innerException),
+
+                new FileServiceException(
+                    message: "File service error occurred, please contact support.",
+                    innerException),
+
+                new CsvHelperClientDependencyException(innerException),
+                new CsvHelperClientServiceException(innerException)
+            };
         }
     }
 }

6ce8714621c33d57b460eef6036372abd566db5f
Hassan Raza <hassan.raza3@nhs.net>
2024-10-23 11:03:11 +0100

    ShouldMapObjectToCsvAsync -> FAIL

---
+++ LHDS.ConfigImportExportTool.Tests.Unit/Services/Foundations/CsvHelpers/CsvHelperServiceTests.Logic.ObjectToCsv.cs
@@ -0,0 +1,46 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace LHDS.ConfigImportExportTool.Tests.Unit.Services.Foundations.CsvHelpers
+{
+    public partial class CsvHelperServiceTests
+    {
+        [Fact]
+        public async Task ShouldMapObjectToCsvAsync()
+        {
+            // given
+            string randomCsv = GetRandomString();
+            string expectedResult = randomCsv;
+            Dictionary<string, int> emptyDict = new Dictionary<string, int>();
+            dynamic inputObject = new ExpandoObject();
+            inputObject.test = "test";
+            List<dynamic> inputObjectList = new List<dynamic> { expectedResult };
+
+            this.csvHelperBrokerMock.Setup(broker =>
+                broker.MapObjectToCsvAsync<dynamic>(inputObjectList, true, emptyDict, false))
+                    .ReturnsAsync(expectedResult);
+
+            // when
+            string actualResult =
+                await this.csvHelperService.MapObjectToCsvAsync<dynamic>(inputObjectList, true, emptyDict, false);
+
+            // then
+            actualResult.Should().BeEquivalentTo(expectedResult);
+
+            this.csvHelperBrokerMock.Verify(broker =>
+                broker.MapObjectToCsvAsync<dynamic>(inputObjectList, true, emptyDict, false),
+                    Times.Once);
+
+            this.csvHelperBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

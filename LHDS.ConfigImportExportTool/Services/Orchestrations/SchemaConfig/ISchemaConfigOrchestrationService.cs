2f8af92a5a62cc0c4785f45b6df2ab0021c57f6e
Hassan Raza <h_raza110@outlook.com>
2024-10-24 11:01:44 +0100

    ORCHESTRATIONS: Schema Config Import

---
+++ LHDS.ConfigImportExportTool/Services/Orchestrations/SchemaConfig/ISchemaConfigOrchestrationService.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LHDS.ConfigImportExportTool.Models.Foundations.ObjectColumns;
+
+namespace LHDS.ConfigImportExportTool.Services.Orchestrations.SchemaConfig
+{
+    internal interface ISchemaConfigOrchestrationService
+    {
+        ValueTask Import(List<ObjectColumn> objectColumnList);
+    }
+}
---
+++ LHDS.ConfigImportExportTool/Services/Orchestrations/SchemaConfig/SchemaConfigOrchestrationService.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LHDS.ConfigImportExportTool.Brokers.Loggings;
+using LHDS.ConfigImportExportTool.Models.Foundations.ObjectColumns;
+using LHDS.ConfigImportExportTool.Services.Foundations.ObjectColumns;
+using LHDS.ConfigImportExportTool.Services.Foundations.SpecificationObjects;
+
+namespace LHDS.ConfigImportExportTool.Services.Orchestrations.SchemaConfig
+{
+    internal partial class SchemaConfigOrchestrationService : ISchemaConfigOrchestrationService
+    {
+        private readonly IObjectColumnService objectColumnService;
+        private readonly ISpecificationObjectService specificationObjectService;
+        private readonly ILoggingBroker loggingBroker;
+
+        public SchemaConfigOrchestrationService(
+            IObjectColumnService objectColumnService,
+            ISpecificationObjectService specificationObjectService,
+            ILoggingBroker loggingBroker)
+        {
+            this.objectColumnService = objectColumnService;
+            this.specificationObjectService = specificationObjectService;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public async ValueTask Import(List<ObjectColumn> objectColumnList) =>
+            throw new NotImplementedException();
+    }
+}

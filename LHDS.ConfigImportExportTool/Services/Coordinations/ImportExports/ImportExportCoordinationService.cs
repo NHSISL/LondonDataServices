320f053b6bfa5a01706c8463b208e72305e02941
Hassan Raza <h_raza110@outlook.com>
2024-10-24 15:21:18 +0100

    CODE RUB: Adding Stubs

---
+++ LHDS.ConfigImportExportTool/Services/Coordinations/ImportExports/ImportExportCoordinationService.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LHDS.ConfigImportExportTool.Brokers.Loggings;
+using LHDS.ConfigImportExportTool.Services.Orchestrations.ReadSchema;
+using LHDS.ConfigImportExportTool.Services.Orchestrations.SchemaConfig;
+
+namespace LHDS.ConfigImportExportTool.Services.Coordinations.ImportExports
+{
+    internal partial class ImportExportCoordinationService : IImportExportCoordinationService
+    {
+        private readonly IReadSchemaOrchestrationService readSchemaOrchestrationService;
+        private readonly ISchemaConfigOrchestrationService schemaConfigOrchestrationService;
+        private readonly ILoggingBroker loggingBroker;
+
+        public ImportExportCoordinationService(
+            IReadSchemaOrchestrationService readSchemaOrchestrationService,
+            ISchemaConfigOrchestrationService schemaConfigOrchestrationService,
+            ILoggingBroker loggingBroker)
+        {
+            this.readSchemaOrchestrationService = readSchemaOrchestrationService;
+            this.schemaConfigOrchestrationService = schemaConfigOrchestrationService;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public async ValueTask Export(string dataSetName, string version, string filePath) =>
+            throw new NotImplementedException();
+
+        public async ValueTask Import(string dataSetName, string version, string filePath) =>
+            throw new NotImplementedException();
+    }
+}
---
+++ LHDS.ConfigImportExportTool/Services/Processings/DataSets/DataSetProcessingService.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LHDS.ConfigImportExportTool.Models.Foundations.Datasets;
+
+namespace LHDS.ConfigImportExportTool.Services.Processings.DataSets
+{
+    internal class DataSetProcessingService : IDataSetProcessingService
+    {
+        public async ValueTask<IQueryable<DataSet>> RetrieveAllDataSetsAsync() =>
+            throw new NotImplementedException();
+    }
+}

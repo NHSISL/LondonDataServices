ef62aaf4c104c5986d1f670a46ec22c877242d2b
Hassan Raza <h_raza110@outlook.com>
2024-10-24 11:58:59 +0100

    ShouldImportSchemaConfigAsync -> FAIL

---
+++ LHDS.ConfigImportExportTool.Tests.Unit/Services/Orchestrations/SchemaConfig/SchemaConfigOrchestrationServiceTests.cs
@@ -0,0 +1,116 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using LHDS.ConfigImportExportTool.Brokers.Loggings;
+using LHDS.ConfigImportExportTool.Models.Foundations.ObjectColumns;
+using LHDS.ConfigImportExportTool.Models.Foundations.SpecificationObjects;
+using LHDS.ConfigImportExportTool.Services.Foundations.ObjectColumns;
+using LHDS.ConfigImportExportTool.Services.Foundations.SpecificationObjects;
+using LHDS.ConfigImportExportTool.Services.Orchestrations.SchemaConfig;
+using Moq;
+using Tynamix.ObjectFiller;
+using Xeptions;
+
+namespace LHDS.ConfigImportExportTool.Tests.Unit.Services.Orchestrations.SchemaConfig
+{
+    public partial class SchemaConfigOrchestrationServiceTests
+    {
+        private readonly Mock<IObjectColumnService> objectColumnServiceMock;
+        private readonly Mock<ISpecificationObjectService> specificationObjectServiceMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ISchemaConfigOrchestrationService schemaConfigOrchestrationService;
+
+        public SchemaConfigOrchestrationServiceTests()
+        {
+            this.objectColumnServiceMock = new Mock<IObjectColumnService>();
+            this.specificationObjectServiceMock = new Mock<ISpecificationObjectService>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.schemaConfigOrchestrationService = new SchemaConfigOrchestrationService(
+                objectColumnService: this.objectColumnServiceMock.Object,
+                specificationObjectService: this.specificationObjectServiceMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+
+        private static string GetRandomString(int length) =>
+           new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 5).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static List<ObjectColumn> CreateRandomObjectColumns()
+        {
+            return CreateObjectColumnFiller(dateTimeOffset: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber())
+                    .ToList();
+        }
+
+        private static ObjectColumn CreateRandomObjectColumn() =>
+            CreateObjectColumnFiller(dateTimeOffset: GetRandomDateTimeOffset()).Create();
+
+        private static ObjectColumn CreateRandomObjectColumn(DateTimeOffset dateTimeOffset) =>
+            CreateObjectColumnFiller(dateTimeOffset).Create();
+
+        private static Filler<ObjectColumn> CreateObjectColumnFiller(DateTimeOffset dateTimeOffset)
+        {
+            string user = Guid.NewGuid().ToString();
+            var filler = new Filler<ObjectColumn>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(objectColumn => objectColumn.CreatedBy).Use(user)
+                .OnProperty(objectColumn => objectColumn.UpdatedBy).Use(user);
+
+            return filler;
+        }
+
+        private static IQueryable<SpecificationObject> CreateRandomSpecificationObjects()
+        {
+            return CreateSpecificationObjectFiller(dateTimeOffset: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber())
+                    .AsQueryable();
+        }
+
+        private static SpecificationObject CreateRandomSpecificationObject() =>
+            CreateSpecificationObjectFiller(dateTimeOffset: GetRandomDateTimeOffset()).Create();
+
+        private static SpecificationObject CreateRandomSpecificationObject(DateTimeOffset dateTimeOffset) =>
+            CreateSpecificationObjectFiller(dateTimeOffset).Create();
+
+        private static Filler<SpecificationObject> CreateSpecificationObjectFiller(DateTimeOffset dateTimeOffset)
+        {
+            string user = GetRandomString(255);
+            var filler = new Filler<SpecificationObject>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(specificationObject => specificationObject.SupplierObjectName).Use(GetRandomString(255))
+                .OnProperty(specificationObject => specificationObject.OurObjectName).Use(GetRandomString(255))
+                .OnProperty(specificationObject => specificationObject.ObjectDescription).Use(GetRandomString(500))
+                .OnProperty(specificationObject => specificationObject.InterchangeProtocol).Use(GetRandomString(255))
+                .OnProperty(specificationObject => specificationObject.DeletionHandling).Use(GetRandomString(255))
+                .OnProperty(specificationObject => specificationObject.CreatedBy).Use(user)
+                .OnProperty(specificationObject => specificationObject.UpdatedBy).Use(user)
+                .OnProperty(specificationObject => specificationObject.ObjectColumns).IgnoreIt()
+                .OnProperty(specificationObject => specificationObject.DataSetSpecification).IgnoreIt();
+
+            return filler;
+        }
+    }
+}

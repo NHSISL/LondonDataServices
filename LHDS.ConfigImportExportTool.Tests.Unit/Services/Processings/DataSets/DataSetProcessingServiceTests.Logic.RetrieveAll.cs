4fdf12ad57a18069b194590658348e5bd042e6cd
Hassan Raza <h_raza110@outlook.com>
2024-10-29 19:12:39 +0000

    ShouldReturnDataSets -> FAIL

---
+++ LHDS.ConfigImportExportTool.Tests.Unit/Services/Processings/DataSets/DataSetProcessingServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,43 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.ConfigImportExportTool.Models.Foundations.Datasets;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processing.DataSets
+{
+    public partial class DataSetProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldReturnDataSets()
+        {
+            // given
+            IQueryable<DataSet> randomDataSets = CreateRandomDataSets();
+            IQueryable<DataSet> storageDataSets = randomDataSets;
+            IQueryable<DataSet> expectedDataSets = storageDataSets;
+
+            this.dataSetServiceMock.Setup(service =>
+                service.RetrieveAllDataSetsAsync())
+                    .ReturnsAsync(storageDataSets);
+
+            // when
+            IQueryable<DataSet> actualDataSets =
+                await this.dataSetProcessingService.RetrieveAllDataSetsAsync();
+
+            // then
+            actualDataSets.Should().BeEquivalentTo(expectedDataSets);
+
+            this.dataSetServiceMock.Verify(broker =>
+                broker.RetrieveAllDataSetsAsync(),
+                    Times.Once);
+
+            this.dataSetServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

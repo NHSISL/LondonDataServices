9b3ce16141eba53ff6028ba37a8bbd78f3bfcf20
Hassan Raza <h_raza110@outlook.com>
2024-10-30 09:01:19 +0000

    ShouldThrowServiceExceptionOnRemoveByIdIfServiceErrorOccursAsync -> FAIL

---
+++ LHDS.ConfigImportExportTool/Models/Processings/SpecificationObjects/Exceptions/SpecificationObjectProcessingDependencyException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.ConfigImportExportTool.Models.Processings.SpecificationObjects.Exceptions
+{
+    public class SpecificationObjectProcessingDependencyException : Xeption
+    {
+        public SpecificationObjectProcessingDependencyException(string message, Xeption? innerException)
+            : base(message, innerException)
+        { }
+    }
+}

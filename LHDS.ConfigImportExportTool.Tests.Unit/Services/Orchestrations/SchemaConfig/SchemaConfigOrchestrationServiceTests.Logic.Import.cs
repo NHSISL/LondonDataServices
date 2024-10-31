1b0e23cb922aa08b05034ab557e7ec52b70035e8
Hassan Raza <hassan.raza3@nhs.net>
2024-10-31 19:44:15 +0000

    WIP

---
+++ LHDS.ConfigImportExportTool.Tests.Unit/Services/Orchestrations/SchemaConfig/SchemaConfigOrchestrationServiceTests.Logic.Import.cs
@@ -0,0 +1,74 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.ConfigImportExportTool.Models.Bases.SchemaConfigs;
+using LHDS.ConfigImportExportTool.Models.Foundations.Datasets;
+using LHDS.ConfigImportExportTool.Models.Foundations.DatasetSpecifications;
+using LHDS.ConfigImportExportTool.Models.Foundations.ObjectColumns;
+using LHDS.ConfigImportExportTool.Models.Foundations.SpecificationObjects;
+using Moq;
+using Xunit;
+
+namespace LHDS.ConfigImportExportTool.Tests.Unit.Services.Orchestrations.SchemaConfigs
+{
+    public partial class SchemaConfigOrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldImportObjectsAsync()
+        {
+            // given
+            Guid inputDataSetId = Guid.NewGuid();
+            string randomDataSetName = GetRandomString(150);
+            string inputDataSetName = randomDataSetName;
+            string randomVersion = GetRandomString(10);
+            string inputVersion = randomVersion;
+            DataSet randomDataSet = CreateRandomDataSet(inputDataSetName);
+            randomDataSet.Id = inputDataSetId;
+
+            DataSetSpecification randomDataSetSpecification = 
+                CreateRandomDataSetSpecification(inputDataSetId, inputVersion);
+
+            DataSetSpecification storageDataSetSpecification = randomDataSetSpecification;
+            randomDataSet.DataSetSpecifications.Add(randomDataSetSpecification);
+            DataSet storageDataSet = randomDataSet;
+            SchemaConfig randomSchemaConfig = CreateRandomSchemaConfig(storageDataSet.Id);
+            SchemaConfig inputSchemaConfig = randomSchemaConfig;
+            List<DataSet> storageDataSets = new List<DataSet> { storageDataSet };
+
+            this.dataSetProcessingServiceMock.Setup(service =>
+                service.RetrieveAllDataSetsAsync())
+                    .ReturnsAsync(storageDataSets.AsQueryable());
+
+            foreach(SpecificationObject specificationObject in inputSchemaConfig.SpecificationObjects) 
+            {
+                this.specificationObjectProcessingServiceMock.Setup(service =>
+                    service.ReadOrInsertSpecificationObjectAsync())
+                        .ReturnsAsync(inputSchemaConfig);
+            };
+
+            // when
+            await this.schemaConfigOrchestrationService.Import(inputSchemaConfig, randomDataSetName, inputVersion);
+
+            // then
+
+            this.specificationObjectProcessingServiceMock.Verify(service =>
+                service.ReadOrInsertSpecificationObjectAsync(),
+                    Times.Once);
+
+            this.csvHelperServiceMock.Verify(service =>
+                service.MapCsvToObjectAsync<ObjectColumn>(inputCsvString, true, fieldMappings)
+                    ,Times.Once);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.csvHelperServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}

6e05fd5fc7de5c81d1582a2862fe803cfc2e7abd
Hassan Raza <h_raza110@outlook.com>
2024-11-11 16:07:14 +0000

    WIP

---
+++ LHDS.ConfigImportExportTool/Models/Orchestrations/ReadSchema/CannonicalSchemaItem.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+namespace LHDS.ConfigImportExportTool.Models.Orchestrations.ReadSchema
+{
+    public class CannonicalSchemaItem
+    {
+        public string TableName { get; set; }
+        public string TableDescription { get; set; }
+        public string ColumnName { get; set; }
+        public string ColumnDescription { get; set; }
+        public string ColumnDataType { get; set; }
+        public string ColumnLength { get; set; }
+        public string DateDefining { get; set; }
+        public string ColumnOrdinal { get; set; }
+        public string LinkedTable { get; set; }
+        public string LinkedColumn1 { get; set; }
+    }
+}
--- LHDS.ConfigImportExportTool/Services/Orchestrations/ReadSchema/ReadSchemaOrchestrationService.cs
+++ LHDS.ConfigImportExportTool/Services/Orchestrations/ReadSchema/ReadSchemaOrchestrationService.cs
@@ -4,9 +4,13 @@
 
 using System.Text;
 using LHDS.ConfigImportExportTool.Brokers.Loggings;
+using LHDS.ConfigImportExportTool.Models.Bases.;
 using LHDS.ConfigImportExportTool.Models.Foundations.ObjectColumns;
+using LHDS.ConfigImportExportTool.Models.Foundations.SpecificationObjects;
+using LHDS.ConfigImportExportTool.Models.Orchestrations.ReadSchema;
 using LHDS.ConfigImportExportTool.Services.Foundations.CsvHelpers;
 using LHDS.ConfigImportExportTool.Services.Foundations.Files;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace LHDS.ConfigImportExportTool.Services.Orchestrations.ReadSchema
 {
@@ -26,14 +30,34 @@ namespace LHDS.ConfigImportExportTool.Services.Orchestrations.ReadSchema
             this.loggingBroker = loggingBroker;
         }
 
-        public ValueTask<List<ObjectColumn>> ReadFile(string path) =>
+        public ValueTask<List<SpecificationObject>> ReadFile(string path) =>
             TryCatch(async () =>
             {
                 ValidateProcessSchemaFileArguments(path);
                 byte[] csvData = await this.fileService.ReadFromFileAsync(path);
                 string csvString = ASCIIEncoding.UTF8.GetString(csvData);
 
-                return await this.csvHelperService.MapCsvToObjectAsync<ObjectColumn>(csvString, true);
+                List<CannonicalSchemaItem> canonicalSchemaObjects = await this.csvHelperService
+                    .MapCsvToObjectAsync<CannonicalSchemaItem>(csvString, true);
+
+                List<SpecificationObject> specificationObjects = new List<SpecificationObject>();
+
+                foreach (var canonicalSchemaObject in canonicalSchemaObjects)
+                {
+                    var newSpecificationObjects = new SpecificationObject
+                    {
+                        SupplierObjectName = canonicalSchemaObject.TableName,
+                    };
+
+                    var newObjectColumn = new ObjectColumn
+                    {
+                        
+
+                               
+                    };
+
+                    newSpecificationObjects.ObjectColumns.Add(newObjectColumn);
+                }
             });
 
         public async ValueTask WriteFile(List<ObjectColumn> data, string path) =>

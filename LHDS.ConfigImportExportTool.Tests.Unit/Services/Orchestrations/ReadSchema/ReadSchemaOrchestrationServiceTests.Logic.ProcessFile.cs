99fed35def6cb95e03468f6cacde767475db2ef4
Hassan Raza <h_raza110@outlook.com>
2024-11-12 10:12:04 +0000

    WIP -> ShouldProcessSchemaFileAsync

--- LHDS.ConfigImportExportTool.Tests.Unit/Services/Orchestrations/ReadSchema/ReadSchemaOrchestrationServiceTests.Logic.ProcessFile.cs
+++ LHDS.ConfigImportExportTool.Tests.Unit/Services/Orchestrations/ReadSchema/ReadSchemaOrchestrationServiceTests.Logic.ProcessFile.cs
@@ -7,8 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
-using LHDS.ConfigImportExportTool.Models.Foundations.ObjectColumns;
 using LHDS.ConfigImportExportTool.Models.Foundations.SpecificationObjects;
+using LHDS.ConfigImportExportTool.Models.Orchestrations.ReadSchema;
 using Moq;
 using Xunit;
 
@@ -27,31 +27,31 @@ namespace LHDS.ConfigImportExportTool.Tests.Unit.Services.Orchestrations.ReadSch
             string inputCsvString = randomCsvString;
             byte[] outputResult = ASCIIEncoding.UTF8.GetBytes(inputCsvString);
             byte[] expectedBytes = outputResult;
-            List<SpecificationObject> randomObjectColumns = CreateRandomObjectColumns();
-            List<SpecificationObject> expectedObjectColumns = randomObjectColumns;
+            List<CannonicalSchemaItem> expectedCannonicalSchemaItems = CreateRandomCannonicalSchemaItems();
+            IQueryable<SpecificationObject> expectedSpecificationObjects = CreateRandomSpecificationObjects();
 
             this.fileServiceMock.Setup(service =>
                 service.ReadFromFileAsync(inputFilePath))
                     .ReturnsAsync(outputResult);
 
             this.csvHelperServiceMock.Setup(service =>
-                service.MapCsvToObjectAsync<SpecificationObject>(inputCsvString, true, fieldMappings))
-                    .ReturnsAsync(expectedObjectColumns.ToList);
+                service.MapCsvToObjectAsync<CannonicalSchemaItem>(inputCsvString, true, fieldMappings))
+                    .ReturnsAsync(expectedCannonicalSchemaItems);
 
             // when
-            List<SpecificationObject> actualObjectColumn = 
+            List<SpecificationObject> actualObjectColumn =
                 await this.readSchemaOrchestrationService.ReadFile(inputFilePath);
 
             // then
-            actualObjectColumn.Should().BeEquivalentTo(expectedObjectColumns);
+            actualObjectColumn.Should().BeEquivalentTo(expectedSpecificationObjects);
 
             this.fileServiceMock.Verify(service =>
                 service.ReadFromFileAsync(inputFilePath),
                     Times.Once);
 
             this.csvHelperServiceMock.Verify(service =>
-                service.MapCsvToObjectAsync<ObjectColumn>(inputCsvString, true, fieldMappings)
-                    ,Times.Once);
+                service.MapCsvToObjectAsync<CannonicalSchemaItem>(inputCsvString, true, fieldMappings),
+                    Times.Once);
 
             this.fileServiceMock.VerifyNoOtherCalls();
             this.csvHelperServiceMock.VerifyNoOtherCalls();
--- LHDS.ConfigImportExportTool.Tests.Unit/Services/Orchestrations/ReadSchema/ReadSchemaOrchestrationServiceTests.cs
+++ LHDS.ConfigImportExportTool.Tests.Unit/Services/Orchestrations/ReadSchema/ReadSchemaOrchestrationServiceTests.cs
@@ -9,6 +9,8 @@ using System.Linq.Expressions;
 using LHDS.ConfigImportExportTool.Brokers.Loggings;
 using LHDS.ConfigImportExportTool.Models.Foundations.Files.Exceptions;
 using LHDS.ConfigImportExportTool.Models.Foundations.ObjectColumns;
+using LHDS.ConfigImportExportTool.Models.Foundations.SpecificationObjects;
+using LHDS.ConfigImportExportTool.Models.Orchestrations.ReadSchema;
 using LHDS.ConfigImportExportTool.Services.Foundations.CsvHelpers;
 using LHDS.ConfigImportExportTool.Services.Foundations.Files;
 using LHDS.ConfigImportExportTool.Services.Orchestrations.ReadSchema;
@@ -45,17 +47,55 @@ namespace LHDS.ConfigImportExportTool.Tests.Unit.Services.Orchestrations.ReadSch
         private static string GetRandomString() =>
             new MnemonicString().GetValue();
 
+        private static string GetRandomString(int length) =>
+           new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
         private static int GetRandomNumber() =>
             new IntRange(min: 2, max: 5).GetValue();
 
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
 
-        private static List<ObjectColumn> CreateRandomObjectColumns()
+        private static IQueryable<SpecificationObject> CreateRandomSpecificationObjects(
+            List<ObjectColumn> objectColumns)
+        {
+            return CreateSpecificationObjectFiller(dateTimeOffset: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber())
+                    .AsQueryable();
+        }
+
+        private static SpecificationObject CreateRandomSpecificationObject(List<ObjectColumn> objectColumns) =>
+            CreateSpecificationObjectFiller(dateTimeOffset: GetRandomDateTimeOffset(), objectColumns).Create();
+
+        private static SpecificationObject CreateRandomSpecificationObject(DateTimeOffset dateTimeOffset, List<ObjectColumn> objectColumns) =>
+            CreateSpecificationObjectFiller(dateTimeOffset, objectColumns).Create();
+
+        private static Filler<SpecificationObject> CreateSpecificationObjectFiller(DateTimeOffset dateTimeOffset, List<ObjectColumn> objectColumns)
+        {
+            string user = GetRandomString(255);
+            var filler = new Filler<SpecificationObject>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(specificationObject => specificationObject.SupplierObjectName).Use(GetRandomString(255))
+                .OnProperty(specificationObject => specificationObject.OurObjectName).Use(GetRandomString(255))
+                .OnProperty(specificationObject => specificationObject.ObjectDescription).Use(GetRandomString(500))
+                .OnProperty(specificationObject => specificationObject.InterchangeProtocol).Use(GetRandomString(255))
+                .OnProperty(specificationObject => specificationObject.DeletionHandling).Use(GetRandomString(255))
+                .OnProperty(specificationObject => specificationObject.CreatedBy).Use(user)
+                .OnProperty(specificationObject => specificationObject.UpdatedBy).Use(user)
+                .OnProperty(specificationObject => specificationObject.ObjectColumns).Use(objectColumns)
+                .OnProperty(specificationObject => specificationObject.DataSetSpecification).IgnoreIt();
+
+            return filler;
+        }
+
+        private static IQueryable<ObjectColumn> CreateRandomObjectColumns()
         {
             return CreateObjectColumnFiller(dateTimeOffset: GetRandomDateTimeOffset())
                 .Create(count: GetRandomNumber())
-                    .ToList();
+                    .AsQueryable();
         }
 
         private static ObjectColumn CreateRandomObjectColumn() =>
@@ -66,14 +106,32 @@ namespace LHDS.ConfigImportExportTool.Tests.Unit.Services.Orchestrations.ReadSch
 
         private static Filler<ObjectColumn> CreateObjectColumnFiller(DateTimeOffset dateTimeOffset)
         {
-            string user = Guid.NewGuid().ToString();
+            string user = GetRandomString(length: 255).ToString();
             var filler = new Filler<ObjectColumn>();
 
             filler.Setup()
                 .OnType<DateTimeOffset>().Use(dateTimeOffset)
                 .OnType<DateTimeOffset?>().Use(dateTimeOffset)
                 .OnProperty(objectColumn => objectColumn.CreatedBy).Use(user)
-                .OnProperty(objectColumn => objectColumn.UpdatedBy).Use(user);
+                .OnProperty(objectColumn => objectColumn.UpdatedBy).Use(user)
+                .OnProperty(objectColumn => objectColumn.SpecificationObject).IgnoreIt();
+
+            return filler;
+        }
+
+        private static List<CannonicalSchemaItem> CreateRandomCannonicalSchemaItems()
+        {
+            return CreateCannonicalSchemaItemFiller()
+                .Create(count: GetRandomNumber())
+                    .ToList();
+        }
+
+        private static CannonicalSchemaItem CreateRandomCannonicalSchemaItem() =>
+            CreateCannonicalSchemaItemFiller().Create();
+
+        private static Filler<CannonicalSchemaItem> CreateCannonicalSchemaItemFiller()
+        {
+            var filler = new Filler<CannonicalSchemaItem>();
 
             return filler;
         }

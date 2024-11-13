0b3040319d5d4bea7992a0387bdbaa47c1ce8ade
Hassan Raza <h_raza110@outlook.com>
2024-11-13 14:30:00 +0000

    ShouldImportObjectsAndColumnAsync -> FAIL

---
+++ LHDS.ConfigImportExportTool.Tests.Unit/Services/Coordinations/ImportExports/ImportExportCoordinationServiceTests.cs
@@ -0,0 +1,87 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using LHDS.ConfigImportExportTool.Brokers.Loggings;
+using LHDS.ConfigImportExportTool.Models.Foundations.SpecificationObjects;
+using LHDS.ConfigImportExportTool.Services.Coordinations.ImportExports;
+using LHDS.ConfigImportExportTool.Services.Orchestrations.ReadSchema;
+using LHDS.ConfigImportExportTool.Services.Orchestrations.SchemaConfigs;
+using Moq;
+using Tynamix.ObjectFiller;
+using Xeptions;
+
+namespace LHDS.ConfigImportExportTool.Tests.Unit.Services.Coordinations.ImportExports
+{
+    public partial class ImportExportCoordinationServiceTests
+    {
+        private readonly Mock<IReadSchemaOrchestrationService> readSchemaOrchestrationServiceMock;
+        private readonly Mock<ISchemaConfigOrchestrationService> schemaConfigOrchestrationServiceMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IImportExportCoordinationService importExportCoordinationService;
+
+        public ImportExportCoordinationServiceTests()
+        {
+            this.readSchemaOrchestrationServiceMock = new Mock<IReadSchemaOrchestrationService>();
+            this.schemaConfigOrchestrationServiceMock = new Mock<ISchemaConfigOrchestrationService>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.importExportCoordinationService = new ImportExportCoordinationService(
+                readSchemaOrchestrationService: this.readSchemaOrchestrationServiceMock.Object,
+                schemaConfigOrchestrationService: this.schemaConfigOrchestrationServiceMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 5).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static string GetRandomString(int length) =>
+           new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
+        private static List<SpecificationObject> CreateRandomSpecificationObjects()
+        {
+            return CreateSpecificationObjectFiller(dateTimeOffset: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber()).ToList();
+        }
+
+        private static SpecificationObject CreateRandomSpecificationObject() =>
+            CreateSpecificationObjectFiller(dateTimeOffset: GetRandomDateTimeOffset()).Create();
+
+        private static SpecificationObject CreateRandomSpecificationObject(DateTimeOffset dateTimeOffset) =>
+            CreateSpecificationObjectFiller(dateTimeOffset).Create();
+
+        private static Filler<SpecificationObject> CreateSpecificationObjectFiller(DateTimeOffset dateTimeOffset)
+        {
+            string user = GetRandomString(255);
+            var filler = new Filler<SpecificationObject>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(specificationObject => specificationObject.SupplierObjectName).Use(GetRandomString(255))
+                .OnProperty(specificationObject => specificationObject.OurObjectName).Use(GetRandomString(255))
+                .OnProperty(specificationObject => specificationObject.ObjectDescription).Use(GetRandomString(500))
+                .OnProperty(specificationObject => specificationObject.InterchangeProtocol).Use(GetRandomString(255))
+                .OnProperty(specificationObject => specificationObject.DeletionHandling).Use(GetRandomString(255))
+                .OnProperty(specificationObject => specificationObject.CreatedBy).Use(user)
+                .OnProperty(specificationObject => specificationObject.UpdatedBy).Use(user)
+                .OnProperty(specificationObject => specificationObject.ObjectColumns).IgnoreIt()
+                .OnProperty(specificationObject => specificationObject.DataSetSpecification).IgnoreIt();
+
+            return filler;
+        }
+    }
+}

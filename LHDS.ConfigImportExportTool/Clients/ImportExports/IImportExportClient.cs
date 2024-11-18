2e9e7a73ab18207fc78c73e7b1f6150cce91d0ca
Hassan Raza <h_raza110@outlook.com>
2024-11-18 12:20:06 +0000

    WIP

--- LHDS.ConfigImportExportTool/Clients/ImportExports/IImportExportClient.cs
+++ LHDS.ConfigImportExportTool/Clients/ImportExports/IImportExportClient.cs
@@ -2,6 +2,8 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using LHDS.ConfigImportExportTool.Brokers.Storages.Sql;
+
 namespace LHDS.ConfigImportExportTool.Clients.ImportExports
 {
     public interface IImportExportClient

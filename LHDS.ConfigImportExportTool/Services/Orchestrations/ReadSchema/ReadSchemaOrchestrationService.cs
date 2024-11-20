a06f539e0e7212d7c32b69c9e111d7b667078720
Hassan Raza <h_raza110@outlook.com>
2024-11-20 16:54:59 +0000

    ShouldWriteSchemaCsvFileAsync -> WIP

--- LHDS.ConfigImportExportTool/Services/Orchestrations/ReadSchema/ReadSchemaOrchestrationService.cs
+++ LHDS.ConfigImportExportTool/Services/Orchestrations/ReadSchema/ReadSchemaOrchestrationService.cs
@@ -69,7 +69,34 @@ namespace LHDS.ConfigImportExportTool.Services.Orchestrations.ReadSchema
                 return specificationObjects;
             });
 
-        public async ValueTask WriteFile(List<SpecificationObject> data, string path) =>
-            throw new NotImplementedException();
+        public async ValueTask WriteFile(List<SpecificationObject> data, string path)
+        {
+            List<CannonicalSchemaItem> mappedCannonicalSchemaItems = new List<CannonicalSchemaItem>();
+
+            foreach (SpecificationObject specificationObject in data)
+            {
+                foreach (ObjectColumn objectColumn in specificationObject.ObjectColumns)
+                {
+                    CannonicalSchemaItem cannonicalSchemaItem = new CannonicalSchemaItem
+                    {
+                        TableName = specificationObject.SupplierObjectName,
+                        TableDescription = specificationObject.ObjectDescription,
+                        ColumnName = objectColumn.SupplierColumnName,
+                        ColumnDataType = objectColumn.SqlDataType,
+                        ColumnDescription = objectColumn.ColumnDescription,
+                        ColumnLength = objectColumn.Length,
+                        ColumnOrdinal = objectColumn.OrdinalPosition,
+                        LinkedTable = objectColumn.ForeignKeyTableName,
+                        LinkedColumn = objectColumn.ForeignKeyColumnName,
+                    };
+
+                    mappedCannonicalSchemaItems.Add(cannonicalSchemaItem);
+                }
+            }
+
+            string csvString = await this.csvHelperService.MapObjectToCsvAsync(mappedCannonicalSchemaItems, true);
+
+            await this.fileService.WriteToFileAsync(path, csvString);
+        }
     }
 }

7388b826203227c4ad807f600155328247eecd5a
Joe Parkes <joe.owens@answerdigital.com>
2024-11-22 10:23:02 +0000

    ShouldThrowDependencyValidationOnExportIfDependencyValidationOccursAndLogItAsync -> FAIL

---
+++ LHDS.ConfigImportExportTool.Tests.Unit/Services/Orchestrations/SchemaConfig/SchemaConfigOrchestrationServiceTests.Exceptions.Export.cs
@@ -0,0 +1,65 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.ConfigImportExportTool.Models.Foundations.SpecificationObjects;
+using LHDS.ConfigImportExportTool.Models.Orchestrations.SchemaConfigs.Exceptions;
+using Moq;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.ConfigImportExportTool.Tests.Unit.Services.Orchestrations.SchemaConfigs
+{
+    public partial class SchemaConfigOrchestrationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(SchemaConfigOrchestrationDependencyValidationExceptions))]
+        public async Task
+            ShouldThrowDependencyValidationOnExportIfDependencyValidationOccursAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            string inputDataSetName = GetRandomString(10);
+            string inputVersion = GetRandomString(10);
+
+            var expectedDependencyException =
+                new SchemaConfigOrchestrationDependencyValidationException(
+                    message: "Schema config orchestration dependency validation error occurred, " +
+                        "please contact support.",
+                    innerException: dependencyValidationException.InnerException as Xeption);
+
+            this.dataSetProcessingServiceMock.Setup(service =>
+                service.RetrieveAllDataSetsAsync())
+                    .ThrowsAsync(dependencyValidationException);
+
+            // when
+            ValueTask<List<SpecificationObject>> exportTask =
+                this.schemaConfigOrchestrationService.Export(inputDataSetName, inputVersion);
+
+            SchemaConfigOrchestrationDependencyValidationException actualException =
+                await Assert.ThrowsAsync<SchemaConfigOrchestrationDependencyValidationException>(
+                    exportTask.AsTask);
+
+            // then
+            actualException.Should()
+                 .BeEquivalentTo(expectedDependencyException);
+
+            this.dataSetProcessingServiceMock.Verify(service =>
+             service.RetrieveAllDataSetsAsync(),
+                 Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogErrorAsync(It.Is(SameExceptionAs(
+                   expectedDependencyException))),
+                       Times.Once);
+
+            this.dataSetProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.specificationObjectProcessingServiceMock.VerifyNoOtherCalls();
+            this.objectColumnProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}

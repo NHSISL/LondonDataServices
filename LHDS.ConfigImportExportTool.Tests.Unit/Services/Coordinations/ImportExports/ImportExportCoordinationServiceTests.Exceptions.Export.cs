9a33081dfa923b30999d3c0b68d9220d7158f215
Joe Parkes <joe.owens@answerdigital.com>
2024-11-22 11:28:03 +0000

    ShouldThrowServiceExceptionOnExportIfServiceErrorOccursAndLogItAsync -> PASS

--- LHDS.ConfigImportExportTool.Tests.Unit/Services/Coordinations/ImportExports/ImportExportCoordinationServiceTests.Exceptions.Export.cs
+++ LHDS.ConfigImportExportTool.Tests.Unit/Services/Coordinations/ImportExports/ImportExportCoordinationServiceTests.Exceptions.Export.cs
@@ -2,6 +2,7 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using LHDS.ConfigImportExportTool.Models.Coordinations.ImportExports.Exceptions;
@@ -100,6 +101,57 @@ namespace LHDS.ConfigImportExportTool.Tests.Unit.Services.Coordinations.ImportEx
             this.loggingBrokerMock.Verify(broker =>
                broker.LogErrorAsync(It.Is(SameExceptionAs(
                    expectedDependencyException))),
+                       Times.Once);
+
+            this.readSchemaOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.schemaConfigOrchestrationServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task
+            ShouldThrowServiceExceptionOnExportIfServiceErrorOccursAndLogItAsync()
+        {
+            //given
+            string inputDataSetName = GetRandomString(10);
+            string inputVersion = GetRandomString(10);
+            string inputFilePath = GetRandomString();
+            var serviceException = new Exception();
+
+            var failedImportExportCoordinationServiceException =
+                new FailedImportExportCoordinationServiceException(
+                    message: "Failed import export coordination service error occurred, please contact support.",
+                    innerException: serviceException);
+
+            var expectedServiceException =
+                new ImportExportCoordinationServiceException(
+                    message: "Import export coordination service error occurred, " +
+                        "please contact support.",
+                    innerException: failedImportExportCoordinationServiceException);
+
+            this.schemaConfigOrchestrationServiceMock.Setup(service =>
+                service.Export(inputDataSetName, inputVersion))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask exportFileTask =
+                this.importExportCoordinationService.Export(inputDataSetName, inputVersion, inputFilePath);
+
+            ImportExportCoordinationServiceException actualException =
+                await Assert.ThrowsAsync<ImportExportCoordinationServiceException>(
+                    exportFileTask.AsTask);
+
+            // then
+            actualException.Should()
+                 .BeEquivalentTo(expectedServiceException);
+
+            this.schemaConfigOrchestrationServiceMock.Verify(service =>
+             service.Export(inputDataSetName, inputVersion),
+                 Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogErrorAsync(It.Is(SameExceptionAs(
+                   expectedServiceException))),
                        Times.Once);
 
             this.readSchemaOrchestrationServiceMock.VerifyNoOtherCalls();

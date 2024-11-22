90c3948e5861952c54a4ed07f55dfcb337046202
Joe Parkes <joe.owens@answerdigital.com>
2024-11-22 11:26:37 +0000

    ShouldThrowDependencyExceptionOnExportIfDependencyErrorOccursAndLogItAsync -> PASS

--- LHDS.ConfigImportExportTool.Tests.Unit/Services/Coordinations/ImportExports/ImportExportCoordinationServiceTests.Exceptions.Export.cs
+++ LHDS.ConfigImportExportTool.Tests.Unit/Services/Coordinations/ImportExports/ImportExportCoordinationServiceTests.Exceptions.Export.cs
@@ -59,5 +59,52 @@ namespace LHDS.ConfigImportExportTool.Tests.Unit.Services.Coordinations.ImportEx
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.schemaConfigOrchestrationServiceMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [MemberData(nameof(ImportExportCoordinationDependencyExceptions))]
+        public async Task
+            ShouldThrowDependencyExceptionOnExportIfDependencyErrorOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            string inputDataSetName = GetRandomString(10);
+            string inputVersion = GetRandomString(10);
+            string inputFilePath = GetRandomString();
+
+            var expectedDependencyException =
+                new ImportExportCoordinationDependencyException(
+                    message: "Import export coordination dependency error occurred, " +
+                        "please contact support.",
+                    innerException: dependencyException.InnerException as Xeption);
+
+            this.schemaConfigOrchestrationServiceMock.Setup(service =>
+                service.Export(inputDataSetName, inputVersion))
+                    .ThrowsAsync(dependencyException);
+
+            // when
+            ValueTask exportFileTask =
+                this.importExportCoordinationService.Export(inputDataSetName, inputVersion, inputFilePath);
+
+            ImportExportCoordinationDependencyException actualException =
+                await Assert.ThrowsAsync<ImportExportCoordinationDependencyException>(
+                    exportFileTask.AsTask);
+
+            // then
+            actualException.Should()
+                 .BeEquivalentTo(expectedDependencyException);
+
+            this.schemaConfigOrchestrationServiceMock.Verify(service =>
+             service.Export(inputDataSetName, inputVersion),
+                 Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogErrorAsync(It.Is(SameExceptionAs(
+                   expectedDependencyException))),
+                       Times.Once);
+
+            this.readSchemaOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.schemaConfigOrchestrationServiceMock.VerifyNoOtherCalls();
+        }
     }
 }

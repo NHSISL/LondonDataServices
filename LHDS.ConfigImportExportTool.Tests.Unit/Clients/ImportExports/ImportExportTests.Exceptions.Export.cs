bd0766c1365fa6a789365a476751e22de26b28ea
Joe Parkes <joe.owens@answerdigital.com>
2024-11-26 14:17:47 +0000

    ShouldThrowDependencyErrorOnExportIfDependencyErrorOccursAndLogItAsync -> PASS

--- LHDS.ConfigImportExportTool.Tests.Unit/Clients/ImportExports/ImportExportTests.Exceptions.Export.cs
+++ LHDS.ConfigImportExportTool.Tests.Unit/Clients/ImportExports/ImportExportTests.Exceptions.Export.cs
@@ -52,5 +52,45 @@ namespace LHDS.ConfigImportExportTool.Tests.Unit.Clients.ImportExports
 
             this.importExportCoordinationServiceMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [MemberData(nameof(ImportExportClientDependencyExceptions))]
+        public async Task
+            ShouldThrowDependencyErrorOnExportIfDependencyErrorOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            string inputDataSetName = GetRandomString(10);
+            string inputVersion = GetRandomString(10);
+            string inputFilePath = GetRandomString();
+
+            var expectedDependencyException =
+                new ImportExportClientDependencyException(
+                    message: "Import export client dependency error occurred, " +
+                        "please contact support.",
+                    innerException: dependencyException.InnerException as Xeption);
+
+            this.importExportCoordinationServiceMock.Setup(service =>
+                service.Export(inputDataSetName, inputVersion, inputFilePath))
+                    .ThrowsAsync(dependencyException);
+
+            // when
+            ValueTask exportFileTask =
+                this.importExportClient.Export(inputDataSetName, inputVersion, inputFilePath);
+
+            ImportExportClientDependencyException actualException =
+                await Assert.ThrowsAsync<ImportExportClientDependencyException>(
+                    exportFileTask.AsTask);
+
+            // then
+            actualException.Should()
+                 .BeEquivalentTo(expectedDependencyException);
+
+            this.importExportCoordinationServiceMock.Verify(service =>
+             service.Export(inputDataSetName, inputVersion, inputFilePath),
+                 Times.Once);
+
+            this.importExportCoordinationServiceMock.VerifyNoOtherCalls();
+        }
     }
 }

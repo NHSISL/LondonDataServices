616fcd24d9204c2e9bf079b7c0b43b31064da897
Joe Parkes <joe.owens@answerdigital.com>
2024-11-26 14:13:08 +0000

    ShouldThrowDependencyValidationOnExportIfDependencyValidationOccursAndLogItAsync -> PASS

--- LHDS.ConfigImportExportTool.Tests.Unit/Clients/ImportExports/ImportExportTests.cs
+++ LHDS.ConfigImportExportTool.Tests.Unit/Clients/ImportExports/ImportExportTests.cs
@@ -3,9 +3,12 @@
 // ---------------------------------------------------------
 
 using LHDS.ConfigImportExportTool.Clients;
+using LHDS.ConfigImportExportTool.Models.Coordinations.ImportExports.Exceptions;
 using LHDS.ConfigImportExportTool.Services.Coordinations.ImportExports;
 using Moq;
 using Tynamix.ObjectFiller;
+using Xeptions;
+using Xunit;
 
 namespace LHDS.ConfigImportExportTool.Tests.Unit.Clients.ImportExports
 {
@@ -22,5 +25,26 @@ namespace LHDS.ConfigImportExportTool.Tests.Unit.Clients.ImportExports
 
         private static string GetRandomString() =>
             new MnemonicString().GetValue();
+
+        private static string GetRandomString(int length) =>
+           new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
+        public static TheoryData<Xeption> ImportExportClientDependencyValidationExceptions()
+        {
+            string randomMessage = GetRandomString();
+            string exceptionMessage = randomMessage;
+            var innerException = new Xeption(exceptionMessage);
+
+            return new TheoryData<Xeption>
+            {
+                new ImportExportCoordinationValidationException(
+                    message: "Import export coordination validation error occurred, fix errors and try again.",
+                    innerException),
+
+                new ImportExportCoordinationDependencyValidationException(
+                    message: "Import export coordination dependency validation error occurred, please contact support.",
+                    innerException)
+            };
+        }
     }
 }

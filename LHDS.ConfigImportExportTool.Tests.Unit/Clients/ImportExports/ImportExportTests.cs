2196cf5ee8b1505a140a2942afdb2e8861087b75
Joe Parkes <joe.owens@answerdigital.com>
2024-11-26 13:57:58 +0000

    ShouldExport -> PASS

---
+++ LHDS.ConfigImportExportTool.Tests.Unit/Clients/ImportExports/ImportExportTests.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LHDS.ConfigImportExportTool.Clients;
+using LHDS.ConfigImportExportTool.Services.Coordinations.ImportExports;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace LHDS.ConfigImportExportTool.Tests.Unit.Clients.ImportExports
+{
+    public partial class ImportExportTests
+    {
+        private readonly Mock<IImportExportCoordinationService> importExportCoordinationServiceMock;
+        private readonly IImportExportClient importExportClient;
+
+        public ImportExportTests()
+        {
+            this.importExportCoordinationServiceMock = new Mock<IImportExportCoordinationService>();
+            this.importExportClient = new ImportExportClient(this.importExportCoordinationServiceMock.Object);
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+    }
+}

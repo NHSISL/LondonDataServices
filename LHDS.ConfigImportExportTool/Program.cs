383b77d8817ea4e6e3772fbdf424d8402335b090
Hassan Raza <hassan.raza3@nhs.net>
2024-12-09 16:12:18 +0000

    CONSOLE: Adding Validaiton For Arguments

--- LHDS.ConfigImportExportTool/Program.cs
+++ LHDS.ConfigImportExportTool/Program.cs
@@ -2,9 +2,13 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using CsvHelper;
 using LHDS.ConfigImportExportTool.Clients.ImportExports;
+using LHDS.ConfigImportExportTool.Models.Coordinations.ImportExports.Exceptions;
+using LHDS.ConfigImportExportTool.Models.Foundations.CsvHelpers.Exceptions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Xeptions;
 
 internal class Program
 {
@@ -47,5 +51,44 @@ internal class Program
         {
             Console.WriteLine("Please enter a correct execution type (import or export)");
         }
+    }
+
+    private void ValidateImportFileArguments(
+        string executionType,
+        string dataSetName,
+        string version,
+        string filePath)
+    {
+        Validate<InvalidArgumentImportExportCoordinationException>(
+            message: "Invalid import export coordination argument(s), please correct the errors and try again.",
+            (Rule: IsInvalid(executionType), Parameter: nameof(executionType)),
+            (Rule: IsInvalid(dataSetName), Parameter: nameof(dataSetName)),
+            (Rule: IsInvalid(version), Parameter: nameof(version)),
+            (Rule: IsInvalid(filePath), Parameter: nameof(filePath)));
+    }
+
+
+    private static dynamic IsInvalid(string? text) => new
+    {
+        Condition = String.IsNullOrWhiteSpace(text),
+        Message = "Text is required"
+    };
+
+    private static void Validate<T>(string message, params (dynamic Rule, string Parameter)[] validations)
+        where T : Xeption
+    {
+        var invalidDataException = (T?)Activator.CreateInstance(typeof(T), message);
+
+        foreach ((dynamic rule, string parameter) in validations)
+        {
+            if (rule.Condition)
+            {
+                invalidDataException?.UpsertDataList(
+                    key: parameter,
+                    value: rule.Message);
+            }
+        }
+
+        invalidDataException?.ThrowIfContainsErrors();
     }
 }

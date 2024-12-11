55370ce98f3a68d58144940be135048207db53e8
Hassan Raza <hassan.raza3@nhs.net>
2024-12-11 11:52:42 +0000

    Migrations: Specificaiton Objects

---
+++ LHDS.Core/Migrations/20241211115225_SpecificationObject_update.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LHDS.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class SpecificationObject_update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeleteCondition",
+                schema: "Configuration",
+                table: "SpecificationObjects",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "SpecificationObjectsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "Configuration")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCaseSensitive",
+                schema: "Configuration",
+                table: "SpecificationObjects",
+                type: "bit",
+                nullable: false,
+                defaultValue: false)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "SpecificationObjectsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "Configuration")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsNumerice",
+                schema: "Configuration",
+                table: "SpecificationObjects",
+                type: "bit",
+                nullable: false,
+                defaultValue: false)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "SpecificationObjectsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "Configuration")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPostcode",
+                schema: "Configuration",
+                table: "SpecificationObjects",
+                type: "bit",
+                nullable: false,
+                defaultValue: false)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "SpecificationObjectsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "Configuration")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeleteCondition",
+                schema: "Configuration",
+                table: "SpecificationObjects")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "SpecificationObjectsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "Configuration")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.DropColumn(
+                name: "IsCaseSensitive",
+                schema: "Configuration",
+                table: "SpecificationObjects")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "SpecificationObjectsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "Configuration")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.DropColumn(
+                name: "IsNumerice",
+                schema: "Configuration",
+                table: "SpecificationObjects")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "SpecificationObjectsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "Configuration")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.DropColumn(
+                name: "IsPostcode",
+                schema: "Configuration",
+                table: "SpecificationObjects")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "SpecificationObjectsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "Configuration")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+        }
+    }
+}

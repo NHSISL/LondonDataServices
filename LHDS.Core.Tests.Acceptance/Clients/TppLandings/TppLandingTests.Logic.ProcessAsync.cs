080203d087406c6c3f0a141afaa5b99cea6dfd89
Hassan Raza <h_raza110@outlook.com>
2024-12-18 10:02:52 +0000

    TPP Tests -> Passing

--- LHDS.Core.Tests.Acceptance/Clients/TppLandings/TppLandingTests.Logic.ProcessAsync.cs
+++ LHDS.Core.Tests.Acceptance/Clients/TppLandings/TppLandingTests.Logic.ProcessAsync.cs
@@ -74,7 +74,8 @@ namespace LHDS.Core.Tests.Acceptance.Clients.TppLandings
                 blobContainers.Ingress);
         }
 
-        [Fact(Skip = "Will fix later")]
+        //[Fact(Skip = "Will fix later")]
+        [Fact]
         public async Task ShouldNotProcessExistingDocumentAsync()
         {
             //Given
@@ -136,7 +137,8 @@ namespace LHDS.Core.Tests.Acceptance.Clients.TppLandings
                 blobContainers.Ingress);
         }
 
-        [Fact(Skip = "Will fix later")]
+        //[Fact(Skip = "Will fix later")]
+        [Fact]
         public async Task ShouldProcessExistingDocumentAndUpdateHashAsync()
         {
             //Given

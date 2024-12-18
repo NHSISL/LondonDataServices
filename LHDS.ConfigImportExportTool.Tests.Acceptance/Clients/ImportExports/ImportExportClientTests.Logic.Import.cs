139f1276435ef89601b37ddcbebc9d6f72afcfe7
Hassan Raza <h_raza110@outlook.com>
2024-12-18 15:06:47 +0000

    ShouldImportSchemaFileAsync

--- LHDS.ConfigImportExportTool.Tests.Acceptance/Clients/ImportExports/ImportExportClientTests.Logic.Import.cs
+++ LHDS.ConfigImportExportTool.Tests.Acceptance/Clients/ImportExports/ImportExportClientTests.Logic.Import.cs
@@ -19,7 +19,8 @@ namespace LHDS.ConfigImportExportTool.Tests.Acceptance.Clients.ImportExports
 {
     public partial class ImportExportClientTests
     {
-        [Fact(Skip = "Hassan can you fix please")]
+        //[Fact(Skip = "Hassan can you fix please")]
+        [Fact]
         public async Task ShouldImportSchemaFileAsync()
         {
             //Given

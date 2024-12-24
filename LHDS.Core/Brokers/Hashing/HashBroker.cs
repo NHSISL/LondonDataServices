93cc34c524b94e553810ef57985d3e706061ea6e
hassaero110 <hassan.raza3@nhs.net>
2024-12-24 12:34:15 +0000

    CODE RUB: Hash Broker Async

--- LHDS.Core/Brokers/Hashing/HashBroker.cs
+++ LHDS.Core/Brokers/Hashing/HashBroker.cs
@@ -5,6 +5,7 @@
 using System;
 using System.IO;
 using System.Security.Cryptography;
+using System.Threading.Tasks;
 
 namespace LHDS.Core.Brokers.Hashing
 {
@@ -27,7 +28,41 @@ namespace LHDS.Core.Brokers.Hashing
             }
         }
 
-        public string GenerateSha256Hash(Stream? data)
+        public async ValueTask<string> GenerateMd5HashAsync(Stream? data)
+        {
+            if (data == null)
+            {
+                return string.Empty;
+            }
+
+            using (MD5 md5 = MD5.Create())
+            {
+                data.Position = 0;
+                byte[] hashBytes = md5.ComputeHash(data);
+                var md5Hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+
+                return md5Hash;
+            }
+        }
+
+        //public string GenerateSha256Hash(Stream? data)
+        //{
+        //    if (data == null)
+        //    {
+        //        return string.Empty;
+        //    }
+
+        //    using (SHA256 sha256 = SHA256.Create())
+        //    {
+        //        data.Position = 0;
+        //        byte[] hashBytes = sha256.ComputeHash(data);
+        //        var sha256Hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+
+        //        return sha256Hash;
+        //    }
+        //}
+
+        public async ValueTask<string> GenerateSha256HashAsync(Stream? data)
         {
             if (data == null)
             {
--- LHDS.Core/Brokers/Hashing/IHashBroker.cs
+++ LHDS.Core/Brokers/Hashing/IHashBroker.cs
@@ -3,12 +3,15 @@
 // ---------------------------------------------------------
 
 using System.IO;
+using System.Threading.Tasks;
 
 namespace LHDS.Core.Brokers.Hashing
 {
     public interface IHashBroker
     {
-        string GenerateSha256Hash(Stream? data);
+        //string GenerateSha256Hash(Stream? data);
+        ValueTask<string> GenerateSha256HashAsync(Stream? data);
         string GenerateMd5Hash(Stream? data);
+        ValueTask<string> GenerateMd5HashAsync(Stream? data);
     }
 }

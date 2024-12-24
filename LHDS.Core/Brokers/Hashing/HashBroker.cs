6b26808cb1e3ee4f713676ea46912102fa313873
hassaero110 <hassan.raza3@nhs.net>
2024-12-24 12:49:55 +0000

    CODE RUB: Removing non async

--- LHDS.Core/Brokers/Hashing/HashBroker.cs
+++ LHDS.Core/Brokers/Hashing/HashBroker.cs
@@ -11,22 +11,6 @@ namespace LHDS.Core.Brokers.Hashing
 {
     public class HashBroker : IHashBroker
     {
-        public string GenerateMd5Hash(Stream? data)
-        {
-            if (data == null)
-            {
-                return string.Empty;
-            }
-
-            using (MD5 md5 = MD5.Create())
-            {
-                data.Position = 0;
-                byte[] hashBytes = md5.ComputeHash(data);
-                var md5Hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
-
-                return md5Hash;
-            }
-        }
 
         public async ValueTask<string> GenerateMd5HashAsync(Stream? data)
         {
@@ -44,23 +28,6 @@ namespace LHDS.Core.Brokers.Hashing
                 return md5Hash;
             }
         }
-
-        //public string GenerateSha256Hash(Stream? data)
-        //{
-        //    if (data == null)
-        //    {
-        //        return string.Empty;
-        //    }
-
-        //    using (SHA256 sha256 = SHA256.Create())
-        //    {
-        //        data.Position = 0;
-        //        byte[] hashBytes = sha256.ComputeHash(data);
-        //        var sha256Hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
-
-        //        return sha256Hash;
-        //    }
-        //}
 
         public async ValueTask<string> GenerateSha256HashAsync(Stream? data)
         {
--- LHDS.Core/Brokers/Hashing/IHashBroker.cs
+++ LHDS.Core/Brokers/Hashing/IHashBroker.cs
@@ -9,9 +9,7 @@ namespace LHDS.Core.Brokers.Hashing
 {
     public interface IHashBroker
     {
-        //string GenerateSha256Hash(Stream? data);
         ValueTask<string> GenerateSha256HashAsync(Stream? data);
-        string GenerateMd5Hash(Stream? data);
         ValueTask<string> GenerateMd5HashAsync(Stream? data);
     }
 }

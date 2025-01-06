7acd7d821cf8203cbaab506223d387ce6d476fd0
Hassan Raza <h_raza110@outlook.com>
2025-01-06 16:17:06 +0000

    CODE RUB: Addresses select All Async

--- LHDS.Core/Brokers/Storages/Sql/IStorageBroker.Address.cs
+++ LHDS.Core/Brokers/Storages/Sql/IStorageBroker.Address.cs
@@ -14,7 +14,8 @@ namespace LHDS.Core.Brokers.Storages.Sql
     {
         ValueTask BulkInsertAddressesAsync(List<Address> address);
         ValueTask<Address> InsertAddressAsync(Address address);
-        IQueryable<Address> SelectAllAddresses();
+        //IQueryable<Address> SelectAllAddresses();
+        ValueTask<IQueryable<Address>> SelectAllAddressesAsync();
         ValueTask<Address> SelectAddressByIdAsync(Guid addressId);
         ValueTask<Address> UpdateAddressAsync(Address address);
         ValueTask BulkUpdateAddressesAsync(List<Address> address);

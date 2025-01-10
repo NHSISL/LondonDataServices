6ea3e647e51d0ee7b0fd0557486cedbdcfdb4784
hassaero110 <hassan.raza3@nhs.net>
2025-01-10 16:32:43 +0000

    WIP

--- LHDS.Core.Tests.Unit/Services/Foundations/TerminologyPolls/TerminologyPollServiceTests.Exceptions.Add.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/TerminologyPolls/TerminologyPollServiceTests.Exceptions.Add.cs
@@ -165,7 +165,7 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.TerminologyPolls
                     Times.Once());
 
             this.loggingBrokerMock.Verify(broker =>
-                broker.LogError(It.Is(SameExceptionAs(
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
                     expectedTerminologyPollValidationException))),
                         Times.Once);
 
@@ -222,7 +222,7 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.TerminologyPolls
                     Times.Never);
 
             this.loggingBrokerMock.Verify(broker =>
-                broker.LogError(It.Is(SameExceptionAs(
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
                     expectedTerminologyPollDependencyException))),
                         Times.Once);
 
@@ -273,7 +273,7 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.TerminologyPolls
                     Times.Never);
 
             this.loggingBrokerMock.Verify(broker =>
-                broker.LogError(It.Is(SameExceptionAs(
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
                     expectedTerminologyPollServiceException))),
                         Times.Once);
 

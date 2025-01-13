2edf22f8dc0a7bfd1e8b07befa795c469fce7cc9
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-13 16:02:23 +0000

    CODE RUB: Assigns Service Logging Async
    Signed-off-by: MarcusElcamel <marcus.elcamel@answerdigital.com>

--- LHDS.Core.Tests.Unit/Services/Foundations/Assigns/AssignServiceTests.Exceptions.MatchAddress.cs.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Assigns/AssignServiceTests.Exceptions.MatchAddress.cs.cs
@@ -51,7 +51,7 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Assigns
                     Times.Once);
 
             this.loggingBrokerMock.Verify(broker =>
-                 broker.LogError(It.Is(SameExceptionAs(
+                 broker.LogErrorAsync(It.Is(SameExceptionAs(
                      expectedAssignAddressServiveException))),
                          Times.Once);
 

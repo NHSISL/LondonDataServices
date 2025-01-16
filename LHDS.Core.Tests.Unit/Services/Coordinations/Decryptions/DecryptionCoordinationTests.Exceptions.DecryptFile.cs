848f11234c237ec6ec980dd01d520483e4db7b83
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-01-16 11:00:01 +0000

    CODE RUB: Coordinations Decryption LogAsync

--- LHDS.Core.Tests.Unit/Services/Coordinations/Decryptions/DecryptionCoordinationTests.Exceptions.DecryptFile.cs
+++ LHDS.Core.Tests.Unit/Services/Coordinations/Decryptions/DecryptionCoordinationTests.Exceptions.DecryptFile.cs
@@ -51,7 +51,7 @@ namespace LHDS.Core.Tests.Unit.Services.Coordinations.Decryptions
                     Times.Once);
 
             this.loggingBrokerMock.Verify(broker =>
-                 broker.LogError(It.Is(IsSameExceptionAs(
+                 broker.LogErrorAsync(It.Is(IsSameExceptionAs(
                      expectedDecryptionCoordinationDependencyValidationException))),
                          Times.Once);
 
@@ -95,7 +95,7 @@ namespace LHDS.Core.Tests.Unit.Services.Coordinations.Decryptions
                     Times.Once);
 
             this.loggingBrokerMock.Verify(broker =>
-                 broker.LogError(It.Is(IsSameExceptionAs(
+                 broker.LogErrorAsync(It.Is(IsSameExceptionAs(
                      expectedDecryptionCoordinationDependencyException))),
                          Times.Once);
 
@@ -144,7 +144,7 @@ namespace LHDS.Core.Tests.Unit.Services.Coordinations.Decryptions
                     Times.Once);
 
             this.loggingBrokerMock.Verify(broker =>
-                 broker.LogError(It.Is(IsSameExceptionAs(
+                 broker.LogErrorAsync(It.Is(IsSameExceptionAs(
                      expectedDecryptionCoordinationServiceException))),
                          Times.Once);
 

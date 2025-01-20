07665d63fc591a33df55ecab35cdb50242f180f3
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-01-20 16:24:44 +0000

    GetAllShouldNotHaveInvisibleApiAttribute -> PASS

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/Addresses/AddressesControllerTests.cs
@@ -0,0 +1,85 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Linq;
+using LHDS.AdminPortal.Api.Controllers;
+using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Foundations.Addresses.Exceptions;
+using LHDS.Core.Services.Foundations.Addresses;
+using Moq;
+using RESTFulSense.Controllers;
+using Tynamix.ObjectFiller;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.Addresses
+{
+    public partial class AddressesControllerTests : RESTFulController
+    {
+        private readonly Mock<IAddressService> addressServiceMock;
+        private readonly AddressesController addressesController;
+
+        public AddressesControllerTests()
+        {
+            addressServiceMock = new Mock<IAddressService>();
+            addressesController = new AddressesController(addressServiceMock.Object);
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static string GetRandomStringWithLengthOf(int length)
+        {
+            string result = new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
+            return result.Length > length ? result.Substring(0, length) : result;
+        }
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static IQueryable<Address> CreateRandomAddresses()
+        {
+            return CreateAddressFiller()
+                .Create(count: GetRandomNumber())
+                    .AsQueryable();
+        }
+
+        private static Filler<Address> CreateAddressFiller()
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.UtcNow;
+            string user = Guid.NewGuid().ToString();
+            var filler = new Filler<Address>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(accessAudit => accessAudit.CreatedBy).Use(user)
+                .OnProperty(accessAudit => accessAudit.UpdatedBy).Use(user);
+
+            return filler;
+        }
+
+        public static TheoryData<Xeption> ServerExceptions()
+        {
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+
+            return new TheoryData<Xeption>
+            {
+                new AddressDependencyException(
+                    message: someMessage,
+                    innerException: someInnerException),
+
+                new AddressServiceException(
+                    message: someMessage,
+                    innerException: someInnerException)
+            };
+        }
+    }
+}

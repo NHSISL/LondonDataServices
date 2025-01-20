f38abe0ed0e7727d38e9af674e99fb092160934e
Hassan Raza <h_raza110@outlook.com>
2025-01-20 11:58:35 +0000

    WIP

--- LHDS.Core.Tests.Unit/Services/Foundations/Suppliers/SupplierServiceTests.Exceptions.RetrieveAll.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/Suppliers/SupplierServiceTests.Exceptions.RetrieveAll.cs
@@ -100,7 +100,7 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.Suppliers
                     Times.Once);
 
             this.loggingBrokerMock.Verify(broker =>
-                broker.LogError(It.Is(SameExceptionAs(
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
                     expectedSupplierServiceException))),
                         Times.Once);
 

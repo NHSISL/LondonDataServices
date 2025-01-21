4d15905f2e421a1d545377d6a69ccd100f777109
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-01-21 11:55:14 +0000

    ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/Addresses/AddressesContollerTests.Exceptions.Post.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/Addresses/AddressesContollerTests.Exceptions.Post.cs
@@ -8,6 +8,7 @@ using LHDS.Core.Models.Foundations.Addresses.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
 using Xeptions;
 using Xunit;
 
@@ -37,6 +38,38 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.Addresses
             this.addressServiceMock.Setup(service =>
                 service.AddAddressAsync(It.IsAny<Address>()))
                     .ThrowsAsync(addressValidationException);
+
+            // when
+            ActionResult<Address> actualActionResult =
+                await this.addressesController.PostAddressAsync(someAddress);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.addressServiceMock.Verify(service =>
+                service.AddAddressAsync(It.IsAny<Address>()),
+                    Times.Once);
+
+            this.addressServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync(
+            Xeption validationException)
+        {
+            // given
+            Address someAddress = CreateRandomAddress();
+
+            InternalServerErrorObjectResult expectedBadRequestObjectResult =
+                InternalServerError(validationException);
+
+            var expectedActionResult =
+                new ActionResult<Address>(expectedBadRequestObjectResult);
+
+            this.addressServiceMock.Setup(service =>
+                service.AddAddressAsync(It.IsAny<Address>()))
+                    .ThrowsAsync(validationException);
 
             // when
             ActionResult<Address> actualActionResult =

24f4a78c6e87a4ad018f2159be190917e303918f
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-01-21 11:57:23 +0000

    ShouldReturnConflictOnPostIfAlreadyExistsAddressErrorOccurredAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/Addresses/AddressesContollerTests.Exceptions.Post.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/Addresses/AddressesContollerTests.Exceptions.Post.cs
@@ -2,6 +2,7 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using LHDS.Core.Models.Foundations.Addresses;
 using LHDS.Core.Models.Foundations.Addresses.Exceptions;
@@ -70,6 +71,48 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.Addresses
             this.addressServiceMock.Setup(service =>
                 service.AddAddressAsync(It.IsAny<Address>()))
                     .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Address> actualActionResult =
+                await this.addressesController.PostAddressAsync(someAddress);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.addressServiceMock.Verify(service =>
+                service.AddAddressAsync(It.IsAny<Address>()),
+                    Times.Once);
+
+            this.addressServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnConflictOnPostIfAlreadyExistsAddressErrorOccurredAsync()
+        {
+            // given
+            Address someAddress = CreateRandomAddress();
+            var someInnerException = new Exception();
+            string someMessage = GetRandomString();
+
+            var alreadyExistsAddressException =
+                new AlreadyExistsAddressException(
+                    message: someMessage,
+                    innerException: someInnerException);
+
+            var addressDependencyValidationException =
+                new AddressDependencyValidationException(
+                    message: someMessage,
+                    innerException: alreadyExistsAddressException);
+
+            ConflictObjectResult expectedConflictObjectResult =
+                Conflict(alreadyExistsAddressException);
+
+            var expectedActionResult =
+                new ActionResult<Address>(expectedConflictObjectResult);
+
+            this.addressServiceMock.Setup(service =>
+                service.AddAddressAsync(It.IsAny<Address>()))
+                    .ThrowsAsync(addressDependencyValidationException);
 
             // when
             ActionResult<Address> actualActionResult =

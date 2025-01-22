152824a9617cd38ea74b25b29d05619541b0a5ec
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-01-22 11:58:44 +0000

    GetAllShouldHaveRoleAttributeWithRoles -> PASS

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/DataSets/DataSetsControllerTests.Security.GetAll.cs
@@ -0,0 +1,84 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Attrify.Attributes;
+using FluentAssertions;
+using LHDS.AdminPortal.Api.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.DataSets
+{
+    public partial class DataSetsControllerTests
+    {
+        [Fact]
+        public void GetAllShouldHaveRoleAttributeWithRoles()
+        {
+            // given
+            var controllerType = typeof(DataSetsController);
+            var methodInfo = controllerType.GetMethod("Get");
+            Type attributeType = typeof(AuthorizeAttribute);
+            string attributeProperty = "Roles";
+
+            List<string> expectedAttributeValues = new List<string>
+            {
+                "ISL.LDS.AdminSpa.Administrators",
+                "ISL.LDS.AdminSpa.Configurations",
+                "ISL.LDS.AdminSpa.ReadOnly"
+            };
+
+            // when
+            var methodAttribute = methodInfo?
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var controllerAttribute = controllerType
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var attribute = methodAttribute ?? controllerAttribute;
+
+            // then
+            attribute.Should().NotBeNull();
+
+            var actualAttributeValue = attributeType
+                .GetProperty(attributeProperty)?
+                .GetValue(attribute) as string ?? string.Empty;
+
+            var actualAttributeValues = actualAttributeValue?
+                .Split(',')
+                .Select(role => role.Trim())
+                .Where(role => !string.IsNullOrEmpty(role))
+                .ToList();
+
+            actualAttributeValues.Should().BeEquivalentTo(expectedAttributeValues);
+        }
+
+        [Fact]
+        public void GetAllShouldNotHaveInvisibleApiAttribute()
+        {
+            // given
+            var controllerType = typeof(AddressesController);
+            var methodInfo = controllerType.GetMethod("Get");
+            Type attributeType = typeof(InvisibleApiAttribute);
+
+            // when
+            var methodAttribute = methodInfo?
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var controllerAttribute = controllerType
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var attribute = methodAttribute ?? controllerAttribute;
+
+            // then
+            attribute.Should().BeNull();
+        }
+    }
+}

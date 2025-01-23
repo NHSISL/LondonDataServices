2b080c0583b1a7b5c1baf5adedae6aa593931d54
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-01-23 16:11:09 +0000

    DeleteShouldHaveRoleAttributeWithRoles -> PASS

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/IngestionTrackings/IngestionTrackingsControllerTests.Security.Delete.cs
@@ -0,0 +1,83 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Attrify.Attributes;
+using FluentAssertions;
+using LHDS.AdminPortal.Api.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.IngestionTrackings
+{
+    public partial class IngestionTrackingsControllerTests
+    {
+        [Fact]
+        public void DeleteShouldHaveRoleAttributeWithRoles()
+        {
+            // given
+            var controllerType = typeof(IngestionTrackingsController);
+            var methodInfo = controllerType.GetMethod("DeleteIngestionTrackingByIdAsync");
+            Type attributeType = typeof(AuthorizeAttribute);
+            string attributeProperty = "Roles";
+
+            List<string> expectedAttributeValues = new List<string>
+            {
+                "ISL.LDS.AdminSpa.Administrators",
+                "ISL.LDS.AdminSpa.IngestionTracking"
+            };
+
+            // when
+            var methodAttribute = methodInfo?
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var controllerAttribute = controllerType
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var attribute = methodAttribute ?? controllerAttribute;
+
+            // then
+            attribute.Should().NotBeNull();
+
+            var actualAttributeValue = attributeType
+                .GetProperty(attributeProperty)?
+                .GetValue(attribute) as string ?? string.Empty;
+
+            var actualAttributeValues = actualAttributeValue?
+                .Split(',')
+                .Select(role => role.Trim())
+                .Where(role => !string.IsNullOrEmpty(role))
+                .ToList();
+
+            actualAttributeValues.Should().BeEquivalentTo(expectedAttributeValues);
+        }
+
+        [Fact]
+        public void DeleteShouldNotHaveInvisibleApiAttribute()
+        {
+            // given
+            var controllerType = typeof(IngestionTrackingsController);
+            var methodInfo = controllerType.GetMethod("DeleteIngestionTrackingByIdAsync");
+            Type attributeType = typeof(InvisibleApiAttribute);
+
+            // when
+            var methodAttribute = methodInfo?
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var controllerAttribute = controllerType
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var attribute = methodAttribute ?? controllerAttribute;
+
+            // then
+            attribute.Should().BeNull();
+        }
+    }
+}

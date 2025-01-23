13ed90aed49b56af31e6f6ed1dbc0ef82c0a70a5
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-23 15:22:36 +0000

    GetByIdShouldHaveRoleAttributeWithRoles -> FAIL

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/TerminologyArtifacts/TerminologyArtifactsControllerTests.Security.GetById.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.AdminPortal.Api.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.TerminologyArtifacts
+{
+    public partial class TerminologyArtifactsControllerTests
+    {
+        [Fact]
+        public void GetByIdShouldHaveRoleAttributeWithRoles()
+        {
+            // given 
+            var controllerType = typeof(TerminologyArtifactsController);
+            var methodInfo = controllerType.GetMethod("GetTerminologyArtifactByIdAsync");
+            Type attributeType = typeof(AuthorizeAttribute);
+            string attributeProperty = "Roles";
+            List<string> expectedAttributeValues = new List<string>()
+            {
+                "ISL.LDS.AdminSpa.Configurations", "ISL.LDS.AdminSpa.Administrators"
+            };
+
+            // when
+            var methodAttribute = methodInfo?
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var controllerAttribute = controllerType
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var attribute = methodAttribute ?? controllerAttribute;
+
+            // then
+            attribute.Should().NotBeNull();
+
+            var actualAttributeValue = attributeType
+                .GetProperty(attributeProperty)?
+                .GetValue(attribute) as string ?? string.Empty;
+
+            var actualAttributeValues = actualAttributeValue?
+                .Split(',')
+                .Select(role => role.Trim())
+                .Where(role => !string.IsNullOrEmpty(role))
+                .ToList();
+
+            actualAttributeValues.Should().BeEquivalentTo(expectedAttributeValues);
+        }
+    }
+}

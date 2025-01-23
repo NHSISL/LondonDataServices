7e1c2748bfb8dc28cd47518c5d0f262855021283
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-01-23 15:23:08 +0000

    PostShouldNotHaveInvisibleApiAttribute -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/IngestionTrackings/IngestionTrackingsControllerTests.Security.Post.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/IngestionTrackings/IngestionTrackingsControllerTests.Security.Post.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Attrify.Attributes;
 using FluentAssertions;
 using LHDS.AdminPortal.Api.Controllers;
 using Microsoft.AspNetCore.Authorization;
@@ -54,6 +55,29 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.IngestionTrackings
                 .ToList();
 
             actualAttributeValues.Should().BeEquivalentTo(expectedAttributeValues);
+        }
+
+        [Fact]
+        public void PostShouldNotHaveInvisibleApiAttribute()
+        {
+            // given
+            var controllerType = typeof(IngestionTrackingsController);
+            var methodInfo = controllerType.GetMethod("PostDataTypeAsync");
+            Type attributeType = typeof(InvisibleApiAttribute);
+
+            // when
+            var methodAttribute = methodInfo?
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var controllerAttribute = controllerType
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var attribute = methodAttribute ?? controllerAttribute;
+
+            // then
+            attribute.Should().BeNull();
         }
     }
 }

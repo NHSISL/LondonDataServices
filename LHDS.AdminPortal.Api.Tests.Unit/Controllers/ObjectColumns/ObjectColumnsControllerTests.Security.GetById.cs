54a07d1c36a0f1d28cc010f0def0272820456ee5
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-23 15:58:38 +0000

    CODE RUB: Added roles to endpoints

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/ObjectColumns/ObjectColumnsControllerTests.Security.GetById.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/ObjectColumns/ObjectColumnsControllerTests.Security.GetById.cs
@@ -16,6 +16,47 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.ObjectColumns
     public partial class ObjectColumnsControllerTests
     {
         [Fact]
+        public void GetByIdShouldHaveRoleAttributeWithRoles()
+        {
+            // given 
+            var controllerType = typeof(ObjectColumnsController);
+            var methodInfo = controllerType.GetMethod("GetObjectColumnByIdAsync");
+            Type attributeType = typeof(AuthorizeAttribute);
+            string attributeProperty = "Roles";
+
+            List<string> expectedAttributeValues = new List<string>()
+            {
+                "ISL.LDS.AdminSpa.Administrators", "ISL.LDS.AdminSpa.Configurations"
+            };
+
+            // when
+            var methodAttribute = methodInfo?
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var controllerAttribute = controllerType
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var attribute = methodAttribute ?? controllerAttribute;
+
+            // then
+            attribute.Should().NotBeNull();
+
+            var actualAttributeValue = attributeType
+                .GetProperty(attributeProperty)?
+                .GetValue(attribute) as string ?? string.Empty;
+
+            var actualAttributeValues = actualAttributeValue?
+                .Split(',')
+                .Select(role => role.Trim())
+                .Where(role => !string.IsNullOrEmpty(role))
+                .ToList();
+
+            actualAttributeValues.Should().BeEquivalentTo(expectedAttributeValues);
+        }
+
+        [Fact]
         public void GetIdShouldHaveInvisibleApiAttribute()
         {
             // Given

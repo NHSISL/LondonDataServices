4e8e8106319ad89866ed762d17e0d7661245baf1
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-24 13:32:28 +0000

    CODE RUB: Added missing security tests

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/TerminologyPolls/TerminologyPollsControllerTests.Security.GetAll.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/TerminologyPolls/TerminologyPollsControllerTests.Security.GetAll.cs
@@ -16,6 +16,30 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.TerminologyPolls
     public partial class TerminologyPollsControllerTests
     {
         [Fact]
+        public void GetAllShouldNotHaveRoleAttribute()
+        {
+            // given 
+            var controllerType = typeof(TerminologyPollsController);
+            var methodInfo = controllerType.GetMethod("GetAllTerminologyPollsAsync");
+
+            Type attributeType = typeof(AuthorizeAttribute);
+
+            // when
+            var methodAttribute = methodInfo?
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var controllerAttribute = controllerType
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var attribute = methodAttribute ?? controllerAttribute;
+
+            // then
+            attribute.Should().BeNull();
+        }
+
+        [Fact]
         public void GetShouldHaveInvisibleApiAttribute()
         {
             // Given

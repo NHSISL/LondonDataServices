8794a4b11063e8c949509f733ecf3e995fdd2803
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-24 14:25:35 +0000

    CODE RUB: Add SubscriberAgreementsControllerTests file

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/SubscriberAgreement/DataTypesControllerTests.cs
@@ -0,0 +1,24 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LHDS.AdminPortal.Api.Controllers;
+using LHDS.Core.Services.Foundations.DataTypes;
+using LHDS.Core.Services.Foundations.SubscriberAgreements;
+using Moq;
+using RESTFulSense.Controllers;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.DataTypes
+{
+    public partial class SubscriberAgreementControllerTests : RESTFulController
+    {
+        private readonly Mock<ISubscriberAgreementService> subscriberAgreementsServiceMock;
+        private readonly SubscriberAgreementsController subscriberAgreementsController;
+
+        public SubscriberAgreementControllerTests()
+        {
+            this.subscriberAgreementsServiceMock = new Mock<ISubscriberAgreementService>();
+            this.subscriberAgreementsController = new SubscriberAgreementsController(this.subscriberAgreementsServiceMock.Object);
+        }
+    }
+}

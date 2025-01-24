ef524fb244f354f5055d17478f6a3e6dd2662176
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-01-24 13:50:09 +0000

    WIP

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/IngestionTrackingAudits/IngestionTrackingAuditsControllerTests.Security.Post.cs
@@ -0,0 +1,12 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.IngestionTrackingAudits
+{
+    public partial class IngestionTrackingAuditsControllerTests
+    {
+
+    }
+}
+
---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/IngestionTrackingAudits/IngestionTrackingAuditsControllerTests.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LHDS.AdminPortal.Api.Controllers;
+using LHDS.Core.Services.Foundations.IngestionTrackingAudits;
+using Moq;
+using RESTFulSense.Controllers;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.IngestionTrackingAudits
+{
+    public partial class IngestionTrackingAuditsControllerTests : RESTFulController
+    {
+        private readonly Mock<IIngestionTrackingAuditService> ingestionTrackingAuditServiceMock;
+        private readonly IngestionTrackingAuditsController ingestionTrackingAuditsController;
+
+        public IngestionTrackingAuditsControllerTests()
+        {
+            this.ingestionTrackingAuditServiceMock = new Mock<IIngestionTrackingAuditService>();
+
+            this.ingestionTrackingAuditsController = new IngestionTrackingAuditsController(
+                this.ingestionTrackingAuditServiceMock.Object);
+        }
+    }
+}
+

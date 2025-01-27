1f8e901e9caf413549f5999a63fd25175270abfb
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-27 15:53:07 +0000

    PutShouldNotHaveInvisibleApiAttribute -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/PdsAudit/PdsAuditControllerTests.Put.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/PdsAudit/PdsAuditControllerTests.Put.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Attrify.Attributes;
 using FluentAssertions;
 using LHDS.AdminPortal.Api.Controllers;
 using Microsoft.AspNetCore.Authorization;
@@ -56,6 +57,29 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.PdsAudit
                 .ToList();
 
             actualAttributeValues.Should().BeEquivalentTo(expectedAttributeValues);
+        }
+
+        [Fact]
+        public void PutShouldNotHaveInvisibleApiAttribute()
+        {
+            // given
+            var controllerType = typeof(PdsAuditsController);
+            var methodInfo = controllerType.GetMethod("PutPdsAuditAsync");
+            Type attributeType = typeof(InvisibleApiAttribute);
+
+            // when
+            var methodAttribute = methodInfo?
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var controllerAttribute = controllerType
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var attribute = methodAttribute ?? controllerAttribute;
+
+            // then
+            attribute.Should().BeNull();
         }
     }
 }

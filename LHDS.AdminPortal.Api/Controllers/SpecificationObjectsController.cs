82336e386cdf1e6da2527e340b385b4cf45507fa
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-28 11:12:13 +0000

    GetAllShouldHaveRoleAttributeWithRoles -> FAIL

--- LHDS.AdminPortal.Api/Controllers/SpecificationObjectsController.cs
+++ LHDS.AdminPortal.Api/Controllers/SpecificationObjectsController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using LHDS.Core.Models.Foundations.SpecificationObjects;
 using LHDS.Core.Models.Foundations.SpecificationObjects.Exceptions;
 using LHDS.Core.Services.Foundations.SpecificationObjects;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Query;
 using RESTFulSense.Controllers;
@@ -17,6 +18,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace LHDS.AdminPortal.Api.Controllers
 {
+    [Authorize(Roles = "ISL.LDS.AdminSpa.Administrators, ISL.LDS.AdminSpa.Configurations")]
     [ApiController]
     [Route("api/[controller]")]
     public class SpecificationObjectsController : RESTFulController

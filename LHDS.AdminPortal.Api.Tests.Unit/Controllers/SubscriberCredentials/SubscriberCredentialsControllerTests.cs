ae72cda898b05b0a6c08916f91d6f42d945fffba
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-28 11:51:26 +0000

    GetAllShouldHaveRoleAttributeWithRoles -> FAIL

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/SubscriberCredentials/SubscriberCredentialsControllerTests.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LHDS.AdminPortal.Api.Controllers;
+using LHDS.Core.Services.Foundations.PdsAudits;
+using LHDS.Core.Services.Orchestrations.SubscriberCredentials;
+using Moq;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.SubscriberCredentials
+{
+    public partial class SubscriberCredentialsControllerTests
+    {
+        private readonly Mock<ISubscriberCredentialOrchestration> subscriberCredentialOrchestrationMock;
+        private readonly SubscriberCredentialsController subscriberCredentialController;
+
+        public SubscriberCredentialsControllerTests()
+        {
+            this.subscriberCredentialOrchestrationMock = new Mock<ISubscriberCredentialOrchestration>();
+            
+            this.subscriberCredentialController = new SubscriberCredentialsController(
+                this.subscriberCredentialOrchestrationMock.Object);
+        }
+    }
+}

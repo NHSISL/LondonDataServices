41e4ac6424e606a33a8f28b5fcb01e2b4e0c1128
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-29 10:20:33 +0000

    ShouldReturnDataSetsOnGetAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/DataSets/DataSetsControllerTests.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/DataSets/DataSetsControllerTests.cs
@@ -2,10 +2,14 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
+using System.Linq;
 using LHDS.AdminPortal.Api.Controllers;
+using LHDS.Core.Models.Foundations.DataSets;
 using LHDS.Core.Services.Foundations.DataSets;
 using Moq;
 using RESTFulSense.Controllers;
+using Tynamix.ObjectFiller;
 
 namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.DataSets
 {
@@ -18,6 +22,31 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.DataSets
         {
             this.dataSetServiceMock = new Mock<IDataSetService>();
             this.dataSetsController = new DataSetsController(this.dataSetServiceMock.Object);
+        }
+
+        private static int GetRandomNumber() =>
+           new IntRange(min: 2, max: 10).GetValue();
+
+        private static IQueryable<DataSet> CreateRandomDataSet()
+        {
+            return CreateDataSetFiller()
+                .Create(count: GetRandomNumber())
+                    .AsQueryable();
+        }
+
+        private static Filler<DataSet> CreateDataSetFiller()
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.UtcNow;
+            string user = Guid.NewGuid().ToString();
+            var filler = new Filler<DataSet>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(accessAudit => accessAudit.CreatedBy).Use(user)
+                .OnProperty(accessAudit => accessAudit.UpdatedBy).Use(user);
+
+            return filler;
         }
     }
 }

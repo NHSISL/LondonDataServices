569376666636e2a0fbaa993fadfa687167f31003
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-29 11:23:23 +0000

    ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/DataSets/DataSetsControllerTests.Exceptions.Post.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/DataSets/DataSetsControllerTests.Exceptions.Post.cs
@@ -12,6 +12,7 @@ using LHDS.Core.Models.Foundations.DataSets.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
 using Xeptions;
 using Xunit;
 
@@ -41,6 +42,38 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.DataSets
             this.dataSetServiceMock.Setup(service =>
                 service.AddDataSetAsync(It.IsAny<DataSet>()))
                     .ThrowsAsync(DataSetValidationException);
+
+            // when
+            ActionResult<DataSet> actualActionResult =
+                await this.dataSetsController.PostDataSetAsync(someDataSet);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.dataSetServiceMock.Verify(service =>
+                service.AddDataSetAsync(It.IsAny<DataSet>()),
+                    Times.Once);
+
+            this.dataSetServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync(
+            Xeption validationException)
+        {
+            // given
+            DataSet someDataSet = CreateRandomDataSet();
+
+            InternalServerErrorObjectResult expectedBadRequestObjectResult =
+                InternalServerError(validationException);
+
+            var expectedActionResult =
+                new ActionResult<DataSet>(expectedBadRequestObjectResult);
+
+            this.dataSetServiceMock.Setup(service =>
+                service.AddDataSetAsync(It.IsAny<DataSet>()))
+                    .ThrowsAsync(validationException);
 
             // when
             ActionResult<DataSet> actualActionResult =

f59de3b99d2eef8f42fc3c7b53f45b373ff9d13e
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-29 15:18:05 +0000

    ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/DataSetSpecifications/DataSetSpecificationsControllerTests.Exceptions.Post.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/DataSetSpecifications/DataSetSpecificationsControllerTests.Exceptions.Post.cs
@@ -12,6 +12,7 @@ using LHDS.Core.Models.Foundations.DataSetSpecifications.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
 using Xeptions;
 using Xunit;
 
@@ -45,6 +46,38 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.DataSetSpecifications
             // when
             ActionResult<DataSetSpecification> actualActionResult =
                 await this.DataSetSpecificationController.PostDataSetSpecificationAsync(someDataSetSpecifications);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.DataSetSpecificationServiceMock.Verify(service =>
+                service.AddDataSetSpecificationAsync(It.IsAny<DataSetSpecification>()),
+                    Times.Once);
+
+            this.DataSetSpecificationServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync(
+            Xeption validationException)
+        {
+            // given
+            DataSetSpecification someDataSet = CreateRandomDataSetSpecification();
+
+            InternalServerErrorObjectResult expectedBadRequestObjectResult =
+                InternalServerError(validationException);
+
+            var expectedActionResult =
+                new ActionResult<DataSetSpecification>(expectedBadRequestObjectResult);
+
+            this.DataSetSpecificationServiceMock.Setup(service =>
+                service.AddDataSetSpecificationAsync(It.IsAny<DataSetSpecification>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<DataSetSpecification> actualActionResult =
+                await this.DataSetSpecificationController.PostDataSetSpecificationAsync(someDataSet);
 
             // then
             actualActionResult.ShouldBeEquivalentTo(expectedActionResult);

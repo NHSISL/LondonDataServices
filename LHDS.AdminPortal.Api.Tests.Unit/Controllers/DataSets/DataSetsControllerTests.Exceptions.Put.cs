d065ee42fd788afc5b75afc29e732d4229d7ac79
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-29 11:28:06 +0000

    ShouldReturnNotFoundOnPutIfItemDoesNotExistAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/DataSets/DataSetsControllerTests.Exceptions.Put.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/DataSets/DataSetsControllerTests.Exceptions.Put.cs
@@ -55,5 +55,46 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.DataSets
 
             this.dataSetServiceMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldReturnNotFoundOnPutIfItemDoesNotExistAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            string someMessage = GetRandomString();
+            DataSet someDataSet = CreateRandomDataSet();
+
+            var notFoundDataSetException =
+                new NotFoundDataSetException(
+                    dataSetId: someId);
+
+            var DataSetValidationException =
+                new DataSetValidationException(
+                    message: someMessage,
+                    innerException: notFoundDataSetException);
+
+            NotFoundObjectResult expectedNotFoundObjectResult =
+                NotFound(notFoundDataSetException);
+
+            var expectedActionResult =
+                new ActionResult<DataSet>(expectedNotFoundObjectResult);
+
+            this.dataSetServiceMock.Setup(service =>
+                service.ModifyDataSetAsync(It.IsAny<DataSet>()))
+                    .ThrowsAsync(DataSetValidationException);
+
+            // when
+            ActionResult<DataSet> actualActionResult =
+                await this.dataSetsController.PutDataSetAsync(someDataSet);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.dataSetServiceMock.Verify(service =>
+                service.ModifyDataSetAsync(It.IsAny<DataSet>()),
+                    Times.Once);
+
+            this.dataSetServiceMock.VerifyNoOtherCalls();
+        }
     }
 }

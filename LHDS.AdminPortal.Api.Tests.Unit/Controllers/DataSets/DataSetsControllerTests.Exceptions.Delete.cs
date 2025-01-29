d907078ee89ba2b178af1df0ce16555babdf06c8
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-29 11:36:05 +0000

    ShouldReturnNotFoundOnDeleteIfItemDoesNotExistAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/DataSets/DataSetsControllerTests.Exceptions.Delete.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/DataSets/DataSetsControllerTests.Exceptions.Delete.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LHDS.Core.Models.Foundations.DataSets;
+using LHDS.Core.Models.Foundations.DataSets.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
@@ -34,6 +35,46 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.DataSets
             this.dataSetServiceMock.Setup(service =>
                 service.RemoveDataSetByIdAsync(It.IsAny<Guid>()))
                     .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<DataSet> actualActionResult =
+                await this.dataSetsController.DeleteDataSetByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.dataSetServiceMock.Verify(service =>
+                service.RemoveDataSetByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.dataSetServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnNotFoundOnDeleteIfItemDoesNotExistAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            string someMessage = GetRandomString();
+
+            var notFoundDataSetException =
+                new NotFoundDataSetException(
+                    dataSetId: someId);
+
+            var DataSetValidationException =
+                new DataSetValidationException(
+                    message: someMessage,
+                    innerException: notFoundDataSetException);
+
+            NotFoundObjectResult expectedNotFoundObjectResult =
+                NotFound(notFoundDataSetException);
+
+            var expectedActionResult =
+                new ActionResult<DataSet>(expectedNotFoundObjectResult);
+
+            this.dataSetServiceMock.Setup(service =>
+                service.RemoveDataSetByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(DataSetValidationException);
 
             // when
             ActionResult<DataSet> actualActionResult =

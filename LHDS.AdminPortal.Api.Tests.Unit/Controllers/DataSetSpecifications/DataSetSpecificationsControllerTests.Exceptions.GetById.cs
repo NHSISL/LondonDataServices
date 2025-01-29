24dcae2af8b048a3aeae59f7dc9a485c34618b14
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-29 14:43:29 +0000

    ShouldReturnInternalServerErrorOnGetByIdIfServerErrorOccurredAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/DataSetSpecifications/DataSetSpecificationsControllerTests.Exceptions.GetById.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/DataSetSpecifications/DataSetSpecificationsControllerTests.Exceptions.GetById.cs
@@ -12,6 +12,7 @@ using LHDS.Core.Models.Foundations.DataSetSpecifications.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
 using Xeptions;
 using Xunit;
 
@@ -52,6 +53,37 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.DataSetSpecifications
             this.DataSetSpecificationServiceMock.Verify(service =>
                 service.RetrieveDataSetSpecificationByIdAsync(someId),
                     Times.Once);
+
+            this.DataSetSpecificationServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnGetByIdIfServerErrorOccurredAsync(Xeption serverException)
+        {
+            // given 
+            Guid someId = Guid.NewGuid();
+
+            InternalServerErrorObjectResult expectedInternalServerErrorObjectResult =
+                InternalServerError(serverException);
+
+            var expectedActionResult =
+                new ActionResult<DataSetSpecification>(expectedInternalServerErrorObjectResult);
+
+            this.DataSetSpecificationServiceMock.Setup(service =>
+                service.RetrieveDataSetSpecificationByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(serverException);
+
+            // when
+            ActionResult<DataSetSpecification> actualActionResult =
+                await this.DataSetSpecificationController.GetDataSetSpecificationByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.DataSetSpecificationServiceMock.Verify(service =>
+                service.RetrieveDataSetSpecificationByIdAsync(It.IsAny<Guid>()),
+                    Times.Once());
 
             this.DataSetSpecificationServiceMock.VerifyNoOtherCalls();
         }

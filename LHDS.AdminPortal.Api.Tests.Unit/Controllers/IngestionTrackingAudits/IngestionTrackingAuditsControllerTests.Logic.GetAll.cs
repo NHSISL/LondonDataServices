2b6b2d0d60242fe073e06206795187c48f15f130
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-30 13:58:20 +0000

    ShouldReturnIngestionTrackingAuditsOnGetAsync -> PASS

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/IngestionTrackingAudits/IngestionTrackingAuditsControllerTests.Logic.GetAll.cs
@@ -0,0 +1,51 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LHDS.Core.Models.Foundations.IngestionTrackingAudits;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.IngestionTrackingAudits
+{
+    public partial class IngestionTrackingAuditsControllerTests
+    {
+        [Fact]
+        public async Task ShouldReturnIngestionTrackingAuditsOnGetAsync()
+        {
+            // given 
+            IQueryable<IngestionTrackingAudit> randomIngestionTrackingAudit = CreateRandomIngestionTrackingAudits();
+            IQueryable<IngestionTrackingAudit> storageIngestionTrackingAudit = randomIngestionTrackingAudit.DeepClone();
+            IQueryable<IngestionTrackingAudit> expectedIngestionTrackingAudit = storageIngestionTrackingAudit.DeepClone();
+            var expectedObjectResult = new OkObjectResult(expectedIngestionTrackingAudit);
+
+            var expectedActionResult =
+                new ActionResult<IQueryable<IngestionTrackingAudit>>(expectedObjectResult);
+
+            this.ingestionTrackingAuditServiceMock.Setup(service =>
+                service.RetrieveAllIngestionTrackingAuditsAsync())
+                    .ReturnsAsync(expectedIngestionTrackingAudit);
+
+            // when
+            ActionResult<IQueryable<IngestionTrackingAudit>> actualActionResult =
+                await this.ingestionTrackingAuditsController.Get();
+
+            // then
+            actualActionResult.Should().BeEquivalentTo(expectedActionResult);
+
+            this.ingestionTrackingAuditServiceMock.Verify(service =>
+                service.RetrieveAllIngestionTrackingAuditsAsync(),
+                    Times.Once());
+
+            this.ingestionTrackingAuditServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}

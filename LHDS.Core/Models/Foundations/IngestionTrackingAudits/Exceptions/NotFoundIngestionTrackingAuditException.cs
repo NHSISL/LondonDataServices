428e2dce07b5bae6814b6b2281ab9a7e7b0c9691
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-30 14:47:30 +0000

    ShouldReturnNotFoundOnGetByIdIfItemDoesNotExistAsync -> PASS

--- LHDS.Core/Models/Foundations/IngestionTrackingAudits/Exceptions/NotFoundIngestionTrackingAuditException.cs
+++ LHDS.Core/Models/Foundations/IngestionTrackingAudits/Exceptions/NotFoundIngestionTrackingAuditException.cs
@@ -2,14 +2,15 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using Xeptions;
 
 namespace LHDS.Core.Models.Foundations.IngestionTrackingAudits.Exceptions
 {
     public class NotFoundIngestionTrackingAuditException : Xeption
     {
-        public NotFoundIngestionTrackingAuditException(string message)
-            : base(message)
+        public NotFoundIngestionTrackingAuditException(Guid ingestionTrackingAuditId)
+            : base(message: $"Couldn't find IngestionTrackingAudit with Id: {ingestionTrackingAuditId}.")
         { }
     }
 }

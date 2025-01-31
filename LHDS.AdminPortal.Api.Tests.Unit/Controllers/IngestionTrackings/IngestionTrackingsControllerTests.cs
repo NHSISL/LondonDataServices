a26b2dbb1ffed5612dd770579f872754fc072c57
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-31 11:07:51 +0000

    ShouldReturnOkOnPutAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/IngestionTrackings/IngestionTrackingsControllerTests.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/IngestionTrackings/IngestionTrackingsControllerTests.cs
@@ -2,10 +2,17 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System.Linq;
+using System;
 using LHDS.AdminPortal.Api.Controllers;
+using LHDS.Core.Models.Foundations.IngestionTrackings.Exceptions;
 using LHDS.Core.Services.Foundations.IngestionTrackings;
 using Moq;
 using RESTFulSense.Controllers;
+using Tynamix.ObjectFiller;
+using Xeptions;
+using Xunit;
+using LHDS.Core.Models.Foundations.IngestionTrackings;
 
 namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.IngestionTrackings
 {
@@ -20,6 +27,67 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.IngestionTrackings
 
             this.ingestionTrackingsController = new IngestionTrackingsController(
                 this.ingestionTrackingServiceMock.Object);
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static int GetRandomNumber() =>
+           new IntRange(min: 2, max: 10).GetValue();
+
+        private static IQueryable<IngestionTracking> CreateRandomIngestionTrackings() =>
+            CreateIngestionTrackingFiller().Create(count: GetRandomNumber()).AsQueryable();
+
+        private static IngestionTracking CreateRandomIngestionTracking() =>
+            CreateIngestionTrackingFiller().Create();
+
+        private static Filler<IngestionTracking> CreateIngestionTrackingFiller()
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.UtcNow;
+            string user = Guid.NewGuid().ToString();
+            var filler = new Filler<IngestionTracking>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(accessAudit => accessAudit.CreatedBy).Use(user)
+                .OnProperty(accessAudit => accessAudit.UpdatedBy).Use(user);
+
+            return filler;
+        }
+
+        public static TheoryData<Xeption> ServerExceptions()
+        {
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+
+            return new TheoryData<Xeption>
+            {
+                new IngestionTrackingDependencyException(
+                    message: someMessage,
+                    innerException: someInnerException),
+
+                new IngestionTrackingServiceException(
+                    message: someMessage,
+                    innerException: someInnerException)
+            };
+        }
+
+        public static TheoryData<Xeption> ValidationExceptions()
+        {
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+
+            return new TheoryData<Xeption>
+            {
+                new IngestionTrackingValidationException(
+                    message: someMessage,
+                    innerException: someInnerException),
+
+                new IngestionTrackingDependencyValidationException(
+                    message: someMessage,
+                    innerException: someInnerException)
+            };
         }
     }
 }

71e4cdfd0c54a7c77b8eac8bed762e4667af09ec
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-31 11:25:01 +0000

    ShouldReturnNotFoundOnDeleteIfItemDoesNotExistAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/IngestionTrackings/IngestionTrackingsControllerTests.Exceptions.Delete.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/IngestionTrackings/IngestionTrackingsControllerTests.Exceptions.Delete.cs
@@ -8,9 +8,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
+using LHDS.Core.Models.Foundations.IngestionTrackings.Exceptions;
 using LHDS.Core.Models.Foundations.IngestionTrackings;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using RESTFulSense.Clients.Extensions;
 using Xeptions;
 using Xunit;
 
@@ -32,7 +34,7 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.IngestionTrackings
                 new ActionResult<IngestionTracking>(expectedBadRequestObjectResult);
 
             this.ingestionTrackingServiceMock.Setup(service =>
-                service.RemoveIngestionTrackingByIdAsync(It.IsAny<Guid>()))
+                service.RemoveIngestionTrackingByIdAsync(someId))
                     .ThrowsAsync(validationException);
 
             // when
@@ -43,7 +45,47 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.IngestionTrackings
             actualActionResult.Should().BeEquivalentTo(expectedActionResult);
 
             this.ingestionTrackingServiceMock.Verify(service =>
-                service.RemoveIngestionTrackingByIdAsync(It.IsAny<Guid>()),
+                service.RemoveIngestionTrackingByIdAsync(someId),
+                    Times.Once);
+
+            this.ingestionTrackingServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnNotFoundOnDeleteIfItemDoesNotExistAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            string someMessage = GetRandomString();
+
+            var notFoundIngestionTrackingException =
+                new NotFoundIngestionTrackingException(
+                    ingestionTrackingId: someId);
+
+            var IngestionTrackingValidationException =
+                new IngestionTrackingValidationException(
+                    message: someMessage,
+                    innerException: notFoundIngestionTrackingException);
+
+            NotFoundObjectResult expectedNotFoundObjectResult =
+                NotFound(notFoundIngestionTrackingException);
+
+            var expectedActionResult =
+                new ActionResult<IngestionTracking>(expectedNotFoundObjectResult);
+
+            this.ingestionTrackingServiceMock.Setup(service =>
+                service.RemoveIngestionTrackingByIdAsync(someId))
+                    .ThrowsAsync(IngestionTrackingValidationException);
+
+            // when
+            ActionResult<IngestionTracking> actualActionResult =
+                await this.ingestionTrackingsController.DeleteIngestionTrackingByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.ingestionTrackingServiceMock.Verify(service =>
+                service.RemoveIngestionTrackingByIdAsync(someId),
                     Times.Once);
 
             this.ingestionTrackingServiceMock.VerifyNoOtherCalls();

a37bddecc18c846ce4f99710a5fb62560b2622e8
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-31 11:48:23 +0000

    ShouldReturnConflictOnPostIfAlreadyExistsIngestionTrackingErrorOccurredAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/IngestionTrackings/IngestionTrackingsControllerTests.Exceptions.Post.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/IngestionTrackings/IngestionTrackingsControllerTests.Exceptions.Post.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.IngestionTrackings.Exceptions;
 using LHDS.Core.Models.Foundations.IngestionTrackings;
 using LHDS.Core.Models.Foundations.IngestionTrackings.Exceptions;
 using Microsoft.AspNetCore.Mvc;
@@ -84,6 +85,48 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.IngestionTrackings
 
             this.ingestionTrackingServiceMock.Verify(service =>
                 service.AddIngestionTrackingAsync(someIngestionTracking),
+                    Times.Once);
+
+            this.ingestionTrackingServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnConflictOnPostIfAlreadyExistsIngestionTrackingErrorOccurredAsync()
+        {
+            // given
+            IngestionTracking someIngestionTracking = CreateRandomIngestionTracking();
+            var someInnerException = new Exception();
+            string someMessage = GetRandomString();
+
+            var alreadyExistsIngestionTrackingException =
+                new AlreadyExistsIngestionTrackingException(
+                    message: someMessage,
+                    innerException: someInnerException);
+
+            var IngestionTrackingDependencyValidationException =
+                new IngestionTrackingDependencyValidationException(
+                    message: someMessage,
+                    innerException: alreadyExistsIngestionTrackingException);
+
+            ConflictObjectResult expectedConflictObjectResult =
+                Conflict(alreadyExistsIngestionTrackingException);
+
+            var expectedActionResult =
+                new ActionResult<IngestionTracking>(expectedConflictObjectResult);
+
+            this.ingestionTrackingServiceMock.Setup(service =>
+                service.AddIngestionTrackingAsync(It.IsAny<IngestionTracking>()))
+                    .ThrowsAsync(IngestionTrackingDependencyValidationException);
+
+            // when
+            ActionResult<IngestionTracking> actualActionResult =
+                await this.ingestionTrackingsController.PostIngestionTrackingAsync(someIngestionTracking);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.ingestionTrackingServiceMock.Verify(service =>
+                service.AddIngestionTrackingAsync(It.IsAny<IngestionTracking>()),
                     Times.Once);
 
             this.ingestionTrackingServiceMock.VerifyNoOtherCalls();

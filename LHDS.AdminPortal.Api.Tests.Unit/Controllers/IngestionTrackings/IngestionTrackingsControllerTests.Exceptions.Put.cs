897ac27b2b2b69070a84c9d7d0f2915ad892acd9
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-01-31 11:51:44 +0000

    ShouldReturnBadRequestOnPutIfValidationErrorOccurredAsync -> PASS

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/IngestionTrackings/IngestionTrackingsControllerTests.Exceptions.Put.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.IngestionTrackings;
+using LHDS.Core.Models.Foundations.IngestionTrackings.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.IngestionTrackings
+{
+    public partial class IngestionTrackingsControllerTests
+    {
+        [Fact]
+        public async Task ShouldReturnBadRequestOnPutIfValidationErrorOccurredAsync()
+        {
+            // given
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+            IngestionTracking someIngestionTracking = CreateRandomIngestionTracking();
+
+            var IngestionTrackingValidationException =
+                new IngestionTrackingValidationException(
+                    message: someMessage,
+                    innerException: someInnerException);
+
+            BadRequestObjectResult expectedBadRequestObjectResult =
+                BadRequest(IngestionTrackingValidationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<IngestionTracking>(expectedBadRequestObjectResult);
+
+            this.ingestionTrackingServiceMock.Setup(service =>
+                service.ModifyIngestionTrackingAsync(someIngestionTracking))
+                    .ThrowsAsync(IngestionTrackingValidationException);
+
+            // when
+            ActionResult<IngestionTracking> actualActionResult =
+                await this.ingestionTrackingsController.PutIngestionTrackingAsync(someIngestionTracking);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.ingestionTrackingServiceMock.Verify(service =>
+                service.ModifyIngestionTrackingAsync(someIngestionTracking),
+                    Times.Once);
+
+            this.ingestionTrackingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}

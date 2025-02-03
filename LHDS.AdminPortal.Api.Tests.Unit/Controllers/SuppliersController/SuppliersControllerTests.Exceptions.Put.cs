a52e08149f01c52b285b78f95359a8b2766c7a65
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-02-03 12:15:27 +0000

    ShouldReturnFailedDependencyOnPutIfInvalidSupplierReferenceAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/SuppliersController/SuppliersControllerTests.Exceptions.Put.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/SuppliersController/SuppliersControllerTests.Exceptions.Put.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Suppliers.Exceptions;
 using LHDS.Core.Models.Foundations.Suppliers;
 using LHDS.Core.Models.Foundations.Suppliers.Exceptions;
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +15,8 @@ using Moq;
 using RESTFulSense.Clients.Extensions;
 using Xeptions;
 using Xunit;
+using LHDS.Core.Models.Foundations.Suppliers.Exceptions;
+using RESTFulSense.Models;
 
 namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.Suppliers
 {
@@ -41,6 +44,89 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.Suppliers
             this.supplierServiceMock.Setup(service =>
                 service.ModifySupplierAsync(It.IsAny<Supplier>()))
                     .ThrowsAsync(SupplierValidationException);
+
+            // when
+            ActionResult<Supplier> actualActionResult =
+                await this.suppliersController.PutSupplierAsync(someSupplier);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.supplierServiceMock.Verify(service =>
+                service.ModifySupplierAsync(It.IsAny<Supplier>()),
+                    Times.Once);
+
+            this.supplierServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnNotFoundOnPutIfItemDoesNotExistAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            string someMessage = GetRandomString();
+            Supplier someSupplier = CreateRandomSupplier();
+
+            var notFoundSupplierException =
+                new NotFoundSupplierException(
+                    supplierId: someId);
+
+            var SupplierValidationException =
+                new SupplierValidationException(
+                    message: someMessage,
+                    innerException: notFoundSupplierException);
+
+            NotFoundObjectResult expectedNotFoundObjectResult =
+                NotFound(notFoundSupplierException);
+
+            var expectedActionResult =
+                new ActionResult<Supplier>(expectedNotFoundObjectResult);
+
+            this.supplierServiceMock.Setup(service =>
+                service.ModifySupplierAsync(It.IsAny<Supplier>()))
+                    .ThrowsAsync(SupplierValidationException);
+
+            // when
+            ActionResult<Supplier> actualActionResult =
+                await this.suppliersController.PutSupplierAsync(someSupplier);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.supplierServiceMock.Verify(service =>
+                service.ModifySupplierAsync(It.IsAny<Supplier>()),
+                    Times.Once);
+
+            this.supplierServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnFailedDependencyOnPutIfInvalidSupplierReferenceAsync()
+        {
+            // given
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+            Supplier someSupplier = CreateRandomSupplier();
+
+            var alreadyExistsSupplierException =
+                new InvalidSupplierReferenceException(
+                    message: someMessage,
+                    innerException: someInnerException);
+
+            var SupplierDependencyValidationException =
+                new SupplierDependencyValidationException(
+                    message: someMessage,
+                    innerException: alreadyExistsSupplierException);
+
+            FailedDependencyObjectResult expectedBadRequestObjectResult =
+                FailedDependency(SupplierDependencyValidationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<Supplier>(expectedBadRequestObjectResult);
+
+            this.supplierServiceMock.Setup(service =>
+                service.ModifySupplierAsync(It.IsAny<Supplier>()))
+                    .ThrowsAsync(SupplierDependencyValidationException);
 
             // when
             ActionResult<Supplier> actualActionResult =

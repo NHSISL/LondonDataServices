2f2b950fb0375b6b10cbd94fc681eae64cd0b7aa
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-02-03 15:51:41 +0000

    Revert "Delete DataTypesControllerTests.Logic.Delete.cs"
    This reverts commit 3f3ff9d0d6d980d0de752a247953f69f7b71adc3.

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/DataTypes/DataTypesControllerTests.Logic.Delete.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Force.DeepCloner;
+using LHDS.Core.Models.Foundations.DataTypes;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.DataTypes
+{
+    public partial class DataTypesControllerTests
+    {
+        [Fact]
+        public async Task ShouldReturnOkOnDeleteAsync()
+        {
+            // given
+            DataType randomDataType = CreateRandomDataType();
+            Guid inputId = randomDataType.Id;
+            DataType storageDataType = randomDataType.DeepClone();
+            DataType expectedDataType = storageDataType.DeepClone();
+
+            var expectedObjectResult =
+                new OkObjectResult(expectedDataType);
+
+            var expectedActionResult =
+                new ActionResult<DataType>(expectedObjectResult);
+
+            this.dataTypeServiceMock.Setup(service =>
+                service.RemoveDataTypeByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(storageDataType);
+
+            // when
+            ActionResult<DataType> actualActionResult =
+                await this.dataTypesController.DeleteDataTypeByIdAsync(inputId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.dataTypeServiceMock.Verify(service =>
+                service.RemoveDataTypeByIdAsync(It.IsAny<Guid>()),
+                   Times.Once);
+
+            this.dataTypeServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}

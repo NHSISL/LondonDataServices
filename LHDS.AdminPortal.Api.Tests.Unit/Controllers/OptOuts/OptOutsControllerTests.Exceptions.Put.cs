728370d71ed435bbe1aa9c50a3b142658924b651
Hassan Raza <h_raza110@outlook.com>
2025-02-04 16:55:55 +0000

    ShouldReturnConflictOnPutIfAlreadyExistsOptOutErrorOccurredAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/OptOuts/OptOutsControllerTests.Exceptions.Put.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/OptOuts/OptOutsControllerTests.Exceptions.Put.cs
@@ -19,7 +19,7 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.OptOuts
     public partial class OptOutsControllerTests
     {
         [Fact]
-        public async Task ShouldReturnBadRequestOnPostIfValidationErrorOccurredAsync()
+        public async Task ShouldReturnBadRequestOnPutIfValidationErrorOccurredAsync()
         {
             // given
             var someInnerException = new Xeption();
@@ -38,57 +38,107 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.OptOuts
                 new ActionResult<OptOut>(expectedBadRequestObjectResult);
 
             this.optOutProcessingServiceMock.Setup(service =>
-                service.RetrieveOrAddOptOutAsync(someOptOut))
+                service.AddOrModifyOptOutAsync(someOptOut))
                     .ThrowsAsync(optOutProcessingValidationException);
 
             // when
             ActionResult<OptOut> actualActionResult =
-                await this.optOutsController.PostOptOutAsync(someOptOut);
+                await this.optOutsController.PutOptOutAsync(someOptOut);
 
             // then
             actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
 
             this.optOutProcessingServiceMock.Verify(service =>
-                service.RetrieveOrAddOptOutAsync(someOptOut),
-                    Times.Once);
-
-            this.optOutProcessingServiceMock.VerifyNoOtherCalls();
-        }
-
-        [Theory]
-        [MemberData(nameof(ServerExceptions))]
-        public async Task ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync(
-            Xeption validationException)
-        {
-            // given
-            OptOut someOptOut = CreateRandomOptOut();
-
-            InternalServerErrorObjectResult expectedBadRequestObjectResult =
-                InternalServerError(validationException);
-
-            var expectedActionResult =
-                new ActionResult<OptOut>(expectedBadRequestObjectResult);
-
-            this.optOutProcessingServiceMock.Setup(service =>
-                service.RetrieveOrAddOptOutAsync(someOptOut))
-                    .ThrowsAsync(validationException);
-
-            // when
-            ActionResult<OptOut> actualActionResult =
-                await this.optOutsController.PostOptOutAsync(someOptOut);
-
-            // then
-            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
-
-            this.optOutProcessingServiceMock.Verify(service =>
-                service.RetrieveOrAddOptOutAsync(someOptOut),
+                service.AddOrModifyOptOutAsync(someOptOut),
                     Times.Once);
 
             this.optOutProcessingServiceMock.VerifyNoOtherCalls();
         }
 
         [Fact]
-        public async Task ShouldReturnConflictOnPostIfAlreadyExistsOptOutErrorOccurredAsync()
+        public async Task ShouldReturnNotFoundOnPutIfItemDoesNotExistAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            string someMessage = GetRandomString();
+            OptOut someOptOut = CreateRandomOptOut();
+
+            var notFoundOptOutException =
+                new NotFoundOptOutException(message: $"Couldn't find optOut with optOutId: {someId}.");
+
+            var OptOutValidationException =
+                new OptOutProcessingValidationException(
+                    message: someMessage,
+                    innerException: notFoundOptOutException);
+
+            NotFoundObjectResult expectedNotFoundObjectResult =
+                NotFound(notFoundOptOutException);
+
+            var expectedActionResult =
+                new ActionResult<OptOut>(expectedNotFoundObjectResult);
+
+            this.optOutProcessingServiceMock.Setup(service =>
+                service.AddOrModifyOptOutAsync(someOptOut))
+                    .ThrowsAsync(OptOutValidationException);
+
+            // when
+            ActionResult<OptOut> actualActionResult =
+                await this.optOutsController.PutOptOutAsync(someOptOut);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.optOutProcessingServiceMock.Verify(service =>
+                service.AddOrModifyOptOutAsync(someOptOut),
+                    Times.Once);
+
+            this.optOutProcessingServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnFailedDependencyOnPutIfInvalidOptOutReferenceAsync()
+        {
+            // given
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+            OptOut someOptOut = CreateRandomOptOut();
+
+            var alreadyExistsOptOutException =
+                new InvalidOptOutReferenceException(
+                    message: someMessage,
+                    innerException: someInnerException);
+
+            var OptOutDependencyValidationException =
+                new OptOutDependencyValidationException(
+                    message: someMessage,
+                    innerException: alreadyExistsOptOutException);
+
+            FailedDependencyObjectResult expectedBadRequestObjectResult =
+                FailedDependency(OptOutDependencyValidationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<OptOut>(expectedBadRequestObjectResult);
+
+            this.optOutProcessingServiceMock.Setup(service =>
+                service.AddOrModifyOptOutAsync(It.IsAny<OptOut>()))
+                    .ThrowsAsync(OptOutDependencyValidationException);
+
+            // when
+            ActionResult<OptOut> actualActionResult =
+                await this.optOutsController.PutOptOutAsync(someOptOut);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.optOutProcessingServiceMock.Verify(service =>
+                service.AddOrModifyOptOutAsync(It.IsAny<OptOut>()),
+                    Times.Once);
+
+            this.optOutProcessingServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnConflictOnPutIfAlreadyExistsOptOutErrorOccurredAsync()
         {
             // given
             OptOut someOptOut = CreateRandomOptOut();
@@ -112,60 +162,50 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.OptOuts
                 new ActionResult<OptOut>(expectedConflictObjectResult);
 
             this.optOutProcessingServiceMock.Setup(service =>
-                service.RetrieveOrAddOptOutAsync(It.IsAny<OptOut>()))
+                service.AddOrModifyOptOutAsync(It.IsAny<OptOut>()))
                     .ThrowsAsync(optOutProcessingDependencyValidationException);
 
             // when
             ActionResult<OptOut> actualActionResult =
-                await this.optOutsController.PostOptOutAsync(someOptOut);
+                await this.optOutsController.PutOptOutAsync(someOptOut);
 
             // then
             actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
 
             this.optOutProcessingServiceMock.Verify(service =>
-                service.RetrieveOrAddOptOutAsync(It.IsAny<OptOut>()),
+                service.AddOrModifyOptOutAsync(It.IsAny<OptOut>()),
                     Times.Once);
 
             this.optOutProcessingServiceMock.VerifyNoOtherCalls();
         }
 
-        [Fact]
-        public async Task ShouldReturnFailedDependencyOnPostIfInvalidOptOutReferenceAsync()
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnPutIfServerErrorOccurredAsync(
+            Xeption validationException)
         {
             // given
-            var someInnerException = new Xeption();
-            string someMessage = GetRandomString();
             OptOut someOptOut = CreateRandomOptOut();
 
-            var alreadyExistsOptOutException =
-                new InvalidOptOutReferenceException(
-                    message: someMessage,
-                    innerException: someInnerException);
-
-            var optOutProcessingDependencyValidationException =
-                new OptOutProcessingDependencyValidationException(
-                    message: someMessage,
-                    innerException: alreadyExistsOptOutException);
-
-            FailedDependencyObjectResult expectedBadRequestObjectResult =
-                FailedDependency(optOutProcessingDependencyValidationException.InnerException);
+            InternalServerErrorObjectResult expectedBadRequestObjectResult =
+                InternalServerError(validationException);
 
             var expectedActionResult =
                 new ActionResult<OptOut>(expectedBadRequestObjectResult);
 
             this.optOutProcessingServiceMock.Setup(service =>
-                service.RetrieveOrAddOptOutAsync(someOptOut))
-                    .ThrowsAsync(optOutProcessingDependencyValidationException);
+                service.AddOrModifyOptOutAsync(It.IsAny<OptOut>()))
+                    .ThrowsAsync(validationException);
 
             // when
             ActionResult<OptOut> actualActionResult =
-                await this.optOutsController.PostOptOutAsync(someOptOut);
+                await this.optOutsController.PutOptOutAsync(someOptOut);
 
             // then
             actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
 
             this.optOutProcessingServiceMock.Verify(service =>
-                service.RetrieveOrAddOptOutAsync(someOptOut),
+                service.AddOrModifyOptOutAsync(It.IsAny<OptOut>()),
                     Times.Once);
 
             this.optOutProcessingServiceMock.VerifyNoOtherCalls();
--- LHDS.AdminPortal.Api/Controllers/OptOutsController.cs
+++ LHDS.AdminPortal.Api/Controllers/OptOutsController.cs
@@ -153,11 +153,11 @@ namespace LHDS.AdminPortal.Api.Controllers
             {
                 return Conflict(optOutProcessingDependencyValidationException.InnerException);
             }
-            catch (OptOutDependencyException optOutDependencyException)
+            catch (OptOutProcessingDependencyException optOutProcessingDependencyException)
             {
-                return InternalServerError(optOutDependencyException);
+                return InternalServerError(optOutProcessingDependencyException);
             }
-            catch (OptOutServiceException optOutProcessingServiceException)
+            catch (OptOutProcessingServiceException optOutProcessingServiceException)
             {
                 return InternalServerError(optOutProcessingServiceException);
             }

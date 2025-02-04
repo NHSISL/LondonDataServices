9cebcd6849a67d2d8cee61f55ddff28e669f2e77
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-02-04 14:21:07 +0000

    ShouldReturnInternalServerErrorOnGetIfServerErrorOccurredAsync -> PASS

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/TerminologyArtifacts/TerminologyArtifactsControllerTests.Exceptions.GetAll.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.TerminologyArtifacts;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Models;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.TerminologyArtifacts
+{
+    public partial class TerminologyArtifactsControllerTests
+    {
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnGetIfServerErrorOccurredAsync(Xeption serverException)
+        {
+            // given 
+            IQueryable<TerminologyArtifact> someTerminologyArtifacts = CreateRandomTerminologyArtifacts();
+
+            InternalServerErrorObjectResult expectedInternalServerErrorObjectResult =
+                InternalServerError(serverException);
+
+            var expectedActionResult =
+                new ActionResult<IQueryable<TerminologyArtifact>>(expectedInternalServerErrorObjectResult);
+
+            this.terminologyArtifactsServiceMock.Setup(service =>
+                service.RetrieveAllTerminologyArtifactsAsync())
+                    .ThrowsAsync(serverException);
+
+            // when
+            ActionResult<IQueryable<TerminologyArtifact>> actualActionResult =
+                await this.terminologyArtifactsController.Get();
+
+            // then
+            actualActionResult.Should().BeEquivalentTo(expectedActionResult);
+
+            this.terminologyArtifactsServiceMock.Verify(service =>
+                service.RetrieveAllTerminologyArtifactsAsync(),
+                    Times.Once());
+
+            this.terminologyArtifactsServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}

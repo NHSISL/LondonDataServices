93014684f3a40386b6a53dbea5fb832bc46369c3
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-02-04 13:54:24 +0000

    ShouldReturnCreatedOnPostAsync -> PASS

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/TerminologyArtifacts/TerminologyArtifactsControllerTests.Logic.Post.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.TerminologyArtifacts
+{
+    public partial class TerminologyArtifactsControllerTests
+    {
+
+    }
+}

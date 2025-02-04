ade8cbb239ce03ef4944f16541b86d2eb990abf2
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-02-04 14:53:57 +0000

    ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/TerminologyArtifacts/TerminologyArtifactsControllerTests.Exceptions.Post.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/TerminologyArtifacts/TerminologyArtifactsControllerTests.Exceptions.Post.cs
@@ -12,6 +12,7 @@ using LHDS.Core.Models.Foundations.TerminologyArtifacts.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
 using Xeptions;
 using Xunit;
 
@@ -41,6 +42,38 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.TerminologyArtifacts
             this.terminologyArtifactsServiceMock.Setup(service =>
                 service.AddTerminologyArtifactAsync(It.IsAny<TerminologyArtifact>()))
                     .ThrowsAsync(TerminologyArtifactValidationException);
+
+            // when
+            ActionResult<TerminologyArtifact> actualActionResult =
+                await this.terminologyArtifactsController.PostTerminologyArtifactAsync(someTerminologyArtifact);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.terminologyArtifactsServiceMock.Verify(service =>
+                service.AddTerminologyArtifactAsync(It.IsAny<TerminologyArtifact>()),
+                    Times.Once);
+
+            this.terminologyArtifactsServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync(
+            Xeption validationException)
+        {
+            // given
+            TerminologyArtifact someTerminologyArtifact = CreateRandomTerminologyArtifact();
+
+            InternalServerErrorObjectResult expectedBadRequestObjectResult =
+                InternalServerError(validationException);
+
+            var expectedActionResult =
+                new ActionResult<TerminologyArtifact>(expectedBadRequestObjectResult);
+
+            this.terminologyArtifactsServiceMock.Setup(service =>
+                service.AddTerminologyArtifactAsync(It.IsAny<TerminologyArtifact>()))
+                    .ThrowsAsync(validationException);
 
             // when
             ActionResult<TerminologyArtifact> actualActionResult =

dd64272d5b948723a969586ab599ac5152d2bd4c
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-02-04 14:08:23 +0000

    ShouldReturnLockedOnDeleteIfLockedTerminologyArtifactAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/TerminologyArtifacts/TerminologyArtifactsControllerTests.Exceptions.Delete.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/TerminologyArtifacts/TerminologyArtifactsControllerTests.Exceptions.Delete.cs
@@ -8,11 +8,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
+using LHDS.Core.Models.Foundations.TerminologyArtifacts.Exceptions;
 using LHDS.Core.Models.Foundations.TerminologyArtifacts;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using RESTFulSense.Clients.Extensions;
 using Xeptions;
 using Xunit;
+using LHDS.Core.Models.Foundations.TerminologyArtifacts.Exceptions;
+using RESTFulSense.Models;
 
 namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.TerminologyArtifacts
 {
@@ -48,5 +52,87 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.TerminologyArtifacts
 
             this.terminologyArtifactsServiceMock.VerifyNoOtherCalls();
         }
-    }
+
+        [Fact]
+        public async Task ShouldReturnNotFoundOnDeleteIfItemDoesNotExistAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            string someMessage = GetRandomString();
+
+            var notFoundTerminologyArtifactException =
+                new NotFoundTerminologyArtifactException(
+                    terminologyArtifactId: someId);
+
+            var TerminologyArtifactValidationException =
+                new TerminologyArtifactValidationException(
+                    message: someMessage,
+                    innerException: notFoundTerminologyArtifactException);
+
+            NotFoundObjectResult expectedNotFoundObjectResult =
+                NotFound(notFoundTerminologyArtifactException);
+
+            var expectedActionResult =
+                new ActionResult<TerminologyArtifact>(expectedNotFoundObjectResult);
+
+            this.terminologyArtifactsServiceMock.Setup(service =>
+                service.RemoveTerminologyArtifactByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(TerminologyArtifactValidationException);
+
+            // when
+            ActionResult<TerminologyArtifact> actualActionResult =
+                await this.terminologyArtifactsController.DeleteTerminologyArtifactByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.terminologyArtifactsServiceMock.Verify(service =>
+                service.RemoveTerminologyArtifactByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.terminologyArtifactsServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnLockedOnDeleteIfLockedTerminologyArtifactAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+
+            var lockedTerminologyArtifactException =
+                new LockedTerminologyArtifactException(
+                    message: someMessage,
+                    innerException: someInnerException);
+
+            var TerminologyArtifactDependencyValidationException =
+                new TerminologyArtifactDependencyValidationException(
+                    message: someMessage,
+                    innerException: lockedTerminologyArtifactException);
+
+            LockedObjectResult expectedBadRequestObjectResult =
+                Locked(TerminologyArtifactDependencyValidationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<TerminologyArtifact>(expectedBadRequestObjectResult);
+
+            this.terminologyArtifactsServiceMock.Setup(service =>
+                service.RemoveTerminologyArtifactByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(TerminologyArtifactDependencyValidationException);
+
+            // when
+            ActionResult<TerminologyArtifact> actualActionResult =
+                await this.terminologyArtifactsController.DeleteTerminologyArtifactByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.terminologyArtifactsServiceMock.Verify(service =>
+                service.RemoveTerminologyArtifactByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.terminologyArtifactsServiceMock.VerifyNoOtherCalls();
+        }
+    }    
 }

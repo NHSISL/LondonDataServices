569d0e59a926db8ead7e1cd49222a627439940b0
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-02-05 10:45:06 +0000

    ShouldReturnListOfDocumentsOnGetAsync -> PASS

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/EmisLandings/EmisLandingsControllerTests.Logic.GetAll.cs
@@ -0,0 +1,50 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.EmisLandings
+{
+    public partial class EmisLandingsControllerTests
+    {
+        [Fact]
+        public async Task ShouldReturnListOfDocumentsOnGetAsync()
+        {
+            // given
+            var subscriberAgreementId = Guid.NewGuid();
+            var expectedDocuments = new List<string> { "file1.pdf", "file2.pdf" };
+
+            OkObjectResult expectedObjectResult = new OkObjectResult(expectedDocuments);
+
+            var expectedActionResult =
+                new ActionResult<List<string>>(expectedObjectResult);
+
+            this.emisLandingCoordinationServiceMock.Setup(service =>
+                service.RetrieveListOfDocumentsToProcessAsync(subscriberAgreementId))
+                    .ReturnsAsync(expectedDocuments);
+
+            // when
+            ActionResult<List<string>> actualActionResult =
+                await this.emisLandingsController.Get(subscriberAgreementId);
+
+            // then
+            actualActionResult.Should().BeEquivalentTo(expectedActionResult);
+
+            this.emisLandingCoordinationServiceMock.Verify(service =>
+                service.RetrieveListOfDocumentsToProcessAsync(subscriberAgreementId),
+                    Times.Once());
+
+            this.emisLandingCoordinationServiceMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/EmisLandings/EmisLandingsControllerTests.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/EmisLandings/EmisLandingsControllerTests.cs
@@ -2,10 +2,17 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System.Linq;
+using System;
 using LHDS.AdminPortal.Api.Controllers;
+using LHDS.Core.Models.Foundations.DataTypes.Exceptions;
 using LHDS.Core.Services.Coordinations.EmisLandings;
 using Moq;
 using RESTFulSense.Controllers;
+using Tynamix.ObjectFiller;
+using Xeptions;
+using Xunit;
+using LHDS.Core.Models.Orchestrations.EmisLandings.Exceptions;
 
 namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.EmisLandings
 {
@@ -18,6 +25,46 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.EmisLandings
         {
             this.emisLandingCoordinationServiceMock = new Mock<IEmisLandingCoordinationService>();
             this.emisLandingsController = new EmisLandingsController(this.emisLandingCoordinationServiceMock.Object);
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static int GetRandomNumber() =>
+           new IntRange(min: 2, max: 10).GetValue();
+
+        public static TheoryData<Xeption> ServerExceptions()
+        {
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+
+            return new TheoryData<Xeption>
+            {
+                new EmisLandingOrchestrationDependencyException(
+                    message: someMessage,
+                    innerException: someInnerException),
+
+                new EmisLandingOrchestrationServiceException(
+                    message: someMessage,
+                    innerException: someInnerException)
+            };
+        }
+
+        public static TheoryData<Xeption> ValidationExceptions()
+        {
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+
+            return new TheoryData<Xeption>
+            {
+                new EmisLandingOrchestrationValidationException(
+                    message: someMessage,
+                    innerException: someInnerException),
+
+                new EmisLandingOrchestrationDependencyValidationException(
+                    message: someMessage,
+                    innerException: someInnerException)
+            };
         }
     }
 }

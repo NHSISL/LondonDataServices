f5b8cc6c2f84b99aa70618f461559bbb6d3b2830
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-02-05 16:01:58 +0000

    ShouldReturnFailedDependencyOnPutIfInvalidResolvedAddressReferenceAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/ResolvedAddresses/ResolvedAddressesControllerTests.Exceptions.Put.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/ResolvedAddresses/ResolvedAddressesControllerTests.Exceptions.Put.cs
@@ -12,6 +12,7 @@ using LHDS.Core.Models.Foundations.ResolvedAddresses.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
 using Xeptions;
 using Xunit;
 
@@ -82,6 +83,48 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.ResolvedAddresses
             this.resolvedAddressServiceMock.Setup(service =>
                 service.ModifyResolvedAddressAsync(It.IsAny<ResolvedAddress>()))
                     .ThrowsAsync(ResolvedAddressValidationException);
+
+            // when
+            ActionResult<ResolvedAddress> actualActionResult =
+                await this.resolvedAddressesController.PutResolvedAddressAsync(someResolvedAddress);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.resolvedAddressServiceMock.Verify(service =>
+                service.ModifyResolvedAddressAsync(It.IsAny<ResolvedAddress>()),
+                    Times.Once);
+
+            this.resolvedAddressServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnFailedDependencyOnPutIfInvalidResolvedAddressReferenceAsync()
+        {
+            // given
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+            ResolvedAddress someResolvedAddress = CreateRandomResolvedAddress();
+
+            var alreadyExistsResolvedAddressException =
+                new InvalidResolvedAddressReferenceException(
+                    message: someMessage,
+                    innerException: someInnerException);
+
+            var ResolvedAddressDependencyValidationException =
+                new ResolvedAddressDependencyValidationException(
+                    message: someMessage,
+                    innerException: alreadyExistsResolvedAddressException);
+
+            FailedDependencyObjectResult expectedBadRequestObjectResult =
+                FailedDependency(ResolvedAddressDependencyValidationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<ResolvedAddress>(expectedBadRequestObjectResult);
+
+            this.resolvedAddressServiceMock.Setup(service =>
+                service.ModifyResolvedAddressAsync(It.IsAny<ResolvedAddress>()))
+                    .ThrowsAsync(ResolvedAddressDependencyValidationException);
 
             // when
             ActionResult<ResolvedAddress> actualActionResult =

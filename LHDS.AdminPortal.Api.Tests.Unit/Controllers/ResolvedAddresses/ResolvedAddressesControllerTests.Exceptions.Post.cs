510cba195f6293e7c50e0fc12ac21c6e792b8992
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-02-05 15:55:00 +0000

    ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/ResolvedAddresses/ResolvedAddressesControllerTests.Exceptions.Post.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/ResolvedAddresses/ResolvedAddressesControllerTests.Exceptions.Post.cs
@@ -12,6 +12,7 @@ using LHDS.Core.Models.Foundations.ResolvedAddresses.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
 using Xeptions;
 using Xunit;
 
@@ -41,6 +42,38 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.ResolvedAddresses
             this.resolvedAddressServiceMock.Setup(service =>
                 service.AddResolvedAddressAsync(It.IsAny<ResolvedAddress>()))
                     .ThrowsAsync(ResolvedAddressValidationException);
+
+            // when
+            ActionResult<ResolvedAddress> actualActionResult =
+                await this.resolvedAddressesController.PostResolvedAddressAsync(someResolvedAddress);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.resolvedAddressServiceMock.Verify(service =>
+                service.AddResolvedAddressAsync(It.IsAny<ResolvedAddress>()),
+                    Times.Once);
+
+            this.resolvedAddressServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync(
+            Xeption validationException)
+        {
+            // given
+            ResolvedAddress someResolvedAddress = CreateRandomResolvedAddress();
+
+            InternalServerErrorObjectResult expectedBadRequestObjectResult =
+                InternalServerError(validationException);
+
+            var expectedActionResult =
+                new ActionResult<ResolvedAddress>(expectedBadRequestObjectResult);
+
+            this.resolvedAddressServiceMock.Setup(service =>
+                service.AddResolvedAddressAsync(It.IsAny<ResolvedAddress>()))
+                    .ThrowsAsync(validationException);
 
             // when
             ActionResult<ResolvedAddress> actualActionResult =

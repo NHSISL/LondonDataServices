aab1b80f814266da3705c9ed385991c8529b48a8
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-02-05 15:50:17 +0000

    ShouldReturnInternalServerErrorOnGetByIdIfServerErrorOccurredAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/ResolvedAddresses/ResolvedAddressesControllerTests.Exceptions.GetById.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/ResolvedAddresses/ResolvedAddressesControllerTests.Exceptions.GetById.cs
@@ -12,6 +12,7 @@ using LHDS.Core.Models.Foundations.ResolvedAddresses.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
 using Xeptions;
 using Xunit;
 
@@ -52,6 +53,37 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.ResolvedAddresses
             this.resolvedAddressServiceMock.Verify(service =>
                 service.RetrieveResolvedAddressByIdAsync(someId),
                     Times.Once);
+
+            this.resolvedAddressServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnGetByIdIfServerErrorOccurredAsync(Xeption serverException)
+        {
+            // given 
+            Guid someId = Guid.NewGuid();
+
+            InternalServerErrorObjectResult expectedInternalServerErrorObjectResult =
+                InternalServerError(serverException);
+
+            var expectedActionResult =
+                new ActionResult<ResolvedAddress>(expectedInternalServerErrorObjectResult);
+
+            this.resolvedAddressServiceMock.Setup(service =>
+                service.RetrieveResolvedAddressByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(serverException);
+
+            // when
+            ActionResult<ResolvedAddress> actualActionResult =
+                await this.resolvedAddressesController.GetResolvedAddressByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.resolvedAddressServiceMock.Verify(service =>
+                service.RetrieveResolvedAddressByIdAsync(It.IsAny<Guid>()),
+                    Times.Once());
 
             this.resolvedAddressServiceMock.VerifyNoOtherCalls();
         }

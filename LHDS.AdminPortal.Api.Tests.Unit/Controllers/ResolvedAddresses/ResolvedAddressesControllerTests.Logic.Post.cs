ad85e52bf0fef35ad4644546f90e2574d465611a
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-02-05 15:21:06 +0000

    ShouldReturnCreatedOnPostAsync -> PASS

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/ResolvedAddresses/ResolvedAddressesControllerTests.Logic.Post.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Force.DeepCloner;
+using LHDS.Core.Models.Foundations.ResolvedAddresses;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.ResolvedAddresses
+{
+    public partial class ResolvedAddressesControllerTests
+    {
+        [Fact]
+        public async Task ShouldReturnCreatedOnPostAsync()
+        {
+            // given
+            ResolvedAddress randomResolvedAddress = CreateRandomResolvedAddress();
+            ResolvedAddress inputResolvedAddress = randomResolvedAddress;
+            ResolvedAddress addedResolvedAddress = inputResolvedAddress.DeepClone();
+            ResolvedAddress expectedResolvedAddress = addedResolvedAddress.DeepClone();
+
+            var expectedObjectResult =
+                new CreatedObjectResult(expectedResolvedAddress);
+
+            var expectedActionResult =
+                new ActionResult<ResolvedAddress>(expectedObjectResult);
+
+            this.resolvedAddressServiceMock.Setup(service =>
+                service.AddResolvedAddressAsync(inputResolvedAddress))
+                    .ReturnsAsync(addedResolvedAddress);
+
+            // when
+            ActionResult<ResolvedAddress> actualActionResult =
+                await this.resolvedAddressesController.PostResolvedAddressAsync(inputResolvedAddress);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.resolvedAddressServiceMock.Verify(service =>
+                service.AddResolvedAddressAsync(inputResolvedAddress),
+                   Times.Once);
+
+            this.resolvedAddressServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}

9da0a2fb6ef5fccef7fb946caf8fc6a30b9691f2
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-02-05 11:57:26 +0000

    ShouldReturnInternalServerErrorOnGetIfServerErrorOccurredAsync -> PASS

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/EmisLandings/EmisLandingsControllerTests.Exceptions.GetAll.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.Downloads.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Models;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.EmisLandings
+{
+    public partial class EmisLandingsControllerTests
+    {
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnGetIfServerErrorOccurredAsync(Xeption serverException)
+        {
+            // given 
+            Guid someSubscriberAgreementId = Guid.NewGuid();
+
+            InternalServerErrorObjectResult expectedInternalServerErrorObjectResult =
+                InternalServerError(serverException);
+
+            var expectedActionResult =
+                new ActionResult<List<string>>(expectedInternalServerErrorObjectResult);
+
+            this.emisLandingCoordinationServiceMock.Setup(service =>
+                service.RetrieveListOfDocumentsToProcessAsync(someSubscriberAgreementId))
+                    .ThrowsAsync(serverException);
+
+            // when
+            ActionResult<List<string>> actualActionResult =
+                await this.emisLandingsController.Get(someSubscriberAgreementId);
+
+            // then
+            actualActionResult.Should().BeEquivalentTo(expectedActionResult);
+
+            this.emisLandingCoordinationServiceMock.Verify(service =>
+                service.RetrieveListOfDocumentsToProcessAsync(someSubscriberAgreementId),
+                    Times.Once());
+
+            this.emisLandingCoordinationServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/EmisLandings/EmisLandingsControllerTests.Exceptions.Put.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.DataTypes.Exceptions;
+using LHDS.Core.Models.Foundations.IngestionTrackings.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.EmisLandings
+{
+    public partial class EmisLandingsControllerTests
+    {
+        
+    }
+}

3b0aeda6962d8c36216de1f8c4d94540a43ff498
hassaero110 <hassan.raza3@nhs.net>
2025-02-06 13:53:52 +0000

    ShouldReturnOkOnPutAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/SpecificationObjects/SpecificationsObjectsControllerTests.Logic.Put.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/SpecificationObjects/SpecificationsObjectsControllerTests.Logic.Put.cs
@@ -2,21 +2,12 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Attrify.Attributes;
-using FluentAssertions;
 using Force.DeepCloner;
-using LHDS.AdminPortal.Api.Controllers;
 using LHDS.Core.Models.Foundations.SpecificationObjects;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
-using RESTFulSense.Models;
 using Xunit;
 
 namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.SpecificationObjects
@@ -24,33 +15,33 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.SpecificationObjects
     public partial class SpecificationObjectsControllerTests
     {
         [Fact]
-        public async Task ShouldReturnCreatedOnPostAsync()
+        public async Task ShouldReturnOkOnPutAsync()
         {
             // given
             SpecificationObject randomSpecificationObject = CreateRandomSpecificationObject();
             SpecificationObject inputSpecificationObject = randomSpecificationObject;
-            SpecificationObject addedSpecificationObject = inputSpecificationObject.DeepClone();
-            SpecificationObject expectedSpecificationObject = addedSpecificationObject.DeepClone();
+            SpecificationObject storageSpecificationObject = inputSpecificationObject.DeepClone();
+            SpecificationObject expectedSpecificationObject = storageSpecificationObject.DeepClone();
 
             var expectedObjectResult =
-                new CreatedObjectResult(expectedSpecificationObject);
+                new OkObjectResult(expectedSpecificationObject);
 
             var expectedActionResult =
                 new ActionResult<SpecificationObject>(expectedObjectResult);
 
             this.specificationObjectServiceMock.Setup(service =>
-                service.AddSpecificationObjectAsync(inputSpecificationObject))
-                    .ReturnsAsync(addedSpecificationObject);
+                service.ModifySpecificationObjectAsync(inputSpecificationObject))
+                    .ReturnsAsync(storageSpecificationObject);
 
             // when
             ActionResult<SpecificationObject> actualActionResult =
-                await this.specificationObjectsController.PostSpecificationObjectAsync(inputSpecificationObject);
+                await this.specificationObjectsController.PutSpecificationObjectAsync(inputSpecificationObject);
 
             // then
             actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
 
             this.specificationObjectServiceMock.Verify(service =>
-                service.AddSpecificationObjectAsync(inputSpecificationObject),
+                service.ModifySpecificationObjectAsync(inputSpecificationObject),
                    Times.Once);
 
             this.specificationObjectServiceMock.VerifyNoOtherCalls();

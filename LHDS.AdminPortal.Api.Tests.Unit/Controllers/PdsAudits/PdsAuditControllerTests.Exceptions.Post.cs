160b74533f4437d0a1bec69af72b59548122246e
hassaero110 <hassan.raza3@nhs.net>
2025-02-06 11:49:46 +0000

    ShouldReturnBadRequestOnPutIfValidationErrorOccurredAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/PdsAudits/PdsAuditControllerTests.Exceptions.Post.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/PdsAudits/PdsAuditControllerTests.Exceptions.Post.cs
@@ -2,17 +2,17 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+
+using System;
 using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.PdsAudits;
+using LHDS.Core.Models.Foundations.PdsAudits.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
-using LHDS.Core.Models.Foundations.PdsAudits;
 using RESTFulSense.Models;
-using Xunit;
-using LHDS.Core.Models.Foundations.PdsAudits.Exceptions;
-using RESTFulSense.Models;
-using System;
 using Xeptions;
+using Xunit;
 
 namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.PdsAudits
 {
@@ -26,20 +26,20 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.PdsAudits
             string someMessage = GetRandomString();
             PdsAudit somePdsAudit = CreateRandomPdsAudit();
 
-            var PdsAuditValidationException =
+            var pdsAuditValidationException =
                 new PdsAuditValidationException(
                     message: someMessage,
                     innerException: someInnerException);
 
-            BadRequestObjectResult expectedBadRequestObjectResult = 
-                BadRequest(PdsAuditValidationException.InnerException);
+            BadRequestObjectResult expectedBadRequestObjectResult =
+                BadRequest(pdsAuditValidationException.InnerException);
 
             var expectedActionResult =
                 new ActionResult<PdsAudit>(expectedBadRequestObjectResult);
 
             this.pdsAuditServiceMock.Setup(service =>
                 service.AddPdsAuditAsync(somePdsAudit))
-                    .ThrowsAsync(PdsAuditValidationException);
+                    .ThrowsAsync(pdsAuditValidationException);
 
             // when
             ActionResult<PdsAudit> actualActionResult =
@@ -63,7 +63,7 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.PdsAudits
             // given
             PdsAudit somePdsAudit = CreateRandomPdsAudit();
 
-            InternalServerErrorObjectResult expectedBadRequestObjectResult =
+            InternalServerErrorObjectResult expectedBadRequestObjectResult = 
                 InternalServerError(validationException);
 
             var expectedActionResult =

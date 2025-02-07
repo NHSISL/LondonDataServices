1421009dcfa92b8bd0faf4a48ab32b53eaa274be
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-02-07 14:47:27 +0000

    ShouldReturnTerminologyPollOnGetByIdAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/TerminologyPolls/TerminologyPollsControllerTests.Logic.Get.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/TerminologyPolls/TerminologyPollsControllerTests.Logic.Get.cs
@@ -7,11 +7,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Force.DeepCloner;
 using LHDS.Core.Models.Foundations.TerminologyPolls;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using RESTFulSense.Clients.Extensions;
 using Xunit;
 
 namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.TerminologyPolls
@@ -19,34 +19,32 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.TerminologyPolls
     public partial class TerminologyPollsControllerTests
     {
         [Fact]
-        public async Task ShouldReturnOkOnDeleteAsync()
+        public async Task ShouldReturnTerminologyPollOnGetByIdAsync()
         {
-            // given
+            // given 
             TerminologyPoll randomTerminologyPoll = CreateRandomTerminologyPoll();
             Guid inputId = randomTerminologyPoll.Id;
             TerminologyPoll storageTerminologyPoll = randomTerminologyPoll.DeepClone();
             TerminologyPoll expectedTerminologyPoll = storageTerminologyPoll.DeepClone();
-
-            var expectedObjectResult =
-                new OkObjectResult(expectedTerminologyPoll);
+            var expectedObjectResult = new OkObjectResult(expectedTerminologyPoll);
 
             var expectedActionResult =
                 new ActionResult<TerminologyPoll>(expectedObjectResult);
 
             this.terminologyPollsServiceMock.Setup(service =>
-                service.RemoveTerminologyPollByIdAsync(inputId))
-                    .ReturnsAsync(storageTerminologyPoll);
+                service.RetrieveTerminologyPollByIdAsync(inputId))
+                    .ReturnsAsync(expectedTerminologyPoll);
 
             // when
             ActionResult<TerminologyPoll> actualActionResult =
-                await this.terminologyPollsController.DeleteTerminologyPollByIdAsync(inputId);
+                await this.terminologyPollsController.GetTerminologyPollByIdAsync(inputId);
 
             // then
-            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+            actualActionResult.Should().BeEquivalentTo(expectedActionResult);
 
             this.terminologyPollsServiceMock.Verify(service =>
-                service.RemoveTerminologyPollByIdAsync(inputId),
-                    Times.Once);
+                service.RetrieveTerminologyPollByIdAsync(inputId),
+                    Times.Once());
 
             this.terminologyPollsServiceMock.VerifyNoOtherCalls();
         }

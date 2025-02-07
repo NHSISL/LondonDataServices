9b2464e0cef425b20bdc03e6763ce179b740c467
MarcusElcamel <marcus.elcamel@answerdigital.com>
2025-02-07 15:28:12 +0000

    ShouldReturnInternalServerErrorOnPutIfServerErrorOccurredAsync -> PASS

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/TerminologyPolls/TerminologyPollsControllerTests.Exceptions.Put.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/TerminologyPolls/TerminologyPollsControllerTests.Exceptions.Put.cs
@@ -3,21 +3,15 @@
 // ---------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using LHDS.Core.Models.Foundations.TerminologyPolls.Exceptions;
 using LHDS.Core.Models.Foundations.TerminologyPolls;
 using LHDS.Core.Models.Foundations.TerminologyPolls.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
 using Xeptions;
 using Xunit;
-using LHDS.Core.Models.Foundations.TerminologyPolls.Exceptions;
-using RESTFulSense.Models;
-using LHDS.Core.Models.Foundations.TerminologyPolls.Exceptions;
 
 namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.TerminologyPolls
 {
@@ -170,6 +164,38 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.TerminologyPolls
             this.terminologyPollsServiceMock.Setup(service =>
                 service.ModifyTerminologyPollAsync(It.IsAny<TerminologyPoll>()))
                     .ThrowsAsync(TerminologyPollDependencyValidationException);
+
+            // when
+            ActionResult<TerminologyPoll> actualActionResult =
+                await this.terminologyPollsController.PutTerminologyPollAsync(someTerminologyPoll);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.terminologyPollsServiceMock.Verify(service =>
+                service.ModifyTerminologyPollAsync(It.IsAny<TerminologyPoll>()),
+                    Times.Once);
+
+            this.terminologyPollsServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnPutIfServerErrorOccurredAsync(
+            Xeption validationException)
+        {
+            // given
+            TerminologyPoll someTerminologyPoll = CreateRandomTerminologyPoll();
+
+            InternalServerErrorObjectResult expectedBadRequestObjectResult =
+                InternalServerError(validationException);
+
+            var expectedActionResult =
+                new ActionResult<TerminologyPoll>(expectedBadRequestObjectResult);
+
+            this.terminologyPollsServiceMock.Setup(service =>
+                service.ModifyTerminologyPollAsync(It.IsAny<TerminologyPoll>()))
+                    .ThrowsAsync(validationException);
 
             // when
             ActionResult<TerminologyPoll> actualActionResult =

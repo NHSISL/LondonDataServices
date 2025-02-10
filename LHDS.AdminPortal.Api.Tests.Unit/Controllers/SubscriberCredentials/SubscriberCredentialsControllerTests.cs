d87689c34656f732d3b8b54f3c70e06eee4b5927
Hassan Raza <h_raza110@outlook.com>
2025-02-10 11:27:38 +0000

    CONTROLLERS: Subscriber Credentials Unit Tests

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/SubscriberCredentials/SubscriberCredentialsControllerTests.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/SubscriberCredentials/SubscriberCredentialsControllerTests.cs
@@ -3,28 +3,105 @@
 // ---------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LHDS.AdminPortal.Api.Controllers;
-using LHDS.Core.Services.Foundations.PdsAudits;
+using LHDS.Core.Models.Orchestrations.SubscriberCredentials.Exceptions;
+using LHDS.Core.Models.Processings.SubscriberCredentials;
+using LHDS.Core.Models.Processings.SubscriberCredentials.Exceptions;
 using LHDS.Core.Services.Orchestrations.SubscriberCredentials;
 using Moq;
+using RESTFulSense.Controllers;
+using Tynamix.ObjectFiller;
+using Xeptions;
+using Xunit;
 
 namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.SubscriberCredentials
 {
-    public partial class SubscriberCredentialsControllerTests
+    public partial class SubscriberCredentialsControllerTests : RESTFulController
     {
         private readonly Mock<ISubscriberCredentialOrchestration> subscriberCredentialOrchestrationMock;
-        private readonly SubscriberCredentialsController subscriberCredentialController;
+        private readonly SubscriberCredentialsController subscriberCredentialsController;
 
         public SubscriberCredentialsControllerTests()
         {
             this.subscriberCredentialOrchestrationMock = new Mock<ISubscriberCredentialOrchestration>();
-            
-            this.subscriberCredentialController = new SubscriberCredentialsController(
+
+            this.subscriberCredentialsController = new SubscriberCredentialsController(
                 this.subscriberCredentialOrchestrationMock.Object);
+        }
+        private static string GetRandomString() =>
+                 new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static string GetRandomStringWithLengthOf(int length)
+        {
+            string result = new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
+            return result.Length > length ? result.Substring(0, length) : result;
+        }
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static IQueryable<SubscriberCredential> CreateRandomSubscriberCredentiales()
+        {
+            return CreateSubscriberCredentialFiller()
+                .Create(count: GetRandomNumber())
+                    .AsQueryable();
+        }
+
+        private static SubscriberCredential CreateRandomSubscriberCredential() =>
+            CreateSubscriberCredentialFiller().Create();
+
+        private static Filler<SubscriberCredential> CreateSubscriberCredentialFiller()
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.UtcNow;
+            string user = Guid.NewGuid().ToString();
+            var filler = new Filler<SubscriberCredential>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(accessAudit => accessAudit.CreatedBy).Use(user)
+                .OnProperty(accessAudit => accessAudit.UpdatedBy).Use(user);
+
+            return filler;
+        }
+
+        public static TheoryData<Xeption> ServerExceptions()
+        {
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+
+            return new TheoryData<Xeption>
+            {
+                new SubscriberCredentialDependencyOrchestrationException(
+                    message: someMessage,
+                    innerException: someInnerException),
+
+                new SubscriberCredentialOrchestrationServiceException(
+                    message: someMessage,
+                    innerException: someInnerException)
+            };
+        }
+
+        public static TheoryData<Xeption> ValidationExceptions()
+        {
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+
+            return new TheoryData<Xeption>
+            {
+                new SubscriberCredentialValidationOrchestrationException(
+                    message: someMessage,
+                    innerException: someInnerException),
+
+                new SubscriberCredentialOrchestrationDependencyValidationException(
+                    message: someMessage,
+                    innerException: someInnerException)
+            };
         }
     }
 }
---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/SubscriberCredentials/SubscriberCredentialsTests.Logic.Delete.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Attrify.Attributes;
+using FluentAssertions;
+using Force.DeepCloner;
+using LHDS.AdminPortal.Api.Controllers;
+using LHDS.Core.Models.Processings.SubscriberCredentials;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.SubscriberCredentials
+{
+    public partial class SubscriberCredentialsControllerTests
+    {
+        [Fact]
+        public async Task ShouldReturnOkOnDeleteAsync()
+        {
+            // given
+            SubscriberCredential randomSubscriberCredential = CreateRandomSubscriberCredential();
+            Guid inputId = randomSubscriberCredential.Id;
+            SubscriberCredential storageSubscriberCredential = randomSubscriberCredential.DeepClone();
+            SubscriberCredential expectedSubscriberCredential = storageSubscriberCredential.DeepClone();
+
+            var expectedObjectResult =
+                new OkObjectResult(expectedSubscriberCredential);
+
+            var expectedActionResult =
+                new ActionResult<SubscriberCredential>(expectedObjectResult);
+
+            this.subscriberCredentialOrchestrationMock.Setup(service =>
+                service.RemoveSubscriberCredentialByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(storageSubscriberCredential);
+
+            // when
+            ActionResult<SubscriberCredential> actualActionResult =
+                await this.subscriberCredentialsController.DeleteSubscriberCredentialByIdAsync(inputId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.subscriberCredentialOrchestrationMock.Verify(service =>
+                service.RemoveSubscriberCredentialByIdAsync(It.IsAny<Guid>()),
+                   Times.Once);
+
+            this.subscriberCredentialOrchestrationMock.VerifyNoOtherCalls();
+        }
+    }
+}

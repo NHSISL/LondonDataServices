eebf4c149b1dbdd5dae0f871f17a67bb60366956
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-02-18 12:15:26 +0000

    WIP -> fix RetrieveOptOutStatusAsync acceptance test

--- LHDS.Core/Services/Orchestrations/OptOuts/OptOutOrchestrationService.cs
+++ LHDS.Core/Services/Orchestrations/OptOuts/OptOutOrchestrationService.cs
@@ -32,6 +32,7 @@ namespace LHDS.Core.Services.Orchestrations.OptOuts
         private readonly BlobContainers blobContainers;
         private readonly IDateTimeBroker dateTimeBroker;
         private readonly IIdentifierBroker identifierBroker;
+        //private readonly ISecurityBroker securityBroker;
         private readonly ILoggingBroker loggingBroker;
         private readonly OptOutConfiguration optOutConfiguration;
         private readonly MeshConfiguration meshConfiguration;
@@ -44,6 +45,7 @@ namespace LHDS.Core.Services.Orchestrations.OptOuts
             ICsvHelperBroker csvHelperBroker,
             IDateTimeBroker dateTimeBroker,
             IIdentifierBroker identifierBroker,
+            //ISecurityBroker securityBroker,
             ILoggingBroker loggingBroker,
             OptOutConfiguration optOutConfiguration,
             MeshConfiguration meshConfiguration)
@@ -55,6 +57,7 @@ namespace LHDS.Core.Services.Orchestrations.OptOuts
             this.csvHelperBroker = csvHelperBroker;
             this.dateTimeBroker = dateTimeBroker;
             this.identifierBroker = identifierBroker;
+            //this.securityBroker = securityBroker;
             this.loggingBroker = loggingBroker;
             this.optOutConfiguration = optOutConfiguration;
             this.meshConfiguration = meshConfiguration;
@@ -100,6 +103,7 @@ namespace LHDS.Core.Services.Orchestrations.OptOuts
                         OptOut processedOptOut = await TryCatch(async () =>
                         {
                             DateTimeOffset timeStamp = await this.dateTimeBroker.GetCurrentDateTimeOffsetAsync();
+                            //EntraUser currentUser = await this.securityBroker.GetCurrentUserAsync();
                             var expirationDate = timeStamp.AddDays(-optOutConfiguration.ExpiredAfterDays);
 
                             OptOut item = await this.optOutProcessingService

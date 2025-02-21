e629a1f0c5632638ab6a2d2d2e5a510e31b3c05d
Hassan Raza <h_raza110@outlook.com>
2025-02-21 13:07:43 +0000

    All Tests passing

--- LHDS.AdminPortal.Api.Tests.Unit/Controllers/SubscriberAgreements/SubscriberAgreementControllerTests.Exceptions.Put.cs
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/SubscriberAgreements/SubscriberAgreementControllerTests.Exceptions.Put.cs
@@ -2,13 +2,16 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using Force.DeepCloner;
 using LHDS.Core.Models.Foundations.SubscriberAgreements;
+using LHDS.Core.Models.Foundations.SubscriberAgreements.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
 using RESTFulSense.Models;
+using Xeptions;
 using Xunit;
 
 namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.SubscriberAgreements
@@ -16,34 +19,195 @@ namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.SubscriberAgreements
     public partial class SubscriberAgreementControllerTests
     {
         [Fact]
-        public async Task ShouldReturnCreatedOnPostAsync()
+        public async Task ShouldReturnBadRequestOnPutIfValidationErrorOccurredAsync()
         {
             // given
-            SubscriberAgreement randomSubscriberAgreement = CreateRandomSubscriberAgreement();
-            SubscriberAgreement inputSubscriberAgreement = randomSubscriberAgreement;
-            SubscriberAgreement addedSubscriberAgreement = inputSubscriberAgreement.DeepClone();
-            SubscriberAgreement expectedSubscriberAgreement = addedSubscriberAgreement.DeepClone();
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+            SubscriberAgreement someSubscriberAgreement = CreateRandomSubscriberAgreement();
 
-            var expectedObjectResult =
-                new CreatedObjectResult(expectedSubscriberAgreement);
+            var addressValidationException =
+                new SubscriberAgreementValidationException(
+                    message: someMessage,
+                    innerException: someInnerException);
+
+            BadRequestObjectResult expectedBadRequestObjectResult =
+                BadRequest(addressValidationException.InnerException);
 
             var expectedActionResult =
-                new ActionResult<SubscriberAgreement>(expectedObjectResult);
+                new ActionResult<SubscriberAgreement>(expectedBadRequestObjectResult);
 
             this.subscriberAgreementServiceMock.Setup(service =>
-                service.AddSubscriberAgreementAsync(inputSubscriberAgreement))
-                    .ReturnsAsync(addedSubscriberAgreement);
+                service.ModifySubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()))
+                    .ThrowsAsync(addressValidationException);
 
             // when
             ActionResult<SubscriberAgreement> actualActionResult =
-                await this.subscriberAgreementsController.PostSubscriberAgreementAsync(inputSubscriberAgreement);
+                await this.subscriberAgreementsController.PutSubscriberAgreementAsync(someSubscriberAgreement);
 
             // then
             actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
 
             this.subscriberAgreementServiceMock.Verify(service =>
-                service.AddSubscriberAgreementAsync(inputSubscriberAgreement),
-                   Times.Once);
+                service.ModifySubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()),
+                    Times.Once);
+
+            this.subscriberAgreementServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnNotFoundOnPutIfItemDoesNotExistAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            string someMessage = GetRandomString();
+            SubscriberAgreement someSubscriberAgreement = CreateRandomSubscriberAgreement();
+
+            var notFoundSubscriberAgreementException =
+                new NotFoundSubscriberAgreementException(
+                    subscriberAgreementId: someId);
+
+            var addressValidationException =
+                new SubscriberAgreementValidationException(
+                    message: someMessage,
+                    innerException: notFoundSubscriberAgreementException);
+
+            NotFoundObjectResult expectedNotFoundObjectResult =
+                NotFound(notFoundSubscriberAgreementException);
+
+            var expectedActionResult =
+                new ActionResult<SubscriberAgreement>(expectedNotFoundObjectResult);
+
+            this.subscriberAgreementServiceMock.Setup(service =>
+                service.ModifySubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()))
+                    .ThrowsAsync(addressValidationException);
+
+            // when
+            ActionResult<SubscriberAgreement> actualActionResult =
+                await this.subscriberAgreementsController.PutSubscriberAgreementAsync(someSubscriberAgreement);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.subscriberAgreementServiceMock.Verify(service =>
+                service.ModifySubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()),
+                    Times.Once);
+
+            this.subscriberAgreementServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnFailedDependencyOnPutIfInvalidSubscriberAgreementReferenceAsync()
+        {
+            // given
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+            SubscriberAgreement someSubscriberAgreement = CreateRandomSubscriberAgreement();
+
+            var alreadyExistsSubscriberAgreementException =
+                new InvalidSubscriberAgreementReferenceException(
+                    message: someMessage,
+                    innerException: someInnerException);
+
+            var addressDependencyValidationException =
+                new SubscriberAgreementDependencyValidationException(
+                    message: someMessage,
+                    innerException: alreadyExistsSubscriberAgreementException);
+
+            FailedDependencyObjectResult expectedBadRequestObjectResult =
+                FailedDependency(addressDependencyValidationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<SubscriberAgreement>(expectedBadRequestObjectResult);
+
+            this.subscriberAgreementServiceMock.Setup(service =>
+                service.ModifySubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()))
+                    .ThrowsAsync(addressDependencyValidationException);
+
+            // when
+            ActionResult<SubscriberAgreement> actualActionResult =
+                await this.subscriberAgreementsController.PutSubscriberAgreementAsync(someSubscriberAgreement);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.subscriberAgreementServiceMock.Verify(service =>
+                service.ModifySubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()),
+                    Times.Once);
+
+            this.subscriberAgreementServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnConflictOnPutIfAlreadyExistsSubscriberAgreementErrorOccurredAsync()
+        {
+            // given
+            SubscriberAgreement someSubscriberAgreement = CreateRandomSubscriberAgreement();
+            var someInnerException = new Exception();
+            string someMessage = GetRandomString();
+
+            var alreadyExistsSubscriberAgreementException =
+                new AlreadyExistsSubscriberAgreementException(
+                    message: someMessage,
+                    innerException: someInnerException);
+
+            var addressDependencyValidationException =
+                new SubscriberAgreementDependencyValidationException(
+                    message: someMessage,
+                    innerException: alreadyExistsSubscriberAgreementException);
+
+            ConflictObjectResult expectedConflictObjectResult =
+                Conflict(alreadyExistsSubscriberAgreementException);
+
+            var expectedActionResult =
+                new ActionResult<SubscriberAgreement>(expectedConflictObjectResult);
+
+            this.subscriberAgreementServiceMock.Setup(service =>
+                service.ModifySubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()))
+                    .ThrowsAsync(addressDependencyValidationException);
+
+            // when
+            ActionResult<SubscriberAgreement> actualActionResult =
+                await this.subscriberAgreementsController.PutSubscriberAgreementAsync(someSubscriberAgreement);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.subscriberAgreementServiceMock.Verify(service =>
+                service.ModifySubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()),
+                    Times.Once);
+
+            this.subscriberAgreementServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnPutIfServerErrorOccurredAsync(
+            Xeption validationException)
+        {
+            // given
+            SubscriberAgreement someSubscriberAgreement = CreateRandomSubscriberAgreement();
+
+            InternalServerErrorObjectResult expectedBadRequestObjectResult =
+                InternalServerError(validationException);
+
+            var expectedActionResult =
+                new ActionResult<SubscriberAgreement>(expectedBadRequestObjectResult);
+
+            this.subscriberAgreementServiceMock.Setup(service =>
+                service.ModifySubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<SubscriberAgreement> actualActionResult =
+                await this.subscriberAgreementsController.PutSubscriberAgreementAsync(someSubscriberAgreement);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.subscriberAgreementServiceMock.Verify(service =>
+                service.ModifySubscriberAgreementAsync(It.IsAny<SubscriberAgreement>()),
+                    Times.Once);
 
             this.subscriberAgreementServiceMock.VerifyNoOtherCalls();
         }

d84bbd310d644e9f9c605468599665bd950f1f81
Hassan Raza <h_raza110@outlook.com>
2025-02-21 12:02:55 +0000

    Adding tests

---
+++ LHDS.AdminPortal.Api.Tests.Unit/Controllers/SubscriberAgreements/SubscriberAgreementControllerTests.Logic.GetAll.cs
@@ -0,0 +1,50 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LHDS.Core.Models.Foundations.SubscriberAgreements;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
+using Xunit;
+
+namespace LHDS.AdminPortal.Api.Tests.Unit.Controllers.SubscriberAgreements
+{
+    public partial class SubscriberAgreementControllerTests
+    {
+        [Fact]
+        public async Task ShouldReturnAddressesOnGetAsync()
+        {
+            // given 
+            IQueryable<Address> randomAddresses = CreateRandomAddresses();
+            IQueryable<Address> storageAddresses = randomAddresses.DeepClone();
+            IQueryable<Address> expectedAddresses = storageAddresses.DeepClone();
+            var expectedObjectResult = new OkObjectResult(expectedAddresses);
+
+            var expectedActionResult =
+                new ActionResult<IQueryable<Address>>(expectedObjectResult);
+
+            this.addressServiceMock.Setup(service =>
+                service.RetrieveAllAddressesAsync())
+                    .ReturnsAsync(expectedAddresses);
+
+            // when
+            ActionResult<IQueryable<Address>> actualActionResult =
+                await this.addressesController.GetAllAddressesAsync();
+
+            // then
+            actualActionResult.Should().BeEquivalentTo(expectedActionResult);
+
+            this.addressServiceMock.Verify(service =>
+                service.RetrieveAllAddressesAsync(),
+                    Times.Once());
+
+            this.addressServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}

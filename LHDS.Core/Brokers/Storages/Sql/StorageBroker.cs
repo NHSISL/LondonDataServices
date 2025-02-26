80545954d72b43e23fb243cdcade9b183cefa0e0
Christo du Toit <christo@dutoit.co.uk>
2025-02-26 15:53:54 +0000

    CODE RUB: code cleanup

--- LHDS.Core/Brokers/Storages/Sql/StorageBroker.cs
+++ LHDS.Core/Brokers/Storages/Sql/StorageBroker.cs
@@ -56,9 +56,6 @@ namespace LHDS.Core.Brokers.Storages.Sql
             optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
             string connectionString = this.configuration.GetConnectionString(name: "DefaultConnection");
             optionsBuilder.UseSqlServer(connectionString);
-
-            optionsBuilder.ConfigureWarnings(warnings =>
-                warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
         }
 
         private IQueryable<T> SelectAll<T>() where T : class => this.Set<T>();

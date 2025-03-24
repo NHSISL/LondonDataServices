3e86a3268cf66d97a7dc849657051cb8552e619a
Hassan Raza <h_raza110@outlook.com>
2025-03-24 15:38:30 +0000

    ShouldAddIngestionTrackingAuditAsync -> FAIL

--- LHDS.Core/Services/Foundations/IngestionTrackingAudits/IngestionTrackingAuditService.cs
+++ LHDS.Core/Services/Foundations/IngestionTrackingAudits/IngestionTrackingAuditService.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.DateTimes;
 using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Brokers.Securities;
 using LHDS.Core.Brokers.Storages.Sql;
 using LHDS.Core.Models.Foundations.IngestionTrackingAudits;
 
@@ -16,15 +17,18 @@ namespace LHDS.Core.Services.Foundations.IngestionTrackingAudits
     {
         private readonly IStorageBroker storageBroker;
         private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ISecurityBroker securityBroker;
         private readonly ILoggingBroker loggingBroker;
 
         public IngestionTrackingAuditService(
             IStorageBroker storageBroker,
             IDateTimeBroker dateTimeBroker,
+            ISecurityBroker securityBroker,
             ILoggingBroker loggingBroker)
         {
             this.storageBroker = storageBroker;
             this.dateTimeBroker = dateTimeBroker;
+            this.securityBroker = securityBroker;
             this.loggingBroker = loggingBroker;
         }
 

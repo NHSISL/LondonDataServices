896101ca7aa7f351b7e83c8c88ca622b03657fc3
Hassan Raza <h_raza110@outlook.com>
2025-03-24 14:24:48 +0000

    ShouldThrowValidationExceptionOnModifyIfTerminologyArtifactIsInvalidAndLogItAsync ->  FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/TerminologyArtifacts/TerminologyArtifactServiceTests.Logic.Modify.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/TerminologyArtifacts/TerminologyArtifactServiceTests.Logic.Modify.cs
@@ -71,9 +71,10 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.TerminologyArtifacts
                 broker.UpdateTerminologyArtifactAsync(inputTerminologyArtifact),
                     Times.Once);
 
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.securityBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }
 }

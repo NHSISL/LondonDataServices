8564a1426e5089c264ff1cea9ec7fb7b62bb76ee
Hassan Raza <h_raza110@outlook.com>
2025-03-24 13:18:22 +0000

    ShouldAddTerminologyArtifactAsync -> FAIL

--- LHDS.Core.Tests.Unit/Services/Foundations/TerminologyArtifacts/TerminologyArtifactServiceTests.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/TerminologyArtifacts/TerminologyArtifactServiceTests.cs
@@ -3,12 +3,15 @@
 // ---------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using LHDS.Core.Brokers.DateTimes;
 using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Brokers.Securities;
 using LHDS.Core.Brokers.Storages.Sql;
+using LHDS.Core.Models.Brokers.Securities;
 using LHDS.Core.Models.Foundations.TerminologyArtifacts;
 using LHDS.Core.Services.Foundations.TerminologyArtifacts;
 using Microsoft.Data.SqlClient;
@@ -23,6 +26,7 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.TerminologyArtifacts
     {
         private readonly Mock<IStorageBroker> storageBrokerMock;
         private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ISecurityBroker> securityBrokerMock;
         private readonly Mock<ILoggingBroker> loggingBrokerMock;
         private readonly ITerminologyArtifactService terminologyArtifactService;
 
@@ -30,11 +34,13 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.TerminologyArtifacts
         {
             this.storageBrokerMock = new Mock<IStorageBroker>();
             this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.securityBrokerMock = new Mock<ISecurityBroker>();
             this.loggingBrokerMock = new Mock<ILoggingBroker>();
 
             this.terminologyArtifactService = new TerminologyArtifactService(
                 storageBroker: this.storageBrokerMock.Object,
                 dateTimeBroker: this.dateTimeBrokerMock.Object,
+                securityBroker: this.securityBrokerMock.Object,
                 loggingBroker: this.loggingBrokerMock.Object);
         }
 
@@ -43,6 +49,13 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.TerminologyArtifacts
 
         private static string GetRandomString() =>
             new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static string GetRandomStringWithLengthOf(int length)
+        {
+            string result = new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
+            return result.Length > length ? result.Substring(0, length) : result;
+        }
 
         public static TheoryData<int> MinutesBeforeOrAfter()
         {
@@ -104,6 +117,46 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.TerminologyArtifacts
                 .OnProperty(terminologyArtifact => terminologyArtifact.UpdatedBy).Use(user);
 
             return filler;
+        }
+
+        private static TerminologyArtifact CreateRandomTerminologyArtifact(
+            DateTimeOffset dateTimeOffset,
+            string userId) =>
+            CreateTerminologyArtifactFiller(dateTimeOffset, userId).Create();
+
+        private static Filler<TerminologyArtifact> CreateTerminologyArtifactFiller(
+            DateTimeOffset dateTimeOffset,
+            string userId)
+        {
+            string user = Guid.NewGuid().ToString();
+            var filler = new Filler<TerminologyArtifact>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(terminologyArtifact => terminologyArtifact.CreatedBy).Use(userId)
+                .OnProperty(terminologyArtifact => terminologyArtifact.UpdatedBy).Use(userId);
+
+            return filler;
+        }
+
+        private EntraUser CreateRandomEntraUser(string entraUserId = "")
+        {
+            var userId = string.IsNullOrWhiteSpace(entraUserId) ? GetRandomStringWithLengthOf(255) : entraUserId;
+
+            return new EntraUser(
+                entraUserId: userId,
+                givenName: GetRandomString(),
+                surname: GetRandomString(),
+                displayName: GetRandomString(),
+                email: GetRandomString(),
+                jobTitle: GetRandomString(),
+                roles: new List<string> { GetRandomString() },
+
+                claims: new List<System.Security.Claims.Claim>
+                {
+                    new System.Security.Claims.Claim(type: GetRandomString(), value: GetRandomString())
+                });
         }
     }
 }

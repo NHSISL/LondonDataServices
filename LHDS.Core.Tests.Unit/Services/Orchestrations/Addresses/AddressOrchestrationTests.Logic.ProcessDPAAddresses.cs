ecda466cdf1c5fddeddc61affaa3e994744990f4
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-04-02 16:12:20 +0100

    ShouldProcessDPAAddressesAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/Addresses/AddressOrchestrationTests.Logic.ProcessDPAAddresses.cs
@@ -0,0 +1,80 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Force.DeepCloner;
+using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Services.Orchestrations.Addresses;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Orchestrations.Addresses
+{
+    public partial class AddressOrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldProcessDPAAddressesAsync()
+        {
+            // Given
+            var addressOrchestrationServiceMock = new Mock<AddressOrchestrationService>
+               (this.addressProcessingServiceMock.Object,
+                this.assignProcessingServiceMock.Object,
+                this.fileBrokerMock.Object,
+                this.csvHelperBrokerMock.Object,
+                this.dateTimeBrokerMock.Object,
+                this.auditBrokerMock.Object,
+                this.loggingBrokerMock.Object)
+            { CallBase = true };
+
+            string randomFile = GetRandomString();
+            string inputDpaCsvFile = randomFile;
+            IQueryable<Address> randomExistingAddresses = CreateRandomAddresses(2);
+            IQueryable<Address> existingAddresses = randomExistingAddresses.DeepClone();
+            List<Address> randomAddressList = CreateRandomAddresses(2).ToList();
+            List<Address> newAddresses = randomAddressList.DeepClone();
+            List<Address> dpaFileAddresses = [.. existingAddresses.DeepClone(), .. newAddresses.DeepClone()];
+
+            addressOrchestrationServiceMock.Setup(service =>
+                service.MapDPADataToAddresses(inputDpaCsvFile))
+                    .ReturnsAsync(dpaFileAddresses);
+
+            this.addressProcessingServiceMock.Setup(service =>
+                service.RetrieveAllAddressesAsync())
+                    .ReturnsAsync(existingAddresses);
+
+            AddressOrchestrationService service = addressOrchestrationServiceMock.Object;
+
+            // When
+            await service.ProcessDPAAddressesAsync(inputDpaCsvFile);
+
+            addressOrchestrationServiceMock.Verify(service =>
+                service.MapDPADataToAddresses(inputDpaCsvFile),
+                    Times.Once());
+
+            this.addressProcessingServiceMock.Verify(service =>
+                service.RetrieveAllAddressesAsync(),
+                    Times.Once);
+
+            this.addressProcessingServiceMock.Verify(service =>
+                service.BulkAddAddressesAsync(It.Is(SameAddressesAs(newAddresses)), inputDpaCsvFile),
+                    Times.Once);
+
+            this.addressProcessingServiceMock.Verify(service =>
+                service.BulkModifyAddressesAsync(It.Is(SameAddressesAs(existingAddresses.ToList())), inputDpaCsvFile),
+                    Times.Once);
+
+            this.fileBrokerMock.VerifyNoOtherCalls();
+            this.csvHelperBrokerMock.VerifyNoOtherCalls();
+            this.auditBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.addressProcessingServiceMock.VerifyNoOtherCalls();
+            this.assignProcessingServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
+

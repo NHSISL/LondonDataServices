512a4bc59b57e589524658b20b7bc0d028523ba3
Christo du Toit <christo.dutoit@nhs.net>
2025-04-02 13:06:16 +0100

    ShouldThrowServiceExceptionOnBulkModifyIfServiceErrorOccursAndLogItAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/Addresses/AddressServiceTests.Exceptions.BulkModify.cs
@@ -0,0 +1,158 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Brokers.Securities;
+using LHDS.Core.Models.Foundations.Addresses;
+using LHDS.Core.Models.Foundations.Addresses.Exceptions;
+using LHDS.Core.Services.Foundations.Addresses;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Addresses
+{
+    public partial class AddressServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowAggregateServiceExceptionOnBulkModifyIfErrorOccursAndLogItAsync()
+        {
+            // given
+            EntraUser randomEntraUser = CreateRandomEntraUser();
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+            string randomFileName = GetRandomString();
+            string inputFileName = randomFileName;
+
+            List<Address> someAddresses = new List<Address>
+                {
+                    CreateRandomAddress(randomDateTimeOffset, randomEntraUser.EntraUserId),
+                    CreateRandomAddress(randomDateTimeOffset, randomEntraUser.EntraUserId)
+                };
+
+            List<Address> inputAddresses = someAddresses;
+            string someFileName = GetRandomString();
+            Exception exception = new Exception("Some exception");
+
+            var aggregateException =
+                new AggregateException(
+                    message: $"Unable to process addresses in 1 of the batch(es) from {someFileName}",
+                    innerExceptions: exception);
+
+            var failedAddressServiceException =
+                new FailedAddressServiceException(
+                    message: "Failed aggregate address service error occurred, please contact support.",
+                    innerException: aggregateException);
+
+            var expectedAddressServiceException =
+                new AddressServiceException(
+                    message: "Address service error occurred, please contact support.",
+                    innerException: failedAddressServiceException);
+
+            var addressServiceMock = new Mock<AddressService>(
+                this.storageBrokerMock.Object,
+                this.dateTimeBrokerMock.Object,
+                this.securityBrokerMock.Object,
+                this.identifierBrokerMock.Object,
+                this.loggingBrokerMock.Object,
+                this.auditBrokerMock.Object)
+            {
+                CallBase = true
+            };
+
+            addressServiceMock.Setup(service =>
+                service.BulkModifyBatch(It.IsAny<List<Address>>(), It.IsAny<string>(), It.IsAny<int>()))
+                    .ThrowsAsync(aggregateException);
+
+            // when
+            ValueTask bulkModifyAddressTask = addressServiceMock.Object
+                .BulkModifyAddressesAsync(addresses: someAddresses, fileName: someFileName);
+
+            AddressServiceException actualAddressDependencyException =
+                await Assert.ThrowsAsync<AddressServiceException>(
+                    bulkModifyAddressTask.AsTask);
+
+            // then
+            actualAddressDependencyException.Should()
+                .BeEquivalentTo(expectedAddressServiceException);
+
+            addressServiceMock.Verify(service =>
+                service.BulkModifyBatch(It.IsAny<List<Address>>(), It.IsAny<string>(), It.IsAny<int>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedAddressServiceException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnBulkModifyIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            var serviceException = new Exception();
+
+            var addressServiceMock = new Mock<AddressService>(
+                this.storageBrokerMock.Object,
+                this.dateTimeBrokerMock.Object,
+                this.securityBrokerMock.Object,
+                this.identifierBrokerMock.Object,
+                this.loggingBrokerMock.Object,
+                this.auditBrokerMock.Object)
+            {
+                CallBase = true
+            };
+
+            addressServiceMock.Setup(service =>
+                service.BulkModifyBatch(It.IsAny<List<Address>>(), It.IsAny<string>(), It.IsAny<int>()))
+                    .ThrowsAsync(serviceException);
+
+            AddressService addressService = addressServiceMock.Object;
+
+            string someFileName = GetRandomString();
+            List<Address> someAddresses = new List<Address> { CreateRandomAddress() };
+
+            var failedAddressServiceException =
+                new FailedAddressServiceException(
+                    message: "Failed address service error occurred, please contact support.",
+                    innerException: serviceException);
+
+            var expectedAddressServiceException =
+                new AddressServiceException(
+                    message: "Address service error occurred, please contact support.",
+                    innerException: failedAddressServiceException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask addAddressTask = addressServiceMock.Object
+                .BulkModifyAddressesAsync(addresses: someAddresses, fileName: someFileName);
+
+            AddressServiceException actualAddressServiceException =
+                await Assert.ThrowsAsync<AddressServiceException>(
+                    addAddressTask.AsTask);
+
+            // then
+            actualAddressServiceException.Should()
+                .BeEquivalentTo(expectedAddressServiceException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedAddressServiceException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

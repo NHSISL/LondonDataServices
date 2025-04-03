fe9a6a83a0b6e752b6de3074f0551a9ddbaacebe
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-04-03 14:16:13 +0100

    CODE RUB: Code cleanup

--- LHDS.Core.Tests.Unit/Services/Orchestrations/Addresses/AddressOrchestrationTests.Logic.MapLPIDataToAddresses.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/Addresses/AddressOrchestrationTests.Logic.MapLPIDataToAddresses.cs
@@ -77,6 +77,10 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.Addresses
             bool hasHeaderRecord = false;
 
             this.fileBrokerMock.Setup(service =>
+                service.CheckIfFileExistsAsync(inputCsvFilePath))
+                    .ReturnsAsync(true);
+
+            this.fileBrokerMock.Setup(service =>
                 service.ReadFileAsync(inputCsvFilePath))
                     .ReturnsAsync(csvData);
 
@@ -98,6 +102,10 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.Addresses
 
             // Then
             actualAddresses.Should().BeEquivalentTo(expectedAddresses);
+
+            this.fileBrokerMock.Verify(service =>
+                service.CheckIfFileExistsAsync(inputCsvFilePath),
+                    Times.Once);
 
             this.fileBrokerMock.Verify(service =>
                 service.ReadFileAsync(inputCsvFilePath),

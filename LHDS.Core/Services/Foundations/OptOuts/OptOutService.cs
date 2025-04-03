6b45893912827f7e29ed4cbb50fb59036fd7b96a
hassaero110 <hassan.raza3@nhs.net>
2025-04-03 12:35:02 +0100

    wip

--- LHDS.Core/Services/Foundations/OptOuts/OptOutService.cs
+++ LHDS.Core/Services/Foundations/OptOuts/OptOutService.cs
@@ -7,8 +7,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using LHDS.Core.Brokers.DateTimes;
 using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Brokers.Securities;
 using LHDS.Core.Brokers.Storages.Sql;
 using LHDS.Core.Models.Foundations.OptOuts;
+using Org.BouncyCastle.Crypto;
 
 namespace LHDS.Core.Services.Foundations.OptOuts
 {
@@ -16,15 +18,18 @@ namespace LHDS.Core.Services.Foundations.OptOuts
     {
         private readonly IStorageBroker storageBroker;
         private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ISecurityBroker securityBroker;
         private readonly ILoggingBroker loggingBroker;
 
         public OptOutService(
             IStorageBroker storageBroker,
             IDateTimeBroker dateTimeBroker,
+            ISecurityBroker securityBroker,
             ILoggingBroker loggingBroker)
         {
             this.storageBroker = storageBroker;
             this.dateTimeBroker = dateTimeBroker;
+            this.securityBroker = securityBroker;
             this.loggingBroker = loggingBroker;
         }
 

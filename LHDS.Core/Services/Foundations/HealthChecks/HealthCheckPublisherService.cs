db9736f37864ab9aa03b67bd0c03207f11f47bab
Christo du Toit <christo@dutoit.co.uk>
2025-04-23 16:32:53 +0100

    FOUNDATIONS: HealthCheckPublisherService

---
+++ LHDS.Core/Services/Foundations/HealthChecks/HealthCheckPublisherService.cs
@@ -0,0 +1,42 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading;
+using System.Threading.Tasks;
+using LHDS.Core.Brokers.Telemetries;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace LHDS.Core.Services.Foundations.HealthChecks
+{
+    public class HealthCheckPublisherService : IHealthCheckPublisher
+    {
+        private readonly ITelemetryBroker telemetryBroker;
+
+        public HealthCheckPublisherService(ITelemetryBroker telemetryBroker)
+        {
+            this.telemetryBroker = telemetryBroker;
+        }
+
+        public async Task PublishAsync(HealthReport report, CancellationToken cancellationToken)
+        {
+            foreach (var entry in report.Entries)
+            {
+                var eventTelemetry = new EventTelemetry(entry.Key);
+                eventTelemetry.Properties.Add("Status", report.Status.ToString());
+
+                foreach (var reading in entry.Value.Data)
+                {
+                    var metric = new MetricTelemetry();
+                    metric.Name = reading.Key;
+                    metric.Sum = double.Parse(reading.Value.ToString());
+                    eventTelemetry.Metrics.Add(metric.Name, metric.Sum);
+                    await telemetryBroker.TrackMetricAsync(metric);
+                }
+
+                await telemetryBroker.TrackEventAsync(eventTelemetry);
+            }
+        }
+    }
+}

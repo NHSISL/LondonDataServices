1520a50a1e157a156ac139502a1ab5cf057e6a13
Christo du Toit <christo@dutoit.co.uk>
2025-04-24 14:41:04 +0100

    CODE RUB: DI registrations for health checks

---
+++ LHDS.Core/Services/Foundations/HealthChecks/Checks/IngestionTracking/IngestionTrackingDecryptionHealthCheckService.cs
@@ -0,0 +1,71 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Storages.Sql;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace LHDS.Core.Services.Foundations.HealthChecks.Checks.IngestionTracking
+{
+    public class IngestionTrackingDecryptionHealthCheckService : IHealthCheck
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly IConfiguration configuration;
+        private readonly IDateTimeBroker dateTimeBroker;
+
+        public IngestionTrackingDecryptionHealthCheckService(
+            IStorageBroker storageBroker,
+            IConfiguration configuration,
+            IDateTimeBroker dateTimeBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.configuration = configuration;
+            this.dateTimeBroker = dateTimeBroker;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            DateTimeOffset currentDateTime = await this.dateTimeBroker.GetCurrentDateTimeOffsetAsync();
+            int thresholdMinutes = this.configuration.GetValue<int>("IngestionTracking:ThresholdMinutes", 1440);
+            DateTimeOffset thresholdDateTime = currentDateTime.AddMinutes(-1 * thresholdMinutes);
+            var ingestionTrackingQuery = await this.storageBroker.SelectAllIngestionTrackingsAsync();
+
+            var decryptionSlowOrStuckCount = ingestionTrackingQuery
+                .Count(ingestionTracking => ingestionTracking.Decrypted == false
+                    && ingestionTracking.IsProcessing == false
+                        && ingestionTracking.UpdatedDate <= thresholdDateTime);
+
+            var vals = new Dictionary<string, object>
+            {
+                { "QueueLength", decryptionSlowOrStuckCount },
+                { "ThresholdMinutes", thresholdMinutes },
+
+                { "Message",
+                  $"{decryptionSlowOrStuckCount} files have not be decrypted within " +
+                    $"the {thresholdMinutes} minute threshold.  Please check that the function " +
+                        $"is running and logs for any issues."
+                },
+            };
+
+            if (decryptionSlowOrStuckCount > 0 || stuckInProcessingCount > 0)
+            {
+                return HealthCheckResult.Degraded(
+                    description: "Ingestion Tracking - Decryption is slow or stuck.",
+                    data: vals);
+            }
+            else
+            {
+                return HealthCheckResult.Healthy(
+                    description: "Ingestion Tracking - Decryption is healthy.",
+                    data: vals);
+            }
+        }
+    }
+}
---
+++ LHDS.Core/Services/Foundations/HealthChecks/HealthCheckPublisherService.Exceptions.cs
@@ -0,0 +1,46 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.HealthChecks.Exceptions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Xeptions;
+
+namespace LHDS.Core.Services.Foundations.HealthChecks
+{
+    public partial class HealthCheckPublisherService : IHealthCheckPublisher
+    {
+        private delegate Task ReturningNothingFunction();
+
+        private async Task TryCatch(ReturningNothingFunction returningNothingFunction)
+        {
+            try
+            {
+                await returningNothingFunction();
+            }
+            catch (Exception exception)
+            {
+                var failedHealthCheckPublisherServiceException =
+                    new FailedHealthCheckPublisherServiceException(
+                        message: "Failed health check publisher service error occurred, please contact support.",
+                        innerException: exception);
+
+                throw await CreateAndLogServiceExceptionAsync(failedHealthCheckPublisherServiceException);
+            }
+        }
+
+        private async ValueTask<HealthCheckPublisherServiceException> CreateAndLogServiceExceptionAsync(Xeption exception)
+        {
+            var healthCheckPublisherServiceException =
+                new HealthCheckPublisherServiceException(
+                    message: "Health check publisher service error occurred, please contact support.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogCriticalAsync(healthCheckPublisherServiceException);
+
+            return healthCheckPublisherServiceException;
+        }
+    }
+}

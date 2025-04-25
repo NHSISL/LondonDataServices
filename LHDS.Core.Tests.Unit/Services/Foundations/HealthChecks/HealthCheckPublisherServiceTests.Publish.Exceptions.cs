6631f9fe060040a3c067e385f50ce4f6a6f9de86
Christo du Toit <christo@dutoit.co.uk>
2025-04-25 11:00:31 +0100

    ShouldPublishAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/HealthChecks/HealthCheckPublisherServiceTests.Publish.Exceptions.cs
@@ -0,0 +1,10 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.HealthChecks
+{
+    public partial class HealthCheckPublisherServiceTests
+    {
+    }
+}

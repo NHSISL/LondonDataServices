be9796d2eafa54d9410a9c11e1e5c3f3b8533daa
davidhayes03 <david.hayes17@nhs.net>
2025-05-08 10:52:46 +0100

    CODE RUB: Remove Unused Columns

--- LHDS.AdminPortal.Web/ClientApp/src/components/ingestionTracking/ingestionTrackingRowView.tsx
+++ LHDS.AdminPortal.Web/ClientApp/src/components/ingestionTracking/ingestionTrackingRowView.tsx
@@ -5,19 +5,16 @@ import TableBaseRow from "../bases/components/Table/TableBase.Row";
 import TableBaseData from "../bases/components/Table/TableBase.Data";
 import { IngestionTracking } from "../../models/ingestionTrackings/ingestionTracking";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileDownload, faFileExport, faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
-import { Dropdown } from "react-bootstrap";
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 import moment from "moment";
 
 type IngestionTrackingRowProps = {
-    onEncryptedDownload: (ingestionTracking: IngestionTracking) => void;
     onReDecrypted: (ingestionTracking: IngestionTracking) => void;
     ingestionTracking: IngestionTracking;
 }
 
 const IngestionTrackingRow: FunctionComponent<IngestionTrackingRowProps> = (props) => {
     const {
-        onEncryptedDownload,
         onReDecrypted,
         ingestionTracking
     } = props;
@@ -54,33 +51,6 @@ const IngestionTrackingRow: FunctionComponent<IngestionTrackingRowProps> = (prop
                 <strong>Created Date: </strong><br />
                 {moment(ingestionTracking.createdDate?.toString()).format("Do-MMM-yyyy HH:mm")}
             </TableBaseData>
-
-
-            {(ingestionTracking.supplier?.canDownloadIngestionTracking
-                || ingestionTracking.supplier?.canRelandIngestionTracking) && (
-                <>
-                    <TableBaseData>
-                        <Dropdown>
-                            <Dropdown.Toggle as={ButtonBase} variant="secondary" id="actions-dropdown" edit>
-                                Actions
-                            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                <Dropdown.Item onClick={() => onEncryptedDownload(ingestionTracking)}
-                                    style={{ color: "#121212" }}>
-                                    <FontAwesomeIcon icon={faFileDownload} />  &nbsp;Download Encrypted File
-                                </Dropdown.Item>
-                                {ingestionTracking.decrypted && (
-                                    <Dropdown.Item onClick={() => onReDecrypted(ingestionTracking)}
-                                        style={{ color: "#121212" }}>
-                                        <FontAwesomeIcon icon={faFileExport} />  Re-Decrypt File
-                                    </Dropdown.Item>
-                                )}
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </TableBaseData>
-                </>
-            )}
 
             <TableBaseData>
                 <Link to={`/ingestionTrackingDetail/${ingestionTracking.id}`}>

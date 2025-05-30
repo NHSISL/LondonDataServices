c30b61bc2a0cbd628e0364e3bc9eeda1916e6d8d
Joe Owens <joe.owens@answerdigital.com>
2025-05-30 16:43:57 +0100

    ShouldThrowServiceExceptionOnGetHealthStatusIfServiceErrorOccursAndLogItAsync -> PASS

---
+++ LHDS.Core/Models/Coordinations/HealthChecks/Exceptions/FailedIngestionTrackingFailedToProcessHealthCheckServiceException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Coordinations.HealthChecks.Exceptions
+{
+    public class FailedIngestionTrackingFailedToProcessHealthCheckServiceException : Xeption
+    {
+        public FailedIngestionTrackingFailedToProcessHealthCheckServiceException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
---
+++ LHDS.Core/Models/Coordinations/HealthChecks/Exceptions/IngestionTrackingFailedToProcessHealthCheckServiceException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Coordinations.HealthChecks.Exceptions
+{
+    public class IngestionTrackingFailedToProcessHealthCheckServiceException : Xeption
+    {
+        public IngestionTrackingFailedToProcessHealthCheckServiceException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}

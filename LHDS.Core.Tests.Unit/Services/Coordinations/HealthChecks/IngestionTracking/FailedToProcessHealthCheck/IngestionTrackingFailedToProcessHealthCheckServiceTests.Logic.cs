7123456253005a15dfac353292cff0a3d64ed4d5
Joe Owens <joe.owens@answerdigital.com>
2025-05-30 15:04:56 +0100

    COORDINATIONS: ShouldGetHealthStatusAsyncUnhealthyWhenAnyUnhealthy -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Coordinations/HealthChecks/IngestionTracking/FailedToProcessHealthCheck/IngestionTrackingFailedToProcessHealthCheckServiceTests.Logic.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Brokers.Storages.Sql;
+using LHDS.Core.Services.Foundations.HealthChecks.IngestionTracking;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Moq;
+using Tynamix.ObjectFiller;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Coordinations.HealthChecks.IngestionTracking.FailedToProcessHealthCheck
+{
+    public partial class IngestionTrackingFailedToProcessHealthCheckServiceTests
+    {
+        [Fact]
+        public async Task ShouldGetHealthStatusAsyncUnhealthyWhenAnyUnhealthy()
+        {
+            // given
+            DateTimeOffset currentDateTime = DateTimeOffset.UtcNow;
+
+            IQueryable<Core.Models.Foundations.IngestionTrackings.IngestionTracking> randomUnhealthyTrackings =
+                CreateRandomUnhealthyIngestionTrackings();
+
+            IQueryable<Core.Models.Foundations.IngestionTrackings.IngestionTracking> randomHealthyTrackings =
+                CreateRandomHealthyIngestionTrackings();
+
+            IQueryable<Core.Models.Foundations.IngestionTrackings.IngestionTracking> randomTrackings =
+                randomUnhealthyTrackings.Concat(randomHealthyTrackings);
+
+            Dictionary<string, object> healthCheckResultValues =
+                GetHealthCheckResultValues(
+                    currentDateTime, 
+                    HealthStatus.Unhealthy, 
+                    unhealthyItemsCount: randomUnhealthyTrackings.Count(), 
+                    healthyItems: randomHealthyTrackings.Count()
+                );
+
+            var expectedHealthCheckResult = HealthCheckResult.Unhealthy(description: CheckName, data: healthCheckResultValues);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ReturnsAsync(currentDateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllIngestionTrackingsAsync())
+                    .ReturnsAsync(randomTrackings);
+
+            // when
+            var result = await this.ingestionTrackingFailedToProcessHealthCheckService.GetHealthStatusAsync();
+
+            // then
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllIngestionTrackingsAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

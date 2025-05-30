9a1a57658dd083b4e76a0e60c6e9e998ff40e8cc
Joe Owens <joe.owens@answerdigital.com>
2025-05-30 16:49:16 +0100

    ShouldThrowServiceExceptionOnGetHealthStatusIfServiceErrorOccursAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/HealthChecks/IngestionTracking/IngestionTrackingFailedToProcessHealthCheckService.cs
+++ LHDS.Core/Services/Foundations/HealthChecks/IngestionTracking/IngestionTrackingFailedToProcessHealthCheckService.cs
@@ -14,7 +14,7 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace LHDS.Core.Services.Foundations.HealthChecks.IngestionTracking
 {
-    public class IngestionTrackingFailedToProcessHealthCheckService : IIngestionTrackingHealthItemService
+    public partial class IngestionTrackingFailedToProcessHealthCheckService : IIngestionTrackingHealthItemService
     {
         private readonly IStorageBroker storageBroker;
         private readonly IConfiguration configuration;
@@ -34,75 +34,76 @@ namespace LHDS.Core.Services.Foundations.HealthChecks.IngestionTracking
             this.loggingBroker = loggingBroker;
         }
 
-        public async ValueTask<HealthCheckResult> GetHealthStatusAsync()
-        {
-            int retryCount = configuration
-                .GetValue("HealthChecks:IngestionTracking:FailedToProcess:RetryCount", 3);
-
-            int degradedThresholdMinutes = configuration
-                .GetValue("HealthChecks:IngestionTracking:FailedToProcess:DegradedThreshold", 1440);
-
-            int unHealthyThresholdMinutes = configuration
-                .GetValue("HealthChecks:IngestionTracking:FailedToProcess:UnHealthyThreshold", 2880);
-
-            DateTimeOffset currentDateTime = await dateTimeBroker.GetCurrentDateTimeOffsetAsync();
-            DateTimeOffset degradedThresholdDateTime = currentDateTime.AddMinutes(-1 * degradedThresholdMinutes);
-            DateTimeOffset unHealthyThresholdDateTime = currentDateTime.AddMinutes(-1 * unHealthyThresholdMinutes);
-            var ingestionTrackingQuery = await storageBroker.SelectAllIngestionTrackingsAsync();
-            var filteredQuery = ingestionTrackingQuery.Where(i => i.RetryCount >= retryCount);
-
-            int baseCount = filteredQuery.Count(ingestionTracking =>
-                ingestionTracking.UpdatedDate > degradedThresholdDateTime);
-
-            int degradedCount = filteredQuery.Count(ingestionTracking =>
-                ingestionTracking.UpdatedDate <= degradedThresholdDateTime &&
-                ingestionTracking.UpdatedDate > unHealthyThresholdDateTime);
-
-            int unHealthyCount = filteredQuery
-                .Count(ingestionTracking => ingestionTracking.UpdatedDate <= unHealthyThresholdDateTime);
-
-            int totalCount = baseCount + degradedCount + unHealthyCount;
-
-            string message = totalCount == 0
-                ? $"Nothing to process. All up to date."
-                : $"{totalCount} files have not been processed. Please check logs and function status.";
-
-            var vals = new Dictionary<string, object>
+        public ValueTask<HealthCheckResult> GetHealthStatusAsync() =>
+            TryCatch(async () =>
             {
-                { "description", "Failed To Process" },
-                { "failedToProcess", totalCount },
-                { "degradedItems", degradedCount},
-                { "unHealthyItems", unHealthyCount},
-                { "degradedThresholdMinutes", degradedThresholdMinutes.ToString() },
-                { "unHealthyThresholdMinutes", unHealthyThresholdMinutes.ToString() },
-                { "checkedAt", currentDateTime.ToString("o") },
-                { "message", message }
-            };
+                int retryCount = configuration
+                    .GetValue("HealthChecks:IngestionTracking:FailedToProcess:RetryCount", 3);
 
-            if (unHealthyCount > 0)
-            {
-                vals.Add("status", HealthStatus.Unhealthy.ToString());
+                int degradedThresholdMinutes = configuration
+                    .GetValue("HealthChecks:IngestionTracking:FailedToProcess:DegradedThreshold", 1440);
 
-                return HealthCheckResult.Unhealthy(
-                    description: CheckName,
-                    data: vals);
-            }
-            else if (degradedCount > 0)
-            {
-                vals.Add("status", HealthStatus.Degraded.ToString());
+                int unHealthyThresholdMinutes = configuration
+                    .GetValue("HealthChecks:IngestionTracking:FailedToProcess:UnHealthyThreshold", 2880);
 
-                return HealthCheckResult.Degraded(
-                    description: CheckName,
-                    data: vals);
-            }
-            else
-            {
-                vals.Add("status", HealthStatus.Healthy.ToString());
+                DateTimeOffset currentDateTime = await dateTimeBroker.GetCurrentDateTimeOffsetAsync();
+                DateTimeOffset degradedThresholdDateTime = currentDateTime.AddMinutes(-1 * degradedThresholdMinutes);
+                DateTimeOffset unHealthyThresholdDateTime = currentDateTime.AddMinutes(-1 * unHealthyThresholdMinutes);
+                var ingestionTrackingQuery = await storageBroker.SelectAllIngestionTrackingsAsync();
+                var filteredQuery = ingestionTrackingQuery.Where(i => i.RetryCount >= retryCount);
 
-                return HealthCheckResult.Healthy(
-                    description: CheckName,
-                    data: vals);
-            }
-        }
+                int baseCount = filteredQuery.Count(ingestionTracking =>
+                    ingestionTracking.UpdatedDate > degradedThresholdDateTime);
+
+                int degradedCount = filteredQuery.Count(ingestionTracking =>
+                    ingestionTracking.UpdatedDate <= degradedThresholdDateTime &&
+                    ingestionTracking.UpdatedDate > unHealthyThresholdDateTime);
+
+                int unHealthyCount = filteredQuery
+                    .Count(ingestionTracking => ingestionTracking.UpdatedDate <= unHealthyThresholdDateTime);
+
+                int totalCount = baseCount + degradedCount + unHealthyCount;
+
+                string message = totalCount == 0
+                    ? $"Nothing to process. All up to date."
+                    : $"{totalCount} files have not been processed. Please check logs and function status.";
+
+                var vals = new Dictionary<string, object>
+                {
+                    { "description", "Failed To Process" },
+                    { "failedToProcess", totalCount },
+                    { "degradedItems", degradedCount},
+                    { "unHealthyItems", unHealthyCount},
+                    { "degradedThresholdMinutes", degradedThresholdMinutes.ToString() },
+                    { "unHealthyThresholdMinutes", unHealthyThresholdMinutes.ToString() },
+                    { "checkedAt", currentDateTime.ToString("o") },
+                    { "message", message }
+                };
+
+                if (unHealthyCount > 0)
+                {
+                    vals.Add("status", HealthStatus.Unhealthy.ToString());
+
+                    return HealthCheckResult.Unhealthy(
+                        description: CheckName,
+                        data: vals);
+                }
+                else if (degradedCount > 0)
+                {
+                    vals.Add("status", HealthStatus.Degraded.ToString());
+
+                    return HealthCheckResult.Degraded(
+                        description: CheckName,
+                        data: vals);
+                }
+                else
+                {
+                    vals.Add("status", HealthStatus.Healthy.ToString());
+
+                    return HealthCheckResult.Healthy(
+                        description: CheckName,
+                        data: vals);
+                }
+            });
     }
 }

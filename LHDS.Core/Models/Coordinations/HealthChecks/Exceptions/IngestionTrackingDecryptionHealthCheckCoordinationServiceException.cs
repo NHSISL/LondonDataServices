6eb319a4ba75bd50ba7deab3defb2c88e0d176cf
Joe Owens <joe.owens@answerdigital.com>
2025-05-30 14:08:18 +0100

    ShouldThrowServiceExceptionOnGetHealthStatusIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Coordinations/HealthChecks/Exceptions/IngestionTrackingDecryptionHealthCheckCoordinationServiceException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Coordinations.HealthChecks.Exceptions
+{
+    public class IngestionTrackingDecryptionHealthCheckCooridinationServiceException : Xeption
+    {
+        public IngestionTrackingDecryptionHealthCheckCooridinationServiceException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}

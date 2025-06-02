cd30821d095c7d1de4b63e47137eebf40bb46dc8
Christo du Toit <christo@dutoit.co.uk>
2025-06-02 10:19:18 +0100

    CODE RUB: Code cleanup

--- LHDS.Core/Models/Coordinations/HealthChecks/Exceptions/FailedIngestionTrackingFailedToProcessHealthCheckServiceException.cs
+++ LHDS.Core/Models/Coordinations/HealthChecks/Exceptions/FailedIngestionTrackingFailedToProcessHealthCheckServiceException.cs
@@ -5,8 +5,9 @@ namespace LHDS.Core.Models.Coordinations.HealthChecks.Exceptions
 {
     public class FailedIngestionTrackingFailedToProcessHealthCheckServiceException : Xeption
     {
-        public FailedIngestionTrackingFailedToProcessHealthCheckServiceException(string message, Exception innerException)
-            : base(message, innerException)
+        public FailedIngestionTrackingFailedToProcessHealthCheckServiceException(
+            string message, 
+            Exception innerException) : base(message, innerException)
         { }
     }
 }

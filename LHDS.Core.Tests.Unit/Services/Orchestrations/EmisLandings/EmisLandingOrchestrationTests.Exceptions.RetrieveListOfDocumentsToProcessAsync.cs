969b8dd5aba6e7b5f0992c706501a53f4591182c
Christo du Toit <christo@dutoit.co.uk>
2025-06-03 10:29:22 +0100

    ShouldProcessDocumentsAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Orchestrations/EmisLandings/EmisLandingOrchestrationTests.Exceptions.RetrieveListOfDocumentsToProcessAsync.cs
+++ LHDS.Core.Tests.Unit/Services/Orchestrations/EmisLandings/EmisLandingOrchestrationTests.Exceptions.RetrieveListOfDocumentsToProcessAsync.cs
@@ -64,7 +64,7 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.EmisLandings
             this.dataSetSpecificationProcessingServiceMock.VerifyNoOtherCalls();
             this.documentProcessingServiceMock.VerifyNoOtherCalls();
             this.hashBrokerMock.VerifyNoOtherCalls();
-            this.auditServiceMock.VerifyNoOtherCalls();
+            this.ingestionTrackingAuditProcessingServiceMock.VerifyNoOtherCalls();
         }
 
         [Theory]
@@ -113,7 +113,7 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.EmisLandings
             this.dataSetSpecificationProcessingServiceMock.VerifyNoOtherCalls();
             this.documentProcessingServiceMock.VerifyNoOtherCalls();
             this.hashBrokerMock.VerifyNoOtherCalls();
-            this.auditServiceMock.VerifyNoOtherCalls();
+            this.ingestionTrackingAuditProcessingServiceMock.VerifyNoOtherCalls();
         }
 
         [Fact]
@@ -166,7 +166,7 @@ namespace LHDS.Core.Tests.Unit.Services.Orchestrations.EmisLandings
             this.dataSetSpecificationProcessingServiceMock.VerifyNoOtherCalls();
             this.documentProcessingServiceMock.VerifyNoOtherCalls();
             this.hashBrokerMock.VerifyNoOtherCalls();
-            this.auditServiceMock.VerifyNoOtherCalls();
+            this.ingestionTrackingAuditProcessingServiceMock.VerifyNoOtherCalls();
         }
     }
 }

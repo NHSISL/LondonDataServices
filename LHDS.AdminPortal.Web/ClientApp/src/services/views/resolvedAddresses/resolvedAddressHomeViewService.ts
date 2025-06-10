802b20b5db654fd64d7b7d69c23f22dc024cfe6f
davidhayes03 <david.hayes17@nhs.net>
2025-06-10 13:52:50 +0100

    CODE RUB : Filters

--- LHDS.AdminPortal.Web/ClientApp/src/services/views/resolvedAddresses/resolvedAddressHomeViewService.ts
+++ LHDS.AdminPortal.Web/ClientApp/src/services/views/resolvedAddresses/resolvedAddressHomeViewService.ts
@@ -11,34 +11,39 @@ type ResolvedAddressHomeViewServiceResponse = {
     isFetchingNextPage: boolean;
     hasNextPage: boolean;
     data: any;
-    refetch: () => void
-}
+    refetch: () => void;
+};
 
 export const ResolvedAddressHomeViewService = {
-    useGetAllResolvedAddresses: (searchTerm?: string): ResolvedAddressHomeViewServiceResponse => {
-        try 
-        {
-            let query = ``;
-            query = `?$filter=matchedWithAssign eq false&$orderby=createdDate desc`;
+    useGetAllResolvedAddresses: (
+        searchTerm?: string,
+        matchedFilterParam?: boolean
+    ): ResolvedAddressHomeViewServiceResponse => {
+        try {
+            let filters: string[] = [];
+
+            if (matchedFilterParam === true) {
+                filters.push(`MatchedWithAssign eq true`);
+            } else if (matchedFilterParam === false) {
+                filters.push(`MatchedWithAssign eq false`);
+            }
 
             if (searchTerm) {
-                const fields = [
-                    'uprn',
-                    'unstructuredPostalAddress'
-                ];
-
-                const filterConditions = fields
-                    .map(field => `contains(${field},'${searchTerm}')`)
-                    .join(' or ');
-
-                query = query + `&$filter=${filterConditions}`;
+                const searchFilter = [
+                    `contains(uprn,'${searchTerm}')`,
+                    `contains(unstructuredPostalAddress,'${searchTerm}')`
+                ].join(" or ");
+                filters.push(`(${searchFilter})`);
             }
+
+            const filterQuery = filters.length ? `$filter=${filters.join(" and ")}` : "";
+            const orderByQuery = `$orderby=createdDate desc`;
+            const query = `?${filterQuery}&${orderByQuery}`;
 
             const response = resolvedAddressService.useRetrieveAllResolvedAddressPages(query);
 
             const [mappedResolvedAddresses, setMappedResolvedAddresses] =
                 useState<Array<ResolvedAddressHomeView>>();
-
             const [pages, setPages] = useState<any>([]);
 
             useEffect(() => {
@@ -47,41 +52,43 @@ export const ResolvedAddressHomeViewService = {
 
                     response.data.pages.forEach((page: any) => {
                         page.data.forEach((resolvedAddress: ResolvedAddress) => {
-                            resolvedAddressArray.push(new ResolvedAddressHomeView(
-                                resolvedAddress.id,
-                                resolvedAddress.uprn,
-                                resolvedAddress.upsn,
-                                resolvedAddress.postCode,
-                                resolvedAddress.retryCount,
-                                resolvedAddress.isProcessing,
-                                resolvedAddress.uniqueReference,
-                                resolvedAddress.unstructuredPostalAddress,
-                                resolvedAddress.alternateUnstructuredPostalAddress ?? undefined,
-                                resolvedAddress.batchReference,
-                                resolvedAddress.addressFormatQuality,
-                                resolvedAddress.algorithm,
-                                resolvedAddress.buildingName,
-                                resolvedAddress.buildingNumber,
-                                resolvedAddress.classification,
-                                resolvedAddress.departmentName,
-                                resolvedAddress.dependentLocality,
-                                resolvedAddress.dependentThoroughfare,
-                                resolvedAddress.doubleDependentLocality,
-                                resolvedAddress.matchPattern,
-                                resolvedAddress.matchedWithAssign,
-                                resolvedAddress.organisationName,
-                                resolvedAddress.postCodeQuality,
-                                resolvedAddress.postTown,
-                                resolvedAddress.qualifier,
-                                resolvedAddress.subBuildingName,
-                                resolvedAddress.thoroughfare,
-                                resolvedAddress.isExported,
-                                resolvedAddress.isProcessed,
-                                resolvedAddress.createdBy,
-                                resolvedAddress.createdDate,
-                                resolvedAddress.updatedBy,
-                                resolvedAddress.updatedDate
-                            ));
+                            resolvedAddressArray.push(
+                                new ResolvedAddressHomeView(
+                                    resolvedAddress.id,
+                                    resolvedAddress.uprn,
+                                    resolvedAddress.upsn,
+                                    resolvedAddress.postCode,
+                                    resolvedAddress.retryCount,
+                                    resolvedAddress.isProcessing,
+                                    resolvedAddress.uniqueReference,
+                                    resolvedAddress.unstructuredPostalAddress,
+                                    resolvedAddress.alternateUnstructuredPostalAddress ?? undefined,
+                                    resolvedAddress.batchReference,
+                                    resolvedAddress.addressFormatQuality,
+                                    resolvedAddress.algorithm,
+                                    resolvedAddress.buildingName,
+                                    resolvedAddress.buildingNumber,
+                                    resolvedAddress.classification,
+                                    resolvedAddress.departmentName,
+                                    resolvedAddress.dependentLocality,
+                                    resolvedAddress.dependentThoroughfare,
+                                    resolvedAddress.doubleDependentLocality,
+                                    resolvedAddress.matchPattern,
+                                    resolvedAddress.matchedWithAssign,
+                                    resolvedAddress.organisationName,
+                                    resolvedAddress.postCodeQuality,
+                                    resolvedAddress.postTown,
+                                    resolvedAddress.qualifier,
+                                    resolvedAddress.subBuildingName,
+                                    resolvedAddress.thoroughfare,
+                                    resolvedAddress.isExported,
+                                    resolvedAddress.isProcessed,
+                                    resolvedAddress.createdBy,
+                                    resolvedAddress.createdDate,
+                                    resolvedAddress.updatedBy,
+                                    resolvedAddress.updatedDate
+                                )
+                            );
                         });
                     });
 
@@ -100,9 +107,8 @@ export const ResolvedAddressHomeViewService = {
                 data: response.data,
                 refetch: response.refetch
             };
-        } catch (err) 
-        {
+        } catch (err) {
             throw err;
         }
-    },
+    }
 };

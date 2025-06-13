6d44344f6d8b8bbaac6c6728e4e0c55e62345b31
Hassan Raza <h_raza110@outlook.com>
2025-06-13 08:23:36 +0100

    CODE RUB: Ingestion Tracking Remove Delete Audit

--- LHDS.Core.Tests.Unit/Services/Foundations/IngestionTrackings/IngestionTrackingServiceTests.Logic.RemoveById.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/IngestionTrackings/IngestionTrackingServiceTests.Logic.RemoveById.cs
@@ -6,7 +6,6 @@ using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Force.DeepCloner;
-using LHDS.Core.Models.Brokers.Securities;
 using LHDS.Core.Models.Foundations.IngestionTrackings;
 using Moq;
 using Xunit;
@@ -19,39 +18,20 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.IngestionTrackings
         public async Task ShouldRemoveIngestionTrackingByIdAsync()
         {
             // given
-            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
-            EntraUser randomEntraUser = CreateRandomEntraUser();
-
-            IngestionTracking randomIngestionTracking = 
-                CreateRandomIngestionTracking(randomDateTimeOffset, randomEntraUser.EntraUserId);
-
-            Guid inputIngestionTrackingId = randomIngestionTracking.Id;
+            Guid randomId = Guid.NewGuid();
+            Guid inputIngestionTrackingId = randomId;
+            IngestionTracking randomIngestionTracking = CreateRandomIngestionTracking();
             IngestionTracking storageIngestionTracking = randomIngestionTracking;
-            IngestionTracking ingestionTrackingWithDeleteAuditApplied = storageIngestionTracking.DeepClone();
-            ingestionTrackingWithDeleteAuditApplied.UpdatedBy = randomEntraUser.EntraUserId.ToString();
-            ingestionTrackingWithDeleteAuditApplied.UpdatedDate = randomDateTimeOffset;
-            IngestionTracking updatedIngestionTracking = storageIngestionTracking;
-            IngestionTracking deletedIngestionTracking = updatedIngestionTracking;
+            IngestionTracking expectedInputIngestionTracking = storageIngestionTracking;
+            IngestionTracking deletedIngestionTracking = expectedInputIngestionTracking;
             IngestionTracking expectedIngestionTracking = deletedIngestionTracking.DeepClone();
-
-            this.dateTimeBrokerMock.Setup(broker =>
-                broker.GetCurrentDateTimeOffsetAsync())
-                    .ReturnsAsync(randomDateTimeOffset);
-
-            this.securityBrokerMock.Setup(broker =>
-                broker.GetCurrentUserAsync())
-                    .ReturnsAsync(randomEntraUser);
 
             this.storageBrokerMock.Setup(broker =>
                 broker.SelectIngestionTrackingByIdAsync(inputIngestionTrackingId))
                     .ReturnsAsync(storageIngestionTracking);
 
             this.storageBrokerMock.Setup(broker =>
-                broker.UpdateIngestionTrackingAsync(randomIngestionTracking))
-                    .ReturnsAsync(updatedIngestionTracking);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.DeleteIngestionTrackingAsync(updatedIngestionTracking))
+                broker.DeleteIngestionTrackingAsync(expectedInputIngestionTracking))
                     .ReturnsAsync(deletedIngestionTracking);
 
             // when
@@ -65,20 +45,8 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.IngestionTrackings
                 broker.SelectIngestionTrackingByIdAsync(inputIngestionTrackingId),
                     Times.Once);
 
-            this.dateTimeBrokerMock.Verify(broker =>
-                broker.GetCurrentDateTimeOffsetAsync(),
-                    Times.Once);
-
-            this.securityBrokerMock.Verify(broker =>
-                broker.GetCurrentUserAsync(),
-                    Times.Exactly(2));
-
             this.storageBrokerMock.Verify(broker =>
-                broker.UpdateIngestionTrackingAsync(randomIngestionTracking),
-                    Times.Once);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.DeleteIngestionTrackingAsync(updatedIngestionTracking),
+                broker.DeleteIngestionTrackingAsync(expectedInputIngestionTracking),
                     Times.Once);
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();

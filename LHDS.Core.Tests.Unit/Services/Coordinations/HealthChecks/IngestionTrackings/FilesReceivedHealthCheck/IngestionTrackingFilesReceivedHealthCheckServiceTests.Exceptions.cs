6fafe769fc453a978d6bc9f4b93ede18f6dd55fe
Joe Owens <joe.owens@answerdigital.com>
2025-06-17 15:52:37 +0100

    ShouldThrowServiceExceptionOnGetHealthStatusIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Coordinations/HealthChecks/IngestionTrackings/FilesReceivedHealthCheck/IngestionTrackingFilesReceivedHealthCheckServiceTests.Exceptions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Coordinations.HealthChecks.Exceptions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Coordinations.HealthChecks.IngestionTrackings.FilesReceivedHealthCheck
+{
+    public partial class IngestionTrackingFilesReceivedHealthCheckServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnGetHealthStatusIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            var serviceException = new Exception();
+
+            var failedIngestionTrackingFilesReceivedHealthCheckServiceException =
+                new FailedIngestionTrackingFilesReceivedHealthCheckServiceException(
+
+                    message: "Failed ingestion tracking failed to process health check service error occurred, " +
+                    "please contact support.",
+
+                    innerException: serviceException);
+
+            var expectedIngestionTrackingFilesReceivedHealthCheckServiceException =
+                new IngestionTrackingFilesReceivedHealthCheckServiceException(
+
+                    message: "Ingestion tracking failed to process health check service error occurred, " +
+                    "please contact support.",
+
+                    innerException: failedIngestionTrackingFilesReceivedHealthCheckServiceException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<HealthCheckResult> getHealthStatusTask =
+                this.ingestionTrackingFilesReceivedHealthCheckService.GetHealthStatusAsync();
+
+            IngestionTrackingFilesReceivedHealthCheckServiceException
+                actualIngestionTrackingFilesReceivedHealthCheckServiceException =
+                    await Assert.ThrowsAsync<IngestionTrackingFilesReceivedHealthCheckServiceException>(
+                        getHealthStatusTask.AsTask);
+
+            // then
+            actualIngestionTrackingFilesReceivedHealthCheckServiceException.Should()
+                .BeEquivalentTo(expectedIngestionTrackingFilesReceivedHealthCheckServiceException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedIngestionTrackingFilesReceivedHealthCheckServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LHDS.Core/Models/Coordinations/HealthChecks/Exceptions/IngestionTrackingFilesReceivedHealthCheckServiceException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Coordinations.HealthChecks.Exceptions
+{
+    public class IngestionTrackingFilesReceivedHealthCheckServiceException : Xeption
+    {
+        public IngestionTrackingFilesReceivedHealthCheckServiceException(
+            string message,
+            Exception innerException
+            ) : base(message, innerException)
+        { }
+    }
+}

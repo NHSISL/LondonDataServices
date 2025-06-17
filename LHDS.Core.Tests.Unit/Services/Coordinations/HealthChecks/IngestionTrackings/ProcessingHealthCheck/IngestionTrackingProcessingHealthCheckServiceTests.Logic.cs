0bcd54c00577424ca8accb73c14b05aada10f2e3
Joe Owens <joe.owens@answerdigital.com>
2025-06-17 17:34:48 +0100

    COORDINATIONS: Adding logic test for processing health check

---
+++ LHDS.Core.Tests.Unit/Services/Coordinations/HealthChecks/IngestionTrackings/ProcessingHealthCheck/IngestionTrackingProcessingHealthCheckServiceTests.Logic.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.IngestionTrackings;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Coordinations.HealthChecks.IngestionTrackings.ProcessingHealthCheck
+{
+    public partial class IngestionTrackingProcessingHealthCheckServiceTests
+    {
+        [Fact]
+        public async Task ShouldGetHealthStatusAsyncUnhealthyWhenAnyUnhealthy()
+        {
+            // given
+            DateTimeOffset currentDateTime = DateTimeOffset.UtcNow;
+
+            IQueryable<IngestionTracking> randomUnhealthyTrackings =
+                CreateRandomUnhealthyIngestionTrackings();
+
+            IQueryable<IngestionTracking> randomHealthyTrackings =
+                CreateRandomHealthyIngestionTrackings();
+
+            IQueryable<IngestionTracking> randomTrackings =
+                randomUnhealthyTrackings.Concat(randomHealthyTrackings);
+
+            Dictionary<string, object> healthCheckResultValues =
+                GetHealthCheckResultValues(
+                    currentDateTime,
+                    HealthStatus.Unhealthy,
+                    unhealthyItemsCount: randomUnhealthyTrackings.Count()
+                );
+
+            var expectedHealthCheckResult = HealthCheckResult.Unhealthy(
+                description: CheckName,
+                data: healthCheckResultValues
+            );
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ReturnsAsync(currentDateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllIngestionTrackingsAsync())
+                    .ReturnsAsync(randomTrackings);
+
+            // when
+            var result = await this.ingestionTrackingProcessingHealthCheckService.GetHealthStatusAsync();
+
+            // then
+            result.Data.Should().BeEquivalentTo(expectedHealthCheckResult.Data);
+            result.Description.Should().BeEquivalentTo(expectedHealthCheckResult.Description);
+            result.Exception.Should().BeEquivalentTo(expectedHealthCheckResult.Exception);
+            result.Status.Should().Be(expectedHealthCheckResult.Status);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllIngestionTrackingsAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldGetHealthStatusAsyncDegradedWhenAnyDegraded()
+        {
+            // given
+            DateTimeOffset currentDateTime = DateTimeOffset.UtcNow;
+
+            IQueryable<IngestionTracking> randomDegradedTrackings =
+                CreateRandomDegradedIngestionTrackings();
+
+            IQueryable<IngestionTracking> randomHealthyTrackings =
+                CreateRandomHealthyIngestionTrackings();
+
+            IQueryable<IngestionTracking> randomTrackings =
+                randomDegradedTrackings.Concat(randomHealthyTrackings);
+
+            Dictionary<string, object> healthCheckResultValues =
+                GetHealthCheckResultValues(
+                    currentDateTime,
+                    HealthStatus.Degraded,
+                    degradedItemsCount: randomDegradedTrackings.Count()
+                );
+
+            var expectedHealthCheckResult = HealthCheckResult.Degraded(
+                description: CheckName,
+                data: healthCheckResultValues
+            );
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ReturnsAsync(currentDateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllIngestionTrackingsAsync())
+                    .ReturnsAsync(randomTrackings);
+
+            // when
+            var result = await this.ingestionTrackingProcessingHealthCheckService.GetHealthStatusAsync();
+
+            // then
+            result.Data.Should().BeEquivalentTo(expectedHealthCheckResult.Data);
+            result.Description.Should().BeEquivalentTo(expectedHealthCheckResult.Description);
+            result.Exception.Should().BeEquivalentTo(expectedHealthCheckResult.Exception);
+            result.Status.Should().Be(expectedHealthCheckResult.Status);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllIngestionTrackingsAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldGetHealthStatusAsyncHealthyWhenAllHealthy()
+        {
+            // given
+            DateTimeOffset currentDateTime = DateTimeOffset.UtcNow;
+
+            IQueryable<IngestionTracking> randomHealthyTrackings =
+                CreateRandomHealthyIngestionTrackings();
+
+            Dictionary<string, object> healthCheckResultValues =
+                GetHealthCheckResultValues(
+                    currentDateTime,
+                    HealthStatus.Healthy
+                );
+
+            var expectedHealthCheckResult = HealthCheckResult.Healthy(
+                description: CheckName,
+                data: healthCheckResultValues
+            );
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ReturnsAsync(currentDateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllIngestionTrackingsAsync())
+                    .ReturnsAsync(randomHealthyTrackings);
+
+            // when
+            var result = await this.ingestionTrackingProcessingHealthCheckService.GetHealthStatusAsync();
+
+            // then
+            result.Data.Should().BeEquivalentTo(expectedHealthCheckResult.Data);
+            result.Description.Should().BeEquivalentTo(expectedHealthCheckResult.Description);
+            result.Exception.Should().BeEquivalentTo(expectedHealthCheckResult.Exception);
+            result.Status.Should().Be(expectedHealthCheckResult.Status);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllIngestionTrackingsAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

e8c30e6642488be76e33f5f4942b745ea0432b7e
Joe Owens <joe.owens@answerdigital.com>
2025-06-19 11:23:01 +0100

    Exception tests for TerminologyPolls NotPolling health checks

---
+++ LHDS.Core/Services/Foundations/HealthChecks/TerminologyPolls/TerminologyPollsNotPollingHealthCheckService.Exceptions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Coordinations.HealthChecks.Exceptions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Xeptions;
+
+namespace LHDS.Core.Services.Foundations.HealthChecks.TerminologyPolls
+{
+    public partial class TerminologyPollsNotPollingHealthCheckService : ITerminologyPollsHealthItemService
+    {
+        private delegate Task<HealthCheckResult> ReturningHealthCheckResultFunction();
+
+        private async ValueTask<HealthCheckResult> 
+            TryCatch(ReturningHealthCheckResultFunction returningHealthCheckResultFunction)
+        {
+            try
+            {
+                return await returningHealthCheckResultFunction();
+            }
+            catch (Exception exception)
+            {
+                var failedTerminologyPollsNotPollingHealthCheckServiceException =
+                    new FailedTerminologyPollsNotPollingHealthCheckServiceException(
+
+                        message: "Failed terminology polls not polling health check service error occurred, "
+                        + "please contact support.",
+
+                        innerException: exception);
+
+                throw await CreateAndLogServiceExceptionAsync(
+                    failedTerminologyPollsNotPollingHealthCheckServiceException
+                );
+            }
+        }
+
+        private async ValueTask<TerminologyPollsNotPollingHealthCheckServiceException> 
+            CreateAndLogServiceExceptionAsync(Xeption exception)
+        {
+            var terminologyPollsNotPollingHealthCheckServiceException =
+                new TerminologyPollsNotPollingHealthCheckServiceException(
+
+                    message: "Terminology polls not polling health check service error occurred, "
+                    + "please contact support.",
+
+                    innerException: exception);
+
+            await this.loggingBroker.LogCriticalAsync(
+                terminologyPollsNotPollingHealthCheckServiceException
+            );
+
+            return terminologyPollsNotPollingHealthCheckServiceException;
+        }
+    }
+}

0e00c7c4433891a1d3de30b158fed50bdc9d9f2d
Christo du Toit <christo@dutoit.co.uk>
2025-06-20 11:06:47 +0100

    ShouldCheckForBatchCompleteAndNotCreateBatchReadyFileAsync -> PASS

--- LHDS.Core/Services/Processings/IngestionTrackings/IIngestionTrackingProcessingService.cs
+++ LHDS.Core/Services/Processings/IngestionTrackings/IIngestionTrackingProcessingService.cs
@@ -20,8 +20,9 @@ namespace LHDS.Core.Services.Processings.IngestionTrackings
         ValueTask<IngestionTracking> ModifyIngestionTrackingAsync(IngestionTracking ingestionTracking);
         ValueTask<IngestionTracking> RemoveIngestionTrackingByIdAsync(Guid ingestionTrackingId);
         ValueTask<List<string>> RetrieveObjectsInBatchByBatchReferenceAsync(
-            string bacthReference, 
-            bool? decrypted = null);
+            string bacthReference,
+            bool? decrypted = null,
+            Guid? subscriberAgreementId = null);
 
         ValueTask MarkAsBatchCompleteAsync(Guid ingestionTrackingId, bool isBatchComplete);
     }

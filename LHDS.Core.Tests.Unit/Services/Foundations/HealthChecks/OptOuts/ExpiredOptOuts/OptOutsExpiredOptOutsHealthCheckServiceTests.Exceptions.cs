7e366798162b4701141eff0ef3daec890175d0d2
Joe Owens <joe.owens@answerdigital.com>
2025-06-20 17:51:53 +0100

    OptOuts ExpiredOptOuts health check exception tests

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/HealthChecks/OptOuts/ExpiredOptOuts/OptOutsExpiredOptOutsHealthCheckServiceTests.Exceptions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Coordinations.HealthChecks.Exceptions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.HealthChecks.OptOuts.ExpiredOptOuts
+{
+    public partial class OptOutsExpiredOptOutsHealthCheckServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnGetHealthStatusIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            var serviceException = new Exception();
+
+            var failedOptOutsExpiredOptOutsHealthCheckServiceException =
+                new FailedOptOutsExpiredOptOutsHealthCheckServiceException(
+
+                    message:
+                        "Failed opt outs expired opt outs health check service error occurred, "
+                        + "please contact support.",
+
+                    innerException: serviceException);
+
+            var expectedOptOutsExpiredOptOutsHealthCheckServiceException =
+                new OptOutsExpiredOptOutsHealthCheckServiceException(
+
+                    message:
+                        "Opt outs expired opt outs health check service error occurred, "
+                        + "please contact support.",
+
+                    innerException: failedOptOutsExpiredOptOutsHealthCheckServiceException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<HealthCheckResult> getHealthStatusTask = this.OptOutsHealthItemService.GetHealthStatusAsync();
+
+            OptOutsExpiredOptOutsHealthCheckServiceException
+                actualOptOutsExpiredOptOutsHealthCheckServiceException =
+                    await Assert.ThrowsAsync<OptOutsExpiredOptOutsHealthCheckServiceException>(
+                        getHealthStatusTask.AsTask);
+
+            // then
+            actualOptOutsExpiredOptOutsHealthCheckServiceException.Should()
+                .BeEquivalentTo(expectedOptOutsExpiredOptOutsHealthCheckServiceException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedOptOutsExpiredOptOutsHealthCheckServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- LHDS.Core/Services/Foundations/HealthChecks/OptOut/OptOutsExpiredOptOutHealthCheckService.cs
+++ LHDS.Core/Services/Foundations/HealthChecks/OptOut/OptOutsExpiredOptOutHealthCheckService.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace LHDS.Core.Services.Foundations.HealthChecks.OptOut
 {
-    public class OptOutsExpiredOptOutHealthCheckService : IOptOutHealthItemService
+    public partial class OptOutsExpiredOptOutHealthCheckService : IOptOutHealthItemService
     {
         private readonly IStorageBroker storageBroker;
         private readonly IConfiguration configuration;
@@ -32,71 +32,75 @@ namespace LHDS.Core.Services.Foundations.HealthChecks.OptOut
             this.loggingBroker = loggingBroker;
         }
 
-        public async ValueTask<HealthCheckResult> GetHealthStatusAsync()
-        {
-            int degradedThresholdMinutes = configuration.GetValue($"{ConfigSectionName}:DegradedThreshold", 1440);
-            int unHealthyThresholdMinutes = configuration.GetValue($"{ConfigSectionName}:UnHealthyThreshold", 2880);
-            int expiredAfterDays = configuration.GetValue($"{ConfigSectionName}:ExpiredAfterDays", 7);
-            int lastSentExpiredAfterDays = configuration.GetValue($"{ConfigSectionName}:LastSentExpiredAfterDays", 2);
-            DateTimeOffset currentDateTime = await dateTimeBroker.GetCurrentDateTimeOffsetAsync();
-            DateTimeOffset expirationDate = currentDateTime.AddDays(-1 * expiredAfterDays);
-            DateTimeOffset lastSentExpirationDate = currentDateTime.AddDays(-1 * lastSentExpiredAfterDays);
-            DateTimeOffset degradedThresholdDateTime = currentDateTime.AddMinutes(-1 * degradedThresholdMinutes);
-            DateTimeOffset unHealthyThresholdDateTime = currentDateTime.AddMinutes(-1 * unHealthyThresholdMinutes);
-            var optOutsQuery = await storageBroker.SelectAllOptOutsAsync();
-
-            var filteredQuery = optOutsQuery.Where(i => i.CacheTime < expirationDate
-                && i.LastSentToMesh < lastSentExpirationDate);
-
-            int degradedCount = filteredQuery.Count(ingestionTracking =>
-                ingestionTracking.UpdatedDate <= degradedThresholdDateTime &&
-                ingestionTracking.UpdatedDate > unHealthyThresholdDateTime);
-
-            int unHealthyCount = filteredQuery
-                .Count(ingestionTracking => ingestionTracking.UpdatedDate <= unHealthyThresholdDateTime);
-
-            int totalCount = degradedCount + unHealthyCount;
-
-            string message = totalCount == 0
-                ? $"Nothing is expired and outdated. All up to date."
-                : $"{totalCount} opt outs expired and outdated. Please check logs and function status.";
-
-            var vals = new Dictionary<string, object>
+        public ValueTask<HealthCheckResult> GetHealthStatusAsync() =>
+            TryCatch(async () =>
             {
-                { "description", CheckNameDescription },
-                { "expiredAndOutdated", totalCount },
-                { "degradedItems", degradedCount},
-                { "unHealthyItems", unHealthyCount},
-                { "degradedThresholdMinutes", degradedThresholdMinutes.ToString() },
-                { "unHealthyThresholdMinutes", unHealthyThresholdMinutes.ToString() },
-                { "checkedAt", currentDateTime.ToString("o") },
-                { "message", message }
-            };
+                int degradedThresholdMinutes = configuration.GetValue($"{ConfigSectionName}:DegradedThreshold", 1440);
+                int unHealthyThresholdMinutes = configuration.GetValue($"{ConfigSectionName}:UnHealthyThreshold", 2880);
+                int expiredAfterDays = configuration.GetValue($"{ConfigSectionName}:ExpiredAfterDays", 7);
 
-            if (unHealthyCount > 0)
-            {
-                vals.Add("status", HealthStatus.Unhealthy.ToString());
+                int lastSentExpiredAfterDays = configuration
+                    .GetValue($"{ConfigSectionName}:LastSentExpiredAfterDays", 2);
 
-                return HealthCheckResult.Unhealthy(
-                    description: CheckName,
-                    data: vals);
-            }
-            else if (degradedCount > 0)
-            {
-                vals.Add("status", HealthStatus.Degraded.ToString());
+                DateTimeOffset currentDateTime = await dateTimeBroker.GetCurrentDateTimeOffsetAsync();
+                DateTimeOffset expirationDate = currentDateTime.AddDays(-1 * expiredAfterDays);
+                DateTimeOffset lastSentExpirationDate = currentDateTime.AddDays(-1 * lastSentExpiredAfterDays);
+                DateTimeOffset degradedThresholdDateTime = currentDateTime.AddMinutes(-1 * degradedThresholdMinutes);
+                DateTimeOffset unHealthyThresholdDateTime = currentDateTime.AddMinutes(-1 * unHealthyThresholdMinutes);
+                var optOutsQuery = await storageBroker.SelectAllOptOutsAsync();
 
-                return HealthCheckResult.Degraded(
-                    description: CheckName,
-                    data: vals);
-            }
-            else
-            {
-                vals.Add("status", HealthStatus.Healthy.ToString());
+                var filteredQuery = optOutsQuery.Where(i => i.CacheTime < expirationDate
+                    && i.LastSentToMesh < lastSentExpirationDate);
 
-                return HealthCheckResult.Healthy(
-                    description: CheckName,
-                    data: vals);
-            }
-        }
+                int degradedCount = filteredQuery.Count(ingestionTracking =>
+                    ingestionTracking.UpdatedDate <= degradedThresholdDateTime &&
+                    ingestionTracking.UpdatedDate > unHealthyThresholdDateTime);
+
+                int unHealthyCount = filteredQuery
+                    .Count(ingestionTracking => ingestionTracking.UpdatedDate <= unHealthyThresholdDateTime);
+
+                int totalCount = degradedCount + unHealthyCount;
+
+                string message = totalCount == 0
+                    ? $"Nothing is expired and outdated. All up to date."
+                    : $"{totalCount} opt outs expired and outdated. Please check logs and function status.";
+
+                var vals = new Dictionary<string, object>
+                {
+                    { "description", CheckNameDescription },
+                    { "expiredAndOutdated", totalCount },
+                    { "degradedItems", degradedCount},
+                    { "unHealthyItems", unHealthyCount},
+                    { "degradedThresholdMinutes", degradedThresholdMinutes.ToString() },
+                    { "unHealthyThresholdMinutes", unHealthyThresholdMinutes.ToString() },
+                    { "checkedAt", currentDateTime.ToString("o") },
+                    { "message", message }
+                };
+
+                if (unHealthyCount > 0)
+                {
+                    vals.Add("status", HealthStatus.Unhealthy.ToString());
+
+                    return HealthCheckResult.Unhealthy(
+                        description: CheckName,
+                        data: vals);
+                }
+                else if (degradedCount > 0)
+                {
+                    vals.Add("status", HealthStatus.Degraded.ToString());
+
+                    return HealthCheckResult.Degraded(
+                        description: CheckName,
+                        data: vals);
+                }
+                else
+                {
+                    vals.Add("status", HealthStatus.Healthy.ToString());
+
+                    return HealthCheckResult.Healthy(
+                        description: CheckName,
+                        data: vals);
+                }
+            });
     }
 }

52c2fd741dc18f0df07ebd8f0a78c80fe1aa60a0
Hassan Raza <h_raza110@outlook.com>
2025-06-25 09:06:18 +0100

    EXCEPTIONS: Ingestion Tracking Health Check Exception Tests

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/HealthChecks/IngestionTrackings/Decryption/IngestionTrackingDecryptionHealthCheckServiceTests.GetHealthStatus.Exceptions.cs
@@ -0,0 +1,70 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Coordinations.HealthChecks.Exceptions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.HealthChecks.IngestionTrackings
+{
+    public partial class IngestionTrackingDecryptionHealthCheckServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnGetHealthStatusIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            var serviceException = new Exception();
+
+            var failedIngestionTrackingDecryptionHealthCheckCooridinationServiceException =
+                new FailedIngestionTrackingDecryptionHealthCheckCooridinationServiceException(
+
+                    message: "Failed ingestion tracking decryption health check service error occurred, " +
+                        "please contact support.",
+
+                    innerException: serviceException);
+
+            var expectedIngestionTrackingDecryptionHealthCheckCooridinationServiceException =
+                new IngestionTrackingDecryptionHealthCheckCooridinationServiceException(
+
+                    message: "Ingestion tracking decryption health check service error occurred, " +
+                        "please contact support.",
+
+                    innerException: failedIngestionTrackingDecryptionHealthCheckCooridinationServiceException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<HealthCheckResult> getHealthStatusTask =
+                this.ingestionTrackingHealthItemService.GetHealthStatusAsync();
+
+            IngestionTrackingDecryptionHealthCheckCooridinationServiceException
+                actualIngestionTrackingDecryptionHealthCheckCooridinationServiceException =
+                    await Assert.ThrowsAsync<IngestionTrackingDecryptionHealthCheckCooridinationServiceException>(
+                        getHealthStatusTask.AsTask);
+
+            // then
+            actualIngestionTrackingDecryptionHealthCheckCooridinationServiceException.Should()
+                .BeEquivalentTo(expectedIngestionTrackingDecryptionHealthCheckCooridinationServiceException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedIngestionTrackingDecryptionHealthCheckCooridinationServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LHDS.Core.Tests.Unit/Services/Foundations/HealthChecks/IngestionTrackings/Processing/IngestionTrackingProcessingHealthCheckServiceTests.GetHealthStatus.Exceptions.cs
@@ -0,0 +1,70 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Coordinations.HealthChecks.Exceptions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.HealthChecks.IngestionTrackings
+{
+    public partial class IngestionTrackingProcessingHealthCheckServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnGetHealthStatusIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            var serviceException = new Exception();
+
+            var failedIngestionTrackingProcessingHealthCheckServiceException =
+                new FailedIngestionTrackingProcessingHealthCheckServiceException(
+
+                    message: "Failed resolved address processing health check service error occurred, " +
+                        "please contact support.",
+
+                    innerException: serviceException);
+
+            var expectedIngestionTrackingProcessingHealthCheckServiceException =
+                new IngestionTrackingProcessingHealthCheckServiceException(
+
+                    message: "Resolved address processing health check service error occurred, " +
+                        "please contact support.",
+
+                    innerException: failedIngestionTrackingProcessingHealthCheckServiceException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<HealthCheckResult> getHealthStatusTask =
+                this.ingestionTrackingHealthItemService.GetHealthStatusAsync();
+
+            IngestionTrackingProcessingHealthCheckServiceException
+                actualIngestionTrackingProcessingHealthCheckServiceException =
+                    await Assert.ThrowsAsync<IngestionTrackingProcessingHealthCheckServiceException>(
+                        getHealthStatusTask.AsTask);
+
+            // then
+            actualIngestionTrackingProcessingHealthCheckServiceException.Should()
+                .BeEquivalentTo(expectedIngestionTrackingProcessingHealthCheckServiceException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedIngestionTrackingProcessingHealthCheckServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

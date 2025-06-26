c3ae55746227904c48f4a2d73907c70df8ae12ba
Joe Owens <joe.owens@answerdigital.com>
2025-06-26 14:59:06 +0100

    ResolvedAddressAuditService logic tests -> FAIL

---
+++ LHDS.Core/Services/Foundations/ResolvedAddressAudits/IResolvedAddressAuditService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.ResolvedAddressesAudits;
+
+namespace LHDS.Core.Services.Foundations.ResolvedAddressAudits
+{
+    public interface IResolvedAddressAuditService
+    {
+        ValueTask<ResolvedAddressAudit> AddResolvedAddressAuditAsync(ResolvedAddressAudit resolvedAddressAudit);
+        ValueTask<IQueryable<ResolvedAddressAudit>> RetrieveAllResolvedAddressAuditsAsync();
+        ValueTask<ResolvedAddressAudit> RetrieveResolvedAddressAuditByIdAsync(Guid resolvedAddressAuditId);
+        ValueTask<ResolvedAddressAudit> ModifyResolvedAddressAuditAsync(ResolvedAddressAudit resolvedAddressAudit);
+        ValueTask<ResolvedAddressAudit> RemoveResolvedAddressAuditByIdAsync(Guid resolvedAddressAuditId);
+    }
+}

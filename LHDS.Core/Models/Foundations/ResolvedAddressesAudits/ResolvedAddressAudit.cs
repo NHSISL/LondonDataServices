383c68d826ef0f5d3e6b8e0bb0a152d215139f92
Joe Owens <joe.owens@answerdigital.com>
2025-06-26 14:38:22 +0100

    DATA: ResolvedAddressAudit model and storage broker setup

---
+++ LHDS.Core/Models/Foundations/ResolvedAddressesAudits/ResolvedAddressAudit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LHDS.Core.Models.Bases;
+
+namespace LHDS.Core.Models.Foundations.ResolvedAddressesAudits
+{
+    public class ResolvedAddressAudit : IKey, IAudit
+    {
+        public Guid Id { get; set; }
+        public Guid UniqueResolvedAddressReference { get; set; }
+        public Guid CorrelationId { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string CreatedBy { get; set; } = string.Empty;
+        public string UpdatedBy { get; set; } = string.Empty;
+        public DateTimeOffset UpdatedDate { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}

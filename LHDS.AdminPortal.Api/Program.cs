4d2f0e5900cb4a2ac06760778a56e8b499283a14
davidhayes03 <david.hayes17@nhs.net>
2025-07-03 11:27:25 +0100

    CODE RUB: Api Route page

--- LHDS.AdminPortal.Api/Program.cs
+++ LHDS.AdminPortal.Api/Program.cs
@@ -83,6 +83,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.OData;
 using Microsoft.EntityFrameworkCore;
@@ -225,6 +226,7 @@ namespace LHDS.AdminPortal.Api
                 ResponseWriter = HealthCheckResponseWriter.WriteResponse
             });
 
+            app.MapGet("/api", () => Results.Ok("AdminPortal API is running"));
             app.UseHttpsRedirection();
             app.UseCors("AllowFrontendOrigin");
             app.UseRouting();

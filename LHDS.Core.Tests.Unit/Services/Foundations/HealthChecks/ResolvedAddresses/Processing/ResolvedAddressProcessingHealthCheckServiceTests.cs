5835eb193bf29e459ff6dc65ea7ca7a45568fa2a
Christo du Toit <christo@dutoit.co.uk>
2025-07-09 16:32:13 +0100

    CODE RUB: Code cleanup

--- LHDS.Core.Tests.Unit/Services/Foundations/HealthChecks/ResolvedAddresses/Processing/ResolvedAddressProcessingHealthCheckServiceTests.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/HealthChecks/ResolvedAddresses/Processing/ResolvedAddressProcessingHealthCheckServiceTests.cs
@@ -85,6 +85,6 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.HealthChecks.ResolvedAddress
         }
 
         private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
-          actualException => actualException.SameExceptionAs(expectedException);
+            actualException => actualException.SameExceptionAs(expectedException);
     }
 }

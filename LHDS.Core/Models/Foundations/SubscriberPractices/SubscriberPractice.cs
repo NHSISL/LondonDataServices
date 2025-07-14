ce345430f97ed00a1aa6f3b9d06fe3339664b920
Hassan Raza <h_raza110@outlook.com>
2025-07-14 15:25:04 +0100

    BROKER: Subscriber Practice

---
+++ LHDS.Core/Models/Foundations/SubscriberPractices/SubscriberPractice.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using LHDS.Core.Models.Bases;
+using LHDS.Core.Models.Foundations.SubscriberAgreements;
+
+namespace LHDS.Core.Models.Foundations.SubscriberPractices
+{
+    public class SubscriberPractice : IKey, IAudit
+    {
+        public Guid Id { get; set; }
+        public Guid SubscriberAgreementId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string PracticeCode { get; set; } = string.Empty;
+        public string CreatedBy { get; set; } = string.Empty;
+        public string UpdatedBy { get; set; } = string.Empty;
+        public DateTimeOffset UpdatedDate { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public SubscriberAgreement SubscriberAgreement { get; set; }
+    }
+}

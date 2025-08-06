3dcd6f1319ac7b73f4daf4fb10de55a9dc2a7050
Christo du Toit <christo@dutoit.co.uk>
2025-08-06 15:09:28 +0100

    ShouldProcessDecryptedItemsForBatchCompleteAsync -> PASS

---
+++ LHDS.Core.Tests.Integration.Decryptions/DecryptionTests.ProcessDecryptedItemsForBatchComplete.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace LHDS.Core.Tests.Integration.Decryptions
+{
+    public partial class DecryptionTests
+    {
+        [ReleaseCandidateFact]
+        public async Task ShouldProcessDecryptedItemsForBatchCompleteAsync()
+        {
+            // given
+
+            // when
+            await decryptionClient.ProcessDecryptedItemsForBatchCompleteAsync();
+
+            // then
+        }
+    }
+}

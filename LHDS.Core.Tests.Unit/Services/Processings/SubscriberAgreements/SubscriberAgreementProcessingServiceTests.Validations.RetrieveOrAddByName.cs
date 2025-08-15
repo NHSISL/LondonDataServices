2353bb26ac3830f0750db9af76df396c7859515a
Hassan Raza <h_raza110@outlook.com>
2025-08-15 12:27:29 +0100

    ShouldThrowValidationExceptionsOnRetrieveOrAddByNameIfSubscriberAgreementProcessingIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Processings/SubscriberAgreements/SubscriberAgreementProcessingServiceTests.Validations.RetrieveOrAddByName.cs
@@ -0,0 +1,53 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.SubscriberAgreements;
+using LHDS.Core.Models.Processings.SubscriberAgreements.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Processings.SubscriberAgreements
+{
+    public partial class SubscriberAgreementProcessingServiceTests
+    {
+        [Fact]
+        public async Task 
+            ShouldThrowValidationExceptionsOnRetrieveOrAddByNameIfSubscriberAgreementProcessingIsNullAndLogItAsync()
+        {
+            // given
+            SubscriberAgreement nullSubscriberAgreement = null;
+
+            var nullSubscriberAgreementProcessingException =
+                new NullSubscriberAgreementProcessingException(message: "Subscriber agreement is null.");
+
+            var expectedSubscriberAgreementProcessingValidationException =
+                new SubscriberAgreementProcessingValidationException(
+                    message: "Subscriber agreement processing validation error occurred, please try again.",
+                    innerException: nullSubscriberAgreementProcessingException);
+
+            // when
+            ValueTask<SubscriberAgreement> subscriberAgreementModifyTask =
+                this.subscriberAgreementProcessingService.RetrieveOrAddSubscriberAgreementByNameAsync(
+                    nullSubscriberAgreement);
+
+            SubscriberAgreementProcessingValidationException actualSubscriberAgreementProcessingValidationException =
+                await Assert.ThrowsAsync<SubscriberAgreementProcessingValidationException>(
+                    subscriberAgreementModifyTask.AsTask);
+
+            //then
+            actualSubscriberAgreementProcessingValidationException.Should()
+                .BeEquivalentTo(expectedSubscriberAgreementProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedSubscriberAgreementProcessingValidationException))),
+                        Times.Once);
+
+            this.subscriberAgreementServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

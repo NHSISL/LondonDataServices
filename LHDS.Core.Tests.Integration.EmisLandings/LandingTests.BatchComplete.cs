24e21b49b1a8745d386183931588bd3670617c15
Christo du Toit <christo.dutoit@nhs.net>
2025-08-18 16:24:21 +0100

    CODE RUB: Code cleanup

---
+++ LHDS.Core.Tests.Integration.EmisLandings/LandingTests.BatchComplete.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LHDS.Core.Tests.Integration.EmisLandings
+{
+    public partial class LandingTests
+    {
+        [Fact]
+        public async Task ShouldCheckForBatchCompleteAsync()
+        {
+            try
+            {
+                // given
+
+                // when
+                await this.decryptionClient.ProcessDecryptedItemsForBatchCompleteAsync();
+
+                // then
+            }
+            catch (Exception ex)
+            {
+                await loggingBroker.LogErrorAsync(ex);
+                Console.WriteLine(ex.Message);
+                Microsoft.VisualStudio.TestTools.UnitTesting.Assert.Fail($"{ex.Message}, {ex?.InnerException?.Message}");
+            }
+        }
+    }
+}

a555c8f3520e394e5c12c3842bf4b26cb971e42a
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-29 15:08:20 +0100

    ShouldAddDecisionPollAsync -> FAIL

---
+++ LHDS.Core/Services/Foundations/DecisionPolls/DecisionPollService.cs
@@ -0,0 +1,37 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Brokers.Securities;
+using LHDS.Core.Brokers.Storages.Sql;
+using LHDS.Core.Models.Foundations.DecisionPolls;
+
+namespace LHDS.Core.Services.Foundations.DecisionPolls
+{
+    public partial class DecisionPollService : IDecisionPollService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ISecurityBroker securityBroker;
+        private readonly ILoggingBroker loggingBroker;
+        public DecisionPollService(
+            IStorageBroker storageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ISecurityBroker securityBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.securityBroker = securityBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<DecisionPoll> AddDecisionPollAsync(DecisionPoll decisionPoll)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

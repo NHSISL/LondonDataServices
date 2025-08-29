6bd1fa68c180752bb4c5fae869adce0d5778d2d4
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-29 11:45:09 +0100

    BROKERS: Decision Poll

---
+++ LHDS.Core/Brokers/Storages/Sql/StorageBroker.DecisionPoll.Configurations.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LHDS.Core.Models.Foundations.DecisionPolls;
+using Microsoft.EntityFrameworkCore;
+
+namespace LHDS.Core.Brokers.Storages.Sql
+{
+    public partial class StorageBroker
+    {
+        private static void AddDecisionPollConfigurations(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DecisionPoll>()
+                .ToTable("DecisionPolls", "Decision");
+
+            modelBuilder.Entity<DecisionPoll>()
+                .Property(decisionPoll => decisionPoll.Id)
+                .IsRequired();
+
+            modelBuilder.Entity<DecisionPoll>()
+                .Property(decisionPoll => decisionPoll.LastPoll)
+                .IsRequired(true);
+
+            modelBuilder.Entity<DecisionPoll>()
+                .Property(decisionPoll => decisionPoll.CreatedBy)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            modelBuilder.Entity<DecisionPoll>()
+                .Property(decisionPoll => decisionPoll.CreatedDate)
+                .IsRequired();
+
+            modelBuilder.Entity<DecisionPoll>()
+                .Property(decisionPoll => decisionPoll.UpdatedBy)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            modelBuilder.Entity<DecisionPoll>()
+                .Property(decisionPoll => decisionPoll.UpdatedDate)
+                .IsRequired();
+        }
+    }
+}

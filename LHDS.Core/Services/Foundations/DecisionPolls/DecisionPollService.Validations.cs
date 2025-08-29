35ef23150f3614e8a027b84f21c5c4af225047e2
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-29 16:48:33 +0100

    ShouldThrowValidationExceptionOnAddIfDecisionPollsIsInvalidAndLogItAsync -> PASS

--- LHDS.Core/Services/Foundations/DecisionPolls/DecisionPollService.Validations.cs
+++ LHDS.Core/Services/Foundations/DecisionPolls/DecisionPollService.Validations.cs
@@ -17,7 +17,31 @@ namespace LHDS.Core.Services.Foundations.DecisionPolls
             EntraUser currentUser = await this.securityBroker.GetCurrentUserAsync();
 
             Validate(
-            (Rule: await IsNotRecentAsync(decisionPoll.CreatedDate), Parameter: nameof(DecisionPoll.CreatedDate)));
+                (Rule: IsInvalid(decisionPoll.Id), Parameter: nameof(DecisionPoll.Id)),
+                (Rule: IsInvalid(decisionPoll.LastPoll), Parameter: nameof(DecisionPoll.LastPoll)),
+                (Rule: IsInvalid(decisionPoll.CreatedDate), Parameter: nameof(DecisionPoll.CreatedDate)),
+                (Rule: IsInvalid(decisionPoll.CreatedBy), Parameter: nameof(DecisionPoll.CreatedBy)),
+                (Rule: IsInvalid(decisionPoll.UpdatedDate), Parameter: nameof(DecisionPoll.UpdatedDate)),
+                (Rule: IsInvalid(decisionPoll.UpdatedBy), Parameter: nameof(DecisionPoll.UpdatedBy)),
+
+                (Rule: IsNotSame(
+                        first: currentUser.EntraUserId,
+                        second: decisionPoll.CreatedBy),
+                    Parameter: nameof(DecisionPoll.CreatedBy)),
+
+                (Rule: IsNotSame(
+                        firstDate: decisionPoll.UpdatedDate,
+                        secondDate: decisionPoll.CreatedDate,
+                        secondDateName: nameof(DecisionPoll.CreatedDate)),
+                    Parameter: nameof(DecisionPoll.UpdatedDate)),
+
+                (Rule: IsNotSame(
+                        first: decisionPoll.UpdatedBy,
+                        second: decisionPoll.CreatedBy,
+                        secondName: nameof(DecisionPoll.CreatedBy)),
+                    Parameter: nameof(DecisionPoll.UpdatedBy)),
+
+                (Rule: await IsNotRecentAsync(decisionPoll.CreatedDate), Parameter: nameof(DecisionPoll.CreatedDate)));
         }
 
         private static void ValidateDecisionPollIsNotNull(DecisionPoll decisionPoll)
@@ -27,6 +51,50 @@ namespace LHDS.Core.Services.Foundations.DecisionPolls
                 throw new NullDecisionPollException(message: "DecisionPoll is null.");
             }
         }
+
+        private static dynamic IsInvalid(Guid id) => new
+        {
+            Condition = id == Guid.Empty,
+            Message = "Id is required"
+        };
+
+        private static dynamic IsInvalid(string? text) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(text),
+            Message = "Text is required"
+        };
+
+        private static dynamic IsInvalid(DateTimeOffset date) => new
+        {
+            Condition = date == default,
+            Message = "Date is required"
+        };
+
+        private static dynamic IsNotSame(
+            DateTimeOffset firstDate,
+            DateTimeOffset secondDate,
+            string secondDateName) => new
+            {
+                Condition = firstDate != secondDate,
+                Message = $"Date is not the same as {secondDateName}"
+            };
+
+        private static dynamic IsNotSame(
+            string first,
+            string second,
+            string secondName) => new
+            {
+                Condition = first != second,
+                Message = $"Text is not the same as {secondName}"
+            };
+
+        private static dynamic IsNotSame(
+            string first,
+            string second) => new
+            {
+                Condition = first != second,
+                Message = $"Expected value to be '{first}' but found '{second}'."
+            };
 
         private async ValueTask<dynamic> IsNotRecentAsync(DateTimeOffset date) => new
         {

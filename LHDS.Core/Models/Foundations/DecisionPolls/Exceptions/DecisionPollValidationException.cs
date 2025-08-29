25d967e2bde7bd954fe5c4b0c8174a1ce7cd3b17
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-29 16:35:07 +0100

    ShouldThrowValidationExceptionOnAddIfDecisionPollIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/DecisionPolls/Exceptions/DecisionPollValidationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.DecisionPolls.Exceptions
+{
+    public class DecisionPollValidationException : Xeption
+    {
+        public DecisionPollValidationException(string message, Xeption? innerException)
+            : base(message, innerException)
+        { }
+    }
+}

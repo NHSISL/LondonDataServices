04993d78b5a74aba26645a1338d0807f1d816537
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-08-29 16:55:55 +0100

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/DecisionPolls/Exceptions/DecisionPollDependencyException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.DecisionPolls.Exceptions
+{
+    public class DecisionPollDependencyException : Xeption
+    {
+        public DecisionPollDependencyException(string message, Xeption? innerException)
+            : base(message, innerException)
+        { }
+    }
+}

c25ea91c0c784da295684441abdd4848c05e8e53
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-09-01 12:43:06 +0100

    ShouldThrowValidationExceptionOnModifyIfReferenceErrorOccursAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Foundations/DecisionPolls/DecisionPollServiceTests.Exceptions.Modify.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/DecisionPolls/DecisionPollServiceTests.Exceptions.Modify.cs
@@ -3,6 +3,7 @@
 // ---------------------------------------------------------
 
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using FluentAssertions;
 using LHDS.Core.Models.Foundations.DecisionPolls;
 using LHDS.Core.Models.Foundations.DecisionPolls.Exceptions;
@@ -62,6 +63,64 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.DecisionPolls
 
             this.storageBrokerMock.Verify(broker =>
                 broker.UpdateDecisionPollAsync(randomDecisionPoll),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfReferenceErrorOccursAndLogItAsync()
+        {
+            // given
+            DecisionPoll someDecisionPoll = CreateRandomDecisionPoll();
+            string randomMessage = GetRandomString();
+            string exceptionMessage = randomMessage;
+
+            var foreignKeyConstraintConflictException =
+                new ForeignKeyConstraintConflictException(exceptionMessage);
+
+            var invalidDecisionPollReferenceException =
+                new InvalidDecisionPollReferenceException(
+                    message: "Invalid decisionPoll reference error occurred.",
+                    innerException: foreignKeyConstraintConflictException);
+
+            DecisionPollDependencyValidationException expectedDecisionPollDependencyValidationException =
+                new DecisionPollDependencyValidationException(
+                    message: "DecisionPoll dependency validation occurred, please try again.",
+                    innerException: invalidDecisionPollReferenceException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ThrowsAsync(foreignKeyConstraintConflictException);
+
+            // when
+            ValueTask<DecisionPoll> modifyDecisionPollTask =
+                this.decisionPollService.ModifyDecisionPollAsync(someDecisionPoll);
+
+            DecisionPollDependencyValidationException actualDecisionPollDependencyValidationException =
+                await Assert.ThrowsAsync<DecisionPollDependencyValidationException>(
+                    modifyDecisionPollTask.AsTask);
+
+            // then
+            actualDecisionPollDependencyValidationException.Should()
+                .BeEquivalentTo(expectedDecisionPollDependencyValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectDecisionPollByIdAsync(someDecisionPoll.Id),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(expectedDecisionPollDependencyValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateDecisionPollAsync(someDecisionPoll),
                     Times.Never);
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();

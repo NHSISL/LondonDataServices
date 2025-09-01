dbf7dd73d8e8cab43718014c38cf63a09ff5e009
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-09-01 16:05:14 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/DecisionPolls/DecisionPollServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,64 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.DecisionPolls;
+using LHDS.Core.Models.Foundations.DecisionPolls.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.DecisionPolls
+{
+    public partial class DecisionPollServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedDecisionPollStorageException =
+                new FailedDecisionPollStorageException(
+                    message: "Failed decisionPoll storage error occurred, please contact support.",
+                    innerException: sqlException);
+
+            var expectedDecisionPollDependencyException =
+                new DecisionPollDependencyException(
+                    message: "DecisionPoll dependency error occurred, please contact support.",
+                    innerException: failedDecisionPollStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllDecisionPollsAsync())
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<IQueryable<DecisionPoll>> retrieveAllDecisionPollsTask =
+                this.decisionPollService.RetrieveAllDecisionPollsAsync();
+
+            DecisionPollDependencyException actualDecisionPollDependencyException =
+                await Assert.ThrowsAsync<DecisionPollDependencyException>(
+                    testCode: retrieveAllDecisionPollsTask.AsTask);
+
+            // then
+            actualDecisionPollDependencyException.Should()
+                .BeEquivalentTo(expectedDecisionPollDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllDecisionPollsAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedDecisionPollDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

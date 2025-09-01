68cf45e3b12f40939d1fae1f03845b47b73a74d8
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-09-01 11:44:46 +0100

    ShouldThrowValidationExceptionOnModifyIfDecisionPollDoesNotExistAndLogItAsync -> FAIL

---
+++ LHDS.Core/Models/Foundations/DecisionPolls/Exceptions/NotFoundDecisionPollException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace LHDS.Core.Models.Foundations.DecisionPolls.Exceptions
+{
+    public class NotFoundDecisionPollException : Xeption
+    {
+        public NotFoundDecisionPollException(Guid decisionPollId)
+            : base(message: $"Couldn't find decisionPoll with decisionPollId: {decisionPollId}.")
+        { }
+    }
+}

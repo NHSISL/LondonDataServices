4fb21bd7f965aa4be1a0acf7f17f638072abb718
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-09-04 16:22:08 +0100

    ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> PASS

--- LHDS.Core.Tests.Unit/Services/Foundations/DecisionPolls/DecisionPollServiceTests.Exceptions.RemoveById.cs
+++ LHDS.Core.Tests.Unit/Services/Foundations/DecisionPolls/DecisionPollServiceTests.Exceptions.RemoveById.cs
@@ -2,11 +2,13 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using LHDS.Core.Models.Foundations.DecisionPolls;
 using LHDS.Core.Models.Foundations.DecisionPolls.Exceptions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
 
@@ -62,6 +64,59 @@ namespace LHDS.Core.Tests.Unit.Services.Foundations.DecisionPolls
 
             this.dateTimeBrokerMock.Verify(broker =>
                 broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someDecisionPollId = Guid.NewGuid();
+
+            var databaseUpdateConcurrencyException =
+                new DbUpdateConcurrencyException();
+
+            var lockedDecisionPollException =
+                new LockedDecisionPollException(
+                    message: "Locked decisionPoll record exception, please try again later",
+                    innerException: databaseUpdateConcurrencyException);
+
+            var expectedDecisionPollDependencyValidationException =
+                new DecisionPollDependencyValidationException(
+                    message: "DecisionPoll dependency validation occurred, please try again.",
+                    innerException: lockedDecisionPollException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectDecisionPollByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(databaseUpdateConcurrencyException);
+
+            // when
+            ValueTask<DecisionPoll> removeDecisionPollByIdTask =
+                this.decisionPollService.RemoveDecisionPollByIdAsync(someDecisionPollId);
+
+            DecisionPollDependencyValidationException actualDecisionPollDependencyValidationException =
+                await Assert.ThrowsAsync<DecisionPollDependencyValidationException>(
+                    removeDecisionPollByIdTask.AsTask);
+
+            // then
+            actualDecisionPollDependencyValidationException.Should()
+                .BeEquivalentTo(expectedDecisionPollDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectDecisionPollByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedDecisionPollDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteDecisionPollAsync(It.IsAny<DecisionPoll>()),
                     Times.Never);
 
             this.storageBrokerMock.VerifyNoOtherCalls();

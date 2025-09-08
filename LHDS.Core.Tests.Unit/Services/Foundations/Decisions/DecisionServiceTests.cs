9c50526a4bc2f93cbb683ebae58a981afc181cd1
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-09-08 15:35:08 +0100

    ShouldGetPatientDecisionsAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/Decisions/DecisionServiceTests.cs
@@ -0,0 +1,127 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Decisions;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Brokers.Securities;
+using LHDS.Core.Models.Foundations.Decisions;
+using LHDS.Core.Services.Decisions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Tynamix.ObjectFiller;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Decisions
+{
+    public partial class DecisionServiceTests
+    {
+        private readonly Mock<IDecisionBroker> decisionBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ISecurityBroker> securityBrokerMock;
+        private readonly Mock<ISecurityAuditBroker> securityAuditBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IDecisionService decisionService;
+
+        public DecisionServiceTests()
+        {
+            this.decisionBrokerMock = new Mock<IDecisionBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.securityBrokerMock = new Mock<ISecurityBroker>();
+            this.securityAuditBrokerMock = new Mock<ISecurityAuditBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.decisionService = new DecisionService(
+                decisionBroker: this.decisionBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                securityBroker: this.securityBrokerMock.Object,
+                securityAuditBroker: this.securityAuditBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
+
+        private static string GetRandomString() =>
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static string GetRandomStringWithLengthOf(int length)
+        {
+            string result = new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
+            return result.Length > length ? result.Substring(0, length) : result;
+        }
+
+        public static TheoryData<int> MinutesBeforeOrAfter()
+        {
+            int randomNumber = GetRandomNumber();
+            int randomNegativeNumber = GetRandomNegativeNumber();
+
+            return new TheoryData<int>
+            {
+                randomNumber,
+                randomNegativeNumber
+            };
+        }
+
+        private static SqlException GetSqlException() =>
+            (SqlException)RuntimeHelpers.GetUninitializedObject(typeof(SqlException));
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static int GetRandomNegativeNumber() =>
+            -1 * new IntRange(min: 2, max: 10).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Decision CreateRandomModifyDecision(DateTimeOffset dateTimeOffset, string userId = "")
+        {
+            int randomDaysInPast = GetRandomNegativeNumber();
+            Decision randomDecision = CreateRandomDecision(dateTimeOffset, userId);
+
+            randomDecision.CreatedDate =
+                randomDecision.CreatedDate.AddDays(randomDaysInPast);
+
+            return randomDecision;
+        }
+
+        private static List<Decision> CreateRandomDecisions()
+        {
+            return CreateDecisionFiller(dateTimeOffset: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber())
+                    .ToList();
+        }
+
+        private static Decision CreateRandomDecision() =>
+            CreateDecisionFiller(dateTimeOffset: GetRandomDateTimeOffset()).Create();
+
+        private static Decision CreateRandomDecision(DateTimeOffset dateTimeOffset, string userId = "") =>
+            CreateDecisionFiller(dateTimeOffset, userId).Create();
+
+        private static Filler<Decision> CreateDecisionFiller(DateTimeOffset dateTimeOffset, string userId = "")
+        {
+            userId = string.IsNullOrEmpty(userId) ? Guid.NewGuid().ToString() : userId;
+            var filler = new Filler<Decision>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(decision => decision.DecisionChoice).Use(GetRandomStringWithLengthOf(255))
+                .OnProperty(decision => decision.CreatedBy).Use(userId)
+                .OnProperty(decision => decision.UpdatedBy).Use(userId)
+                .OnProperty(decision => decision.DecisionType).IgnoreIt()
+                .OnProperty(decision => decision.Patient).IgnoreIt();
+
+            return filler;
+        }
+    }
+}
---
+++ LHDS.Core/Services/Decisions/DecisionService.cs
@@ -0,0 +1,43 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.Core.Brokers.DateTimes;
+using LHDS.Core.Brokers.Decisions;
+using LHDS.Core.Brokers.Loggings;
+using LHDS.Core.Brokers.Securities;
+using LHDS.Core.Models.Foundations.Decisions;
+
+namespace LHDS.Core.Services.Decisions
+{
+    public class DecisionService : IDecisionService
+    {
+        private readonly IDecisionBroker decisionBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ISecurityBroker securityBroker;
+        private readonly ISecurityAuditBroker securityAuditBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public DecisionService(
+            IDecisionBroker decisionBroker,
+            IDateTimeBroker dateTimeBroker,
+            ISecurityBroker securityBroker,
+            ISecurityAuditBroker securityAuditBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.decisionBroker = decisionBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.securityBroker = securityBroker;
+            this.securityAuditBroker = securityAuditBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public async ValueTask<List<Decision>> GetPatientDecisions(DateTimeOffset? lastPollDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

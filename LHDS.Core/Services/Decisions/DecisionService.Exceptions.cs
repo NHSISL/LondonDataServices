69b31033022094a2ca987b7ba6a1d332fab18aaf
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-09-08 16:43:06 +0100

    ShouldThrowNullDecisionsExceptionOnGetPatientDecisionsIfDecisionsIsNullAndLogItAsync -> FAIL

---
+++ LHDS.Core/Services/Decisions/DecisionService.Exceptions.cs
@@ -0,0 +1,28 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Decisions;
+
+namespace LHDS.Core.Services.Decisions
+{
+    public partial class DecisionService
+    {
+        private delegate ValueTask<List<Decision>> ReturningDecisionsFunction();
+
+        private async ValueTask<List<Decision>> TryCatch(ReturningDecisionsFunction returningDecisionsFunction)
+        {
+            try
+            {
+                return await returningDecisionsFunction();
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}

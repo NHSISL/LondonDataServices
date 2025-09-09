93c110e7b43ad54bbe57974527d2d55db20e6db3
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-09-09 11:32:29 +0100

    ShouldRecordAdoptionAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/Decisions/DecisionServiceTests.Logic.RecordAdoption.cs
@@ -0,0 +1,37 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Decisions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.Decisions
+{
+    public partial class DecisionServiceTests
+    {
+        [Fact]
+        public async Task ShouldRecordAdoptionAsync()
+        {
+            // given
+            List<Decision> randomDecisionsAdopted = CreateRandomDecisions();
+            List<Decision> inputDecisionsAdopted = randomDecisionsAdopted;
+
+            this.decisionBrokerMock.Setup(broker =>
+                broker.RecordAdoption(inputDecisionsAdopted));
+
+            // when
+            await this.decisionService.RecordAdoption(inputDecisionsAdopted);
+
+            // then
+            this.decisionBrokerMock.Verify(broker =>
+                broker.RecordAdoption(inputDecisionsAdopted),
+                    Times.Once);
+
+            this.decisionBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

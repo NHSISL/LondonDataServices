ef5713706590d02a20118c89e0f03ccf2eb3beab
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-09-10 15:01:57 +0100

    ShouldThrowValidationExceptionOnDecryptIfBlobContainerIsNullAndLogItAsync -> PASS

---
+++ LHDS.Core/Services/Orchestrations/Decisions/DecisionOrchestrationService.Validations.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LHDS.Core.Models.Orchestrations.Decisions.Exceptions;
+
+namespace LHDS.Core.Services.Orchestrations.Decisions
+{
+    public partial class DecisionOrchestrationService
+    {
+        private void ValidateBlobContainersIsNotNull()
+        {
+            if (this.blobContainers is null)
+            {
+                throw new NullBlobContainersDecisionOrchestrationException(
+                    message: "Null blob container decision orchestration exception, " +
+                             "please correct the errors and try again.");
+            }
+        }
+    }
+}

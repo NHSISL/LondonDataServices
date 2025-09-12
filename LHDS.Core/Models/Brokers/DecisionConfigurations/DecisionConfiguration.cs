9f837fd0489ba03d41c926b773700174e8fcc2e3
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-09-12 12:01:13 +0100

    NotifyAdoptionOfDecisions

---
+++ LHDS.Core/Models/Brokers/DecisionConfigurations/DecisionConfiguration.cs
@@ -0,0 +1,11 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+namespace LHDS.Core.Models.Brokers.DecisionConfigurations
+{
+    public class DecisionConfiguration
+    {
+        public string HashPepper { get; set; } = string.Empty;
+    }
+}

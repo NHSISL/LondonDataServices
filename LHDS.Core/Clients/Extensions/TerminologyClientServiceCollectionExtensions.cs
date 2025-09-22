5b5ffa5aa2d69b4cd1e5802a0826d987a8d40ebd
Hassan Raza <h_raza110@outlook.com>
2025-09-22 10:40:20 +0100

    CODE RUB: Terminology Client Extentions Validation Changes

--- LHDS.Core/Clients/Extensions/TerminologyClientServiceCollectionExtensions.cs
+++ LHDS.Core/Clients/Extensions/TerminologyClientServiceCollectionExtensions.cs
@@ -35,6 +35,7 @@ using LHDS.Core.Services.Processings.TerminologyPolls;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Xeptions;
 
 namespace LHDS.Core.Clients.Extensions
 {
@@ -200,6 +201,8 @@ namespace LHDS.Core.Clients.Extensions
             }
 
             Validate(
+                createException: (message, data) => new InvalidConfigurationException(message, data: data),
+
                 (Rule: IsInvalid(blobStorageSettings.AzureBlobServiceUri),
                     Parameter: "blobStorage__azureBlobServiceUri"),
 
@@ -215,6 +218,8 @@ namespace LHDS.Core.Clients.Extensions
             }
 
             Validate(
+                createException: (message, data) => new InvalidConfigurationException(message, data: data),
+
                 (Rule: IsInvalid(ontologyConfiguration.TerminologyServerBaseUrl),
                     Parameter: "ontologySettings__terminologyServerBaseUrl"),
 
@@ -246,35 +251,41 @@ namespace LHDS.Core.Clients.Extensions
             Message = "Configuration value does not exist"
         };
 
-        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        private static void Validate<T>(
+            Func<string, IDictionary, T> createException,
+            params (dynamic Rule, string Parameter)[] validations)
+            where T : Xeption
         {
             StringBuilder validationErrors = new StringBuilder();
             validationErrors.AppendLine("Configuration error(s):");
+
+            // Non-generic IDictionary to match InvalidConfigurationException
             IDictionary errors = new Dictionary<string, List<string>>();
 
             foreach ((dynamic rule, string parameter) in validations)
             {
                 if (rule.Condition)
                 {
-                    validationErrors.AppendLine(
-                        $"{parameter} -> Configuration value does not exist or does not meet validation criteria");
+                    validationErrors.AppendLine($"{parameter}");
 
                     if (errors.Contains(parameter))
                     {
                         (errors[parameter] as List<string>)?.Add(rule.Message);
-                        return;
                     }
-
-                    errors.Add(parameter, new List<string> { rule.Message });
+                    else
+                    {
+                        errors.Add(parameter, new List<string> { rule.Message });
+                    }
                 }
             }
 
-            var invalidConfigurationException = new InvalidConfigurationException(
-                message: validationErrors.ToString(),
-                data: errors);
+            T invalidDataException = createException(
+                validationErrors.ToString(),
+                errors);
 
-            invalidConfigurationException.ThrowIfContainsErrors();
+            invalidDataException.ThrowIfContainsErrors();
         }
+
 
         /// <summary>
         /// Extracts a <see cref="ClaimsPrincipal"/> from a given JWT token.

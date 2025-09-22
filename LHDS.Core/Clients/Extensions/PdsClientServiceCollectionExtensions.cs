4dfa5221c11e2d1bfe66445e20555385ac459eec
Hassan Raza <h_raza110@outlook.com>
2025-09-22 10:44:36 +0100

    CODE RUB: Pds Client Extention Validation

--- LHDS.Core/Clients/Extensions/PdsClientServiceCollectionExtensions.cs
+++ LHDS.Core/Clients/Extensions/PdsClientServiceCollectionExtensions.cs
@@ -39,6 +39,7 @@ using LHDS.Core.Services.Orchestrations.Pds;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Xeptions;
 
 namespace LHDS.Core.Clients.Extensions
 {
@@ -341,6 +342,8 @@ namespace LHDS.Core.Clients.Extensions
             }
 
             Validate(
+                createException: (message, data) => new InvalidConfigurationException(message, data: data),
+
                 (Rule: IsInvalid(meshConfigurationSettings.MailboxId),
                     Parameter: "meshConfiguration__mailboxId"),
 
@@ -365,6 +368,8 @@ namespace LHDS.Core.Clients.Extensions
             if (acceptanceTest)
             {
                 Validate(
+                    createException: (message, data) => new InvalidConfigurationException(message, data: data),
+
                     (Rule: IsInvalid(meshConfigurationSettings.TlsRootCertificates),
                         Parameter: "meshConfiguration__tlsRootCertificates__0"),
 
@@ -385,6 +390,8 @@ namespace LHDS.Core.Clients.Extensions
             }
 
             Validate(
+                createException: (message, data) => new InvalidConfigurationException(message, data: data),
+
                 (Rule: IsInvalid(pdsConfiguration.InputFolder),
                     Parameter: "pdsSettings__inputFolder"),
 
@@ -413,6 +420,8 @@ namespace LHDS.Core.Clients.Extensions
             }
 
             Validate(
+                createException: (message, data) => new InvalidConfigurationException(message, data: data),
+
                 (Rule: IsInvalid(blobStorageSettings.AzureBlobServiceUri),
                     Parameter: "blobStorage__azureBlobServiceUri"),
 
@@ -438,35 +447,41 @@ namespace LHDS.Core.Clients.Extensions
             Message = "Configuration value does not exist"
         };
 
-        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        private static void Validate<T>(
+            Func<string, IDictionary, T> createException,
+            params (dynamic Rule, string Parameter)[] validations)
+            where T : Xeption
         {
             StringBuilder validationErrors = new StringBuilder();
             validationErrors.AppendLine("Configuration error(s):");
+
+            // Non-generic IDictionary to match InvalidConfigurationException
             IDictionary errors = new Dictionary<string, List<string>>();
 
             foreach ((dynamic rule, string parameter) in validations)
             {
                 if (rule.Condition)
                 {
-                    validationErrors.AppendLine(
-                        $"{parameter} -> Configuration value does not exist or does not meet validation criteria");
+                    validationErrors.AppendLine($"{parameter}");
 
                     if (errors.Contains(parameter))
                     {
                         (errors[parameter] as List<string>)?.Add(rule.Message);
-                        return;
                     }
-
-                    errors.Add(parameter, new List<string> { rule.Message });
+                    else
+                    {
+                        errors.Add(parameter, new List<string> { rule.Message });
+                    }
                 }
             }
 
-            var invalidConfigurationException = new InvalidConfigurationException(
-                message: validationErrors.ToString(),
-                data: errors);
+            T invalidDataException = createException(
+                validationErrors.ToString(),
+                errors);
 
-            invalidConfigurationException.ThrowIfContainsErrors();
+            invalidDataException.ThrowIfContainsErrors();
         }
+
 
         /// <summary>
         /// Extracts a <see cref="ClaimsPrincipal"/> from a given JWT token.

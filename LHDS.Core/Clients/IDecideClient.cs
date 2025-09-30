9cba1abbfac7ea6283203d9a548907c4f0f8340c
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-09-30 14:02:03 +0100

    EXPOSERS: IDecideClient Function

---
+++ LHDS.Core/Clients/IDecideClient.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LHDS.Core.Models.Foundations.Decisions;
+
+namespace LHDS.Core.Clients
+{
+    public class IDecideClient : IIDecideClient
+    {
+        public ValueTask<List<Decision>> GetPatientDecisions()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

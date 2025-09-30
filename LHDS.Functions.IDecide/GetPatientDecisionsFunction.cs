926d141023f705fb05a488ad098370737d7e9e4c
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-09-30 14:54:00 +0100

    Make timer configurable in GetPatientDecisionsFunction

--- LHDS.Functions.IDecide/GetPatientDecisionsFunction.cs
+++ LHDS.Functions.IDecide/GetPatientDecisionsFunction.cs
@@ -8,6 +8,7 @@ using LHDS.Core.Brokers.Loggings;
 using LHDS.Core.Clients;
 using LHDS.Functions.IDecide.Models;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
 namespace LHDS.Functions.IDecide
 {
@@ -15,17 +16,20 @@ namespace LHDS.Functions.IDecide
     {
         private readonly ILoggingBroker loggingBroker;
         private readonly IIDecideClient iDecideClient;
+        private readonly ILogger logger;
 
         public GetPatientDecisionsFunction(
             ILoggingBroker loggingBroker,
-            IIDecideClient iDecideClient)
+            IIDecideClient iDecideClient,
+            ILoggerFactory loggerFactory)
         {
             this.loggingBroker = loggingBroker;
             this.iDecideClient = iDecideClient;
+            this.logger = loggerFactory.CreateLogger<GetPatientDecisionsFunction>();
         }
 
         [Function("GetPatientDecisionsFunction")]
-        public async Task Run([TimerTrigger("0 */15 * * * *")] MyInformation myTimer)
+        public async Task Run([TimerTrigger("%getPatientDecisionsFunction%")] MyInformation myTimer)
         {
             await this.loggingBroker.LogInformationAsync($"C# Timer trigger function executed at: {DateTime.Now}");
 

e1ab41a3d660a0e4e7bb534439bd74fb7a99c3e4
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-01 15:39:24 +0100

    ShouldThrowDependencyValidationOnGetPatientDecisionsIfDependencyValidationOccursAndLogItAsync -> FAIL

---
+++ LHDS.Core.Tests.Unit/Clients/IDecide/IDecideClientTests.Exceptions.GetPatientDecisions.cs
@@ -0,0 +1,51 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Clients.IDecideClient.Exceptions;
+using LHDS.Core.Models.Foundations.Decisions;
+using Moq;
+using Xeptions;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Clients.IDecide
+{
+    public partial class IDecideClientTests
+    {
+        [Theory]
+        [MemberData(nameof(IDecideClientDependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationOnGetPatientDecisionsIfDependencyValidationOccursAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+
+            // given
+            var expectedDependencyValidationException = new IDecideClientValidationException(
+                message: "IDecide client validation error occurred, fix errors and try again.",
+                innerException: dependencyValidationException.InnerException as Xeption);
+
+            this.decisionOrchestrationServiceMock.Setup(orchestration =>
+                orchestration.GetPatientDecisions())
+                    .ThrowsAsync(dependencyValidationException);
+
+            // when
+            ValueTask<List<Decision>> getPatientDecisionsTask =
+                this.iDecideClient.GetPatientDecisions();
+
+            IDecideClientValidationException actualException =
+                await Assert.ThrowsAsync<IDecideClientValidationException>(
+                    getPatientDecisionsTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedDependencyValidationException);
+
+            this.decisionOrchestrationServiceMock.Verify(orchestration =>
+                orchestration.GetPatientDecisions(),
+                    Times.Once);
+
+            this.decisionOrchestrationServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}

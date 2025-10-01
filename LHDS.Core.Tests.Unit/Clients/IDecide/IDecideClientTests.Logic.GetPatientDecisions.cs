008e9982bce7ad450b81ce530e19da5e0dc2195e
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-01 14:09:32 +0100

    ShouldGetPatientDecisions -> FAIL

---
+++ LHDS.Core.Tests.Unit/Clients/IDecide/IDecideClientTests.Logic.GetPatientDecisions.cs
@@ -0,0 +1,40 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LHDS.Core.Models.Foundations.Decisions;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Clients.IDecide
+{
+    public partial class IDecideClientTests
+    {
+        [Fact]
+        public async Task ShouldGetPatientDecisions()
+        {
+            // given
+            List<Decision> randomDecisions = CreateRandomDecisions();
+            List<Decision> expectedDecisions = randomDecisions.DeepClone();
+
+            this.decisionOrchestrationServiceMock
+                .Setup(orchestration => orchestration.GetPatientDecisions())
+                .ReturnsAsync(expectedDecisions);
+
+            // when
+            List<Decision> actualDecisions = await this.iDecideClient.GetPatientDecisions();
+
+            // then
+            actualDecisions.Should().NotBeNullOrEmpty();
+            actualDecisions.Should().BeEquivalentTo(expectedDecisions);
+
+            this.decisionOrchestrationServiceMock.Verify(service =>
+                service.GetPatientDecisions(),
+                    Times.Once);
+        }
+    }
+}
---
+++ LHDS.Core.Tests.Unit/Clients/IDecide/IDecideClientTests.cs
@@ -0,0 +1,79 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using LHDS.Core.Clients;
+using LHDS.Core.Models.Foundations.Decisions;
+using LHDS.Core.Models.Foundations.DecisionTypes;
+using LHDS.Core.Models.Foundations.Patients;
+using LHDS.Core.Services.Orchestrations.Decisions;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace LHDS.Core.Tests.Unit.Clients.IDecide
+{
+    public partial class IDecideClientTests
+    {
+        private readonly Mock<IDecisionOrchestrationService> decisionOrchestrationServiceMock;
+        private readonly IIDecideClient iDecideClient;
+
+        public IDecideClientTests()
+        {
+            this.decisionOrchestrationServiceMock = new Mock<IDecisionOrchestrationService>();
+
+            this.iDecideClient = new IDecideClient(
+                decisionOrchestrationService: this.decisionOrchestrationServiceMock.Object);
+        }
+
+        private static string GetRandomStringWithLengthOf(int length)
+        {
+            string result = new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
+            return result.Length > length ? result.Substring(0, length) : result;
+        }
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static List<Decision> CreateRandomDecisions()
+        {
+            return CreateDecisionFiller(dateTimeOffset: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber())
+                .ToList();
+        }
+
+        private static Filler<Decision> CreateDecisionFiller(DateTimeOffset dateTimeOffset, string userId = "")
+        {
+            userId = string.IsNullOrEmpty(userId) ? Guid.NewGuid().ToString() : userId;
+
+            DecisionType decisionType = new()
+            {
+                Id = Guid.NewGuid()
+            };
+
+            Patient patient = new()
+            {
+                Id = Guid.NewGuid()
+            };
+
+            var filler = new Filler<Decision>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(decision => decision.DecisionChoice).Use(GetRandomStringWithLengthOf(255))
+                .OnProperty(decision => decision.CreatedBy).Use(userId)
+                .OnProperty(decision => decision.UpdatedBy).Use(userId)
+                .OnProperty(decision => decision.DecisionType).Use(decisionType)
+                .OnProperty(decision => decision.Patient).Use(patient);
+
+            return filler;
+        }
+    }
+}

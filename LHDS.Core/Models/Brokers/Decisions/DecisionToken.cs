127ced302362498c9a31f47c979702deb7c10948
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-02 12:08:57 +0100

    BROKERS: Decision GetToken

---
+++ LHDS.Core/Models/Brokers/Decisions/DecisionToken.cs
@@ -0,0 +1,29 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using Newtonsoft.Json;
+
+namespace LHDS.Core.Models.Brokers.Decisions
+{
+    public class DecisionToken
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; } = string.Empty;
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; } = "Bearer";
+
+        [JsonProperty("not-before-policy")]
+        public int NotBeforePolicy { get; set; }
+
+        [JsonProperty("scope")]
+        public string Scope { get; set; } = string.Empty;
+
+        public DateTimeOffset ExpiresAt { get; set; } = DateTimeOffset.UtcNow;
+    }
+}

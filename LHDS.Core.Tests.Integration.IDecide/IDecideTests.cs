96da404921699b65f3098b96950adac006eab62e
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-22 09:42:51 +0100

    INTEGRATIONS: IDecide GetPatientDecisions

---
+++ LHDS.Core.Tests.Integration.IDecide/IDecideTests.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Security.Claims;
+using System.Security.Principal;
+using LHDS.Core.Brokers.Storages.Sql;
+using LHDS.Core.Clients;
+using LHDS.Core.Clients.Extensions;
+using LHDS.Core.Models.Brokers.Storages.Blobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace LHDS.Core.Tests.Integration.IDecide
+{
+    public partial class IDecideTests
+    {
+        private readonly IIDecideClient iDecideClient;
+        private readonly IStorageBroker storageBroker;
+        private readonly BlobContainers blobContainers;
+        private readonly ITestOutputHelper output;
+
+        public IDecideTests(ITestOutputHelper output)
+        {
+            this.output = output;
+            var environmentName = "Development";
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            IConfiguration configuration = configurationBuilder.Build();
+            var windowsIdentity = WindowsIdentity.GetCurrent();
+            var claimsPrincipal = new ClaimsPrincipal(windowsIdentity);
+
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(builder =>
+                {
+                    builder.AddConsole();
+                    builder.AddApplicationInsights();
+                })
+                .AddDbContextFactory<StorageBroker>()
+                .AddIDecideClient(configuration, claimsPrincipal)
+                .BuildServiceProvider();
+
+            this.storageBroker = serviceProvider.GetService<StorageBroker>();
+            this.blobContainers = serviceProvider.GetService<BlobContainers>();
+            iDecideClient = serviceProvider.GetService<IIDecideClient>();
+        }
+    }
+}

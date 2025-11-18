ec85a2664c5f52f8b07e6cbe4fd25d43b01dac2a
davidhayes03 <david.hayes17@nhs.net>
2025-11-18 10:49:32 +0000

    CODE RUB: Start to Fix

---
+++ LHDS.Core/Models/Foundations/OptOuts/OptOutSummary.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using CsvHelper.Configuration.Attributes;
+
+namespace LHDS.Core.Models.Foundations.OptOuts
+{
+    public class OptOutSummary
+    {
+        [Name("Id")]
+        public Guid Id { get; set; }
+        [Name("NHSNo")]
+        public string NhsNumber { get; set; } 
+    }
+}
--- LHDS.Core/Services/Processings/OptOuts/IOptOutProcessingService.cs
+++ LHDS.Core/Services/Processings/OptOuts/IOptOutProcessingService.cs
@@ -18,7 +18,7 @@ namespace LHDS.Core.Services.Processings.OptOuts
         ValueTask<OptOut> RemoveOptOutByIdAsync(Guid optOutId);
         ValueTask<OptOut> RetrieveOptOutByIdAsync(Guid optOutId);
         ValueTask<OptOut?> RetrieveOptOutByNhsNumberAsync(string optOutNhsNumber);
-        ValueTask<List<OptOut>> RetrieveAllExpiredOptOutsAsync(int olderThanDays);
+        ValueTask<List<OptOutSummary>> RetrieveAllExpiredOptOutsAsync(int olderThanDays);
         ValueTask<List<OptOut>> RetrieveAllOptOutsByBatchReferenceAsync(string batchReference);
 
         ValueTask<List<OptOut>> ConsolidateOptOutChangesAndReturnChangesOnly(

9f913bd0aabbbfc66c35bd2575eaeac6b6e3683d
Hassan Raza <h_raza110@outlook.com>
2026-01-04 17:45:18 +0000

    CODE RUB: Opt Out Service Security Audit Broker Changes

--- LHDS.Core/Services/Foundations/OptOuts/OptOutService.cs
+++ LHDS.Core/Services/Foundations/OptOuts/OptOutService.cs
@@ -17,25 +17,27 @@ namespace LHDS.Core.Services.Foundations.OptOuts
     {
         private readonly IStorageBroker storageBroker;
         private readonly IDateTimeBroker dateTimeBroker;
-        private readonly ISecurityBroker securityBroker;
+        private readonly ISecurityAuditBroker securityAuditBroker;
         private readonly ILoggingBroker loggingBroker;
 
         public OptOutService(
             IStorageBroker storageBroker,
             IDateTimeBroker dateTimeBroker,
-            ISecurityBroker securityBroker,
+            ISecurityAuditBroker securityAuditBroker,
             ILoggingBroker loggingBroker)
         {
             this.storageBroker = storageBroker;
             this.dateTimeBroker = dateTimeBroker;
-            this.securityBroker = securityBroker;
+            this.securityAuditBroker = securityAuditBroker;
             this.loggingBroker = loggingBroker;
         }
 
         public ValueTask<OptOut> AddOptOutAsync(OptOut optOut) =>
             TryCatch(async () =>
             {
-                OptOut optOutWithAddAuditApplied = await ApplyAddOptOutAsync(optOut);
+                OptOut optOutWithAddAuditApplied =
+                    await this.securityAuditBroker.ApplyAddAuditValuesAsync(optOut);
+
                 await ValidateOptOutOnAddAsync(optOutWithAddAuditApplied);
 
                 return await this.storageBroker.InsertOptOutAsync(optOutWithAddAuditApplied);
@@ -60,7 +62,9 @@ namespace LHDS.Core.Services.Foundations.OptOuts
         public ValueTask<OptOut> ModifyOptOutAsync(OptOut optOut) =>
             TryCatch(async () =>
             {
-                OptOut optOutWithModifyAuditApplied = await ApplyModifyAuditAsync(optOut);
+                OptOut optOutWithModifyAuditApplied =
+                    await this.securityAuditBroker.ApplyModifyAuditValuesAsync(optOut);
+
                 await ValidateOptOutOnModifyAsync(optOutWithModifyAuditApplied);
 
                 OptOut maybeOptOut =
@@ -84,29 +88,5 @@ namespace LHDS.Core.Services.Foundations.OptOuts
 
                 return await this.storageBroker.DeleteOptOutAsync(maybeOptOut);
             });
-
-        virtual internal async ValueTask<OptOut> ApplyAddOptOutAsync(OptOut optOut)
-        {
-            ValidateOptOutIsNotNull(optOut);
-            var auditDateTimeOffset = await this.dateTimeBroker.GetCurrentDateTimeOffsetAsync();
-            var auditUser = await this.securityBroker.GetCurrentUserAsync();
-            optOut.CreatedBy = auditUser?.EntraUserId.ToString() ?? string.Empty;
-            optOut.CreatedDate = auditDateTimeOffset;
-            optOut.UpdatedBy = auditUser?.EntraUserId.ToString() ?? string.Empty;
-            optOut.UpdatedDate = auditDateTimeOffset;
-
-            return optOut;
-        }
-
-        virtual internal async ValueTask<OptOut> ApplyModifyAuditAsync(OptOut optOut)
-        {
-            ValidateOptOutIsNotNull(optOut);
-            var auditDateTimeOffset = await this.dateTimeBroker.GetCurrentDateTimeOffsetAsync();
-            var auditUser = await this.securityBroker.GetCurrentUserAsync();
-            optOut.UpdatedBy = auditUser?.EntraUserId.ToString() ?? string.Empty;
-            optOut.UpdatedDate = auditDateTimeOffset;
-
-            return optOut;
-        }
     }
 }

5f5ff36a5a376bb045a2d489a386ab031bde0a0f
davidhayes03 <david.hayes17@nhs.net>
2026-02-18 12:29:32 +0000

    CODE RUB: Add Config

--- LHDS.Functions.Landings.Tpp/TppLandingFunction.cs
+++ LHDS.Functions.Landings.Tpp/TppLandingFunction.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using LHDS.Core.Brokers.Loggings;
 using LHDS.Core.Clients;
 using LHDS.Core.Models.Orchestrations.EmisLandings;
+using LHDS.Core.Services.Foundations.FileNameValidations;
 using Microsoft.Azure.Functions.Worker;
 
 namespace LHDS.Functions.Landings.Tpp
@@ -17,15 +18,19 @@ namespace LHDS.Functions.Landings.Tpp
         private readonly ILoggingBroker loggingBroker;
         private readonly ITppLandingClient tppLandingClient;
         private readonly LandingConfiguration landingConfiguration;
+        private readonly IFileNameValidationService fileNameValidationService;
+
 
         public TppLandingFunction(
             ILoggingBroker loggingBroker,
             ITppLandingClient tppLandingClient,
-            LandingConfiguration landingConfiguration)
+            LandingConfiguration landingConfiguration,
+            IFileNameValidationService fileNameValidationService)
         {
             this.loggingBroker = loggingBroker;
             this.tppLandingClient = tppLandingClient;
             this.landingConfiguration = landingConfiguration;
+            this.fileNameValidationService = fileNameValidationService;
         }
 
         [Function("TppLandingFunction")]
@@ -34,23 +39,23 @@ namespace LHDS.Functions.Landings.Tpp
         {
             try
             {
-            //    bool shouldProcess =
-            //        await this.fileNameValidationService.ShouldProcessFileAsync(
-            //            fileName: name,
-            //            includePattern: landingConfiguration.FileNameIncludePattern,
-            //            excludePattern: landingConfiguration.FileNameExcludePattern);
+                bool shouldProcess =
+                    await this.fileNameValidationService.ShouldProcessFileAsync(
+                        fileName: name,
+                        includePattern: landingConfiguration.FileNameIncludePattern,
+                        excludePattern: landingConfiguration.FileNameExcludePattern);
 
-                //string action
-                //    = shouldProcess ? "PROCESSING" : "SKIPPING";
+                string action
+                    = shouldProcess ? "PROCESSING" : "SKIPPING";
 
-                //await this.loggingBroker.LogInformationAsync(
-                //    $"C# Blob trigger function {action} document\n " +
-                //    $"Name: FileName: {name}");
+                await this.loggingBroker.LogInformationAsync(
+                    $"C# Blob trigger function {action} document\n " +
+                    $"Name: FileName: {name}");
 
-                //if (!shouldProcess)
-                //{
-                //    return;
-                //}
+                if (!shouldProcess)
+                {
+                    return;
+                }
 
                 await tppLandingClient.ProcessAsync(
                     fileName: name,

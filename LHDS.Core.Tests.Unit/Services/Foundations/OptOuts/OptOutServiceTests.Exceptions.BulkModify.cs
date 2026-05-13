eae174864289e56609d92759b386625171d50b61
Christo du Toit <christo@dutoit.co.uk>
2026-05-13 23:31:32 +0100

    ShouldThrowAggregateServiceExceptionOnBulkModifyIfErrorOccursAndLogItAsync -> PASS; ShouldThrowServiceExceptionOnBulkModifyIfServiceErrorOccursAndLogItAsync -> PASS

---
+++ LHDS.Core.Tests.Unit/Services/Foundations/OptOuts/OptOutServiceTests.Exceptions.BulkModify.cs
@@ -0,0 +1,170 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LHDS.Core.Models.Foundations.OptOuts;
+using LHDS.Core.Models.Foundations.OptOuts.Exceptions;
+using LHDS.Core.Services.Foundations.OptOuts;
+using Moq;
+using Xunit;
+
+namespace LHDS.Core.Tests.Unit.Services.Foundations.OptOuts
+{
+    public partial class OptOutServiceTests
+    {
+        [Fact]
+        public async Task
+            ShouldThrowAggregateServiceExceptionOnBulkModifyIfErrorOccursAndLogItAsync()
+        {
+            // given
+            int randomCount = GetRandomNumber();
+            string someFileName = GetRandomString();
+            List<OptOut> someOptOuts = CreateRandomOptOuts(
+                count: randomCount,
+                dateTimeOffset: GetRandomDateTimeOffset(),
+                userId: GetRandomString());
+
+            Exception exception = new Exception("Some exception");
+
+            var aggregateException =
+                new AggregateException(
+                    message: $"Unable to process optOuts in 1 of the"
+                        + $" batch(es) from {someFileName}",
+                    innerExceptions: exception);
+
+            var failedOptOutServiceException =
+                new FailedOptOutServiceException(
+                    message: "Failed aggregate optOut service error"
+                        + " occurred, please contact support.",
+                    innerException: aggregateException);
+
+            var expectedOptOutServiceException =
+                new OptOutServiceException(
+                    message: "OptOut service error occurred,"
+                        + " please contact support.",
+                    innerException: failedOptOutServiceException);
+
+            var optOutServiceMock = new Mock<OptOutService>(
+                this.storageBrokerMock.Object,
+                this.dateTimeBrokerMock.Object,
+                this.securityAuditBrokerMock.Object,
+                this.identifierBrokerMock.Object,
+                this.loggingBrokerMock.Object,
+                this.auditBrokerMock.Object)
+            {
+                CallBase = true
+            };
+
+            optOutServiceMock.Setup(service =>
+                service.BulkAddOrModifyBatchAsync(
+                    It.IsAny<List<OptOut>>(),
+                    It.IsAny<string>(),
+                    It.IsAny<int>()))
+                        .ThrowsAsync(aggregateException);
+
+            // when
+            ValueTask bulkModifyOptOutTask = optOutServiceMock.Object
+                .BulkModifyOptOutsAsync(
+                    optOuts: someOptOuts,
+                    fileName: someFileName);
+
+            OptOutServiceException actualOptOutServiceException =
+                await Assert.ThrowsAsync<OptOutServiceException>(
+                    bulkModifyOptOutTask.AsTask);
+
+            // then
+            actualOptOutServiceException.Should()
+                .BeEquivalentTo(expectedOptOutServiceException);
+
+            optOutServiceMock.Verify(service =>
+                service.BulkAddOrModifyBatchAsync(
+                    It.IsAny<List<OptOut>>(),
+                    It.IsAny<string>(),
+                    It.IsAny<int>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedOptOutServiceException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task
+            ShouldThrowServiceExceptionOnBulkModifyIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            var serviceException = new Exception();
+
+            var optOutServiceMock = new Mock<OptOutService>(
+                this.storageBrokerMock.Object,
+                this.dateTimeBrokerMock.Object,
+                this.securityAuditBrokerMock.Object,
+                this.identifierBrokerMock.Object,
+                this.loggingBrokerMock.Object,
+                this.auditBrokerMock.Object)
+            {
+                CallBase = true
+            };
+
+            optOutServiceMock.Setup(service =>
+                service.BulkAddOrModifyBatchAsync(
+                    It.IsAny<List<OptOut>>(),
+                    It.IsAny<string>(),
+                    It.IsAny<int>()))
+                        .ThrowsAsync(serviceException);
+
+            int randomCount = GetRandomNumber();
+            string someFileName = GetRandomString();
+
+            List<OptOut> someOptOuts = CreateRandomOptOuts(
+                count: randomCount,
+                dateTimeOffset: GetRandomDateTimeOffset(),
+                userId: GetRandomString());
+
+            var failedOptOutServiceException =
+                new FailedOptOutServiceException(
+                    message: "Failed optOut service error occurred,"
+                        + " please contact support.",
+                    innerException: serviceException);
+
+            var expectedOptOutServiceException =
+                new OptOutServiceException(
+                    message: "OptOut service error occurred,"
+                        + " please contact support.",
+                    innerException: failedOptOutServiceException);
+
+            // when
+            ValueTask modifyOptOutTask = optOutServiceMock.Object
+                .BulkModifyOptOutsAsync(
+                    optOuts: someOptOuts,
+                    fileName: someFileName);
+
+            OptOutServiceException actualOptOutServiceException =
+                await Assert.ThrowsAsync<OptOutServiceException>(
+                    modifyOptOutTask.AsTask);
+
+            // then
+            actualOptOutServiceException.Should()
+                .BeEquivalentTo(expectedOptOutServiceException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedOptOutServiceException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

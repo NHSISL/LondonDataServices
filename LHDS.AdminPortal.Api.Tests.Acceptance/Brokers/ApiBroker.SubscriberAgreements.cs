0dd963b92b2364b4413e19f5f64c6679992bf06e
hassaero110 <h_raza110@outlook.com>
2026-06-23 09:57:13 +0100

    API: Broker Subscriber Agreement Paging

--- LHDS.AdminPortal.Api.Tests.Acceptance/Brokers/ApiBroker.SubscriberAgreements.cs
+++ LHDS.AdminPortal.Api.Tests.Acceptance/Brokers/ApiBroker.SubscriberAgreements.cs
@@ -13,6 +13,7 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Brokers
     public partial class ApiBroker
     {
         private const string SubscriberAgreementsRelativeUrl = "api/subscriberAgreements";
+        private const string subscriberAgreementsRelativeOdataUrl = "odata/subscriberAgreements";
 
         public async ValueTask<SubscriberAgreement> PostSubscriberAgreementAsync(SubscriberAgreement subscriberAgreement) =>
             await this.apiFactoryClient.PostContentAsync(SubscriberAgreementsRelativeUrl, subscriberAgreement);
@@ -20,8 +21,25 @@ namespace LHDS.AdminPortal.Api.Tests.Acceptance.Brokers
         public async ValueTask<SubscriberAgreement> GetSubscriberAgreementByIdAsync(Guid subscriberAgreementId) =>
             await this.apiFactoryClient.GetContentAsync<SubscriberAgreement>($"{SubscriberAgreementsRelativeUrl}/{subscriberAgreementId}");
 
-        public async ValueTask<List<SubscriberAgreement>> GetAllSubscriberAgreementsAsync() =>
-          await this.apiFactoryClient.GetContentAsync<List<SubscriberAgreement>>($"{SubscriberAgreementsRelativeUrl}/");
+        public async ValueTask<List<SubscriberAgreement>> GetAllSubscriberAgreementsAsync()
+        {
+            var allItems = new List<SubscriberAgreement>();
+            string? nextUrl = $"{subscriberAgreementsRelativeOdataUrl}/";
+
+            while (nextUrl is not null)
+            {
+                OdataResponse<SubscriberAgreement> response =
+                    await this.GetOdataResponseAsync<SubscriberAgreement>(nextUrl);
+
+                allItems.AddRange(response.Items);
+
+                nextUrl = response.NextLink is not null
+                    ? new Uri(response.NextLink).PathAndQuery.TrimStart('/')
+                    : null;
+            }
+
+            return allItems;
+        }
 
         public async ValueTask<OdataResponse<SubscriberAgreement>> GetSubscriberAgreementsOdataResponseAsync(
             string relativeOdataUrl) =>
